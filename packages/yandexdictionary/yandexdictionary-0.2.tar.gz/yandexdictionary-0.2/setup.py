5f4df130ef8d1436c9b1a9f319e82ff4030930e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:03 +0000

    Add package yandexdictionary/0.2/yandexdictionary-0.2.tar.gz

---
+++ packages/yandexdictionary/yandexdictionary-0.2.tar.gz/yandexdictionary-0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'yandexdictionary',
+    version = '0.2',
+    py_modules = ['yandexdictionary'],
+    author = 'nimit_s',
+    author_email = 'nimit.web@gmail.com',
+    url = 'http://www.example.com',
+    description = 'A simple dictionary rest client',
+    )
