7589511fa286b48e4c615faa56c5e065b41d7dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:58 +0000

    Add package zipstream/1.1.4/zipstream-1.1.4.tar.gz

---
+++ packages/zipstream/zipstream-1.1.4.tar.gz/zipstream-1.1.4/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zipstream',
+    version='1.1.4',
+    description='Zipfile generator',
+    author='Allan Lei',
+    author_email='allanlei@helveticode.com',
+    url='https://github.com/allanlei/python-zipstream',
+    packages=find_packages(exclude=['tests']),
+    keywords='zip streaming',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
