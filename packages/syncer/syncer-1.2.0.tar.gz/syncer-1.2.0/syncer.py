4c1fce0318eae5ad8fa4b46a0f866e033d11b615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:09 +0000

    Add package syncer/1.2.0/syncer-1.2.0.tar.gz

---
+++ packages/syncer/syncer-1.2.0.tar.gz/syncer-1.2.0/syncer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import functools
+import asyncio
+import inspect
+import types
+
+
+PY35 = sys.version_info >= (3, 5)
+
+
+def _is_awaitable(co):
+    if PY35:
+        return inspect.isawaitable(co)
+    else:
+        return (isinstance(co, types.GeneratorType) or
+                isinstance(co, asyncio.Future))
+
+
+@functools.singledispatch
+def sync(co):
+    raise TypeError('Called with unsupported argument: {}'.format(co))
+
+
+@sync.register(asyncio.Future)
+@sync.register(types.GeneratorType)
+def sync_co(co):
+    if not _is_awaitable(co):
+        raise TypeError('Called with unsupported argument: {}'.format(co))
+    return asyncio.get_event_loop().run_until_complete(co)
+
+
+@sync.register(types.FunctionType)
+@sync.register(types.MethodType)
+def sync_fu(f):
+    if not asyncio.iscoroutinefunction(f):
+        raise TypeError('Called with unsupported argument: {}'.format(f))
+
+    @functools.wraps(f)
+    def run(*args, **kwargs):
+        return asyncio.get_event_loop().run_until_complete(f(*args, **kwargs))
+    return run
+
+
+if PY35:
+    sync.register(types.CoroutineType)(sync_co)
