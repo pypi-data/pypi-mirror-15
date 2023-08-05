1650abb6a49c1a375a05d6ddc22fe83ec82d7863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:26 +0000

    Add package python-stack/0.0.4.1/python-stack-0.0.4.1.tar.gz

---
+++ packages/python-stack/python-stack-0.0.4.1.tar.gz/python-stack-0.0.4.1/setup.py
@@ -0,0 +1,50 @@
+# coding:utf8
+
+try:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+except:
+    pass
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+with open('requirements.txt', 'r') as f:
+    requires = [x.strip() for x in f if x.strip()]
+
+with open('Readme.md', 'r') as f:
+    readme = f.read()
+
+
+setup(
+    name='python-stack',
+    version='0.0.4.1',
+    url='www.darwintree.org',
+    description='`stack` is a Python version of [stack](http://docs.haskellstack.org/en/stable/README/),',
+    author='Ryan Kung',
+    author_email='ryankung@ieee.org;kongzhen@xunlei.com',
+    license='MIT',
+    long_description=readme,
+    packages=find_packages(exclude=['tests']),
+    package_dir={'': '.'},
+    tests_require=['nose'],
+    install_requires=requires,
+    test_suite='nose.collector',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        "Programming Language :: Python :: 3.5",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    include_package_data=True,
+    entry_points={'console_scripts': [
+        'stack = stack.main:main',
+        'scaffold = scaffold.main:main',
+        'pystack = stack.main:main'
+    ]},
+
+)
