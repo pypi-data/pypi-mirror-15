06007f8ed436e09e384aac1680730ca5483d8562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:37 +0000

    Add package abilian-core/0.6.0/abilian-core-0.6.0.tar.gz

---
+++ packages/abilian-core/abilian-core-0.6.0.tar.gz/abilian-core-0.6.0/abilian/core/extensions/login.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+"""
+"""
+from __future__ import absolute_import, division, print_function
+
+from flask import current_app
+from flask_login import AnonymousUserMixin, LoginManager
+
+
+class AnonymousUser(AnonymousUserMixin):
+
+    def has_role(self, role):
+        return current_app.services['security'].has_role(self, role)
+
+    @property
+    def groups(self):
+        return set()
+
+
+login_manager = LoginManager()
+login_manager.anonymous_user = AnonymousUser
---
+++ packages/abilian-core/abilian-core-0.6.0.tar.gz/abilian-core-0.6.0/abilian/core/tests/dummy.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, division, print_function
+
+from sqlalchemy import Column, Text, UnicodeText
+
+from abilian.core.entities import SEARCHABLE, Entity
+
+
+class DummyContact(Entity):
+    salutation = Column(UnicodeText, default=u"")
+    first_name = Column(UnicodeText, default=u"", info=SEARCHABLE)
+    last_name = Column(UnicodeText, default=u"", info=SEARCHABLE)
+    email = Column(Text, default=u"")
