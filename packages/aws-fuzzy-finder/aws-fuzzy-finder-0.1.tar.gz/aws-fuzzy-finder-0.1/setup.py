ff9f32780a03f788e526d4265a51f23953ce9a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package aws-fuzzy-finder/0.1/aws-fuzzy-finder-0.1.tar.gz

---
+++ packages/aws-fuzzy-finder/aws-fuzzy-finder-0.1.tar.gz/aws-fuzzy-finder-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='aws-fuzzy-finder',
+    version='0.1',
+    url='https://github.com/pmazurek/aws-fuzzy-finder',
+    description='SSH into AWS instances using fuzzy search through tags.',
+    download_url='https://github.com/pmazurek/aws-fuzzy-finder/tarball/0.1',
+    author='Piotr Mazurek, Daria Rudkiewicz',
+    keywords=['aws', 'ssh', 'fuzzy', 'ec2'],
+    packages=['aws_fuzzy_finder'],
+    install_requires=[
+        'boto3==1.3.1'
+    ],
+    entry_points=dict(
+        console_scripts=[
+            'aws-fuzzy = aws_fuzzy_finder.main:entrypoint',
+        ]
+    )
+)
