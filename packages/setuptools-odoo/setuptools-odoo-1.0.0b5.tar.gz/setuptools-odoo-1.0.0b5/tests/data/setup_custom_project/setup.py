0487004b410c97232a2c2c76039fee025721121b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:50 +0000

    Add package setuptools-odoo/1.0.0b5/setuptools-odoo-1.0.0b5.tar.gz

---
+++ packages/setuptools-odoo/setuptools-odoo-1.0.0b5.tar.gz/setuptools-odoo-1.0.0b5/tests/data/setup_custom_project/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='test-custom-project',
+    version='0.9.0',
+    setup_requires=['setuptools-odoo'],
+    odoo_addons=True,
+    # force zip_safe which would otherwise be set to False via odoo_addons=True
+    zip_safe=True,
+)
