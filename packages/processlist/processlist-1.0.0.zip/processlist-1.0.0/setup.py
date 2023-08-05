e25f3b2bc7905327b8f6c30283a79ca4d7daf51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package processlist/1.0.0/processlist-1.0.0.zip

---
+++ packages/processlist/processlist-1.0.0.zip/processlist-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'processlist',
+	version = '1.0.0',
+	py_modules = ['processlist'],
+	author = 'caparniyazi',
+	author_email = 'capar.niyazi@gmail.com',
+	url = 'niyazi-capar.appspot.com',
+	description = 'A simple printer of nested lists',
+)
