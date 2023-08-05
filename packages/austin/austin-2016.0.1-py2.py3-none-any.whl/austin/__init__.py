dadf9c0f01d295dd8f1e11460486624c5aa1c3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:29 +0000

    Add package austin/2016.0.1/austin-2016.0.1-py2.py3-none-any.whl

---
+++ packages/austin/austin-2016.0.1-py2.py3-none-any.whl/austin/__init__.py
@@ -0,0 +1,9 @@
+"""
+austin scipy package
+
+see scipy 2015 tshirt
+"""
+
+__version__ = '2016.0.1'
+
+from . import scipy
---
+++ packages/austin/austin-2016.0.1-py2.py3-none-any.whl/austin/scipy.py
@@ -0,0 +1,28 @@
+import datetime as _datetime
+
+date = _datetime.date(2016, 7, 6)
+
+url = "http://scipy2016.scipy.org/"
+
+code_of_conduct = "http://scipy2016.scipy.org/ehome/146062/388087/"
+
+location = "AT&T Executive Education and Conference Center, Austin, TX, USA"
+climate = "Humid subtropical"
+
+sections = [
+    {'name': 'tutorials',
+     'start': _datetime.date(2016, 7, 11),
+     'end': _datetime.date(2016, 7, 12),
+     'url': 'http://scipy2016.scipy.org/ehome/146062/332960/',
+    },
+    {'name': 'talks',
+     'start': _datetime.date(2016, 7, 13),
+     'end': _datetime.date(2016, 7, 15),
+     'url': 'http://scipy2016.scipy.org/ehome/146062/332968/',
+    },
+    {'name': 'sprints',
+     'start': _datetime.date(2016, 7, 16),
+     'end': _datetime.date(2016, 7, 17),
+     'url': 'http://scipy2016.scipy.org/ehome/146062/332969/',
+    },
+]
