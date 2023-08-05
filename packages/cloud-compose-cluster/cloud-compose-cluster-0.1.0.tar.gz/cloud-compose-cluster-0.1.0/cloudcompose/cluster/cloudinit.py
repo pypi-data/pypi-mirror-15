51e49332f1a4d2e8ad652aa5d55a1b09ac7bfae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package cloud-compose-cluster/0.1.0/cloud-compose-cluster-0.1.0.tar.gz

---
+++ packages/cloud-compose-cluster/cloud-compose-cluster-0.1.0.tar.gz/cloud-compose-cluster-0.1.0/cloudcompose/cluster/cloudinit.py
@@ -0,0 +1,29 @@
+from cloudcompose.template import Template
+from cloudcompose.cluster.dockercompose import DockerCompose
+from os.path import join
+from pprint import pprint
+
+class CloudInit():
+    def __init__(self, cloud_config):
+        self.cloud_config = cloud_config
+
+    def build(self, **kwargs):
+        config_data, config_dir = self.cloud_config.config_data('cluster')
+
+        for key, value in kwargs.iteritems():
+            config_data['_' + key] = value
+
+        template_dir = join(config_dir, 'templates')
+        self._add_docker_compose(template_dir, config_data)
+        return self._render_template(template_dir, config_data['template'], config_data)
+
+    def _add_docker_compose(self, template_dir, config_data):
+        docker_compose = DockerCompose()
+        docker_compose, docker_compose_override = docker_compose.yaml_files(config_data)
+        config_data['docker_compose'] = {}
+        config_data['docker_compose']['yaml'] = docker_compose
+        config_data['docker_compose']['override_yaml'] = docker_compose_override
+
+    def _render_template(self, template_dir, template_file, template_data):
+        template = Template()
+        return template.render(join(template_dir, template_file), template_data)
