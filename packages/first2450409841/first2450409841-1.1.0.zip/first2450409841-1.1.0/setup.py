2b47799f3fe527dd30f9693eb8e8e50896d89ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:40 +0000

    Add package first2450409841/1.1.0/first2450409841-1.1.0.zip

---
+++ packages/first2450409841/first2450409841-1.1.0.zip/first2450409841-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='first2450409841',
+	version='1.1.0',
+	py_modules=['nester'],
+	author = 'hfpython',
+	author_email='hypython@headfirstlabs.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nested lists',
+	)
