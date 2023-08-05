41d8bfdc8d4cc70dc6b4ae0adc830d18e90370a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:51 +0000

    Add package arctic/1.23.0/arctic-1.23.0.tar.gz

---
+++ packages/arctic/arctic-1.23.0.tar.gz/arctic-1.23.0/tests/unit/date/test_mktz.py
@@ -0,0 +1,45 @@
+from datetime import datetime as dt
+from mock import patch
+from pytest import raises
+
+from arctic.date import mktz, TimezoneError
+from arctic.date._mktz import DEFAULT_TIME_ZONE_NAME, tzfile, TIME_ZONE_DATA_SOURCE
+
+
+def test_mktz():
+    tz = mktz("Europe/London")
+    d = dt(2012, 2, 2, tzinfo=tz)
+    assert d.tzname() == 'GMT'
+    d = dt(2012, 7, 2, tzinfo=tz)
+    assert d.tzname() == 'BST'
+
+    tz = mktz('UTC')
+    d = dt(2012, 2, 2, tzinfo=tz)
+    assert d.tzname() == 'UTC'
+    d = dt(2012, 7, 2, tzinfo=tz)
+    assert d.tzname() == 'UTC'  # --------replace_empty_timezones_with_default -----------------
+
+
+def test_mktz_noarg():
+    tz = mktz()
+    assert DEFAULT_TIME_ZONE_NAME in str(tz)
+
+
+def test_mktz_zone():
+    tz = mktz('UTC')
+    assert tz.zone == "UTC"
+    tz = mktz('/usr/share/zoneinfo/UTC')
+    assert tz.zone == "UTC"
+
+
+def test_mktz_fails_if_invalid_timezone():
+    with patch('os.path.exists') as file_exists:
+        file_exists.return_value = False
+        with raises(TimezoneError):
+            mktz('junk')
+
+
+def test_tzfile_raises():
+    t = tzfile(TIME_ZONE_DATA_SOURCE + DEFAULT_TIME_ZONE_NAME)
+    with raises(ValueError):
+        t._find_ttinfo(None) 
