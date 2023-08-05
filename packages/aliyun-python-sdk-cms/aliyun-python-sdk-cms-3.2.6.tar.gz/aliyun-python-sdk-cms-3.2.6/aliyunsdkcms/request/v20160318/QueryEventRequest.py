8eb381d47c4d45fed58a4dbc1f57b765c12d51e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package aliyun-python-sdk-cms/3.2.6/aliyun-python-sdk-cms-3.2.6.tar.gz

---
+++ packages/aliyun-python-sdk-cms/aliyun-python-sdk-cms-3.2.6.tar.gz/aliyun-python-sdk-cms-3.2.6/aliyunsdkcms/request/v20160318/QueryEventRequest.py
@@ -0,0 +1,84 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class QueryEventRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Cms', '2016-03-18', 'QueryEvent')
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_callby_cms_owner(self):
+		return self.get_query_params().get('callby_cms_owner')
+
+	def set_callby_cms_owner(self,callby_cms_owner):
+		self.add_query_param('callby_cms_owner',callby_cms_owner)
+
+	def get_Project(self):
+		return self.get_query_params().get('Project')
+
+	def set_Project(self,Project):
+		self.add_query_param('Project',Project)
+
+	def get_EventName(self):
+		return self.get_query_params().get('EventName')
+
+	def set_EventName(self,EventName):
+		self.add_query_param('EventName',EventName)
+
+	def get_StartTime(self):
+		return self.get_query_params().get('StartTime')
+
+	def set_StartTime(self,StartTime):
+		self.add_query_param('StartTime',StartTime)
+
+	def get_EndTime(self):
+		return self.get_query_params().get('EndTime')
+
+	def set_EndTime(self,EndTime):
+		self.add_query_param('EndTime',EndTime)
+
+	def get_Dimensions(self):
+		return self.get_query_params().get('Dimensions')
+
+	def set_Dimensions(self,Dimensions):
+		self.add_query_param('Dimensions',Dimensions)
+
+	def get_Page(self):
+		return self.get_query_params().get('Page')
+
+	def set_Page(self,Page):
+		self.add_query_param('Page',Page)
+
+	def get_Length(self):
+		return self.get_query_params().get('Length')
+
+	def set_Length(self,Length):
+		self.add_query_param('Length',Length)
+
+	def get_Filter(self):
+		return self.get_query_params().get('Filter')
+
+	def set_Filter(self,Filter):
+		self.add_query_param('Filter',Filter)
