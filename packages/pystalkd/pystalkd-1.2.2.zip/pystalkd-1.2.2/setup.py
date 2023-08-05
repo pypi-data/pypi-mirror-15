aa04c492bd4f83280ea1fd50f970aa7f2c04d61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:50 +0000

    Add package pystalkd/1.2.2/pystalkd-1.2.2.zip

---
+++ packages/pystalkd/pystalkd-1.2.2.zip/pystalkd-1.2.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+long_description = 'See https://github.com/menezes-/pystalkd'
+
+# used to create the description to the pypi page
+if os.path.exists('README.md'):
+    # pandoc --from=markdown --to=rst --output=README.rst README.md
+    import subprocess
+
+    process = subprocess.Popen(['pandoc', '--from=markdown', "--to=rst", "README.md"], stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    long_description = str(out, 'utf8')
+
+setup(
+    name='pystalkd',
+    version='1.2.2',
+    packages=['pystalkd'],
+    url='https://github.com/menezes-/pystalkd',
+    download_url='https://github.com/menezes-/pystalkd/archive/v1.2.2.zip',
+    keywords=['beanstalkd', 'python3', 'bindings'],
+    license='Apache-2.0',
+    author='Gabriel',
+    author_email='gabrielmenezesvi@gmail.com',
+    description='Beanstalkd bindings for python3',
+    long_description=long_description,
+    extras_require={'yaml': ["PyYAML"]}
+)
