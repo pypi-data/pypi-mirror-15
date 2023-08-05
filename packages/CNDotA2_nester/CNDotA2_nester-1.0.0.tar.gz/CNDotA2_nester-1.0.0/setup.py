f319531d5a15c6de7866df0ebca01edc1b8ad966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:27 +0000

    Add package CNDotA2_nester/1.0.0/CNDotA2_nester-1.0.0.tar.gz

---
+++ packages/CNDotA2_nester/CNDotA2_nester-1.0.0.tar.gz/CNDotA2_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'CNDotA2_nester',
+	version = '1.0.0',
+	py_modules = ['nester'],
+	author = 'dong',
+	author_email = '838282876@qq.com',
+	url = 'www.github.com/cndota2',
+	description = 'A simple printer of nested lists',
+)
