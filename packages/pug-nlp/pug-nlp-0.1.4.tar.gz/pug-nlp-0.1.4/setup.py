7caf276c5b1f71b34d34e239803ed91c14992f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package pug-nlp/0.1.4/pug-nlp-0.1.4.tar.gz

---
+++ packages/pug-nlp/pug-nlp-0.1.4.tar.gz/pug-nlp-0.1.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for pug-nlp project (pug.nlp package).
+
+    This file was generated with PyScaffold 2.5.5, a tool that easily
+    puts up a scaffold for your new Python project. Learn more under:
+    http://pyscaffold.readthedocs.org/
+"""
+
+import sys
+from setuptools import setup
+
+
+def setup_package():
+    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+    sphinx = ['sphinx'] if needs_sphinx else []
+    setup(setup_requires=['six', 'setuptools>=20.3.1', 'pyscaffold>=2.5,<2.6a0'] + sphinx,
+          tests_require=['pytest_cov', 'pytest'],
+          use_pyscaffold=True)
+
+
+if __name__ == "__main__":
+    setup_package()
