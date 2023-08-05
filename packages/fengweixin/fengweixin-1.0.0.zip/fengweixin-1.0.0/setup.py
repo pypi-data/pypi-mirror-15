e2d14ca641e8d740eabaac074dac76ba395bb965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:23 +0000

    Add package fengweixin/1.0.0/fengweixin-1.0.0.zip

---
+++ packages/fengweixin/fengweixin-1.0.0.zip/fengweixin-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core  import setup
+setup(name='fengweixin',
+version='1.0.0',
+py_modules=['fengweixin'],
+author='hfpython',
+author_email='hfpython@headfirstlabs.com',
+url='http://www.headfirstlabs.com',
+description='A simple printer of fengweixin lists',)
