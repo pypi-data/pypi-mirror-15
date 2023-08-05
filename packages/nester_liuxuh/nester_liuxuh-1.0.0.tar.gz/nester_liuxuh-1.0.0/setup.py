c770b4f458627fc2023ea4411784baf54cd382c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:44 +0000

    Add package nester_liuxuh/1.0.0/nester_liuxuh-1.0.0.tar.gz

---
+++ packages/nester_liuxuh/nester_liuxuh-1.0.0.tar.gz/nester_liuxuh-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name            ='nester_liuxuh',
+    version         ='1.0.0',
+    py_modules      =['nester_liuxuh'],
+    author          ='liuxuh',
+    author_email    ='846790820@qq.com',
+    url             ='http://www.headfirstlabs.com',
+    description     ='A simple printer of nested lists',
+    )
+    
+    
