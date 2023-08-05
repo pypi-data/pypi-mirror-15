330be5348e51651f53ebb02002086faf83466975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package controlpy/0.1/controlpy-0.1.zip

---
+++ packages/controlpy/controlpy-0.1.zip/controlpy-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='controlpy',
+      version='0.1',
+      description='Python control library',
+      url='http://github.com/markwmuller/controlpy',
+      download_url = 'https://github.com/markwmuller/controlpy/archive/0.1',
+      author='Mark W. Mueller',
+      author_email='mwm@mwm.im',
+      license='GPL V3',
+      packages=['controlpy'],
+      zip_safe=False,
+      classifiers=[],
+      keywords='control lqr robust H2 Hinf Hinfinity',
+      )
