7ed54b71593a3699942440fd382b9a7062ff5104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:48 +0000

    Add package lybp/0.1.0/lybp-0.1.0.tar.gz

---
+++ packages/lybp/lybp-0.1.0.tar.gz/lybp-0.1.0/setup.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+setup(
+    name='lybp',
+    version=VERSION,
+    description='a simple test',
+    license='MIT',
+    author='pypi060104',
+    author_email='uijsj1102@163.com',
+    packages=['lybp'],
+    install_requires=[],
+)
