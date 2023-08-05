0458cd219bb285650f943b184558848021cc0b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:58 +0000

    Add package zy1221_nester/1.1.0/zy1221_nester-1.1.0.zip

---
+++ packages/zy1221_nester/zy1221_nester-1.1.0.zip/zy1221_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name="zy1221_nester",
+    version="1.1.0",
+    py_modules=['zy1221_nester'],
+    author='zypython',
+    author_email='1083060476@qq.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
