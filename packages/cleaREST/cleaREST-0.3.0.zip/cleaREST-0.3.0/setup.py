b12c305d1db52892bdef3c69e89312c3d8a678f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:37 +0000

    Add package cleaREST/0.3.0/cleaREST-0.3.0.zip

---
+++ packages/cleaREST/cleaREST-0.3.0.zip/cleaREST-0.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+import clearest
+
+setup(
+    name="cleaREST",
+    version=clearest.__version__,
+    author=clearest.__author__,
+    author_email=clearest.__contact__,
+    description="Light-weight Python framework for building REST APIs.",
+    license="MIT",
+    keywords="rest api framework json xml",
+    url=clearest.__homepage__,
+    packages=["clearest", "tests", "sample"],
+    test_suite="tests",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+    ],
+)
