aec2e4317d78cbe022c0ffe549f5e2e98e56232a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package cloud-compose-cluster/0.1.1/cloud-compose-cluster-0.1.1.tar.gz

---
+++ packages/cloud-compose-cluster/cloud-compose-cluster-0.1.1.tar.gz/cloud-compose-cluster-0.1.1/cloudcompose/cluster/commands/cli.py
@@ -0,0 +1,41 @@
+import click
+from cloudcompose.cluster.cloudinit import CloudInit
+from cloudcompose.cluster.aws.cloudcontroller import CloudController
+from cloudcompose.config import CloudConfig
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option('--cloud-init/--no-cloud-init', default=True)
+def up(cloud_init):
+    """
+    creates a new cluster
+    """
+    cloud_config = CloudConfig()
+    ci = None
+
+    if cloud_init:
+        ci = CloudInit(cloud_config)
+
+    cloud_controller = CloudController(cloud_config)
+    cloud_controller.up(ci)
+
+@cli.command()
+def down():
+    """
+    destroys an existing cluster
+    """
+    cloud_config = CloudConfig()
+    cloud_controller = CloudController(cloud_config)
+    cloud_controller.down()
+
+@cli.command()
+def build():
+    """
+    builds the cloud_init script
+    """
+    cloud_config = CloudConfig()
+    cloud_init = CloudInit(cloud_config)
+    print cloud_init.build()
