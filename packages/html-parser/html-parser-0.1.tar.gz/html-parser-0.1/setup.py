cfc383c09e7a7b135e3be5c7dcc09d3ee63d2a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:34 +0000

    Add package html-parser/0.1/html-parser-0.1.tar.gz

---
+++ packages/html-parser/html-parser-0.1.tar.gz/html-parser-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+ 
+setup(
+    name='html-parser',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['bin/parser'],                  # The name of your scipt, and also 
+    install_requires=[
+          'ply',
+    ],
+) 
