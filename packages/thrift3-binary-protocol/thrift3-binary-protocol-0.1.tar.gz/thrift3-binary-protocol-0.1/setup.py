bad8a448d837381faaa9de711f7c344e4339d4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:30 +0000

    Add package thrift3-binary-protocol/0.1/thrift3-binary-protocol-0.1.tar.gz

---
+++ packages/thrift3-binary-protocol/thrift3-binary-protocol-0.1.tar.gz/thrift3-binary-protocol-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name= 'thrift3-binary-protocol',
+	packages = ['thrift3-binary-protocol'],
+	version = '0.1',
+	description = 'Apache Thrift with modifications for compatibility for Python 3',
+	author = 'Victor Araujo',
+	author_email = 've.ar91@gmail.com',
+	url = 'https://github.com/https://github.com/vears91/thrift3-binary-protocol',
+	download_url = 'https://github.com/vears91/thrift3-binary-protocol/tarball/0.1',
+	keywords = ['thrift', 'python3', 'binary protocol'],
+	classifiers = []
+)
