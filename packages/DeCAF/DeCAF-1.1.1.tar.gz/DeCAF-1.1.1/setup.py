e013cd349f310c3a24d56846daa93bd42b19a455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:11 +0000

    Add package DeCAF/1.1.1/DeCAF-1.1.1.tar.gz

---
+++ packages/DeCAF/DeCAF-1.1.1.tar.gz/DeCAF-1.1.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='DeCAF',
+    version='1.1.1',
+    description='Discrimination, Comparison, Alignment tool for small molecules',
+    author='Marta M. Stepniewska',
+    author_email='martasd@ibb.waw.pl',
+    url='https://bitbucket.org/marta-sd/decaf',
+    license='BSD',
+    packages=['decaf',
+              'decaf.toolkits'],
+    install_requires=['numpy>=1.6.2', 'scipy>=0.10', 'matplotlib>=1.3.1']
+)
