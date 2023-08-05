6789ff4bb6f57432b6452d7938c2b9d8a0bd69c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:37 +0000

    Add package vcf2clinvar/0.1.4/vcf2clinvar-0.1.4.tar.gz

---
+++ packages/vcf2clinvar/vcf2clinvar-0.1.4.tar.gz/vcf2clinvar-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='vcf2clinvar',
+    version='0.1.4',
+    description='Match a personal genome VCF datafile to ClinVar',
+    url='https://github.com/PersonalGenomesOrg/vcf2clinvar',
+    author='Personal Genome Project Informatics Group',
+    author_email='pgp-informatics@arvados.org',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ],
+
+    packages=['vcf2clinvar'],
+
+    # Core dependencies should be listed here (will be installed by pip).
+    install_requires=[],
+
+    scripts=['clinvar_report.py'],
+
+)
