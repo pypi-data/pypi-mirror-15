85eee6fe7c11b800ee989e3e6ea886fb9142929b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:45 +0000

    Add package sergeymakinen/1.1.0b2/sergeymakinen-1.1.0b2.tar.gz

---
+++ packages/sergeymakinen/sergeymakinen-1.1.0b2.tar.gz/sergeymakinen-1.1.0b2/sergeymakinen/__init__.py
@@ -0,0 +1,9 @@
+"""
+Different functions I often use in my public and private scripts
+"""
+
+__version__ = '1.1.0b2'
+__author__ = 'Sergey Makinen <sergey@makinen.ru>'
+__license__ = 'MIT'
+
+from . import shell_utils
---
+++ packages/sergeymakinen/sergeymakinen-1.1.0b2.tar.gz/sergeymakinen-1.1.0b2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='sergeymakinen',
+      version='1.1.0b2',
+      description='Sergey Makinen Python modules',
+      license='MIT',
+      author='Sergey Makinen',
+      author_email='sergey@makinen.ru',
+      url='https://github.com/sergeymakinen/python-modules',
+      packages=['sergeymakinen'],
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+      ],
+      keywords='sergeymakinen library development',
+      install_requires=['python-dateutil>=2.3'])
