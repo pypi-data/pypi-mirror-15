5ed748bae5d518322dbf00cb000f95f3cf71b269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:47 +0000

    Add package virustotalparser/0.3.10/virustotalparser-0.3.10.tar.gz

---
+++ packages/virustotalparser/virustotalparser-0.3.10.tar.gz/virustotalparser-0.3.10/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='virustotalparser',
+    author='Maciej Walerczuk',
+    author_email='mwalerczuk@gmail.com',
+    url='http://github.com',
+    license='MIT',
+    version='0.3.10',
+    packages=find_packages(),
+    scripts=['bin/vtparser'],
+    install_requires=[
+          'ply==3.8'
+    ],
+
+) 
