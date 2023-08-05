4b45ff045f9b74abe2e1f08101f0d06bbb965b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package TryingToGetSleep/1.2.0/TryingToGetSleep-1.2.0.tar.gz

---
+++ packages/TryingToGetSleep/TryingToGetSleep-1.2.0.tar.gz/TryingToGetSleep-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'TryingToGetSleep',
+	version = '1.2.0',
+	py_modules = ['TryingToGetSleep'],
+	author = 'Elijah Yu',
+	author_email = 'don'' email me',
+	description = ' I''m just trying to get sleep'
+	)
