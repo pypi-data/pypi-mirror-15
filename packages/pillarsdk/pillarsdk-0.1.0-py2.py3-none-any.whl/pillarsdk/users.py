73fe53976ce20be911041086eec948ed4947c825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:25 +0000

    Add package pillarsdk/0.1.0/pillarsdk-0.1.0-py2.py3-none-any.whl

---
+++ packages/pillarsdk/pillarsdk-0.1.0-py2.py3-none-any.whl/pillarsdk/users.py
@@ -0,0 +1,21 @@
+import hashlib
+import urllib
+from .resource import List
+from .resource import Find
+from .resource import Create
+from .resource import Post
+from .resource import Update
+from .resource import Delete
+
+
+class User(List, Find, Create, Post, Update, Delete):
+    """User class wrapping the REST nodes endpoint
+    """
+    path = "users"
+
+    def gravatar(self, size=64):
+        """Generate Gravatar link on the fly using the email value"""
+        parameters = {'s':str(size), 'd':'mm'}
+        return "https://www.gravatar.com/avatar/" + \
+            hashlib.md5(self.email.lower()).hexdigest() + \
+            "?" + urllib.urlencode(parameters)
