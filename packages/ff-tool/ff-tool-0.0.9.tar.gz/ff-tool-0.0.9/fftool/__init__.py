aba0694ad4da33721eaca321b753ce3b6f1d68f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:14 +0000

    Add package ff-tool/0.0.9/ff-tool-0.0.9.tar.gz

---
+++ packages/ff-tool/ff-tool-0.0.9.tar.gz/ff-tool-0.0.9/fftool/__init__.py
@@ -0,0 +1,28 @@
+import os
+from subprocess import Popen, PIPE
+from firefox_env_handler import IniHandler
+
+
+__version__ = '0.0.9'
+
+
+CHANNELS = ['release',
+            'beta',
+            'aurora',
+            'nightly']
+
+DEFAULT_CHANNEL = 'nightly'
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+DIR_TEMP = '_temp'
+DIR_TEMP_BROWSERS = os.path.join(DIR_TEMP, 'browsers')
+DIR_CONFIGS = '{0}/configs'.format(HERE)
+OS_CONFIG = IniHandler()
+OS_CONFIG.load_os_config(DIR_CONFIGS)
+DIR_TEMP_PROFILES = os.path.join(DIR_TEMP, 'profiles')
+PATH_PREFS_ROOT = os.environ.get('PATH_PREFS_ROOT')
+
+
+def local(cmd):
+    output = Popen(cmd, stdout=PIPE, shell=True)
+    return output.stdout.read().strip()
