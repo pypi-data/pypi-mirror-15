b476188b218ca7e95b807ebe3c41c5d7c2d213fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:39 +0000

    Add package python-holdup/0.0/python-holdup-0.0.tar.gz

---
+++ packages/python-holdup/python-holdup-0.0.tar.gz/python-holdup-0.0/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    author_email='contact@ionelmc.ro',
+    description='A tool to wait for services and execute command. Useful for Docker containers that depend on slow to start services (like almost everything).',
+    install_requires=['holdup'],
+    long_description='''Use `holdup <https://pypi.python.org/pypi/holdup/>`_ instead.''',
+    maintainer='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    maintainer_email='contact@ionelmc.ro',
+    name='python-holdup',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/holdup/',
+    version="0.0",
+    zip_safe=False,
+)
