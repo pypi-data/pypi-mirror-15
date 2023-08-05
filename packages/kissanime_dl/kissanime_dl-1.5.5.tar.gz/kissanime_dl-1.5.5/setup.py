31c4c8550e63229aac5f7209fcc2d8e613f35ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package kissanime_dl/1.5.5/kissanime_dl-1.5.5.tar.gz

---
+++ packages/kissanime_dl/kissanime_dl-1.5.5.tar.gz/kissanime_dl-1.5.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='kissanime_dl',
+	version='1.5.5',
+	description='Easy downloading .mp4s from kissanime.to',
+	url="https://github.com/wileyyugioh/kissanime_dl",
+	author='Wiley Y.',
+	author_email="wileythrowaway001@gmail.com",
+	license='MIT',
+	packages=['kissanime_dl'],
+	install_requires=[
+	'requests==2.9.1',
+	'lxml==3.5.0',
+	'js2py==0.35',
+	],
+	zip_safe=False,
+	#Disabled because it doesn't work in windows
+	#scripts=['bin/kissanime-dl.py']
+	entry_points = {
+		"console_scripts" : ['kissanime-dl = kissanime_dl.command_line:main']
+	}
+	)
