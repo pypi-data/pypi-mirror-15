75bbd513025fabd5ccd2c6d16d00dc44d81dcf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package uwhoisd/0.0.7/uwhoisd-0.0.7.tar.gz

---
+++ packages/uwhoisd/uwhoisd-0.0.7.tar.gz/uwhoisd-0.0.7/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+
+import os.path
+
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    """Read files relative to this file."""
+    full_path = os.path.join(os.path.dirname(__file__), filename)
+    with open(full_path, 'r') as fh:
+        return fh.read()
+
+
+setup(
+    name='uwhoisd',
+    version='0.0.7',
+    description="Universal domain WHOIS proxy server.",
+    long_description=read('README') + "\n\n" + read("ChangeLog"),
+    url='https://github.com/kgaughan/uwhoisd/',
+    license='MIT',
+    packages=find_packages(exclude='tests'),
+    zip_safe=True,
+    install_requires=[
+        'diesel>=2.1.1',
+    ],
+
+    entry_points={
+        'console_scripts': (
+            'uwhoisd = uwhoisd:main',
+        ),
+    },
+
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: System :: Networking',
+    ),
+
+    author='Keith Gaughan',
+    author_email='k@stereochro.me',
+)
