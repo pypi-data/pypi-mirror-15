113d571ad365e7d36ddcee6601b9939f91ef4750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:35 +0000

    Add package aristotle-metadata-registry/1.2.4/aristotle-metadata-registry-1.2.4.tar.gz

---
+++ packages/aristotle-metadata-registry/aristotle-metadata-registry-1.2.4.tar.gz/aristotle-metadata-registry-1.2.4/aristotle_mdr/views/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+from .views import *
+from . import wizards
+from . import user_pages
+from . import workgroups
+from . import discussions
+from . import comparator
+from . import bulk_actions
+from . import editors
