b0f35dcee0e678fc3308b362c54d098baeff59b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:33 +0000

    Add package humilis/0.4.9/humilis-0.4.9.tar.gz

---
+++ packages/humilis/humilis-0.4.9.tar.gz/humilis-0.4.9/humilis/metadata.py
@@ -0,0 +1,12 @@
+"""Project metadata."""
+
+package = 'humilis'
+project = 'humilis'
+version = '0.4.9'
+description = 'Helps you deploy infrastructure in the AWS cloud'
+authors = ['Innovative Travel Ltd', 'German Gomez-Herrero']
+authors_string = ', '.join(authors)
+emails = ['german@innovativetravel.eu']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = 'http://github.com/InnovativeTravel/humilis'
