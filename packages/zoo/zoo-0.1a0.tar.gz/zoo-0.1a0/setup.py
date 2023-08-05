4e68c65a1dadfbf491e15ebc9cf103c3cbc741e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:42 +0000

    Add package zoo/0.1a0/zoo-0.1a0.tar.gz

---
+++ packages/zoo/zoo-0.1a0.tar.gz/zoo-0.1a0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zoo',
+    version='0.1a0',
+    packages=find_packages(),
+    install_requires=[],
+
+    author='omsobliga',
+    author_email='omsobliga@gmail.com',
+    description='Zoo is a rpc framework for Python',
+    license='MIT',
+    url='https://github.com/omsobliga/zoo'
+)
