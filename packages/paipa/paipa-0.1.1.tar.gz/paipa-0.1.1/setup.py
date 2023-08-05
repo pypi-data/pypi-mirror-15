442f7bd4e2e6d5509d076b43fd8938221ff580a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:55 +0000

    Add package paipa/0.1.1/paipa-0.1.1.tar.gz

---
+++ packages/paipa/paipa-0.1.1.tar.gz/paipa-0.1.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+try:
+    from os.path import abspath, dirname, join
+    _desc = join(dirname(abspath(__file__)), 'README.rst')
+    long_description = open(_desc, 'r').read()
+except IOError:
+    long_description = "paipa"
+
+
+setup(
+    name='paipa',
+    version='0.1.1',
+    description='Python pipeline library. Maori: (noun) pipe.',
+    long_description=long_description,
+    packages=['paipa'],
+    author="Bizdev Engineers",
+    author_email="busdev_engineers@stylight.com",
+    url='https://github.com/stylight/python-paipa',
+    tests_require=[
+        'tox',
+    ],
+    install_requires=[
+        'six',
+        'enum34',
+    ],
+    extras_require={
+        'glue': ['tornado'],
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
