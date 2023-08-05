04539f0b615e7e91646673eddfe238f2e2aef8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package sampleProject123/0.1.3/sampleProject123-0.1.3.tar.gz

---
+++ packages/sampleProject123/sampleProject123-0.1.3.tar.gz/sampleProject123-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+with open("README.rst") as f:
+	readme=f.read()
+setup(
+	#Application Name
+	name="sampleProject123",
+	#version
+	version="0.1.3",
+	#Application author details:
+	author="ashwinraju101",
+	author_email="ashwinraju101@gmail.com",
+	py_modules=["my_package"],
+	
+	description="A faster way to read image from bioformats",
+	long_description=readme,
+	install_requires=["bioformats","numpy","h5py","cython","numexpr","javabridge","blosc","tables",]
+	)
