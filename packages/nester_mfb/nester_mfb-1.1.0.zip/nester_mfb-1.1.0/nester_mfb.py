c95a9711c3fcf5945304f82a60e6bf168ca52498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:24 +0000

    Add package nester_mfb/1.1.0/nester_mfb-1.1.0.zip

---
+++ packages/nester_mfb/nester_mfb-1.1.0.zip/nester_mfb-1.1.0/nester_mfb.py
@@ -0,0 +1,21 @@
+﻿""" Este es el modulo "nester_mfb.py", ofrece una funcion "print_lol"
+    que muestra listas que pueden contener otras listas (o no).
+    Version 1.1.0
+"""
+def print_lol(the_list, level):
+    """ El parametro posicional "the_list"
+        es un lista de Phyton que puede contener otra sublistas-
+        Cada elemento en la lista es (recursivamente) mostrado
+        en la pantalla en su propia linea.
+	El parametro "level" indica el numero de TAB que se indenta una línea
+    """
+    for item in the_list:
+        if isinstance(item, list):
+            print_lol(item, level+1)
+        else:
+            for num in range(level):
+                print("\t", end='')
+            print(item)
+	    
+
+			
---
+++ packages/nester_mfb/nester_mfb-1.1.0.zip/nester_mfb-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_mfb',
+    version = '1.1.0',
+    py_modules = ['nester_mfb'],
+    author = 'mfb_lcc_uma',
+    author_email = 'bertoa@lcc.uma.es',
+    url = 'http://www.lcc.uma.es/contenidos/informacion_personal.action?id=17',
+    description = 'Un impresor simple de listas Phyton',
+    )
