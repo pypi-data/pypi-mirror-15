e5658ac0458fd0d896dcee62071778bf770222bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:18 +0000

    Add package beatle/0.0.11/beatle-0.0.11.tar.gz

---
+++ packages/beatle/beatle-0.0.11.tar.gz/beatle-0.0.11/beatle/dlg/__init__.py
@@ -0,0 +1,9 @@
+import wx
+#from ClassDiagram import ClassDiagramDialog
+from Preferences import PreferencesDialog
+from Workspace import WorkspaceDialog
+#from GenerateSources import GenerateSourcesDialog
+#from ContextItems import ContextItems
+from BuildTools import BuildTools
+from CodeNavigator import CodeNavigator
+from Working import WorkingDialog
