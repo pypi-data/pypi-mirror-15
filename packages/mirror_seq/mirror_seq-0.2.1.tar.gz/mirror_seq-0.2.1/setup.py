80e0cae9c9967daf0e7389a20da03dec175ed806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:43 +0000

    Add package mirror_seq/0.2.1/mirror_seq-0.2.1.tar.gz

---
+++ packages/mirror_seq/mirror_seq-0.2.1.tar.gz/mirror_seq-0.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import pypandoc
+
+exec(open('mirror_seq/version.py').read())
+
+try:
+   long_description = pypandoc.convert('README.md', 'rst')
+except:
+   long_description = open('README.md').read()
+
+INSTALL_REQUIRES = [
+    'pandas==0.18.0',
+    'pysam==0.9.0',
+    'cutadapt==1.9.1',
+    'tables==3.2.2',
+]
+
+setup(name='mirror_seq',
+    version=__version__,
+    description='The bioinformatics tool for Mirror-seq.',
+    long_description=long_description,
+    url='https://github.com/Zymo-Research/mirror-seq',
+    author='Hunter Chung',
+    author_email='b89603112@gmail.com',
+    licence='Apache License 2.0',
+    scripts=['bin/mirror-seq'],
+    packages=['mirror_seq'],
+    install_requires=INSTALL_REQUIRES,
+    keywords='mirror sequencing next-gen hydroxymethylation bisulfite bioinformatics')
