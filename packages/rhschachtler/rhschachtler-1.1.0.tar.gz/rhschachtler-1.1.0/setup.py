3d44e76fc9af4d50beb022205aa44acdc680ebe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:32 +0000

    Add package rhschachtler/1.1.0/rhschachtler-1.1.0.tar.gz

---
+++ packages/rhschachtler/rhschachtler-1.1.0.tar.gz/rhschachtler-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'rhschachtler',
+    version = '1.1.0',
+    py_modules = ['schachtler'],
+    author = 'har39234',
+    author_email = 'reinhard.hartl@hs-regensburg.de',
+    url = 'http://headfirstlabs.com',
+    description = 'Einfache Ausgabe geschachtelter Listen',
+    )
