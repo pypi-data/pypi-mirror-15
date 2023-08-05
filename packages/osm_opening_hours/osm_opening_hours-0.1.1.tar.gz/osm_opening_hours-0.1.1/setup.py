dafc45e25aa329b8dfc04aec62fafa1e38d9faac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package osm_opening_hours/0.1.1/osm_opening_hours-0.1.1.tar.gz

---
+++ packages/osm_opening_hours/osm_opening_hours-0.1.1.tar.gz/osm_opening_hours-0.1.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup 
+
+setup(name='osm_opening_hours',
+      version='0.1.1',
+      description='Tools to parse opening hours format from OpenStreetMap.',
+      author='Martin Filliau',
+      author_email='martin@filliau.com',
+      url='https://github.com/k-nut/osm-opening-hours',
+      py_modules=['osm_time', 'osm_time.opening_hours'],
+      classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+      test_suite = "tests",
+     )
