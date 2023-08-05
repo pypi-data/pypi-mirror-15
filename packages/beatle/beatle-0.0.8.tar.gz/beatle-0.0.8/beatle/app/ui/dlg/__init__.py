b803fd4fb8b9a0f14b8a6a7d54c6e7d92e3f4e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package beatle/0.0.8/beatle-0.0.8.tar.gz

---
+++ packages/beatle/beatle-0.0.8.tar.gz/beatle-0.0.8/beatle/app/ui/dlg/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from FindText import FindText
+from Folder import FolderDialog
+from FindInFiles import FindInFilesDialog
+from ImportProject import ImportProjectDialog
+from Wait import WaitDialog
