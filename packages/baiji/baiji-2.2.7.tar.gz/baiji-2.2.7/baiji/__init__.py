d4f6452f59999e21c21e092161f437690fee2465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:19 +0000

    Add package baiji/2.2.7/baiji-2.2.7.tar.gz

---
+++ packages/baiji/baiji-2.2.7.tar.gz/baiji-2.2.7/baiji/__init__.py
@@ -0,0 +1,8 @@
+# Allow other packages to publish modules into the `baiji` namespace.
+#
+# http://stackoverflow.com/a/1676069/893113
+# https://www.python.org/dev/peps/pep-0420/
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+__version__ = '2.2.7'
---
+++ packages/baiji/baiji-2.2.7.tar.gz/baiji-2.2.7/setup.py
@@ -0,0 +1,42 @@
+# https://coderwall.com/p/qawuyq
+# Thanks James.
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+    print 'warning: pandoc or pypandoc does not seem to be installed; using empty long_description'
+
+import os
+requirements_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'requirements.txt')
+with open(requirements_file, 'r') as f:
+    install_requires = [x.strip() for x in f.readlines()]
+
+from setuptools import setup
+
+setup(
+    name='baiji',
+    version=__import__('baiji').__version__,
+    author='Body Labs',
+    author_email='alex@bodylabs.com, paul.melnikow@bodylabs.com, chenyang.liu@bodylabs.com',
+    description='High-level Python abstraction layer for Amazon S3',
+    long_description=long_description,
+    url='https://github.com/bodylabs/baiji',
+    license='MIT',
+    packages=[
+        'baiji',
+        'baiji/util',
+    ],
+    scripts=[
+        'bin/s3',
+    ],
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
