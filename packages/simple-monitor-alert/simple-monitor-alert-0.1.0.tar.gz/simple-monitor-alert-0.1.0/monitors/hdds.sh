d59d8122a3fd97be6c78d3bec115c84d6cc58689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:54 +0000

    Add package simple-monitor-alert/0.1.0/simple-monitor-alert-0.1.0.tar.gz

---
+++ packages/simple-monitor-alert/simple-monitor-alert-0.1.0.tar.gz/simple-monitor-alert-0.1.0/monitors/hdds.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Hdds monitor.
+# Check the free space on partitions.
+# No parameters required.
+
+echo "X-Run-Every-Seconds: 300"
+
+IFS=$'\n'
+for part_data in `df -h`; do
+    device=$(basename $(echo $part_data | awk '{ print $1 }'));
+    if [[ `grep "$device" /proc/partitions` == "" ]]; then
+        # Ignore dev, run, tmpfs...
+        continue
+    fi
+    echo "pcnt_use($device).name = 'Percentage of space used in $device ("`echo $part_data | awk '{ print $6 }'`")'";
+    echo "pcnt_use($device).expected = <= 80"
+    echo "pcnt_use($device).value = "`echo $part_data | awk '{ print substr($5, 1, length($5)-1) }'`;
+    echo -n "pcnt_use($device).extra_info = 'Space: "`echo $part_data | awk '{ print $3 }'`"/";
+    echo `echo $part_data | awk '{ print $2 }'`"'"
+done
