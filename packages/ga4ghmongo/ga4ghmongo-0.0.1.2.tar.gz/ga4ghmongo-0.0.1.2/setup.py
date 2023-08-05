c6add14ae7e53d39377f8ebf42060896e86b007a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:22 +0000

    Add package ga4ghmongo/0.0.1.2/ga4ghmongo-0.0.1.2.tar.gz

---
+++ packages/ga4ghmongo/ga4ghmongo-0.0.1.2.tar.gz/ga4ghmongo-0.0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='ga4ghmongo',
+    version='0.0.1.2',
+    packages=[
+        'ga4ghmongo',
+        'ga4ghmongo.schema',
+        'ga4ghmongo.schema.models'],
+    license='MIT',
+    url='https://github.com/Phelimb/ga4gh-mongo',
+    description='A document based Variant database inspired by ga4gh Variants schema',
+    author='Phelim Bradley',
+    author_email='wave@phel.im',
+    install_requires=["mongoengine"])
