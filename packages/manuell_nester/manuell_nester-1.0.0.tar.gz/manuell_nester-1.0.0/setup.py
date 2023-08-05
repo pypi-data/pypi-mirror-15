a02c136bdd99cdc39549f169a4a03e3e10608e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:22 +0000

    Add package manuell_nester/1.0.0/manuell_nester-1.0.0.tar.gz

---
+++ packages/manuell_nester/manuell_nester-1.0.0.tar.gz/manuell_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name 		= 'manuell_nester',
+	version 	= '1.0.0',
+	py_modules	= ['manuell_nester'],
+	author		= 'hfpython',
+	author_email 	= 'me@manuell.it',
+	url		= 'http://www.manuell.it',
+	description	= 'A simple test to print all of nested lists',
+	
+)
