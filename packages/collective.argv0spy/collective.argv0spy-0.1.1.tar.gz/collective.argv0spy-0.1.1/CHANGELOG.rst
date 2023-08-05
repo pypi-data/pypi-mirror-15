f952dc4611cf8048a623b0aca41aa7c1b25f2bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:23 +0000

    Add package collective.argv0spy/0.1.1/collective.argv0spy-0.1.1.tar.gz

---
+++ packages/collective.argv0spy/collective.argv0spy-0.1.1.tar.gz/collective.argv0spy-0.1.1/CHANGELOG.rst
@@ -0,0 +1,19 @@
+Changelog
+=========
+
+0.1.1 (2016-05-02)
+------------------
+
+- Fix broken egg.
+
+
+0.1.0 (2016-04-30)
+------------------
+
+- Add posibility to define an overall shorter name for the zope process.
+
+
+0.0.1 (2016-04-30)
+------------------
+
+- Initial release.
---
+++ packages/collective.argv0spy/collective.argv0spy-0.1.1.tar.gz/collective.argv0spy-0.1.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+import os
+
+
+def read(*paths):
+    return open(os.path.join(os.path.dirname(__file__), *paths), 'r').read()
+
+version = '0.1.1'
+
+setup(name='collective.argv0spy',
+      version=version,
+      description='Change process title to show current urls being processed',
+      long_description='\n\n'.join([
+          read('README.rst'),
+          read('CHANGELOG.rst'),
+      ]),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          'Programming Language :: Python',
+      ],
+      keywords='',
+      author='Patrick Gerken',
+      author_email='gerken@patrick-gerken.de',
+      url='https://github.com/collective/collective.argv0spy',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_package=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setproctitle',
+
+      ],
+      )
