3756caf4c0187200ada820a59fe70ff248f154fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package zess_chapter5/1.2.0/zess_chapter5-1.2.0.zip

---
+++ packages/zess_chapter5/zess_chapter5-1.2.0.zip/zess_chapter5-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'zess_chapter5',
+    version = '1.2.0',
+    py_modules = ['zess_chapter5'],
+    author = 'zess',
+    author_email = 'zess1982@126.com',
+    url = 'http://satellite.sinaapp.com/',
+    description = 'Parsing data.',
+    )
