4fbb323e5c23897c5f98687b10aaa3658f2a73d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:22 +0000

    Add package Token/1.5.9/Token-1.5.9.tar.gz

---
+++ packages/Token/Token-1.5.9.tar.gz/Token-1.5.9/Token/demos/dev-demo1.py
@@ -0,0 +1,40 @@
+import Token
+
+ProviderFIDOR = Token.ProviderService('fidor')
+ProviderBBVA = Token.ProviderService('bbva')
+BankFIDOR = Token.BankService('fidor')
+BankBBVA = Token.BankService('bbva')
+
+sk1, pk1 = Token.create_keypair()
+sk2, pk2 = Token.create_keypair()
+
+alice = ProviderFIDOR.create_member('alice', '123', pk1)
+bob = ProviderBBVA.create_member('bob', '123', pk2)
+
+Token.set_context('bank-fidor', Token.keys["bankFIDOR"])
+access = BankFIDOR.create_access('1008', '1008', 'fidor', session_id='test')
+
+Token.set_context('bank-bbva', Token.keys["bankBBVA"])
+access2 = BankBBVA.create_access('1008', '1008', 'bbva', session_id='test')
+
+
+Token.set_context(alice, sk1)
+ProviderFIDOR.create_alias('ALICE'+pk1[:5])
+
+alice_acc = ProviderFIDOR.create_account('fidor', access.id, "Personal Checking")
+ProviderFIDOR.get_account(alice_acc.id)
+
+Token.set_context(bob, sk2)
+bob_acc = ProviderBBVA.create_account('bbva', access2.id, "Merchant Business Account")
+token = ProviderBBVA.create_token('ALICE'+pk1[:5])
+
+Token.set_context(alice, sk1)
+ProviderFIDOR.get_tokens()
+ProviderFIDOR.endorse_token(token.id, alice_acc.id)
+
+
+Token.set_context(bob, sk2)
+payment = ProviderBBVA.create_payment(token.id, bob_acc.id, 0.02, "EUR")
+
+Token.set_context(alice, sk1)
+ProviderFIDOR.get_account(alice_acc.id)
