5a72fa5c5c0fd017e2de8141a0776bc751bc17b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:59 +0000

    Add package PyUpdater-S3-Plugin/3.0/PyUpdater-s3-Plugin-3.0.tar.gz

---
+++ packages/PyUpdater-S3-Plugin/PyUpdater-s3-Plugin-3.0.tar.gz/PyUpdater-s3-Plugin-3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyUpdater-s3-Plugin',
+    version='3.0',
+    description='Amazon S3 plugin for PyUpdater',
+    author='JohnyMoSwag',
+    author_email='johnymoswag@gmail.com',
+    url='https://github.com/JohnyMoSwag/PyUpdater-s3-Plugin',
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 2.7',
+                 'Intended Audience :: Developers',
+                 'Environment :: Console',
+                 ],
+    platforms=['Any'],
+    install_requires=[
+        'boto3',
+        'jms-utils >= 1.0.1',
+        ],
+    packages=find_packages(),
+    include_package_data=True,
+    namespace_packages = ['pyupdater.plugins'],
+    zip_safe=False,
+)
