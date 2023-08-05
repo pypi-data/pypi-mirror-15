621e970d786a900de3ab2d74f503e7fd9d151d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:25 +0000

    Add package nester_mfb/1.3.0/nester_mfb-1.3.0.zip

---
+++ packages/nester_mfb/nester_mfb-1.3.0.zip/nester_mfb-1.3.0/README.txt
@@ -0,0 +1,4 @@
+V.1.0.0 Impresor simple de listas Phyton
+V.1.1.0 Nuevo parametro level para indicar numero de TABS que se indentan las lineas
+V.1.2.0 El parametro level se hace opcional con valor por defecto de 0.
+V.1.3.0 Nuevo parametro indent=False, para indicar si se desea o no indentacion
---
+++ packages/nester_mfb/nester_mfb-1.3.0.zip/nester_mfb-1.3.0/nester_mfb.py
@@ -0,0 +1,26 @@
+﻿""" Este es el modulo "nester_mfb.py", ofrece una funcion "print_lol"
+    que muestra listas que pueden contener otras listas (o no).
+    Version 1.3.0
+"""
+def print_lol(the_list, indent=False, level=0):
+    """-El parametro posicional "the_list"
+        es un lista de Phyton que puede contener otra sublistas.
+        Cada elemento en la lista es (recursivamente) mostrado
+        en la pantalla en su propia linea.
+       -El parametro indent indica si se desea que las líneas se indenten
+        por defecto la opcion es FALSE (no se indenta ninguna línea).
+       -El parametro "level" (opcional) indica el numero de TABs que se indenta una línea
+	"level" tiene un valor por defecto de 0.
+	
+    """
+    for item in the_list:
+        if isinstance(item, list):
+            print_lol(item, indent, level+1)
+        else:
+            if indent:
+                for num in range(level):
+                    print("\t", end='')
+            print(item)
+	    
+
+			
