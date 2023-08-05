251c14e0668e8d6be04ba66945a7b24f0998be57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:15 +0000

    Add package jumpdir/0.7.4/jumpdir-0.7.4.tar.gz

---
+++ packages/jumpdir/jumpdir-0.7.4.tar.gz/jumpdir-0.7.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='jumpdir',
+    version='0.7.4',
+    description='A command line tool for quickly navigating your home directory.',
+    author='cgmcintyr',
+    author_email='me@cgmcintyre.com',
+    url='http://github.com/chrsintyre/jumpdir',
+    zip_safe=False,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX :: Linux',
+    ],
+    keywords='commandline navigation',
+    packages=['jumpdir'],
+    scripts=['scripts/jumpdir', 'scripts/jumpdir-search'],
+    install_requires=[
+        'pychalk',
+    ],
+    test_suite='tests',
+)
