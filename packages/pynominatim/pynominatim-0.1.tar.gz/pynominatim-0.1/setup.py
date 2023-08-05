6d5e99009f079f0e0202ff3c1ed3602a64620cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package pynominatim/0.1/pynominatim-0.1.tar.gz

---
+++ packages/pynominatim/pynominatim-0.1.tar.gz/pynominatim-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='pynominatim',
+    version='0.1',
+    author='Xavier Barnada',
+    author_email='brunek5252@gmail.com',
+    url='https://github.com/Xevib/pynominatim.git',
+    description='OSM Nominatim API module',
+    packages=['pynominatim'],
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+    )
