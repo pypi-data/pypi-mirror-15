c2fb545e510c99f9e6fc08b44551333da1ee2968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package mixpanel_export/0.2.post4.dev0/mixpanel_export-0.2.post4.dev0.tar.gz

---
+++ packages/mixpanel_export/mixpanel_export-0.2.post4.dev0.tar.gz/mixpanel_export-0.2.post4.dev0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from io import open
+import versioneer
+
+setup(
+    name='mixpanel_export',
+    packages=find_packages(exclude=["tests"]),
+    version=versioneer.get_version(),
+    description='A streaming library for reading raw event '
+                'data from Mixpanel\'s export API',
+    author='Dillon Dixon',
+    author_email='dillondixon@gmail.com',
+    url='https://github.com/ownaginatious/mixpanel-export-stream',
+    license='MIT',
+    keywords=['mixpanel', 'export', 'stream'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    install_requires=[line.strip()
+                      for line in open("requirements.txt", "r",
+                                       encoding="utf-8").readlines()],
+)
