7ee7919f271d96a9f3ad41259bec8aab4ecf51a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:30 +0000

    Add package CNDotA2_nester/1.0.5/CNDotA2_nester-1.0.5.tar.gz

---
+++ packages/CNDotA2_nester/CNDotA2_nester-1.0.5.tar.gz/CNDotA2_nester-1.0.5/CNDotA2_nester.py
@@ -0,0 +1,12 @@
+"""This is a function to print elements of a list and its nested lists, and you can add tabs before every elements"""
+def print_lol(the_list, indent = False, level = 0):
+	for each_item in the_list:
+		"""If the item is a list, then call the function again, if
+                   the item is already an element, then print it directly"""
+		if isinstance(each_item, list):
+			print_lol(each_item, indent, level + 1)
+		else:
+			if indent:
+				for tab_stop in range(level):
+					print("\t", end="")
+			print(each_item)
---
+++ packages/CNDotA2_nester/CNDotA2_nester-1.0.5.tar.gz/CNDotA2_nester-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'CNDotA2_nester',
+	version = '1.0.5',
+	py_modules = ['CNDotA2_nester'],
+	author = 'CNDotA2',
+	author_email = '838282876@qq.com',
+	url = 'www.github.org/cndota2',
+	description = 'A simple printer of nested lists, and you can add tabs, the default is level 0, you can set indent to false to close the tab',
+)
