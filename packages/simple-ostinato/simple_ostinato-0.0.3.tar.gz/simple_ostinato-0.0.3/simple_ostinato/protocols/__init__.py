778b2df0f8b60748b7ad6c86674b616138111fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:17 +0000

    Add package simple-ostinato/0.0.3/simple_ostinato-0.0.3.tar.gz

---
+++ packages/simple-ostinato/simple_ostinato-0.0.3.tar.gz/simple_ostinato-0.0.3/simple_ostinato/protocols/__init__.py
@@ -0,0 +1,9 @@
+from .baseclass import Protocol
+from .overrides import MacAddress, Mac, Ethernet, IPv4Address, IPv4, Payload, \
+    Tcp, Udp
+
+__all__ = [
+    'Protocol',
+    'MacAddress', 'Mac', 'Ethernet', 'IPv4Address', 'IPv4', 'Payload',
+    'Tcp', 'Udp',
+]
