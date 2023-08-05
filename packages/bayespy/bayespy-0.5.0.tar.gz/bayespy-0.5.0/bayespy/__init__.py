7ce919a01a957a4e23337854a1b85e65fbaf0954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:33 +0000

    Add package bayespy/0.5.0/bayespy-0.5.0.tar.gz

---
+++ packages/bayespy/bayespy-0.5.0.tar.gz/bayespy-0.5.0/bayespy/__init__.py
@@ -0,0 +1,13 @@
+################################################################################
+# Copyright (C) 2011-2013 Jaakko Luttinen
+#
+# This file is licensed under the MIT License.
+################################################################################
+
+from .version import __version__
+
+
+from . import utils
+from . import inference
+from . import nodes
+from . import plot
