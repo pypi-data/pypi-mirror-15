f0af29bb54d8d9d19f7c7a7eefaceead1257f840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:25 +0000

    Add package lastsmk_FP/1.3.0/lastsmk_FP-1.3.0.zip

---
+++ packages/lastsmk_FP/lastsmk_FP-1.3.0.zip/lastsmk_FP-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'lastsmk_FP',
+    version         = '1.3.0',
+    py_modules      = ['lastsmkV160428'],
+    author          = 'hfpython',
+    author_email    = 'hfpython@headfirstlabs.com',
+    url             = 'http://www.headfirstlabs.com',
+    description     = 'A simple printer of nested lists',
+    )
