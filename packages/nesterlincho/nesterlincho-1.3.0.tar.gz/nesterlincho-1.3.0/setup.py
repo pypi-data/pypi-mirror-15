44782ca5aaf9fc25a9b557e3c94ed3099d640425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:13 +0000

    Add package nesterlincho/1.3.0/nesterlincho-1.3.0.tar.gz

---
+++ packages/nesterlincho/nesterlincho-1.3.0.tar.gz/nesterlincho-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nesterlincho',
+	version		= '1.3.0',
+	py_modules	= ['nesterlincho'],
+	author		= 'lincho',
+	author_email	= 'neo.eva@163.com',
+	url		= 'http://www.spora.bg',
+	description	= 'A simple printer of nested lists',
+      )
