73d348929cb6e439715dcc1ad4997d88f377c8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:56 +0000

    Add package infinityctl/0.3.1/infinityctl-0.3.1.zip

---
+++ packages/infinityctl/infinityctl-0.3.1.zip/infinityctl-0.3.1/infinityctl/__main__.py
@@ -0,0 +1,65 @@
+import argparse
+import sys
+from distutils.spawn import _nt_quote_args
+
+from . import update
+from . import buildchange
+from .startstop import start, stop
+from .buildchange import build_change
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='infinityctl', description='Инструмент для управления сервером Infinity.',
+                                     argument_default=argparse.SUPPRESS)
+
+    parser.add_argument('--quiet',
+                        action='store_true',
+                        help='отключить вывод')
+    parser.add_argument('--version', '-v',
+                        action='version',
+                        version='%(prog)s 0.3.1')
+
+    subparsers = parser.add_subparsers(help='Команды. Справка по команде: <команда> -h')
+
+    start_parser = subparsers.add_parser("start", help='запустить сервер')
+    start_parser.add_argument('--build',
+                              '-b',
+                              metavar='BUILD_NAME',
+                              help='целевой билд',
+                              default='tgstation',)
+    start_parser.set_defaults(func=start)
+    stop_parser = subparsers.add_parser("stop",
+                                        help='остановить сервер')
+    stop_parser.set_defaults(func=stop)
+    restart_parser = subparsers.add_parser("restart",
+                                           help='перезапустить сервер')
+    status_parser = subparsers.add_parser("status",
+                                          help='статус сервера')
+    update_parser = subparsers.add_parser("update",
+                                          help='обновить сервер')
+    update_parser.add_argument('--autostart', '-s',
+                               action='store_true',
+                               help='автозапуск')
+    update_parser.add_argument('--build', '-b',
+                               type=str,
+                               default='tgstation',
+                               help='билд для обновления (по умолчанию: tgstation)')
+    update_parser.set_defaults(func=update.update)
+    changemap_parser = subparsers.add_parser("map", help='сменить карту')
+    changebuild_parser = subparsers.add_parser("changebuild", help='сменить билд')
+    changebuild_parser.add_argument('--build',
+                                    '-b',
+                                    metavar='BUILD',
+                                    help='целевой билд',
+                                    required=True)
+    changebuild_parser.set_defaults(func=build_change)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    args.func(args)
+
+if __name__ == '__main__':
+    main()
---
+++ packages/infinityctl/infinityctl-0.3.1.zip/infinityctl-0.3.1/infinityctl/update.py
@@ -0,0 +1,128 @@
+import subprocess
+import sys
+import tempfile
+import shutil
+import os
+import glob
+
+from .util import print_status
+from .config import Config
+from .startstop import start
+
+
+def update(args):
+    config = Config()
+
+    buildname = config.get_build(args.build)
+    if not args:
+        print('Указанный билд не найден')
+        exit(1)
+    builddir = config.basedir
+    if not builddir:
+        print('Базовая директроия не указана, проверьте конфиг.')
+        sys.exit(1)
+    buildrepo = config.basedir
+    if not buildrepo:
+        print('Базовая директроия не указана, проверьте конфиг.')
+        sys.exit(1)
+
+    builddir += buildname.get('folder')
+    if not builddir or (builddir == config.basedir):
+        print('Запись о каталоге не найдена. Проверьте конфиг.')
+        sys.exit(1)
+    buildrepo += buildname.get('repo')
+    if not buildrepo or (buildrepo == config.basedir):
+        print('Запись о репозитории не найдена. Проверьте конфиг.')
+        sys.exit(1)
+
+    if not os.path.exists(config.basedir):
+        print('Базовый каталог, указанный в конфигурации не найден.')
+        sys.exit(1)
+
+    if not os.path.exists(builddir):
+        print('Каталог билда не найден.')
+        sys.exit(1)
+
+    if not os.path.exists(buildrepo):
+        print('Каталог репозитория билда не найден.')
+        sys.exit(1)
+
+
+
+    print("Обновление билда " + builddir + " " + "из " + buildrepo)
+
+    sys.stdout.write("Обновление тестовой версии")
+    try:
+        if subprocess.call("git fetch", shell=True, cwd=buildrepo):
+            print_status("FAIL")
+            print("Возникла ошибка. Выход.")
+            sys.exit(1)
+    except FileNotFoundError:
+        print_status("FAIL")
+        print("Каталог " + buildrepo + " не найден")
+        sys.exit(1)
+    print_status("OK")
+    sys.stdout.write("Слияние удаленной ветки с локальной")
+    if subprocess.call(["git", "pull", "origin", "master"], shell=False, cwd=buildrepo):
+        print_status("FAIL")
+        print("Не удается слить ветки")
+        sys.exit(1)
+    print_status("OK")
+    sys.stdout.write("Бэкап конфигов")
+    # бэкапы файлов храним во временном каталоге в /tmp
+    with tempfile.TemporaryDirectory() as tmpdir:
+        try:
+            if not os.path.isdir(tmpdir + "/code"):
+                os.makedirs(tmpdir + "/code")
+            shutil.copy(builddir + "/code/hub.dm", tmpdir + "/code/hub.dm")
+            shutil.copytree(builddir + "/config/", tmpdir + "/config/")
+
+        except FileNotFoundError:
+            print_status("FAIL")
+            print("Каталог " + builddir + " не найден")
+            sys.exit(1)
+        except:
+            print_status("FAIL")
+            print("Неизвестная ошибка")
+            sys.exit(1)
+        print_status("OK")
+        sys.stdout.write("Копирование в основной билд")
+        try:
+            if os.path.exists(builddir):
+                shutil.rmtree(builddir)
+            shutil.copytree(buildrepo, builddir)
+            # удалим каталог с конфигами. Все равно у нас есть бэкап
+            shutil.rmtree(builddir + "/config")
+        except FileNotFoundError:
+            print_status("FAIL")
+            print("Каталог " + builddir + " не найден")
+            sys.exit(1)
+        print_status("OK")
+        sys.stdout.write("Восстановление конфигов")
+        try:
+            # восстановление конфигов из временного каталога
+            shutil.copytree(tmpdir + "/config", builddir + "/config")
+            shutil.copy(tmpdir + "/code/hub.dm",builddir + "/code/")
+        except FileNotFoundError:
+            print_status("FAIL")
+            print("Каталог " + builddir + " не найден")
+            sys.exit(1)
+        print_status("OK")
+    sys.stdout.write("Компиляция")
+    # получаем все dme файлы и компилируем их
+    projects = glob.glob(buildrepo + "/*.dme")
+    if not projects:
+        print_status("FAIL")
+        print("Файлы dme не найдены")
+        sys.exit(1)
+    if subprocess.call(["DreamMaker", projects[0]], shell=False, cwd=builddir):
+        print_status("FAIL")
+        print("Ошибка компиляции")
+        sys.exit(1)
+    print_status("OK")
+
+    print("Обновление завершено!")
+
+    if args.autostart:
+        print("Запуск сервера")
+        start(buildname)
