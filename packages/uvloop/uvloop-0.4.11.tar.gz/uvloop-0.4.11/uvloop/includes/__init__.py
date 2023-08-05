9c1caf5ec0046ed80f2412de983770517e62ff54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package uvloop/0.4.11/uvloop-0.4.11.tar.gz

---
+++ packages/uvloop/uvloop-0.4.11.tar.gz/uvloop-0.4.11/uvloop/includes/__init__.py
@@ -0,0 +1,14 @@
+# These have to be synced with the stdlib.pxi
+import asyncio
+import collections
+import concurrent.futures
+import functools
+import itertools
+import os
+import signal
+import socket
+import subprocess
+import ssl
+import sys
+import threading
+import warnings
