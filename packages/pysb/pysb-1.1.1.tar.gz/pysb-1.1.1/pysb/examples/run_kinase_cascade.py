f730ecd20dbe902085e6d64da86d75f7b1c9e47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:18 +0000

    Add package pysb/1.1.1/pysb-1.1.1.tar.gz

---
+++ packages/pysb/pysb-1.1.1.tar.gz/pysb-1.1.1/pysb/examples/run_kinase_cascade.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from pysb.integrate import odesolve
+from pylab import linspace, plot, legend, show
+
+from kinase_cascade import model
+
+
+tspan = linspace(0, 1200)
+print("Simulating...")
+yfull = odesolve(model, tspan)
+plot(tspan, yfull['ppMEK'], label='ppMEK')
+plot(tspan, yfull['ppERK'], label='ppERK')
+legend(loc='upper left')
+show()
