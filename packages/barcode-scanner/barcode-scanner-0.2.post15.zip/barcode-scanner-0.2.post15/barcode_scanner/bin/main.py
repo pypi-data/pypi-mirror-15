6ac86da7e02edee91cdea7bd6c01a80408032be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:50 +0000

    Add package barcode-scanner/0.2.post15/barcode-scanner-0.2.post15.zip

---
+++ packages/barcode-scanner/barcode-scanner-0.2.post15.zip/barcode-scanner-0.2.post15/barcode_scanner/bin/main.py
@@ -0,0 +1,129 @@
+from argparse import ArgumentParser
+import json
+import logging
+import pprint
+import sys
+
+import gtk
+from redirect_io import nostderr
+
+
+from ..scanner import BarcodeScanner
+
+logger = logging.getLogger(__name__)
+
+
+def gui_main(pipeline_command):
+    from ..gtk_matplotlib import DebugScannerView
+
+    gtk.gdk.threads_init()
+    scanner = BarcodeScanner()
+
+    with nostderr():
+        pipeline, status = scanner.start(pipeline_command)
+
+    def on_exit(*args):
+        scanner.stop()
+        gtk.main_quit()
+
+    window = gtk.Window()
+    scanner_view = DebugScannerView(scanner)
+    window.add(scanner_view.widget)
+    window.connect('destroy', on_exit)
+    window.show_all()
+
+    gtk.main()
+
+
+def parse_args(args=None):
+    """Parses arguments, returns (options, args)."""
+
+    if args is None:
+        args = sys.argv
+
+    parser = ArgumentParser(description='Barcode scanner based on GStreamer, '
+                            'zbar, and gtk.')
+    log_levels = ('critical', 'error', 'warning', 'info', 'debug', 'notset')
+
+    parser_pipeline = ArgumentParser(add_help=False)
+    parser_pipeline.add_argument('-i', '--interactive', action='store_true',
+                                 help='Do not start main loop.')
+    parser_pipeline.add_argument('-l', '--log-level', type=str, choices=log_levels,
+                        default='info')
+
+    default_pipeline = [
+        'autovideosrc name=video-source', '!',
+        'ffmpegcolorspace', '!',
+        'video/x-raw-rgb,format=(fourcc)I420,framerate=30/1,'
+        'width=640,height=480', '!',
+        'videorate', '!',
+        'appsink',
+            'name=app-video',
+            #'enable-last-buffer=true',
+            'emit-signals=true',
+            #'sync=true',
+    ]
+    default_pipeline_command = " ".join(default_pipeline)
+
+    subparsers = parser.add_subparsers(help='sub-command help', dest='command')
+
+    parser_launch = subparsers.add_parser('launch', help='Configure pipeline '
+                                          'using `gst-launch` syntax.',
+                                          parents=[parser_pipeline])
+    parser_launch.add_argument('pipeline', nargs='?',
+                               default=default_pipeline_command,
+                               help='Default: %(default)s')
+
+    parser_json = subparsers.add_parser('fromjson', help='Configure pipeline'
+                                        'from json object including: '
+                                        'device_name, width, height, '
+                                        'framerate_num, framerate_denom',
+                                        parents=[parser_pipeline])
+    parser_json.add_argument('json', help='JSON object including: '
+                             'device_name, width, height, framerate_num, '
+                             'framerate_denom')
+
+    subparsers.add_parser('device_list', help='List available device names')
+
+    parser_device_caps = subparsers.add_parser('device_caps', help='List '
+                                               'JSON serialized capabilities '
+                                               'for device (compatible with '
+                                               '`fromjson` subcommand).')
+    parser_device_caps.add_argument('device_name')
+
+    args = parser.parse_args()
+    if hasattr(args, 'log_level'):
+        args.log_level = getattr(logging, args.log_level.upper())
+    return args
+
+
+def main(args=None):
+    args = parse_args(args)
+
+    if args.command == 'launch':
+        gui_main(args.pipeline)
+    elif args.command == 'fromjson':
+        with nostderr():
+            from pygst_utils import pipeline_command_from_json
+        json_config = json.loads(args.json)
+        pipeline_command = pipeline_command_from_json(json_config,
+                                                      colorspace='rgb')
+        gui_main(pipeline_command)
+    elif args.command == 'device_list':
+        with nostderr():
+            from pygst_utils.video_source import get_video_source_names
+
+            print '\n'.join(get_video_source_names())
+    elif args.command == 'device_caps':
+        with nostderr():
+            from pygst_utils.video_source import (expand_allowed_capabilities,
+                                                  get_allowed_capabilities)
+
+            df_allowed_caps = get_allowed_capabilities(args.device_name)
+            df_source_caps = expand_allowed_capabilities(df_allowed_caps)
+            print '\n'.join([c.to_json() for i, c in
+                             df_source_caps.iterrows()])
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/barcode-scanner/barcode-scanner-0.2.post15.zip/barcode-scanner-0.2.post15/pavement.py
@@ -0,0 +1,55 @@
+import platform
+import sys
+
+from paver.easy import task, needs, path, sh, cmdopts, options
+from paver.setuputils import setup, install_distutils_tasks
+from distutils.extension import Extension
+from distutils.dep_util import newer
+
+sys.path.insert(0, path('.').abspath())
+import version
+
+install_requires = ['Pillow>=3.1.0', 'matplotlib>=1.5.0', 'numpy',
+                    'pygst-utils>=0.3.post8',
+                    'wheeler.pygtkhelpers>=0.13.post8', 'zbar']
+
+# Platform-specific package requirements.
+if platform.system() == 'Windows':
+    install_requires += ['opencv-python', 'pygtk2-win', 'pycairo-gtk2-win',
+                         'pygst-0.10-win']
+else:
+    try:
+        import gtk
+    except ImportError:
+        print >> sys.err, ('Please install Python bindings for Gtk 2 using '
+                           'your systems package manager.')
+    try:
+        import cv2
+    except ImportError:
+        print >> sys.err, ('Please install OpenCV Python bindings using your '
+                           'systems package manager.')
+    try:
+        import gst
+    except ImportError:
+        print >> sys.err, ('Please install GStreamer Python bindings using '
+                           'your systems package manager.')
+
+setup(name='barcode-scanner',
+      version=version.getVersion(),
+      description='Barcode scanner based on GStreamer, zbar, and gtk.',
+      keywords='gtk, zbar, gstreamer',
+      author='Christian Fobel and Michael D. M. Dryden',
+      author_email='christian@fobel.net and mdryden@chem.utoronto.ca',
+      url='https://github.com/wheeler-microfluidics/barcode-scanner',
+      license='GPL',
+      packages=['barcode_scanner', ],
+      install_requires=install_requires,
+      # Install data listed in `MANIFEST.in`
+      include_package_data=True)
+
+
+@task
+@needs('generate_setup', 'minilib', 'setuptools.command.sdist')
+def sdist():
+    """Overrides sdist to make sure that our setup.py is generated."""
+    pass
