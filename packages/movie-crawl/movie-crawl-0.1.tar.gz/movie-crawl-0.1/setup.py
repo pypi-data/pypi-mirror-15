c1a3d53d716463e9bf245ae2e68b5fc693d868b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:08 +0000

    Add package movie-crawl/0.1/movie-crawl-0.1.tar.gz

---
+++ packages/movie-crawl/movie-crawl-0.1.tar.gz/movie-crawl-0.1/setup.py
@@ -0,0 +1,24 @@
+'''
+    This source code is licensed under the MIT license found in the
+    LICENSE file in the root directory of this source tree.
+'''
+from distutils.core import setup
+setup(
+    name='movie-crawl',
+    packages=['movie-crawl'],
+    version='0.1',
+    description='Movie Crawler Library',
+    author='Sharvil Katariya',
+    author_email='sharvil.2009@gmail.com',
+    url='https://github.com/scorpionhiccup/pip-package',
+    # I'll explain this in a second
+    download_url='https://github.com/scorpionhiccup/pip-package/tarball/0.1',
+    keywords=['crawler', 'movie', 'ticket', \
+        'testing', 'logging'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
