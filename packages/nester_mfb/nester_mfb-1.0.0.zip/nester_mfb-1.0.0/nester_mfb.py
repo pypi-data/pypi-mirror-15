614e2ab662a2b0f33615029f065a5714396a70c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:10 +0000

    Add package nester_mfb/1.0.0/nester_mfb-1.0.0.zip

---
+++ packages/nester_mfb/nester_mfb-1.0.0.zip/nester_mfb-1.0.0/nester_mfb.py
@@ -0,0 +1,14 @@
+﻿""" Este es el modulo "nester_mfb.py", ofrece una funcion "print_lol"
+    que muestra listas que pueden contener otras listas (o no)
+"""
+def print_lol(the_list):
+	""" Esta funcion tiene un parametro posicional "the_list"
+            que es un lista de Phyton que puede contener otra sublistas-
+            Cada elemento en la lista es (recursivamente) mostrado
+	    en la pantalla en su propia linea.
+	"""
+	for item in the_list:
+		if isinstance(item, list):
+			print_lol(item)
+		else:
+			print(item)
