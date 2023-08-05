bb9f0f2cd0221e1f09a557dbc8dc7b75846545ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:33 +0000

    Add package simplewebshell/0.1.0/simplewebshell-0.1.0.tar.gz

---
+++ packages/simplewebshell/simplewebshell-0.1.0.tar.gz/simplewebshell-0.1.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from distutils.command.install import INSTALL_SCHEMES
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+setup(
+
+    name='simplewebshell',
+    version='0.1.0',
+    description='simplewebshell',
+    long_description='',
+    url='http://github.com/krotkiewicz/simplewebshell',
+    author='Konrad Rotkiewicz',
+    author_email='konrad.rotkiewicz@gmail.com',
+    license='MIT',
+    py_modules=['simplewebshell'],
+    packages=find_packages(),
+    include_package_data=True,
+    data_files=[('', ['index.html', 'termlib.js'])],
+    entry_points = {
+        'console_scripts': [
+            'simplewebshell = simplewebshell:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
