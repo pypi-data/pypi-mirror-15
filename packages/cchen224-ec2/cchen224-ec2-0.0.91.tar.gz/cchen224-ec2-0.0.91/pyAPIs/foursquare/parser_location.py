eb1f0637f886d327bfe2e5bc21e59e4e798da75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:57 +0000

    Add package cchen224-ec2/0.0.91/cchen224-ec2-0.0.91.tar.gz

---
+++ packages/cchen224-ec2/cchen224-ec2-0.0.91.tar.gz/cchen224-ec2-0.0.91/pyAPIs/foursquare/parser_location.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+
+def get_city(latlong, access_token):
+    url = get_url(latlong, access_token)
+
+    try:
+        r = requests.get(url)
+        jsontext = json.loads(r.text)
+
+        venue = jsontext['response']['venues'][0]
+        city = venue['location'].get('city', '').encode('utf-8')
+        state = venue['location'].get('state', '').encode('utf-8')
+        country = venue['location'].get('country', '').encode('utf-8')
+        return [city, state, country]
+    except:
+        return []
+
+
+def get_url(latlong, access_token):
+    return 'https://api.foursquare.com/v2/venues/search?ll=' + str(latlong) + '&query=&limit=1&oauth_token=' + access_token + '&v=20151216'
