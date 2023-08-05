91982bdf008f60bdd49086d5a0ebdeb34e9594e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:20 +0000

    Add package sigopt-sklearn/1.1.0/sigopt_sklearn-1.1.0.tar.gz

---
+++ packages/sigopt-sklearn/sigopt_sklearn-1.1.0.tar.gz/sigopt_sklearn-1.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from sigopt_sklearn.version import VERSION
+
+# keep this in sync with requirements.txt
+install_requires=[
+   'sigopt',
+   'numpy',
+   'sklearn',
+   'joblib']
+
+setup(
+  name='sigopt_sklearn',
+  version=VERSION,
+  description='SigOpt + scikit-learn Integrations',
+  author='SigOpt',
+  author_email='support@sigopt.com',
+  url='https://sigopt.com/',
+  packages=['sigopt_sklearn'],
+  install_requires=install_requires,
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ]
+)
