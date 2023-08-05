405cfdcf264342da4e35b25922e1b7022c1f190a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:56 +0000

    Add package ff-tool/0.0.6/ff_tool-0.0.6-py2.7.egg

---
+++ packages/ff-tool/ff_tool-0.0.6-py2.7.egg/fftool/__init__.py
@@ -0,0 +1,30 @@
+import os
+from subprocess import Popen, PIPE
+from firefox_env_handler import IniHandler
+from outlawg import Outlawg
+
+
+__version__ = '0.0.6'
+
+
+CHANNELS = ['release',
+            'beta',
+            'aurora',
+            'nightly']
+
+DEFAULT_CHANNEL = 'nightly'
+
+DIR_TEMP = '_temp'
+DIR_TEMP_BROWSERS = os.path.join(DIR_TEMP, 'browsers')
+DIR_CONFIGS = 'fftool/configs'
+OS_CONFIG = IniHandler()
+OS_CONFIG.load_os_config(DIR_CONFIGS)
+DIR_TEMP_PROFILES = os.path.join(DIR_TEMP, 'profiles')
+PATH_PREFS_ROOT = os.environ.get('PATH_PREFS_ROOT')
+
+Log = Outlawg()
+
+
+def local(cmd):
+    output = Popen(cmd, stdout=PIPE, shell=True)
+    return output.stdout.read().strip()
