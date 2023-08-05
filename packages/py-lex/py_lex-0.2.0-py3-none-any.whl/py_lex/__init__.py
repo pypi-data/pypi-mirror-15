528bd5d6db6a4f1fc48ed2ecec5275784e2302f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:10 +0000

    Add package py-lex/0.2.0/py_lex-0.2.0-py3-none-any.whl

---
+++ packages/py-lex/py_lex-0.2.0-py3-none-any.whl/py_lex/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import (ascii, bytes, chr, dict, filter, hex, input,
+                      int, map, next, oct, open, pow, range, round,
+                      str, super, zip)
+
+from py_lex.liwc import Liwc
+from py_lex.liwc_parser import LiwcParser
+from py_lex.emo_lex import EmoLex
+from py_lex.nrc_discrete_parser import NrcDiscreteParser
