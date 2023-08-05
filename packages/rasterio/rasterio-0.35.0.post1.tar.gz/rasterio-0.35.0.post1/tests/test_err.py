855ad1b0f5cf474a422534c9a3f217485578a9d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:43 +0000

    Add package rasterio/0.35.0.post1/rasterio-0.35.0.post1.tar.gz

---
+++ packages/rasterio/rasterio-0.35.0.post1.tar.gz/rasterio-0.35.0.post1/tests/test_err.py
@@ -0,0 +1,26 @@
+# Testing use of cpl_errs
+
+import pytest
+
+import rasterio
+from rasterio.env import Env
+from rasterio.errors import RasterioIOError
+
+
+def test_io_error(tmpdir):
+    """RasterioIOError is raised when a disk file can't be opened.
+    Newlines are removed from GDAL error messages."""
+    with pytest.raises(RasterioIOError) as exc_info:
+        rasterio.open(str(tmpdir.join('foo.tif')))
+    msg, = exc_info.value.args
+    assert "\n" not in msg
+
+
+def test_io_error_env(tmpdir):
+    with pytest.raises(RasterioIOError):
+        rasterio.open(str(tmpdir.join('foo.tif')))
+
+
+def test_bogus_band_error():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        assert src._has_band(4) is False
