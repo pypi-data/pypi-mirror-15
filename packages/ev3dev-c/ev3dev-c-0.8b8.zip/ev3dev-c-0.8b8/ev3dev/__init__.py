283304987a11823275b9bc02b2fd604bb5a8820b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:10 +0000

    Add package ev3dev-c/0.8b8/ev3dev-c-0.8b8.zip

---
+++ packages/ev3dev-c/ev3dev-c-0.8b8.zip/ev3dev-c-0.8b8/ev3dev/__init__.py
@@ -0,0 +1,9 @@
+from ev3 import *
+
+__library__ = "ev3dev-c"
+__version__ = "0.8b8"
+__kernel__ = "3.16.7-ckt26-10-ev3dev-ev3"
+__description__ = "LEGO Mindstorms EV3 Debian C library + Python, Ruby and Perl wrappers"
+__author__ = "Vitaly Kravtsov"
+__author_email__ = "in4lio@gmail.com"
+__url__ = "https://github.com/in4lio/ev3dev-c"
