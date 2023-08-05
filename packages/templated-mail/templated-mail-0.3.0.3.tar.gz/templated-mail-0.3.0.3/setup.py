b0d04d245773a78c2ff8ed26989cec60ab5c7fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:23 +0000

    Add package templated-mail/0.3.0.3/templated-mail-0.3.0.3.tar.gz

---
+++ packages/templated-mail/templated-mail-0.3.0.3.tar.gz/templated-mail-0.3.0.3/setup.py
@@ -0,0 +1,30 @@
+"""
+Package that allows defining an email subject, 
+plain text, & html in a template, thus keeping it 
+separate from application logic, and providing a 
+consistent way to interpolate data into it. 
+
+"""
+
+from distutils.core import setup
+import setuptools  # this import is needed so that some options and commands work
+
+setup(
+    name='templated-mail',
+    version='0.3.0.3',
+    author='Brian E. Peterson',
+    author_email='bepetersondev@gmail.com',
+    url='https://github.com/bepetersn/templated-mail',
+    zip_safe=False,
+    description=__doc__,
+    packages=[
+        'templated_mail'
+    ],
+    install_requires=[
+        'simple_mail==0.1.5.1',
+        'mock',
+        'bunch',
+        'jinja2',
+        'simple_configparser'
+    ],
+)
