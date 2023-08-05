cfa6cef06a3ba7446b4f6727fca06733604d96b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package untwisted/1.0.1/untwisted-1.0.1.tar.gz

---
+++ packages/untwisted/untwisted-1.0.1.tar.gz/untwisted-1.0.1/setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python2
+
+from distutils.core import setup
+setup(name="untwisted",
+      version="1.0.1",
+      packages=["untwisted", 
+                "untwisted.plugins"],
+      scripts=['quickserv'],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/untwisted',
+      download_url='https://github.com/iogf/untwisted/releases',
+      keywords=['asynchronous', 'programming', 'twisted', 'untwisted', 'library', 'framework', 'networking', 'protocols', 'rapidserv', 'irc', 'requests'],
+      classifiers=[],
+      description="A library for asynchronous programming in python.",)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
