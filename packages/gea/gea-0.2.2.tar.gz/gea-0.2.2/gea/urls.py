eb0b4bea0e9a9299a1fddff270052106597a3e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:50 +0000

    Add package gea/0.2.2/gea-0.2.2.tar.gz

---
+++ packages/gea/gea-0.2.2.tar.gz/gea-0.2.2/gea/urls.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+
+from gea.views import Home, catastros_locales, lugares, secciones, manzanas, \
+parcelas, listado_alfabetico, caratula, solicitud, visacion, plano, set, \
+catastro, dvapi, ExpedienteList, ExpedienteDetail, PersonaList, PersonaDetail, \
+CatastroLocalList
+
+urlpatterns = [
+    url(r'^$', Home.as_view(), name='home'),
+    # Catastros Locales
+    url(r'^catastros-locales/$', CatastroLocalList.as_view(), name='catastros_locales'),
+#     url(r'^catastros-locales/$', catastros_locales, name='catastros_locales'),
+#     url(r'^catastros-locales/(?P<l>[^/]+)/$', lugares),
+#     url(r'^catastros-locales/(?P<l>[^/]+)/(?P<s>[^/]+)/$', secciones),
+#     url(r'^catastros-locales/(?P<l>[^/]+)/(?P<s>[^/]+)/(?P<m>[^/]+)/$',
+#         manzanas),
+    url(r'^catastros-locales/%s' %
+    '(?P<l>[^/]+)/(?P<s>[^/]+)/(?P<m>[^/]+)/(?P<p>[^/]+)/$', parcelas),
+    # Listado alfabético
+    url(r'^listado-alfabetico/$', listado_alfabetico,
+        name='listado_alfabetico'),
+    url(r'^listado-alfabetico/(?P<inicial>[^/]+)/$', listado_alfabetico,
+        name="listado_alfabetico"),
+    # Expedientes
+    url(r'^expedientes/$', ExpedienteList.as_view(), name="expedientes"),
+    url(r'^expedientes/(?P<pk>\d+)/$', ExpedienteDetail.as_view(),
+        name="expediente"),
+    # Personas
+    url(r'^personas/$', PersonaList.as_view(), name="personas"),
+    url(r'^personas/(?P<pk>\d+)/$', PersonaDetail.as_view(),
+        name="persona"),
+    # Notas
+    url(r'^caratula/$', caratula, name="caratula"),
+    url(r'^solicitud/$', solicitud, name="solicitud"),
+    url(r'^visacion/$', visacion, name="visacion"),
+    # Búsquedas
+    url(r'^plano/$', plano),
+    url(r'^set/$', set),
+    url(r'^catastro/$', catastro),
+    # Herramientas
+    url(r'^dvapi/$', dvapi, name='dvapi'),
+]
