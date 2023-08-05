663b400b0283c777479737d274e8838cded1cf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package mdtraj/1.7.2/mdtraj-1.7.2.zip

---
+++ packages/mdtraj/mdtraj-1.7.2.zip/mdtraj-1.7.2/mdtraj/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '1.7.2'
+short_version = '1.7.2'
+full_version = '1.7.2'
+git_revision = '2602839a1d8dfffcc8d6b37c4de8461b0c3fb531'
+release = True
