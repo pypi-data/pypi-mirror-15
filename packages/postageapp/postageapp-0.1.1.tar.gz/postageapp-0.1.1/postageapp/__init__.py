06521c2675d6cbbc991eaf18f21cd4929fd741e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:00 +0000

    Add package postageapp/0.1.1/postageapp-0.1.1.tar.gz

---
+++ packages/postageapp/postageapp-0.1.1.tar.gz/postageapp-0.1.1/postageapp/__init__.py
@@ -0,0 +1,12 @@
+# PostageApp Python Plugin
+
+__title__ = 'postageapp'
+__version__ = '0.1.1'
+__author__ = 'Scott Tadman'
+__license__ = 'MIT'
+__copyright__ = '(C) 2016 Scott Tadman, PostageApp'
+
+from request import Request
+from config import Config
+
+config = Config()
