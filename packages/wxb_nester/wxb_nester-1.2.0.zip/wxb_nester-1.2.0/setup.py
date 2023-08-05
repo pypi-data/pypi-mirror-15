0c1ce5d4b26637534c64a66ab45acdd4f9442439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package wxb_nester/1.2.0/wxb_nester-1.2.0.zip

---
+++ packages/wxb_nester/wxb_nester-1.2.0.zip/wxb_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        ='wxb_nester',
+    version     = '1.2.0',
+    py_modules  = ['wxb_nester'],
+    author      ='hfpython',
+    author_email = 'wxb3469@163.com',
+    url         ='http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
