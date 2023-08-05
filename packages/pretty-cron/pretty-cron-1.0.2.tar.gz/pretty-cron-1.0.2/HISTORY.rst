dd1c41874278758cfe9d8e2361a176c349399564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:49 +0000

    Add package pretty-cron/1.0.2/pretty-cron-1.0.2.tar.gz

---
+++ packages/pretty-cron/pretty-cron-1.0.2.tar.gz/pretty-cron-1.0.2/HISTORY.rst
@@ -0,0 +1,28 @@
+.. :changelog:
+
+History
+-------
+
+Pending Release
+---------------
+
+* New release notes here
+
+1.0.2 (2016-05-03)
+------------------
+
+* Now supports expressions with multiple weekdays and ordinal days - thanks
+  @jbondia.
+
+1.0.1 (2016-02-09)
+------------------
+
+* Now interprets day 7 as Sunday as well as 0, like Linux crontab parsers -
+  thanks @vetyy.
+* Now supports expressions with multiple months separated by commas - thanks
+  @MerreM.
+
+1.0.0 (2015-07-28)
+------------------
+
+* First release on PyPI, featuring ``prettify_cron`` function.
---
+++ packages/pretty-cron/pretty-cron-1.0.2.tar.gz/pretty-cron-1.0.2/setup.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+
+try:
+    from setuptools import setup
+    from setuptools.command.test import test as TestCommand
+except ImportError:
+    from distutils.core import setup
+    from distutils.command.test import test as TestCommand
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `__init__.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+version = get_version('pretty_cron')
+
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='pretty-cron',
+    version=version,
+    description="Converts crontab expressions to human-readable descriptions.",
+    long_description=readme + '\n\n' + history,
+    author="Adam Johnson",
+    author_email='me@adamj.eu',
+    url='https://github.com/adamchainz/pretty-cron',
+    packages=get_packages('pretty_cron'),
+    include_package_data=True,
+    install_requires=[],
+    license="MIT",
+    zip_safe=False,
+    keywords='cron',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+)
