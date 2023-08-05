626fee7141e60af7e6afbe8e969e94131619c260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package flist/0.7/flist-0.7.zip

---
+++ packages/flist/flist-0.7.zip/flist-0.7/flist/__init__.py
@@ -0,0 +1,4 @@
+from .flist import flist, iflist
+__all__ = [ "flist", "iflist" ]
+__version__ = "0.7"
+__version_info__ = ( 0, 7, 0 )
---
+++ packages/flist/flist-0.7.zip/flist-0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name = 'flist',
+    packages = ['flist'],
+    version = '0.7',
+    description = 'File List Expansion',
+    author = 'Avner Herskovits',
+    author_email = 'avnr_ at outlook.com',
+    url = 'https://github.com/avnr/flist',
+    download_url = 'https://github.com/avnr/flist/tarball/0.602',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: No Input/Output (Daemon)',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities',
+    ],
+)
