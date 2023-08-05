a92dc0d43b4f061cf0d67ef3414f6555b7e11dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:10 +0000

    Add package PySteamWeb/1.0.1/PySteamWeb-1.0.1.zip

---
+++ packages/PySteamWeb/PySteamWeb-1.0.1.zip/PySteamWeb-1.0.1/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='PySteamWeb',
+    version='1.0.1',
+    packages=['pysteamweb'],
+    url='https://github.com/patryk4815/PySteamWeb',
+    license='MIT',
+    author='Patryk Sondej',
+    author_email='patryk.sondej@gmail.com',
+    description='python3 steam web login async',
+    keywords=['python3', 'steam', 'login', 'api', 'async', 'aiohttp', 'asyncio', 'asynchronous'],
+    platforms='Posix; MacOS X; Windows',
+    install_requires=[
+        'pycrypto>=2.6',
+        'aiohttp>=0.21',
+    ],
+    include_package_data=True,
+)
