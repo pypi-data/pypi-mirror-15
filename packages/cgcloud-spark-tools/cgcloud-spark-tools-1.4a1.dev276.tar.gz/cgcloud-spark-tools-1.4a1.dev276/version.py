54bf6f17ab8e24f4aabcaae25b09b647337276bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package cgcloud-spark-tools/1.4a1.dev276/cgcloud-spark-tools-1.4a1.dev276.tar.gz

---
+++ packages/cgcloud-spark-tools/cgcloud-spark-tools-1.4a1.dev276.tar.gz/cgcloud-spark-tools-1.4a1.dev276/version.py
@@ -0,0 +1,5 @@
+boto_dep='boto==2.38.0'
+bd2k_python_lib_dep='bd2k-python-lib>=1.11.dev6'
+cgcloud_version='1.4a1.dev276'
+parse_version='<function parse_version at 0x2b9d2f3e7488>'
+fabric_dep='Fabric==1.10.2'
