81099965924465a95eb12fd7052aed1d9e445217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package jsonify/0.0.4/jsonify-0.0.4.tar.gz

---
+++ packages/jsonify/jsonify-0.0.4.tar.gz/jsonify-0.0.4/setup.py
@@ -0,0 +1,19 @@
+"""
+A tool for quickly converting .csv files 
+to .json format
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+setup(
+    name='jsonify',
+    version='0.0.4',
+    packagers=['jsonify',],
+    description='A csv to json converter',
+    author='Jonathan Cox',
+    author_email='jonathan.cox.c@gmail.com',
+    license='MIT',
+    keywords='csv json'
+)
