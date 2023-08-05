67195ef5b575c1ba10b1d8c2ee7c94628ff5f527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:06 +0000

    Add package nester001/1.2.0/nester001-1.2.0.zip

---
+++ packages/nester001/nester001-1.2.0.zip/nester001-1.2.0/nester.py
@@ -0,0 +1,24 @@
+
+movies=[
+	"The Holy Grail",1975,"Terry Jones & Terry Gilliam",91,
+	  ["Graham Chapman",
+	   ["Michael Palin","John Cleese","Terry Gilliam","Eric Idle","Terry Jones"]]]
+"""This id the"nester.py"module and it provodes one function called print_lol() while prints lists that may not include nested lists."""
+def print_lol(the_list,level=0):
+   """This function takes one pstitional argument called "the list",whitch is any python list(of-possibly-nested lists).Sach data item in the provided list is (recursively)printed to the screen on it'own line"""
+   for each_item in the_list:
+      if isinstance(each_item,list):
+         print_lol(each_item,level+1)
+      else:
+         for tab_stop in range(level):
+            print("\t",end='')
+         print(each_item)
+
+import nester
+
+cast=['palin','cleese','Idle','Jones','Gilliam','Chapman']
+nester.print_lol(cast)
+
+
+
+
