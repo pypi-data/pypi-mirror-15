ba76b68ce5594dc1d16fc3da1db9f4306d24243c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:07 +0000

    Add package django_custom_500/0.1.0/django_custom_500-0.1.0-py2.py3-none-any.whl

---
+++ packages/django_custom_500/django_custom_500-0.1.0-py2.py3-none-any.whl/django_custom_500/handlers.py
@@ -0,0 +1,30 @@
+# !/usr/bin/python
+# coding=utf-8
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+from django.conf import settings
+from django.http import HttpResponseServerError
+from django.template import Context, loader
+
+
+def handler500(request):
+    """
+    500 error handler which includes ``request`` in the context.
+
+    Template: `500.html`
+    Context: None
+    """
+
+    template_500 = getattr(settings, 'CUSTOM_500_TEMPLATE', '500.html')
+    template = loader.get_template(template_500)
+
+    return HttpResponseServerError(
+        template.render(
+            Context(
+                {
+                    'request': request
+                }
+            )
+        )
+    )
