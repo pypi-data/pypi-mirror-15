6fdb470d910d3c8e3e0a00cd610be55ffd57ecd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:31 +0000

    Add package lj_commons/0.1/lj_commons-0.1.tar.gz

---
+++ packages/lj_commons/lj_commons-0.1.tar.gz/lj_commons-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lj_commons',
+      version='0.1',
+      description='The common parameters, methods and classes for Luojilab Pythonists',
+      url='',
+      author='Fu Jian',
+      author_email='fujian_en@126.com',
+      license='MIT',
+      packages=['lj_commons'],
+      zip_safe=False)
