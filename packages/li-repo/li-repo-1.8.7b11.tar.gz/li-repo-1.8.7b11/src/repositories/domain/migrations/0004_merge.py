ada5a6564153a998cd33be22509038d5f1bb64c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:32 +0000

    Add package li-repo/1.8.7b11/li-repo-1.8.7b11.tar.gz

---
+++ packages/li-repo/li-repo-1.8.7b11.tar.gz/li-repo-1.8.7b11/src/repositories/domain/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('domain', '0003_auto_20160426_2312'),
+        ('domain', '0003_auto_20160426_2238'),
+    ]
+
+    operations = [
+    ]
