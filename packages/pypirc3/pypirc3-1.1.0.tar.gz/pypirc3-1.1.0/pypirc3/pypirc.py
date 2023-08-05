51d18073de5932ddd15f5d3d187d49b45f205d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:07 +0000

    Add package pypirc3/1.1.0/pypirc3-1.1.0.tar.gz

---
+++ packages/pypirc3/pypirc3-1.1.0.tar.gz/pypirc3-1.1.0/pypirc3/pypirc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from pypirc3.option import *
+from pypirc3.executor.creator import Creator
+from pypirc3.executor.viewer import Viewer
+
+class Pypirc(object):
+
+    def __init__(self, action, username='', password=''):
+        path = self._build_path()
+
+        if action == ACTION_CREATE:
+            self.executor = Creator(path, username, password)
+        else:
+            self.executor = Viewer(path)
+
+    def _build_path(self):
+        return os.path.join(os.path.expanduser('~'), '.pypirc')
+
+    def execute(self):
+        return self.executor.execute()
+
