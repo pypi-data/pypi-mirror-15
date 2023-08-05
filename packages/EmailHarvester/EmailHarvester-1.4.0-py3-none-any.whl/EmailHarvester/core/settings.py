f5c717fd8516bd5e4e1f7a7ae3fa72c083faf852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:48 +0000

    Add package EmailHarvester/1.4.0/EmailHarvester-1.4.0-py3-none-any.whl

---
+++ packages/EmailHarvester/EmailHarvester-1.4.0-py3-none-any.whl/EmailHarvester/core/settings.py
@@ -0,0 +1,16 @@
+from .splash import DEFAULT
+import os
+
+__author__ = "maldevel"
+__copyright__ = "Copyright (c) 2016 @maldevel"
+__credits__ = ["maldevel", "PaulSec", "cclauss", "Christian Martorella", "Hervé Beraud"]
+__license__ = "GPLv3"
+__version__ = "1.4.0"
+__maintainer__ = "Hervé Beraud"
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SPLASH_SCREEN = DEFAULT
+
+DFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"
+
