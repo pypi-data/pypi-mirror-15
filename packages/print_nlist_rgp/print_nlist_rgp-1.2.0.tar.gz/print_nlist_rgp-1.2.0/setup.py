2d72aa66ab07227e145e6a85edbd0793997e9301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package print_nlist_rgp/1.2.0/print_nlist_rgp-1.2.0.tar.gz

---
+++ packages/print_nlist_rgp/print_nlist_rgp-1.2.0.tar.gz/print_nlist_rgp-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'print_nlist_rgp',
+    version = '1.2.0',
+    py_modules = ['print_nlist_rgp'],   #name should be same as .py filename
+    author = 'ec_rahul46',
+    author_email = 'ec_rahul46@yahoo.com',
+    url = 'http://profrahulpatel.weebly.com/',
+    description = 'A simple, recurrsion based printer of nested lists',
+    )
