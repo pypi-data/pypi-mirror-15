baf6859409a36b7764bd2524678d558afc4b1ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:01 +0000

    Add package zess_chapter6/1.2.0/zess_chapter6-1.2.0.zip

---
+++ packages/zess_chapter6/zess_chapter6-1.2.0.zip/zess_chapter6-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'zess_chapter6',
+    version = '1.2.0',
+    py_modules = ['zess_chapter6'],
+    author = 'zess',
+    author_email = 'zess1982@126.com',
+    url = 'http://satellite.sinaapp.com/',
+    description = 'Class, add methods.',
+    )
