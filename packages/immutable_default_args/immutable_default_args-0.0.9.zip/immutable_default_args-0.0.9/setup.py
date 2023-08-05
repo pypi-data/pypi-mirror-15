999f495882203f9a6f48bb7d1f26f50474ad2dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:36 +0000

    Add package immutable_default_args/0.0.9/immutable_default_args-0.0.9.zip

---
+++ packages/immutable_default_args/immutable_default_args-0.0.9.zip/immutable_default_args-0.0.9/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+from setuptools import(
+    setup,
+    find_packages
+)
+
+
+description = str("Very small package to automatically safeguard mutable "
+                  "function arguments, preventing them from being modified.")
+
+
+with io.open('README.rst', 'r', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name='immutable_default_args',
+    version='0.0.9',
+    description=description,
+    long_description=readme,
+    url='http://timmwagener.com/',
+    author="Timm Wagener",
+    author_email='wagenertimm@gmail.com',
+    packages=find_packages(),
+    license='MIT',
+    zip_safe=False,
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
