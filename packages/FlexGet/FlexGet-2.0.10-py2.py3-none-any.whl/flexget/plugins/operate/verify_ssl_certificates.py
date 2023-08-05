325d9b80297e6337a18acc5421c13637a7e67b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:36 +0000

    Add package FlexGet/2.0.10/FlexGet-2.0.10-py2.py3-none-any.whl

---
+++ packages/FlexGet/FlexGet-2.0.10-py2.py3-none-any.whl/flexget/plugins/operate/verify_ssl_certificates.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals, division, absolute_import
+from builtins import *  # pylint: disable=unused-import, redefined-builtin
+
+import logging
+
+from flexget import plugin, validator
+from flexget.event import event
+
+log = logging.getLogger('verify_ssl')
+
+
+class VerifySSLCertificates(object):
+    """
+    Plugin that can off SSL certificate verification.
+
+    Example::
+      verify_ssl_certificates: no
+    """
+
+    def validator(self):
+        return validator.factory('boolean')
+
+    @plugin.priority(253)
+    def on_task_start(self, task, config):
+        if config is False:
+            task.requests.verify = False
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(VerifySSLCertificates, 'verify_ssl_certificates', api_ver=2)
