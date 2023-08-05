956ddd35921ff3b24eba85d345e36210ab7d56f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package liu_nester/1.0.0/liu_nester-1.0.0.zip

---
+++ packages/liu_nester/liu_nester-1.0.0.zip/liu_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name     ='liu_nester',
+    version  ='1.0.0',
+    py_modules=['liu_nester'],
+    author    ='hfpyrhon',
+    author_email='hfpython@headfirstlabs.com',
+    url         ='http://www.headfirstlabs.com',
+    description ='a simple printer of nested lists',
+    )
+    
