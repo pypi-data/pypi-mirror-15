e375951023b8897e636ab928386a4281efcfb1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:38 +0000

    Add package Emojipedia/0.1/Emojipedia-0.1.tar.gz

---
+++ packages/Emojipedia/Emojipedia-0.1.tar.gz/Emojipedia-0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Emojipedia',
+      packages=['emojipedia'],
+      version='0.1',
+      description='Emoji data from Emojipedia',
+      author='Benjamin Congdon',
+      author_email='bcongdo2@illinois.edu',
+      url='https://github.com/benjamincongdon',
+      download_url = 'https://github.com/benjamincongdon/python-emojipedia/tarball/0.1',
+      keywords = ['emoji', 'emojipedia'],
+      classifiers = ['Programming Language :: Python :: 2.7',
+                     'Topic :: Communications :: Chat',
+                     'Topic :: Text Processing',
+                     'Topic :: Utilities'],
+      )
