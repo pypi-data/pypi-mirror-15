a437689907544b57bd24cffe1d52596ec383c85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package cgcloud-spark/1.4a1.dev276/cgcloud-spark-1.4a1.dev276.tar.gz

---
+++ packages/cgcloud-spark/cgcloud-spark-1.4a1.dev276.tar.gz/cgcloud-spark-1.4a1.dev276/version.py
@@ -0,0 +1,5 @@
+boto_dep='boto==2.38.0'
+bd2k_python_lib_dep='bd2k-python-lib>=1.11.dev6'
+cgcloud_version='1.4a1.dev276'
+parse_version='<function parse_version at 0x2b6ce9b74488>'
+fabric_dep='Fabric==1.10.2'
