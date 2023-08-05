a8c1762064c4292ca6f65efb046a6f8bf0ebe60d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:21 +0000

    Add package jujuclient/0.52.0/jujuclient-0.52.0.tar.gz

---
+++ packages/jujuclient/jujuclient-0.52.0.tar.gz/jujuclient-0.52.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+long_description = """
+Python client for juju-core websocket api.
+"""
+
+setup(
+    name="jujuclient",
+    version="0.52.0",
+    description="A juju-core/gojuju simple synchronous python api client.",
+    author="Kapil Thangavelu",
+    author_email="kapil.foss@gmail.com",
+    url="http://juju.ubuntu.com",
+    install_requires=["PyYAML", "websocket-client>=0.18.0"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: "
+            "GNU Library or Lesser General Public License (LGPL)",
+        "Intended Audience :: Developers"],
+    py_modules=["jujuclient"])
