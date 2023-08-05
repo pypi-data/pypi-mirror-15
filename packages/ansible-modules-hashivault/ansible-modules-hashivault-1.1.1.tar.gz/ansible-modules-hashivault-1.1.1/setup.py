b47f48752c4ef45f1627259836f6620ad6be4e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:48 +0000

    Add package ansible-modules-hashivault/1.1.1/ansible-modules-hashivault-1.1.1.tar.gz

---
+++ packages/ansible-modules-hashivault/ansible-modules-hashivault-1.1.1.tar.gz/ansible-modules-hashivault-1.1.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+files = [
+    "ansible/module_utils",
+    "ansible/modules/hashivault",
+]
+
+setup(name='ansible-modules-hashivault',
+    version='1.1.1',
+    description='Ansible Modules for Hashicorp Vault',
+    author='Terry Howe',
+    author_email='terrylhowe@example.com',
+    url='https://github.com/TerryHowe/ansible-modules-hashivault',
+    packages=files,
+    install_requires = [
+        'hvac',
+        'ansible>2.0.0',
+    ],
+)
