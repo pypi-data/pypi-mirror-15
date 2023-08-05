111b2fc260a9cfd80f7f47acc9afa1442089882d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:12 +0000

    Add package django-parler/1.6.3/django-parler-1.6.3.tar.gz

---
+++ packages/django-parler/django-parler-1.6.3.tar.gz/django-parler-1.6.3/parler/tests/test_admin.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+try:
+    from django.contrib.admin.utils import label_for_field
+except ImportError:
+    from django.contrib.admin.util import label_for_field
+from .utils import AppTestCase
+from .testapp.models import SimpleModel, ConcreteModel, AbstractModel
+
+
+class AdminTests(AppTestCase):
+    """
+    Test admin features
+    """
+
+    def test_list_label(self):
+        # See that adding a field to the admin list_display also receives the translated title
+        # This happens by TranslatedFieldDescriptor.short_description
+        self.assertEqual(label_for_field('tr_title', SimpleModel), "Translated Title")
+
+    def test_list_label_abc(self):
+        # See that the TranslatedFieldDescriptor of the concrete model properly routes to the proper model
+        self.assertEqual(label_for_field('tr_title', ConcreteModel), "Translated Title")
+
+        # See that the TranslatedFieldDescriptor of the abstract model handles the fallback properly.
+        self.assertEqual(label_for_field('tr_title', AbstractModel), "Tr title")
