66314867d9e7ec94b0da6012e3c69635b822e7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:47 +0000

    Add package pyuarm/1.2.3/pyuarm-1.2.3.tar.gz

---
+++ packages/pyuarm/pyuarm-1.2.3.tar.gz/pyuarm-1.2.3/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+import pyuarm
+setup(name='pyuarm',
+    version=pyuarm.VERSION,
+    py_modules=['pyuarm'],
+    author='Joey Song/Alex Tan',
+    packages=['pyuarm'],
+    author_email='developer@ufactory.cc',
+    description='A python library for uArm',
+    url="https://github.com/uarm-developer/pyuarm",
+    keywords="pyuarm uarm4py uarmForPython uarm ufactory",
+    install_requires = ['pyserial>=3.0'],
+)
