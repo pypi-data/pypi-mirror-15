95fcb55d0b62867e6ff0a008cf29b87ecf6fc084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:51 +0000

    Add package PokeapiWrapper/0.6/PokeapiWrapper-0.6.tar.gz

---
+++ packages/PokeapiWrapper/PokeapiWrapper-0.6.tar.gz/PokeapiWrapper-0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name = "PokeapiWrapper",
+    version = "0.6",
+    packages = ["pokeapi",],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['requests'],
+
+    # metadata for upload to PyPI
+    author = "Adithya Mohan",
+    author_email = "Socketphys@gmail.com",
+    description = "This is a Pokeapi wrapper",
+    license = "MIT",
+    keywords = "pokeapi wrapper",
+    url = "https://github.com/SocketPhys/Pokeapi-Wrapper",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+)
