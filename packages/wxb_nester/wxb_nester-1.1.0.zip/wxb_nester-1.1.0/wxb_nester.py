aab5ec5b787d83febbed79a745c613677cf5376b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:59 +0000

    Add package wxb_nester/1.1.0/wxb_nester-1.1.0.zip

---
+++ packages/wxb_nester/wxb_nester-1.1.0.zip/wxb_nester-1.1.0/wxb_nester.py
@@ -0,0 +1,12 @@
+"""this is the standard way to include a multiple-line comment in your code."""
+
+def print_lol(the_list,level):
+       
+        for pri in the_list:
+                if isinstance(pri,list):
+                        print_lol(pri,level+1)
+                       
+                else:
+                        for prij in range(level):
+                          print("\t",end='')
+                        print(pri)
