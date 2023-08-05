aef1605a78733c066823f70812bed6f3687b90d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:11 +0000

    Add package leo-funniest/0.1/leo-funniest-0.1.tar.gz

---
+++ packages/leo-funniest/leo-funniest-0.1.tar.gz/leo-funniest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="leo-funniest",
+      version="0.1",
+      description="The funniest joke in the world",
+      url="http://github.com/jwplayer/discovery-integration-tests",
+      author="Flying Circus",
+      author_email="flyingcircus@example.com",
+      license="MIT",
+      packages=["funniest"],
+      zip_safe=False)
