676c6252757921426184edace8de383e0c2431f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:27 +0000

    Add package practic_e/1.1.0/practic_e-1.1.0.zip

---
+++ packages/practic_e/practic_e-1.1.0.zip/practic_e-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name="practic_e",
+    version ="1.1.0",
+    py_modules =['practice'],
+    author='William',
+    author_email='china_aisa@live.com'
+
+
+)
