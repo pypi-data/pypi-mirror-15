3e5c3980f1dcfedf5f12aa11e77247c6638099cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:26 +0000

    Add package coala-bears/0.2.1.dev20160510115528/coala_bears-0.2.1.dev20160510115528-py3-none-any.whl

---
+++ packages/coala-bears/coala_bears-0.2.1.dev20160510115528-py3-none-any.whl/bears/go/GoReturnsBear.py
@@ -0,0 +1,16 @@
+from coalib.bearlib.abstractions.Lint import Lint
+from coalib.bears.LocalBear import LocalBear
+
+
+class GoReturnsBear(Lint, LocalBear):
+    executable = 'goreturns'
+    diff_message = "Imports or returns need to be added/removed."
+    use_stdin = True
+    gives_corrected = True
+    LANGUAGES = "Go"
+
+    def run(self, filename, file):
+        """
+        Proposes corrections of Go code using gofmt.
+        """
+        return self.lint(filename, file)
