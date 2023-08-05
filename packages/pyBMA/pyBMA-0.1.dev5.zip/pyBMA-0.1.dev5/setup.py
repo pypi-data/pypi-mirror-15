c9d3c4d306669015d0e0f7f31b5765cd8ce6a79a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:25 +0000

    Add package pyBMA/0.1.dev5/pyBMA-0.1.dev5.zip

---
+++ packages/pyBMA/pyBMA-0.1.dev5.zip/pyBMA-0.1.dev5/setup.py
@@ -0,0 +1,23 @@
+from os import path
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+#with open(path.join(here, 'README.md')) as f:
+#    long_description = f.read()
+
+setup(
+    name='pyBMA',
+    version='0.1.dev5',
+    description='Bayesian Model Averaging in Python',
+    long_description="Bayesian Model Averaging in Python",
+    url='https://github.com/JakeColtman/pyBMA',
+    author='Jake Coltman, Jacob Goodwin',
+    author_email='jakecoltman@sky.com',
+    install_requires=[
+        'lifelines',
+        'pandas',
+        'numpy'
+    ]
+)
