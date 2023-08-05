55905e150765319e206646330d81405c06aa36f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:19 +0000

    Add package matchmock/0.5.0/matchmock-0.5.0.tar.gz

---
+++ packages/matchmock/matchmock-0.5.0.tar.gz/matchmock-0.5.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='matchmock',
+    version='0.5.0',
+    description='Hamcrest matchers for mock objects',
+    url='https://github.com/keis/matchmock',
+    py_modules=['matchmock']
+)
