4b22a587967d16dbc7755a6a411fd5c4485c1f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:59 +0000

    Add package codev/0.5.2a5/codev-0.5.2a5-py3-none-any.whl

---
+++ packages/codev/codev-0.5.2a5-py3-none-any.whl/codev/providers/sources/actual.py
@@ -0,0 +1,29 @@
+from codev.source import Source
+import shutil
+from time import time
+from uuid import uuid1
+
+
+class ActualSource(Source):
+    provider_name = 'actual'
+
+    def install(self, performer):
+        filename = uuid1()
+        archive = shutil.make_archive('/tmp/{filename}'.format(filename=filename), 'gztar')
+
+        remote_archive = '/tmp/{filename}.tar.gz'.format(filename=filename)
+        performer.send_file(archive, remote_archive)
+
+        # install gunzip
+        # TODO requirements
+        # performer.install_packages('gzip')
+
+        performer.execute('mkdir -p {directory}'.format(directory=self.directory))
+        performer.execute(
+            'tar -xzf {archive} --directory {directory}'.format(
+                archive=remote_archive, directory=self.directory
+            )
+        )
+
+    def process_options(self, options):
+        return options or str(time())
