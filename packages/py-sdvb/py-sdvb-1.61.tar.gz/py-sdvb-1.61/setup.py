91c565c493d60d54f2beab7d9e9b0cbdb1ae5a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:25 +0000

    Add package py-sdvb/1.61/py-sdvb-1.61.tar.gz

---
+++ packages/py-sdvb/py-sdvb-1.61.tar.gz/py-sdvb-1.61/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='py-sdvb',
+    version='1.61',
+
+    description='Dictionary interface of stardict, support regular expression (regex) search and colored output.',
+    long_description=long_description,
+
+    url='https://github.com/noinil/py_sdvb',
+
+    author='Cheng Tan',
+    author_email='noinil@gmail.com',
+
+    license='GPL3',
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='dictionary, stardict, vocabulary builder, English, Chinese',
+
+    packages = ['pysdvb'],
+
+    install_requires=['pystardict'],
+
+    entry_points={
+        'console_scripts': [
+            'pysdcv=pysdvb.sdcv:translate_word',
+            'pysdvb=pysdvb.sdvb:select_dictionary',
+        ],
+    },
+)
