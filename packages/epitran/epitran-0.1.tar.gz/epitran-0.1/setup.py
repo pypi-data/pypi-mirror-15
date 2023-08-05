47b6c26ea821ed8d47d7c8388a5e3c79a14c60f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:11 +0000

    Add package epitran/0.1/epitran-0.1.tar.gz

---
+++ packages/epitran/epitran-0.1.tar.gz/epitran-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='epitran',
+      version='0.1',
+      description='Tools for transcribing languages into IPA.',
+      url='http://github.com/dmort27/epitran',
+      download_url='http://github.com/dmort27/epitran/tarball/0.1',
+      author='David R. Mortensen',
+      author_email='dmortens@cs.cmu.edu',
+      license='MIT',
+      install_requires=['setuptools',
+                        'unicodecsv',
+                        'regex',
+                        'panphon>=0.3'],
+      scripts=['epitran/bin/epitranscribe.py',
+               'epitran/bin/detectcaps.py',
+               'epitran/bin/connl2ipaspace.py',
+               'epitran/bin/testvectorgen.py'],
+      packages=['epitran'],
+      package_dir={'epitran': 'epitran'},
+      package_data={'epitran': ['data/*.csv', 'data/*.json']},
+      zip_safe=True
+      )
