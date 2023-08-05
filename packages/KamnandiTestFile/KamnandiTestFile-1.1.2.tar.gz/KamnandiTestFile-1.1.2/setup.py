629b794ce0f99b024e9243151951210928ff4040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:57 +0000

    Add package KamnandiTestFile/1.1.2/KamnandiTestFile-1.1.2.tar.gz

---
+++ packages/KamnandiTestFile/KamnandiTestFile-1.1.2.tar.gz/KamnandiTestFile-1.1.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'KamnandiTestFile',
+		version = '1.1.2',
+		py_modules = ['KamnandiTestFile'],
+		author = 'Kamnandi',
+		author_email = 'clyde@c12.co.za',
+		url = '',
+		description = 'A simple printer of nested list',
+		)
