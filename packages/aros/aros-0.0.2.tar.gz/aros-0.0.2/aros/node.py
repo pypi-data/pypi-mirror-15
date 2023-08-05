1a512d5d4f3b6e50b6fc1dcb68ac9f43a691930a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:32 +0000

    Add package aros/0.0.2/aros-0.0.2.tar.gz

---
+++ packages/aros/aros-0.0.2.tar.gz/aros-0.0.2/aros/node.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+
+class Node(object):
+    '''
+
+    '''
+    def __init__(self):
+        self.name = 'node'
+
+    def show_sample(self):
+        print self.name
+
+
---
+++ packages/aros/aros-0.0.2.tar.gz/aros-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+      name="aros",
+      version="0.0.2",
+      description="A Python Robot Operating System",
+      author="Liu Peng",
+      url="http://roboyun.com",
+      license="BSD",
+      packages = ['aros','aros.test'],
+      )
