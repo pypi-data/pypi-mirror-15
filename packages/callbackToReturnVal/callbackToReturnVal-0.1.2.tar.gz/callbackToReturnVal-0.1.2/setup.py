c2c39f46cb2f94311a40b96a6bfb3c826f5c1f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:33 +0000

    Add package callbackToReturnVal/0.1.2/callbackToReturnVal-0.1.2.tar.gz

---
+++ packages/callbackToReturnVal/callbackToReturnVal-0.1.2.tar.gz/callbackToReturnVal-0.1.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+from setuptools import setup
+
+setup(name="callbackToReturnVal",
+    version="0.1.2",
+    description="Wrapper function that returns the value that is sent to a callback value as the value that the wrapper gives",
+    licence='MIT',
+    author="Jeandre Le Roux",
+    author_email="theblazehen@theblazehen.com",
+    url="http://github.com/theblazehen/python-callbackToReturnVal",
+    packages=['callbackToReturnVal'])
