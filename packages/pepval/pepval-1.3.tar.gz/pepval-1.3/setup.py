7f8ea85ffeb6c0224e74f6d6092f5d64980d7ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:56 +0000

    Add package pepval/1.3/pepval-1.3.tar.gz

---
+++ packages/pepval/pepval-1.3.tar.gz/pepval-1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pepval',
+    version='1.3',
+    description = "A PEP8 validator that combines Flake8 and PyDocStyle.",
+    scripts = ['pepval'],
+    author='AJ Holloway',
+    author_email = "ajhollowayvrm@gmail.com",
+    url = "https://pypi.python.org/pypi/pepval/1.3"
+)
