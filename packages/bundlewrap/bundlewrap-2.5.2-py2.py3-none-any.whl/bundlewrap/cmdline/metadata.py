ad65679fed74673fd30a5d12c6ba5b9e8541de27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:54 +0000

    Add package bundlewrap/2.5.2/bundlewrap-2.5.2-py2.py3-none-any.whl

---
+++ packages/bundlewrap/bundlewrap-2.5.2-py2.py3-none-any.whl/bundlewrap/cmdline/metadata.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from json import dumps, JSONEncoder
+
+from ..exceptions import NoSuchNode
+from ..utils.text import force_text, mark_for_translation as _, red
+
+
+class ReprJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        return repr(obj)
+
+
+def bw_metadata(repo, args):
+    try:
+        node = repo.get_node(args['node'])
+    except NoSuchNode:
+        yield _("{x} No such node: {node}").format(
+            node=args['node'],
+            x=red("!!!"),
+        )
+        yield 1
+        raise StopIteration()
+
+    for line in dumps(node.metadata, cls=ReprJSONEncoder, indent=4, sort_keys=True).splitlines():
+        yield force_text(line)
