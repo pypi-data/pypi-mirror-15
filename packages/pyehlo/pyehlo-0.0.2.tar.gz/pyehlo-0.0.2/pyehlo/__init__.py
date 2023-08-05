22d6bdecf7976aaa111b2835b5b153153b26d4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package pyehlo/0.0.2/pyehlo-0.0.2.tar.gz

---
+++ packages/pyehlo/pyehlo-0.0.2.tar.gz/pyehlo-0.0.2/pyehlo/__init__.py
@@ -0,0 +1,1 @@
+from .pyehlo import GmailClient
---
+++ packages/pyehlo/pyehlo-0.0.2.tar.gz/pyehlo-0.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyehlo',
+    version='0.0.2',
+    description='Library for sending email',
+    packages=find_packages(),
+)
