e191a01ad719867ca7d0add7bff020b34fb350a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:56 +0000

    Add package doom-py/0.0.4/doom-py-0.0.4.tar.gz

---
+++ packages/doom-py/doom-py-0.0.4.tar.gz/doom-py-0.0.4/doom_py/CMakeFiles/vizdoompython.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vizdoompython.dir/vizdoom_api_bindings/python/ViZDoomPythonBinding.cpp.o"
+  "CMakeFiles/vizdoompython.dir/vizdoom_api_bindings/python/ViZDoomGamePython.cpp.o"
+  "bin/python/vizdoom.pdb"
+  "bin/python/vizdoom.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vizdoompython.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ packages/doom-py/doom-py-0.0.4.tar.gz/doom-py-0.0.4/doom_py/vizdoom_src/tools/re2c/CMakeFiles/re2c.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/actions.cc" "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/CMakeFiles/re2c.dir/actions.o"
+  "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/code.cc" "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/CMakeFiles/re2c.dir/code.o"
+  "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/dfa.cc" "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/CMakeFiles/re2c.dir/dfa.o"
+  "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/main.cc" "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/CMakeFiles/re2c.dir/main.o"
+  "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/mbo_getopt.cc" "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/CMakeFiles/re2c.dir/mbo_getopt.o"
+  "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/parser.cc" "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/CMakeFiles/re2c.dir/parser.o"
+  "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/scanner.cc" "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/CMakeFiles/re2c.dir/scanner.o"
+  "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/substr.cc" "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/CMakeFiles/re2c.dir/substr.o"
+  "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/translate.cc" "/Users/gdb/third/doom-py/doom_py/vizdoom_src/tools/re2c/CMakeFiles/re2c.dir/translate.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "HAVE_CONFIG_H"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "vizdoom_src/tools/re2c"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
