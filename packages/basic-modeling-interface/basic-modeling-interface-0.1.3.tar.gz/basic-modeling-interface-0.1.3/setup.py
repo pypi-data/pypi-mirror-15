b121a65f64f0c15b0bb07e65222503fd848f3fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:43 +0000

    Add package basic-modeling-interface/0.1.3/basic-modeling-interface-0.1.3.tar.gz

---
+++ packages/basic-modeling-interface/basic-modeling-interface-0.1.3.tar.gz/basic-modeling-interface-0.1.3/setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+from setuptools import setup, find_packages
+from basic_modeling_interface import __version__
+
+
+setup(name='basic-modeling-interface',
+      version=__version__,
+      author='Eric Hutton',
+      author_email='eric.hutton@colorado.edu',
+      url='https://github.com/bmi-forum/bmi-python',
+      # download_url='https://github.com/bmi-forum/bmi-python/tarball/0.1.0',
+      license='MIT',
+      description='Python bindings for the Basic Modeling Interface',
+      long_description=open('README.md').read(),
+      keywords='BMI model coupling',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+      ],
+      packages=find_packages(),
+)
