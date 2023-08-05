b37c51216c9c854d80d786a7bd31cb0cbfa48893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:33 +0000

    Add package infinityctl/0.3.6.2/infinityctl-0.3.6.2.zip

---
+++ packages/infinityctl/infinityctl-0.3.6.2.zip/infinityctl-0.3.6.2/infinityctl/startstop.py
@@ -0,0 +1,32 @@
+import subprocess
+import sys
+import glob
+import os
+import signal
+
+from .config import Config
+from .util import get_and_check_config, print_status
+
+
+# получить PID процесса. только Unix-системы
+def get_pid(name):
+    return subprocess.check_output(["pidof",name])
+
+
+def start(args):
+    config = get_and_check_config(args)
+    build = config.get_build(args.build)
+    folder = build.get('folder')
+
+    dmb = glob.glob(folder + "/*.dmb")
+    if not dmb:
+        print("Билд не скомпилирован")
+        sys.exit(1)
+    cmd = "DreamDaemon {0} {1} -trusted -logself".format(dmb[0], str(config.port))
+    subprocess.call(cmd)
+
+
+def stop(args):
+    pid = int(get_pid('DreamDaemon'))
+    os.kill(pid, signal.SIGTERM)
+
