760a8a357003be94a70c3fd93ca00878a5e0a893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:36 +0000

    Add package eea.alchemy/6.4/eea.alchemy-6.4.zip

---
+++ packages/eea.alchemy/eea.alchemy-6.4.zip/eea.alchemy-6.4/setup.py
@@ -0,0 +1,43 @@
+""" EEA Alchemy Installer
+"""
+import os
+from setuptools import setup, find_packages
+
+NAME = 'eea.alchemy'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
+
+setup(name=NAME,
+      version=VERSION,
+      description=("EEA Alchemy allows you to bulk auto-discover "
+                   "geographical coverage, temporal coverage, keywords "
+                   "and more"),
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='eea alchemy tags tagging',
+      author='Alin Voinea (Eaudeweb), European Environment Agency (EEA)',
+      author_email="webadmin@eea.europa.eu",
+      url='http://svn.eionet.europa.eu/projects/'
+          'Zope/browser/trunk/eea.alchemy',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['eea'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'beautifulsoup4',
+          'plone.api',
+          'eea.faceted.vocabularies',
+          'collective.js.jqueryui',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """
+      )
