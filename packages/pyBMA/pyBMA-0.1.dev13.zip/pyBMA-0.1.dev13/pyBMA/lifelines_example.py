f9163f437076f7dc3d7b5c2193a90cd8933aa58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package pyBMA/0.1.dev13/pyBMA-0.1.dev13.zip

---
+++ packages/pyBMA/pyBMA-0.1.dev13.zip/pyBMA-0.1.dev13/pyBMA/lifelines_example.py
@@ -0,0 +1,9 @@
+from lifelines.datasets import load_rossi
+
+from pyBMA.Survival.CoxPH import CoxPH
+
+##Replication of http://lifelines.readthedocs.io/en/latest/Survival%20Regression.html
+
+bmaCox = CoxPH(load_rossi(), "week", "arrest")
+posterior = bmaCox.run()
+print(posterior[0])
