e5a8205f1ce7688c03eab1ff23305ace9b547a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:18 +0000

    Add package python-chaika/0.1.0/python-chaika-0.1.0.tar.gz

---
+++ packages/python-chaika/python-chaika-0.1.0.tar.gz/python-chaika-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='python-chaika',
+    py_modules=['chaika'],
+    version='0.1.0',
+    description='Python logging handler for chaika.',
+    long_description=open('README.MD').read(),
+    author='Thinh "Adolf Weismann" Tran <duythinht@gmail.com>',
+    author_email='duythinht@gmail.com',
+    url='https://github.com/duythinht/python-chaika',
+)
