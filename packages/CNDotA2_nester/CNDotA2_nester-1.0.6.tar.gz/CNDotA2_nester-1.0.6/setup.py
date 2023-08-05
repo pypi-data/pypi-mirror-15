ee3d1f30ccb6f7a900ca91c8d4854b35c3857e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:14 +0000

    Add package CNDotA2_nester/1.0.6/CNDotA2_nester-1.0.6.tar.gz

---
+++ packages/CNDotA2_nester/CNDotA2_nester-1.0.6.tar.gz/CNDotA2_nester-1.0.6/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'CNDotA2_nester',
+	version = '1.0.6',
+	py_modules = ['CNDotA2_nester'],
+	author = 'CNDotA2',
+	author_email = '838282876@qq.com',
+	url = 'on github, search cndota2',
+	description = 'A simple printer of nested lists, and you can add tab and its level, also, you can output it to file on disk',
+)
