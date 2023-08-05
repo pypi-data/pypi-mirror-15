abb75fef900751eaa1b42c159086146769432afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package microcosm-postgres/0.12.0/microcosm-postgres-0.12.0.tar.gz

---
+++ packages/microcosm-postgres/microcosm-postgres-0.12.0.tar.gz/microcosm-postgres-0.12.0/microcosm_postgres/types.py
@@ -0,0 +1,33 @@
+"""
+Custom types.
+
+"""
+from sqlalchemy.types import TypeDecorator, Unicode
+
+
+class EnumType(TypeDecorator):
+    """
+    SQLAlchemy enum type that persists the enum name (not value).
+
+    Note that this type is very similar to the `ChoiceType` from `sqlalchemy_utils`,
+    with the key difference being persisting by name (and not value).
+
+    """
+    impl = Unicode(255)
+
+    def __init__(self, enum_class):
+        self.enum_class = enum_class
+
+    @property
+    def python_type(self):
+        return self.impl.python_type
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return None
+        return unicode(self.enum_class(value).name)
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return None
+        return self.enum_class[value]
