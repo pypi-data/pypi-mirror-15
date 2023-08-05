1377ebf1b7b3d7c8e0cab36852510b6b7cb4d82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:12 +0000

    Add package cchen224-ec2/0.0.100/cchen224-ec2-0.0.100.tar.gz

---
+++ packages/cchen224-ec2/cchen224-ec2-0.0.100.tar.gz/cchen224-ec2-0.0.100/pyAPIs/foursquare/parser_location.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import traceback
+
+
+def parse_location(latlong, access_token):
+    url = get_url(latlong, access_token)
+
+    for _ in xrange(5):
+        try:
+            r = requests.get(url)
+            jsontext = json.loads(r.text)
+            print jsontext
+
+            if jsontext['meta'].get('code', 1) == 403:
+                continue
+            else:
+                venue = jsontext['response']['venues'][0]
+                name = venue['name'].encode('utf-8')
+                city = venue['location'].get('city', '').encode('utf-8')
+                state = venue['location'].get('state', '').encode('utf-8')
+                zip = venue['location'].get('postalCode', '')
+                country = venue['location'].get('country', '').encode('utf-8')
+                distance = venue['location'].get('distance', '')
+                category = venue['categories'][0].get('name', '').encode('utf-8')
+                checkinsCount = venue['stats'].get('checkinsCount', 0)
+                return [name, category, checkinsCount, zip, city, state, country, distance]
+        except:
+            # print traceback.format_exc()
+            return []
+
+
+def get_url(latlong, access_token):
+    if ',' in access_token:
+        token = access_token.split(',')
+        clientid = token[0]
+        clientsecret = token[1]
+        return 'https://api.foursquare.com/v2/venues/search?ll=' + str(latlong) + '&query=&limit=1&client_id=' + clientid + '&client_secret=' + clientsecret +'&v=20151216'
+    else:
+        return 'https://api.foursquare.com/v2/venues/search?ll=' + str(latlong) + '&query=&limit=1&oauth_token=' + access_token + '&v=20151216'
