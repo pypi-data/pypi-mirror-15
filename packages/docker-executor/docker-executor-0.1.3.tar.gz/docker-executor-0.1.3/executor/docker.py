4b4f3daeaae23a328ea89f5ac2a3ca5a6f5d2d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:46 +0000

    Add package docker-executor/0.1.3/docker-executor-0.1.3.tar.gz

---
+++ packages/docker-executor/docker-executor-0.1.3.tar.gz/docker-executor-0.1.3/executor/docker.py
@@ -0,0 +1,132 @@
+import os
+import subprocess
+import re
+import consul
+import registry
+
+
+def run(nodedn):
+    """Run a given container
+
+    This command gets the info needed to launch the container from the registry.
+    Information retrieved from the registry:
+
+        node.name
+        node.docker_image
+        node.docker_opts
+        node.disks
+        node.networks
+        node.tags
+    
+        bridge = network.bridge
+        device = network.device
+        address = network.address
+        netmask = network.netmask
+        gateway = network.gateway
+
+        /service REST endpoint should return:
+        service.docker_image
+        service.docker_opts
+    """
+    node = registry.Node(nodedn)
+
+    container_name = '{0}-{1}'.format(node.clustername, node.name)
+    container_image = node.docker_image
+    docker_opts = node.docker_opts
+    service = node.clustername
+    tags = node.tags
+    disks = node.disks
+    networks = node.networks
+
+    opts = generate_docker_opts(docker_opts)
+    volumes = generate_volume_opts(disks)
+
+    _cmd('docker run {opts} {volumes} -h {name} --name {name} {image}'.format(
+        name=container_name, opts=opts, volumes=volumes, image=container_image))
+    add_network_connectivity(container_name, networks)
+    register_in_consul(container_name, name=service,
+                       address=networks[0].address,
+                       tags=tags, check='SSH')
+
+
+def generate_volume_opts(disks):
+    volumes = ' '
+    for disk in disks:
+        if not os.path.exists(disk.origin):
+            os.mkdir(disk.origin)
+        volumes += '-v {}:{}:{}'.format(disk.origin, disk.destination, disk.mode)
+    return volumes
+
+
+def generate_docker_opts(extra_opts):
+    # Do not assign a local IP to the node
+    opts = '--net="none" '
+    opts += extra_opts + ' '
+
+    #opts += '--privileged '
+    #opts += '-v /sys/fs/cgroup:/sys/fs/cgroup:ro '
+    #opts += '-v /dev/log:/dev/log '
+    opts += '-v /root/.ssh/authorized_keys:/root/.ssh/authorized_keys '
+    opts += '-d '
+    opts += '-ti '
+    # DOCKER_FIX trick to avoid this issue:
+    # https://github.com/docker/docker/issues/14203
+    opts += "-e DOCKER_FIX='' "
+    return opts
+
+
+def add_network_connectivity(container_name, networks):
+    """Adds the public networks interfaces to the given container"""
+    #put('files/bin/pipework', '/tmp/pipework')
+    #run('chmod u+x /tmp/pipework')
+    for network in networks:
+        add_network_interface(container_name, network)
+
+
+def add_network_interface(container_name, network):
+    """Adds one network interface using pipework
+
+    TODO: If not address is specified obtain one from the network service
+    """
+    device = network.device
+    bridge = network.bridge
+    address = network.address
+    netmask = network.netmask
+    gateway = network.gateway
+
+    if re.search(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$', gateway):
+        _cmd(
+            'pipework {bridge} -i {device} {name} {ip}/{mask}@{gateway}'
+            .format(bridge=bridge, device=device, name=container_name,
+                    ip=address,
+                    mask=netmask,
+                    gateway=gateway))
+    else:
+        _cmd(
+            'pipework {bridge} -i {device} {name} {ip}/{mask}'
+            .format(bridge=bridge, device=device, name=container_name,
+                    ip=address,
+                    mask=netmask))
+
+    #subprocess.call('pipework virbrSTORAGE -i eth0 {name} {ip}/{mask}'
+    #    .format(name=container_name, ip=networks['storage']['address'],
+    #            mask=networks['storage']['netmask']))
+    #subprocess.call('pipework virbrPRIVATE -i eth1 {name} {ip}/{mask}@{gateway}'
+    #    .format(name=container_name, ip=networks['private']['address'],
+    #            mask=networks['private']['netmask'],
+    #            gateway=networks['gateway']))
+
+
+def register_in_consul(container_name, service_name, address, tags=None, check=None):
+    """Register the docker container in consul service discovery"""
+    sd = consul.Client()
+    if check == 'SSH':
+        check = {'id': container_name,
+                 'name': 'SSH', 'tcp': '{}:{}'.format(address, 22),
+                 'Interval': '30s', 'timeout': '4s'}
+    sd.register(container_name, service_name, address, tags=tags, check=check)
+
+
+def _cmd(cmd):
+    """Execute cmd on the shell"""
+    subprocess.call(cmd, shell=True)
