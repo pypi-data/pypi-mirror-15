e1f176f0a6c6dbda7dad901b32a4f378fa5fb4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:09 +0000

    Add package pylongecity/0.1.1/pylongecity-0.1.1.tar.gz

---
+++ packages/pylongecity/pylongecity-0.1.1.tar.gz/pylongecity-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pylongecity',
+      version='0.1.1',
+      description='A simple script to quickly search longecity threads for relevant information',
+      url='http://github.com/cyrbon/pylongecity',
+      download_url = 'https://github.com/cyrbon/pylongecity/tarball/0.1.1',
+      author='Jon Chase',
+      author_email='code.cyrbon@gmail.com',
+      license='Apache 2.0',
+      packages=['pylongecity'],
+      )
