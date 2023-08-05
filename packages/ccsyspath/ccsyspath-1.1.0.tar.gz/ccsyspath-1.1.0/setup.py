8dabe55da1e33ea1bef8841d23a2715de357b395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:59 +0000

    Add package ccsyspath/1.1.0/ccsyspath-1.1.0.tar.gz

---
+++ packages/ccsyspath/ccsyspath-1.1.0.tar.gz/ccsyspath-1.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(filename):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    contents = open(path).read()
+    return contents
+
+
+setup(
+    name         = "ccsyspath",
+    version      = "1.1.0",
+    description  = "Find the system include paths for clang and gcc C/C++ compilers",
+    long_description = read('README.rst'),
+    author       = "Andrew Walker",
+    author_email = "walker.ab@gmail.com",
+    maintainer   = "Andrew Walker",
+    maintainer_email = "walker.ab@gmail.com",
+    url          = "http://github.com/AndrewWalker/ccsyspath",
+    license      = "MIT",
+    keywords     = [ 'clang', 'gcc', 'compiler' ],
+    packages     = find_packages(), 
+    classifiers  = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: OS Independent',
+    ],
+    tests_require=['unittest2'],
+    test_suite='unittest2.collector'
+)
+
