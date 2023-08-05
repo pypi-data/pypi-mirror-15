61a92d31093824e72e9f7f7b70c2c786b719595b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:51 +0000

    Add package Django-Org-Associations/0.1.0/Django-Org-Associations-0.1.0.tar.gz

---
+++ packages/Django-Org-Associations/Django-Org-Associations-0.1.0.tar.gz/Django-Org-Associations-0.1.0/setup.py
@@ -0,0 +1,40 @@
+"""
+PyPi Setup file.
+"""
+# pylint: disable=no-name-in-module, import-error
+from distutils.core import setup
+VERSION = '0.1.0'
+BASE_URL = 'https://github.com/blancltd/django-associations'
+DOWNLOAD = 'https://github.com/blancltd/django-associations/archive/master.zip'
+
+DEPENDENCIES = [
+   'django',
+]
+
+CLASSIFIERS = [
+   'Programming Language :: Python :: 3',
+   'Environment :: Web Environment',
+   'Framework :: Django',
+   'Framework :: Django :: 1.9',
+   'License :: OSI Approved :: BSD License',
+   'Operating System :: OS Independent',
+]
+
+setup(
+   name = 'Django-Org-Associations',
+   packages = ['django_associations'],
+   version = VERSION,
+   description = 'Django Organisation/Member associations',
+   author = 'Blanc Limited',
+   author_email = 'studio@blanc.tld.uk',
+   maintainer = 'Martin P. Hellwig',
+   maintainer_email = 'martin.hellwig@gmail.com',
+   platforms=['any'],
+   url = BASE_URL,
+   download_url = DOWNLOAD,
+   keywords = ['django'],
+   license = 'BSD',
+   install_requires = DEPENDENCIES,
+   classifiers = CLASSIFIERS,
+)
+
