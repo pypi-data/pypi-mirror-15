a3a7ba0a139852b5c519be5d492121e4bfdb681f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:52 +0000

    Add package staticdict/0.0.1/staticdict-0.0.1.tar.gz

---
+++ packages/staticdict/staticdict-0.0.1.tar.gz/staticdict-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#coding:utf8
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+        name = 'staticdict',
+
+        version = '0.0.1',
+
+        author = 'ZHANG Kaixu',
+        author_email = 'zhangkaixu@hotmail.com',
+        url = 'https://github.com/zhangkaixu/staticdict',
+
+        ext_modules = cythonize("staticdict/hashdict_pyx.pyx"),
+        packages = ['staticdict'],
+        package_dir = {'staticdict':'staticdict'}
+    )
