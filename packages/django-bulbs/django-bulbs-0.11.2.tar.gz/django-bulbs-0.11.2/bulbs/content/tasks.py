2cc03d34ac1466257ea466e6157e87b3b087657e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:23 +0000

    Add package django-bulbs/0.11.2/django-bulbs-0.11.2.tar.gz

---
+++ packages/django-bulbs/django-bulbs-0.11.2.tar.gz/django-bulbs-0.11.2/bulbs/content/tasks.py
@@ -0,0 +1,28 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from celery import shared_task
+
+
+@shared_task(default_retry_delay=5)
+def index(content_type_id, pk, refresh=False):
+    from django.contrib.contenttypes.models import ContentType
+    content_type = ContentType.objects.get_for_id(content_type_id)
+    obj = content_type.model_class().objects.get(id=pk)
+    obj.index(refresh=refresh)
+
+
+@shared_task(default_retry_delay=5)
+def index_content_contributions(content_pk):
+    from bulbs.contributions.models import Contribution
+    for contribution in Contribution.objects.filter(content__pk=content_pk):
+        contribution.save()
+
+
+@shared_task(default_retry_delay=5)
+def index_content_report_content_proxy(content_pk):
+    from bulbs.contributions.models import ReportContent
+    try:
+        proxy = ReportContent.reference.get(id=content_pk)
+        proxy.index()
+    except ObjectDoesNotExist:
+        pass
