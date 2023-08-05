979b217c006f30324817f0bca87c71bdb341ab7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:08 +0000

    Add package django-softdeletev2/0.4.1/django-softdeletev2-0.4.1.tar.gz

---
+++ packages/django-softdeletev2/django-softdeletev2-0.4.1.tar.gz/django-softdeletev2-0.4.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='django-softdeletev2',
+      version='0.4.1',
+      description='Soft delete support for Django ORM, with undelete.',
+      author='Sachin Prabhu',
+      author_email='sachin.prabhu@kuliza.com',
+      maintainer='Steve Coursen',
+      maintainer_email='sachin.prabhu@kuliza.com',
+      url="https://github.com/galacticsurfer/django-softdelete",
+      packages=find_packages(),
+      install_requires=['setuptools',],
+      include_package_data=True,
+      setup_requires=['setuptools_hg',],
+      classifiers=[
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Web Environment',
+        ]
+)
