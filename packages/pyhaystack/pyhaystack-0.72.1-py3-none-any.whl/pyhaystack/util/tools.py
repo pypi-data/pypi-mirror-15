e6c33c9b59f4e4001f136ba6e2ba1878587fca30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package pyhaystack/0.72.1/pyhaystack-0.72.1-py3-none-any.whl

---
+++ packages/pyhaystack/pyhaystack-0.72.1-py3-none-any.whl/pyhaystack/util/tools.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Tools
+
+@author: CTremblay
+"""
+
+import json
+
+def isfloat(value):
+    """
+    Helper function to detect if a value is a float
+    """
+    if value != '':
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+    else:
+        return False
+
+def isBool(value):
+    """
+    Helper function to detect if a value is a float
+    """
+    if value != '':
+        if isinstance(value, bool):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def prettyprint(jsonData):
+    """
+    Pretty print json object
+    """
+    print('%s' % json.dumps(jsonData, sort_keys=True, indent=4))
