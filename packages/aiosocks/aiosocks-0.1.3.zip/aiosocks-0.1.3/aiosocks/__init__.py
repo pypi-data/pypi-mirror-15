fa08c5774b06d3deeb024e3f1a51a8096440c7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:49 +0000

    Add package aiosocks/0.1.3/aiosocks-0.1.3.zip

---
+++ packages/aiosocks/aiosocks-0.1.3.zip/aiosocks-0.1.3/aiosocks/__init__.py
@@ -0,0 +1,84 @@
+import asyncio
+from .errors import (
+    SocksError, NoAcceptableAuthMethods, LoginAuthenticationFailed,
+    SocksConnectionError, InvalidServerReply, InvalidServerVersion
+)
+from .helpers import (
+    SocksAddr, Socks4Addr, Socks5Addr, Socks4Auth, Socks5Auth
+)
+from .protocols import Socks4Protocol, Socks5Protocol
+
+__version__ = '0.1.3'
+
+__all__ = ('Socks4Protocol', 'Socks5Protocol', 'Socks4Auth',
+           'Socks5Auth', 'Socks4Addr', 'Socks5Addr', 'SocksError',
+           'NoAcceptableAuthMethods', 'LoginAuthenticationFailed',
+           'SocksConnectionError', 'InvalidServerVersion',
+           'InvalidServerReply', 'create_connection')
+
+
+@asyncio.coroutine
+def create_connection(protocol_factory, proxy, proxy_auth, dst, *,
+                      remote_resolve=True, loop=None, ssl=None, family=0,
+                      proto=0, flags=0, sock=None, local_addr=None,
+                      server_hostname=None):
+    assert isinstance(proxy, SocksAddr), (
+        'proxy must be Socks4Addr() or Socks5Addr() tuple'
+    )
+
+    assert proxy_auth is None or isinstance(proxy_auth,
+                                            (Socks4Auth, Socks5Auth)), (
+        'proxy_auth must be None or Socks4Auth() '
+        'or Socks5Auth() tuple', proxy_auth
+    )
+    assert isinstance(dst, (tuple, list)) and len(dst) == 2, (
+        'invalid dst format, tuple("dst_host", dst_port))'
+    )
+
+    if (isinstance(proxy, Socks4Addr) and not
+            (proxy_auth is None or isinstance(proxy_auth, Socks4Auth))):
+        raise ValueError(
+            "proxy is Socks4Addr but proxy_auth is not Socks4Auth"
+        )
+
+    if (isinstance(proxy, Socks5Addr) and not
+            (proxy_auth is None or isinstance(proxy_auth, Socks5Auth))):
+        raise ValueError(
+            "proxy is Socks5Addr but proxy_auth is not Socks5Auth"
+        )
+
+    loop = loop or asyncio.get_event_loop()
+
+    def socks_factory():
+        if isinstance(proxy, Socks4Addr):
+            socks_proto = Socks4Protocol
+        else:
+            socks_proto = Socks5Protocol
+
+        return socks_proto(
+            proxy=proxy, proxy_auth=proxy_auth, dst=dst,
+            remote_resolve=remote_resolve, loop=loop)
+
+    try:
+        transport, protocol = yield from loop.create_connection(
+            socks_factory, proxy.host, proxy.port, ssl=ssl, family=family,
+            proto=proto, flags=flags, sock=sock, local_addr=local_addr,
+            server_hostname=server_hostname)
+    except OSError as exc:
+        raise SocksConnectionError(
+            '[Errno %s] Can not connect to proxy %s:%d [%s]' %
+            (exc.errno, proxy.host, proxy.port, exc.strerror)) from exc
+
+    # Wait until communication with proxy server is finished
+    try:
+        yield from protocol.negotiate_done()
+    except SocksError as exc:
+        raise SocksError('Can not connect to %s:%s [%s]' %
+                         (dst[0], dst[1], exc))
+
+    if protocol_factory:
+        protocol = protocol_factory()
+        protocol.connection_made(transport)
+        transport._protocol = protocol
+
+    return transport, protocol
---
+++ packages/aiosocks/aiosocks-0.1.3.zip/aiosocks-0.1.3/aiosocks/connector.py
@@ -0,0 +1,82 @@
+import socket
+import asyncio
+import aiohttp
+import ipaddress
+from aiohttp.errors import ProxyConnectionError
+from .errors import SocksError, SocksConnectionError
+from . import create_connection
+
+__all__ = ('SocksConnector',)
+
+
+class SocksConnector(aiohttp.TCPConnector):
+    def __init__(self, proxy, proxy_auth=None, *, remote_resolve=True, **kwgs):
+        super().__init__(**kwgs)
+
+        self._proxy = proxy
+        self._proxy_auth = proxy_auth
+        self._remote_resolve = remote_resolve
+
+    @property
+    def proxy(self):
+        """Proxy info.
+        Should be Socks4Server/Socks5Server instance.
+        """
+        return self._proxy
+
+    @property
+    def proxy_auth(self):
+        """Proxy auth info.
+        Should be Socks4Auth/Socks5Auth instance.
+        """
+        return self._proxy_auth
+
+    @asyncio.coroutine
+    def _create_connection(self, req):
+        if not self._remote_resolve:
+            dst_hosts = yield from self._resolve_host(req.host, req.port)
+            dst = dst_hosts[0]['host'], dst_hosts[0]['port']
+        else:
+            dst = req.host, req.port
+        exc = None
+
+        # if self._resolver is AsyncResolver and self._proxy.host
+        # is ip address, then aiodns raise DNSError.
+        # It's aiohttp bug? Hot fix:
+        try:
+            ipaddress.ip_address(self._proxy.host)
+            proxy_hosts = yield from self._loop.getaddrinfo(self._proxy.host,
+                                                            self._proxy.port)
+            family, _, proto, _, address = proxy_hosts[0]
+
+            proxy_hosts = ({'hostname': self._proxy.host,
+                            'host': address[0], 'port': address[1],
+                            'family': family, 'proto': proto,
+                            'flags': socket.AI_NUMERICHOST},)
+        except ValueError:
+            proxy_hosts = yield from self._resolve_host(self._proxy.host,
+                                                        self._proxy.port)
+
+        for hinfo in proxy_hosts:
+            try:
+                proxy = self._proxy.__class__(host=hinfo['host'],
+                                              port=hinfo['port'])
+
+                transp, proto = yield from create_connection(
+                    self._factory, proxy, self._proxy_auth, dst,
+                    loop=self._loop, remote_resolve=self._remote_resolve,
+                    ssl=None, family=hinfo['family'], proto=hinfo['proto'],
+                    flags=hinfo['flags'], local_addr=self._local_addr)
+
+                return transp, proto
+            except (OSError, SocksError, SocksConnectionError) as e:
+                exc = e
+        else:
+            if isinstance(exc, SocksConnectionError):
+                raise ProxyConnectionError(*exc.args)
+            if isinstance(exc, SocksError):
+                raise exc
+            else:
+                raise aiohttp.ClientOSError(
+                    exc.errno, 'Can not connect to %s:%s [%s]' %
+                               (req.host, req.port, exc.strerror)) from exc
