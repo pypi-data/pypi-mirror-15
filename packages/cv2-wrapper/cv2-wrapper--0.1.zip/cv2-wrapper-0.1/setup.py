3155434c1845652a27d5451ca3ee0ce923976582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:38 +0000

    Add package cv2-wrapper/0.1/cv2-wrapper--0.1.zip

---
+++ packages/cv2-wrapper/cv2-wrapper--0.1.zip/cv2-wrapper-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cv2-wrapper',
+  packages = ['cv2-wrapper'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Wrapper for python opencv 2.4.12 32bit',
+  author = 'Lucas V. Oliveira',
+  author_email = 'lucas.o@live.com',
+  url = 'https://github.com/lucasolivier/cv2-wrapper', # use the URL to the github repo
+  download_url = 'https://github.com/lucasolivier/cv2-wrapper/tarball/0.1', # I'll explain this in a second
+  keywords = ['opencv', 'cv2', 'python'], # arbitrary keywords
+  classifiers = []
+)
