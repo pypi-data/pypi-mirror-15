969f1061fb120f9a8c8b4eea5792a6f68e7318f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package figure-sdk/0.1.0/figure-sdk-0.1.0.tar.gz

---
+++ packages/figure-sdk/figure-sdk-0.1.0.tar.gz/figure-sdk-0.1.0/figure/__init__.py
@@ -0,0 +1,39 @@
+
+
+__version__ = "0.1.0"
+
+token = None
+api_base = 'https://api.figuredevices.com'
+
+# Resources
+from figure.resource import (
+    Photobooth,
+    Place,
+    Event,
+    TicketTemplate,
+    Text,
+    TextVariable,
+    Image,
+    ImageVariable,
+    Portrait,
+    PosterOrder,
+    WifiNetwork,
+    CodeList,
+    User,
+    Auth
+)
+
+from figure.error import (
+    APIConnectionError,
+    BadRequestError,
+    AuthenticationError,
+    AuthorizationError,
+    NotFoundError,
+    RateLimitError,
+    InternalServerError,
+    NotAvailableYetError
+)
+
+
+
+
