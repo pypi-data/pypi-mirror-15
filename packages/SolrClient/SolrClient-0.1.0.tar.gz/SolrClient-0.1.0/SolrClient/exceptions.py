829b0c1cdb4098b22e776d56a75f83698c5e0563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:33 +0000

    Add package SolrClient/0.1.0/SolrClient-0.1.0.tar.gz

---
+++ packages/SolrClient/SolrClient-0.1.0.tar.gz/SolrClient-0.1.0/SolrClient/exceptions.py
@@ -0,0 +1,20 @@
+
+class SolrError(Exception):
+    """
+    Class to handle any issues that Solr Reports
+    """
+    
+class SolrResponseError(Exception):
+    '''
+    Errors relatd to parsing Solr Response
+    '''
+    
+class ConnectionError(Exception):
+    '''
+    Errors connecting to Solr
+    '''
+    
+class ZookeeperError(Exception):
+    '''
+    Errors connecting to Zookeeper
+    '''
