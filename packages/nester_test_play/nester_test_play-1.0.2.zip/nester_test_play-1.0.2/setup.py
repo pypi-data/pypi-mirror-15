349fbc433b490d605b6b1635048ad970d1197e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:06 +0000

    Add package nester_test_play/1.0.2/nester_test_play-1.0.2.zip

---
+++ packages/nester_test_play/nester_test_play-1.0.2.zip/nester_test_play-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_test_play',
+    version = '1.0.2',
+    py_modules = ['nester_test_play'],
+    author = 'LJD',
+    description = 'test module'
+    )
