ea70650287787b6c5c2c14b3d492f4270cea8834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:58 +0000

    Add package dumdba/0.0.1/dumdba-0.0.1.tar.gz

---
+++ packages/dumdba/dumdba-0.0.1.tar.gz/dumdba-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 			= 'dumdba',
+	version 		= '0.0.1',
+	py_modules  	= ['dumdba'],
+	author 			= 'Juciano Cardoso',
+	author_email 	= 'cjuciano@gmail.com',
+	url				= 'https://github.com/JucianoC',
+	description		= 'A simple dbadaptor do MySQL using python 2.7.6'
+	)
