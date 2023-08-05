065bbb979c61a5fe1628a38ea0ca5d7e0901d8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:59 +0000

    Add package zess_chapter6/1.1.0/zess_chapter6-1.1.0.zip

---
+++ packages/zess_chapter6/zess_chapter6-1.1.0.zip/zess_chapter6-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'zess_chapter6',
+    version = '1.1.0',
+    py_modules = ['zess_chapter6'],
+    author = 'zess',
+    author_email = 'zess1982@126.com',
+    url = 'http://satellite.sinaapp.com/',
+    description = 'Class, return a class object in get_coach_data function.',
+    )
