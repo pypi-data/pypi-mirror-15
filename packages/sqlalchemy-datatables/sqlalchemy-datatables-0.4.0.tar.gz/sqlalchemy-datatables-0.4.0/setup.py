f75406eeac6f427bf3d98c10aff9c2aaffe3185b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:15 +0000

    Add package sqlalchemy-datatables/0.4.0/sqlalchemy-datatables-0.4.0.tar.gz

---
+++ packages/sqlalchemy-datatables/sqlalchemy-datatables-0.4.0.tar.gz/sqlalchemy-datatables-0.4.0/setup.py
@@ -0,0 +1,44 @@
+"""App configuration."""
+from setuptools import setup
+
+
+__VERSION__ = '0.4.0'
+
+
+setup(
+    name='sqlalchemy-datatables',
+    version=__VERSION__,
+    description='SQLAlchemy integration of jQuery DataTables',
+    long_description=open('README.rst').read(),
+    license='MIT',
+    keywords='sqlalchemy datatables jquery pyramid flask dt1.9 dt1.10',
+    author='Michel Nemnom',
+    author_email='michel.nemnom+pypi@gmail.com',
+    url='https://github.com/pegase745/sqlalchemy-datatables',
+    packages=['datatables'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['sqlalchemy'],
+    py_modules=['datatables'],
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
