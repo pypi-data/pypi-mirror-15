4a8b17a548d790a62f284c6ac768b26a13f717e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:35 +0000

    Add package nester_lym/1.3.0/nester_lym-1.3.0.tar.gz

---
+++ packages/nester_lym/nester_lym-1.3.0.tar.gz/nester_lym-1.3.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='nester_lym',
+
+    version='1.3.0',
+
+    py_modules=['nester_lym'],
+
+    author='Tony',
+
+    author_email='lyming9x@gmail.com',
+
+    description='A simple printer of nested lists'
+
+)
