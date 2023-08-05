d0611b2a3d96a28ce4ffd6ac6bd69daf3907bbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:24 +0000

    Add package pur/2.0.2/pur-2.0.2.tar.gz

---
+++ packages/pur/pur-2.0.2.tar.gz/pur-2.0.2/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup
+
+from pur import (
+    __author__,
+    __author_email__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
+
+
+packages = [
+    __title__,
+]
+
+install_requires = [x.strip() for x in open('requirements.txt').readlines()]
+
+setup(
+    name=__title__,
+    version=__version__,
+    license=__license__,
+    description=__description__,
+    long_description=open('README.rst').read(),
+    author=__author__,
+    author_email=__author_email__,
+    url=__url__,
+    packages=packages,
+    package_dir={__title__: __title__},
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': ['pur = pur.__init__:pur'],
+    },
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ),
+)
