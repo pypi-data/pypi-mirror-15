9ae83e8e2088ccf0d523ea7617f22a8f4e420328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:15 +0000

    Add package python-qosf/1.1.0/python_qosf-1.1.0-py2.py3-none-any.whl

---
+++ packages/python-qosf/python_qosf-1.1.0-py2.py3-none-any.whl/QOpenScienceFramework/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "1.1.0"
+__author__ = "Daniel Schreij"
+
+import os
+import sys
+from QOpenScienceFramework.compat import *
+dirname = safe_decode(os.path.dirname(__file__),
+	enc=sys.getfilesystemencoding())
+
+import QOpenScienceFramework.connection
+import QOpenScienceFramework.manager
+import QOpenScienceFramework.widgets
