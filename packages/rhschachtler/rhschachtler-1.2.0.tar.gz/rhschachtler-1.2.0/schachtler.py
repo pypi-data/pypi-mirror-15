bf2d86556e8e23f402ad54ca0385be2c23b9dffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package rhschachtler/1.2.0/rhschachtler-1.2.0.tar.gz

---
+++ packages/rhschachtler/rhschachtler-1.2.0.tar.gz/rhschachtler-1.2.0/schachtler.py
@@ -0,0 +1,20 @@
+"""Dies ist das Modul "schachtler.py". Es stellt eine Funktion namens print_lvl()
+bereit, die eine Liste mit beliebig viel eingebetten Listen ausgibt."""
+
+def print_lvl(liste,ebene=0):
+    """Die Funktion erwartet ein positionelles Argument namens "liste" das eine
+    beliegige Python-Liste (mit eventuell eingebetten Listen) ist. Jedes Element
+    der Liste wird (rekursiv) auf dem Bildschirm jeweils in einer eigenen Zeile
+    ausgegeben.
+    Mit dem zweiten Argument "ebene" können bei eingebettene Listen Tabulatoren
+    eingesetzt werden.
+    """
+    
+    for element in liste:
+        if (isinstance(element,list)):
+            print_lvl(element,ebene+1)
+        else:
+            for einrücken in range(ebene):
+                print("\t",end="")
+            print(element)
+            
