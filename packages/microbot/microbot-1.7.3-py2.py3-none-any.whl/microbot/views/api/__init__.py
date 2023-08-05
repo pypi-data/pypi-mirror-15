cb1a968ba269c61c60b17802279acdb1602c9efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package microbot/1.7.3/microbot-1.7.3-py2.py3-none-any.whl

---
+++ packages/microbot/microbot-1.7.3-py2.py3-none-any.whl/microbot/views/api/__init__.py
@@ -0,0 +1,6 @@
+from microbot.views.api.bot import BotList, BotDetail, TelegramBotList, TelegramBotDetail, KikBotList, KikBotDetail, MessengerBotList, MessengerBotDetail  # NOQA
+from microbot.views.api.environment_vars import EnvironmentVarList, EnvironmentVarDetail  # NOQA
+from microbot.views.api.handler import (HandlerList, HandlerDetail, HeaderParameterDetail, HeaderParameterList,  # NOQA
+                                        UrlParameterList, UrlParameterDetail, SourceStateList, SourceStateDetail)  # NOQA
+from microbot.views.api.hook import HookList, HookDetail, TelegramRecipientList, TelegramRecipientDetail, KikRecipientList, KikRecipientDetail, MessengerRecipientList, MessengerRecipientDetail  # NOQA
+from microbot.views.api.state import StateList, StateDetail, TelegramChatStateList, TelegramChatStateDetail, KikChatStateList, KikChatStateDetail, MessengerChatStateList, MessengerChatStateDetail  # NOQA
