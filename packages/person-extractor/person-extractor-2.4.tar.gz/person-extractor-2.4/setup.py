e0b29d74320b5fcad36f14b78baefc1242db97b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package person-extractor/2.4/person-extractor-2.4.tar.gz

---
+++ packages/person-extractor/person-extractor-2.4.tar.gz/person-extractor-2.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'person-extractor',
+  packages = ['person_extractor'],
+  version = '2.4',
+  description = 'Extracts people from text',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/person-extractor',
+  download_url = 'https://github.com/DanielJDufour/person-extractor/tarball/download',
+  keywords = ['language','persons','people','parsing','python'],
+  classifiers = [],
+)
