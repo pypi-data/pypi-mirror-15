704f37b96c2b92639c6681c6b4d2cd28d6812b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package git-multimail/1.3.0/git-multimail-1.3.0.tar.gz

---
+++ packages/git-multimail/git-multimail-1.3.0.tar.gz/git-multimail-1.3.0/setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+import sys
+import os
+from setuptools import setup
+
+assert 0x02040000 <= sys.hexversion, \
+    "Install Python, version 2.4 or greater"
+
+def read_version():
+    sys.path.insert(0, os.path.join('git-multimail'))
+    import git_multimail
+    return git_multimail.__version__
+
+def read_readme():
+    return open(os.path.join('git-multimail', 'README')).read()
+
+setup(
+    name='git-multimail',
+    version=read_version(),
+    description='Send notification emails for Git pushes',
+    long_description=read_readme(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications :: Email',
+        'Topic :: Software Development :: Version Control',
+        ],
+    keywords='git hook email',
+    url='https://github.com/git-multimail/git-multimail',
+    author='Michael Haggerty',
+    author_email='mhagger@alum.mit.edu',
+    maintainer='Matthieu Moy',
+    maintainer_email='Matthieu.Moy@imag.fr',
+    license='GPLv2',
+    package_dir={'': 'git-multimail'},
+    py_modules=['git_multimail'],
+    )
