1e79409810a1759f66dd499ab0404f72ff0220ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package pytsite/0.67.1/PytSite-0.67.1.tar.gz

---
+++ packages/pytsite/PytSite-0.67.1.tar.gz/PytSite-0.67.1/pytsite/auth_ui/_widget.py
@@ -0,0 +1,127 @@
+"""Auth UI Widgets.
+"""
+from pytsite import auth as _auth, widget as _widget, html as _html, tpl as _tpl, odm as _odm, assetman as _assetman
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class Profile(_widget.Base):
+    """User Profile Widget.
+    """
+    def __init__(self, uid: str, **kwargs):
+        """Init.
+
+        :type user: pytsite.auth._model.User | pytsite.auth_ui._model.UserUI
+        """
+        super().__init__(uid, **kwargs)
+
+        self._user = kwargs.get('user')
+        if not self._user:
+            raise ValueError('User is not specified.')
+
+        self._tpl = kwargs.get('tpl', 'pytsite.auth_ui@widget/profile')
+        self._css += ' widget-auth-ui-profile'
+        self._col_image_css = kwargs.get('col_image_css', 'col-xs-B-12 col-xs-4 col-sm-3 col-lg-2 text-center')
+        self._col_content_css = kwargs.get('col_content_css', 'col-xs-B-12 col-xs-8 col-sm-9 col-lg-10')
+
+        _assetman.add('pytsite.auth_ui@css/widget/profile.css')
+
+    def get_html_em(self, **kwargs) -> _html.Element:
+        """Render the widget.
+        :param **kwargs:
+        """
+        current_user = _auth.get_current_user()
+
+        # Hidden profiles are visible only for owners and administrators
+        if not self._user.profile_is_public and current_user.id != self._user.id and not current_user.is_admin:
+            return _html.TagLessElement()
+
+        # Check whether to show 'Edit' button
+        profile_is_editable = False
+        if not current_user.is_anonymous:
+            if current_user.id == self._user.id or current_user.has_permission('pytsite.odm_ui.modify.user'):
+                profile_is_editable = True
+
+        # Rendering widget's template
+        content = _html.TagLessElement(_tpl.render(self._tpl, {
+            'user': self._user,
+            'profile_is_editable': profile_is_editable,
+            'col_image_css': self._col_image_css,
+            'col_content_css': self._col_content_css,
+        }))
+
+        return self._group_wrap(content)
+
+
+class Follow(_widget.Base):
+    """Follow Widget.
+    """
+    def __init__(self, uid: str, **kwargs):
+        """Init.
+
+        :type user: pytsite.auth._model.User|pytsite.auth_ui._model.UserUI
+        """
+        super().__init__(uid, **kwargs)
+
+        self._user = kwargs.get('user')
+        if not self._user:
+            raise ValueError('User is not specified.')
+
+        self._current_user = _auth.get_current_user()
+        self._tpl = kwargs.get('tpl', 'pytsite.auth_ui@widget/follow')
+        self._follow_msg_id = kwargs.get('follow_msg_id', 'pytsite.auth_ui@follow')
+        self._unfollow_msg_id = kwargs.get('unfollow_msg_id', 'pytsite.auth_ui@unfollow')
+        self._following_msg_id = 'pytsite.auth_ui@following'
+
+        _assetman.add('pytsite.auth_ui@css/widget/follow.css')
+        _assetman.add('pytsite.auth_ui@js/widget/follow.js')
+
+    def get_html_em(self, **kwargs) -> _html.Element:
+        """Render the widget.
+        :param **kwargs:
+        """
+        # Don't show the widget to unauthorized and profile owners
+        if self._current_user.is_anonymous or self._current_user == self._user:
+            return _html.TagLessElement()
+
+        content = _tpl.render(self._tpl, {
+            'current_user': _auth.get_current_user(),
+            'user': self._user,
+            'follow_msg_id': self._follow_msg_id,
+            'unfollow_msg_id': self._unfollow_msg_id,
+            'following_msg_id': self._following_msg_id,
+        })
+
+        return _html.TagLessElement(content)
+
+
+class UserSelect(_widget.select.Select):
+    """User Select Widget.
+    """
+    def __init__(self, uid: str, **kwargs):
+        super().__init__(uid, **kwargs)
+
+        f = _auth.find_users().sort([('first_name', _odm.I_ASC)])
+
+        current_user = _auth.get_current_user()
+        if not current_user.has_permission('pytsite.odm_ui.browse.user'):
+            f.where('login', '=', current_user.login)
+
+        for user in f.get():
+            self._items.append(('user:' + str(user.id), '{} ({})'.format(user.full_name, user.login)))
+
+    def set_val(self, value, **kwargs):
+        if isinstance(value, _auth.model.User):
+            value = 'user:' + str(value.id)
+
+        return super().set_val(value, **kwargs)
+
+    def get_val(self, **kwargs) -> _auth.model.User:
+        value = super().get_val(**kwargs)
+        if value:
+            value = _odm.get_by_ref(value)
+            if isinstance(value, _auth.model.User):
+                return value
+        else:
+            return None
---
+++ packages/pytsite/PytSite-0.67.1.tar.gz/PytSite-0.67.1/pytsite/file/_api.py
@@ -0,0 +1,147 @@
+"""File manager.
+"""
+import re as _re
+import os as _os
+import shutil as _shutil
+import magic as _magic
+from mimetypes import guess_extension as _guess_extension
+from urllib.request import urlopen as _urlopen
+from urllib.parse import urlparse as _urlparse
+from bson.dbref import DBRef as _DBRef
+from pytsite import reg as _reg, util as _util, odm as _odm, validation as _validation
+from . import _model
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def _build_store_path(mime: str, model: str='file', propose: str=None) -> str:
+    """Build unique path to store file on the filesystem.
+    """
+    storage_dir = _os.path.join(_reg.get('paths.storage'), model)
+    store_path = ''
+    rnd_str = _util.random_str
+
+    # Determine extension for the file in the storage
+    extension = _guess_extension(mime)
+    if extension == '.jpe':
+        extension = '.jpg'
+
+    # Possible (but not final) path
+    possible_target_path = _os.path.join(storage_dir, rnd_str(2), rnd_str(2), rnd_str(16)) + extension
+
+    # Check if the proposed path suits the requirements
+    if propose:
+        m = _re.match('(\w{2})/(\w{2})/(\w{16})(\.\w+)$', propose)
+        if m:
+            extension = m.group(4)
+            possible_target_path = _os.path.join(storage_dir, m.group(1), m.group(2), m.group(3)) + extension
+
+    # Finding path which doesn't exist on the filesystem
+    while True:
+        if not _os.path.exists(possible_target_path):
+            store_path = possible_target_path
+            break
+        else:
+            possible_target_path = _os.path.join(storage_dir, rnd_str(2), rnd_str(2), rnd_str()) + extension
+
+    return store_path
+
+
+def create(source_path: str, name: str=None, description: str=None, model='file', remove_source=False,
+           propose_store_path: str=None) -> _model.File:
+    """Create a file from path or URL.
+    """
+
+    # Store remote file to the local if URL was specified
+    try:
+        _validation.rule.Url(source_path).validate()
+
+        # Copying remote file to the temporary local file
+        with _urlopen(source_path) as src:
+            data = src.read()
+
+        tmp_file_fd, tmp_file_path = _util.mk_tmp_file()
+        _os.write(tmp_file_fd, data)
+        _os.close(tmp_file_fd)
+
+        if not name:
+            name = _urlparse(source_path).path.split('/')[-1]
+        if not description:
+            description = 'Downloaded from ' + source_path
+
+        remove_source = True
+        source_path = tmp_file_path
+    except _validation.error.RuleError:
+        pass
+
+    # Determining file's MIME type
+    mime = _magic.from_file(source_path, True).decode()
+
+    # Generating unique file path in storage
+    abs_target_path = _build_store_path(mime, model, propose_store_path)
+
+    # Make sure that directory in storage exists
+    target_dir = _os.path.dirname(abs_target_path)
+    if not _os.path.exists(target_dir):
+        _os.makedirs(target_dir, 0o755, True)
+
+    # Copy file to the storage
+    _shutil.copy(source_path, abs_target_path)
+
+    # Setting file's ODM entity name
+    if not name:
+        name = _os.path.basename(source_path)
+
+    # Setting file's ODM entity description
+    if not description:
+        description = 'Created from local file ' + source_path
+
+    # Remove source file
+    if remove_source:
+        _os.unlink(source_path)
+
+    # Create File ODM entity
+    storage_dir = _reg.get('paths.storage')
+    file_entity = _odm.dispense(model)
+    if not isinstance(file_entity, _model.File):
+        raise Exception('File entity expected.')
+    file_entity.f_set('path', abs_target_path.replace(storage_dir + '/', ''))
+    file_entity.f_set('name', name)
+    file_entity.f_set('description', description)
+    file_entity.f_set('mime', mime)
+    file_entity.f_set('length', _os.stat(abs_target_path).st_size)
+
+    # Setting entity's owner
+    from pytsite import auth
+    user = auth.get_current_user()
+    if user and not user.is_anonymous:
+        file_entity.f_set('owner', user)
+
+    return file_entity.save()
+
+
+def get(uid: str=None, rel_path: str=None, model: str='file') -> _model.File:
+    """Get file.
+    """
+    if not uid and not rel_path:
+        raise Exception("Not enough arguments.")
+
+    if uid:
+        return _odm.find(model).where('_id', '=', uid).first()
+    elif rel_path:
+        return _odm.find(model).where('path', '=', rel_path).first()
+
+
+def get_by_ref(ref: _DBRef) -> _model.File:
+    """Get file by ref.
+    """
+    entity = _odm.get_by_ref(ref)
+    if not entity:
+        return
+
+    if not isinstance(entity, _model.File):
+        raise Exception('Entity is not File.')
+
+    return entity
