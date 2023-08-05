90b7c35d6dd67df8baf604e5af731db084d9d084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:53 +0000

    Add package abilian-core/0.6.4/abilian-core-0.6.4.tar.gz

---
+++ packages/abilian-core/abilian-core-0.6.4.tar.gz/abilian-core-0.6.4/abilian/web/forms/filters.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+"""
+Field filters for WTForm.
+"""
+from __future__ import absolute_import, division, print_function
+
+from future.utils import string_types
+
+
+def strip(data):
+    """
+    Strip data if data is a string
+    """
+    if data is None:
+        return ""
+    if not isinstance(data, string_types):
+        return data
+    return data.strip()
+
+
+def uppercase(data):
+    if not isinstance(data, string_types):
+        return data
+    return data.upper()
+
+
+def lowercase(data):
+    if not isinstance(data, string_types):
+        return data
+    return data.lower()
