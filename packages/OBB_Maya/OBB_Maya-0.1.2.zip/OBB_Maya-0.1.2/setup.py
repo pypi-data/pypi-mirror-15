c8a95c1d4156a6c20583e184b42d897916e182f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:45 +0000

    Add package OBB_Maya/0.1.2/OBB_Maya-0.1.2.zip

---
+++ packages/OBB_Maya/OBB_Maya-0.1.2.zip/OBB_Maya-0.1.2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'pypi':
+    os.system('python setup.py sdist bdist_wheel upload')
+    sys.exit()
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+setup(
+    name='OBB_Maya',
+    version='0.1.2',
+    description="Oriented Bounding Boxes in Maya.",
+    long_description=readme,
+    author="Christopher DeVito",
+    author_email='chrisdevito@chribis.com',
+    url='https://github.com/chrisdevito/OBB',
+    packages=[
+        'OBB',
+    ],
+    package_dir={'OBB':
+                 'OBB'},
+    include_package_data=True,
+    license="MIT",
+    zip_safe=False,
+    keywords='OBB',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
