4c22e11adb2b597b8db7a65ba75a56272500cc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package microbot/1.7.1/microbot-1.7.1-py2.py3-none-any.whl

---
+++ packages/microbot/microbot-1.7.1-py2.py3-none-any.whl/microbot/views/hooks/__init__.py
@@ -0,0 +1,4 @@
+from microbot.views.hooks.microbot_hook import MicrobotHookView  # NOQA
+from microbot.views.hooks.telegram_hook import TelegramHookView  # NOQA
+from microbot.views.hooks.kik_hook import KikHookView  # NOQA
+from microbot.views.hooks.messenger_hook import MessengerHookView  # NOQA
