c3339160e929406b144b3c0903265b67e2a77c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:11 +0000

    Add package epsg-ident/0.1.0/epsg_ident-0.1.0.tar.gz

---
+++ packages/epsg-ident/epsg_ident-0.1.0.tar.gz/epsg_ident-0.1.0/tests/test_epsg-ident.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_epsg_ident
+----------------------------------
+
+Tests for `epsg_ident` module.
+"""
+
+import unittest
+
+from epsg_ident import EpsgIdent
+
+
+class TestEpsgIdent(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
