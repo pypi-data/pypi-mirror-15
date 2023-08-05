6e3d72b175116876671bdd835bebd19324102539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:54 +0000

    Add package glb-slave/0.0.33/glb-slave-0.0.33.tar.gz

---
+++ packages/glb-slave/glb-slave-0.0.33.tar.gz/glb-slave-0.0.33/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='glb-slave',
+    description='GLB slave client.',
+    version='0.0.33',
+    author='Lain',
+    author_email='softliunaisen@gmail.com',
+    url='https://pypi.python.org/pypi/glb-slave/',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'glb_slave=glb_slave:process'
+        ]
+    },
+    install_requires=['click', 'websocket-client', 'Jinja2'],
+    tests_require=['pytest'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
