6ac372a5b656d1bf0f4aecb822fa23d89dd32b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:48 +0000

    Add package ftw.profilehook/1.2.0/ftw.profilehook-1.2.0.tar.gz

---
+++ packages/ftw.profilehook/ftw.profilehook-1.2.0.tar.gz/ftw.profilehook-1.2.0/ftw/profilehook/subscribers.py
@@ -0,0 +1,28 @@
+from ftw.profilehook.interfaces import IBeforeImportHook
+from ftw.profilehook.interfaces import IProfileHook
+from Products.CMFCore.utils import getToolByName
+from zope.component import queryAdapter
+from zope.component.hooks import getSite
+import re
+
+
+def profile_imported(event):
+    if not event.full_import:
+        return
+
+    trigger_hook_for(event.profile_id, IProfileHook)
+
+
+def before_profile_import(event):
+    if not event.full_import:
+        return
+
+    trigger_hook_for(event.profile_id, IBeforeImportHook)
+
+
+def trigger_hook_for(profile, providing):
+    profile = re.sub('^profile-', '', profile)
+    site = getToolByName(getSite(), 'portal_url').getPortalObject()
+    hook = queryAdapter(site, providing, name=profile)
+    if hook:
+        hook(site)
