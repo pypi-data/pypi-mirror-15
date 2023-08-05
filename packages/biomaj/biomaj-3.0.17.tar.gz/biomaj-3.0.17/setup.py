14175c3ba803de796785ae55c81f0a9543192d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package biomaj/3.0.17/biomaj-3.0.17.tar.gz

---
+++ packages/biomaj/biomaj-3.0.17.tar.gz/biomaj-3.0.17/setup.py
@@ -0,0 +1,51 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'BioMAJ',
+    'author': 'Olivier Sallou',
+    'url': 'http://biomaj.genouest.org',
+    'download_url': 'http://biomaj.genouest.org',
+    'author_email': 'olivier.sallou@irisa.fr',
+    'version': '3.0.17',
+     'classifiers': [
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4'
+    ],
+    'install_requires': ['nose',
+                            'pymongo==3.2',
+                            'pycurl',
+                            'ldap3',
+                            'mock',
+                            'py-bcrypt',
+                            'mock',
+                            'drmaa',
+                            'future',
+                            'tabulate',
+                            'elasticsearch'],
+    'packages': find_packages(),
+    'include_package_data': True,
+    'scripts': ['bin/biomaj-cli.py'],
+    'name': 'biomaj'
+}
+
+setup(**config)
