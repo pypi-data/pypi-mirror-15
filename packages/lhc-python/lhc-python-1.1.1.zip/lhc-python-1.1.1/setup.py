36fc36499dc18f67388939e16e7502e45778cb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package lhc-python/1.1.1/lhc-python-1.1.1.zip

---
+++ packages/lhc-python/lhc-python-1.1.1.zip/lhc-python-1.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='lhc-python',
+    version='1.1.1',
+    author='Liam H. Childs',
+    author_email='liam.h.childs@gmail.com',
+    packages=['lhc'],
+    scripts=[],
+    url='https://github.com/childsish/lhc-python',
+    license='LICENSE.txt',
+    description='My python library of classes and functions that help me work',
+    long_description=open('README.md').read()
+)
