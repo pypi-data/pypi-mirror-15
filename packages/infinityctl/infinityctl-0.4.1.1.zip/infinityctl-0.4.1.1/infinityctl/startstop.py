1342517d0f769acc127cddb28476652e4c469dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package infinityctl/0.4.1.1/infinityctl-0.4.1.1.zip

---
+++ packages/infinityctl/infinityctl-0.4.1.1.zip/infinityctl-0.4.1.1/infinityctl/startstop.py
@@ -0,0 +1,40 @@
+import glob
+import os
+import signal
+import subprocess
+import sys
+
+from .util import get_and_check_config
+
+
+# получить PID процесса. только Unix-системы
+def get_pid(name):
+    return subprocess.check_output(["pidof",name])
+
+
+def start(args, **kwargs):
+    config = get_and_check_config(args)
+    build = config.get_build(args.build)
+    folder = build.get('folder')
+
+    dmb = glob.glob(folder + "/*.dmb")
+    if not dmb:
+        print("Билд не скомпилирован")
+        sys.exit(1)
+    try:
+        cpid = os.fork()
+    except OSError:
+        print("Не могу форкнуться :(")
+    if cpid == 0:
+        try:
+            pid = subprocess.call(['DreamDaemon', dmb[0], config.port, '-trusted', '-logself'])
+        except OSError:
+            print('Не удалось запустить сервер')
+        print('Сервер запущен, PID: {0}'.format(pid))
+    sys.exit(0)
+
+
+def stop(args):
+    pid = int(get_pid('DreamDaemon'))
+    os.kill(pid, signal.SIGTERM)
+
