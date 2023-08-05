8ff3881b14563384f9be53c31f71acb748f63131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:06 +0000

    Add package pybythec/0.2.15/pybythec-0.2.15.tar.gz

---
+++ packages/pybythec/pybythec-0.2.15.tar.gz/pybythec-0.2.15/pybythec/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from pybythec import main
+import logging
+
+# DEPLOY
+# TODO: verbose should be set in the normal configuration
+logging.basicConfig(level = logging.INFO, format = '%(message)s') # DEBUG INFO
+
+# wrapper functions to be used by the outside world
+def build(argv):
+  main.build(argv)
+
+def clean(argv):
+  main.clean(argv)
+  
+def cleanall(argv):
+  main.cleanall(argv)
+
