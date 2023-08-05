00e4fca3b9e2444725e2433e832714113482af8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:17 +0000

    Add package infinityctl/0.3.6.4/infinityctl-0.3.6.4.zip

---
+++ packages/infinityctl/infinityctl-0.3.6.4.zip/infinityctl-0.3.6.4/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='infinityctl',
+    version='0.3.6.4',
+    packages=['infinityctl'],
+    url='https://bitbucket.org/Unicode4all/infinityctl',
+    license='BSD-3-Clause',
+    author='Unicode4all Foundation',
+    author_email='anthon@unicode4all.org',
+    description='Infinity Station 13 server management tool',
+    platforms='linux',
+    install_requires=[
+        'pyyaml',
+    ],
+    entry_points={
+        'console_scripts': [
+            'infinityctl = infinityctl.__main__:main'
+        ]
+    },
+)
