7c0f8face609f29566715c5858502f77ef9f8f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package makerestapiclient/1.2.0/makerestapiclient-1.2.0.tar.gz

---
+++ packages/makerestapiclient/makerestapiclient-1.2.0.tar.gz/makerestapiclient-1.2.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+version = '1.2.0'
+
+setup(name='makerestapiclient',
+	version=version,
+	description="Simple python utility to build a python client class for interfacing with a REST API",
+	classifiers=[
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Natural Language :: English",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.5",
+		"Topic :: Internet :: WWW/HTTP",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		],
+	entry_points={
+		'console_scripts': [
+			'makerestapiclient = makerestapiclient.__main__:main',
+			]
+		},
+	keywords='python http rest management',
+	author='Taylor C. Richberger <tcr@absolute-performance.com>',
+	author_email='tcr@absolute-performance.com',
+	url='https://github.com/Taywee/makerestapiclient',
+	download_url='https://github.com/Taywee/makerestapiclient',
+	license='MIT',
+	packages=['makerestapiclient'],
+	include_package_data=False,
+	zip_safe=False,
+	)
