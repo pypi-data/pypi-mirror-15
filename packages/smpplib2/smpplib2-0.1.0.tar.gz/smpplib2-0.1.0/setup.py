ca2ab1586efcdacc4638fc5fc8b602d498d6d953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:33 +0000

    Add package smpplib2/0.1.0/smpplib2-0.1.0.tar.gz

---
+++ packages/smpplib2/smpplib2-0.1.0.tar.gz/smpplib2-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import sys
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+setup(name="smpplib2",
+      version='0.1.0',
+      url='https://github.com/komuW/smpplib2',
+      description='SMPP library for python',
+      packages=find_packages(),
+      zip_safe=True,
+      classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+        ],
+      **extra
+)
