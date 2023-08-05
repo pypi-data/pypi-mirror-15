4fb61b456eb8cfd533733ee03c2b369a96ffa488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package simplesat/0.2.2/simplesat-0.2.2.tar.gz

---
+++ packages/simplesat/simplesat-0.2.2.tar.gz/simplesat-0.2.2/simplesat/_version.py
@@ -0,0 +1,9 @@
+# THIS FILE IS GENERATED FROM SETUP_EXT
+version = '0.2.2'
+full_version = '0.2.2'
+git_revision = '745fbd3d5a32b0b8cc379f4b7f20c60acecfec97'
+is_released = True
+
+msi_version = '0.2.2.723'
+
+version_info = (0, 2, 2, 'final', 0)
