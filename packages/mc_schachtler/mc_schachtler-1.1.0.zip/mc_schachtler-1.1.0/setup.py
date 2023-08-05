ef15567d4bdcd3eda24e2b54f8e8997bad943257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package mc_schachtler/1.1.0/mc_schachtler-1.1.0.zip

---
+++ packages/mc_schachtler/mc_schachtler-1.1.0.zip/mc_schachtler-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		=	'mc_schachtler',
+	version		=	'1.1.0',
+	py_modules	=	['mc_schachtler'],
+	author		=	'MrMcDev',
+	author_email	=	'laugh@aol.com',
+	url		=	'http://www.google.com',
+	description	=	'Einfache Ausgabe verketteter Listen',
+)
