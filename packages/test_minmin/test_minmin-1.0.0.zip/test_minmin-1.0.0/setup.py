9fae04d2f3ee133e267dfcb643e10f75eb1b9b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:08 +0000

    Add package test_minmin/1.0.0/test_minmin-1.0.0.zip

---
+++ packages/test_minmin/test_minmin-1.0.0.zip/test_minmin-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+   name = 'test_minmin',
+   version = '1.0.0',
+   py_modules = ['test_minmin'],
+   author= 'MinMin',
+   author_email = 'kuninminh@gmail.com',
+   url = '',
+   description = 'The first step to be a ...',
+    )
---
+++ packages/test_minmin/test_minmin-1.0.0.zip/test_minmin-1.0.0/test_minmin.py
@@ -0,0 +1,6 @@
+from  Bul_ga import *
+
+
+list = ['aloha',1973,[1,2,3,'salomon'], 'the end']
+
+print_list(list,2)
