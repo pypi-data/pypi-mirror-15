22858bf4437912f3ba561b1c0e428e7cabe57f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:19 +0000

    Add package corlorprinter/0.2.2/corlorprinter-0.2.2.tar.gz

---
+++ packages/corlorprinter/corlorprinter-0.2.2.tar.gz/corlorprinter-0.2.2/corlorprinter/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: linlin
+# @Date:   2016-05-24 16:20:39
+# @Last Modified by:   drinks
+# @Last Modified time: 2016-05-24 23:08:30
+
+from pprint import pprint, PrettyPrinter, pformat, isreadable, isrecursive, saferepr
+from termcorlor import color_write, cprint
+
+__all__ = ("pprint", "pformat", "isreadable", "isrecursive", "saferepr",
+           "PrettyPrinter", 'color_write', 'cprint')
