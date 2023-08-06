64e0f01e6e898e22e6aaad1d0c84f849235495c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:21 +0000

    Add package csgo/0.2.0/csgo-0.2.0.tar.gz

---
+++ packages/csgo/csgo-0.2.0.tar.gz/csgo-0.2.0/csgo/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.2.0"
+__author__ = "Rossen Georgiev"
+
+version_info = (0, 2, 0)
+
+
+# proxy object
+# avoids importing csgo.enums unless it's needed
+class CSGOClient(object):
+    def __new__(cls, *args, **kwargs):
+        from csgo.client import CSGOClient as CSC
+        return CSC(*args, **kwargs)
