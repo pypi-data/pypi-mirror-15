7a3cfb7255084b424958eb0cb2285c4c765b10d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:28 +0000

    Add package manoelgadi12/1.1111/manoelgadi12-1.1111.tar.gz

---
+++ packages/manoelgadi12/manoelgadi12-1.1111.tar.gz/manoelgadi12-1.1111/manoelgadi12/__init__.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+
+__author__ = 'ersh'
+__email__ = 'ershov@student.ie.edu'
+__version__ = '1.111'
+
+import compare_stepwise_genetic
+def hello ():
+    print ("Hello World")
+
