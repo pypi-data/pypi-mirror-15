3ecd248189a6dca72a75914ea8165676c79083e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:42 +0000

    Add package pep8-naming/0.4.0/pep8-naming-0.4.0.tar.gz

---
+++ packages/pep8-naming/pep8-naming-0.4.0.tar.gz/pep8-naming-0.4.0/testsuite/N805.py
@@ -0,0 +1,16 @@
+#: N805
+class Foo(object):
+    def good(self, ads):
+        pass
+
+    def bad(ads, self):
+        pass
+#: Okay
+class Foo(object):
+    def __new__(cls):
+        return object.__new__(Foo)
+#: Okay
+class Foo(object):
+    @classmethod
+    def __prepare__(cls):
+        pass
