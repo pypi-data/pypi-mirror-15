4f242e5ac8b3f468b5e6ad59a61a1d6461d6288b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:13 +0000

    Add package GPlayWeb/0.1.3.4/GPlayWeb-0.1.3.4.tar.gz

---
+++ packages/GPlayWeb/GPlayWeb-0.1.3.4.tar.gz/GPlayWeb-0.1.3.4/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, Command, find_packages
+from setuptools.command.install import install
+import os, glob, stat, sys
+
+class ChmodXCommand(install):
+    """Make /etc/init.d/gplayweb executable"""
+    def run(self):
+        install.run(self)
+        # Make service executable
+        st = os.stat('/etc/init.d/gplayweb')
+        os.chmod('/etc/init.d/gplayweb', st.st_mode | stat.S_IEXEC)
+
+templates = [f for f in glob.glob(os.path.join('templates','*'))]
+fonts = [f for f in glob.glob(os.path.join('static/fonts','*'))]
+css = [f for f in glob.glob(os.path.join('static/css','*'))]
+
+setup(name='GPlayWeb',
+        version='0.1.3.4',
+        description='GPlayWeb, A GPlayCli web interface',
+        author="Matlink",
+        author_email="matlink@matlink.fr",
+        url="https://github.com/matlink/gplayweb",
+        license="AGPLv3",
+        cmdclass={
+            'install': ChmodXCommand,
+        },
+        scripts=['gplayweb'],
+        data_files=[
+            ['/etc/gplayweb/', ['gplayweb.conf.example']],
+            ['/etc/init.d/', ['init_script/gplayweb']],
+            ['/usr/share/gplayweb/templates', templates],
+            ['/usr/share/gplayweb/static/css', css],
+            ['/usr/share/gplayweb/static/fonts', fonts],
+        ],
+        install_requires=[
+            'gplaycli',
+            'tornado',
+        ],
+    )
+
