c46ff62ed8fad60dab597e7ce2618feaea88b9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:21 +0000

    Add package jppy/1.0.2/jppy-1.0.2.tar.gz

---
+++ packages/jppy/jppy-1.0.2.tar.gz/jppy-1.0.2/JDAQEventReader.h
@@ -0,0 +1,28 @@
+#ifndef JDAQEVENTREADER_H
+#define JDAQEVENTREADER_H
+
+namespace jppy {
+    class JDAQEventReader {
+    public:
+        JDAQEventReader();
+        JDAQEventReader(char* filename);
+        void retrieveNextEvent();
+        int getFrameIndex();
+        int getRunNumber();
+        int getDetectorID();
+        int getTriggerCounter();
+        int getTriggerMask();
+        unsigned int getOverlays();
+        int getUTCSeconds();
+        int getUTCNanoseconds();
+        int getNumberOfSnapshotHits();
+        void getHits(int* channel_ids, int* dom_ids, int* times, int* tots,
+                     int* triggereds);
+        bool hasNext();
+//  private:
+//      JSUPPORT::JFileScanner<KM3NETDAQ::JDAQEvent> file_scanner;
+//      KM3NETDAQ::JDAQEvent* event;
+    };
+}
+
+#endif /* JDAQEVENTREADER_H */
