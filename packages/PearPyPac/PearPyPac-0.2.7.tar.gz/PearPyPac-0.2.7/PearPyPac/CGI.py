cb1da367a47cd8f1df44f75ce57679abd39d9511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:17 +0000

    Add package PearPyPac/0.2.7/PearPyPac-0.2.7.tar.gz

---
+++ packages/PearPyPac/PearPyPac-0.2.7.tar.gz/PearPyPac-0.2.7/PearPyPac/CGI.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import os
+import cgi
+
+
+class PearCGI(object):
+
+	def __init__(self):
+		self.param = None
+
+	def getMethod(self):
+		methodName = os.environ['REQUEST_METHOD']
+		return methodName
+
+	def isMethod(self, methodName):
+		if(self.getMethod() is methodNeme):
+			return True
+		else:
+			return False
+
+	def getParam(self):
+		self.param = cgi.FieldStorage()
+		return self.param
+
+	def getValue(self,valueName):
+		value = self.param.getvalue(valueName)
+		return value
+
+	def isValue(self, value):
+		if not value:
+			return False
+		else:
+			return True
+
+if __name__ == '__main__':
+	cgi = PearCGI()
