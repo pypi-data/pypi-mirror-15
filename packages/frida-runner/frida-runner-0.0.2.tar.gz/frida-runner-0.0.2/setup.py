647c8161a88aff5d10a3c406711f0999d7bd0ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:53 +0000

    Add package frida-runner/0.0.2/frida-runner-0.0.2.tar.gz

---
+++ packages/frida-runner/frida-runner-0.0.2.tar.gz/frida-runner-0.0.2/setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: setup.py
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2016-06-29
+"""
+
+from setuptools import setup
+
+setup(
+    name='frida-runner',
+    description="A Stupid CLI script to run Frida on iOS or Android",
+    url='https://github.com/appknox/frida-runner',
+    version='0.0.2',
+    py_modules=['frida_runner'],
+    author='dhilipsiva',
+    author_email='dhilipsiva@gmail.com',
+    install_requires=[
+        'Click',
+        'frida',
+        'six==1.4.1',
+    ],
+    entry_points='''
+        [console_scripts]
+        frida-runner=frida_runner:frida_runner
+    ''',
+)
