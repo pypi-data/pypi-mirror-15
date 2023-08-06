c0c9cbeedfeb5b8ed69ec6be3c98777764b29aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:53 +0000

    Add package yh_nester/1.0.0/yh_nester-1.0.0.zip

---
+++ packages/yh_nester/yh_nester-1.0.0.zip/yh_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='yh_nester',
+    version='1.0.0',
+    py_modules=['yh_nester'],
+    author='yhpython',
+    author_email='2857010991@qq.com',
+    url='http://www.aiyoo.me',
+    description='a simple printer of nester lists',
+)
