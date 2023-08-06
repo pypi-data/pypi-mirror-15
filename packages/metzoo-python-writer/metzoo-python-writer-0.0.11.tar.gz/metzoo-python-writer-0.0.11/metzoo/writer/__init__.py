995b5f08b854081d31dbf210cd3d14e52ade289b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:10 +0000

    Add package metzoo-python-writer/0.0.11/metzoo-python-writer-0.0.11.tar.gz

---
+++ packages/metzoo-python-writer/metzoo-python-writer-0.0.11.tar.gz/metzoo-python-writer-0.0.11/metzoo/writer/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'metzoo-python-writer'
+__version__ = '0.0.11'
+__build__ = 0x00000B
+__author__ = 'Edrans S.A.'
+__copyright__ = 'Copyright 2016 Edrans S.A.'
+
+from .writer import write
