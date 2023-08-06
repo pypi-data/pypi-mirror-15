f6159d4ab9236e230f0e0dd9aafe1cca8ec9ffba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:26 +0000

    Add package minpy/0.0.7/minpy-0.0.7-py3-none-any.whl

---
+++ packages/minpy/minpy-0.0.7-py3-none-any.whl/minpy/array_variants/mxnet/mxnet_wrapper.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Wrapper for MXNet namespace."""
+import types
+
+def wrap_namespace(nspace, reg, prim_wrapper):
+    """Register all functions in a given namespace in the primitive registry
+    Args:
+        nspace:
+            Namespace from which functions are to be registered
+        reg:
+            Primitive registry
+        prim_wrapper:
+            Wrapper to convert a raw function to primitive
+    """
+    function_types = {types.FunctionType, types.BuiltinFunctionType}
+    for name, obj in nspace.items():
+        if len(list(filter(lambda x: isinstance(obj, x), function_types))) != 0:
+            prim = prim_wrapper(obj)
+            reg.register(name, prim)
