f074cec1b4c7fae3ff536a74aae6466cbda8c671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:07 +0000

    Add package azure-mgmt-resource/0.30.0rc4/azure-mgmt-resource-0.30.0rc4.zip

---
+++ packages/azure-mgmt-resource/azure-mgmt-resource-0.30.0rc4.zip/azure-mgmt-resource-0.30.0rc4/azure/mgmt/resource/__init__.py
@@ -0,0 +1,5 @@
+﻿from .features import FeatureClient
+from .locks import ManagementLockClient
+from .policy import PolicyClient
+from .resources import ResourceManagementClient
+from .subscriptions import SubscriptionClient
