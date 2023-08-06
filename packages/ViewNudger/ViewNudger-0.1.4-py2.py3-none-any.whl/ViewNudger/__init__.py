3a04fe1f0b2c7ca86779c20680d0ce3e451fdebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:38 +0000

    Add package ViewNudger/0.1.4/ViewNudger-0.1.4-py2.py3-none-any.whl

---
+++ packages/ViewNudger/ViewNudger-0.1.4-py2.py3-none-any.whl/ViewNudger/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__title__ = 'ViewNudger'
+__author__ = 'Christopher DeVito'
+__email__ = 'chrisdevito@chribis.com'
+__url__ = 'https://github.com/chrisdevito/ViewNudger'
+__version__ = '0.1.4'
+__license__ = 'MIT'
+__description__ = '''A Maya Camera or Object pixel nudger.'''
+
+from .logger import myLogger
+log = myLogger(debug=True)
