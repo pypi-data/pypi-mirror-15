087bc0942acada3c571bdc1a6d0eadfb8c94a0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:42 +0000

    Add package cc.utils/0.0.10/cc.utils-0.0.10.tar.gz

---
+++ packages/cc.utils/cc.utils-0.0.10.tar.gz/cc.utils-0.0.10/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'cc.utils',
+    version = '0.0.10',
+    keywords = ('cchen224'),
+    description = 'cc personal use',
+    license = 'MIT License',
+    install_requires = ['google-api-python-client'],
+    include_package_data=True,
+    zip_safe=True,
+
+    author = 'cchen224',
+    author_email = 'phantomkidding@gmail.com',
+
+    packages = find_packages(),
+    platforms = 'any',
+)
