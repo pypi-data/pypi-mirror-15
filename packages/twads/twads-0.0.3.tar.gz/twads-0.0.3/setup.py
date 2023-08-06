3cb8e1e2b721b434abe57e46564eb7c99d8818fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:36 +0000

    Add package twads/0.0.3/twads-0.0.3.tar.gz

---
+++ packages/twads/twads-0.0.3.tar.gz/twads-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='twads',
+    packages=['twads'],
+    version='0.0.3',
+    description='A wrapper for the Twitter Ads API',
+    author='Jacob Gillespie',
+    author_email='jdgillespie91@gmail.com',
+    url='https://github.com/jdgillespie91/twads'
+)
