1b60cfb4bdc3f651717c3595de2e066f9a01ee5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:13 +0000

    Add package nester_rc54/1.1.0/nester_rc54-1.1.0.tar.gz

---
+++ packages/nester_rc54/nester_rc54-1.1.0.tar.gz/nester_rc54-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			=	'nester_rc54',
+	version 		=	'1.1.0',
+	py_modules		=	['nester_rc54'],
+	author			=	'rchilders',
+	author_email	=	'rchilders54@gmail.com',
+	url				=	'http://richardchilders.ca',
+	description		=	'A simple printer of nested lists',
+	)
