32b2632b155be85e5ce9b8f8770f7ebec132f76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:19 +0000

    Add package django-peeringdb/0.2.2/django-peeringdb-0.2.2.tar.gz

---
+++ packages/django-peeringdb/django-peeringdb-0.2.2.tar.gz/django-peeringdb-0.2.2/setup.py
@@ -0,0 +1,35 @@
+
+from setuptools import find_packages, setup
+
+
+version = open('facsimile/VERSION').read().strip()
+requirements = open('facsimile/requirements.txt').read().split("\n")
+test_requirements = open('facsimile/requirements-test.txt').read().split("\n")
+
+
+setup(
+    name='django-peeringdb',
+    version=version,
+    author='PeeringDB',
+    author_email='support@peeringdb.com',
+    description='PeeringDB models and local synchronization for Django',
+    long_description='',
+    license='LICENSE.txt',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+    ],
+    packages = find_packages(),
+    include_package_data=True,
+
+    url='https://github.com/peeringdb/django-peeringdb',
+    download_url='https://github.com/peeringdb/django-peeringdb/%s' % version,
+
+    install_requires=requirements,
+    test_requires=test_requirements,
+
+    zip_safe=True
+)
