fda93f08d1e04c53cde35a05cec9c43028161705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:17 +0000

    Add package pyuca/1.1.1/pyuca-1.1.1.tar.gz

---
+++ packages/pyuca/pyuca-1.1.1.tar.gz/pyuca-1.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="pyuca",
+    version="1.1.1",
+    description="a Python implementation of the Unicode Collation Algorithm",
+    license="MIT",
+    url="http://github.com/jtauber/pyuca",
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    packages=["pyuca"],
+    package_data={"": ["allkeys-5.2.0.txt", "allkeys-6.3.0.txt"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Text Processing",
+    ],
+)
