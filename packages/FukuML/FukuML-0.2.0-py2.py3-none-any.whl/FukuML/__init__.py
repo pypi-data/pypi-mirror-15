c4648bc3343b3514cbbf0368556fd3d31069a46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:21 +0000

    Add package FukuML/0.2.0/FukuML-0.2.0-py2.py3-none-any.whl

---
+++ packages/FukuML/FukuML-0.2.0-py2.py3-none-any.whl/FukuML/__init__.py
@@ -0,0 +1,29 @@
+#encoding=utf8
+
+__version__ = '0.2.0'
+__all__ = [
+    'MLBase',
+    'PLA',
+    'PLA.BinaryClassifier',
+    'PLA.MultiClassifier',
+    'PocketPLA',
+    'PocketPLA.BinaryClassifier',
+    'PocketPLA.MultiClassifier',
+    'LinearRegression',
+    'LinearRegression.LinearRegression',
+    'LinearRegression.BinaryClassifier',
+    'LinearRegression.MultiClassifier',
+    'LinearRegression.Accelerator',
+    'LogisticRegression.LogisticRegression',
+    'LogisticRegression.BinaryClassifier',
+    'LogisticRegression.MultiClassifier',
+    'RidgeRegression',
+    'RidgeRegression.RidgeRegression',
+    'RidgeRegression.BinaryClassifier',
+    'RidgeRegression.MultiClassifier',
+    'SupportVectorMachine.BinaryClassifier',
+    'Utility',
+    'Utility.DatasetLoader',
+    'Utility.CrossValidator',
+    'Utility.Serializer'
+]
