7b2963612c3f12308ac64d6cf63fedcdb9b4954e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:13 +0000

    Add package octoproxy/1.0/octoproxy-1.0.tar.gz

---
+++ packages/octoproxy/octoproxy-1.0.tar.gz/octoproxy-1.0/setup.py
@@ -0,0 +1,50 @@
+import setuptools
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setuptools.setup(
+    name="octoproxy",
+    version="1.0",
+    url="https://github.com/mverteuil/octoproxy",
+
+    author="M. de Verteuil",
+    author_email="mverteuil@github.com",
+
+    description="Proxy for Sidelaunching on GitHub Events",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=['Flask>=0.10.1', 'requests>=2.0.0'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
