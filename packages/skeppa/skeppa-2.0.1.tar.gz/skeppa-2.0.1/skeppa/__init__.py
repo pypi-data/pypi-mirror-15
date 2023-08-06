33a57ca22dbea44309eee723ff22c337207403e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:26 +0000

    Add package skeppa/2.0.1/skeppa-2.0.1.tar.gz

---
+++ packages/skeppa/skeppa-2.0.1.tar.gz/skeppa-2.0.1/skeppa/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+skeppa
+------------
+:copyright: (c) 2016 Martin Sandström
+:license: MIT, see LICENSE for more details.
+"""
+
+
+__title__ = "skeppa"
+__version__ = '2.0.1'
+__build__ = 201
+__license__ = "MIT"
+__copyright__ = "Copyright 2016 Martin Sandström"
