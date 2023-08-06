6f80f72d36616075bdbdc8a0dbd5deae95b09eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:08 +0000

    Add package nester_leo/1.0.0/nester_leo-1.0.0.zip

---
+++ packages/nester_leo/nester_leo-1.0.0.zip/nester_leo-1.0.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 27 16:37:35 2016
+
+@author: AMANO
+"""
+
+from distutils.core import setup
+setup(
+        name         =   'nester_leo',
+        version      =   '1.0.0',
+        py_modules   =   ['nester_leo'], 
+        auther       =   'hfpython',
+        auther_email =   'hfpython@headfirstlabs.com',
+        url          =   'http://www.headfirstlabs.com',
+        description  =   'A simple printer of nested lists',
+        )
