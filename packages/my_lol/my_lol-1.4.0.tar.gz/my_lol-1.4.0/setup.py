4cece6536e6500a2c12cdd28aea5444769631691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:06 +0000

    Add package my_lol/1.4.0/my_lol-1.4.0.tar.gz

---
+++ packages/my_lol/my_lol-1.4.0.tar.gz/my_lol-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='my_lol',
+    version='1.4.0',
+    py_moudles=['my_lol'],
+    author='yang',
+    author_email='1098534980@qq.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+)
