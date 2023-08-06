612bf06c9838f7460e96182bf80bb6ac959ac0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:40 +0000

    Add package GangGang/0.1/GangGang-0.1.tar.gz

---
+++ packages/GangGang/GangGang-0.1.tar.gz/GangGang-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'GangGang',
+  version = '0.1',
+  description = 'A library to use a remote server to execute code, mainly for GhPython in Grasshopper3D.',
+  author = 'Dan Taeyoung',
+  author_email = 'provolot@gmail.com',
+  url = 'https://github.com/provolot/GangGang', # use the URL to the github repo
+  download_url = 'https://github.com/provolot/GangGang/tarball/0.1', # I'll explain this in a second
+  keywords = ['grasshopper'], # arbitrary keywords
+  classifiers = [],
+)
