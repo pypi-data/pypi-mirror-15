a67d959e86004c467d403d6ee946500d179553b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:44 +0000

    Add package Flask-Maple/0.3.2/Flask-Maple-0.3.2.tar.gz

---
+++ packages/Flask-Maple/Flask-Maple-0.3.2.tar.gz/Flask-Maple-0.3.2/flask_maple/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding=UTF-8 -*-
+# **************************************************************************
+# Copyright © 2016 jianglin
+# File Name: __init__.py
+# Author: jianglin
+# Email: xiyang0807@gmail.com
+# Created: 2016-04-18 19:04:49 (CST)
+# Last Update:星期一 2016-5-30 23:56:17 (CST)
+#          By: jianglin
+# Description:
+# **************************************************************************
+from .captcha import Captcha
+from .bootstrap import Bootstrap
+from .auth import Auth
+from .error import Error
+from .markdown import MapleMkd
