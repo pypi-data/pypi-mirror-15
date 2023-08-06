1a2223214a44dc5f054b9986bb152b8f4f256698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:24 +0000

    Add package drudge-parser/1.0/drudge_parser-1.0.tar.gz

---
+++ packages/drudge-parser/drudge_parser-1.0.tar.gz/drudge_parser-1.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+
+setup(name='drudge_parser',
+      version='1.0',
+      description='Naive parser for the Drudge Report',
+      author='Matt Rasband',
+      author_email='matt.rasband@gmail.com',
+      url='https://github.com/mrasband/drudge_parser',
+      keywords=['parsing', 'webscraping', 'drudge', 'drudgereport'],
+      packages=['drudge_parser'],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Developers',
+          'Development Status :: 3 - Alpha',
+      ],
+      install_requires=[
+          'beautifulsoup4',
+          'html5lib'
+      ])
