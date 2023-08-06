1963ee24849f70943fcdcd0babe8545b95bfc51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:17 +0000

    Add package avchd-toolkit/0.4.1/avchd-toolkit-0.4.1.tar.gz

---
+++ packages/avchd-toolkit/avchd-toolkit-0.4.1.tar.gz/avchd-toolkit-0.4.1/avchdtoolkit/finder.py
@@ -0,0 +1,39 @@
+import os
+import mimetypes
+import archive
+
+
+def find_files(directory):
+    results = []
+    for dirpath, dirnames, filenames in os.walk(directory, topdown=False):
+        for name in filenames:
+            fullpath = os.path.join(dirpath, name)
+            mimetype,enc=mimetypes.guess_type('file://%s' % fullpath)
+            if mimetype:
+                category = mimetype.split('/')[0]
+                if category=='video':
+                    results.append(fullpath)
+
+    return sorted(results)
+
+
+def find_archives(directory, reel=None):
+    results = []
+    if reel:
+        reel_lower = reel.lower()
+
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for dirname in dirnames:
+            try:
+                arc = archive.read(os.path.join(dirpath,dirname))
+            except IOError:
+                pass
+            else:
+                if reel:
+                    if arc.reel_name and (reel_lower=='*' or reel_lower in arc.reel_name.lower()):
+                        results.append(arc)
+                else:
+                    results.append(arc)
+
+    return results
+
