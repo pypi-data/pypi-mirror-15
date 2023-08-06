0bb81d3fd043d96ccde49e7a8dfe61e501ccdd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:31 +0000

    Add package printByLzx/1.1.0/printByLzx-1.1.0.tar.gz

---
+++ packages/printByLzx/printByLzx-1.1.0.tar.gz/printByLzx-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name		='printByLzx',
+		version		='1.1.0',
+		py_modules	=['printByLzx'],
+		author		='zongxlseed',
+		author_email	='873929454@qq.com',
+		url		='http://www.baidu.com',
+		description	='a test function',
+)
