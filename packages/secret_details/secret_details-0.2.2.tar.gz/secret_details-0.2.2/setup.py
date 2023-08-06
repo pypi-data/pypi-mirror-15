8ba11b98bd2f015e848fcf03e625ff7cad530713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:36 +0000

    Add package secret_details/0.2.2/secret_details-0.2.2.tar.gz

---
+++ packages/secret_details/secret_details-0.2.2.tar.gz/secret_details-0.2.2/setup.py
@@ -0,0 +1,19 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'secret_details',
+    'author': 'Eran Zimbler',
+    'url': 'https://github.com/srgrn/secrets_details',
+    'author_email': 'eranz@rumble.me',
+    'version': '0.2.2',
+    'install_requires': ['nose'],
+    'packages': ['secrets_details'],
+    'scripts': ['bin/ProvExplorer'],
+    'name': 'secret_details'
+}
+
+setup(**config)
