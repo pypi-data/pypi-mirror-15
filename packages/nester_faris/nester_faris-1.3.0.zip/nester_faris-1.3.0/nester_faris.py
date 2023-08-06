dbe03ae2ac961cd90d92de40c69dd6e28377ccd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:01 +0000

    Add package nester_faris/1.3.0/nester_faris-1.3.0.zip

---
+++ packages/nester_faris/nester_faris-1.3.0.zip/nester_faris-1.3.0/nester_faris.py
@@ -0,0 +1,10 @@
+#这是我的第一个python模块代码，函数实现列表的打印输出功能
+def print_lol(the_list,indent=False,level=0,fh=sys.stdout):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1,fh)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='',file=fh)
+            print(each_item,file=fh)
