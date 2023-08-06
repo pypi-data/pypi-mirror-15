3e779310f2475daea6516c96d69164840f8295a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:14 +0000

    Add package plasticparser/0.3.3/plasticparser-0.3.3.tar.gz

---
+++ packages/plasticparser/plasticparser-0.3.3.tar.gz/plasticparser-0.3.3/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+long_description = """
+ Let's to convert Google Like Query Language into
+ ElasticSearch understandable Query DSL
+ """
+
+setup(
+    name='plasticparser',
+    version='0.3.3',
+    description='An Elastic Search Query Parser',
+    long_description=long_description,
+    url='https://github.com/Aplopio/plasticparser',
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords=[
+        "elasticsearch ", "query language", "query parser"
+    ],
+    packages=['plasticparser'],
+    install_requires=[
+        'pyparsing==2.0.2',
+        'ProxyTypes==0.9'
+    ],
+)
