3d9f6b71f24f423430b4eba923286a5dbe462912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:37 +0000

    Add package ftw.profilehook/1.2.1/ftw.profilehook-1.2.1.tar.gz

---
+++ packages/ftw.profilehook/ftw.profilehook-1.2.1.tar.gz/ftw.profilehook-1.2.1/ftw/profilehook/subscribers.py
@@ -0,0 +1,27 @@
+from ftw.profilehook.interfaces import IBeforeImportHook
+from ftw.profilehook.interfaces import IProfileHook
+from zope.component import queryAdapter
+from zope.component.hooks import getSite
+import re
+
+
+def profile_imported(event):
+    if not event.full_import:
+        return
+
+    trigger_hook_for(event.profile_id, IProfileHook)
+
+
+def before_profile_import(event):
+    if not event.full_import:
+        return
+
+    trigger_hook_for(event.profile_id, IBeforeImportHook)
+
+
+def trigger_hook_for(profile, providing):
+    profile = re.sub('^profile-', '', profile)
+    site = getSite()
+    hook = queryAdapter(site, providing, name=profile)
+    if hook:
+        hook(site)
