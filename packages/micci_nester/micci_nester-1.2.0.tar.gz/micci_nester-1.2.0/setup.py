70a9828acfc768bca60a4a753628e1d1d6ba4b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:19 +0000

    Add package micci_nester/1.2.0/micci_nester-1.2.0.tar.gz

---
+++ packages/micci_nester/micci_nester-1.2.0.tar.gz/micci_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'micci_nester',
+    version = '1.2.0',
+    py_modules = ['nester'],
+    author = 'Micci',
+    author_email = 'luiz.miccieli@gmail.com',
+    url = 'http://www.google.com',
+    description = 'A simples printer of nested lists',
+)
