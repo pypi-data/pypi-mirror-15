d06a66ec80b88a719b37c82b9980517025c7f856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:58 +0000

    Add package webbpsf/0.5.0/webbpsf-0.5.0.tar.gz

---
+++ packages/webbpsf/webbpsf-0.5.0.tar.gz/webbpsf-0.5.0/webbpsf/tests/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This packages contains affiliated package tests.
+"""
+# basic unit tests of software functionality:
+#from . import test_webbpsf
+
+# more detailed validation against other people's sims:
+#from . import validate_webbpsf
+from . import test_webbpsf
+from . import test_utils
