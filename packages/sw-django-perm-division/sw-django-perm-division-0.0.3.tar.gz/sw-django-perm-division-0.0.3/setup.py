3153506200b94b9c7896a686900c68c67f685cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:22 +0000

    Add package sw-django-perm-division/0.0.3/sw-django-perm-division-0.0.3.tar.gz

---
+++ packages/sw-django-perm-division/sw-django-perm-division-0.0.3.tar.gz/sw-django-perm-division-0.0.3/setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# python setup.py sdist register upload
+# from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='sw-django-perm-division',
+    version='0.0.3',
+    description='Soft Way company permission system based on employee division affiliations.',
+    author='Telminov Sergey',
+    author_email='sergey@telminov.ru',
+    url='https://github.com/telminov/sw-django-division-perm',
+    include_package_data=True,
+    packages=find_packages(),
+    license='The MIT License',
+    requires=[
+        'django',
+    ],
+)
