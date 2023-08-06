fb2fe374ff075107f4e28cb68558b20ff4da22ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:23 +0000

    Add package jsdpkg/1.0/jsdpkg-1.0.zip

---
+++ packages/jsdpkg/jsdpkg-1.0.zip/jsdpkg-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'jsdpkg',
+
+    version='1.0',
+      description='Python Distribution Utilities',
+      author='Jagdeep Singh',
+      author_email='786jagdeepsingh@gmail.com',
+      url='https://pypi.python.org/id/jsdpython',
+      package_dir={'jsdpkg': 'src/jsdpkg'},
+)
