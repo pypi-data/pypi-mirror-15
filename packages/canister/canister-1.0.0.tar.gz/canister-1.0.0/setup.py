0f7e23240d026077db6a79aab59874bedc0cbb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:16 +0000

    Add package canister/1.0.0/canister-1.0.0.tar.gz

---
+++ packages/canister/canister-1.0.0.tar.gz/canister-1.0.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='canister',
+
+    version='1.0.0',
+
+    description='a bottle wrapper to provide logging, sessions and authentication.',
+    
+    # The project's main homepage.
+    url='https://github.com/dagnelies/canister',
+
+    # Author details
+    author='Arnaud Dagnelies',
+    author_email='arnaud.dagnelies@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='bottle server webserver session logging authentication',
+    py_modules=['canister'],
+    scripts=['canister.py']
+)
