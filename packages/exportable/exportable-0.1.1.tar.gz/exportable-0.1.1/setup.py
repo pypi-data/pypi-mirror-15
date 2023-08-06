e94e8a9768b64821efccacc33233c960897ccb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:08 +0000

    Add package exportable/0.1.1/exportable-0.1.1.tar.gz

---
+++ packages/exportable/exportable-0.1.1.tar.gz/exportable-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from distutils.core import setup
+
+
+def get_requirements():
+    """Get requirements from requirements.txt. Lines starting with a # are skipped."""
+    here = os.path.dirname(__file__)
+    req = os.path.join(here, "requirements.txt")
+    for line in open(req, "r", encoding="utf-8"):
+        if line.startswith("#") or not line.strip():
+            continue
+        yield line.strip()
+
+
+setup(
+    name='exportable',
+    version='0.1.1',
+    packages=[
+        'exportable',
+        'exportable.exporters',
+        'exportable.exporters.tests'
+    ],
+    package_data={
+        '': ['LICENSE.txt', 'requirements.txt'],
+    },
+    url='https://github.com/amcat/exportable',
+    license='AGPL',
+    author='AmCAT Developers',
+    author_email='',
+    description='',
+    install_requires=list(get_requirements())
+)
