1a14b9c604ef3561df198dc4b50825fcff500204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:23 +0000

    Add package biotoolkit/0.1/biotoolkit-0.1.tar.gz

---
+++ packages/biotoolkit/biotoolkit-0.1.tar.gz/biotoolkit-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+def main():
+	setup(
+		name='biotoolkit',
+		version='0.1',
+		description='biotoolkit contains GCfromDNA: Analyze GC-content of DNA sequences from fasta files',
+		author='meh',
+		packages=['biotoolkit'],
+	)
+	
+	pass
+if __name__ == '__main__':
+	main()
