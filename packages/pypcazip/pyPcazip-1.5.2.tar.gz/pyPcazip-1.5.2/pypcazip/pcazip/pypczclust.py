d7f7e9efcfe1216e48094668994f9640b089c637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:14 +0000

    Add package pypcazip/1.5.2/pyPcazip-1.5.2.tar.gz

---
+++ packages/pypcazip/pyPcazip-1.5.2.tar.gz/pyPcazip-1.5.2/pypcazip/pcazip/pypczclust.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python -W ignore
+'''
+                 *** The command line interface for pyPczclust ***
+
+                       Adapted to use the mapping module.
+'''
+
+import logging as log
+
+import numpy as np
+from scipy import ndimage
+
+from MDPlus.analysis.pca import Pczfile
+from MDPlus.analysis import mapping
+
+def pczclust(args): 
+    '''
+    Performs histogram/watershed based clustering on data from a .pcz
+    file.
+    '''
+
+    if args.verbosity > 0:
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+        log.info("Verbose output")
+    else:
+        log.basicConfig(format="%(levelname)s: %(message)s")
+
+    p = Pczfile(args.pczfile)
+
+    projs = p.projs[:args.dims].T
+
+    m = mapping.Map(projs,resolution=args.bins, boundary=1)
+    mapping.watershed(m)
+    out = [m.cluster_id(id) for id in projs]
+
+    np.savetxt(args.outfile,np.c_[projs,out], fmt=("%8.3f"*args.dims + "%5d"))
