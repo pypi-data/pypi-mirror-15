9edc918ec4dd9b6b4e3070ef0f71469cb1527afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:24 +0000

    Add package dork-compose/0.1.8/dork-compose-0.1.8.tar.gz

---
+++ packages/dork-compose/dork-compose-0.1.8.tar.gz/dork-compose-0.1.8/dork_compose/plugins/single.py
@@ -0,0 +1,16 @@
+import dork_compose.plugin
+
+
+class Plugin(dork_compose.plugin.Plugin):
+    def get_instance(self):
+        return self.basedir.split('/')[-1]
+
+    def environment(self):
+        return {
+            'DORK_INSTANCE': self.get_instance()
+        }
+
+    def info(self):
+        return {
+            'Instance': self.get_instance()
+        }
