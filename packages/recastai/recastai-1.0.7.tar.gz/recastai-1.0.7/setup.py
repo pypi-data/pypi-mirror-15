b300e7d1d72912768e4ffe448728e79f745778fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:01 +0000

    Add package recastai/1.0.7/recastai-1.0.7.tar.gz

---
+++ packages/recastai/recastai-1.0.7.tar.gz/recastai-1.0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="recastai",
+    version="1.0.7",
+    py_modules=["recastai"],
+    description="Recast.AI official SDK for python",
+    long_description="Recast.AI official SDK for python",
+    author="Paul Renvoise",
+    author_email="paul.renvoise@recast.ai",
+    url="https://github.com/RecastAI/sdk-python")
