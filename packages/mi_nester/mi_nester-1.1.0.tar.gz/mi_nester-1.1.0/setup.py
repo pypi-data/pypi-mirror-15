cbfe601e83bb3d5852bf8af2179ce00774baef64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:07 +0000

    Add package mi_nester/1.1.0/mi_nester-1.1.0.tar.gz

---
+++ packages/mi_nester/mi_nester-1.1.0.tar.gz/mi_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name         = 'mi_nester',
+	version      = '1.1.0',
+	py_modules   = ['mi_nester'],
+	author       = 'miki',
+	author_email = 'fangyu1313@gmail.com',
+	url          = 'https://pypi.python.org/pypi?%3Aaction=pkg_edit&name=mi_nester',
+	description  = 'A simple',
+)
