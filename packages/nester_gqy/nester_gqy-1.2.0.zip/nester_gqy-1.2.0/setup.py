fd965885d5fe0272cfb6e31cf572988f84e99235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:55 +0000

    Add package nester_gqy/1.2.0/nester_gqy-1.2.0.zip

---
+++ packages/nester_gqy/nester_gqy-1.2.0.zip/nester_gqy-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name 	= 'nester_gqy',
+	version	= '1.2.0',
+	py_modules = ['nester'],
+	author	= 'gqy',
+	author_email = 'china_gqy@163.com',
+        url	=	'https://www.python.org',
+	description	= 'A simple printer of nested lists',
+
+	)
