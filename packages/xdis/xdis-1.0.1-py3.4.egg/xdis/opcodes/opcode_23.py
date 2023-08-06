96d02a788254581f6a121d3d200e801bd156fb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:45 +0000

    Add package xdis/1.0.1/xdis-1.0.1-py3.4.egg

---
+++ packages/xdis/xdis-1.0.1-py3.4.egg/xdis/opcodes/opcode_23.py
@@ -0,0 +1,39 @@
+"""
+CPython 2.3 bytecode opcodes
+
+This is used in bytecode disassembly. This is equivalent of to the
+opcodes in Python's dis.py library.
+
+"""
+
+from copy import deepcopy
+
+import xdis.opcodes.opcode_2x as opcode_2x
+
+# FIXME: can we DRY this even more?
+
+# Make a *copy* of opcode_2x values so we don't pollute 2x
+hasconst = list(opcode_2x.hasconst)
+hascompare = list(opcode_2x.hascompare)
+hasfree = list(opcode_2x.hasfree)
+haslocal = list(opcode_2x.haslocal)
+hasnargs = list(opcode_2x.hasnargs)
+opmap = list(opcode_2x.opmap)
+opname = list(opcode_2x.opname)
+EXTENDED_ARG = opcode_2x.EXTENDED_ARG
+
+for object in opcode_2x.fields2copy:
+    globals()[object] =  deepcopy(getattr(opcode_2x, object))
+
+def updateGlobal():
+    globals().update(dict([(k.replace('+', '_'), v) for (k, v) in opcode_2x.opmap.items()]))
+    globals().update({'JUMP_OPs': map(lambda op: opcode_2x.opname[op],
+                                          opcode_2x.hasjrel + opcode_2x.hasjabs)})
+
+from xdis import PYTHON_VERSION
+if PYTHON_VERSION == 2.3:
+    import dis
+    # print(set(dis.opmap.items()) - set(opmap.items()))
+    # print(set(opmap.items()) - set(dis.opmap.items()))
+    assert all(item in opmap.items() for item in dis.opmap.items())
+    assert all(item in dis.opmap.items() for item in opmap.items())
