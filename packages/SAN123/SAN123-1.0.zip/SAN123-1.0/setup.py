e945a5dc2936475d03d2a963b98babfc419f551b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:16 +0000

    Add package SAN123/1.0/SAN123-1.0.zip

---
+++ packages/SAN123/SAN123-1.0.zip/SAN123-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'SAN123',
+    version = '1.0',
+    py_modeules = ['hello'],
+    author = 'SANTOSH',
+    author_email = 'sa@sa.com',
+    url = 'http://www.sa.com',
+    description = 'hello setup',
+    )
