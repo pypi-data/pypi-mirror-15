bd706d35e67b70e8f942cec1838a19a0d5354e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:58 +0000

    Add package fio_shipping/3.4.25.2/fio_shipping-3.4.25.2.tar.gz

---
+++ packages/fio_shipping/fio_shipping-3.4.25.2.tar.gz/fio_shipping-3.4.25.2/__init__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__.py
+
+"""
+from trytond.pool import Pool
+
+from carrier import Carrier, Service, CarrierService, BoxType, CarrierBoxType
+from party import (
+    Address, AddressValidationMsg, AddressValidationWizard,
+    AddressValidationSuggestionView
+)
+from shipment import (
+    ShipmentOut, GenerateShippingLabelMessage, GenerateShippingLabel,
+    ShippingCarrierSelector, ShippingLabelNoModules, Package, ShipmentTracking,
+)
+from stock import StockMove
+from sale import Sale, SaleLine, ReturnSale, ApplyShippingStart, \
+    ApplyShippingSelectRate, ApplyShipping
+from configuration import PartyConfiguration
+from log import CarrierLog
+from manifest import ShippingManifest
+
+
+def register():
+    Pool.register(
+        PartyConfiguration,
+        Carrier,
+        Service,
+        CarrierService,
+        BoxType,
+        CarrierBoxType,
+        CarrierLog,
+        Address,
+        ShipmentTracking,
+        ShippingManifest,
+        ShipmentOut,
+        StockMove,
+        Package,
+        Sale,
+        SaleLine,
+        ApplyShippingStart,
+        ApplyShippingSelectRate,
+        GenerateShippingLabelMessage,
+        ShippingLabelNoModules,
+        ShippingCarrierSelector,
+        AddressValidationMsg,
+        AddressValidationSuggestionView,
+        module='shipping', type_='model'
+    )
+    Pool.register(
+        GenerateShippingLabel,
+        AddressValidationWizard,
+        ReturnSale,
+        ApplyShipping,
+        module='shipping', type_='wizard'
+    )
