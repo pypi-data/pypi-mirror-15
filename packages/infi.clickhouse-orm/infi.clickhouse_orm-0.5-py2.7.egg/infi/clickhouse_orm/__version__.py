a8c25e2d03d92b73df54899dce93341eb3631be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:00 +0000

    Add package infi.clickhouse-orm/0.5/infi.clickhouse_orm-0.5-py2.7.egg

---
+++ packages/infi.clickhouse-orm/infi.clickhouse_orm-0.5-py2.7.egg/infi/clickhouse_orm/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.5"
+__git_commiter_name__ = "Itai Shirav"
+__git_commiter_email__ = "itais@infinidat.com"
+__git_branch__ = '(detached from v0.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '0e892909f62434e74988d2ac5edb76b1eb6d69e4'
+__git_head_subject__ = 'Finished Release v0.5'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-06-26 18:09:04'
