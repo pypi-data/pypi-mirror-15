bf13da80e4cd72c608aca59f722197ac6ab4ec95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:19 +0000

    Add package courtana/0.3.1/courtana-0.3.1-py3-none-any.whl

---
+++ packages/courtana/courtana-0.3.1-py3-none-any.whl/courtana/courtana.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Courtana
+========
+
+
+"""
+
+gender_color = {'f': '#dd1c77', 'm': '#3182bd'}
+
+
+class Courtana(object):
+
+    """
+    Courtana
+    """
+
+    def __init__(self):
+        pass
