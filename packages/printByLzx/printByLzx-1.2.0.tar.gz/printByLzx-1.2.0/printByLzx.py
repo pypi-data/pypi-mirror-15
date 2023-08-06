fedff59bd40c13cc2242422cb802eb568b0090fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:31 +0000

    Add package printByLzx/1.2.0/printByLzx-1.2.0.tar.gz

---
+++ packages/printByLzx/printByLzx-1.2.0.tar.gz/printByLzx-1.2.0/printByLzx.py
@@ -0,0 +1,8 @@
+def print_list(list_data,level=0):
+	for data in list_data:
+		if isinstance(data,list):
+			print_list(data,level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t",end='')
+			print(data)
