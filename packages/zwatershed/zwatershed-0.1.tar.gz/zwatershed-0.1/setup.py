f92d622dbbbc566cb02bc56d1ebae55fd19dbb7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:54 +0000

    Add package zwatershed/0.1/zwatershed-0.1.tar.gz

---
+++ packages/zwatershed/zwatershed-0.1.tar.gz/zwatershed-0.1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='zwatershed',
+      version='0.1',
+      description='Fast watersheds',
+      url='https://github.com/TuragaLab/zwatershed',
+      author='Chandan Singh',
+      author_email='cs3hq@virginia.edu',
+      license='MIT',
+      packages=['zwatershed'],
+      zip_safe=False)
