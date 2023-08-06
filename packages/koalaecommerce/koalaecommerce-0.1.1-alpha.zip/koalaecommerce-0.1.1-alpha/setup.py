4a619f3f5d9dafff4c340fc62ea5e799a94b73a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:20 +0000

    Add package koalaecommerce/0.1.1-alpha/koalaecommerce-0.1.1-alpha.zip

---
+++ packages/koalaecommerce/koalaecommerce-0.1.1-alpha.zip/koalaecommerce-0.1.1-alpha/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='koalaecommerce',
+    packages=['koalaecommerce'],  # this must be the same as the name above
+    version='0.1.1-alpha',
+    description='',
+    author='Matt Badger',
+    author_email='foss@lighthouseuk.net',
+    url='https://github.com/LighthouseUK/koalaecommerce',  # use the URL to the github repo
+    download_url='https://github.com/LighthouseUK/koalaecommerce/tarball/0.1.1-alpha',  # I'll explain this in a second
+    keywords=['gae', 'lighthouse', 'koala'],  # arbitrary keywords
+    classifiers=[],
+    requires=['koalacore', 'blinker', 'satchless', 'itsdangerous', 'prices', 'pycrypto'],
+    #tests_require=['paste', 'webapp2', 'requests', 'splinter', 'koalamail', 'koalasendgrid'],
+)
