32138f5e5dfa32d76486893c9237dad7a86b9584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:26 +0000

    Add package bw2io/0.5.1/bw2io-0.5.1-py3-none-any.whl

---
+++ packages/bw2io/bw2io-0.5.1-py3-none-any.whl/bw2io/errors.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+from eight import *
+
+class InvalidPackage(Exception):
+    """bw2package data doesn't validate"""
+    pass
+
+
+class UnsafeData(Exception):
+    """bw2package data comes from a class that isn't recognized by Brightway2"""
+    pass
+
+
+class UnsupportedExchange(Exception):
+    """This exchange uncertainty type can't be rescaled automatically"""
+    pass
+
+
+class StrategyError(Exception):
+    """The strategy could not be applied"""
+    pass
