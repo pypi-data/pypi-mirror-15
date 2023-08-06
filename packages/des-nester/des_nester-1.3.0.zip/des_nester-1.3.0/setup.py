3d773c58ae5815d032f3af72d8a5f1088e7bd23e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:17 +0000

    Add package des-nester/1.3.0/des_nester-1.3.0.zip

---
+++ packages/des-nester/des_nester-1.3.0.zip/des_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = "des_nester",
+    version      = "1.3.0",
+    py_modules   = ["des_nester"],
+    author       = "desterio",
+    author_email = "desteriodennis@gmail.com",
+    url          = "http://headfirstlabs.com",
+    description  = "A simple printer of nested lists"
+)
