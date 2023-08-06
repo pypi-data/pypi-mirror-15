ce3c59b51cb10e1aeb4e31864fcb17d9c1164717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:05 +0000

    Add package timingdecorator/0.1.3/timingdecorator-0.1.3.tar.gz

---
+++ packages/timingdecorator/timingdecorator-0.1.3.tar.gz/timingdecorator-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'timingdecorator',
+  packages = ['timingdecorator'],
+  version = '0.1.3',
+  description = 'A timing decorator',
+  author = 'Iddo Berger',
+  author_email = 'iddoberger@gmail.com',
+  url = 'https://github.com/iddoberger/timingdecorator',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1.3',
+  keywords = ['timing',], # arbitrary keywords
+  classifiers = [],
+)
