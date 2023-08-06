e0de05edcc87e749a39ce60c95738920e5073bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:14 +0000

    Add package bipol/0.0.5/bipol-0.0.5.tar.gz

---
+++ packages/bipol/bipol-0.0.5.tar.gz/bipol-0.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bipol',
+    version='0.0.5',
+    description='A bipol plugin',
+    license='MIT',
+    author='Patrick Sanders',
+    author_email='mail@patricksanders.net',
+    url='https://github.com/patricksanders/bipol',
+    install_requires=[
+        'requests',
+        'helga',
+    ],
+    packages=find_packages(),
+    py_modules=['bipol'],
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points=dict(
+        helga_plugins=[
+            'bipol = bipol:bipol',
+        ],
+    ),
+)
