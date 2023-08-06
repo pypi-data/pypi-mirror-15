11d21c281b0c4ebf0720843db156dc6135648a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:25 +0000

    Add package orionclient/0.0.2/orionclient-0.0.2.tar.gz

---
+++ packages/orionclient/orionclient-0.0.2.tar.gz/orionclient-0.0.2/test/test_settings.py
@@ -0,0 +1,8 @@
+class settings:
+	orion_host_url='http://orion.lab.fiware.org'
+	orion_host_port=1026  #80   #or 443
+	authMethod='fiware-token'
+	username='opentrack@reportlab.com'
+	password='0penTrackFiware'
+	orion_token_url='https://orion.lab.fiware.org/token'
+settings=settings()
