af8f22bbb88eb932a88718eeb698de64aa484a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:00 +0000

    Add package ztv/0.2.3.post2/ztv-0.2.3.post2-py2-none-any.whl

---
+++ packages/ztv/ztv-0.2.3.post2-py2-none-any.whl/ztv_examples/fits_faker_panel/launch_ztv.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+from ztv import ZTV
+import wx
+import time
+
+# intended to be run with, e.g.:
+#     ipython
+#     run launch_ztv.py
+# or
+#     ipython
+#     from ztv_examples.fits_faker_panel.launch_ztv import launch_ztv
+#     z = launch_ztv()
+
+def launch_ztv():
+    z = ZTV(control_panels_module_path='ztv_examples.fits_faker_panel.control_panels')
+    z.start_fits_faker = lambda: z._send_to_ztv('fits-faker-start')
+    z.stop_fits_faker = lambda: z._send_to_ztv('fits-faker-stop')
+    z.control_panel('Faker')
+    return z
+
+if __name__ == '__main__':
+    z = launch_ztv()
