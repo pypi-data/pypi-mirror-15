8b387ef050041d8f660b6299b3f7ed62038003b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:38 +0000

    Add package mpris2/1.0.2/mpris2-1.0.2.tar.gz

---
+++ packages/mpris2/mpris2-1.0.2.tar.gz/mpris2-1.0.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+'''
+Created on Nov 6, 2011
+
+@author: hugosenari
+'''
+
+from distutils.core import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(name='mpris2',
+      version='1.0.2',
+      description='Python mpris2 definition',
+      author='hugosenari',
+      author_email='hugosenari@gmail.com',
+      url='https://github.com/hugosenari/mpris2',
+      keywords = ["dbus", "mpris2"],
+      packages=('mpris2', 'mpris2.decorator', 'mpris2.types'),
+      requires=(),
+      license = "GPL",
+      classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Topic :: Multimedia :: Sound/Audio",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: GNU General Public License (GPL)",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.4"
+      ],
+      long_description = readme
+)
