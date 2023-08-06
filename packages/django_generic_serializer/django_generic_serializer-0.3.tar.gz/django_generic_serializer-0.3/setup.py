e863e6be7fcfc334553267d15e14cd81536ecf9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:25 +0000

    Add package django_generic_serializer/0.3/django_generic_serializer-0.3.tar.gz

---
+++ packages/django_generic_serializer/django_generic_serializer-0.3.tar.gz/django_generic_serializer-0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'django_generic_serializer',
+  packages = ['django_generic_serializer'], # this must be the same as the name above
+  version = '0.3',
+  description = 'This serializes and given a object according to the dict passed to it',
+  author = 'Abhishek K gowda',
+  author_email = 'abhishek.k6006@gmail.com',
+  url = 'https://github.com/abhishek246/django_generic_serializers.git', # use the URL to the github repo
+  download_url = 'https://github.com/abhishek246/django_generic_serializers.git/tarball/0.3', # I'll explain this in a second
+  keywords = ['django', 'serializer', 'django-serializers'], # arbitrary keywords
+  classifiers = [],
+)
