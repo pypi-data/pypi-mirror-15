344391e8386bdd7145cb8eb755cdbde2cc9d3ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:38 +0000

    Add package matfactor/1.0.3/matfactor-1.0.3.tar.gz

---
+++ packages/matfactor/matfactor-1.0.3.tar.gz/matfactor-1.0.3/matfactor/_factorize.pyx
@@ -0,0 +1,191 @@
+#cython: boundscheck=False, wraparound=False, cdivision=True, initializedcheck=False
+
+from __future__ import print_function
+
+from cpython cimport bool
+
+from cython.parallel import prange
+
+import numpy as np
+cimport numpy as np
+
+from libc.math cimport sqrt, log, pow
+
+cdef packed struct Entry:
+    int row, col
+    double val
+
+cdef inline int max(int a, int b) nogil: return a if a > b else b
+
+def factorize(x,
+              int dim=40,
+              int epochs=5,
+              bool shuffle=True,
+              bool bias=False,
+              double learning_rate=0.05,
+              int x_max=100,
+              double alpha=0.75,
+              int threads=8,
+              bool verbose=True):
+    """
+    factorize(x, dim=40, epochs=5, learning_rate=0.05, x_max, alpha=0.75, threads=8)
+
+    Factorizes a sparse matrix `x` as `e ** (u * v)`
+
+    Solves the equation `x = e ** (u * v)` by minimizing
+    `sum_{i,j} f(X_ij)(u_i \dot v_j + b_i + b_j - log X_ij)^2`
+    where `f(x) = min((x / x_max) ** alpha, 1)`. This function
+    uses the Adagrad gradient descent algorithm.
+
+    This function expands on the algorithm used by Pennington
+    et Al in their GloVe paper. You can view their site at
+    `nlp.stanford.edu/projects/glove/` This function allows
+    an arbitrarily shaped sparse matrix as input instead of
+    only a square matrix.
+
+    Parameters
+    ----------
+    x : a scipy.sparse matrix
+        A real matrix of shape (M, N)
+    dim : int, optional
+        The number of columns of u and v
+    shuffle : bool, optional
+        Whether to shuffle the input matrix
+    bias : bool, optional
+        Whether to learn bias vectors
+    learning_rate : float, optional
+        The initial learning rate
+    epochs : int, optional
+        The number of epochs to run
+    x_max : int, optional
+        Each entry is weighted by min((x / x_max) ** alpha, 1)
+    alpha : double, optional
+        Each entry is weighted by min((x / x_max) ** alpha, 1)
+    threads : int, optional
+        The number of threads to use
+    verbose: bool, optional
+        Whether to print extra information
+
+    Returns
+    -------
+    u:(M, dim) array
+    v:(N, dim) array
+    ub: (M, dim) row bias. Only returned if `bias` is `True`
+    vb: (N, dim) col bias. Only returned if `bias` is `True`
+    """
+
+    cdef int n_rows, n_cols, epoch, i, j, bias_bit
+    cdef int[:] rows, cols
+    cdef double total_error, prediction, diff, fdiff, t1, t2
+    cdef double[:] vals, error
+    cdef double[:] row_bias, col_bias, rb_grad_sq, cb_grad_sq,
+    cdef double[:, :] row_vecs, col_vecs, row_grad_sq, col_grad_sq
+    cdef Entry elem
+    cdef Entry[:] coo_matrix
+
+    x = x.tocoo()
+    rows = x.row
+    cols = x.col
+    vals = x.data
+
+    coo_matrix = np.zeros(
+        vals.shape[0],
+        dtype=np.dtype([('row', np.int32), ('col', np.int32), ('val', np.float64)])
+    )
+
+    bias_bit = bias
+
+
+
+    if verbose:
+        print("Reshaping matrix")
+
+    for i in range(vals.shape[0]):
+        coo_matrix[i].row = rows[i]
+        coo_matrix[i].col = cols[i]
+        coo_matrix[i].val = vals[i]
+
+    if verbose:
+        print("Shuffling data")
+
+    if shuffle:
+        np.random.shuffle(coo_matrix)
+
+
+    n_rows = 0
+    n_cols = 0
+    for i in range(coo_matrix.shape[0]):
+        n_rows = max(n_rows, coo_matrix[i].row)
+        n_cols = max(n_cols, coo_matrix[i].col)
+
+    n_rows += 1
+    n_cols += 1
+
+    row_vecs = (np.random.rand(n_rows, dim) - 0.5) / dim
+    col_vecs = (np.random.rand(n_cols, dim) - 0.5) / dim
+
+    row_grad_sq = np.ones_like(row_vecs)
+    col_grad_sq = np.ones_like(col_vecs)
+
+    if bias_bit:
+        row_bias = np.zeros(n_rows)
+        col_bias = np.zeros(n_cols)
+        rb_grad_sq = np.zeros(n_rows)
+        cb_grad_sq = np.zeros(n_cols)
+
+    error = np.zeros(coo_matrix.shape[0])
+
+    for epoch in range(epochs):
+
+        if verbose:
+          print("Epoch %s: "%(epoch+1,), end='', flush=True)
+
+        for i in prange(coo_matrix.shape[0],
+                        num_threads=threads,
+                        nogil=True,
+                        chunksize=coo_matrix.shape[0] // threads + 1,
+                        schedule='static'):
+
+            elem = coo_matrix[i]
+
+            prediction = 0.0
+            for j in range(dim):
+                prediction = prediction + row_vecs[elem.row, j] * col_vecs[elem.col, j]
+
+            diff = prediction - log(elem.val)
+
+            if bias_bit:
+                diff = diff + row_bias[elem.row] + col_bias[elem.col]
+
+            fdiff = diff if elem.val > x_max else pow(elem.val / x_max, alpha) * diff
+
+            error[i] = 0.5 * fdiff * diff
+            fdiff = fdiff * learning_rate
+
+            for j in range(dim):
+                t1 = fdiff * row_vecs[elem.row, j]
+                t2 = fdiff * col_vecs[elem.col, j]
+                row_vecs[elem.row, j] -= t2 / sqrt(row_grad_sq[elem.row, j])
+                col_vecs[elem.col, j] -= t1 / sqrt(col_grad_sq[elem.col, j])
+
+                row_grad_sq[elem.row, j] += t2 * t2
+                col_grad_sq[elem.col, j] += t1 * t1
+
+            if bias_bit:
+                row_bias[elem.row] -= fdiff / sqrt(rb_grad_sq[elem.row])
+                col_bias[elem.col] -= fdiff / sqrt(cb_grad_sq[elem.col])
+                fdiff = fdiff * fdiff
+                cb_grad_sq[elem.row] += fdiff
+                rb_grad_sq[elem.col] += fdiff
+
+        total_error = 0.0
+        for i in range(coo_matrix.shape[0]):
+            total_error += error[i]
+        if verbose:
+          print("error %s"%(total_error / error.shape[0]))
+
+    if bias_bit:
+        return (np.asarray(row_vecs), np.asarray(col_vecs),
+                np.asarray(row_bias), np.asarray(col_bias))
+    else:
+        return np.asarray(row_vecs), np.asarray(col_vecs)
---
+++ packages/matfactor/matfactor-1.0.3.tar.gz/matfactor-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages, Extension
+
+try:
+    from Cython.Distutils import build_ext
+except:
+    print("You don't seem to have cython installed.")
+    print("Please install it by running sudo pip install cython")
+    import sys
+    sys.exit(1)
+
+setup(
+    name='matfactor',
+    version='1.0.3',
+    description='A small library for factorizing matrices',
+    long_description="A small library for factorizing matrices. Given a sparse matrix M, it finds matrices u,v that minimize M - e ** (u*v)",
+    author='Joshua Chin',
+    author_email='JoshuaRChin@gmail.com',
+    url='https://github.com/Joshua-Chin/matfactor/tree/master',
+    license='License :: OSI Approved :: Apache Software License',
+    install_requires=['cython'],
+    packages=find_packages(),
+    cmdclass={'build_ext':build_ext},
+    ext_modules=[
+        Extension(
+            'matfactor._factorize',
+            ['matfactor/_factorize.pyx'],
+            extra_compile_args=['-fopenmp'],
+            extra_link_args=['-fopenmp']
+        )
+    ]
+)
