4cce902ff0992af70723b76ea6e4eb390542813a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:11 +0000

    Add package sulley/0.0.1-alpha/sulley-0.0.1-alpha.tar.gz

---
+++ packages/sulley/sulley-0.0.1-alpha.tar.gz/sulley-0.0.1-alpha/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name = 'sulley',
+    packages = ['sulley'],
+version = '0.0.1-alpha',
+    description = 'With Sulley, you can build SMS bots in just a few lines of code. Powered by Twilio & Plivo, Sulley requires very minimal configuration and code to bring your bot to life!',
+    author = 'Sandeep Raju Prabhakar',
+    author_email = 'me@sandeepraju.in',
+    url = 'https://github.com/sandeepraju/sulley',
+    download_url = 'https://github.com/sandeepraju/sulley/archive/master.zip',
+    keywords = ['sms', 'message', 'twilio', 'plivo'],
+    classifiers = [
+        'Programming Language :: Python',
+    ],
+)
+
