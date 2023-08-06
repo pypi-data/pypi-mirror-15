788db82c2f7a70dda7450ff471af60826b1d67d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:25 +0000

    Add package cloudpayments/1.0/cloudpayments-1.0.zip

---
+++ packages/cloudpayments/cloudpayments-1.0.zip/cloudpayments-1.0/cloudpayments/errors.py
@@ -0,0 +1,12 @@
+class CloudPaymentsError(Exception):
+    def __init__(self, response, message=None):
+        self.response = response
+        super(CloudPaymentsError, self).__init__(message or
+                                                 response.get('Message'))
+
+
+class PaymentError(CloudPaymentsError):
+    def __init__(self, response):
+        self.reason = response['Model']['Reason']
+        self.reason_code = response['Model']['ReasonCode']
+        super(PaymentError, self).__init__(response, self.reason)
