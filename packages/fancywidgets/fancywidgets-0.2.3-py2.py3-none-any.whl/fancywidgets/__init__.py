8501cb0807a33f83c290346f4a5133ffd17c865a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:43 +0000

    Add package fancywidgets/0.2.3/fancywidgets-0.2.3-py2.py3-none-any.whl

---
+++ packages/fancywidgets/fancywidgets-0.2.3-py2.py3-none-any.whl/fancywidgets/__init__.py
@@ -0,0 +1,8 @@
+'''
+Various QT-based widgets for every day usage.
+'''
+__version__ = '0.2.3'
+__author__ = 'Karl Bedrich'
+__email__ = 'karl@bedrich.de'
+__url__ = 'https://github.com/radjkarl/fancyWidgets'
+__license__ = 'GPLv3'
