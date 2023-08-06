357fb426b344f0f51b4cfece438d70f96721a19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:57 +0000

    Add package syncano-cli/0.2/syncano-cli-0.2.tar.gz

---
+++ packages/syncano-cli/syncano-cli-0.2.tar.gz/syncano-cli-0.2/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import find_packages, setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+setup(
+    name='syncano-cli',
+    version='0.2',
+    description='Syncano command line utilities',
+    long_description=README,
+    author='Marcin Swiderski',
+    author_email='marcin.swiderski@syncano.com',
+    url='https://github.com/Syncano/syncano-cli',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=['syncano>=5.0', 'PyYaml>=3.11'],
+    test_suite='tests',
+    entry_points="""
+        [console_scripts]
+        syncano=syncano_cli.main:main
+    """
+)
