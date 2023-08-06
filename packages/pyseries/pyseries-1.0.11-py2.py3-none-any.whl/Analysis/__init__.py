5d886ba4920831ae5565cf21ff8207670c2881e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:14 +0000

    Add package pyseries/1.0.11/pyseries-1.0.11-py2.py3-none-any.whl

---
+++ packages/pyseries/pyseries-1.0.11-py2.py3-none-any.whl/Analysis/__init__.py
@@ -0,0 +1,11 @@
+"""Default init. Carry on.
+
+.. moduleauthor:: Ryszard Cetnarski <cetnarski.ryszard@gmail.com.com>
+
+"""
+
+import Anova
+import CrossCorrelate
+import example
+import Normalize
+import Trends
