0eeb3f3b7e644a5cbf2eeca4faf9489d91754ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:02 +0000

    Add package pkglts/1.0.0/pkglts-1.0.0.tar.gz

---
+++ packages/pkglts/pkglts-1.0.0.tar.gz/pkglts-1.0.0/src/pkglts_data/base/doc/AUTHORS.rst
@@ -0,0 +1,15 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+{% for name, email in base.authors %}
+* {{ name }}, <{{ email }}>
+{% endfor %}
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ packages/pkglts/pkglts-1.0.0.tar.gz/pkglts-1.0.0/test/test_option/test_pypi/test_handlers.py
@@ -0,0 +1,18 @@
+from pkglts.config_management import pkg_env
+
+
+def test_badge():
+    env = pkg_env(dict(base={'pkgname': "pkg", 'namespace': None,
+                             "url": "http://toto"},
+                       pysetup={"intended_versions": ["27", "28"]},
+                       pypi={'classifiers': []}))
+    assert ".. image:" in env.globals['pypi'].badge
+
+
+def test_auto_classifiers():
+    env = pkg_env(dict(base={'pkgname': "pkg", 'namespace': None,
+                             "url": "http://toto"},
+                       pysetup={"intended_versions": ["27", "28"]},
+                       pypi={'classifiers': []}))
+    assert "Programming Language :: Python" in env.globals['pypi'].auto_classifiers
+    assert "Programming Language :: Python :: 2.7" in env.globals['pypi'].auto_classifiers
