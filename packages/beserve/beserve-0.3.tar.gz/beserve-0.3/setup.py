e10a255bc52c14e8614a6bb6fd0b61d8220dc3a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:51 +0000

    Add package beserve/0.3/beserve-0.3.tar.gz

---
+++ packages/beserve/beserve-0.3.tar.gz/beserve-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='beserve',
+      version='0.3',
+      description='versatile server emulator',
+      url='https://github.com/perrin-ay/beserve',
+      author='perrin-ay',
+      author_email='arnabchat@hotmail.com',
+      license='',
+      packages=['beserve'],
+      install_requires=[''],
+      zip_safe=False)
+
