669a82cdc1185e1c3ec2dfab9a77acdb8c14c566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:32 +0000

    Add package li-repo/1.9.0b30/li-repo-1.9.0b30.tar.gz

---
+++ packages/li-repo/li-repo-1.9.0b30.tar.gz/li-repo-1.9.0b30/src/repositories/integration/migrations/0001_initial.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import repositories.catalogo.models
+import repositories.custom_models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+    ]
