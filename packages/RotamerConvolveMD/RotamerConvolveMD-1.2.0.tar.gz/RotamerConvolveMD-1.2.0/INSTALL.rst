3fc74c3883d2b55e1503ed0bb9806c9a6f2b6ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:51 +0000

    Add package RotamerConvolveMD/1.2.0/RotamerConvolveMD-1.2.0.tar.gz

---
+++ packages/RotamerConvolveMD/RotamerConvolveMD-1.2.0.tar.gz/RotamerConvolveMD-1.2.0/INSTALL.rst
@@ -0,0 +1,26 @@
+Installation instructions
+=========================
+
+From the sources::
+
+     python setup.py install --user
+
+From a tarball::
+
+     pip install --user rotamers-1.0.tar.gz
+
+
+Either form should automatically download required packages:
+
+* numpy_
+* MDAnalysis_
+
+.. Note:: The package requires at least MDAnalysis 0.11.0.
+
+Please see http://www.mdanalysis.org for hints if you have problems
+with the automatic installation of MDAnalysis_.
+
+See README.rst for further notes.
+
+.. _numpy: http://numpy.scipy.org/
+.. _MDAnalysis: http://www.mdanalysis.org
---
+++ packages/RotamerConvolveMD/RotamerConvolveMD-1.2.0.tar.gz/RotamerConvolveMD-1.2.0/setup.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    RELEASE = "1.2.0"
+    with open("README.rst") as summary:
+        LONG_DESCRIPTION = summary.read()
+    CLASSIFIERS = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+    setup(name='RotamerConvolveMD',
+          version=RELEASE,
+          description='Analysis of spin label distances over structural ensembles',
+          author='Philip W. Fowler',
+          author_email='philip.fowler@ndm.ox.ac.uk',
+          url='https://github.com/MDAnalysis/RotamerConvolveMD',
+          requires=['numpy (>=1.6)', 'MDAnalysis (>=0.11.0)'],
+          install_requires=['numpy>=1.6.0', 'MDAnalysis>=0.11.0'],
+          provides=['rotcon'],
+          license='GPL 2',
+          packages=find_packages(exclude=['scripts', 'rotcon/data']),
+          package_data={'rotcon': ['data/*.pdb', 'data/*.dcd', 'data/*.dat']},
+          scripts=['scripts/convolve-mtss-rotamers.py'],
+          classifiers=CLASSIFIERS,
+          long_description=LONG_DESCRIPTION,
+          zip_safe=True,
+          )
