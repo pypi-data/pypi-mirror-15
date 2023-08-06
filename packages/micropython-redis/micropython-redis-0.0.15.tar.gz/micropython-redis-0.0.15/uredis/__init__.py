efde7cdbe689b38a55cb39b5c240f895e09bf676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:56 +0000

    Add package micropython-redis/0.0.15/micropython-redis-0.0.15.tar.gz

---
+++ packages/micropython-redis/micropython-redis-0.0.15.tar.gz/micropython-redis-0.0.15/uredis/__init__.py
@@ -0,0 +1,12 @@
+# If we remove the following 2 lines we loose basic compatibility with the redis-py module
+# but allow for importing only the redis functionality we need which should use less
+# memory.
+from .uredis import Redis
+from .uredis import Redis as StrictRedis
+
+__version__ = '0.0.15'
+__copyright__ = "Copyright 2016 Dwight Hubbard"
+
+all = [
+    'client', 'connection', 'geo', 'key', 'list', 'pubsub', 'uredis'
+]
