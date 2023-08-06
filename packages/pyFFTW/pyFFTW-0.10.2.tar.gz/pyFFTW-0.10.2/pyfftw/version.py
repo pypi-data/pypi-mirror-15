76a448bc0e2aafa4fb9ad61b0243eb87a4320273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:45 +0000

    Add package pyFFTW/0.10.2/pyFFTW-0.10.2.tar.gz

---
+++ packages/pyFFTW/pyFFTW-0.10.2.tar.gz/pyFFTW-0.10.2/pyfftw/version.py
@@ -0,0 +1,13 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.10.1'
+version = '0.10.1'
+full_version = '0.10.1.dev0+7a80cb3'
+git_revision = '7a80cb3f6f5ed272be2a511e5ea7d61e49002403'
+release = False
+if not release:
+    version = full_version
+
+if __name__ == "__main__":
+    print(short_version)
+    print(version)
