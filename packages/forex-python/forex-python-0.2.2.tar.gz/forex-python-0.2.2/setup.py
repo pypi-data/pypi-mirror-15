f0274997e0a2abd3830f160e89e2557d7b588953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:15 +0000

    Add package forex-python/0.2.2/forex-python-0.2.2.tar.gz

---
+++ packages/forex-python/forex-python-0.2.2.tar.gz/forex-python-0.2.2/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = '0.2.2'
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    LONG_DESCRIPTION = readme.read()
+
+
+setup(
+    name='forex-python',
+    version=VERSION,
+    author='Micro Pyramid Informatic Pvt. Ltd.',
+    author_email='hello@micropyramid.com',
+    url='https://github.com/MicroPyramid/forex-python',
+    description='Foreign exchange rates and currency conversion.',
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Internationalization',
+    ],
+)
