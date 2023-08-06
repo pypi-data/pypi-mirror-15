5067567e81206a2920ea322377c279e17fe70669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:58 +0000

    Add package nepalicalendar/0.4/nepalicalendar-0.4.tar.gz

---
+++ packages/nepalicalendar/nepalicalendar-0.4.tar.gz/nepalicalendar-0.4/nepalicalendar/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .nepdate import NepDate
+from .values import *
+from .nepcal import NepCal
+from .tithis import *
+from .events import *
+
+__version__ = '0.4'
+VERSION = __version__
