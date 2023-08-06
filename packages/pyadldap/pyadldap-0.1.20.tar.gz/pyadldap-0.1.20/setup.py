3f5d89b94dcb1a40b4ea2183966da5e4a7fe988c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:32 +0000

    Add package pyadldap/0.1.20/pyadldap-0.1.20.tar.gz

---
+++ packages/pyadldap/pyadldap-0.1.20.tar.gz/pyadldap-0.1.20/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+requires = []
+
+try:
+	import ldap
+except:
+	requires.append('python-ldap')
+
+setup(
+	name="pyadldap",
+	version="0.1.20",
+	description="FrameWork for managing Microsoft Active Directory from LDAP",
+	author="Jonas Delgado Mesa",
+	author_email="jdelgado@yohnah.net",
+	url="https://github.com/Wengex/PyADLdap",
+	license="GPLv2",
+	packages=["pyadldap"],
+	long_description=open('README.txt').read(),
+	install_requires = requires
+)
