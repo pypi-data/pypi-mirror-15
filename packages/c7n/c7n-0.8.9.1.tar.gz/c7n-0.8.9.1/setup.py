a19ee7df0ce22b46b17e20b38d2ea4e60d9a03b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:23 +0000

    Add package c7n/0.8.9.1/c7n-0.8.9.1.tar.gz

---
+++ packages/c7n/c7n-0.8.9.1.tar.gz/c7n-0.8.9.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="c7n",
+    version='0.8.9.1',
+    description="Cloud Custodian - Policy Rules Engine",
+    long_description_markdown_filename='README.md',
+    classifiers=[
+      "Topic :: System :: Systems Administration",
+      "Topic :: System :: Distributed Computing"
+    ],
+    url="https://github.com/capitalone/cloud-custodian",
+    license="Apache-2.0",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'custodian = c7n.cli:main']},
+    install_requires=["boto3", "pyyaml", "jsonschema"],
+)
+
