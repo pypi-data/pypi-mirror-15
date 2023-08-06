1cd09d2ac82f09a103f88e6f898a20c5aca0d28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:32 +0000

    Add package taskcluster-s3-uploader/0.3.0/taskcluster-s3-uploader-0.3.0.tar.gz

---
+++ packages/taskcluster-s3-uploader/taskcluster-s3-uploader-0.3.0.tar.gz/taskcluster-s3-uploader-0.3.0/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+def read(fname):
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+required = [
+    'boto3',
+    'taskcluster'
+]
+
+setup(
+    name='taskcluster-s3-uploader',
+    version='0.3.0',
+    packages=find_packages(),
+    install_requires=required,
+    tests_require=required,
+    # Meta-data for upload to PyPI
+    author='Armen Zambrano G.',
+    author_email='armenzg@mozilla.com',
+    description='Upload files to S3 with your TaskCluster authentication',
+    long_description=read('README.md'),
+    license='MPL',
+    url='https://github.com/armenzg/treeherder_submitter',
+)
