85166618b0fbf5bd7bb36ff0976d59d316afe8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:48 +0000

    Add package Trivial-Sudoku/1.1/Trivial-Sudoku-1.1.tar.gz

---
+++ packages/Trivial-Sudoku/Trivial-Sudoku-1.1.tar.gz/Trivial-Sudoku-1.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) 2016 Alexandre Syenchuk (alexpirine)
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='Trivial-Sudoku',
+        version='1.1',
+        url='https://github.com/alexpirine/python-trivial-sudoku',
+        license='New BSD License',
+        author='Alexandre Syenchuk',
+        author_email='as@netica.fr',
+        description='A trvial Python implementation of a Sudoku solver',
+        py_modules=('sudoku',),
+        include_package_data=True,
+        zip_safe=False,
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Environment :: Other Environment',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Education',
+            'Topic :: Games/Entertainment :: Puzzle Games',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    )
