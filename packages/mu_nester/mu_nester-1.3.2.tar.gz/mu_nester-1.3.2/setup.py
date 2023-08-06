a7412a3dfd8af2b25d94a870b89191226121629c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:17 +0000

    Add package mu_nester/1.3.2/mu_nester-1.3.2.tar.gz

---
+++ packages/mu_nester/mu_nester-1.3.2.tar.gz/mu_nester-1.3.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'mu_nester',
+	version		= '1.3.2',
+	py_modules	= ['mu_nester'],
+	author		= 'muyangzi',
+	author_email	= 'muyangzi521@163.com',
+	url			= '',
+	description	= 'A simple printer of nested lists',
+	)
