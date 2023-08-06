270a4d4ae89e29d7cb8aa80a6ce1e50324af87fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:20 +0000

    Add package rollme/0.0.7/rollme-0.0.7.tar.gz

---
+++ packages/rollme/rollme-0.0.7.tar.gz/rollme-0.0.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='rollme',
+    version='0.0.7',
+    packages=['rollme'],
+    author='Roy Keyes',
+    author_email='keyes.roy@gmail.com',
+    url='https://github.com/psyclone241/rollme',
+    description='Python script to roll the dice',
+    install_requires=[
+        'inflect'
+    ]
+)
