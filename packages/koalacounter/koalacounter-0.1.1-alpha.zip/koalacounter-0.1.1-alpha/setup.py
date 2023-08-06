7d286590ae16e2a76ccfe71d7461e9a548a8e370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:25 +0000

    Add package koalacounter/0.1.1-alpha/koalacounter-0.1.1-alpha.zip

---
+++ packages/koalacounter/koalacounter-0.1.1-alpha.zip/koalacounter-0.1.1-alpha/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='koalacounter',
+    packages=['koalacounter'],  # this must be the same as the name above
+    version='0.1.1-alpha',
+    description='',
+    author='Matt Badger',
+    author_email='foss@lighthouseuk.net',
+    url='https://github.com/LighthouseUK/koalacounter',  # use the URL to the github repo
+    download_url='https://github.com/LighthouseUK/koalacounter/tarball/0.1.1-alpha',  # I'll explain this in a second
+    keywords=['gae', 'lighthouse', 'koala'],  # arbitrary keywords
+    classifiers=[],
+    requires=[]
+)
