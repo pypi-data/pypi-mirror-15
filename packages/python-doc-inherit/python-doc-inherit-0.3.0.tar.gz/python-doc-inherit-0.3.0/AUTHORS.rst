7c967d2b1decb4e1c05c757d5f3ec3a2a65fb21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:48 +0000

    Add package python-doc-inherit/0.3.0/python-doc-inherit-0.3.0.tar.gz

---
+++ packages/python-doc-inherit/python-doc-inherit-0.3.0.tar.gz/python-doc-inherit-0.3.0/AUTHORS.rst
@@ -0,0 +1,14 @@
+Authors
+=======
+
+Development Lead
+----------------
+
+* Alexander Kavanaugh (@kavdev)
+
+Content Authors
+---------------
+
+* Shai Berger
+* unutbu
+* Martijn Pieters
---
+++ packages/python-doc-inherit/python-doc-inherit-0.3.0.tar.gz/python-doc-inherit-0.3.0/doc_inherit/__init__.py
@@ -0,0 +1,15 @@
+from .decorators import DocInherit as method_doc_inherit  # noqa
+from .decorators import class_inherit_docstrings as class_doc_inherit  # noqa
+
+__title__ = "python-doc-inherit"
+__summary__ = "Utilities to inherit method documentation from parent classes."
+__uri__ = "https://github.com/kavdev/python-doc-inherit/"
+
+__version__ = "0.3.0"
+
+__author__ = "Alexander Kavanaugh"
+__email__ = "alex@kavdev.io"
+
+__license__ = "MIT"
+__license__ = "License :: OSI Approved :: MIT License"
+__copyright__ = "Copyright (c) 2016 Alexander Kavanaugh"
