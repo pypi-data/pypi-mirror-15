d87032bb427c96e2a9e277c491dd9b8394f5d42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:32 +0000

    Add package HashTable/0.1/HashTable-0.1.tar.gz

---
+++ packages/HashTable/HashTable-0.1.tar.gz/HashTable-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'HashTable',
+    version = '0.1',
+    url = 'https://github.com/munendrasn/hash',
+    author = 'S N Munendra',
+    author_email = 'sn.munendra52@gmail.com',
+    description = 'Simple HashTable implementation in Python',
+    packages = ['hash'] ,
+    zip_safe=False
+
+)
