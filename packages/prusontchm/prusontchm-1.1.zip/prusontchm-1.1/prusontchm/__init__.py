ce885668240eb19abd970dac1715c22c43e9bc11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:51 +0000

    Add package prusontchm/1.1/prusontchm-1.1.zip

---
+++ packages/prusontchm/prusontchm-1.1.zip/prusontchm-1.1/prusontchm/__init__.py
@@ -0,0 +1,3 @@
+# this is a package
+__version__ = '1.1'
+__description__ = 'Practica 3'
---
+++ packages/prusontchm/prusontchm-1.1.zip/prusontchm-1.1/prusontchm/ratios.py
@@ -0,0 +1,7 @@
+def div0( a, b ):
+    import numpy as np
+    """ ignore / 0, div0( [-1, 0, 1], 0 ) -> [0, 0, 0] """
+    with np.errstate(divide='ignore', invalid='ignore'):
+        c = np.true_divide( a, b )
+        c[ ~ np.isfinite( c )] = a.max()  # -inf inf NaN
+    return c
