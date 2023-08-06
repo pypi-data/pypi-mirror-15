0e307fdbf70a0f6ec701dd65e3b3810fc737ab9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:19 +0000

    Add package redicrypt/0.0.2/redicrypt-0.0.2.zip

---
+++ packages/redicrypt/redicrypt-0.0.2.zip/redicrypt-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="redicrypt",
+    version="0.0.2",
+    author="Chris Dutra",
+    author_email="cdutra@apprenda.com",
+    description="Python-based cryptography package for redis.",
+    license="MIT",
+    keywords="redis data cryptography",
+    url="http://github.com/dutronlabs/redicrypt",
+    packages=['redicrypt'],
+    install_requires=['pycrypto'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
