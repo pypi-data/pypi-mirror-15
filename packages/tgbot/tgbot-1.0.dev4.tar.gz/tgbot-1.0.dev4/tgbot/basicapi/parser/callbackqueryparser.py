6598be90b88413610bc4d73cb3f5a9c698941758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:52 +0000

    Add package tgbot/1.0.dev4/tgbot-1.0.dev4.tar.gz

---
+++ packages/tgbot/tgbot-1.0.dev4.tar.gz/tgbot-1.0.dev4/tgbot/basicapi/parser/callbackqueryparser.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Thomas'
+
+import ast
+from tgbot.tglogging import logger
+from tgbot.basicapi.parser.methodparser import parsemethods
+
+
+def parsecallbackquery(callbackquery,args):
+    logger.info(str(callbackquery))
+    data = ast.literal_eval(callbackquery.data)
+    method = data["method"]
+    parsemethods(callbackquery,method,args)
