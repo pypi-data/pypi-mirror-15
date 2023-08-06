32706d989eebabd94fa3d67996f630ec65dcd618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:36 +0000

    Add package ezhost/1.2.1.2/ezhost-1.2.1.2.tar.gz

---
+++ packages/ezhost/ezhost-1.2.1.2.tar.gz/ezhost-1.2.1.2/ezhost/main.py
@@ -0,0 +1,12 @@
+"""
+This module contains `main` method plus related subroutines.
+`main` is executed as the command line ``ezhost`` program and takes care of
+parsing options and commands
+
+The other callables defined in this module are internal only. Anything useful
+to individuals leveraging Fabric as a library, should be kept elsewhere.
+"""
+
+def main():
+    command = 'sudo touch /usr/local/bin/ezhost'
+    os.system(command)
