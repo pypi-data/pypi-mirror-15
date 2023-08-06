fe8b974f4a4f832a5ac86b6e0b3509501f500fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:22 +0000

    Add package esp/0.1.1/esp-0.1.1.tar.gz

---
+++ packages/esp/esp-0.1.1.tar.gz/esp-0.1.1/tests/test_auth.py
@@ -0,0 +1,25 @@
+import mock
+import unittest
+from esp.auth import ESPAuth
+
+
+class MockRequest(object):
+
+    def __init__(self, url, body):
+        self.url = url
+        self.body = body
+        self.headers = {}
+
+
+class TestAuth(unittest.TestCase):
+
+    def test_apiauth_can_sign_request(self):
+        request = MockRequest('http://example.com/api/v2/external_accounts',
+                              '{"data":{"attributes":{"name":"Testing"}}}')
+        ESPAuth._request_date = mock.MagicMock(
+            return_value='Mon, 21 October 2015 04:20:01 GMT')
+        auth = ESPAuth('abc', 'abc123')
+        request = auth(request)
+
+        self.assertEqual(request.headers['Authorization'],
+                         'APIAuth abc:hdhT6Ufm5jACjdsoJhdaO7JnEmQ=')
