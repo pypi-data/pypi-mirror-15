5fce626a3bffabe5d465f616a46c82734a64fcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:57 +0000

    Add package cvxpy/0.4.1/cvxpy-0.4.1.tar.gz

---
+++ packages/cvxpy/cvxpy-0.4.1.tar.gz/cvxpy-0.4.1/cvxpy/expressions/cvxtypes.py
@@ -0,0 +1,67 @@
+"""
+Copyright 2013 Steven Diamond
+
+This file is part of CVXPY.
+
+CVXPY is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CVXPY is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CVXPY.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# Utility functions to solve circular imports.
+def add_expr():
+    from cvxpy.atoms.affine import add_expr
+    return add_expr.AddExpression
+
+def constant():
+    from cvxpy.expressions import constants
+    return constants.Constant
+
+def index():
+    from cvxpy.atoms.affine import index
+    return index.index
+
+def mul_expr():
+    from cvxpy.atoms.affine import binary_operators
+    return binary_operators.MulExpression
+
+def rmul_expr():
+    from cvxpy.atoms.affine import binary_operators
+    return binary_operators.RMulExpression
+
+def affine_prod_expr():
+    from cvxpy.atoms import affine_prod
+    return affine_prod
+
+def div_expr():
+    from cvxpy.atoms.affine import binary_operators
+    return binary_operators.DivExpression
+
+def neg_expr():
+    from cvxpy.atoms.affine import unary_operators
+    return unary_operators.NegExpression
+
+def power():
+    from cvxpy.atoms.elementwise import power
+    return power.power
+
+def reshape():
+    from cvxpy.atoms.affine import reshape
+    return reshape.reshape
+
+def transpose():
+    from cvxpy.atoms.affine import transpose
+    return transpose.transpose
+
+def vec():
+    from cvxpy.atoms.affine import vec
+    return vec.vec
