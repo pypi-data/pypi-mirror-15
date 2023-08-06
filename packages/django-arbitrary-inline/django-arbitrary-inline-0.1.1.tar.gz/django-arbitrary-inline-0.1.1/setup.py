8941ca57c3b6b348928d3581e2a89c3a592fd775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:32 +0000

    Add package django-arbitrary-inline/0.1.1/django-arbitrary-inline-0.1.1.tar.gz

---
+++ packages/django-arbitrary-inline/django-arbitrary-inline-0.1.1.tar.gz/django-arbitrary-inline-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+__doc__ = """
+Django Admin compatible ModelInline that can be related
+to the primary model on fields other than a Primary Key.
+"""
+
+
+setup(
+    name='django-arbitrary-inline',
+    version='0.1.1',
+    author='James Robert',
+    author_email='james@mediapredict.com',
+    description='Django Admin compatible ModelInline that can be related '
+                'to the primary model on fields other than a Primary Key',
+    license='BSD',
+    keywords='django django-admin',
+    url='https://github.com/mediapredict/django_arbitrary_inline',
+    packages=['arbitrary_inline'],
+    long_description=__doc__,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Environment :: Web Environment',
+    ]
+)
