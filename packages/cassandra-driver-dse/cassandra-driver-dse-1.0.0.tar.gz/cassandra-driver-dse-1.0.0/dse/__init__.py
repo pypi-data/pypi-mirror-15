1e4b098caa16c5220dccf21975d8db95738f8115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:02 +0000

    Add package cassandra-driver-dse/1.0.0/cassandra-driver-dse-1.0.0.tar.gz

---
+++ packages/cassandra-driver-dse/cassandra-driver-dse-1.0.0.tar.gz/cassandra-driver-dse-1.0.0/dse/__init__.py
@@ -0,0 +1,21 @@
+# Copyright 2016 DataStax, Inc.
+#
+# Licensed under the DataStax DSE Driver License;
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+# http://www.datastax.com/terms/datastax-dse-driver-license-terms
+
+import logging
+import os
+
+from cassandra import NullHandler
+
+logging.getLogger('dse').addHandler(NullHandler())
+
+__version_info__ = (1, 0, 0)
+__version__ = '.'.join(map(str, __version_info__))
+
+_core_driver_target_version = '3.5.1'
+_use_any_core_driver_version = bool(os.environ.get('DSE_DRIVER_PERMIT_UNSUPPORTED_CORE'))
