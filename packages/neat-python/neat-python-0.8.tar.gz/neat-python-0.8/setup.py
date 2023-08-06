7a1638d0119bf4d688eaa20bffb2f7a1c1bbe149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:35 +0000

    Add package neat-python/0.8/neat-python-0.8.tar.gz

---
+++ packages/neat-python/neat-python-0.8.tar.gz/neat-python-0.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='neat-python',
+    version='0.8',
+    author='cesar.gomes, mirrorballu2',
+    author_email='nobody@nowhere.com',
+    maintainer='CodeReclaimers, LLC',
+    maintainer_email='alan@codereclaimers.com',
+    url='https://github.com/CodeReclaimers/neat-python',
+    license="BSD",
+    description='A NEAT (NeuroEvolution of Augmenting Topologies) implementation',
+    long_description='Python implementation of NEAT (NeuroEvolution of Augmenting Topologies), a method ' +
+                     'developed by Kenneth O. Stanley for evolving arbitrary neural networks.',
+    packages=['neat', 'neat/iznn', 'neat/nn', 'neat/ctrnn'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Scientific/Engineering'
+    ]
+)
