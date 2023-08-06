96289e2b44165df78657c6f210bbc6035e3631a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:33 +0000

    Add package jimit/0.56/jimit-0.56.tar.gz

---
+++ packages/jimit/jimit-0.56.tar.gz/jimit-0.56/jimit/router.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'James Iter'
+__date__ = '15/5/16'
+__contact__ = 'james.iter.cn@gmail.com'
+__copyright__ = '(c) 2015 by James Iter.'
+
+from jimit import Common
+
+router_table = {}
+
+
+class Router(object):
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def not_found(content):
+        return Common.exchange_state(50100)
+
+    @staticmethod
+    def launcher(**kwargs):
+        action = kwargs.get('action', None)
+        content = kwargs.get('content', None)
+        return router_table.get(action, Router.not_found)(content)
