b38f7e71e078233952a8437228db6a11d85a3da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:07 +0000

    Add package pydaikin/0.4/pydaikin-0.4.tar.gz

---
+++ packages/pydaikin/pydaikin-0.4.tar.gz/pydaikin-0.4/pydaikin/entity.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+    entity
+    ~~~~~~~~~~~~
+
+    Models the base class for objects representing a Daikin HVAC appliance
+
+    :copyright: (c) 2015 by Yari Adan.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import urllib
+
+class Entity:
+    def __init__(self):
+        self.values = {}
+
+    def __getitem__(self, name):
+        if name in self.values:
+            return self.values[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def parse_response(self, body):
+        d = dict([ e.split('=') for e in body.split(',') ])
+
+        if not 'ret' in d or d['ret'] != 'OK':
+            raise ValueError("non-OK return on response")
+
+        if 'name' in d:
+            d['name'] = urllib.parse.unquote(d['name'])
+
+        return d
+
