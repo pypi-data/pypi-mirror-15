3852c3903e9255c573bb12f8f0ac3353ee9ea85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:55 +0000

    Add package django-cradmin/1.0.1b72/django_cradmin-1.0.1b72.tar.gz

---
+++ packages/django-cradmin/django_cradmin-1.0.1b72.tar.gz/django_cradmin-1.0.1b72/django_cradmin/apps/cradmin_register_account/urls.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+from django.conf.urls import url, patterns
+
+from django_cradmin.apps.cradmin_register_account.views.begin import BeginRegisterAccountView
+from django_cradmin.apps.cradmin_register_account.views.email_sent import EmailSentView
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^begin',
+        BeginRegisterAccountView.as_view(),
+        name="cradmin-register-account-begin"),
+    url(r'^email-sent',
+        EmailSentView.as_view(),
+        name="cradmin-register-account-email-sent"),
+)
