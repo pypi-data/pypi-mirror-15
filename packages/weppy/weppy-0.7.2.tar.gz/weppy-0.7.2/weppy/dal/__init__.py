2d899ca786ee05318c1dc7743a7bc53d2e039276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:18 +0000

    Add package weppy/0.7.2/weppy-0.7.2.tar.gz

---
+++ packages/weppy/weppy-0.7.2.tar.gz/weppy-0.7.2/weppy/dal/__init__.py
@@ -0,0 +1,5 @@
+from .base import DAL, Field
+from .models import Model
+from .apis import belongs_to, refers_to, has_one, has_many, computation, \
+    rowattr, rowmethod, before_insert, before_update, before_delete, \
+    after_insert, after_update, after_delete, scope, virtualfield, fieldmethod
