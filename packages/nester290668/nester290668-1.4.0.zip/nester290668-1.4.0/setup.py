ae2c4a1c19b7547fa6a5b4ea5f7426b20b182764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:07 +0000

    Add package nester290668/1.4.0/nester290668-1.4.0.zip

---
+++ packages/nester290668/nester290668-1.4.0.zip/nester290668-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester290668',
+	version = '1.4.0',
+	py_modules = ['nester290668'],
+	author = 'mgatica',
+	author_email = 'mag290668@gmail.com',
+	url = 'https://mag290668.com',
+	description = 'A simple printer of nested lists',
+	)
