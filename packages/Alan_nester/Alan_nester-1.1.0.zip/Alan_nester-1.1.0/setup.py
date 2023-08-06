3596347f015600deb35d8a06c87e0bc20eb6fba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:22 +0000

    Add package Alan_nester/1.1.0/Alan_nester-1.1.0.zip

---
+++ packages/Alan_nester/Alan_nester-1.1.0.zip/Alan_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'Alan_nester',
+    version      = '1.1.0',
+    py_modules   = ['nester'],
+    author       = 'Alan_Stallman',
+    author_email = 'arvin.lanxiaocheng@gmail.com',
+    url          = 'http://www.headfirstlabs.com',
+    description  = 'A simple printer of nested lists',
+)
