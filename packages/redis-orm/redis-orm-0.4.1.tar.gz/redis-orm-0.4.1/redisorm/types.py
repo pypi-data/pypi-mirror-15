7d5f1e5bd4bd393d4e7e31005e444be102392f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:43 +0000

    Add package redis-orm/0.4.1/redis-orm-0.4.1.tar.gz

---
+++ packages/redis-orm/redis-orm-0.4.1.tar.gz/redis-orm-0.4.1/redisorm/types.py
@@ -0,0 +1,58 @@
+class RedisType:
+    __orderable__ = False
+
+    def __init__(self, obj):
+        if not isinstance(obj, type(self)) and obj is not None:
+            obj = self.parse(obj)
+        self.obj = obj
+
+    @classmethod
+    def parse(cls, string):
+        return string
+
+    def freeze(self):
+        return self.obj
+
+    def __str__(self):
+        return self.freeze()
+
+
+class String(RedisType):
+    pass
+
+
+class Integer(RedisType):
+    __orderable__ = True
+
+    @classmethod
+    def parse(cls, string):
+        return int(string)
+
+    def freeze(self):
+        return str(self.obj)
+
+
+class DateTime(RedisType):
+    __orderable__ = True
+
+    @classmethod
+    def parse(cls, string):
+        import dateutil.parser
+        import datetime
+        if isinstance(string, datetime.datetime):
+            return string
+        return dateutil.parser.parse(string)
+
+    def freeze(self):
+        return self.obj.strftime("%Y-%m-%d %H:%M:%S")
+
+
+class Boolean(RedisType):
+    @classmethod
+    def parse(cls, string):
+        if isinstance(string, bool):
+            return string
+        return "True" == string
+
+    def freeze(self):
+        return str(self.obj)
