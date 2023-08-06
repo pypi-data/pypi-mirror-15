7b7a7822fdf91259a1361e945ec3aef4ab41547f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:27 +0000

    Add package PandasSelector/0.1/PandasSelector-0.1.tar.gz

---
+++ packages/PandasSelector/PandasSelector-0.1.tar.gz/PandasSelector-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PandasSelector',
+    version='0.1',
+    packages=['pandas_selector',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
