fb710751e74981544547dfc62f2a897942e379c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:28 +0000

    Add package Algos/0.0.1/Algos-0.0.1.tar.gz

---
+++ packages/Algos/Algos-0.0.1.tar.gz/Algos-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name         = 'Algos',
+    version      = '0.0.1',
+    py_modules   = ['algos'],
+    author       = 'Chitrank Dixit',
+    author_email = 'chitrankdixit@gmail.com',
+    url          = 'http://chitrank-dixit.github.io',
+    description  = 'A simple library that consists of all basic operations and complex algorithm present on the globe' 
+
+)
