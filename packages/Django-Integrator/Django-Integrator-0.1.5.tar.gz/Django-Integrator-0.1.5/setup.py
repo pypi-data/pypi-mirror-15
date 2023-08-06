3ce9e75da916c32f0f8fb3ae71ff005d01ca3ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:28 +0000

    Add package Django-Integrator/0.1.5/Django-Integrator-0.1.5.tar.gz

---
+++ packages/Django-Integrator/Django-Integrator-0.1.5.tar.gz/Django-Integrator-0.1.5/setup.py
@@ -0,0 +1,27 @@
+"""
+PyPi Setup file.
+"""
+# pylint: disable=no-name-in-module, import-error
+from distutils.core import setup
+VERSION = '0.1.5'
+BASE_URL = 'https://bitbucket.org/hellwig/django-integrator'
+
+setup(
+  name = 'Django-Integrator',
+  packages = ['django_integrator'],
+  version = VERSION,
+  description = 'Tool for integrating compliant 3rd party django apps',
+  author = 'Martin P. Hellwig',
+  author_email = 'martin.hellwig@gmail.com',
+  url = BASE_URL,
+  download_url = BASE_URL + '/get/' + VERSION + '.zip',
+  keywords = ['django'],
+  license = 'BSD',
+  classifiers = ['Programming Language :: Python :: 3',],
+  install_requires = ['django'],
+   entry_points = {
+        'console_scripts':\
+   ['mk_django_integrator_app=script.mk_django_integrator_app:main'],
+    }
+)
+
