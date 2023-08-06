55ca4af08114f5a8686be3eddd30bb8c0f9e5feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:08 +0000

    Add package config-reader/0.3/config-reader-0.3.tar.gz

---
+++ packages/config-reader/config-reader-0.3.tar.gz/config-reader-0.3/config_reader/exceptions.py
@@ -0,0 +1,18 @@
+class ConfigTypeError(TypeError):
+    def __init__(self, key, value, expected):
+        fmt_dict = {"key": key, "type": type(value), "value": value, "expected": expected}
+        msg = "Invalid type {type} of value {value} for configuration key {key}, expected {expected}".format(**fmt_dict)
+        super(ConfigTypeError, self).__init__(msg)
+
+
+class ConfigTypeCastError(TypeError):
+    def __init__(self, key, value, expected):
+        fmt_dict = {"key": key, "value": value, "expected": expected}
+        msg = "Cannot cast value {value} of configuration key {key} to {expected}".format(**fmt_dict)
+        super(ConfigTypeCastError, self).__init__(msg)
+
+
+class ConfigKeyNotFoundError(KeyError):
+    def __init__(self, key):
+        msg = "Configuration key {key} was not found.".format(key=key)
+        super(ConfigKeyNotFoundError, self).__init__(msg)
