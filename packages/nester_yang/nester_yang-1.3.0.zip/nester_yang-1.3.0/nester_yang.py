71983f0dad9f74d96cc838995d3702b3bc86bf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:19 +0000

    Add package nester_yang/1.3.0/nester_yang-1.3.0.zip

---
+++ packages/nester_yang/nester_yang-1.3.0.zip/nester_yang-1.3.0/nester_yang.py
@@ -0,0 +1,25 @@
+"""
+movies=["The Holy Grail",1975,"Terry Jones & Terry Gilliam",91,
+        ["Graham Chapman",["Michael Palin","Jhon Cleese",
+        "Terry Gilliam","Eric Idle","Terry Jones"]]]
+
+for item in movies:
+    if isinstance(item,list):
+        for item1 in item:
+            if isinstance(item1,list):
+                for item2 in item1:vb
+                    print (item2)
+            else:
+                print (item1)
+    else:
+        print (item)
+"""
+def print_lol(the_list,indent=False,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print (each_item)
