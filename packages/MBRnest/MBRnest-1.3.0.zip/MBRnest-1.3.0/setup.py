cae6b94c56d6849f7db6978f5d8cb6cb4064c22f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:41 +0000

    Add package MBRnest/1.3.0/MBRnest-1.3.0.zip

---
+++ packages/MBRnest/MBRnest-1.3.0.zip/MBRnest-1.3.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+        name = 'MBRnest',
+        version = '1.3.0',
+        py_modules = ['MBRnest'],
+        author = 'hfpython',
+        author_email = 'hfpython@headfirstlabes.com',
+        url = 'http://www.muitobr.com',
+        description = 'A simple printer of nested lists with tab level',
+)
+
+			
+ 
+
+
