4782833d9b3cd7bfde719f93b22973a0eb4fc1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:28 +0000

    Add package bagel/0.3.1/bagel-0.3.1.tar.gz

---
+++ packages/bagel/bagel-0.3.1.tar.gz/bagel-0.3.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='bagel',
+    packages=['bagel'],
+    version='0.3.1',
+    description='Bagel is a naive static website generator.',
+    author='Adam Schwartz',
+    author_email='adam@anschwa.com',
+    url='https://github.com/anschwa/bagel',
+    download_url='https://github.com/anschwa/bagel/tarball/0.3.1',
+    entry_points={
+        'console_scripts': [
+            'bagel=bagel.bagel:main',
+        ]
+    },
+    keywords=['static website generator', 'bagel'],
+    classifiers=[],
+)
