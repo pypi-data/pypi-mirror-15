49241e46505ab37333d9de90547b6fdb7c6023ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:47 +0000

    Add package python-opensesame/3.1.0a44/python-opensesame-3.1.0a44.tar.gz

---
+++ packages/python-opensesame/python-opensesame-3.1.0a44.tar.gz/python-opensesame-3.1.0a44/opensesame_extensions/get_started/get_started.py
@@ -0,0 +1,121 @@
+#-*- coding:utf-8 -*-
+
+"""
+This file is part of OpenSesame.
+
+OpenSesame is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenSesame is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from libopensesame.py3compat import *
+from libqtopensesame.misc.config import cfg
+import os
+import sys
+from libopensesame import misc, metadata
+from libqtopensesame.extensions import base_extension
+from libqtopensesame.misc.translate import translation_context
+from libqtopensesame.misc import template_info
+_ = translation_context(u'get_started', category=u'extension')
+
+class get_started(base_extension):
+
+	"""
+	desc:
+		Shows the get-started tab and opens an experiment on startup, if one was
+		passed on the command line.
+	"""
+
+	def activate(self):
+
+		"""
+		desc:
+			Is called when the extension is activated through the menu/ toolbar
+			action.
+		"""
+
+		# Initialize templates
+		templates = []
+		for i, (path, desc) in enumerate(template_info.templates):
+			try:
+				path = self.experiment.resource(path)
+			except:
+				continue
+			if not i:
+				cls = u'important-button'
+			else:
+				cls = u'button'
+			path = os.path.abspath(path)
+			md = u'<a href="opensesame://%s" class="%s">%s</a><br />' \
+				% (path, cls, desc)
+			templates.append(md)
+		# Initialize recent experiments
+		if not self.main_window.recent_files:
+			recent = []
+		else:
+			recent = [_(u'Continue with a recent experiment:')+u'<br />']
+			for i, path in enumerate(self.main_window.recent_files):
+				if not i:
+					cls = u'important-button'
+				else:
+					cls = u'button'
+				md = u'<a href="opensesame://event.open_recent_%d" class="%s">%s</a><br />' % \
+					(i, cls, os.path.basename(path))
+				recent.append(md)
+		# Create markdown
+		with open(self.ext_resource(u'get_started.md')) as fd:
+			md = safe_decode(fd.read())
+		md = md % {
+			u'version' : metadata.__version__,
+			u'codename' : metadata.codename,
+			u'templates' : u'  \n'.join(templates),
+			u'recent_experiments' : u'  \n'.join(recent)
+			}
+		if cfg.enable_ping:
+			with open(self.ext_resource(u'tracking-wrapper.html')) as fd:
+				tmpl = safe_decode(fd.read())
+			if cfg.enable_ping_info:
+				self.extension_manager.fire(u'notify',
+					message=_('Anonymous usage data is sent to cogsci.nl. You can disable this under preferences.'),
+					category=u'info', timeout=0, buttontext=_(u'Got it!'))
+				cfg.enable_ping_info = 0
+		else:
+			tmpl = None
+		self.tabwidget.open_markdown(md, title=_(u'Get started!'),
+			icon=u'document-new', url='get-started', tmpl=tmpl)
+
+	def event_open_recent_0(self):
+		self.main_window.open_file(path=self.main_window.recent_files[0])
+	def event_open_recent_1(self):
+		self.main_window.open_file(path=self.main_window.recent_files[1])
+	def event_open_recent_2(self):
+		self.main_window.open_file(path=self.main_window.recent_files[2])
+	def event_open_recent_3(self):
+		self.main_window.open_file(path=self.main_window.recent_files[3])
+	def event_open_recent_4(self):
+		self.main_window.open_file(path=self.main_window.recent_files[4])
+
+	def event_startup(self):
+
+		"""
+		desc:
+			Called on startup.
+		"""
+
+		# Open an experiment if it has been specified as a command line argument
+		# and suppress the new wizard in that case.
+		if len(sys.argv) >= 2 and os.path.isfile(sys.argv[1]):
+			path = safe_decode(sys.argv[1], enc=misc.filesystem_encoding(),
+				errors=u'ignore')
+			self.main_window.open_file(path=path)
+			return
+		self.activate()
---
+++ packages/python-opensesame/python-opensesame-3.1.0a44.tar.gz/python-opensesame-3.1.0a44/opensesame_extensions/get_started/locale/fr_FR/get_started.md
@@ -0,0 +1,32 @@
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-26205412-1', 'auto');
+	ga('send', 'pageview');
+</script>
+
+# Lancez-vous !
+
+Bienvenue chez OpenSesame ! Que puis-je pour vous ?
+
+<table><tr><td>
+
+Commencer avec une nouvelle expérience :<br />
+%(templates)s
+
+</td><td>
+
+%(recent_experiments)s
+
+</td></tr></table>
+
+Avez-vous pensé à faire un don à OpenSesame ? C'est facile et rapide.
+
+[Faire un don par PayPal](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QEWKSAY4WMKRW){: .important-button}
+
+Ou en savoir plus :
+
+[Lire la documentation](http://osdoc.cogsci.nl){: .button}<br />
+[Poser une question sur le forum](http://forum.cogsci.nl){: .button}<br />
