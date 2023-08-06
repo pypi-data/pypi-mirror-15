84ffbf48240008d8bf7d1b421902b380cb9713c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:24 +0000

    Add package pypiserver/1.2.0b1/pypiserver-1.2.0b1.zip

---
+++ packages/pypiserver/pypiserver-1.2.0b1.zip/pypiserver-1.2.0b1/bin/test_standalone.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+## Test standalone generation & execution.
+##
+
+my_dir="$(dirname "$0")"
+cd $my_dir/..
+
+git fetch origin standalone:origin/standalone
+git branch --track standalone origin/standalone
+./bin/commit-standalone.sh no_commit
+
+./pypi-server-standalone.py . &
+server_pid=$!
+sleep 2
+
+kill $server_pid  && echo "Server killed nicely." # Kill fails if server failed.
