aee82d10b41fa824f5b27ab9b2e4d7efae0346c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:45 +0000

    Add package Flask-PAM/0.1rc1/Flask-PAM-0.1rc1.tar.gz

---
+++ packages/Flask-PAM/Flask-PAM-0.1rc1.tar.gz/Flask-PAM-0.1rc1/flask_pam/__init__.py
@@ -0,0 +1,3 @@
+from auth import Auth  # pylint: disable=unused-import
+import token_storage  # pylint: disable=unused-import
+import token  # pylint: disable=unused-import
---
+++ packages/Flask-PAM/Flask-PAM-0.1rc1.tar.gz/Flask-PAM-0.1rc1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from datetime import datetime
+
+setup(name='Flask-PAM',
+      version='0.1rc1',
+      description='Flask authentication using PAM',
+      author='Kacper Kołodziej',
+      author_email='kacper@kolodziej.in',
+      url='https://github.com/KujiraProject/Flask-PAM',
+      packages=['flask_pam', 'flask_pam.token', 'flask_pam.token_storage',],
+      install_requires=[
+        'simplepam>=0.1.5',
+        'Flask>=0.10.1',
+        'python-jose>=0.6.1',
+      ],    
+)
