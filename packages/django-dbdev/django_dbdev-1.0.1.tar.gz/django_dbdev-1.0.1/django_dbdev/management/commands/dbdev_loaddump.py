9862f2248f1c5a2d006a3037999420aaf5a2fcbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:44 +0000

    Add package django-dbdev/1.0.1/django_dbdev-1.0.1.tar.gz

---
+++ packages/django-dbdev/django_dbdev-1.0.1.tar.gz/django_dbdev-1.0.1/django_dbdev/management/commands/dbdev_loaddump.py
@@ -0,0 +1,15 @@
+from django.core.management import call_command
+
+from ._base import BaseDbdevCommand
+
+
+class Command(BaseDbdevCommand):
+    help = 'Load a database dump.'
+    args = '<dumpfile>'
+
+    def dbdev_handle(self):
+        if len(self.args) != 1:
+            self.stderr.write('Dumpfile is required. See --help.')
+            raise SystemExit()
+        dumpfile = self.args[0]
+        self.dbdev_backend.load_dbdump(dumpfile)
