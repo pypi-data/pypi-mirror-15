382aac94680644fe8abd5d7543a5efb7b2eff805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:51 +0000

    Add package Onlinenet_serial_console/0.0.0/Onlinenet_serial_console-0.0.0.tar.gz

---
+++ packages/Onlinenet_serial_console/Onlinenet_serial_console-0.0.0.tar.gz/Onlinenet_serial_console-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    scripts=['onlinenet_serial'],
+    name='Onlinenet_serial_console',
+    version='',
+    install_requires=['websocket-client', 'requests'],
+    packages=[''],
+    url='',
+    license='',
+    author='Pierre Barre',
+    author_email='pierre.barre@openmailbox.org',
+    description=''
+)
