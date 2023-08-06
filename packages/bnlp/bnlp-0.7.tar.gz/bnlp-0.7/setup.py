09f349c7a8bebf952939944300ff254961da0d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:45 +0000

    Add package bnlp/0.7/bnlp-0.7.tar.gz

---
+++ packages/bnlp/bnlp-0.7.tar.gz/bnlp-0.7/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'bnlp',
+  packages = ['bnlp'],
+  package_dir = {'bnlp': 'bnlp'},
+  package_data = {'bnlp': ['*','data/*','stopwords/*','keywords/*']},
+  version = '0.7',
+  description = 'Better Natural Language Processing, a collection of open source nlp methods used by Better Know The Opposition',
+  author = 'Better Know The Opposition',
+  author_email = 'hello@betterknowtheopposition.com',
+  url = 'https://github.com/BKTO/bnlp',
+  download_url = 'https://github.com/BKTO/bnlp',
+  keywords = ['nlp'],
+  classifiers = [],
+)
