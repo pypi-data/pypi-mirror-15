5d1a5fe23e54090688476cb51ae117f5d6c7f868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:05 +0000

    Add package groupgfa/0.2/groupgfa-0.2.tar.gz

---
+++ packages/groupgfa/groupgfa-0.2.tar.gz/groupgfa-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='groupgfa',
+      version='0.02',
+      description='Group G',
+      url='https://github.com/caropinzon/groupgFA.git',
+      author='Carolina Pinzon',
+      author_email='caropinzon@student.ie.edu',
+      license='MIT',
+      packages=['groupgfa'],
+      zip_safe=False)
