cdbabe0ed3860db40a83f38d2b6f2dbc37d5acd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:52 +0000

    Add package image2pyarray/0.0.5/image2pyarray-0.0.5.tar.gz

---
+++ packages/image2pyarray/image2pyarray-0.0.5.tar.gz/image2pyarray-0.0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+     readme = f.read()
+
+setup(
+    name="image2pyarray",
+    version="0.0.5",
+    packages=['image2pyarray'],
+    description='load image',
+    long_description=readme,
+    url='https://github.com/',
+    author='me',
+    author_email='me@etwings.com',
+    license='MIT',
+    install_requires=[
+        'Pillow','numpy'
+    ],
+)
