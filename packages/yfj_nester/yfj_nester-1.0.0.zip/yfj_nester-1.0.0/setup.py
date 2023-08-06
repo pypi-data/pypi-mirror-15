c74e5523dd1969ee33029a510adecaf0503ddd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:57 +0000

    Add package yfj_nester/1.0.0/yfj_nester-1.0.0.zip

---
+++ packages/yfj_nester/yfj_nester-1.0.0.zip/yfj_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+				name  =  'yfj_nester',
+				version = '1.0.0', 
+				py_modules = ['yfj_nester'],
+				author = 'tinybird',
+				author_email = 'tinybird@qq.com',
+				url = 'http://oschina.com',
+				description = 'A simple printer of nested lists',
+			)
