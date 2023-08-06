cfd5595422106c4f02b583522fa7c91008f3a12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:23 +0000

    Add package snappy/2.4.1/snappy-2.4.1.tar.gz

---
+++ packages/snappy/snappy-2.4.1.tar.gz/snappy-2.4.1/mac_osx_app/release.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+import os, sys, re, glob
+
+# We build the thing and install it twice to make sure the
+# documentation is up to date.
+
+python26 = "/Library/Frameworks/Python.framework/Versions/2.6/bin/python"
+python26_sys = "/usr/bin/python2.6"
+framework = '/Library/Frameworks/Python-10.5-intel.framework'
+if not os.path.exists(framework):
+    framework = '/Library/Frameworks/Python.framework'
+print 'Using python from %s'%framework
+python27 = os.path.join(framework, 'Versions', '2.7', 'bin', 'python')
+
+os.chdir("../")
+os.system("hg pull")
+os.system("hg up")
+#os.system(python27 + " setup.py clean")
+for python in [python27]:
+    os.system(python + " setup.py install")
+    os.system(python + " setup.py build_docs install")
+    
+# Now build the .app
+
+os.chdir("mac_osx_app")
+os.system(python27 + " setup.py py2app")
+
+# Make things a little smaller.
+
+os.system("rm -rf dist/SnapPy.app/Contents/Frameworks/Tcl.framework/Versions/*/Resources/English.lproj/ActiveTcl-*")
+os.system("rm -rf dist/SnapPy.app/Contents/Frameworks/Tk.framework/Versions/*/Resources/Scripts/demos")
+
+# Make sure we use the correct version of Tk (testing 8.6):
+os.system("pushd dist/SnapPy.app/Contents/Frameworks/Tcl.framework/Versions/; "
+          "rm Current; ln -s 8.6 Current; popd")
+os.system("pushd dist/SnapPy.app/Contents/Frameworks/Tk.framework/Versions/; "
+          "rm Current; ln -s 8.6 Current; popd")
+
+# Then make the disk image file.  
+
+os.chdir("dmg-maker")
+os.system("./dmg-maker.py")
---
+++ packages/snappy/snappy-2.4.1.tar.gz/snappy-2.4.1/windows_exe/make.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import os, sys, re, shutil
+
+python27 = "c:\Python27\python.exe "
+
+try:
+    import pyx
+except ImportError:
+    print "ERROR: Need to install PyX!"
+    sys.exit()
+
+os.chdir("../windows_exe/../")
+os.system("hg pull")
+os.system("hg update")
+os.system("rm dist/*.egg")
+
+for python in [python27]:
+    os.system(python + "setup.py install")
+    os.system(python + "setup.py build_docs")
+    os.system(python + "setup.py install")
+
+# Now build the .exe
+
+os.chdir("windows_exe")
+os.system("rm -rf build dist InstallSnappy.exe")
+os.system("pyinstaller SnapPy.spec")
+
+print "Starting the app to force lib2to3 to build pickles."
+print "Close the app to continue."
+os.system(os.path.join("dist", "SnapPy", "SnapPy.exe"))
+
+# Build the Inno Setup installer
+os.system("compil32 /cc InnoSnapPy.iss")
