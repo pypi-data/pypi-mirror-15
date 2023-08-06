9f9ce05b2e3f2b2c70b8f5b3d11f7044f94b7226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:32 +0000

    Add package yantra/0.2/yantra-0.2.tar.gz

---
+++ packages/yantra/yantra-0.2.tar.gz/yantra-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='yantra',
+    version='0.2',
+    description='A simple plugin framework for Python',
+    url='https://github.com/sdelements/yantra',
+    author='Hanif Virani',
+    author_email='hanif@securitycompass.com',
+    license='MIT',
+    packages=['yantra'],
+    install_requires=[],
+)
