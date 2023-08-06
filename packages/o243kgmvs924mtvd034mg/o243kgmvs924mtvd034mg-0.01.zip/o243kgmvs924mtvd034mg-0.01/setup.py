49108d11c0383248db929a1ad38574d6e7f76571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:25 +0000

    Add package o243kgmvs924mtvd034mg/0.01/o243kgmvs924mtvd034mg-0.01.zip

---
+++ packages/o243kgmvs924mtvd034mg/o243kgmvs924mtvd034mg-0.01.zip/o243kgmvs924mtvd034mg-0.01/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup (
+	name = 'o243kgmvs924mtvd034mg',
+	packages = ['o243kgmvs924mtvd034mg'],
+	version = '0.01',
+	description = 'NumPy Testing',
+	author = 'Peter Downs',
+	author_email = 'fake@gmail.com',
+	url = 'https://github.com/dsokoler/PyPi-Testing', # use the URL to the github repo
+	download_url = 'https://github.com/dsokoler/PyPi-Testing/tarball/0.1', # I'll explain this in a second
+	keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+	classifiers = [],
+)
