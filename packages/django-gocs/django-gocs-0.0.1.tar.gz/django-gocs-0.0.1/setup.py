976f35a164b1402ca9445dbe9cc9cdb2b8316d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:56 +0000

    Add package django-gocs/0.0.1/django-gocs-0.0.1.tar.gz

---
+++ packages/django-gocs/django-gocs-0.0.1.tar.gz/django-gocs-0.0.1/setup.py
@@ -0,0 +1,44 @@
+import os
+import time
+from setuptools import setup
+from os.path import join, dirname, abspath
+
+__version__ = '0.0'
+__author__ = 'APS Lite team'
+
+
+def version():
+    def version_file(mode='r'):
+        return open(join(dirname(abspath(__file__)), 'version.txt'), mode)
+
+    if os.getenv('TRAVIS'):
+        build_version = os.getenv('TRAVIS_BUILD_NUMBER')
+    elif os.getenv('JENKINS_HOME'):
+        build_version = 'jenkins{}'.format(os.getenv('BUILD_NUMBER'))
+    else:
+        build_version = 'dev{}'.format(int(time.time()))
+
+    with version_file('w') as verfile:
+        verfile.write('{0}.{1}'.format(__version__, build_version))
+
+    with version_file() as verfile:
+        data = verfile.readlines()
+        return data[0].strip()
+
+setup(
+    name='django-gocs',
+    packages=['django_gocs'],
+    version=version(),
+    description='Django file storage backend and temporary file handler for '
+                'Google Cloud Storage',
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    author=__author__,
+    author_email='aps@odin.com',
+    install_requires=[
+        'django',
+        'GoogleAppEngineCloudStorageClient==1.9.15.0'
+    ],
+    url='https://github.com/odin-public/django-gocs',
+    keywords=['django', 'storage', 'gcs', 'google cloud storage'],
+    classifiers=[],
+)
