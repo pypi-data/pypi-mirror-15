9ab2e3c3d0b731fcce218a556b3cde55dde58835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:01 +0000

    Add package bravocapital/1.0.3/bravocapital-1.0.3.tar.gz

---
+++ packages/bravocapital/bravocapital-1.0.3.tar.gz/bravocapital-1.0.3/bravocapital/__init__.py
@@ -0,0 +1,11 @@
+from .safechars import safe_upperstr
+from .safechars import safe_chars_nombrepersona
+from .safechars import safe_upperstr_razonsocial
+
+
+def imprime():
+	return 3
+
+def sumados(valor):
+	return valor+2
+
