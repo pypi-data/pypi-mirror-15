5eb595c86970642ae778ece169617914e5811b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:29 +0000

    Add package test_nester_adsr/1.0.0/test_nester_adsr-1.0.0.zip

---
+++ packages/test_nester_adsr/test_nester_adsr-1.0.0.zip/test_nester_adsr-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'test_nester_adsr',
+    version = '1.0.0',
+    py_modules = ['test_nester_adsr'],
+    author = 'Arsh',
+    author_email = 'arsh5randhawa@gmail.com',
+    description = 'A simple printer of nested lists',
+    )
+
+
