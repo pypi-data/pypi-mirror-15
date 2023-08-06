e802b3917667d338c0374cce8a8f216947e7865a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:33 +0000

    Add package ongair-whatsapp/1.0.25/ongair-whatsapp-1.0.25.tar.gz

---
+++ packages/ongair-whatsapp/ongair-whatsapp-1.0.25.tar.gz/ongair-whatsapp-1.0.25/ongair/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+ongair-whatsapp
+~~~~~~~~~~~~~~~
+
+
+This is the ongair layer that allows
+ongair platform to interact with whatsapp.
+It is builts on top of the yowsup layer
+
+"""
+
+__version__ = "1.0.25"
+
+__title__ = 'ongair-whatsapp'
+__package_name__ = 'ongair-whatsapp'
+__author__ = 'Ongair Limited'
+__description__ = 'Ongair layer that connects to whatsapp'
+__email__ = 'trevor@ongair.im'
+__license__ = 'GPL'
+__copyright__ = 'Copyright 2016 Ongair Limited'
