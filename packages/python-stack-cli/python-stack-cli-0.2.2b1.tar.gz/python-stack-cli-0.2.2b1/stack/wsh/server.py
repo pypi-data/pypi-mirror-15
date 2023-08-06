89f320fe1958f4a0a69d70b52771be751dabced3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:17 +0000

    Add package python-stack-cli/0.2.2b1/python-stack-cli-0.2.2b1.tar.gz

---
+++ packages/python-stack-cli/python-stack-cli-0.2.2b1.tar.gz/python-stack-cli-0.2.2b1/stack/wsh/server.py
@@ -0,0 +1,44 @@
+# coding utf8
+
+
+from aiohttp import web
+from typing import Callable
+from functools import partial
+
+
+def command_parser(cmd: str, fns: dict) -> Callable:
+    '''
+    get target function from funs dict and \
+    map command like "cmd arg0 arg1 arg2 --key1=v1 --key2=v2" to \
+    partial(fn, arg0, arg1, arg2, key1=v1, key2=v2)
+
+    '''
+    args = cmd.strip().split(' ')
+    fn_name = args[0]
+    args = [i for i in args[1:] if not i.startswith('-')]
+    kwargs = dict([i.replace('-', '').split('=') for i in args[1:] if i.startswith('-')])
+    return partial(fns.get(fn_name, lambda: 'None'), *args, **kwargs)()
+
+
+async def wsh(request, handler=print):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    async for msg in ws:
+        if msg.tp == web.MsgType.text:
+            res = handler(msg.data)
+            ws.send_str("Out: {}".format(res))
+        elif msg.tp == web.MsgType.binary:
+            ws.send_bytes(msg.data)
+        elif msg.tp == web.MsgType.close:
+            print('websocket connection closed')
+    return ws
+
+
+def main(host='127.0.0.1', port='8964', pattern={}):
+    app = web.Application()
+    app.router.add_route('GET', '/ws', partial(wsh, handler=partial(command_parser, fns=pattern)))
+    web.run_app(app, host=host, port=port)
+
+
+if __name__ == '__main__':
+    main()
