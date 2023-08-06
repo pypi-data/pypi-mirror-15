208f753356f1e0f53e070cb6bd100c2bf171aefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:47 +0000

    Add package hubugs/0.18.0/hubugs-0.18.0.tar.bz2

---
+++ packages/hubugs/hubugs-0.18.0.tar.bz2/hubugs-0.18.0/hubugs/_version.py
@@ -0,0 +1,10 @@
+# This is hubugs version 0.18.0 (2016-05-16)
+# pylint: skip-file
+
+dotted = '0.18.0'
+libtool = '18:20'
+hex = 0x001200
+date = '2016-05-16'
+tuple = (0, 18, 0)
+dict = {'major': 0, 'minor': 18, 'micro': 0}
+web = 'hubugs/0.18.0'
