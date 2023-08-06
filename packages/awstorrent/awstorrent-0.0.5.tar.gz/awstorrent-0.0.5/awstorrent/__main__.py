1b60811c31629eb2b472956fc8957214c58073a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:10 +0000

    Add package awstorrent/0.0.5/awstorrent-0.0.5.tar.gz

---
+++ packages/awstorrent/awstorrent-0.0.5.tar.gz/awstorrent-0.0.5/awstorrent/__main__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import pyperclip
+import argparse
+from .dl import download
+# Address of my amazon-web-service downloading server 
+
+def main(): 
+    aws="firearasi@52.53.224.102"
+
+    # use transmission [torrent file address]
+    parser=argparse.ArgumentParser(prog="My torrent downloader \
+        through aws",prefix_chars='-')
+    parser.add_argument('torrent', help='torrent address',nargs='?',
+        default=pyperclip.paste())
+    parser.add_argument('-o','--output_dir', default='.',
+        help='set local destination dir')
+    parser.add_argument('-t','--title', help='directory title')
+    parser.add_argument('-k','--keep_in_aws',
+    action='store_true',help='keep file in aws server')
+    args=parser.parse_args()
+
+
+    download(aws,args.torrent,args.title,args.output_dir,args.keep_in_aws)
+
+if __name__=='__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
