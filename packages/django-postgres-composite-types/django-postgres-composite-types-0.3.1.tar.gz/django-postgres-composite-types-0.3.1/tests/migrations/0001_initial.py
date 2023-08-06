66bd4aa6f2e7c5bb7e813d1b61293172b01ef78e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:01 +0000

    Add package django-postgres-composite-types/0.3.1/django-postgres-composite-types-0.3.1.tar.gz

---
+++ packages/django-postgres-composite-types/django-postgres-composite-types-0.3.1.tar.gz/django-postgres-composite-types-0.3.1/tests/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# pylint:disable=invalid-name
+"""
+Migration to create custom types
+"""
+
+from django.db import migrations
+
+from ..base import Box, Card, OptionalBits, Point, SimpleType
+
+
+class Migration(migrations.Migration):
+    """Migration."""
+
+    dependencies = [
+    ]
+
+    operations = [
+        SimpleType.Operation(),
+        OptionalBits.Operation(),
+        Card.Operation(),
+        Point.Operation(),
+        Box.Operation(),
+    ]
