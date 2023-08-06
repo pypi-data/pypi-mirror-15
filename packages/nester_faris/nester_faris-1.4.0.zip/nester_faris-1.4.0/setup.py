8a04222f2d1c326345294b67c15f063224aa6eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:02 +0000

    Add package nester_faris/1.4.0/nester_faris-1.4.0.zip

---
+++ packages/nester_faris/nester_faris-1.4.0.zip/nester_faris-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name          = 'nester_faris',
+    version       = '1.4.0',
+    py_modules    = ['nester_faris'],
+    author        = 'faris_yzf',
+    author_email  = 'yangzhenfa_0814@163.com',
+    #url           = 'yangzhenfa_0814@163.com',
+    description   = '实现列表的输出',
+    )
