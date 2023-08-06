52d5de45cefcff733ecdfecd9ddaf07c48daea54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:48 +0000

    Add package wow_nester/1.3.0/wow_nester-1.3.0.tar.gz

---
+++ packages/wow_nester/wow_nester-1.3.0.tar.gz/wow_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name = 'wow_nester',
+	version = '1.3.0',
+	py_modules = ['wow_nester'],
+	author = 'none',
+	author_eamil = 'none',
+	url = 'none',
+	descriptiom = 'A simple printer of nested lists',
+)
