c979c0ca57d1782d4ad505a5705f004612c7ec5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:31 +0000

    Add package asyncssh/1.5.6/asyncssh-1.5.6.tar.gz

---
+++ packages/asyncssh/asyncssh-1.5.6.tar.gz/asyncssh-1.5.6/examples/stream_math_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.5
+#
+# Copyright (c) 2013-2016 by Ron Frederick <ronf@timeheart.net>.
+# All rights reserved.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License v1.0 which accompanies this
+# distribution and is available at:
+#
+#     http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     Ron Frederick - initial implementation, API, and documentation
+
+import asyncio, asyncssh, sys
+
+async def run_client():
+    async with asyncssh.connect('localhost') as conn:
+        stdin, stdout, stderr = await conn.open_session('bc')
+
+        for op in ['2+2', '1*2*3*4', '2^32']:
+            stdin.write(op + '\n')
+            result = await stdout.readline()
+            print(op, '=', result, end='')
+
+try:
+    asyncio.get_event_loop().run_until_complete(run_client())
+except (OSError, asyncssh.Error) as exc:
+    sys.exit('SSH connection failed: ' + str(exc))
