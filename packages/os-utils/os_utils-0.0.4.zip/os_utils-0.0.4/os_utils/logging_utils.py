a689aeacfeaeec1303be3d748a3b2842aea2ee0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:39 +0000

    Add package os-utils/0.0.4/os_utils-0.0.4.zip

---
+++ packages/os-utils/os_utils-0.0.4.zip/os_utils-0.0.4/os_utils/logging_utils.py
@@ -0,0 +1,44 @@
+import logging
+import sys
+from contextlib import contextmanager
+from datetime import datetime
+from os import getcwd
+from os.path import join
+
+LOGGING_FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
+DATE_FORMAT = '[%Y-%m-%d %H:%M:%S]'
+
+logger = logging.getLogger()
+logger.level = logging.DEBUG
+
+
+def configure_stream_logger(stream=sys.stdout, level='DEBUG'):
+    stream_handler = logging.StreamHandler(stream=stream)
+    stream_handler.level = logging.getLevelName(level)
+    formatter = logging.Formatter(datefmt=DATE_FORMAT, fmt=LOGGING_FORMAT)
+    stream_handler.setFormatter(formatter)
+    logging.getLogger().addHandler(stream_handler)
+
+
+def configure_file_logger(filename='app.log', level='DEBUG', rotate_file=True):
+    file_path = join(getcwd(), filename)
+    file_handler = logging.FileHandler(filename=file_path)
+    file_handler.level = logging.getLevelName(level)
+    formatter = logging.Formatter(datefmt=DATE_FORMAT, fmt=LOGGING_FORMAT)
+    file_handler.setFormatter(formatter)
+    logging.getLogger().addHandler(file_handler)
+    if rotate_file:
+        open(file_path, 'w').close()
+
+
+def configure_file_and_stream_logger(stream=sys.stdout, filename='app.log', level='DEBUG', rotate_file=True):
+    configure_stream_logger(stream, level)
+    configure_file_logger(filename, level, rotate_file)
+
+
+@contextmanager
+def timeit():
+    start_time = datetime.now()
+    logger.info('START TIME: %s', start_time)
+    yield
+    logger.info('TIME SPENT: %s', (datetime.now() - start_time))
---
+++ packages/os-utils/os_utils-0.0.4.zip/os_utils-0.0.4/os_utils/operations.py
@@ -0,0 +1,25 @@
+import hashlib
+from distutils.filelist import findall
+from os.path import relpath, dirname, join, basename
+from zipfile import ZipFile
+
+
+def get_md5(file_path):
+    md5_hash = hashlib.md5()
+    with open(file_path, 'rb') as file_:
+        batch_size = 4096
+        chunk = file_.read(batch_size)
+        while chunk:
+            md5_hash.update(chunk)
+            chunk = file_.read(batch_size)
+
+        return hashlib.md5(file_.read()).hexdigest()
+
+
+def zip_dir(path, zip_name=None, zip_root=None):
+    zip_name = zip_name or '{}.zip'.format(path)
+    zip_root = zip_root or basename(path)
+    with ZipFile(zip_name, 'w') as zip_file:
+        for file in findall(path):
+            zip_file.write(file, join(zip_root, relpath(file, path)))
+
