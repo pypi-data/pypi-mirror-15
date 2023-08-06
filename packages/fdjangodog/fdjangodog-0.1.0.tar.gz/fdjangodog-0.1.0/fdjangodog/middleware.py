78c94840cbd93cd2332306cb45e093803fa9da9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:02 +0000

    Add package fdjangodog/0.1.0/fdjangodog-0.1.0.tar.gz

---
+++ packages/fdjangodog/fdjangodog-0.1.0.tar.gz/fdjangodog-0.1.0/fdjangodog/middleware.py
@@ -0,0 +1,68 @@
+import time
+
+from django.conf import settings
+from django.core.urlresolvers import resolve, Resolver404
+
+from datadog import statsd
+
+
+class FDjangoDogMiddleware(object):
+    APP_NAME = settings.FDJANGODOG_APP_NAME
+    DD_TIMING_ATTRIBUTE = '_dd_start_time'
+
+    def __init__(self):
+        self.stats = statsd
+
+        self.timing_metric = '{}.request_time'.format(self.APP_NAME)
+
+    def _get_elapsed_time(self, request):
+        return time.time() - getattr(request, self.DD_TIMING_ATTRIBUTE)
+
+    def _get_request_handler(self, request):
+        try:
+            match = resolve(request.path)
+        except Resolver404:
+            return None
+
+        if match.url_name:
+            return "url:{}".format(match.url_name)
+        return "view:{}".format(match.view_name)
+
+    def _get_metric_tags(self, request, response=None, exception=None):
+        tags = []
+
+        handler = self._get_request_handler(request)
+        if handler:
+            tags.append('handler:{}'.format(handler))
+
+        if response:
+            tags.append('status_code:{}'.format(response.status_code))
+
+        if exception:
+            tags.append('exception:{}'.format(exception.__class__.__name__))
+
+        return tags
+
+    def _record_request_time(self, request, tags):
+        self.stats.histogram(metric=self.timing_metric, value=self._get_elapsed_time(request), tags=tags)
+
+    def process_request(self, request):
+        setattr(request, self.DD_TIMING_ATTRIBUTE, time.time())
+
+    def process_response(self, request, response):
+        """Submit timing metrics from the current request."""
+        if not hasattr(request, self.DD_TIMING_ATTRIBUTE):
+            return response
+
+        tags = self._get_metric_tags(request, response)
+        self._record_request_time(request, tags)
+
+        return response
+
+    def process_exception(self, request, exception):
+        """Captures Django view exceptions as Datadog events."""
+        if not hasattr(request, self.DD_TIMING_ATTRIBUTE):
+            return
+
+        tags = self._get_metric_tags(request, exception=exception)
+        self._record_request_time(request, tags)
