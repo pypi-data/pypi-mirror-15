ddb75f95a05e0b90c9c71d646e40ffa9377bcf2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:39 +0000

    Add package trakt.py/2.6.1/trakt.py-2.6.1.zip

---
+++ packages/trakt.py/trakt.py-2.6.1.zip/trakt.py-2.6.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+base_dir = os.path.dirname(__file__)
+
+version = {}
+with open(os.path.join(base_dir, "trakt", "version.py")) as f:
+    exec(f.read(), version)
+
+setup(
+    name='trakt.py',
+    version=version['__version__'],
+    license='MIT',
+    url='https://github.com/fuzeman/trakt.py',
+
+    author='Dean Gardiner',
+    author_email='me@dgardiner.net',
+
+    description='Python interface for the trakt.tv API',
+    packages=find_packages(exclude=[
+        'examples',
+
+        'tests',
+        'tests.*'
+    ]),
+    platforms='any',
+
+    install_requires=[
+        'arrow',
+        'requests>=2.4.0',
+        'six'
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+)
