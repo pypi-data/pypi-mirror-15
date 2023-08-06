75e71463c015df5548514c13a6035bf30b5d1dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:11 +0000

    Add package kapsel/0.0.0/kapsel-0.0.0.tar.gz

---
+++ packages/kapsel/kapsel-0.0.0.tar.gz/kapsel-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "kapsel",
+    version = "0.0.0",
+    author = "Kale Franz",
+    author_email = "kale@franz.io",
+    description = "Take your medicine.",
+)
