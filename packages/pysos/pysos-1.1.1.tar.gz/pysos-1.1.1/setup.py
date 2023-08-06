7fad2355dadab6a3091c4e065c00856c0e9e06e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:33 +0000

    Add package pysos/1.1.1/pysos-1.1.1.tar.gz

---
+++ packages/pysos/pysos-1.1.1.tar.gz/pysos-1.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pysos',
+    version='1.1.1',
+    description='Simple Object Storage - Persistent dicts and lists for python.',
+    packages=["pysos"],
+    url='https://github.com/dagnelies/pysos',
+    author='Arnaud Dagnelies',
+    author_email='arnaud.dagnelies@gmail.com',
+    license='MIT',
+    classifiers=[],
+    keywords='persistent persistence dict list file'
+)
