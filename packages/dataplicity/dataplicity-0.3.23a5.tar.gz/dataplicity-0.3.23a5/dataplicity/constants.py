5740a88224676029c56a1b68828100909938dc91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:51 +0000

    Add package dataplicity/0.3.23a5/dataplicity-0.3.23a5.tar.gz

---
+++ packages/dataplicity/dataplicity-0.3.23a5.tar.gz/dataplicity-0.3.23a5/dataplicity/constants.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+
+SERVER_URL = os.environ.get('DATAPLICITY_API_URL', "https://api.dataplicity.com")
+PUSH_URL = "https://sync.dataplicity.com/pushwait/"
+CONF_PATH = "/etc/dataplicity/dataplicity.conf"
+SETTINGS_PATH = "/var/dataplicity/"
+FIRMWARE_PATH = "/srv/dataplicity/fw/"
+TIMELINE_PATH = "/tmp/dataplicitytimeline/"
+PID_PATH = "/var/run/dataplicity.pid"
+M2M_URL = os.environ.get('DATAPLICITY_M2M_URL', "wss://m2m.dataplicity.com/m2m/")
