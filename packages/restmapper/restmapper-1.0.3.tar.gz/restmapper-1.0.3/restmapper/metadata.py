47bebf59a27c8a70a986cbad2a384c3ee276483f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:26 +0000

    Add package restmapper/1.0.3/restmapper-1.0.3.tar.gz

---
+++ packages/restmapper/restmapper-1.0.3.tar.gz/restmapper-1.0.3/restmapper/metadata.py
@@ -0,0 +1,8 @@
+__version__ = "1.0.3"
+__author__ = "Dan Loewenherz"
+__copyright__ = "Copyright 2016, Lionheart Software"
+__maintainer__ = "Dan Loewenherz"
+__email__ = "dan@lionheartsw.com"
+__license__ = "Apache 2.0"
+__url__ = "https://github.com/lionheart/python-restmapper"
+
