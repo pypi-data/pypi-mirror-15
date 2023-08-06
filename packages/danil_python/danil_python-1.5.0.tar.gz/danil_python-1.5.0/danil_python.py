fa198ed462e89aa8aa18af21448b519db181ace1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:18 +0000

    Add package danil_python/1.5.0/danil_python-1.5.0.tar.gz

---
+++ packages/danil_python/danil_python-1.5.0.tar.gz/danil_python-1.5.0/danil_python.py
@@ -0,0 +1,15 @@
+"""this is danil python study file"""
+import sys
+
+def print_lol(the_list, indent=False, level=0, fo=sys.stdout):
+#hello, this is parameter def
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1, fo)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='', file=fo)
+            print(each_item, file=fo)
+
+
