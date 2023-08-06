0c5a47de41c56c4e86a39a9abb998c758392fa2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:55 +0000

    Add package nickyyyyy_nester/1.1.0/nickyyyyy_nester-1.1.0.zip

---
+++ packages/nickyyyyy_nester/nickyyyyy_nester-1.1.0.zip/nickyyyyy_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='nickyyyyy_nester',
+	version='1.1.0',
+	py_modules=['nester'],
+	author='nickyyyyy',
+	author_email='xhnickyyyyy@gmail.com',
+	url='https://github.com/nickyyyyy',
+	)
