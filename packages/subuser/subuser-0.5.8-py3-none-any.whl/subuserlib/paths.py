aecb569ff02cfddd8b414c3f4983a3f53db32d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:21 +0000

    Add package subuser/0.5.8/subuser-0.5.8-py3-none-any.whl

---
+++ packages/subuser/subuser-0.5.8-py3-none-any.whl/subuserlib/paths.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module used for determining non-user-configurable paths.
+"""
+
+#external imports
+import os
+import inspect
+#internal imports
+#import ...
+
+home = os.path.expanduser("~")
+
+def upNDirsInPath(path,n):
+  if n > 0:
+    return os.path.dirname(upNDirsInPath(path,n-1))
+  else:
+    return path
+
+def getSubuserDir():
+  """
+  Get the toplevel directory for subuser.
+  """
+  pathToThisSourceFile = os.path.abspath(inspect.getfile(inspect.currentframe()))
+  return upNDirsInPath(pathToThisSourceFile,3)
+
+def getSubuserDataFile(filename):
+  dataFile = os.path.join(getSubuserDir(),"logic","subuserlib","data",filename)
+  if os.path.exists(dataFile):
+    return dataFile
+  else:
+    import pkg_resources
+    dataFile = pkg_resources.resource_filename("subuserlib",os.path.join("data",filename))
+    if not os.path.exists(dataFile):
+      exit("Data file does not exist:"+str(dataFile))
+    else:
+      return dataFile
