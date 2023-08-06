89593afbd1120d6b0c572c9bdb1a04962f57ee55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:03 +0000

    Add package gentletool/0.2/gentletool-0.2.tar.gz

---
+++ packages/gentletool/gentletool-0.2.tar.gz/gentletool-0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#! coding: utf-8
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+setup(
+    name='gentletool',
+    version='0.2',
+    maintainer='liyong',
+    maintainer_email='liyong19861014@gmail.com',
+    url='https://github.com/airhuman/gentletool',
+    description='some gentle tool',
+    packages=['gentle'],
+    entry_points={
+        'console_scripts': [
+            'gentle_rm = gentle.rm:main',
+        ],
+    }
+)
