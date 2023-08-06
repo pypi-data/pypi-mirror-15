85f5e1d47b3c3f4a328e26a31b7836bef7d839c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:23 +0000

    Add package django-hhypermap/0.1/django-hhypermap-0.1.tar.gz

---
+++ packages/django-hhypermap/django-hhypermap-0.1.tar.gz/django-hhypermap-0.1/setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='django-hhypermap',
+    version='0.1',
+    author='',
+    author_email='',
+    url='https://github.com/cga-harvard/HHyperMap',
+    download_url='https://github.com/cga-harvard/HHyperMap',
+    description="Use Registry in your django projects.",
+    long_description=open(os.path.join(here, 'README.md')).read(),
+    license='BSD',
+    packages=find_packages(),
+    keywords="hypermap django",    
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'amqplib==1.0.2',
+        'arcrest==10.3',
+        'celery==3.1.19',
+        'Django==1.8.7',
+        'django-debug-toolbar==1.4',
+        'django-pagination==1.0.7',
+        'django-polymorphic==0.8.1',
+        'django-taggit==0.18.0',
+        'django-wsgi==1.0b1',
+        'django-extensions==1.6.7',
+        'dj-database-url==0.4.0',
+        'pika==0.10.0',
+        'pycsw==2.0.0-alpha1',
+        'flake8==2.5.1',
+        'httmock==1.2.5',
+        'MapProxy==1.8.1',
+        'pyelasticsearch==1.4',
+        'django-celery==3.1.17',
+        'nose==1.3.7',
+        'OWSLib==0.10.3',
+        'Paver==1.2.4',
+        'Pillow==3.1.0.rc1',
+        'python-memcached==1.57',
+        'psycopg2==2.6.1',
+        'pysolr==3.3.3',
+        'pyelasticsearch==1.4',
+        'requests==2.9.1',
+        'webtest==2.0.20',
+    ]
+)
