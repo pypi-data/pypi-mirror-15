2ece42110be482d21ddaa6de7b86631b03db2356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:26 +0000

    Add package hitchhttp/0.2.4/hitchhttp-0.2.4.tar.gz

---
+++ packages/hitchhttp/hitchhttp-0.2.4.tar.gz/hitchhttp-0.2.4/hitchhttp/mock_rest_uri.py
@@ -0,0 +1,106 @@
+import re
+import xeger
+from urllib import parse as urlparse
+import urllib
+from hitchhttp import status_codes
+
+def convert_querystring(qs):
+    """Allow for non-lists to be sent to querystring."""
+    converted_qs = {}
+    for key, value in qs.items():
+        if type(value) is not list:
+            converted_qs[key] = [value, ]
+        else:
+            converted_qs[key] = value
+    return converted_qs
+
+
+class MockRestURI(object):
+    """Representation of a mock URI."""
+    def __init__(self, uri_dict):
+        self.name = uri_dict.get('name', None)
+        self.fullpath = uri_dict['request']['path']
+        self._regexp = False
+
+        self.path = urlparse.urlparse(self.fullpath).path
+        self.querystring = urlparse.parse_qs(urlparse.urlparse(self.fullpath).query)
+
+        self.method = uri_dict['request'].get('method', None)
+        self.headers = uri_dict['request'].get('headers', None)
+        self.return_code = int(uri_dict['response'].get('code', '200'))
+        self.response_content_type = uri_dict['response'].get('content-type', 'text/plain')
+        self.response_location = uri_dict['response'].get('location', None)
+        self.response_content = uri_dict['response'].get('content', "")
+        self.wait = float(uri_dict['response'].get('wait', 0.0))
+        self.request_data = uri_dict['request'].get('data', None)
+        self.querystring = convert_querystring(uri_dict['request'].get("querystring", {}))
+        self.encoding = uri_dict['request'].get("encoding", None)
+        self.response_headers = uri_dict['response'].get("headers", {})
+
+    def match(self, request):
+        """Does this URI match the request?"""
+        # Match HTTP verb - GET, POST, PUT, DELETE, etc.
+        if self.method is not None:
+            if request.command.lower() != self.method.lower():
+                return False
+
+        # Match path
+        if self.path != urlparse.urlparse(request.path).path:
+            return False
+
+        # Match headers
+        if self.headers is not None:
+            for header_var, header_value in self.headers.items():
+                if header_var not in request.headers:
+                    return False
+                if request.headers[header_var] != header_value:
+                    return False
+
+        #if not self._regexp and self.path != request.basepath():
+            #return False
+
+        #if self._regexp and re.compile(self.path).match(request.path) is not None:
+            #return False
+
+        # Match querystring
+        if request.querystring() != self.querystring:
+            return False
+
+        # Match processed request data
+        if self.request_data is not None:
+            if request.request_data != self.request_data:
+                return False
+
+        # Match encoding
+        if self.encoding is not None:
+            if request.ctype != self.encoding:
+                return False
+
+        return True
+
+    def querystring_string(self):
+        # TODO : Refactor this and docstring.
+        query = ''
+        for key in self.querystring.keys():
+            for item in self.querystring[key]:
+                query += str(key) + '=' + item + "&"
+        query = query.rstrip("&")
+        return "?" + query if query else ""
+
+    def example_path(self):
+        return xeger.xeger(self.path) if self._regexp else self.path + self.querystring_string()
+
+    def return_code_description(self):
+        return status_codes.CODES.get(self.return_code)[0]
+
+    def request_data_values(self):
+        if self.request_data is not None:
+            return self.request_data.get('values', {}).iteritems()
+        else:
+            return []
+
+    def request_data_type(self):
+        if self.request_data is not None:
+            return self.request_data.get('encoding')
+        else:
+            return None
---
+++ packages/hitchhttp/hitchhttp-0.2.4.tar.gz/hitchhttp-0.2.4/hitchhttp/rest.py
@@ -0,0 +1,110 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse as urlparse
+from hitchhttp import http_request
+import requests
+import yaml
+import json
+import time
+import sys
+import cgi
+import re
+
+
+class MockRestHandler(BaseHTTPRequestHandler):
+    """Mock REST server request handling."""
+    default_response = ("""<html><head><title>Nothing configured!</title></head><body>No matching URI found for {0}<br/><br/>"""
+                       """See <a href="http://hitchtest.readthedocs.org/">the docs</a> """
+                       """for more information.</body>\n""")
+
+    def log_request(self, code):
+        """Logging is done by process method."""
+        pass
+
+    def log_json(self, name, request, response):
+        """JSON to log to indicate what just happened."""
+        pair = {}
+        pair['match'] = name
+        pair['request'] = request
+        pair['response'] = response
+
+        sys.stdout.write(u"{0}\n".format(json.dumps(pair)))
+        sys.stdout.flush()
+
+    def process(self, method):
+        """Determine if a request matches a listed URI in the config, and if so, respond."""
+        request = http_request.MockRequest(self.command, self.path, self.headers, self.rfile)
+
+        if self.record:
+            response = requests.request(
+                method,
+                "{}{}".format(self.redirection_url, self.path),
+                headers=request.headers_without_host,
+                data=request.request_data
+            )
+
+
+            self.send_response(response.status_code)
+            for header_var, header_val in response.headers.items():
+                if header_var.lower() not in ["transfer-encoding", "content-encoding", ]:
+                    self.send_header(header_var, header_val)
+            self.end_headers()
+            self.wfile.write(response.content)
+            self.wfile.flush()
+            self.log_json(self.path, {"method": method}, response.content.decode('utf8'))
+
+            yaml_snip = {}
+            yaml_snip['request'] = {
+                "path": request.basepath(),
+                "method": method,
+                "headers": request.headers,
+            }
+
+            if request.request_data is not None:
+                yaml_snip['request']['data'] = request.request_data
+
+            if request.querystring != {}:
+                yaml_snip['request']['querystring'] = request.querystring()
+
+            yaml_snip['response'] = {
+                "content": response.content.decode('utf8'),
+                "code": response.status_code,
+                "headers": dict(response.headers),
+            }
+
+            with open(self.record_to_filename, 'a') as handle:
+                handle.write("\n{}".format(yaml.dump([yaml_snip], default_flow_style=False)))
+        else:
+            uri = self.config.get_matching_uri(request)
+
+            if uri is not None:
+                time.sleep(uri.wait)
+                self.send_response(uri.return_code)
+                #self.send_header('Content-type', uri.response_content_type)
+                #if uri.response_location is not None:
+                    #self.send_header('Location', uri.response_location)
+                for header_var, header_val in uri.response_headers.items():
+                    if header_var.lower() not in ["transfer-encoding", "content-encoding", ]:
+                        self.send_header(header_var, header_val)
+                self.end_headers()
+                self.wfile.write(uri.response_content.encode('utf8'))
+                self.wfile.flush()
+                self.log_json(uri.name, request.to_dict(uri.name), uri.response_content)
+            else:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(self.default_response.format(self.path).encode('utf8'))
+                self.wfile.flush()
+                self.log_json(None, request.to_dict(None), self.default_response.format(self.path))
+
+    def do_GET(self):
+        self.process('GET')
+
+    def do_POST(self):
+        self.process('POST')
+
+    def do_PUT(self):
+        self.process('PUT')
+
+    def do_DELETE(self):
+        self.process('DELETE')
