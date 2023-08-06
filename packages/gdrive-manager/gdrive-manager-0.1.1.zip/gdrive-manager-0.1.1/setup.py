1d920a71013c5780eec00dbf73301bdf6ed0b5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:55 +0000

    Add package gdrive-manager/0.1.1/gdrive-manager-0.1.1.zip

---
+++ packages/gdrive-manager/gdrive-manager-0.1.1.zip/gdrive-manager-0.1.1/setup.py
@@ -0,0 +1,9 @@
+# ref: http://marthall.github.io/blog/how-to-package-a-python-app/
+
+from setuptools import setup
+
+setup (
+	name='gdrive-manager',    				# This is the name of your PyPI-package.
+	version='0.1.1',                          # Update the version number for new releases
+	scripts=['gdrive-manager']              # The name of your scipt, and also the command you'll be using for calling it
+)
