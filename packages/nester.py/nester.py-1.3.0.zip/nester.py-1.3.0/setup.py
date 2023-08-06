fd38c7aa2d18a94702028423016b59f6cdd5f389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:45 +0000

    Add package nester.py/1.3.0/nester.py-1.3.0.zip

---
+++ packages/nester.py/nester.py-1.3.0.zip/nester.py-1.3.0/setup.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Piza
+#
+# Created:     07/06/2016
+# Copyright:   (c) Piza 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from distutils.core import setup
+
+setup(
+        name            = 'nester.py',
+        version         = '1.3.0',
+        py_modeles      = ['nester'],
+        author          = 'use a cabeça',
+        author_email    = 'useacabeca@gmail.com',
+        url             = 'http://www.headfirstlabs.com',
+        description     = 'um exemplo',
+    )
