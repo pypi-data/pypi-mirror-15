d25d398170e1d8f5b7dddac309eedc9d58a491bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:52 +0000

    Add package peloton_bloomfilters/0.0.3/peloton_bloomfilters-0.0.3.tar.gz

---
+++ packages/peloton_bloomfilters/peloton_bloomfilters-0.0.3.tar.gz/peloton_bloomfilters-0.0.3/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+setup(name='peloton_bloomfilters',
+      author = 'Adam DePrince',
+      author_email = 'adam@pelotoncycle.com',
+      url = 'https://github.com/pelotoncycle/peloton_bloomfilters',
+      version='0.0.3',
+      description="Peloton Cycle's Bloomin fast Bloomfilters",
+      ext_modules=(
+          [
+              Extension(
+                  name='peloton_bloomfilters',
+                  sources=['peloton_bloomfiltersmodule.c']),
+          ]
+      )
+)
