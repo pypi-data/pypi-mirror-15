4a19569d15eca33fd6bfcd644a0585dc0ce8d0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:46 +0000

    Add package revrand/0.4.0/revrand-0.4.0.tar.gz

---
+++ packages/revrand/revrand-0.4.0.tar.gz/revrand-0.4.0/revrand/utils/__init__.py
@@ -0,0 +1,22 @@
+"""
+Reusable utility functions
+"""
+
+from .decorators import (vectorize_args, unvectorize_args, vectorize_result)
+from .base import (flatten, unflatten, couple, decouple, nwise, map_indices,
+                   append_or_extend, atleast_list, issequence)
+
+__all__ = [
+    'vectorize_args',
+    'unvectorize_args',
+    'vectorize_result',
+    'flatten',
+    'unflatten',
+    'couple',
+    'decouple',
+    'nwise',
+    'map_indices',
+    'append_or_extend',
+    'atleast_list',
+    'issequence'
+]
