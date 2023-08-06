72c77f64673f32c0d1e4cb19d70f25e67fff5e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:42 +0000

    Add package iftest/1.0.0/iftest-1.0.0.tar.gz

---
+++ packages/iftest/iftest-1.0.0.tar.gz/iftest-1.0.0/iftest.py
@@ -0,0 +1,12 @@
+a = input ("Input value: ")
+b = input ("Input second value: ")
+if a > b:
+    c = a + b
+else:
+    c = b * a
+print ( c )
+
+
+           
+ 
+
---
+++ packages/iftest/iftest-1.0.0.tar.gz/iftest-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name    = 'iftest',
+    version = '1.0.0',
+    py_modules = ['iftest'],
+    author  = 'hfpython',
+    author_email= 'hfpython@headfirstlabs.com',
+    url      = 'http://www.headfirstlabs.com',
+    escription   = 'A simple printer of nested lists',
+    )
