0c3084e933ba716c4ea5df29223cdab513703579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:28 +0000

    Add package twip/0.0.7/twip-0.0.7.tar.gz

---
+++ packages/twip/twip-0.0.7.tar.gz/twip-0.0.7/twip/settings_secret.py
@@ -0,0 +1,19 @@
+# 1. copy this file to settings_secret.py
+# 2. make sure the settings_secret.py file is ignored (should be listed in .gitignore)
+# 3. add your TWITTER secrets to this file (see below) or set your environment variables to contain them
+# 4. twyth = Twython(settings_secret.TWITTER_API_KEY, settings_secret.TWITTER_API_SECRET)
+
+# TWITTER_API_KEY and _SECRET are required to download tweets
+try:
+    TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
+    TWITTER_API_SECRET = os.getenv('TWITTER_API_SECRET')
+except:
+    TWITTER_API_KEY = '25AlphaNumericCharactersX'
+    TWITTER_API_SECRET = '50AlphaNumericCharactersX50AlphaNumericCharactersX'
+
+# Optional account identifying information that doesn't need to be protected
+TWITTER_API_OWNER = 'hobsonlane'  # the app owner's @username
+TWITTER_API_OWNER_ID = 59275999   # see the app
+
+# This is how to use these keys to instantiate a Twython connection to the twitter API
+# twyth = Twython(TWITTER_API_KEY, TWITTER_API_SECRET)
