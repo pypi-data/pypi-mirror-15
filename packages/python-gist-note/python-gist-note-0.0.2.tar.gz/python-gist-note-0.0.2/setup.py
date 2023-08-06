b92ef4632a120f5b625ca2353b8fa849b3fed27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:20 +0000

    Add package python-gist-note/0.0.2/python-gist-note-0.0.2.tar.gz

---
+++ packages/python-gist-note/python-gist-note-0.0.2.tar.gz/python-gist-note-0.0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-gist-note',
+    version='0.0.2',
+    keywords=('note', 'gist', 'gist-note', 'gn'),
+    description='just a simple gist note',
+    license='MIT License',
+    install_requires=['requests'],
+
+    author='Samuel Tong',
+    author_email='tonghuashuai@gmail.com',
+
+    packages=find_packages(),
+    platforms='any',
+)
