f1f26521634acaa1636765a66c68c88a49a21140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:25 +0000

    Add package django-fastsettings/0.2/django-fastsettings-0.2.tar.gz

---
+++ packages/django-fastsettings/django-fastsettings-0.2.tar.gz/django-fastsettings-0.2/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-fastsettings',
+    version='0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache 2.0',
+    description='A simple Django app to store/access project wide persistent settings.',
+    long_description=README,
+    url='https://github.com/wickeym/django-fastsettings',
+    author='Michael Wickey',
+    author_email='wickeym@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
