394cceb8e176edb9977d31b97a4c1e0ad66be69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:08 +0000

    Add package panddas/0.1.0/panddas-0.1.0.tar.gz

---
+++ packages/panddas/panddas-0.1.0.tar.gz/panddas-0.1.0/lib-python/pandda/__init__.py
@@ -0,0 +1,45 @@
+import os, sys
+
+PANDDA_LIB_TOP = os.path.realpath(os.path.abspath(os.path.join(__path__[0], '..')))
+PANDDA_VERSION = 1.0
+PANDDA_TEXT = """
+------------------------------------------------------------------>
+-
+-  Pandda Version {!s}
+-
+-  __________                    .___  .___
+-  \\______   \\_____    ____    __| _/__| _/____    ______
+-   |     ___/\\__  \\  /    \\  / __ |/ __ |\\__  \\  /  ___/
+-   |    |     / __ \\|   |  \\/ /_/ / /_/ | / __ \\_\\___ \\
+-   |____|    (____  /___|  /\\____ \\____ |(____  /____  >
+-                  \\/     \\/      \\/    \\/     \\/     \\/
+-
+-  PAN-Dataset Density Analysis of Crystallographic Fragment Screens
+-
+-  When using this software, please cite:
+-
+-       Unpublished,
+-       N Pearce, et al.
+-
+-  and include "PANDDA" in the KEYWRD section of related PDB files
+-
+------------------------------------------------------------------>
+""".format(PANDDA_VERSION)
+
+def welcome(user=None):
+    """Welcome message"""
+
+    if user is None:
+        try:    user = os.getlogin()
+        except: user = "Unknown user"
+
+    try:
+        from greetings import get_greeting
+        print get_greeting(username=user)
+    except SystemExit: raise
+    except: pass
+
+    try:
+        print 'Hi {!s}. Welcome to Pandda.'.format(user.upper())
+    except: pass
+
