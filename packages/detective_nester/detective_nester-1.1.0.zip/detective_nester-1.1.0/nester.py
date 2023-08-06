e19bbf36cce14965032befa0d73cd6564e869a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:46 +0000

    Add package detective_nester/1.1.0/detective_nester-1.1.0.zip

---
+++ packages/detective_nester/detective_nester-1.1.0.zip/detective_nester-1.1.0/nester.py
@@ -0,0 +1,10 @@
+﻿"""这一模块的作用是打印列表，其中可能包含嵌套列表"""
+def print_lol(the_list,level):
+    """这一函数接受参数，可以是任何python列表，函数会将列表的每个数据项递归输出到屏幕，每个数据项占一行"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
---
+++ packages/detective_nester/detective_nester-1.1.0.zip/detective_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+﻿from distutils.core import setup
+
+setup(
+        name        ='detective_nester',
+        version     ='1.1.0',
+        py_modules  =['nester'],
+        author      ='hfpython',
+        author_email='hfpython@headfirstlabs.com',
+        description ='A simple printer of nested lists',
+)
