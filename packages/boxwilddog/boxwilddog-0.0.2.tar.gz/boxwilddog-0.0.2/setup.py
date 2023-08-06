0bf1fd6608099ec65aac39e940fd94d665859e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:08 +0000

    Add package boxwilddog/0.0.2/boxwilddog-0.0.2.tar.gz

---
+++ packages/boxwilddog/boxwilddog-0.0.2.tar.gz/boxwilddog-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'boxwilddog',
+    version = '0.0.2',
+    keywords = ('wilddog', 'json', 'db', 'value'),
+    description = 'wilddog tool',
+    license = 'MIT License',
+    author = 'CcdjhMarx',
+    author_email = 'ccdjh.marx@all.com',
+    packages = ['boxwilddog'],
+)
