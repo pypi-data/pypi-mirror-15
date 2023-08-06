686136e4b90f00f6b8dec268fb09efddd97cb4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:27 +0000

    Add package ansible-bundle/0.2.2/ansible-bundle-0.2.2.tar.gz

---
+++ packages/ansible-bundle/ansible-bundle-0.2.2.tar.gz/ansible-bundle-0.2.2/ansible_bundle/scm.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# -*- encoding: utf8 -*-
+
+import shell
+
+
+class Git:
+    url = None
+    path = None
+    branch = None
+
+    def __init__(self, url, path='.', branch='master'):
+        self.url = url
+        self.path = path
+        self.branch = branch
+
+    def get(self):
+        cmd = ['git', 'clone', '--branch', self.branch,
+               '--depth', '1', self.url, self.path]
+        stdout, rc = shell.run(cmd, show=False)
+        if rc == shell.OK:
+            return True
+        else:
+            return False
+
+    def update(self):
+        shell.cd(self.path)
+        cmd = ['git', 'pull', 'origin', self.branch]
+        stdout, rc = shell.run(cmd)
+        shell.cd(shell.WORKDIR)
+        if rc == shell.OK:
+            return True
+        else:
+            return False
