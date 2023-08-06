58d6349dad5d1abeb99be4c342d6752bbd10057a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:00 +0000

    Add package mimir-cli/0.0.7/mimir-cli-0.0.7.zip

---
+++ packages/mimir-cli/mimir-cli-0.0.7.zip/mimir-cli-0.0.7/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""setup file for mimir cli"""
+from setuptools import setup, find_packages
+from mimir.main import __version__
+PROJECT = 'mimir-cli'
+VERSION = __version__
+setup(
+    name=PROJECT,
+    version=VERSION,
+    description='mimir cli application',
+    long_description='mimir cli application',
+    author='Jacobi Petrucciani',
+    author_email='jacobi@mimirhq.com',
+    url='',
+    download_url='',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.4',
+                 'Intended Audience :: Developers',
+                 'Environment :: Console',
+                ],
+    platforms=['Any'],
+    scripts=[],
+    provides=[],
+    setup_requires=[
+        'cliff>=2.0.0'
+    ],
+    install_requires=[
+        'requests>=2.10.0'
+    ],
+    namespace_packages=[],
+    packages=find_packages(),
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'mimir = mimir.main:main'
+        ],
+        'mimir.cli': [
+            'complete = mimir.main:Complete',
+            'version = mimir.main:Version',
+            'submit = mimir.main:Submit',
+            'login = mimir.main:Login',
+            'logout = mimir.main:Logout',
+        ],
+    },
+    zip_safe=False,
+)
