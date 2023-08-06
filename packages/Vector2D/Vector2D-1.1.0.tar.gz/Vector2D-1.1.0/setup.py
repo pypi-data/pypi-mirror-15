78343083a5cc4a22977f3ac74c0374fb01a202ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:12 +0000

    Add package Vector2D/1.1.0/Vector2D-1.1.0.tar.gz

---
+++ packages/Vector2D/Vector2D-1.1.0.tar.gz/Vector2D-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'Vector2D',
+    version = '1.1.0',
+    py_modules = ['Vector2D'],
+    author = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://www.headfirstpython.com',
+    description = 'A simple vector\s math for games',
+    )
