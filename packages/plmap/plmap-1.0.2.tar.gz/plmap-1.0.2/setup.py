fde8665aeafea175977eccb956d957d0494341fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:19 +0000

    Add package plmap/1.0.2/plmap-1.0.2.tar.gz

---
+++ packages/plmap/plmap-1.0.2.tar.gz/plmap-1.0.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='plmap',
+      version='1.0.2',
+      description='Parallel map using multi-threading and multi-processing',
+      author='Lohith B R',
+      packages=['plmap'],
+      license = "MIT",
+      keywords = "parallelization plmap plmapp plmapt pmapp multi-threading multi-processing parallel map",
+      url = "https://github.com/brlohith/parallelization",
+     )
