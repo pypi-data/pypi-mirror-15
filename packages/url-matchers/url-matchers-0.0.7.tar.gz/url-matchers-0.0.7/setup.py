020bad50c619f1d654b39fc3101912398040331d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:14 +0000

    Add package url-matchers/0.0.7/url-matchers-0.0.7.tar.gz

---
+++ packages/url-matchers/url-matchers-0.0.7.tar.gz/url-matchers-0.0.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from os import path
+from pip.req import parse_requirements
+
+setup(
+    name='url-matchers',
+    description="Url matchers for PyHamcrest",
+    version='0.0.7',
+    py_modules=['url_matchers'],
+    install_requires = [
+        "PyHamcrest>=1.8.0",
+        "six>=1.7.3",
+    ],
+    author="Alex Good",
+    author_email="alex@makerlabs.co.uk",
+    url="https://github.com/alexjg/url-matchers",
+)
