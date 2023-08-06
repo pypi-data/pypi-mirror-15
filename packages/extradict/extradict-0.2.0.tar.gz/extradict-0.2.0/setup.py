0af135ad34c5e9738bd5a37c790bf8ec31409728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:15 +0000

    Add package extradict/0.2.0/extradict-0.2.0.tar.gz

---
+++ packages/extradict/extradict-0.2.0.tar.gz/extradict-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name = 'extradict',
+    packages = ['extradict'],
+    version = "0.2.0",
+    license = "LGPLv3+",
+    author = "João S. O. Bueno",
+    author_email = "gwidion@gmail.com",
+    description = "Enhanced, maybe useful, data containers. For now, a versioned dict and an ordered versioned dict",
+    keywords = "versioned bijective assigner getter unpack transactional container collection dict dictionary",
+    py_modules = ['extradict'],
+    url = 'https://github.com/jsbueno/extradict',
+    long_description = open('README.md').read(),
+    test_requites = ['pytest'],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ]
+)
