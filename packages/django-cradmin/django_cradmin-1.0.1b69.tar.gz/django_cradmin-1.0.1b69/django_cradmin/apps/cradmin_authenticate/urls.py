61c97b7e4e9c7142d1d7e995435fd7a2779b00ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:02 +0000

    Add package django-cradmin/1.0.1b69/django_cradmin-1.0.1b69.tar.gz

---
+++ packages/django-cradmin/django_cradmin-1.0.1b69.tar.gz/django_cradmin-1.0.1b69/django_cradmin/apps/cradmin_authenticate/urls.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+from django.conf.urls import patterns, url
+from django_cradmin.apps.cradmin_authenticate.views.login import LoginView
+
+
+urlpatterns = patterns(
+    'django_cradmin.apps.cradmin_authenticate.views',
+    url(r'^login$', LoginView.as_view(), name='cradmin-authenticate-login'),
+    url(r'^logout$', 'logout.cradmin_logoutview', name='cradmin-authenticate-logout'),
+)
