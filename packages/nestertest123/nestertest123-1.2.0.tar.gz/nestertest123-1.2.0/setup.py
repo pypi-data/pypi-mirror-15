2c6f16e7e2b2272b34d6391264ca9ba9fe1e2542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:55 +0000

    Add package nestertest123/1.2.0/nestertest123-1.2.0.tar.gz

---
+++ packages/nestertest123/nestertest123-1.2.0.tar.gz/nestertest123-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name      	 ='nestertest123',
+		version 	 ='1.2.0',
+		py_modules   =['nestertest123'],
+		author  	 ='hfpython',
+		author_email ='ceshi@163.com',
+		url			 ='http://www.testerhome.com',
+		description  ='A simple printer of nested lists', 
+	)
