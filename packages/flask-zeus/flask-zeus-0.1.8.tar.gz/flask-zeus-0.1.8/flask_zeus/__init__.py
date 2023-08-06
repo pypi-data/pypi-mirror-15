581a2ffa99b1dd9cd6d889e6552b7ee2933f89de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:09 +0000

    Add package flask-zeus/0.1.8/flask-zeus-0.1.8.tar.gz

---
+++ packages/flask-zeus/flask-zeus-0.1.8.tar.gz/flask-zeus-0.1.8/flask_zeus/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.8"
+from .wraper.application import create_app
+from .wraper.blueprint import Blueprint
+from .wraper.form import Form
+from .wraper.celery import Celery
---
+++ packages/flask-zeus/flask-zeus-0.1.8.tar.gz/flask-zeus-0.1.8/flask_zeus/model/mixin.py
@@ -0,0 +1,4 @@
+from .crud import CRUDMixin
+from .delete import DeletedMixin
+from .account import AccountMixin
+from .entry import (EntryMixin, MarshalLabelMixin)
