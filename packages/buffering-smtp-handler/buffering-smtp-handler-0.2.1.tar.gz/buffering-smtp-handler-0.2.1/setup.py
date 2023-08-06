9eabe236bc63c3ef731cd1be0fa1ca7206071917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:33 +0000

    Add package buffering-smtp-handler/0.2.1/buffering-smtp-handler-0.2.1.tar.gz

---
+++ packages/buffering-smtp-handler/buffering-smtp-handler-0.2.1.tar.gz/buffering-smtp-handler-0.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(name='buffering-smtp-handler',
+      version='0.2.1',
+      py_modules=['buffering_smtp_handler'],
+      url='https://github.com/mike-charles/python-buffering-smtp-handler',
+      author='Mike Charles',
+      author_email='mike.e.charles@gmail.com',
+      description='Enables sending multiple logging events in a single email',
+      classifiers=["Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 3",
+                   "Intended Audience :: Developers",
+                   "Topic :: Software Development :: Libraries",
+                   "Topic :: Utilities",
+                   ],
+      )
