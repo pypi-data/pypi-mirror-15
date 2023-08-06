6a9f92d32b54b1cd74d2020002091529462146a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:09 +0000

    Add package empath/0.9/empath-0.9.tar.gz

---
+++ packages/empath/empath-0.9.tar.gz/empath-0.9/test/test.py
@@ -0,0 +1,7 @@
+from context import Empath
+
+empath = Empath()
+
+print(empath.analyze(["this","that"]))
+print(empath.analyze(["this","that"]))
+print(empath.analyze("the person typed on the keyboard"))
