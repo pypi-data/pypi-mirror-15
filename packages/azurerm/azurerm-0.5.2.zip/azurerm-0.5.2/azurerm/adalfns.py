a259bc8aaa237d1f091daf0297c053c4a5c6e70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:48 +0000

    Add package azurerm/0.5.2/azurerm-0.5.2.zip

---
+++ packages/azurerm/azurerm-0.5.2.zip/azurerm-0.5.2/azurerm/adalfns.py
@@ -0,0 +1,22 @@
+
+"""
+Copyright (c) 2016, Guy Bowerman
+Description: Azure Resource Manager Python library
+License: MIT (see LICENSE.txt file for details)
+"""
+
+# adalfns - place to store azurerm functions which call adal routines
+
+import adal
+
+authentication_endpoint = 'https://login.microsoftonline.com/'
+
+# get_access_token(tenant_id, application_id, application_secret)
+# get an Azure access token using the adal library
+def get_access_token(tenant_id, application_id, application_secret):
+    token_response = adal.acquire_token_with_client_credentials(
+        authentication_endpoint + tenant_id,
+        application_id,
+        application_secret
+    )
+    return token_response.get('accessToken')
