60a1aaf107e1d90c472a0acff57b9cd101ad25fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:53 +0000

    Add package songsong/1.3.0/songsong-1.3.0.zip

---
+++ packages/songsong/songsong-1.3.0.zip/songsong-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'songsong',
+    version = '1.3.0',
+    py_modules = ['songsong'],
+    author = 'songsong',
+    author_email = 'jb3690@naver.com',
+    url = 'http://mm.sookmyung.ac.kr/~m1410198',
+    description = 'A simple printer of nested lists',
+    )
