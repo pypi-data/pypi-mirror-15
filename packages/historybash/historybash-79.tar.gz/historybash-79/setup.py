f1b2458de610c579b2bd2df19f0e59c45e0af592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:44 +0000

    Add package historybash/79/historybash-79.tar.gz

---
+++ packages/historybash/historybash-79.tar.gz/historybash-79/setup.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+"""
+appinstance
+--
+Active8 (04-03-15)
+author: erik@a8.nl
+"""
+from setuptools import setup
+setup(name='historybash',
+      version='79',
+      description='Bash history command colorized on levenshtein',
+      url='https://github.com/erikdejonge/historybash',
+      author='Erik de Jonge',
+      author_email='erik@a8.nl',
+      license='GPL',
+      entry_points={
+          'console_scripts': [
+              'hist=historybash:main',
+          ],
+      },
+      packages=['historybash'],
+      zip_safe=True,
+      install_requires=['sh', 'arguments', 'python-Levenshtein', 'docopt', 'ujson', 'consoleprinter', 'future', 'pygments', 'pyyaml'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+          "Operating System :: POSIX",
+          "Environment :: MacOS X",
+          "Topic :: System",
+      ])
