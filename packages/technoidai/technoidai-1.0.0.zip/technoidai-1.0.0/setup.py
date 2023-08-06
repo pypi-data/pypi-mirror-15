4e8be1ce8abfb7ad1286f39b453c8b23a18111b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:22 +0000

    Add package technoidai/1.0.0/technoidai-1.0.0.zip

---
+++ packages/technoidai/technoidai-1.0.0.zip/technoidai-1.0.0/setup.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+from distutils.core import setup
+setup(
+    name = "technoidai",
+    version = "1.0.0",
+    py_modules = ['bidhu'],
+    author = 'bidhu',
+    author_email = 'technoidaicommunity@gmail.com',
+    url = 'http://www.artificialintelligence.org',
+    description = 'a good thing, it is a good thing',
+    )
