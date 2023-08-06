a677d70523823135167b1f18039dc04ebd8f4c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:48 +0000

    Add package ims/2.0/ims-2.0.tar.gz

---
+++ packages/ims/ims-2.0.tar.gz/ims-2.0/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="ims",
+	version="2.0",
+	description="Instantly stream movies/ tv episodes",
+	url="http://iyask.me",
+	author="Yask Srivastava",
+	author_email="yask123@gmail.com",
+	license='MIT',
+	packages=["ims"],
+	scripts=["bin/ims"],
+	zip_safe=False)
