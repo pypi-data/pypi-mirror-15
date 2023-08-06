06873eb94429621f23fcc61b75bb14912303c56f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:01 +0000

    Add package eemeter/0.3.20/eemeter-0.3.20-py2.py3-none-any.whl

---
+++ packages/eemeter/eemeter-0.3.20-py2.py3-none-any.whl/eemeter/__init__.py
@@ -0,0 +1,16 @@
+# Version
+VERSION = (0, 3, 20)
+
+def get_version():
+    return '{}.{}.{}'.format(VERSION[0], VERSION[1], VERSION[2])
+
+# Units
+try:
+    from pint import UnitRegistry
+    ureg = UnitRegistry()
+    Q_ = ureg.Quantity
+except ImportError:
+    import warnings
+    warnings.warn("Skipping pint to get version for setuptools.")
+    ureg = None
+    Q_ = None
