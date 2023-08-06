9e819147e11b077c86527bb5615ae9dbac0677c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:39 +0000

    Add package batchcompute/2.0.6a15/batchcompute-2.0.6a15-py2.5.egg

---
+++ packages/batchcompute/batchcompute-2.0.6a15-py2.5.egg/batchcompute/utils/__init__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "RequestClient", "get_region", "str_md5", "utf8", "iget", "gmt_time",
+    "partial", "import_json", "add_metaclass", "add_region", "CamelCasedClass", 
+    "remap", "set_log_level", "get_logger", "ConfigError", "set_api_version",
+    "get_api_version", "get_all_region",
+]
+
+from .http import RequestClient
+from .canonicalization import CamelCasedClass, remap
+from .functions import (
+    get_region, str_md5, utf8, iget, gmt_time, partial, import_json,
+    add_metaclass, add_region, ConfigError, set_api_version, 
+    get_api_version, get_all_region,
+)
+from .constants import CN_QINGDAO, CN_SHENZHEN, CN_HANGZHOU
+from .log import set_log_level, get_logger
