b16a2d7c0eb0e46df297ec9b28ffeddd4633beae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:49 +0000

    Add package djangocms-page-meta/0.6b1/djangocms_page_meta-0.6b1-py2.py3-none-any.whl

---
+++ packages/djangocms-page-meta/djangocms_page_meta-0.6b1-py2.py3-none-any.whl/djangocms_page_meta/admin.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from cms.extensions import PageExtensionAdmin, TitleExtensionAdmin
+from django.conf import settings
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from .forms import TitleMetaAdminForm
+from .models import GenericMetaAttribute, PageMeta, TitleMeta
+
+
+class GenericAttributePageInline(admin.TabularInline):
+    model = GenericMetaAttribute
+    fields = ('page', 'attribute', 'name', 'value')
+
+
+class GenericAttributeTitleInline(admin.TabularInline):
+    model = GenericMetaAttribute
+    fields = ('title', 'attribute', 'name', 'value')
+
+
+class PageMetaAdmin(PageExtensionAdmin):
+    raw_id_fields = ('og_author',)
+    inlines = (GenericAttributePageInline,)
+    fieldsets = (
+        (None, {'fields': ('image',)}),
+        (_('OpenGraph'), {
+            'fields': (
+                'og_type', ('og_author', 'og_author_url', 'og_author_fbid'),
+                ('og_publisher', 'og_app_id', 'fb_pages')
+            ),
+            'classes': ('collapse',)
+        }),
+        (_('Twitter Cards'), {
+            'fields': ('twitter_type', 'twitter_author'),
+            'classes': ('collapse',)
+        }),
+        (_('Google+ Snippets'), {
+            'fields': ('gplus_type', 'gplus_author'),
+            'classes': ('collapse',)
+        }),
+    )
+
+    class Media:
+        css = {
+            'all': ('%sdjangocms_page_meta/css/%s' % (
+                settings.STATIC_URL, 'djangocms_page_meta_admin.css'),)
+        }
+
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+
+admin.site.register(PageMeta, PageMetaAdmin)
+
+
+class TitleMetaAdmin(TitleExtensionAdmin):
+    form = TitleMetaAdminForm
+    inlines = (GenericAttributeTitleInline,)
+
+    class Media:
+        css = {
+            'all': ('%sdjangocms_page_meta/css/%s' % (
+                settings.STATIC_URL, 'djangocms_page_meta_admin.css'),)
+        }
+
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+
+admin.site.register(TitleMeta, TitleMetaAdmin)
