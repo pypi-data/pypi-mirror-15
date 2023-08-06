d8974ca1b83d7c6f48f543e07cd551e240d8c6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:36 +0000

    Add package nester_j/1.2.0/nester_j-1.2.0.tar.gz

---
+++ packages/nester_j/nester_j-1.2.0.tar.gz/nester_j-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester_j',
+        version = '1.2.0',
+        py_modules = ['nester_j'],
+        author = 'hfpython',
+        author_email = 'jiahuawei@meicai.cn',
+        url = 'http://www.headfirstlabs,com',
+        description = 'A simple printer of nested lists',
+    )
+
