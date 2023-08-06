81b3c89e1fdb032f26a08266b014b92e06559bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:09 +0000

    Add package athletelist_cww/1.2.2/athletelist_cww-1.2.2.zip

---
+++ packages/athletelist_cww/athletelist_cww-1.2.2.zip/athletelist_cww-1.2.2/athletelist_cww.py
@@ -0,0 +1,12 @@
+class Athlete:
+    def sanitize(time_string):
+        return(time_string.replace('-','.').replace(':','.'))
+    
+    def __init_(self, a_name, a_dob = None, a_times = []):
+        self.name = a_name
+        self.dob = a_dob
+        self.times = a_times
+        
+    def top3(self):
+        return (sorted(set([sanitize(t) for t in self.times]))[0:3])
+    
---
+++ packages/athletelist_cww/athletelist_cww-1.2.2.zip/athletelist_cww-1.2.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name            = "athletelist_cww",
+    version         = "1.2.2",
+    py_modules      = ["athletelist_cww"],
+    author          = "David",
+    author_email    = "tdavidf@163.com",
+    url             = "http://www.headfirstlabs.com",
+    description     = "A class to order athlete times",
+    )
