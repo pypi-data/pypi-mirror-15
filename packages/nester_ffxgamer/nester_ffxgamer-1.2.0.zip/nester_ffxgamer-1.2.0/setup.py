f4fb6219ff536bb1a640128d26f915c3ae20cb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:42 +0000

    Add package nester_ffxgamer/1.2.0/nester_ffxgamer-1.2.0.zip

---
+++ packages/nester_ffxgamer/nester_ffxgamer-1.2.0.zip/nester_ffxgamer-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester_ffxgamer',
+        version         = '1.2.0',
+        py_modules      = ['nester'],
+        author          = 'ffxgamer',
+        author_email    = 'ffxgamer@163.com',
+        url             = 'http://mail.ffxgamer.com',
+        description     = 'A simple print',
+        
+        )
