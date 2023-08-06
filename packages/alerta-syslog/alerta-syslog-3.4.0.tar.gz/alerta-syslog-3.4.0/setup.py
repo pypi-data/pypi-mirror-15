0fdf8fe291ed47a15bd7817072166d2005359e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:12 +0000

    Add package alerta-syslog/3.4.0/alerta-syslog-3.4.0.tar.gz

---
+++ packages/alerta-syslog/alerta-syslog-3.4.0.tar.gz/alerta-syslog-3.4.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import setuptools
+
+version = '3.4.0'
+
+setuptools.setup(
+    name="alerta-syslog",
+    version=version,
+    description='Alerta script for Syslog messages',
+    url='https://github.com/alerta/alerta-contrib',
+    license='Apache License 2.0',
+    author='Nick Satterly',
+    author_email='nick.satterly@theguardian.com',
+    py_modules=['syslog', 'transform', 'settings'],
+    install_requires=[
+        'alerta'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'alerta-syslog = syslog:main'
+        ]
+    },
+    keywords="alerta syslog monitoring",
+    classifiers=[
+        'Topic :: System :: Monitoring',
+    ]
+)
