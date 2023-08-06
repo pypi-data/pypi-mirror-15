50d6e3d8916f9725feb735eb2599501e7bf464fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:38 +0000

    Add package django-wechat-member/0.1.3/django-wechat-member-0.1.3.tar.gz

---
+++ packages/django-wechat-member/django-wechat-member-0.1.3.tar.gz/django-wechat-member-0.1.3/wechat_member/apps.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8 :
+from django.apps import AppConfig
+
+class WechatMemberConfig(AppConfig):
+    name = u'wechat_member'
+    verbose_name = u'微信会员'
