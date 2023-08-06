78fe15c1a094d2df8221738c1c5120fd90713cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:49 +0000

    Add package simple_wilson35/1.0.0/simple_wilson35-1.0.0.zip

---
+++ packages/simple_wilson35/simple_wilson35-1.0.0.zip/simple_wilson35-1.0.0/config.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'simple_wilson35',
+	version = '1.0.0',
+	py_modules = ['simple_wilson35'],
+	author = 'qiao',
+	author_email = 'hfpython@headfirstlabs.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists',
+)
---
+++ packages/simple_wilson35/simple_wilson35-1.0.0.zip/simple_wilson35-1.0.0/simple_wilson35.py
@@ -0,0 +1,3 @@
+def printStr(pyt):
+	for p in pyt:
+		print(p)
