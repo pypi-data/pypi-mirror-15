a7e37e1ae20aa44770e25893741f79d91a0a1844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:15 +0000

    Add package SnowNasaPython/1.2/SnowNasaPython-1.2.tar.gz

---
+++ packages/SnowNasaPython/SnowNasaPython-1.2.tar.gz/SnowNasaPython-1.2/SnowNasaPython/main.py
@@ -0,0 +1,9 @@
+import sys
+from apod import Apod
+from SnowNasaPython import SnowNasaPython
+
+if __name__=="__main__":
+  api_key = raw_input("Please enter your Nasa API key here: ")
+  s = SnowNasaPython()
+  s.get_pic(api_key)
+  
