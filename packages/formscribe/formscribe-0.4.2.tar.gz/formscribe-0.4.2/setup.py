227833c9896928d423eea2a736924aaa0ec5dc52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:16 +0000

    Add package formscribe/0.4.2/formscribe-0.4.2.tar.gz

---
+++ packages/formscribe/formscribe-0.4.2.tar.gz/formscribe-0.4.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='formscribe',
+    packages=['formscribe'],
+    version='0.4.2',
+    description='A practical form validation and submitting library',
+    author='Martin Jungblut Schreiner',
+    author_email='martinjungblut@gmail.com',
+    url='https://github.com/martinjungblut/formscribe',
+    download_url='https://github.com/martinjungblut/formscribe/archive/v0.4.2.zip',
+    keywords=['form', 'validation'],
+    license='MIT'
+)
