431d6d997e6b80c4f4b1d854a0dd03cd941012bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:53 +0000

    Add package eegevt/0.2.1/eegevt-0.2.1.tar.gz

---
+++ packages/eegevt/eegevt-0.2.1.tar.gz/eegevt-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open('README.md') as rfile:
+    long_description = rfile.read()
+
+setup(name='eegevt',
+      version='0.2.1',
+      description='Module for working with various EEG event file formats',
+      long_description=long_description,
+      url='https://github.com/gjcooper/eegevt',
+      author='Gavin Cooper',
+      author_email='gjcooper@gmail.com',
+      license='GPL v2.0',
+      packages=['eegevt'],
+      keywords=['EEG', 'event'])
