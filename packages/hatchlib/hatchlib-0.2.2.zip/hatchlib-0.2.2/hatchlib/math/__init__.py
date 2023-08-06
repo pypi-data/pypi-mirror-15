4e196e8e7e9d8c6e567cb11b687b6184f25352aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:12 +0000

    Add package hatchlib/0.2.2/hatchlib-0.2.2.zip

---
+++ packages/hatchlib/hatchlib-0.2.2.zip/hatchlib-0.2.2/hatchlib/math/__init__.py
@@ -0,0 +1,16 @@
+from random import randint
+from math import pi,e,sqrt
+
+def lcm(a,b):
+	t=a
+	while t%b!=0: t+=a
+	return t
+
+def isprime(n):
+	if n==1: return False
+
+	for i in range(3):
+		rn=randint(2,n)-1
+		if pow(rn,n-1,n)!=1: return False
+
+	return True
