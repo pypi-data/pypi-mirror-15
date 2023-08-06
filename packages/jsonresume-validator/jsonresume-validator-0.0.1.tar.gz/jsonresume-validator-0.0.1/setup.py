bd7120bfb65e1cacc75c426dc80e12ff8b35b05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:25 +0000

    Add package jsonresume-validator/0.0.1/jsonresume-validator-0.0.1.tar.gz

---
+++ packages/jsonresume-validator/jsonresume-validator-0.0.1.tar.gz/jsonresume-validator-0.0.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    with open(filename, 'r') as infile:
+        return infile.read()
+
+requires = [
+    "colander==1.2"
+]
+
+extras_require = {
+    "test": [
+        "nose",
+        "coverage",
+        "flake8",
+    ]
+}
+
+setup(name='jsonresume-validator',
+      version='0.0.1',
+      description='validates JSON resumes',
+      long_description=read_file('README.md'),
+      author='Kelvin Tay',
+      author_email='kelvintay@gmail.com',
+      url='https://github.com/kelvintaywl/jsonresume-validator',
+      download_url='https://github.com/kelvintaywl/jsonresume-validator/tarball/0.0.1',
+      keywords='jsonresume, validation, colander',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='jsonresume',
+      install_requires=requires,
+      extras_require=extras_require)
