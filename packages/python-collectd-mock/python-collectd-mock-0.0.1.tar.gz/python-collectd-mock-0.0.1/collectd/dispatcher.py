de134f632ead9ef7e1967ee7fb822ccc10b1ea7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:44 +0000

    Add package python-collectd-mock/0.0.1/python-collectd-mock-0.0.1.tar.gz

---
+++ packages/python-collectd-mock/python-collectd-mock-0.0.1.tar.gz/python-collectd-mock-0.0.1/collectd/dispatcher.py
@@ -0,0 +1,64 @@
+
+
+__all__ = ("Values", )
+
+
+class Data(object):
+    def __init__(
+            self,
+            plugin_instance=None,
+            meta=None,
+            plugin=None,
+            host=None,
+            type=None,
+            type_instance=None,
+            dsnames=None,
+            dstypes=None,
+            interval=None,
+            time=None,
+            values=None,
+    ):
+        self.plugin = plugin
+        self.plugin_instance = plugin_instance,
+        self.meta = meta
+        self.host = host
+        self.type = type
+        self.type_instance = type_instance
+        self.dsnames = dsnames
+        self.dstypes = dstypes
+        self.values = values
+        self.interval = interval
+        self.time = time
+
+    def dispatch(self):
+        pass
+
+
+def mk_values(
+        plugin_instance=None,
+        meta=None,
+        plugin=None,
+        host=None,
+        type=None,
+        type_instance=None,
+        dsnames=None,
+        dstypes=None,
+        interval=None,
+        time=None,
+        values=None,
+):
+    return Data(
+        plugin_instance=plugin_instance,
+        meta=meta,
+        plugin=plugin,
+        host=host,
+        type=type,
+        type_instance=type_instance,
+        dsnames=dsnames,
+        dstypes=dstypes,
+        interval=interval,
+        time=time,
+        values=values,
+    )
+
+Values = mk_values
