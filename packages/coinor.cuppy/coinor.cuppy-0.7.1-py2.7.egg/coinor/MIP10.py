1a2109f0ce9ee2dda46eb4533bfa3f809107914a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:42 +0000

    Add package coinor.cuppy/0.7.1/coinor.cuppy-0.7.1-py2.7.egg

---
+++ packages/coinor.cuppy/coinor.cuppy-0.7.1-py2.7.egg/coinor/MIP10.py
@@ -0,0 +1,16 @@
+numVars = 2
+numCons = 2
+points = None
+rays = []
+A = [[ -2,     1],
+     [ 2,    1],
+     [0,   -1]]
+b = [0,
+     2,
+     0
+    ]
+
+c = [0, -1]
+sense = ('Min', '<=')
+integerIndices = [0]
+
