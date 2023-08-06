fde6bdc2977eabdee887126ce627080ac099636a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:14 +0000

    Add package sssh/0.5/sssh-0.5.tar.gz

---
+++ packages/sssh/sssh-0.5.tar.gz/sssh-0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'sssh',
+    packages = ['sssh'],
+    entry_points={
+        "console_scripts": ['sssh = sssh.ssher:main']
+    },
+    version = '0.5',
+    description = 'Stop remembering all the hosts when using SSH',
+    author = 'Konpat Preechakul',
+    author_email = 'the.akita.ta@gmail.com',
+    url = 'https://github.com/phizaz/ssher', # use the URL to the github repo
+    download_url = 'https://github.com/phizaz/ssher/tarball/0.5', # I'll explain this in a second
+    keywords = ['ssh'], # arbitrary keywords
+    classifiers = [],
+)
