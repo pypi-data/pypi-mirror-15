f25b5b9ac5fe6f781f00361cf3b1ace06b504a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:40 +0000

    Add package neo4j-doc-manager/0.1.2/neo4j-doc-manager-0.1.2.tar.gz

---
+++ packages/neo4j-doc-manager/neo4j-doc-manager-0.1.2.tar.gz/neo4j-doc-manager-0.1.2/mongo_connector/doc_managers/neo4j_doc_manager.py
@@ -0,0 +1,140 @@
+"""
+Neo4j implementation for the DocManager. Receives documents and 
+communicates with Neo4j Server.
+"""
+import base64
+import logging
+import os
+import os.path as path, sys
+
+import bson.json_util
+
+from mongo_connector.doc_managers.nodes_and_relationships_builder import NodesAndRelationshipsBuilder
+from mongo_connector.doc_managers.nodes_and_relationships_updater import NodesAndRelationshipsUpdater
+from mongo_connector.doc_managers.error_handler import ErrorHandler
+
+from py2neo import Graph, authenticate
+
+
+from mongo_connector import errors
+from mongo_connector.compat import u
+from mongo_connector.constants import (DEFAULT_COMMIT_INTERVAL,
+                                       DEFAULT_MAX_BULK)
+from mongo_connector.util import exception_wrapper, retry_until_ok
+from mongo_connector.doc_managers.doc_manager_base import DocManagerBase
+from mongo_connector.doc_managers.formatters import DefaultDocumentFormatter
+
+errors_handler = ErrorHandler()
+wrap_exceptions = exception_wrapper(errors_handler.error_hash)
+
+LOG = logging.getLogger(__name__)
+
+class DocManager(DocManagerBase):
+  """
+  Neo4j implementation for the DocManager. Receives documents and 
+  communicates with Neo4j Server.
+  """
+
+  def __init__(self, url, auto_commit_interval=DEFAULT_COMMIT_INTERVAL,
+                 unique_key='_id', chunk_size=DEFAULT_MAX_BULK, **kwargs):
+    
+    self.graph = Graph(url)
+    self.auto_commit_interval = auto_commit_interval
+    self.unique_key = unique_key
+    self.chunk_size = chunk_size
+    self._formatter = DefaultDocumentFormatter()
+    self.kwargs = kwargs.get("clientOptions")
+
+  def apply_id_constraint(self, doc_types):
+    for doc_type in doc_types:
+      constraint = "CREATE CONSTRAINT ON (d:`{doc_type}`) ASSERT d._id IS UNIQUE".format(doc_type=doc_type)
+      self.graph.cypher.execute(constraint)
+
+  def stop(self):
+    """Stop the auto-commit thread."""
+    self.auto_commit_interval = None
+  
+  @wrap_exceptions
+  def upsert(self, doc, namespace, timestamp):
+    """Inserts a document into Neo4j."""
+    index, doc_type = self._index_and_mapping(namespace)
+    doc_id = u(doc.pop("_id"))
+    metadata = { "_ts": timestamp }
+    doc = self._formatter.format_document(doc)
+    builder = NodesAndRelationshipsBuilder(doc, doc_type, doc_id, metadata)
+    self.apply_id_constraint(builder.doc_types)
+    tx = self.graph.cypher.begin()
+    for statement in builder.query_nodes.keys():
+      tx.append(statement, builder.query_nodes[statement])
+    for relationship in builder.relationships_query.keys():
+      tx.append(relationship, builder.relationships_query[relationship])
+    tx.commit()
+
+  @wrap_exceptions
+  def bulk_upsert(self, docs, namespace, timestamp):
+    """Insert multiple documents into Neo4j."""
+    """Maximum chunk size is 1000. Transaction blocks won't have more than 1000 statements."""
+    metadata = { "_ts": timestamp }
+    tx = self.graph.cypher.begin()
+    for doc in docs:
+      index, doc_type = self._index_and_mapping(namespace)
+      doc_id = u(doc.pop("_id"))
+      doc = self._formatter.format_document(doc)
+      builder = NodesAndRelationshipsBuilder(doc, doc_type, doc_id, metadata)
+      self.apply_id_constraint(builder.doc_types)
+      for statement in builder.query_nodes.keys():
+        tx.append(statement, builder.query_nodes[statement])
+      for relationship in builder.relationships_query.keys():
+        tx.append(relationship, builder.relationships_query[relationship])
+    tx.commit()
+
+  @wrap_exceptions
+  def update(self, document_id, update_spec, namespace, timestamp):
+    doc_id = u(document_id)
+    tx = self.graph.cypher.begin()
+    index, doc_type = self._index_and_mapping(namespace)
+    updater = NodesAndRelationshipsUpdater()
+    updater.run_update(update_spec, doc_id, doc_type)
+    for statement in updater.statements_with_params:
+      for key in statement.keys():
+        tx.append(key, statement[key])
+    tx.commit()
+
+  @wrap_exceptions
+  def remove(self, document_id, namespace, timestamp):
+    """Removes a document from Neo4j."""
+    doc_id = u(document_id)
+    index, doc_type = self._index_and_mapping(namespace)
+    params_dict = {"doc_id": doc_id}
+    tx = self.graph.cypher.begin()
+    statement = "MATCH (d:Document) WHERE d._id={doc_id} OPTIONAL MATCH (d)-[r]-() DELETE d, r"
+    tx.append(statement, params_dict)
+    tx.commit()
+
+  @wrap_exceptions
+  def search(self, start_ts, end_ts):
+    statement = "MATCH (d:Document) WHERE d._ts>={start_ts} AND d._ts<={end_ts} RETURN d".format(start_ts=start_ts, end_ts=end_ts)
+    results = self.graph.cypher.execute(statement)
+    return results
+
+
+  def commit(self):
+    LOG.error("Commit")
+    
+
+  @wrap_exceptions
+  def get_last_doc(self):
+    """Get the most recently modified node from Neo4j.
+    This method is used to help define a time window within which documents
+    may be in conflict after a MongoDB rollback.
+    """
+    LOG.error("Commit")    
+
+    
+  def handle_command(self, doc, namespace, timestamp):
+    db = namespace.split('.', 1)[0]
+
+  def _index_and_mapping(self, namespace):
+    """Helper method for getting the index and type from a namespace."""
+    index, doc_type = namespace.split('.', 1)
+    return index.lower(), doc_type
---
+++ packages/neo4j-doc-manager/neo4j-doc-manager-0.1.2.tar.gz/neo4j-doc-manager-0.1.2/mongo_connector/doc_managers/nodes_and_relationships_builder.py
@@ -0,0 +1,97 @@
+"""
+Class that builds Nodes and Relationships
+according to a Mongo document.
+"""
+import re
+import logging
+
+LOG = logging.getLogger(__name__)
+
+class NodesAndRelationshipsBuilder(object):
+  def __init__(self, doc, doc_type, doc_id, metadata={}, doc_types=[]):
+    self.doc_id = doc_id
+    self.query_nodes = {}
+    self.relationships_query = {}
+    self.metadata = metadata or {}
+    self.doc_types = doc_types or []
+    self.explicit_ids = {}
+    self.build_nodes_query(doc_type, doc, doc_id)
+    
+  def build_nodes_query(self, doc_type, document, id):
+    self.doc_types.append(doc_type)
+    parameters = {'_id':id}
+    if self.is_dict(self.metadata):
+      parameters.update(self.metadata) 
+    for key in document.keys():
+      if self.is_reference(key):
+        self.build_node_with_reference(doc_type, key, id, document[key])
+        continue
+      if document[key] is None:
+        continue
+      elif self.is_dict(document[key]):
+        self.build_relationships_query(doc_type, key, id, id)
+        self.build_nodes_query(key, document[key], id)
+      elif self.is_json_array(document[key]):
+        for json in self.format_params(document[key]):
+          json_key = key + str(document[key].index(json))
+          self.build_relationships_query(doc_type, json_key, id, id)
+          self.build_nodes_query(json_key, json, id)
+      elif self.is_multimensional_array(document[key]):
+        parameters.update(self.flatenned_property(key, document[key]))
+      else:
+        parameters.update({ key: self.format_params(document[key]) })
+    query = "CREATE (c:Document:`{doc_type}` {{parameters}})".format(doc_type=doc_type)
+    self.query_nodes.update({query: {"parameters":parameters}})
+
+  def format_params(self, params):
+    if (type(params) is list):
+      return list(filter(None, params))
+    return params
+
+  def build_node_with_reference(self, root_type, key, doc_id, document_key):
+    if document_key is None:
+      return
+    doc_type = key.split("_id")[0]
+
+    # ignore _id property of subdocuments
+    if not doc_type or doc_type == "":
+      return
+
+    parameters = {'_id':document_key}
+    statement = "MERGE (d:Document:`{doc_type}` {{ _id: {{parameters}}._id}})".format(doc_type=doc_type)
+    self.query_nodes.update({statement: {"parameters":parameters}})
+    self.build_relationships_query(root_type, doc_type, doc_id, document_key)
+    self.explicit_ids.update({document_key: doc_type})
+
+  def is_dict(self, doc_key):
+    return (type(doc_key) is dict)
+
+  def is_reference(self, key):
+    return (re.search(r"_id$", key))
+
+  def is_multimensional_array(self, doc_key):
+    return ((type(doc_key) is list) and (doc_key) and (type(doc_key[0]) is list))
+
+  def flatenned_property(self, key, doc_key):
+    parameters = {}
+    flattened_list = doc_key
+    if ((type(flattened_list[0]) is list) and (flattened_list[0])):
+      inner_list = flattened_list[0]
+      if (type(inner_list[0]) is list):
+        flattened_list = [val for sublist in flattened_list for val in sublist]
+        self.flatenned_property(key, flattened_list)
+      else: 
+        for element in flattened_list:
+          element_key = key + str(flattened_list.index(element))
+          parameters.update({ element_key: element })
+    return parameters
+
+  def is_json_array(self, doc_key):
+    return ((type(doc_key) is list) and (doc_key) and (type(doc_key[0]) is dict))
+
+
+  def build_relationships_query(self, main_type, node_type, doc_id, explicit_id):
+    relationship_type = main_type + "_" + node_type
+    statement = "MATCH (a:`{main_type}`), (b:`{node_type}`) WHERE a._id={{doc_id}} AND b._id ={{explicit_id}} CREATE (a)-[r:`{relationship_type}`]->(b)".format(main_type=main_type, node_type=node_type, relationship_type=relationship_type)
+    params = {"doc_id": doc_id, "explicit_id": explicit_id}
+    self.relationships_query.update({statement: params})
