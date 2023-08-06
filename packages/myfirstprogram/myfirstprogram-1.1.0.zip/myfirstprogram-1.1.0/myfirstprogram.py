608856857c930d077b18600fb3dce79c8ec3579f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:09 +0000

    Add package myfirstprogram/1.1.0/myfirstprogram-1.1.0.zip

---
+++ packages/myfirstprogram/myfirstprogram-1.1.0.zip/myfirstprogram-1.1.0/myfirstprogram.py
@@ -0,0 +1,3 @@
+def print123(i) :
+    print(123123132)
+    print(123456789)
---
+++ packages/myfirstprogram/myfirstprogram-1.1.0.zip/myfirstprogram-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='myfirstprogram',
+    version='1.1.0',
+    py_modules=['myfirstprogram'],
+    author='program',
+    author_email='wangyunpeng_bio@qq.com',
+    url='http://www.baidu.com',
+    description='A simple program',
+    )
