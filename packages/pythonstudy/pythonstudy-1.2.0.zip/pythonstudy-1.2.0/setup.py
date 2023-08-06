aa44516c7e8bd4455fb3620cc0e92f8615e7cb70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:25 +0000

    Add package pythonstudy/1.2.0/pythonstudy-1.2.0.zip

---
+++ packages/pythonstudy/pythonstudy-1.2.0.zip/pythonstudy-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name='pythonstudy',
+		version='1.2.0',
+		py_modules=['pythonstudy'],
+		author='liushtong',
+		author_email='liushitong0315@163.com',
+		url='http://www.163.com',
+		description='A simple printer of nested lists',
+	)
