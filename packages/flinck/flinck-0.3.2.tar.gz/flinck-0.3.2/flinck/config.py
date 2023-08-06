8932d5e5809f5701488038568a42d91baf10de65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:43 +0000

    Add package flinck/0.3.2/flinck-0.3.2.tar.gz

---
+++ packages/flinck/flinck-0.3.2.tar.gz/flinck-0.3.2/flinck/config.py
@@ -0,0 +1,14 @@
+from . import confit
+
+config = confit.LazyConfig('flinck', __name__)
+config.add({
+    'file_extensions': ['avi', 'm4v', 'mkv', 'mp4'],
+    'file_min_size_mb': 20,
+    'google_api_key': '',
+    'link_root_dir': '',
+    'verbose': False,
+})
+
+FIELDS = ('country', 'director', 'year', 'decade', 'genre', 'rating', 'runtime',
+    'title')
+DEFAULT_FIELDS = set(config.keys()).intersection(FIELDS)
