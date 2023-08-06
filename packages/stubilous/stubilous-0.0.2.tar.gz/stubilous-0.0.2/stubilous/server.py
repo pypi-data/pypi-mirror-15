f7f46fd6e515ff0df88676ec43a0b7d83ac3f2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:08 +0000

    Add package stubilous/0.0.2/stubilous-0.0.2.tar.gz

---
+++ packages/stubilous/stubilous-0.0.2.tar.gz/stubilous-0.0.2/stubilous/server.py
@@ -0,0 +1,41 @@
+from flask import Flask, make_response
+from stubilous.config import Config
+
+
+def create_app():
+    return Flask(__name__)
+
+
+def default_view(body, status=200, headers=None, *args, **kwargs):
+    response = make_response(body, status)
+    if headers:
+        response.headers = headers
+    return response
+
+
+def wrap(fn):
+    import uuid
+    fn.__name__ = str(uuid.uuid4())
+    return fn
+
+
+def init_routes(app, routes):
+    from functools import partial
+    for route in routes:
+        app.add_url_rule(route.path,
+                         methods=[route.method],
+                         view_func=wrap(partial(default_view,
+                                                route.body,
+                                                route.status,
+                                                route.headers)))
+
+
+def run(args):
+    import yaml
+    with open(args.config, "r") as f:
+        config = Config.from_dict(yaml.load(f))
+
+    app = create_app()
+    init_routes(app, config.routes)
+
+    app.run(config.host, config.port)
