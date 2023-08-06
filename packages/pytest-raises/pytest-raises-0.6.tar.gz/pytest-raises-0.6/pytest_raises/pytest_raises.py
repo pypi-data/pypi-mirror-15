b137677cd31e82a3594063f91110ce677fb503ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:26 +0000

    Add package pytest-raises/0.6/pytest-raises-0.6.tar.gz

---
+++ packages/pytest-raises/pytest-raises-0.6.tar.gz/pytest-raises-0.6/pytest_raises/pytest_raises.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import sys
+
+import pytest
+
+class ExpectedException(Exception):
+    pass
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_call(item):
+    outcome = yield
+    raises_marker = item.get_marker('raises')
+    if raises_marker:
+        exception = raises_marker.kwargs.get('exception')
+        exception = exception or Exception
+
+        raised_exception = outcome.excinfo[1] if outcome.excinfo else None
+        traceback = outcome.excinfo[2] if outcome.excinfo else None
+        if isinstance(raised_exception, exception):
+            outcome.force_result(None)
+        else:
+            try:
+                raise raised_exception or ExpectedException('Expected exception {}, but it did not raise'.format(exception))
+            except(ExpectedException):
+                excinfo = sys.exc_info()
+                if traceback:
+                    outcome.excinfo = excinfo[:2] + (traceback, )
+                else:
+                    outcome.excinfo = excinfo
