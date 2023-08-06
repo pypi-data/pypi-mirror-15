a2f898501a0b8d32dad880ddb2892a8134d21fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:07 +0000

    Add package gswww/0.1/gswww-0.1.zip

---
+++ packages/gswww/gswww-0.1.zip/gswww-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gswww',
+  packages = ['gswww'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Unknown',
+  author_email = 'avikalp22@gmail.com',
+  url = 'https://github.com/Avikalp7/Golden-Shoe', # use the URL to the github repo
+  download_url = 'https://github.com/Avikalp7/Golden-Shoe/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
