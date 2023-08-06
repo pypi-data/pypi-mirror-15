5411e987c7134e8984bf65e3b65bec787fe1f5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:28 +0000

    Add package ensembler/0.2/ensembler-0.2.zip

---
+++ packages/ensembler/ensembler-0.2.zip/ensembler-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ensembler',
+      version='0.2',
+      description='A python package for ensemling models via Stacking/Bleanding, bagging etc',
+      url='https://github.com/pankajr141/ensembler',
+      author='Pankaj Rawat',
+      author_email='pankajr141@gmail.com',
+      license='MIT',
+      packages=['ensembler'],
+      zip_safe=False)
