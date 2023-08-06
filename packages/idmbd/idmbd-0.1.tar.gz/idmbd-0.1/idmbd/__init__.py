b4ea27a515ee5b1759c1a7f61da9100ab5cce6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:50 +0000

    Add package idmbd/0.1/idmbd-0.1.tar.gz

---
+++ packages/idmbd/idmbd-0.1.tar.gz/idmbd-0.1/idmbd/__init__.py
@@ -0,0 +1,13 @@
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
+            
+            
+def a1():
+    return ('Algoritmo 1')
+    
+def a2():
+    return ('Algoritmo 2')
+    
+def a3(v1):
+    return ('Algoritmo 3 ', v1)
---
+++ packages/idmbd/idmbd-0.1.tar.gz/idmbd-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='idmbd',
+      version='0.1',
+      description='Innovando el Desarrollo de Modelos a traves del Big Data',
+      url='http://github.com/storborg/funniest',
+      author='Paola Lopez, Diego Martin, Carlos Paz',
+      author_email='cpazsantos@gmail.com',
+      packages=['idmbd'],
+      zip_safe=False)
