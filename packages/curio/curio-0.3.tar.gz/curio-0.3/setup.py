a8e007b12442df2f2fb0e57047c900ea4ca1da54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:26 +0000

    Add package curio/0.3/curio-0.3.tar.gz

---
+++ packages/curio/curio-0.3.tar.gz/curio-0.3/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name = "curio",
+            description="Curio - Concurrent I/O",
+            long_description = """
+Curio is a library for performing concurrent I/O with coroutines in Python 3. 
+""",
+            license="""BSD""",
+            version = "0.3",
+            author = "David Beazley",
+            author_email = "dave@dabeaz.com",
+            maintainer = "David Beazley",
+            maintainer_email = "dave@dabeaz.com",
+            url = "https://github.com/dabeaz/curio",
+            packages = ['curio'],
+            classifiers = [
+              'Programming Language :: Python :: 3',
+              ]
+            )
