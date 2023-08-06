9262c3d24c5deb40cfd75bd01da31424c3866ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:31 +0000

    Add package straitlets/0.2.7/straitlets-0.2.7.tar.gz

---
+++ packages/straitlets/straitlets-0.2.7.tar.gz/straitlets-0.2.7/straitlets/__init__.py
@@ -0,0 +1,29 @@
+from .serializable import Serializable
+from .traits import (
+    Bool,
+    Dict,
+    Enum,
+    Float,
+    Instance,
+    Integer,
+    LengthBoundedUnicode,
+    List,
+    Set,
+    Tuple,
+    Unicode,
+)
+
+__all__ = (
+    'Bool',
+    'Dict',
+    'Enum',
+    'Float',
+    'Instance',
+    'Integer',
+    'LengthBoundedUnicode',
+    'List',
+    'Set',
+    'Tuple',
+    'Unicode',
+    'Serializable',
+)
