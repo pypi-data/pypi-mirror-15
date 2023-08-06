b4c85ceeffcfd01a2a80877d3aa3a693a679469d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:27 +0000

    Add package pyseries/1.0.15/pyseries-1.0.15-py2.py3-none-any.whl

---
+++ packages/pyseries/pyseries-1.0.15-py2.py3-none-any.whl/pyseries/Analysis/__init__.py
@@ -0,0 +1,9 @@
+"""Default init. Carry on.
+
+.. moduleauthor:: Ryszard Cetnarski <cetnarski.ryszard@gmail.com.com>
+
+"""
+import Anova as anova
+import CrossCorrelate as crossCorrelate 
+import Normalize as normalize
+import Trends as trends
---
+++ packages/pyseries/pyseries-1.0.15-py2.py3-none-any.whl/pyseries/LoadingData/__init__.py
@@ -0,0 +1,8 @@
+"""Default init. Carry on.
+
+.. moduleauthor:: Ryszard Cetnarski <cetnarski.ryszard@gmail.com.com>
+
+"""
+
+import Read_edf as read_edf
+import ReadData as read_mat
