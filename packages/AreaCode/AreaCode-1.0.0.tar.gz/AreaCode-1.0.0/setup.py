ae3e79ea493953d79bfe8cf48d6559bf32993364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:21 +0000

    Add package AreaCode/1.0.0/AreaCode-1.0.0.tar.gz

---
+++ packages/AreaCode/AreaCode-1.0.0.tar.gz/AreaCode-1.0.0/setup.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name = "AreaCode",
+      version="1.0.0",
+      keywords=("china", "area", "code"),
+      description="China area code list",
+      license="MIT License",
+
+      author="Alvin Ren",
+      author_email="renqingmail@126.com",
+
+      packages=find_packages(),
+      platforms="any",
+)
