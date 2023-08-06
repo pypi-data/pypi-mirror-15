f474d093e1f77c7d3be41046899f6b672ca931ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:55 +0000

    Add package rask/0.0.45/rask-0.0.45.tar.gz

---
+++ packages/rask/rask-0.0.45.tar.gz/rask-0.0.45/rask/raven/__init__.py
@@ -0,0 +1,40 @@
+from tornado.log import gen_log
+from tornado.process import task_id
+
+__all__ = ['Raven']
+
+class Raven(object):
+    __logger = None
+
+    @property
+    def logger(self):
+        try:
+            assert self.__logger
+        except AssertionError:
+            self.__logger = gen_log
+        except:
+            raise
+        return self.__logger
+
+    def critical(self,arg):
+        self.logger.critical(arg)
+        return True
+
+    def debug(self,arg):
+        self.logger.debug(arg)
+        return True
+
+    def error(self,arg):
+        self.logger.error(arg)
+        return True
+
+    def info(self,arg):
+        self.logger.info(arg)        
+        return True
+
+    def mark(self,_):
+        return '[%s]> %s' % (task_id(),_)
+    
+    def warning(self,arg):
+        self.logger.warning(arg)
+        return True
