2c080c0b14cf02ea511707cbd45f6e3354d1cea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:50 +0000

    Add package hellorap/0.1.2/hellorap-0.1.2.tar.gz

---
+++ packages/hellorap/hellorap-0.1.2.tar.gz/hellorap-0.1.2/hellorap/main.py
@@ -0,0 +1,16 @@
+print 'Starting'
+
+
+
+
+def initialize():
+    tot_arg =len(sys.argv)
+    if tot_arg !=2:
+        print 'Please provide only one command!!'
+        return
+    if sys.argv[1]=='create':
+        print 'Create Directory'
+        
+
+
+if __name__ == "__main__":initialize()
