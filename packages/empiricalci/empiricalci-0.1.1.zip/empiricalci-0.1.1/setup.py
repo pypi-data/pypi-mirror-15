876147ed3583dc4ba73ee8cc0087a56d0e38574d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:41 +0000

    Add package empiricalci/0.1.1/empiricalci-0.1.1.zip

---
+++ packages/empiricalci/empiricalci-0.1.1.zip/empiricalci-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+	name = 'empiricalci',
+	packages = ['empiricalci'],
+	version = '0.1.1',
+	install_requires = ['zerorpc', 'requests'],
+	description = 'Library for Empirical CI',
+	author = 'Alan Torres',
+	author_email = 'alan@empiricalci.com',
+	url = 'https://github.com/empiricalci/empiricalci',
+	download_url = 'https://github.com/empiricalci/empiricalci/tarball/0.1.1',
+	keywords = ['research', 'empiric', 'scientific'],
+	classifiers = []
+)
