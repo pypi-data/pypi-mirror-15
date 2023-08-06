6897b73647287d09c890ae120cd4f07f1a48d902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:12 +0000

    Add package xdjango/0.3.14/xdjango-0.3.14-py2.7.egg

---
+++ packages/xdjango/xdjango-0.3.14-py2.7.egg/xdjango/contrib/auth/validators.py
@@ -0,0 +1,20 @@
+from django.forms import Form, CharField, BooleanField, EmailField
+
+
+class LoginFormValidator(Form):
+    username = CharField()
+    password = CharField()
+
+
+class UserQueryValidator(Form):
+    send_email = BooleanField(required=False)
+
+
+class PasswordResetFormValidator(Form):
+    new_password = CharField()
+    token = CharField()
+    email = EmailField()
+
+
+class EmailFormValidator(Form):
+    email = EmailField()
