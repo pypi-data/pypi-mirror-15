e2c5b1799834a86ff11584dad79997ddc83a348d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:50 +0000

    Add package slackbottools/0.3/slackbottools-0.3.zip

---
+++ packages/slackbottools/slackbottools-0.3.zip/slackbottools-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='slackbottools',
+      version='0.3',
+      description='A python package built for writing SlackBots',
+      url='https://github.com/coderplans/SlackBot',
+      author='coderplans',
+      author_email='thomas@khandaan.org',
+      license='Apache 2.0',
+      packages=['slackbottools'],
+      zip_safe=False)
