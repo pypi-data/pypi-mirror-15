aaa116a41b5c1f971d2b333a72b3c8211423128a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:59 +0000

    Add package django-mock-queries/0.0.13/django_mock_queries-0.0.13.tar.gz

---
+++ packages/django-mock-queries/django_mock_queries-0.0.13.tar.gz/django_mock_queries-0.0.13/django_mock_queries/patches.py
@@ -0,0 +1,11 @@
+# from mock import patch
+#
+#
+# def patches(*objects):
+#     def decorator(func):
+#         def wrapper(*args, **kwargs):
+#             return func(*args, **kwargs)
+#
+#         return wrapper
+#
+#     return decorator
