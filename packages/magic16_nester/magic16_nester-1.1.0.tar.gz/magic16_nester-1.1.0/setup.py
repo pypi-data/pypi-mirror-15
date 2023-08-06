88827d98ad661b99d0e6cf93418d7b95306aec5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:41 +0000

    Add package magic16_nester/1.1.0/magic16_nester-1.1.0.tar.gz

---
+++ packages/magic16_nester/magic16_nester-1.1.0.tar.gz/magic16_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'magic16_nester',
+	version		= '1.1.0',
+	py_modules	= ['magic16_nester'],
+	author		= 'xiaobo',
+	author_email	= 'yuxiaobohit@gmail.com',
+	url		= 'http://www.xiaobo16psu.com',
+	description	= 'A simple printer of nested lists'
+)
