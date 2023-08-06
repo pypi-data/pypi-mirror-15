d37baaa151e7413fdde68610c055499c680e8a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:53 +0000

    Add package supra/1.1.2/supra-1.1.2.tar.gz

---
+++ packages/supra/supra-1.1.2.tar.gz/supra-1.1.2/supra/auths/oauth/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+"""
+	@name: OAthToken
+	@author: exile.sas
+	@date: 27/06/2016
+	@licence: creative commons
+"""
+class OAthToken(models.Model):
+	date = models.DateTimeField(auto_now_add=True)
+	token = models.CharField(max_length=120)
+# end class
