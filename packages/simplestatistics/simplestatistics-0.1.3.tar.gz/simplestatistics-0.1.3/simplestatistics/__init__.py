c1e1487710a4f2ab5e853310dd07efcbda8acfed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:02 +0000

    Add package simplestatistics/0.1.3/simplestatistics-0.1.3.tar.gz

---
+++ packages/simplestatistics/simplestatistics-0.1.3.tar.gz/simplestatistics-0.1.3/simplestatistics/__init__.py
@@ -0,0 +1,23 @@
+from .statistics.mean import mean
+from .statistics.median import median
+from .statistics.mode import mode
+
+from .statistics.geometric_mean import geometric_mean
+
+from .statistics.min import min
+from .statistics.max import max
+
+from .statistics.sum import sum
+
+from .statistics.quantile import quantile
+
+from .statistics.variance import variance
+from .statistics.standard_deviation import standard_deviation
+
+from .statistics.z_scores import z_scores
+
+from .statistics.product import product
+
+from .statistics.correlate import correlate
+
+from .statistics.t_test import t_test
