83e7ecad587b1e9c9714a826c534cc954c10747a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:44 +0000

    Add package myprintlist/1.0.0/myprintlist-1.0.0.tar.gz

---
+++ packages/myprintlist/myprintlist-1.0.0.tar.gz/myprintlist-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+   name='myprintlist',
+   version='1.0.0',
+   py_modules=['myprintlist'],
+   author='quanlong.huangql',
+   author_email='quanlong.huangql@alibaba-inc.com',
+)
