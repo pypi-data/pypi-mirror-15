5c605601a07399121bfc27f54b2a35f75b41a21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:58 +0000

    Add package m3u8-generator/1.1/m3u8-generator-1.1.tar.gz

---
+++ packages/m3u8-generator/m3u8-generator-1.1.tar.gz/m3u8-generator-1.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(name='m3u8-generator',
+                 version=1.1,
+                 description='m3u8 playlist generator',
+                 author='cambackup.com',
+                 author_email='info@cambackup.com',
+                 url='https://github.com/cambackup/m3u8-generator',
+                 py_modules=['m3u8_generator'],
+                 install_requires=[],
+                 license='MIT License',
+                 zip_safe=False,
+                 keywords='m3u8 generaror',
+                 )
