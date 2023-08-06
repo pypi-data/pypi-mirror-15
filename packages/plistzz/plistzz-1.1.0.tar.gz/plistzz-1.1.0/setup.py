19cd1d7ce0dca2a41ad110b6983884d392078427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:24 +0000

    Add package plistzz/1.1.0/plistzz-1.1.0.tar.gz

---
+++ packages/plistzz/plistzz-1.1.0.tar.gz/plistzz-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+
+	name = 'plistzz',
+	version = '1.1.0',
+	py_modules = ['plistzz'],
+	author = 'ezngzen',
+	author_email = 'showhat@163.com',
+	description = 'A simple test'
+)
