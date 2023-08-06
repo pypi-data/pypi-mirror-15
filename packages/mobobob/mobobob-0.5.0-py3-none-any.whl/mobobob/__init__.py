4504d3777593ca5594ccd742ca09b25a52ea414c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:27 +0000

    Add package mobobob/0.5.0/mobobob-0.5.0-py3-none-any.whl

---
+++ packages/mobobob/mobobob-0.5.0-py3-none-any.whl/mobobob/__init__.py
@@ -0,0 +1,12 @@
+"""
+An alias for matplotlib
+
+In memory of John Hunter, who I met for the first time at SciPy 2012, and
+passed recently after. I recently learned that his children were calling
+"mobmobmob"
+
+"""
+from matplotlib import *
+__version__ = '0.5.0'
+
+        
