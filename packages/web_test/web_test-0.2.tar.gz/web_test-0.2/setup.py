de9076c9c6ab8b5f881bb05da70e33c2b4ceddb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:43 +0000

    Add package web_test/0.2/web_test-0.2.tar.gz

---
+++ packages/web_test/web_test-0.2.tar.gz/web_test-0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='web_test',
+      version='0.2',
+      description='The funniest joke in the world',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='web_test joke comedy flying circus',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['web_test'],
+      install_requires=[
+          'markdown',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      entry_points={
+          'console_scripts': ['web_test-joke=web_test.command_line:main'],
+      },
+      include_package_data=True,
+      zip_safe=False)
