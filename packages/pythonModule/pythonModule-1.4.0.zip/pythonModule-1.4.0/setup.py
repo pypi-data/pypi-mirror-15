c3de97b921aa0c967a727ff697bb1a54a2326d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:09 +0000

    Add package pythonModule/1.4.0/pythonModule-1.4.0.zip

---
+++ packages/pythonModule/pythonModule-1.4.0.zip/pythonModule-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'pythonModule', 
+	version = '1.4.0', 
+	py_modules = ['printList'], 
+	author = 'ZhangGuo', 
+	author_email = '2451666365@qq.com', 
+	url = 'http://www.headfirstlabs.com', 
+	description = 'A simple printer of nested lists', 
+	)
