58a4d211798dcfb214a7442298cb3d39037fe08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:42 +0000

    Add package ansible-bundle/0.2.7/ansible-bundle-0.2.7.tar.gz

---
+++ packages/ansible-bundle/ansible-bundle-0.2.7.tar.gz/ansible-bundle-0.2.7/ansible_bundle/main.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python2
+# -*- encoding: utf8 -*-
+#
+
+import argparse
+import shell
+from bundle import Bundle, PATH
+
+DEFAULT_VERBOSITY=0
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', dest='filename', default='site.yml',
+                        help='YML file to be processed (default:site.yml)')
+    parser.add_argument('--run', action='store_true', dest='run_playbook',
+                        default=False,
+                        help='Runs ansible-playbook with default params' +
+                        ' after getting bundles')
+    parser.add_argument('--args', dest='args', nargs='?',
+                        help='ansible-playbook arguments, if needed. Must be' +
+                        ' put into quotes')
+    parser.add_argument('--clean', action='store_true', default=False,
+                        help='clean roles and libraries directories')
+    parser.add_argument('-v', '--verbose', action='count', default=VERBOSITY,
+                        help='Be verbose on tasks')
+
+    return parser.parse_args()
+
+
+def get_dependencies_tree(pathDir):
+    deps = list()
+    for (directory, _, filenames) in shell.walk(pathDir):
+        for filename in filenames:
+            path = shell.path(directory, filename)
+            if 'meta/main.yml' in path:
+                deps += get_bundles_from(shell.load(path))
+    return list(set(deps))
+
+
+def get_bundles_from(yml):
+    bundles = list()
+    for bundle in yml.get('roles', list()):
+        b = Bundle('role', bundle)
+        bundles.append(b)
+    for bundle in yml.get('dependencies', list()):
+        if bundle.get('role', None):
+            b = Bundle('role', bundle)
+            bundles.append(b)
+    return bundles
+
+
+def load_bundles(filename):
+    if filename is None: return list()
+    if not shell.isfile(filename):
+        raise Exception('File %s not found' % filename)
+    bundlelist=list()
+    yml = shell.load(filename)
+    for item in yml:
+        bundlelist += load_bundles(item.get('include', None)) +\
+                      get_bundles_from(item)
+    return bundlelist
+
+def purify(array):
+    elements = list()
+    pure = list()
+    for item in array:
+        element = (item.name, item.version)
+        if element not in elements:
+            pure.append(item)
+            elements.append(element)
+    return pure
+
+
+def download(filename, verbose=DEFAULT_VERBOSITY):
+    bundles = purify(load_bundles(filename))
+    already_downloaded = list()
+    while len(bundles) > 0:
+        for bundle in bundles:
+            bundles.remove(bundle)
+            if bundle.download(verbose=verbose) is True:
+                already_downloaded.append((bundle.name, bundle.version))
+            else:
+                return shell.ERROR
+        dependencies = get_dependencies_tree(
+            PATH['role']) + get_dependencies_tree(PATH['library'])
+        bundles = [item for item in bundles if (item.name, item.version) not in already_downloaded] + \
+            [item for item in dependencies if (item.name, item.version) not in already_downloaded]
+
+
+def run_playbook(filename, args):
+        ansiblecmd = ['ansible-playbook', filename] + args
+        shell.echo('Running', typeOf='info', lr=False)
+        shell.echo(shell.Color.text(' '.join(ansiblecmd), decoration='bold'))
+        shell.call(ansiblecmd)
+
+
+def clean_dirs(directories):
+    for directory in directories:
+        shell.echo('Cleaning %s directory...' %directory,
+                   typeOf='info', lr=False)
+        if shell.rmdir('roles') is True:
+            shell.echo('OK', typeOf='ok')
+        else:
+            shell.echo('ERROR', typeOf='error')
+
+
+
+def main():
+    global VERBOSITY
+    params = get_arguments()
+
+    if params.clean is True:
+        clean_dirs(['roles'])
+
+    download(
+            filename=params.filename, 
+            verbose=params.verbose
+            )
+
+    # After getting bundles, run playbook if set to
+    if params.run_playbook is True:
+        run_playbook(params.filename,
+                     params.args.split() if params.args else list()
+                    )
+
+########################################################
+if __name__ == "__main__":
+    shell.exit(main())
---
+++ packages/ansible-bundle/ansible-bundle-0.2.7.tar.gz/ansible-bundle-0.2.7/ansible_bundle/shell.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python2
+# -*- encoding: utf8 -*-
+#
+
+from __future__ import print_function
+import os
+import sys
+from subprocess import Popen, PIPE
+from subprocess import call as Call
+import yaml
+import shutil
+
+# Color + decoration for messages printed on terminal
+MESSAGES = {
+    'none': (None, None),
+    'ok': ('green', None),
+    'info': ('blue', None),
+    'warning': ('yellow', None),
+    'error': ('red', 'bold'),
+}
+
+
+class Color:
+    COLORS = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        'yellow': '\033[93m',
+        'blue': '\033[94m',
+    }
+
+    DECORATIONS = {
+        'bold': '\033[1m',
+        'underline': '\033[4m',
+    }
+
+    END = '\033[0m'
+
+    @classmethod
+    def text(cls, s, **kwargs):
+        msg = ''
+        color = kwargs.get('color', None)
+        decoration = kwargs.get('decoration', None)
+        if decoration is not None:
+            msg += cls.DECORATIONS[decoration]
+        if color is not None:
+            msg += cls.COLORS[color]
+        msg += s
+        if color or decoration:
+            msg += cls.END
+        return (msg)
+
+
+class Defaults:
+    SCM = 'git'
+    SCM_VERSION = 'master'
+    SCM_PREFIX = ''
+    SCM_ROLES = ''
+    SCM_MODULES = ''
+    VERBOSITY = 0
+
+    def __init__(self):
+        yml = self.load()
+        if yml is not '':
+            self.setvalues(yml)
+        else:
+            raise Exception('%s not found or incorrect.' % path)
+
+    def load(self):
+        LOAD_ORDER = (
+            path(pwd(), 'bundle.cfg'),
+            path(HOME, '.ansible', 'bundle', 'bundle.cfg')
+        )
+        for filename in LOAD_ORDER:
+            if isfile(filename):
+                return load(filename)
+
+    def setvalues(self, yml):
+        for key, value in yml.items():
+            setattr(self, key, value)
+        if self.SCM_PREFIX:
+            self.SCM_ROLES = self.SCM_PREFIX + self.SCM_ROLES
+            self.SCM_MODULES = self.SCM_PREFIX + self.SCM_MODULES
+
+
+def load(filename):
+    if os.path.isfile(filename):
+        with open(filename, 'r') as fn:
+            return yaml.load(fn)
+
+
+def echo(message, lr=True, typeOf=None):
+    if lr:
+        end = '\n'
+    else:
+        end = ' '
+    if typeOf:
+        color, decoration = MESSAGES[typeOf]
+        msg = Color.text(message, color=color, decoration=decoration)
+    else:
+        msg = message
+    print(msg, end=end)
+    sys.stdout.flush()
+
+
+def run(command, verbose=0):
+    if verbose > 0:
+        print('\n[DEBUG IN]\n', ' '.join(command), '\n-------------------')
+    process = Popen(command, shell=False, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    output = stdout + stderr
+    returncode = process.returncode
+    if verbose > 0:
+        print ('\n[DEBUG OUT]\n', output, '\n-------------------')
+    return output, returncode
+
+
+def pwd():
+    return os.getcwd()
+
+
+def exit(arg):
+    return sys.exit(arg)
+
+
+def isfile(filename):
+    return os.path.isfile(filename)
+
+
+def isdir(dirname):
+    return os.path.exists(dirname)
+
+
+def path(*args):
+    return os.path.join(*args)
+
+
+def call(args):
+    return Call(args)
+
+
+def walk(args):
+    return os.walk(args)
+
+
+def cd(dirname):
+    return os.chdir(dirname)
+
+def rmdir(dirname):
+    if os.path.exists(dirname):
+        try:
+            shutil.rmtree(dirname)
+            return True
+        except:
+            return False
+    else:
+        return True
+
+
+###############################
+OK = 0
+ERROR = 1
+HOME = os.getenv('HOME')
+DEFAULTS = Defaults()
+WORKDIR = os.getcwd()
