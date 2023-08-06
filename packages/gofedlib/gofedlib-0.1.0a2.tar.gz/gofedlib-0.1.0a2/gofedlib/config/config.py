66c8e61e93df606782a9dc6921a7a0b0bcd6f23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:58 +0000

    Add package gofedlib/0.1.0a2/gofedlib-0.1.0a2.tar.gz

---
+++ packages/gofedlib/gofedlib-0.1.0a2.tar.gz/gofedlib-0.1.0a2/gofedlib/config/config.py
@@ -0,0 +1,22 @@
+import ConfigParser
+from gofedlib.utils import getScriptDir
+import os
+
+class Config(object):
+
+	def __init__(self, config_file):
+		config_etc = "/etc/gofed/%s" % config_file
+		config_default = "%s/%s" % (self._classDir(), config_file)
+
+		if os.path.isfile(config_etc):
+			self._parse(config_etc)
+		else:
+			self._parse(config_default)
+
+	def _classDir(self):
+		return getScriptDir(__file__)
+
+	def _parse(self, config_file):
+		self._config = ConfigParser.ConfigParser()
+		self._config.read(config_file)
+
---
+++ packages/gofedlib/gofedlib-0.1.0a2.tar.gz/gofedlib-0.1.0a2/gofedlib/go/snapshot.py
@@ -0,0 +1,69 @@
+import json
+
+class Snapshot(object):
+
+	def __init__(self):
+		self._packages = {}
+
+	def clear(self):
+		self._packages = {}
+		return self
+
+	def addPackage(self, package, commit):
+		self._packages[package] = commit
+		return self
+
+	def packages(self):
+		# TODO(jchaloup): introduce iterator instead
+		return self._packages
+
+	def Godeps(self):
+		"""Return the snapshot in Godeps.json form
+		"""
+		dict = []
+		for package in sorted(self._packages.keys()):
+			dict.append({
+				"ImportPath": str(package),
+				"Rev": str(self._packages[package])
+			})
+
+		return dict
+
+	def GLOGFILE(self):
+		"""Return the snapshot in GLOGFILE form
+		"""
+		lines = []
+		for package in sorted(self._packages.keys()):
+			lines.append("%s %s" % (str(package), str(self._packages[package])))
+
+		return "\n".join(lines)
+
+	def readGodepsFile(self, file):
+		with open(file, "r") as f:
+			data = json.load(f)
+
+		# Deps key?
+		if "Deps" not in data:
+			raise ValueError("Deps key missing in %s" % file)
+
+		packages = {}
+		for package in data["Deps"]:
+			if "ImportPath" not in package:
+				raise ValueError("ImportPath key missing in %s" % file)
+
+			if "Rev" not in package:
+				raise ValueError("Rev key missing in %s" % file)
+
+			packages[package["ImportPath"]] = package["Rev"]
+
+		# The file is valid Godeps.json file
+		self.clear()
+		self._packages = packages
+
+		return self
+
+	def readGLOGFILE(self, file):
+		raise NotImplementedError()
+		with open(file, "r") as f:
+			data = json.load(f)
+
