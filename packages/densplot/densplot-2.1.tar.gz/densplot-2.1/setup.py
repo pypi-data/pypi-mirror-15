54a3e236416afc9d39b980de4fda8d810761b091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:42 +0000

    Add package densplot/2.1/densplot-2.1.tar.gz

---
+++ packages/densplot/densplot-2.1.tar.gz/densplot-2.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='densplot',
+      version='2.1',
+      packages=['densplot'],
+      description=['Plotting free energy landscapes'],
+      scripts = ['bin/densplot'],
+      license='LICENSE.txt',
+      maintainer='Eugen Hruska',
+      maintainer_email='eh22@rice.edu',
+      url='https://github.com/ClementiGroup/DensityPlotMaker',
+      #long_description=open('README.md').read(),
+     )
