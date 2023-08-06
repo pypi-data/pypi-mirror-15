ff16e8a54c9fef463e545f85eef7bb2b4328ed9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:29 +0000

    Add package rypy/0.0.6/rypy-0.0.6.tar.gz

---
+++ packages/rypy/rypy-0.0.6.tar.gz/rypy-0.0.6/setup.py
@@ -0,0 +1,18 @@
+# python setup.py register -r pypitest
+# python setup.py sdist upload -r pypitest
+# python setup.py register -r pypi
+# python setup.py sdist upload -r pypi
+
+from distutils.core import setup
+setup(
+  name = 'rypy',
+  packages = ['rypy'],
+  version = '0.0.6',
+  description = 'RYan\'s PYthon tools',
+  author = 'Ryan Smith',
+  author_email = 'rms1000watt@gmail.com',
+  url = 'https://github.com/rms1000watt/rypy',
+  download_url = 'https://github.com/rms1000watt/rypy/tarball/0.1', 
+  keywords = ['logging', 'tools'], 
+  classifiers = [],
+)
