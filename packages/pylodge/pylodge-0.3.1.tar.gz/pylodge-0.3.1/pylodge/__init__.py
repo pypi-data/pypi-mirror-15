4760a87222f9246db622cdaa0e4eec64e85b2ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:52 +0000

    Add package pylodge/0.3.1/pylodge-0.3.1.tar.gz

---
+++ packages/pylodge/pylodge-0.3.1.tar.gz/pylodge-0.3.1/pylodge/__init__.py
@@ -0,0 +1,14 @@
+"""
+
+:copyright: (c) 2015 by Ashwin Kondapalli.
+:license: MIT, see license.txt for more details.
+
+"""
+
+__title__ = 'pylodge'
+__version__ = '0.3.0'
+__author__ = 'Ashwin Kondapalli'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Ashwin Kondapalli'
+
+__all__ = ['PyLodge']
