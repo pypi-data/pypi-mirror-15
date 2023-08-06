88a30da49011837af34f624f3f44a2d4ba103f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:30 +0000

    Add package infi.exceptools/0.2.11/infi.exceptools-0.2.11.tar.gz

---
+++ packages/infi.exceptools/infi.exceptools-0.2.11.tar.gz/infi.exceptools-0.2.11/src/infi/exceptools/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.11"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.2.11)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ffb857c14b698a59dcab5b1bce14a1a77414e8d3'
+__git_head_subject__ = 'Finished Release v0.2.11'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
