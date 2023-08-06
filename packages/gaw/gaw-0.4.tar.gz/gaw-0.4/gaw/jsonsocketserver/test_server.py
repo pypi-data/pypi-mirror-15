bffbb5802dc20f2494af03c0f454a16cd3af96b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:50 +0000

    Add package gaw/0.4/gaw-0.4.tar.gz

---
+++ packages/gaw/gaw-0.4.tar.gz/gaw-0.4/gaw/jsonsocketserver/test_server.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from gaw.jsonsocketserver.server import JsonSocketServer
+
+def home(path):
+    print('home!')
+    print('path:', path)
+    raise ValueError('test error')
+    return 'welcome home!'
+
+def plus(a, b, path):
+    print('plus!')
+    print('path:', path)
+    return a + b
+
+server = JsonSocketServer('0.0.0.0', 4444)
+server.register_route('home', home)
+server.register_route('plus', plus)
+server.start()
