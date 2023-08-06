4f68bbd1b2ff33131a31e3e397bf79b71f8322fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:08 +0000

    Add package cached/0.0.14/cached-0.0.14.tar.gz

---
+++ packages/cached/cached-0.0.14.tar.gz/cached-0.0.14/.setup/.packages.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from __init__ import REPO
+
+__all__ = ["packages", "package_dir", "package_data"]
+
+# distname.py/
+# distname.py/packages/
+# distname.py/packages/pkgname1/
+# distname.py/packages/pkgname2/
+
+
+def _packages(path):
+    listdir = os.listdir(path)
+    for l in listdir:
+        if os.path.exists(os.path.join(path, l, "__init__.py")):
+            yield l
+
+path = os.path.join(REPO, "packages")
+if os.path.exists(path) and os.path.isdir(path):
+    packages = list(_packages(path))
+    if packages:
+        package_dir = dict()
+        package_data = dict()
+        for package in packages:
+            package_dir[package] = "packages/%s" % package
+            package_data[package] = ['*']
+else:
+    if __name__ == "__main__":
+        print("%s/ NOT EXISTS" % path)
+
+if __name__ == "__main__":
+    for k in __all__:
+        if k in globals():
+            print("%s: %s" % (k, globals()[k]))
