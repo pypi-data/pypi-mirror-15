667134955768e37f06083c7c18c103b8ddd4259c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:12 +0000

    Add package px4tools/0.3.0/px4tools-0.3.0.tar.gz

---
+++ packages/px4tools/px4tools-0.3.0.tar.gz/px4tools-0.3.0/px4tools/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.3.0'
+version = '0.3.0'
+full_version = '0.3.0'
+git_revision = '143c11a7a5cb4347ac6d4bf065894187160175e0'
+release = True
+
+if not release:
+    version = full_version
