0f9c5f2acf6db176710adbff5bf003b4ee30ab84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:00 +0000

    Add package micropython-redis-cloudclient/0.0.10/micropython-redis-cloudclient-0.0.10.tar.gz

---
+++ packages/micropython-redis-cloudclient/micropython-redis-cloudclient-0.0.10.tar.gz/micropython-redis-cloudclient-0.0.10/redis_cloudclient/__init__.py
@@ -0,0 +1,17 @@
+from .eventloop import start
+
+
+__version__ = '0.0.10'
+
+
+def autostart():
+    """
+    Add startup code to main.py
+    """
+    code = """
+# Added by redis_cloudclient
+import redis_cloudclient
+redis_cloudclient.start()
+"""
+    with open('main.py', 'a') as fh:
+        fh.write(code)
