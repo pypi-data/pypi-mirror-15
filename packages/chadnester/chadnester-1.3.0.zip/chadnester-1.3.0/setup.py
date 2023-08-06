44f3cd457f809b7f070bfb7c92a3e114dae18776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:08 +0000

    Add package chadnester/1.3.0/chadnester-1.3.0.zip

---
+++ packages/chadnester/chadnester-1.3.0.zip/chadnester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name         = 'chadnester',
+       version      = '1.3.0',
+       py_modules   = ['chadnester'],
+       author       = 'ccpython',
+       author_email = 'chad03681@163.com',
+       url          = 'http://www.headfirstlabs.com',
+       description  = 'A simle printer of nested lists',
+     )
