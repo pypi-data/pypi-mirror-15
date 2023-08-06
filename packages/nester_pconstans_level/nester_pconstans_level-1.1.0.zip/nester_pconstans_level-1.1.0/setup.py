8e01649d87dc1ec4c9c896cecdebc46abe68032e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:05 +0000

    Add package nester_pconstans_level/1.1.0/nester_pconstans_level-1.1.0.zip

---
+++ packages/nester_pconstans_level/nester_pconstans_level-1.1.0.zip/nester_pconstans_level-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name         = 'nester_pconstans_level',
+        version      = '1.1.0',
+        py_modules   = ['nester'],
+        author       = 'Pierre CONSTANS',
+        author_email = 'constans.pierre@laposte.net',
+        description  = 'A simple printer of nested lists',
+    )
