31fae3c8c3d49306ff677aaa8c7042f1dcf107ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:47 +0000

    Add package nyuki/0.19.1/nyuki-0.19.1.tar.gz

---
+++ packages/nyuki/nyuki-0.19.1.tar.gz/nyuki-0.19.1/nyuki/workflow/tasks/__init__.py
@@ -0,0 +1,10 @@
+from .factory import FactoryTask
+from .report import ReportTask
+from .sleep import SleepTask
+
+
+# Generic schema to reference a task ID
+TASKID_SCHEMA = {
+    'type': 'string',
+    'description': 'task_id'
+}
