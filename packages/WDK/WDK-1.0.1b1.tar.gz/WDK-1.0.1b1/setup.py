95863b7d2ed0e497c41172446fd5d2d0097e55a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:15 +0000

    Add package WDK/1.0.1b1/WDK-1.0.1b1.tar.gz

---
+++ packages/WDK/WDK-1.0.1b1.tar.gz/WDK-1.0.1b1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='WDK',
+      version='1.0.1b1',
+      description='Python API for D-Link WDK',
+      author='Martin Sundhaug',
+      author_email='martinsundhaug@gmail.com',
+      url='https://github.com/sundhaug92/dlink-wdk',
+      py_modules=['wdk'],
+      license='MIT',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 2 :: Only',
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Security',
+          'Topic :: Software Development :: Embedded Systems',
+          'Topic :: Software Development :: Libraries'
+      ],
+      keywords='dlink dir100 wdk web cliget api'
+)
