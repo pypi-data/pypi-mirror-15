c355992927b89036366418b3ba13ad6b96820879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:54 +0000

    Add package micropython-bootconfig/0.0.14/micropython-bootconfig-0.0.14.tar.gz

---
+++ packages/micropython-bootconfig/micropython-bootconfig-0.0.14.tar.gz/micropython-bootconfig-0.0.14/bootconfig/init.py
@@ -0,0 +1,16 @@
+boot_operations = {
+    'webrepl': b"""import webrepl
+webrepl.start()
+"""
+}
+
+def create_boot_py(boot_operations=None):
+    with open("/boot.py", "w") as f:
+        f.write(b'# This file is executed on every boot (including wake-boot from deepsleep)\n\n')
+        f.write(b'from bootconfig.device import configuration\n')
+        f.write(b'configuration()\n')
+        f.write(b'del configuration\n')
+
+        if boot_operations:
+            for boot_operation in boot_operations:
+                f.write(boot_operations[boot_operation])
