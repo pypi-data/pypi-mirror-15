f55934f8a12f5e4ffe338c3a172e4988776d8fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:47 +0000

    Add package pyzk/0.3/pyzk-0.3.tar.gz

---
+++ packages/pyzk/pyzk-0.3.tar.gz/pyzk-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='pyzk',
+    version='0.3',
+    description='an unofficial library of zksoftware the fingerprint attendance machine.',
+    url='https://github.com/fananimi/pyzk',
+    author='Fanani M. Ihsan',
+    author_email='fanani.mi@gmail.com',
+    license='LICENSE.txt',
+    packages=['zk'],
+    keywords = [
+        'zksoftware',
+        'pyzk',
+        'zk',
+        'attendance machine',
+        'fingerprint',
+        'biometrics',
+        'security'
+    ],
+    zip_safe=False
+)
