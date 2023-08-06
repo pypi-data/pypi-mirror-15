e08f6a7c24ff427cb009da64e5ae07be504a502f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:10 +0000

    Add package APPLPy/0.4.2/APPLPy-0.4.2.tar.gz

---
+++ packages/APPLPy/APPLPy-0.4.2.tar.gz/APPLPy-0.4.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='APPLPy',
+    version='0.4.2',
+    author='Matthew Robinson',
+    author_email='mthw.wm.robinson@gmail.com',
+    packages=['applpy','applpy.test'],
+    scripts=[],
+    url='https://pypi.python.org/pypi/APPLPy/',
+    license='LICENSE.txt',
+    description='open source computational probability software',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "sympy",
+        "scipy",
+        "matplotlib",
+        "seaborn",
+        "numpy",
+        "pylab",
+        "mpmath",
+        "pandas"
+
+        ]
+    )
