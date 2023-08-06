f1e213ff7865ed86124a5ea3dc4e0635699b8562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:27 +0000

    Add package quip/0.1.7/quip-0.1.7.tar.gz

---
+++ packages/quip/quip-0.1.7.tar.gz/quip-0.1.7/setup.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import re
+from setuptools import setup, find_packages
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+requires = [
+    'Plim',
+    'tornado',
+]
+if sys.version_info[0] == 2:
+    requires.extend(['futures', 'pathlib2'])
+
+
+with open('quip/__init__.py') as fp:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fp.read(), re.MULTILINE).group(1)
+
+with open('README.rst') as fp:
+    readme = fp.read()
+
+
+setup(
+    name='quip',
+    version=version,
+    description='Quick user interfaces for Python',
+    long_description=readme,
+    author='Feihong Hsu',
+    author_email='feihong.hsu@gmail.com',
+    url='https://github.com/feihong/quip',
+    packages=find_packages(),
+    package_data={'': '*.html'},
+    install_requires=requires,
+    license='Apache 2.0',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
