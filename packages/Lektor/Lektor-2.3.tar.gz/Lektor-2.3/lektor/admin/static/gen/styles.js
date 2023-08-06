3d7a3ffab3eba3df53bbf532285e1ad25ab9bef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:12 +0000

    Add package Lektor/2.3/Lektor-2.3.tar.gz

---
+++ packages/Lektor/Lektor-2.3.tar.gz/Lektor-2.3/lektor/admin/static/gen/styles.js
@@ -0,0 +1,8 @@
+webpackJsonp([1],[
+/* 0 */
+/***/ function(module, exports) {
+
+	eval("// removed by extract-text-webpack-plugin\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2xlc3MvbWFpbi5sZXNzP2FkODEiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gcmVtb3ZlZCBieSBleHRyYWN0LXRleHQtd2VicGFjay1wbHVnaW5cblxuXG4vKioqKioqKioqKioqKioqKipcbiAqKiBXRUJQQUNLIEZPT1RFUlxuICoqIC4vbGVzcy9tYWluLmxlc3NcbiAqKiBtb2R1bGUgaWQgPSAwXG4gKiogbW9kdWxlIGNodW5rcyA9IDFcbiAqKi8iXSwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlUm9vdCI6IiJ9");
+
+/***/ }
+]);
---
+++ packages/Lektor/Lektor-2.3.tar.gz/Lektor-2.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Lektor',
+    version='2.3',
+    url='http://github.com/lektor/lektor/',
+    description='A static content management system.',
+    license='BSD',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Jinja2>=2.4',
+        'click>=6.0',
+        'watchdog',
+        'mistune',
+        'Flask',
+        'EXIFRead',
+        'inifile',
+        'Babel',
+        'setuptools',
+        'pip',
+        'requests[security]',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    entry_points='''
+        [console_scripts]
+        lektor=lektor.cli:main
+    '''
+)
