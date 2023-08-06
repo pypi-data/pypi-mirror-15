0813d2daff3e610391365e88ddaa0cf63600867d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:27 +0000

    Add package idmbd/0.2/idmbd-0.2.tar.gz

---
+++ packages/idmbd/idmbd-0.2.tar.gz/idmbd-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='idmbd',
+      version='0.2',
+      description='Innovando el Desarrollo de Modelos a traves del Big Data',
+      url='http://github.com/cpazsantos/ciffmbd2016pldmcp',
+      author='Paola Lopez, Diego Martin, Carlos Paz',
+      author_email='cpazsantos@gmail.com',
+      packages=['idmbd'],
+      zip_safe=False)
