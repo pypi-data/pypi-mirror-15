a8362e2c94ef9d8f487de95fb6330797c06ea5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:51 +0000

    Add package test-upload-pypi/3.0.1/test-upload-pypi-3.0.1.zip

---
+++ packages/test-upload-pypi/test-upload-pypi-3.0.1.zip/test-upload-pypi-3.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name = 'test-upload-pypi',
+      version = '3.0.1',
+      py_modules = ['test'],
+      author = 'nratnani',
+      author_email = 'nratnani@cimpress.com',
+      description = 'testing python distribution upload to pypi',
+      )
