a17f4ba660bd4f9bee6090a0844c16c97b65549d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:27 +0000

    Add package vexparser/0.0.6/vexparser-0.0.6.tar.gz

---
+++ packages/vexparser/vexparser-0.0.6.tar.gz/vexparser-0.0.6/vexparser/util.py
@@ -0,0 +1,32 @@
+import os as _os
+import nltk as _nltk
+import unicodedata
+import sys
+
+from nltk.corpus import stopwords as stopwords
+"""
+if not "stopwords" in _os.listdir(_nltk.data.find("corpora")):
+    _nltk.download("stopwords")
+"""
+
+_tbl = None
+_stopwords = None
+
+def _get_stopwords():
+    _tbl = dict.fromkeys(i for i in range(sys.maxunicode)
+                         if unicodedata.category(chr(i)).startswith('P'))
+
+    _stopwords = dict.fromkeys(s for s in set(stopwords.words('english')))
+
+
+def clean_text(str):
+    if _tbl is None:
+        _get_stopwords()
+
+    # get rid of the puncuation
+    str = str.translate(_tbl)
+    # get rid of the stopwords
+    str = str.translate(_stopwords)
+    # lowercase everything
+    str.lower()
+    return str
