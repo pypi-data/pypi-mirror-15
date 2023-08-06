9f53cd8734d15097e2f22b82f8845138edf2f163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:09 +0000

    Add package orgtup/1.1.0/orgtup-1.1.0.tar.gz

---
+++ packages/orgtup/orgtup-1.1.0.tar.gz/orgtup-1.1.0/orgtup/__init__.py
@@ -0,0 +1,24 @@
+"""Named tuples related to common services."""
+
+from collections import namedtuple
+
+GithubPersonalAccessToken = namedtuple(
+    'GithubPersonalAccessToken', (
+        'username',
+        'key',
+    )
+)
+
+AwsCredentials = namedtuple(
+    'AwsCredentials', (
+        'access_key_id',
+        'secret_access_key',
+    )
+)
+
+S3Path = namedtuple(
+    'S3Path', (
+        'bucket',
+        'key',
+    )
+)
