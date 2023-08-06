7657e13fce06721a6eb6dca91eef05f3737e308d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:58 +0000

    Add package wrenlab/0.1.1/wrenlab-0.1.1.tar.gz

---
+++ packages/wrenlab/wrenlab-0.1.1.tar.gz/wrenlab-0.1.1/wrenlab/signature.py
@@ -0,0 +1,16 @@
+"""
+Signature analysis:
+
+Problem
+=======
+
+Given a set of genes with numeric attributes (expression levels) and a
+"database" of genes with numeric attributes, signature analysis provides
+different methods of assessing the correlation between the two.
+
+A concrete example: given a set of gene fold changes and a database of genes
+that are positive and negative regulators of a certain pathway, is this pathway likely
+to be up or down-regulated in this experiment?
+"""
+
+
