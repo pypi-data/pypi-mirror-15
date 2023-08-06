bd0f0dc8912347c12755a2c6e6a8a665b353857e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:30 +0000

    Add package django-account-keeping/0.2.10/django-account-keeping-0.2.10.tar.gz

---
+++ packages/django-account-keeping/django-account-keeping-0.2.10.tar.gz/django-account-keeping-0.2.10/account_keeping/urls.py
@@ -0,0 +1,55 @@
+"""URLs for the account_keeping app."""
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+    url(r'transaction/(?P<pk>\d+)/$',
+        views.TransactionUpdateView.as_view(),
+        name='account_keeping_transaction_update'),
+
+    url(r'transaction/create(?:/(?P<invoice_pk>\w+))?/$',
+        views.TransactionCreateView.as_view(),
+        name='account_keeping_transaction_create'),
+
+    url(r'invoice/(?P<pk>\d+)/$',
+        views.InvoiceUpdateView.as_view(),
+        name='account_keeping_invoice_update'),
+
+    url(r'invoice/create/$',
+        views.InvoiceCreateView.as_view(),
+        name='account_keeping_invoice_create'),
+
+    url(r'accounts/$',
+        views.AccountListView.as_view(),
+        name='account_keeping_accounts'),
+
+    url(r'payees/$',
+        views.PayeeListView.as_view(),
+        name='account_keeping_payees'),
+
+    url(r'all/$',
+        views.AllTimeView.as_view(),
+        name='account_keeping_all'),
+
+    url(r'(?P<year>\d+)/(?P<month>\d+)/$',
+        views.MonthView.as_view(),
+        name='account_keeping_month'),
+
+    url(r'(?P<year>\d+)/$',
+        views.YearOverviewView.as_view(),
+        name='account_keeping_year'),
+
+    url(r'current-year/$',
+        views.CurrentYearRedirectView.as_view(),
+        name='account_keeping_current_year'),
+
+    url(r'current-month/$',
+        views.CurrentMonthRedirectView.as_view(),
+        name='account_keeping_current_month'),
+
+    url(r'$',
+        views.IndexView.as_view(),
+        name='account_keeping_index'),
+]
