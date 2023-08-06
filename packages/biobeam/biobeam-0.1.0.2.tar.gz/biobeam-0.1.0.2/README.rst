291c9cf52f03b280f9db3b0580fcb48d17fffdd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:28 +0000

    Add package biobeam/0.1.0.2/biobeam-0.1.0.2.tar.gz

---
+++ packages/biobeam/biobeam-0.1.0.2.tar.gz/biobeam-0.1.0.2/README.rst
@@ -0,0 +1,8 @@
+biobeam - GPU accelerated simulation of light interacting with tissue
+=====================================================================
+
+
+
+
+
+The full documentation with examples can be found `here <https://maweigert.github.io/biobeam/>`_.
---
+++ packages/biobeam/biobeam-0.1.0.2.tar.gz/biobeam-0.1.0.2/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name='biobeam',
+    version='0.1.0.2',
+    description='beam propagation',
+    url='https://maweigert.github.io/biobeam',
+    author='Martin Weigert',
+    author_email='mweigert@mpi-cbg.de',
+    license='MIT',
+    packages=find_packages(),
+
+    install_requires=[
+        'numpy',
+        'scipy',
+        "pyopencl>=2016.1",
+        'gputools',
+    ],
+
+    package_data={"biobeam":['bpm/kernels/*.cl',
+                             'focus_field/kernels/*cl'],},
+
+)
