127f76a1873f8789b2b67428f72c4d8e9436ff07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:52 +0000

    Add package abstraction/2016.6.1.1602/abstraction-2016.6.1.1602.tar.gz

---
+++ packages/abstraction/abstraction-2016.6.1.1602.tar.gz/abstraction-2016.6.1.1602/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+import pypandoc
+
+def main():
+
+    setuptools.setup(
+        name             = "abstraction",
+        version          = "2016.06.01.1602",
+        description      = "machine learning framework",
+        long_description = pypandoc.convert("README.md", "rst"),
+        url              = "https://github.com/wdbm/abstraction",
+        author           = "Will Breaden Madden",
+        author_email     = "w.bm@cern.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "abstraction"
+                           ],
+        install_requires = [
+                           "dataset",
+                           "datavision",
+                           "flask",
+                           "numpy",
+                           "matplotlib",
+                           "praw",
+                           "propyte",
+                           "pyprel",
+                           "skflow",
+                           "sklearn",
+                           "shijian",
+                           "technicolor"
+                           ],
+        scripts          = [
+                           "abstraction_interface.py"
+                           ],
+        entry_points     = """
+            [console_scripts]
+            abstraction = abstraction:abstraction
+        """
+    )
+
+def read(*paths):
+    with open(os.path.join(*paths), "r") as filename:
+        return filename.read()
+
+if __name__ == "__main__":
+    main()
