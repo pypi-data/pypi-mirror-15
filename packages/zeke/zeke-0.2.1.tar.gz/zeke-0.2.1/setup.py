9c0a03a14dab68415e493dfd2246beb0cf0b58f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:03 +0000

    Add package zeke/0.2.1/zeke-0.2.1.tar.gz

---
+++ packages/zeke/zeke-0.2.1.tar.gz/zeke-0.2.1/setup.py
@@ -0,0 +1,37 @@
+import sys
+from setuptools import setup
+
+
+if sys.version_info >= (3,):
+    dnspython = "dnspython3"
+else:
+    dnspython = "dnspython"
+
+setup(
+    name='zeke',
+    version='0.2.1',
+    author='Yodle',
+    author_email='mdrago@yodle.com',
+    url='https://github.com/yodle/zeke',
+    description='A command-line tool for Zookeeper that is a pleasure to use',
+    packages=['zeke'],
+    entry_points={
+        'console_scripts': [
+            'zeke = zeke.zeke:main'
+        ]
+    },
+    tests_require=['mock'],
+    install_requires=['kazoo', dnspython],
+    test_suite='test',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities'
+    ],
+    long_description="""
+**Zeke is a command-line tool for Zookeeper that is a pleasure to use.**
+"""
+)
