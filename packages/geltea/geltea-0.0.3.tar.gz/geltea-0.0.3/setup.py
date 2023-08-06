a12c99102604cad165f00e8239c2382141e54066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:47 +0000

    Add package geltea/0.0.3/geltea-0.0.3.tar.gz

---
+++ packages/geltea/geltea-0.0.3.tar.gz/geltea-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="geltea",
+    packages= ['geltea'],
+    license='MIT',
+    author='Jelte Hoekstra',
+    url='https://github.com/jcshoekstra/mlutils',
+    install_requires=['pandas', 'seaborn', 'sklearn', 'numpy', 'matplotlib'],
+    version='0.0.3')
