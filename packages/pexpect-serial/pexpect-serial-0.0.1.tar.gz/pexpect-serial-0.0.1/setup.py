6d8b37bad48e4ad7e8943608823f4786a2bfce0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:11 +0000

    Add package pexpect-serial/0.0.1/pexpect-serial-0.0.1.tar.gz

---
+++ packages/pexpect-serial/pexpect-serial-0.0.1.tar.gz/pexpect-serial-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pexpect-serial',
+    version='0.0.1',
+    description='pexpect with pyserial',
+    long_description=readme,
+    author='High Wall',
+    author_email='hiwall@126.com',
+    url='https://github.com/highwall/pexpect-serial',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
