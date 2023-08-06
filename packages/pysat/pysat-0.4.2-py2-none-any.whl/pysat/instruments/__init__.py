e723e10cc2d60ea75c34b23b117e4e0cfbc4739f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:10 +0000

    Add package pysat/0.4.2/pysat-0.4.2-py2-none-any.whl

---
+++ packages/pysat/pysat-0.4.2-py2-none-any.whl/pysat/instruments/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+
+__all__ = ['cosmic2013_gps', 'cnofs_vefi', 'cnofs_ivm', 'cosmic_gps', 
+           'superdarn_grdex', 'omni_hro', 'rocsat_ivm', 'champ_star']
+from . import *
