c10c3feecc452c2145b9c77a90640123e1fa617d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:11 +0000

    Add package lolcat/0.44/lolcat-0.44.tar.gz

---
+++ packages/lolcat/lolcat-0.44.tar.gz/lolcat-0.44/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='lolcat',
+      version='0.44',
+      description='Rainbows and unicorns!',
+      author='Wijnand Modderman-Lenstra',
+      author_email='maze@pyth0n.org',
+      url='https://github.com/tehmaze/lolcat/',
+      scripts=['lolcat'],
+     )
