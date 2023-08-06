7672b36753520f7ec28b92a658d4a30177c81216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:46 +0000

    Add package pycodequality/45/pycodequality-45.tar.gz

---
+++ packages/pycodequality/pycodequality-45.tar.gz/pycodequality-45/README.rst
@@ -0,0 +1,35 @@
+pycodequality
+=============
+
+Pylint measures code quality for every file in the folder, returns an
+average
+
+dependencies
+------------
+
+python3
+
+install
+-------
+
+.. code:: bash
+
+    pip3 install pycodequality
+
+usage:
+------
+
+.. code:: bash
+
+    pcq -fs ~/myproject
+
+output
+------
+
+::
+
+    Rating your code: 
+    1. pycodequality/setup.py: 10.00  
+    2. pycodequality/__init__.py: 8.92 (was: 8.89, +0.03) 
+    3. pycodequality/__init__.py: 8.92  
+    totalscore:  9.28
---
+++ packages/pycodequality/pycodequality-45.tar.gz/pycodequality-45/setup.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+"""
+author  : rabshakeh (erik@a8.nl)
+project : pycodequality
+created : 26-05-15 / 15:00
+"""
+
+# import sys
+# if (sys.version_info.major == 2):
+
+#     print("only python3 is supported")
+
+#     exit(1)
+
+import sys
+
+from setuptools import setup
+def main():
+    my_entry_points = {
+        'console_scripts': [
+            'pcq=pycodequality:main',
+        ],
+    }
+
+
+    setup(name='pycodequality',
+          version='45',
+          description='Pylint measures code quality for every file in the folder, returns an average',
+          url='https://github.com/erikdejonge/pycodequality',
+          author='Erik de Jonge',
+          author_email='erik@a8.nl',
+          license='GPL',
+          packages=['pycodequality'],
+          entry_points=my_entry_points,
+          zip_safe=True,
+          install_requires=['pylint', 'pylint-django', 'cmdssh'],
+          classifiers=[
+              "Programming Language :: Python :: 2",
+              "Programming Language :: Python :: 3",
+              "Development Status :: 4 - Beta ",
+              "Intended Audience :: Developers",
+              "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+              "Operating System :: POSIX",
+              "Topic :: Software Development :: Libraries :: Python Modules",
+              "Topic :: Terminals",
+              "Topic :: System",
+          ])
+
+if __name__=="__main__":
+    main()
