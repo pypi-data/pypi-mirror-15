0aec591e37d8d08b77e48de973e904b2d6ba278f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:43 +0000

    Add package danil_python/1.2.0/danil_python-1.2.0.tar.gz

---
+++ packages/danil_python/danil_python-1.2.0.tar.gz/danil_python-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name          = 'danil_python',
+    version       = '1.2.0',
+    py_modules    = ['danil_python'],
+    author        = 'danil',
+    author_email  = '384417599@qq.com',
+    url           = 'http://www.danilpython.com',
+    description   = 'A simple printer of nested lists',
+    )
