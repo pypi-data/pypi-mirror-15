d32ff78944fc6812bbf50e6d5c7f361d6e19999c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:36 +0000

    Add package flask-konch/1.0.0/flask-konch-1.0.0.tar.gz

---
+++ packages/flask-konch/flask-konch-1.0.0.tar.gz/flask-konch-1.0.0/flask_konch/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+    flask_konch
+    ~~~~~~~~~~~
+
+    An improved shell commmand for the Flask CLI.
+
+    :copyright: (c) 2016 by Steven Loria
+    :license: MIT, see LICENSE for more details.
+"""
+__version__ = '1.0.0'
+__author__ = 'Steven Loria'
+__license__ = 'MIT'
+
+__all__ = [
+    'EXTENSION_NAME',
+]
+
+EXTENSION_NAME = 'flask-konch'
