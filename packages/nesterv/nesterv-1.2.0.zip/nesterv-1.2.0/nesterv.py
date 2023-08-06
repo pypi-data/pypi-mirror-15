f47cc1d058b2a460405e0536bbf5738543b3f914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:04 +0000

    Add package nesterv/1.2.0/nesterv-1.2.0.zip

---
+++ packages/nesterv/nesterv-1.2.0.zip/nesterv-1.2.0/nesterv.py
@@ -0,0 +1,12 @@
+"""这是“nesterv.py”模块，提供了"""
+
+def print_lol(the_list,level=0):
+    """这个函数去一个位置函数，名为“the_list”"""
+
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end="") 
+            print(each_item)
