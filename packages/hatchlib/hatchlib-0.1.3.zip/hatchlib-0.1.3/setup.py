0bbd6f760d91eb8678a29e5152af78125285c725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:15 +0000

    Add package hatchlib/0.1.3/hatchlib-0.1.3.zip

---
+++ packages/hatchlib/hatchlib-0.1.3.zip/hatchlib-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name='hatchlib',
+	packages=['hatchlib'],
+	version='0.1.3',
+	description='Hatch Alpha algorithm library',
+	author='Lia',
+	author_email='inoriy.sh@gmail.com',
+	url='https://github.com/LiaSL/hatchlib',
+	download_url='https://github.com/LiaSL/hatchlib/tarball/pypi',
+	keywords=['algorithms','library'],
+	classifiers=[],
+)
