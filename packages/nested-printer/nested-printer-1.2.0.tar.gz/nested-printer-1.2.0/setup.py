8e9f9df5cac389d659731891db85a712384c855d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:39 +0000

    Add package nested-printer/1.2.0/nested-printer-1.2.0.tar.gz

---
+++ packages/nested-printer/nested-printer-1.2.0.tar.gz/nested-printer-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name           = 'nested-printer',
+        version        = '1.2.0',
+        py_modules     = ['nested_print_lol'],
+        author         = 'test',
+        author_email   = 'test@gmail.com',
+        url            = 'http://www.testit.com',
+        description    = 'A simple printer of nested list',
+)
