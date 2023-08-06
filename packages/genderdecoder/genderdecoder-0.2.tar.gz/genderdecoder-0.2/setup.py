fcbe9fdaff5cbe903fb5d8b59735ffbb5e82c6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:16 +0000

    Add package genderdecoder/0.2/genderdecoder-0.2.tar.gz

---
+++ packages/genderdecoder/genderdecoder-0.2.tar.gz/genderdecoder-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='genderdecoder',
+      version='0.2',
+      description='Check for gender-coded words in job adverts. A package of the service provided at http://gender-decoder.katmatfield.com',
+      url='http://github.com/Doteveryone/genderdecoder',
+      author='Richard Pope',
+      author_email='richard@memespring.co.uk',
+      license='MIT',
+      packages=['genderdecoder'],
+      zip_safe=False)
