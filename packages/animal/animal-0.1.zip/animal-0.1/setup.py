9a386e996955d72d3b6fd8e048d4326369cf8653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:18 +0000

    Add package animal/0.1/animal-0.1.zip

---
+++ packages/animal/animal-0.1.zip/animal-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+setup(
+  name = 'animal',
+  packages = ['animal'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random animal lib',
+  author = 'Carlos Clavero',
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
+
