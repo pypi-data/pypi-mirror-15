04205a9466d53b50d952b7e97b0322c2ef2d6873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:31 +0000

    Add package metu-cafeteria-menu/0.0.2/metu-cafeteria-menu-0.0.2.tar.gz

---
+++ packages/metu-cafeteria-menu/metu-cafeteria-menu-0.0.2.tar.gz/metu-cafeteria-menu-0.0.2/setup.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='metu-cafeteria-menu',
+    version='0.0.2',
+    packages=['metu_cafeteria_menu'],
+    description="A utility for fetching Middle East Technical University's cafeteria menu.",
+    keywords=['metu', 'cafeteria', 'menu'],
+    url='https://github.com/th0th/metu-cafeteria-menu',
+    download_url='https://github.com/th0th/metu-cafeteria-menu/tarball/0.0.1',
+    author=u'Gökhan Sarı',
+    author_email='me@th0th.me',
+    license='MIT',
+    install_requires=[
+        'beautifulsoup4==4.4.1',
+    ],
+    zip_safe=False,
+)
