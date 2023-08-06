26cca5c0194bea6c62360ec03b311808eee0363e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:08 +0000

    Add package djshorty/0.5.1/djshorty-0.5.1.tar.gz

---
+++ packages/djshorty/djshorty-0.5.1.tar.gz/djshorty-0.5.1/shorty/autoconfig.py
@@ -0,0 +1,13 @@
+DEFAULT_SETTINGS = {
+    'AUTOCONFIG_URL_PREFIXES': {
+        'shorty': '',
+    },
+}
+
+SETTINGS = {
+    'INSTALLED_APPS': [
+        'django.contrib.admin',
+        'nuit',
+    ],
+    'NUIT_GLOBAL_TITLE': 'Shorty',
+}
