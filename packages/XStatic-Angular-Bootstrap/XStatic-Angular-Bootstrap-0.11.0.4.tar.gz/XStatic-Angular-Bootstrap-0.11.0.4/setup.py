19f746e59010b2ce768b2eb129d41f9dc5ca9ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:53 +0000

    Add package XStatic-Angular-Bootstrap/0.11.0.4/XStatic-Angular-Bootstrap-0.11.0.4.tar.gz

---
+++ packages/XStatic-Angular-Bootstrap/XStatic-Angular-Bootstrap-0.11.0.4.tar.gz/XStatic-Angular-Bootstrap-0.11.0.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name='XStatic-Angular-Bootstrap',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm', 'wheel'],
+    packages=find_packages()
+)
