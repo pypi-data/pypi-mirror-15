2f19a7550d88d2cd8e1c860515fd2a1c7b0b77cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:03 +0000

    Add package wjnester/1.0.3/wjnester-1.0.3.tar.gz

---
+++ packages/wjnester/wjnester-1.0.3.tar.gz/wjnester-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+     name       = "wjnester",
+     version    = "1.0.3",
+     py_modules = ['wjnester'],
+     author     = 'wenjie',
+     author_email= "826834678@qq.com",
+     url        = "https://pypi.python.org/pypi?:action=display&name=wjnester&version=1.0.3",
+     description = "A sample printer of nested lists",
+    )
