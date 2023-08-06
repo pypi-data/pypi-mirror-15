8df6dfda8cffcd84378ca7d5e793f7a88f4b2760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:36 +0000

    Add package cpl/0.1.1/cpl-0.1.1.tar.gz

---
+++ packages/cpl/cpl-0.1.1.tar.gz/cpl-0.1.1/cpl/metadata.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+
+
+NAME = 'cpl'
+VERSION = '0.1.1'
+AUTHORS = [
+    'huntzhan',
+]
+EMAILS = [
+    'programmer.zhx@gmail.com',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = 'cpl stands for "copy-paste loop".'
