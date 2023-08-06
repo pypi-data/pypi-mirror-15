4b3eafc7b68d44299e00b907eb77013027023f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:44 +0000

    Add package esader/0.2/esader-0.2.tar.gz

---
+++ packages/esader/esader-0.2.tar.gz/esader-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='esader',
+    version='0.2',
+    packages=[''],
+    url='http://github.com/ExitStars',
+    license='GPL',
+    author='Exit Stars',
+    author_email='starsexit@gmail.com',
+    description='Python Programlari Icin Logo ve Renklendirme'
+)
