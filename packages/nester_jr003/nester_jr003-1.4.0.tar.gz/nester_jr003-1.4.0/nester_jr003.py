b6ac0e7a51661135a834cb608deccc01262ff880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:18 +0000

    Add package nester_jr003/1.4.0/nester_jr003-1.4.0.tar.gz

---
+++ packages/nester_jr003/nester_jr003-1.4.0.tar.gz/nester_jr003-1.4.0/nester_jr003.py
@@ -0,0 +1,10 @@
+"""The module produces one function called print_lol which  prints nested lists if they are there"""
+def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item, indent, level+1, fh)
+		else:
+			if indent:
+				for tab_stop in range(level):
+					print("\t", end='', file=fh)
+			print(each_item, file = fh)
