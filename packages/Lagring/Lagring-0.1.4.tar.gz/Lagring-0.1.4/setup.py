d779e3426360ff6c8fb9ac7c3525f6bec671a228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:24 +0000

    Add package Lagring/0.1.4/Lagring-0.1.4.tar.gz

---
+++ packages/Lagring/Lagring-0.1.4.tar.gz/Lagring-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Lagring",
+    version="0.1.4",
+    author="Lenar Imamutdinov",
+    author_email="lenar.imamutdinov@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/neoden/lagring",
+    license="MIT",
+    description="Asset storage for Flask",
+    long_description=open("README.txt").read(),
+    install_requires=[
+        "flask",
+    ],
+    extras_require = {
+        'Pillow': []
+    }
+)
