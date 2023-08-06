f71ea7f6fa149bdb97cc317256e4895f4809b80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:32 +0000

    Add package twads/0.1.2/twads-0.1.2.tar.gz

---
+++ packages/twads/twads-0.1.2.tar.gz/twads-0.1.2/setup.py
@@ -0,0 +1,20 @@
+import re
+from distutils.core import setup
+
+
+with open('VERSION', 'r') as f:
+    try:
+        version = re.search('^v([0-9.]*)', f.read()).group(1)
+    except AttributeError:
+        raise RuntimeError('Version not found')
+
+
+setup(
+    name='twads',
+    packages=['twads'],
+    version=version,
+    description='A wrapper for the Twitter Ads API',
+    author='Jacob Gillespie',
+    author_email='jdgillespie91@gmail.com',
+    url='https://github.com/jdgillespie91/twads'
+)
