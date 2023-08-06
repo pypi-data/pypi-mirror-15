c5375e1e4f860c87ef5d3aaf864f808db1d3652e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:52 +0000

    Add package McImage/1.0/McImage-1.0.zip

---
+++ packages/McImage/McImage-1.0.zip/McImage-1.0/setup.py
@@ -0,0 +1,23 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(
+    name = "McImage",
+    version = "1.0",
+    packages = find_packages(),
+    scripts = ['mcimage.py'],
+
+    
+    install_requires = ['beautifulsoup4 >= 4', 'requests >= 2'],
+
+   
+    # metadata for upload to PyPI
+    author = "Darryl McClellan",
+    author_email = "darryljm34@gmail.com",
+    description = "Bulk image downloader command line tool",
+    license = "GNU GPL",
+    keywords = "image download downloader picture pictures .jpg bulk automatic picture",
+
+
+    # could also include long_description, download_url, classifiers, etc.
+)
