8782af9194859007a6aa8db7bae945f466b2ef75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:17 +0000

    Add package django-model-repr/0.0.5/django-model-repr-0.0.5.tar.gz

---
+++ packages/django-model-repr/django-model-repr-0.0.5.tar.gz/django-model-repr-0.0.5/django_model_repr/models.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.conf import settings
+
+class Model(models.Model):
+	class Meta:
+		abstract = True
+
+	def __repr__(self):
+		fields = self._meta.get_fields()
+		classname = self.__class__.__name__
+		exclude = getattr(settings, "MODEL_EXCLUDE", {}).get(classname, [])
+
+		buf = "<%s"%(classname)
+		buf += "\n"
+
+		for field in fields:
+			if not field.concrete or field in exclude:
+				continue
+
+			buf += "\t%s: %s"%(field.name, getattr(self, field.name))
+			buf += "\n"
+
+		buf += ">"
+
+		return buf
+
+
+if getattr(settings, "MODEL_REPR_MONEY_PATCHING", True):
+	setattr(models, "Model", Model)
