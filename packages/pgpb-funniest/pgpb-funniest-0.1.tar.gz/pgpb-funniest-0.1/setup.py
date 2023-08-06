9a38fef45c5e3a2e5bf9246e873eb4f5b0d6175d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:32 +0000

    Add package pgpb-funniest/0.1/pgpb-funniest-0.1.tar.gz

---
+++ packages/pgpb-funniest/pgpb-funniest-0.1.tar.gz/pgpb-funniest-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pgpb-funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/pgpbpadilla/pgpb-funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['pgpb-funniest'],
+      zip_safe=False)
+
