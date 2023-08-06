656924bc9bd4f4cb9bbe7a752ff80a988f4421dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:16 +0000

    Add package zouaho_nester/1.2.0/zouaho_nester-1.2.0.zip

---
+++ packages/zouaho_nester/zouaho_nester-1.2.0.zip/zouaho_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'zouaho_nester',
+	version			= '1.2.0',
+	py_modules		= ['zouhao_nester'],
+	author			= 'pksophiya',
+	author_email	= '826841540@qq.com',
+	url				= 'https://pypi.python.org/id/pksophiya',
+	description		= 'A simple printer of nested lists',
+)
