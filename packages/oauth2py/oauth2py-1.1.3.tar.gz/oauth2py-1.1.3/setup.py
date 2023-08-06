0e9e1963085c9c25e1e4416bf71dfaeaa5f133f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:40 +0000

    Add package oauth2py/1.1.3/oauth2py-1.1.3.tar.gz

---
+++ packages/oauth2py/oauth2py-1.1.3.tar.gz/oauth2py-1.1.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+def long_description():
+    try:
+        return open('README.rst').read()
+    except IOError:
+        return 'a simple, lightweight oauth client'
+
+config = {
+    'name': 'oauth2py',
+    'version': '1.1.3',
+    'description': 'a simple, lightweight oauth client',
+    'author': 'caoyue',
+    'author_email': 'i@caoyue.me',
+    'url': 'https://github.com/caoyue/oauth2py',
+    'download_url': 'https://github.com/caoyue/oauth2py',
+    'license': 'http://www.apache.org/licenses/LICENSE-2.0',
+    'install_requires': [''],
+    'packages': ['oauth2py'],
+    'classifiers': [
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    'long_description': long_description(),
+}
+
+setup(**config)
