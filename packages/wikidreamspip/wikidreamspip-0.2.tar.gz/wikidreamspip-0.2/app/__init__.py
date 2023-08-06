fdb9b4cd4a8b39ebd33cd82d5114257a73025803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:41 +0000

    Add package wikidreamspip/0.2/wikidreamspip-0.2.tar.gz

---
+++ packages/wikidreamspip/wikidreamspip-0.2.tar.gz/wikidreamspip-0.2/app/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+def hello(string):
+	print string
---
+++ packages/wikidreamspip/wikidreamspip-0.2.tar.gz/wikidreamspip-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wikidreamspip',
+      version='0.2',
+      description='Pip example',
+      url='http://github.com/WikiDreams/wikidreamspip',
+      author='Wiki Dreams',
+      author_email='danielsantosvieiramail@gmail.com',
+      license='MIT',
+      packages=['app'],
+      zip_safe=False)
