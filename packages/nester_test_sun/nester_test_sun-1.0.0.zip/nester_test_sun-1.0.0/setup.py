7d7c0ea3a8ed9f5440d2e2f4f71f23c46ab6ee5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:54 +0000

    Add package nester_test_sun/1.0.0/nester_test_sun-1.0.0.zip

---
+++ packages/nester_test_sun/nester_test_sun-1.0.0.zip/nester_test_sun-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+'''metadata'''
+setup(
+    name = 'nester_test_sun',
+    version = '1.0.0',
+    py_modules = ['nester'],
+	py_functions='print_list',
+    author = 'sunil',
+    author_email = 'sunil.ror@gmail.com',
+    url = 'http://ssdssdds',
+    description = 'A simple printer of nested list'
+    )
+
+    
