ead78b0cf28bc0d7c9fab41b3fe0975e410ea2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:08 +0000

    Add package bravocapital/1.0.9/bravocapital-1.0.9.tar.gz

---
+++ packages/bravocapital/bravocapital-1.0.9.tar.gz/bravocapital-1.0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bravocapital',
+      version='1.0.9',  # Development release
+      description='Python package for Bravo Capital!',
+      url='https://github.com/manoelgadi/bravocapital/bravocapital',
+      author='Manoel Gadi',
+      author_email='manoel.gadi@bravocapital.es',
+      license='MIT',
+      packages=['bravocapital'],
+      zip_safe=False)
