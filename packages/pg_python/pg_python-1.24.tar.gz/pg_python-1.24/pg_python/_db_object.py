f66d31aad3df4c9b8118bcc7bc65e3b40b1066ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:45 +0000

    Add package pg_python/1.24/pg_python-1.24.tar.gz

---
+++ packages/pg_python/pg_python-1.24.tar.gz/pg_python-1.24/pg_python/_db_object.py
@@ -0,0 +1,42 @@
+import psycopg2
+
+class Db(object):
+    params = None
+    connection = None
+    logger = None
+
+    def __init__(self, params):
+        self.params = params
+        self._make_connection()
+
+    def _make_connection(self):
+        print self.params
+        try:
+            self.connection = psycopg2.connect(**self.params)
+        except Exception as e:
+            print ("Error %s" % e)
+
+    def get_connection(self):
+        return self.connection
+
+    '''
+    Cursor will be created per crawler
+    '''
+    def get_cursor(self):
+        try:
+            cursor = self.connection.cursor()
+            return cursor
+        except Exception as err:
+            self.logger.warning("Connection seems to have expired, remaking it")
+            self._make_connection()
+            cursor = self.connection.cursor()
+            return cursor
+
+    def close_cursor(self, cursor):
+        cursor.close()
+
+    def commit(self):
+        self.connection.commit()
+
+    def close_connection(self):
+        self.connection.close()
