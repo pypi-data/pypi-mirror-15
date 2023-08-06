341822ec8628c96a607562abc02043a7a80bdf5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:01 +0000

    Add package nester_cxx/1.4.0/nester_cxx-1.4.0.zip

---
+++ packages/nester_cxx/nester_cxx-1.4.0.zip/nester_cxx-1.4.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'nester_cxx', 
+        version      = '1.4.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nester_cxx'],
+        author       = 'chenx212',
+        author_email = 'chenx212@gmail.com',
+        url          = 'https://www.baidu.com',
+        description  = 'A simple printer of nested lists',
+     )
