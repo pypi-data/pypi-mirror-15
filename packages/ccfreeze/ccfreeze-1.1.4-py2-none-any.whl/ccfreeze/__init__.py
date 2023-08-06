ed6662c43281ab2ab24ef8be5763e0802cd879ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:02 +0000

    Add package ccfreeze/1.1.4/ccfreeze-1.1.4-py2-none-any.whl

---
+++ packages/ccfreeze/ccfreeze-1.1.4-py2-none-any.whl/ccfreeze/__init__.py
@@ -0,0 +1,25 @@
+version = "1.1.4"
+
+import sys
+from ccfreeze import modulegraph
+sys.modules['modulegraph'] = modulegraph
+
+from ccfreeze.freezer import Freezer
+
+version = "1.1.4"
+
+
+def main():
+    scripts = sys.argv[1:]
+    if not scripts:
+        print "Version: %s (Python %s)" % (version)
+        print "Usage: ccfreeze SCRIPT1 [SCRIPT2...]"
+        print "   creates standalone executables from python scripts SCRIPT1,..."
+        print
+
+        sys.exit(0)
+
+    f = Freezer()
+    for x in scripts:
+        f.addScript(x)
+    f()
