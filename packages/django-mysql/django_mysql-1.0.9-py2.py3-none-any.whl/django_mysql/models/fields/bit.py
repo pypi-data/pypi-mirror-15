eea57c430652935810c60b8787e7504173826106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:44 +0000

    Add package django-mysql/1.0.9/django_mysql-1.0.9-py2.py3-none-any.whl

---
+++ packages/django-mysql/django_mysql-1.0.9-py2.py3-none-any.whl/django_mysql/models/fields/bit.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+from django.db.models import BooleanField, NullBooleanField
+from django.utils import six
+
+from django_mysql.compat import field_class
+
+
+class Bit1Mixin(object):
+    def db_type(self, connection):
+        return 'bit(1)'
+
+    def to_python(self, value):
+        # Meant to be binary/bytes but can come back as unicode strings
+        if isinstance(value, six.binary_type):
+            value = (value == b'\x01')
+        elif isinstance(value, six.text_type):
+            # Only on older versions of mysqlclient and Py 2.7
+            value = (value == '\x01')  # pragma: no cover
+        return value
+
+    def from_db_value(self, value, expression, connection, context):
+        # Meant to be binary/bytes but can come back as unicode strings
+        if isinstance(value, six.binary_type):
+            value = (value == b'\x01')
+        elif isinstance(value, six.text_type):
+            # Only on older versions of mysqlclient and Py 2.7
+            value = (value == '\x01')  # pragma: no cover
+        return value
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+        else:
+            return 1 if value else 0
+
+
+class Bit1BooleanField(field_class(Bit1Mixin, BooleanField)):
+    pass
+
+
+class NullBit1BooleanField(field_class(Bit1Mixin, NullBooleanField)):
+    pass
