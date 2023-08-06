e7bdf2fdeaaccfb929c8f47a77c663275b584ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:08 +0000

    Add package Algos/0.0.2/Algos-0.0.2.tar.gz

---
+++ packages/Algos/Algos-0.0.2.tar.gz/Algos-0.0.2/algos/operations/swap.py
@@ -0,0 +1,18 @@
+"""
+    This is the operations.py module that has all the basic operations specified that will be used frquently in programming
+"""
+
+class Swap(object):
+    """
+	Takes two values and swaps them 
+        Basic Usage:
+        
+	    >>> swap_obj = Swap(1,2)
+        >>> value2, value1 = swap_obj.get_swap_values()
+    """
+    def __init__(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
+
+    def get_swap_values(self):
+        return self.value2, self.value1
---
+++ packages/Algos/Algos-0.0.2.tar.gz/Algos-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+modules = [
+	#'algos.structures',
+	'algos.operations.swap'
+]
+
+
+setup(
+    name         = 'Algos',
+    version      = '0.0.2',
+    py_modules   =  modules,
+    author       = 'Chitrank Dixit',
+    author_email = 'chitrankdixit@gmail.com',
+    url          = 'http://chitrank-dixit.github.io',
+    description  = 'A simple library that consists of all basic operations and complex algorithm present on the globe' 
+
+)
