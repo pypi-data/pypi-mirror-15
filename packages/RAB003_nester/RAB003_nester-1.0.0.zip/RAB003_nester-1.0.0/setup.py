da33f424c37c6c8aa6a86c74aa2f7fe8812e07b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:23 +0000

    Add package RAB003_nester/1.0.0/RAB003_nester-1.0.0.zip

---
+++ packages/RAB003_nester/RAB003_nester-1.0.0.zip/RAB003_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'RAB003_nester',
+	version		= '1.0.0',
+	py_modules	= ['nester'],
+	author		= 'RAB0003',
+	author_email 	= 'rosie.butcher@btinternet.com',
+	url		= 'http://www.headfirstlabs.com',
+	description 	= 'A simple printer of nested lists',
+	)
