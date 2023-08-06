fbbcae1a698a1f7c1c24e4a0e3c1ff93d7584dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:47 +0000

    Add package geltea/0.0.3/geltea-0.0.3-py2.py3-none-any.whl

---
+++ packages/geltea/geltea-0.0.3-py2.py3-none-any.whl/geltea/notebook_options.py
@@ -0,0 +1,8 @@
+get_ipython().magic('matplotlib inline')
+
+import pandas as pd
+pd.set_option('display.max_rows', 80)
+pd.set_option('precision', 2)
+
+from IPython.core.display import display, HTML
+display(HTML("<style>.container { width:100% !important; }</style>"))
