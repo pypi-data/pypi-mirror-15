9621f9a3aaf89c28450d081ffdfec19acdb79746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:41 +0000

    Add package VyattaConfParser/0.4.4/VyattaConfParser-0.4.4.tar.gz

---
+++ packages/VyattaConfParser/VyattaConfParser-0.4.4.tar.gz/VyattaConfParser-0.4.4/vyattaconfparser/__init__.py
@@ -0,0 +1,8 @@
+from .parser import parse_conf
+
+__title__ = 'Vyatta Config Parser'
+__version__ = '0.4.4'
+__author__ = 'Alexander Mironov'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Alexander Mironov'
+__all__ = ['parse_conf']
