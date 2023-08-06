d7bff82c686cec350359e4f294ab5300e4674bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:23 +0000

    Add package colorclass/2.1.1/colorclass-2.1.1.tar.gz

---
+++ packages/colorclass/colorclass-2.1.1.tar.gz/colorclass-2.1.1/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+"""Setup script for the project."""
+
+from __future__ import print_function
+
+import codecs
+import os
+
+from setuptools import setup
+
+
+def readme():
+    """Try to read README.rst or return empty string if failed.
+
+    :return: File contents.
+    :rtype: str
+    """
+    path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'README.rst'))
+    handle = None
+    try:
+        handle = codecs.open(path, encoding='utf-8')
+        return handle.read(131072)
+    except IOError:
+        return ''
+    finally:
+        getattr(handle, 'close', lambda: None)()
+
+
+setup(
+    author='@Robpol86',
+    author_email='robpol86@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Terminals',
+        'Topic :: Text Processing :: Markup',
+    ],
+    description='Colorful worry-free console applications for Linux, Mac OS X, and Windows.',
+    install_requires=[],
+    keywords='Shell Bash ANSI ASCII terminal console colors automatic',
+    license='MIT',
+    long_description=readme(),
+    name='colorclass',
+    packages=['colorclass'],
+    url='https://github.com/Robpol86/colorclass',
+    version='2.1.1',
+    zip_safe=True,
+)
