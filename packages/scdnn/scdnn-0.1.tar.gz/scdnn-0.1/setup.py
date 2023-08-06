ec10d3608582b269ff6a04332ac2feb5fdc1b725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:47 +0000

    Add package scdnn/0.1/scdnn-0.1.tar.gz

---
+++ packages/scdnn/scdnn-0.1.tar.gz/scdnn-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scdnn',
+      version='0.1',
+      description='A Package for sparse coding using DNNs',
+      url='https://github.com/Chunmun91/scdnn',
+      author='Chunmun91',
+      author_email='chunmun.mahapatra0@gmail.com',
+      license='MIT',
+      packages=['scdnn'],
+      zip_safe=False)
