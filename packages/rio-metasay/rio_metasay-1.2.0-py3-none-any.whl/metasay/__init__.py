135d7f767d3606020919006ddf9425c6e779572d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:36 +0000

    Add package rio-metasay/1.2.0/rio_metasay-1.2.0-py3-none-any.whl

---
+++ packages/rio-metasay/rio_metasay-1.2.0-py3-none-any.whl/metasay/__init__.py
@@ -0,0 +1,20 @@
+# rio_say
+
+from random import choice
+
+from cowpy import cow
+
+
+__version__ = '1.2.0'
+
+
+def moothedata(metadata, key=None):
+    keys = list(metadata.keys())
+    # Blacklist metadata items that cows can't pronounce.
+    blacklist = ['transform', 'affine']
+    for k in blacklist:
+        if k in keys:
+            keys.remove(k)
+    key = key or choice(keys)
+    msg = cow.Moose().milk("%s: %s" % (key.capitalize(), metadata[key]))
+    return msg
