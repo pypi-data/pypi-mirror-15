9fd6b7c0e7162da89adf55b47bfb5f905ad54c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:07 +0000

    Add package bk_flower/0.9.0/bk_flower-0.9.0.tar.gz

---
+++ packages/bk_flower/bk_flower-0.9.0.tar.gz/bk_flower-0.9.0/README.rst
@@ -0,0 +1,4 @@
+Celery Flower
+=============
+
+Do some change from origin [flower](https://github.com/mher/flower)
---
+++ packages/bk_flower/bk_flower-0.9.0.tar.gz/bk_flower-0.9.0/flower/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+VERSION = (0, 9, 0)
+__version__ = '.'.join(map(str, VERSION)) + '-dev'
