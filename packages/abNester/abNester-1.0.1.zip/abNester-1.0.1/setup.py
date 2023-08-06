e1b3fdd3d16d6cc003031c2954f4f8ed586ffab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:55 +0000

    Add package abNester/1.0.1/abNester-1.0.1.zip

---
+++ packages/abNester/abNester-1.0.1.zip/abNester-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'abNester',
+		version = '1.0.1',
+		py_modules = ['abNester'],
+		author  = 'anurag bajaj',
+		author_email = 'anuragb26@gmail.com',
+		description = 'Printer of nester lists',
+		url = 'http://www.google.co.in'
+     )
