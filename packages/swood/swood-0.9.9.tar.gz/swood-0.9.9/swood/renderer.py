cf55c47ac4fb126f4b247258000707d7a8d44899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:07 +0000

    Add package swood/0.9.9/swood-0.9.9.tar.gz

---
+++ packages/swood/swood-0.9.9.tar.gz/swood-0.9.9/swood/renderer.py
@@ -0,0 +1,114 @@
+from enum import Enum
+import math
+
+from PIL import Image
+import progressbar
+import numpy as np
+from numpy import zeros, asarray, argmin, resize
+
+from . import midiparse
+from . import wavout
+
+CachedNote = midiparse.CachedNote
+
+
+class FileSaveType(Enum):
+    ARRAY_TO_DISK = 0
+    ARRAY_IN_MEM = 1
+    SMART_CACHING = 2
+
+
+class NoteRenderer:
+    def __init__(self, sample, fullclip=False, cachesize=7.5, threshold=0.075):
+        if threshold < 0:
+            return ValueError("The threshold must be a positive number.")
+        self.sample = sample
+        self.fullclip = fullclip
+
+        self.cachesize = cachesize * sample.framerate
+        self.threshold = int(threshold * sample.framerate)
+
+        self.notecache = {}
+
+    def zoom(self, img, multiplier):
+        return asarray(img.resize((int(round(img.size[0] * multiplier)), self.sample.channels), resample=Image.BICUBIC), dtype=np.int32)
+
+    def render_note(self, note):
+        scaled = self.zoom(self.sample.img, self.sample.maxfreq / note.pitch)
+        if self.fullclip or len(scaled) < note.length + self.threshold:
+            return scaled
+        channels = self.sample.channels.shape[0]
+        if channels == 1:
+            cutoff = argmin(v + (d * 20) for d, v in enumerate(scaled[0][note.length:note.length + self.threshold]))
+            return np.resize(scaled, (1, note.length + cutoff))
+        else:
+            cutoffs = [None] * channels
+            for chan in range(channels):
+                # find the nearest/closest zero crossing within the threshold and continue until that
+                sample_end = np.empty(self.threshold)
+                for distance, val in enumerate(scaled[chan][note.length:note.length + self.threshold]):
+                    sample_end[distance] = (val) + (distance * 20)
+                cutoffs[chan] = argmin(sample_end)
+            merged_channels = zeros((channels, note.length + max(cutoffs)), dtype=np.int32)
+            for chan in range(channels):
+                cutoff = note.length + cutoffs[chan]
+                merged_channels[chan][:cutoff] = scaled[chan][:cutoff]
+            return merged_channels
+
+    def render(self, midi, filename, pbar=True, savetype=FileSaveType.ARRAY_TO_DISK, clear_cache=True):
+        if self.fullclip:
+            # leave a small buffer at the end with space for one more sample
+            end_buffer = int(math.ceil(midi.maxpitch * len(self.sample)))
+        else:
+            end_buffer = self.threshold  # it has to cut off sounds at the threshold anyway
+        output_length = midi.length + end_buffer
+
+        if savetype == FileSaveType.SMART_CACHING:
+            #output = CachedWavFile(output_length, filename, self.sample.framerate)
+            raise complain.ComplainToUser("Smart caching will be implemented in the future.")
+        else:
+            output = wavout.UncachedWavFile(output_length, filename, self.sample.framerate, self.sample.channels)
+
+        if pbar:
+            bar = progressbar.ProgressBar(widgets=[progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()], max_value=midi.notecount)
+            progress = 0
+
+        caching = self.cachesize > 0
+
+        if caching:
+            tick = 8
+
+        for time, notes in midi.notes:
+            for note in notes:
+                if hash(note) in self.notecache:
+                    rendered_note = self.notecache[hash(note)]
+                    rendered_note.used += 1  # increment the used counter each time for the "GC" below
+                else:
+                    rendered_note = CachedNote(time, self.render_note(note))
+                    self.notecache[hash(note)] = rendered_note
+                note_volume = (note.volume / midi.maxvolume) * self.sample.volume
+                output.add_data(time, (rendered_note.data * note_volume).astype(np.int32))
+
+                if pbar:
+                    # increment progress bar
+                    progress += 1
+                    bar.update(progress)
+
+            
+            if caching:
+                # cache "garbage collection":
+                # if a CachedNote is more than 7.5 (default) seconds old it removes it from the cache to save mem(e)ory
+                tick += 1
+                if tick == 15:
+                    tick = 0
+                    for k in list(self.notecache.keys()):
+                        if time - self.notecache[k].length > self.cachesize and self.notecache[k].used < 3:
+                            del self.notecache[k]
+
+        if clear_cache:
+            self.notecache.clear()
+
+        if savetype == FileSaveType.ARRAY_IN_MEM:
+            return output.channels
+        else:
+            output.save()
---
+++ packages/swood/swood-0.9.9.tar.gz/swood-0.9.9/swood/sample.py
@@ -0,0 +1,101 @@
+from PIL import Image
+import numpy as np
+from . import complain
+import pyfftw
+import wave
+
+pyfftw.interfaces.cache.enable()
+
+
+class CalculatedFFT:
+    def __init__(self, avgdata, spacing):
+        self.avgdata = avgdata
+        self.spacing = spacing
+
+
+class Sample:
+    def __init__(self, filename, binsize, volume=0.8, delete_raw_data=True):
+        self.binsize = binsize
+
+        if binsize < 2:
+            raise complain.ComplainToUser("FFT bin size must be at least 2.")
+
+        self.delete_raw = delete_raw_data  # delete raw data after FFT analysis
+        self._maxfreq = None
+        self._fft = None
+
+        self.wav = self.parse_wav(filename)
+
+        max_amplitude = max(max(abs(min(chan)), abs(max(chan))) for chan in self.wav)
+        self.volume = 256 ** 4 / (max_amplitude * 2) * volume
+        self.img = Image.frombytes("I",
+                                   (self.length, self.channels),
+                                   self.wav.astype(np.int32).tobytes(),
+                                   "raw", "I", 0, 1)
+        # Pillow recommends those last args because of a bug in the raw parser
+        # See http://pillow.readthedocs.io/en/3.2.x/reference/Image.html?highlight=%22raw%22#PIL.Image.frombuffer
+
+    def parse_wav(self, filename):
+        try:
+            with wave.open(filename, "rb") as wavfile:
+                self.sampwidth = wavfile.getsampwidth()
+                self.framerate = wavfile.getframerate()
+                self.channels = wavfile.getnchannels()
+                self.length = wavfile.getnframes()
+
+                if self.sampwidth == 1:
+                    self.size = np.int8
+                elif self.sampwidth == 2:
+                    self.size = np.int16
+                elif self.sampwidth == 3 or self.sampwidth == 4:
+                    self.size = np.int32
+                else:
+                    raise wave.Error
+
+                wav = np.zeros((self.channels, self.length), dtype=self.size)
+                for i in range(0, self.length):
+                    frame = wavfile.readframes(1)
+                    for chan in range(self.channels):
+                        wav[chan][i] = int.from_bytes(frame[self.sampwidth * chan:self.sampwidth * (chan + 1)], byteorder="little", signed=True)
+                return wav
+        except IOError:
+            raise complain.ComplainToUser("Error opening WAV file at path '{}'.".format(filename))
+        except wave.Error:
+            raise complain.ComplainToUser("This WAV type is not supported. Try opening the file in Audacity and exporting it as a standard WAV.")
+
+    @property
+    def fft(self):
+        if not self._fft:
+            if self.binsize % 2 != 0:
+                print("Warning: Bin size must be a multiple of 2, correcting automatically")
+                self.binsize += 1
+            spacing = float(self.framerate) / self.binsize
+            avgdata = np.zeros(self.binsize // 2, dtype=np.float64)
+            for chan in range(self.channels):
+                for i in range(0, self.wav.shape[1], self.binsize):
+                    data = np.array(self.wav[chan][i:i + self.binsize], dtype=self.size)
+                    if len(data) != self.binsize:
+                        continue
+                    fft = pyfftw.interfaces.numpy_fft.fft(data)
+                    fft = np.abs(fft[:self.binsize // 2])
+                    avgdata += fft
+                    del data
+                    del fft
+            if max(avgdata) == 0:
+                print("Warning: Bin size is too large to analyze sample; dividing by 2 and trying again")
+                self.binsize = self.binsize // 2
+                self._fft = self.fft
+            else:
+                if self.delete_raw:
+                    del self.wav
+                self._fft = CalculatedFFT(avgdata, spacing)
+        return self._fft
+
+    @property
+    def maxfreq(self):
+        if not self._maxfreq:
+            self._maxfreq = (np.argmax(self.fft.avgdata[1:]) * self.fft.spacing) + (self.fft.spacing / 2)
+        return self._maxfreq
+
+    def __len__(self):
+        return self.length
