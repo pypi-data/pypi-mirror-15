9c74dfb0800bee36c34c42d03af29a126b60b930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:59 +0000

    Add package oxtimelines/0.34/oxtimelines-0.34.tar.gz

---
+++ packages/oxtimelines/oxtimelines-0.34.tar.gz/oxtimelines-0.34/oxtimelines/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+# GPL 2008-2014
+__version__ = '0.34'
+
+
+from timeline import Timelines
---
+++ packages/oxtimelines/oxtimelines-0.34.tar.gz/oxtimelines-0.34/setup.py
@@ -0,0 +1,36 @@
+# setup.py
+# -*- coding: UTF-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+def get_version():
+    return 34 #import subprocess
+    rev = subprocess.check_output(['git', 'rev-list', 'HEAD', '--count']).strip()
+    return rev or u'unknown'
+
+setup(name='oxtimelines',
+    version='0.%s' % get_version() ,
+    scripts=[
+        'bin/oxtimelines',
+    ],
+    packages=[
+        'oxtimelines',
+    ],
+    author='0x2620',
+    author_email='0x2620@0x2620.org',
+    url="https://wiki.0x2620.org/wiki/oxtimelines",
+    download_url="http://code.0x2620.org/oxtimelines/download",
+    license="GPLv3",
+    description='extract timelines from videos',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+)
+
