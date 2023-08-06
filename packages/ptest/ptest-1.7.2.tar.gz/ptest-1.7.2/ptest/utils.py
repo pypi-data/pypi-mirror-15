7d1d0846d687ab6b3003cc8b2fa7f7e2dcc25969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:15 +0000

    Add package ptest/1.7.2/ptest-1.7.2.tar.gz

---
+++ packages/ptest/ptest-1.7.2.tar.gz/ptest-1.7.2/ptest/utils.py
@@ -0,0 +1,30 @@
+import errno
+import os
+from functools import wraps
+
+
+def make_dirs(dir_path):
+    try:
+        os.makedirs(dir_path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:  # be happy if someone already created the path
+            raise
+
+
+def remove_tree(dir_path, remove_root=True):
+    if os.path.exists(dir_path):
+        for root, dirs, files in os.walk(dir_path, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        if remove_root is True:
+            os.rmdir(dir_path)
+
+
+def mock_func(func):
+    @wraps(func)
+    def mock(self, *args, **kwargs):
+        func(self, *args, **kwargs)
+
+    return mock
