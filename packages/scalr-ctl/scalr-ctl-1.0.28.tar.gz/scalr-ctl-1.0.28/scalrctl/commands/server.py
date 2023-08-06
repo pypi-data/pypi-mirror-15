fd4990e9e2db9cea1e1e8ec5b24ec2ca2a29fafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:30 +0000

    Add package scalr-ctl/1.0.28/scalr-ctl-1.0.28.tar.gz

---
+++ packages/scalr-ctl/scalr-ctl-1.0.28.tar.gz/scalr-ctl-1.0.28/scalrctl/commands/server.py
@@ -0,0 +1,89 @@
+__author__ = 'Dmitriy Korsakov'
+__doc__ = 'Server management'
+import copy
+from scalrctl import commands
+from scalrctl import click
+
+
+class RebootServer(commands.Action):
+
+    post_template = {
+        "serverRebootOptions": {"hard": True}
+    }
+
+    def get_options(self):
+        hlp = "Reboot type. By default it does soft reboot unless this \
+        option is set to true. Beware that some types of the instances do \
+        not support soft reboot."
+        hard_reboot = click.Option(('--hard-reboot', 'hard'), is_flag=True, default=False, help=hlp)
+        options = super(RebootServer, self).get_options()
+        options.append(hard_reboot)
+        return options
+
+
+    def pre(self, *args, **kwargs):
+        """
+        before request is made
+        """
+        hard = kwargs.pop("hard", None)
+        post_data = copy.deepcopy(self.post_template)
+        post_data["serverRebootOptions"]["hard"] = hard
+        kv = {"import-data": post_data}
+        kv.update(kwargs)
+        arguments, kw = super(RebootServer, self).pre(*args, **kv)
+        return arguments, kw
+
+
+class ResumeServer(commands.Action):
+
+    post_template = {}
+
+    def pre(self, *args, **kwargs):
+        """
+        before request is made
+        """
+        kv = {"import-data": {}}
+        kv.update(kwargs)
+        arguments, kw = super(ResumeServer, self).pre(*args, **kv)
+        return arguments, kw
+
+
+class SuspendServer(commands.Action):
+
+    post_template = {}
+
+    def pre(self, *args, **kwargs):
+        """
+        before request is made
+        """
+        kv = {"import-data": {}}
+        kv.update(kwargs)
+        arguments, kw = super(SuspendServer, self).pre(*args, **kv)
+        return arguments, kw
+
+
+class TerminateServer(commands.Action):
+
+    post_template = {
+        "serverTerminationOptions": {"hard": True}
+    }
+
+    def get_options(self):
+        hlp = "It is used to terminate the Server immediately ignoring scalr.system.server_terminate_timeout."
+        hard_terminate = click.Option(('--hard-terminate', 'hard'), is_flag=True, default=False, help=hlp)
+        options = super(TerminateServer, self).get_options()
+        options.append(hard_terminate)
+        return options
+
+
+    def pre(self, *args, **kwargs):
+        """
+        before request is made
+        """
+        hard = kwargs.pop("hard", None)
+        post_data = copy.deepcopy(self.post_template)
+        post_data["serverTerminationOptions"]["hard"] = hard
+        kv = {"import-data": post_data}
+        kv.update(kwargs)
+        arguments, kw = super(TerminateServer, self).pre(*args, **kv)
+        return arguments, kw
