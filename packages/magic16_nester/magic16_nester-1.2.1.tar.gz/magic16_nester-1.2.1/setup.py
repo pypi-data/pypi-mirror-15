8059edc2c6fd75693d4f52e70a0d94c7af13158d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:42 +0000

    Add package magic16_nester/1.2.1/magic16_nester-1.2.1.tar.gz

---
+++ packages/magic16_nester/magic16_nester-1.2.1.tar.gz/magic16_nester-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'magic16_nester',
+	version		= '1.2.1',
+	py_modules	= ['magic16_nester'],
+	author		= 'xiaobo',
+	author_email	= 'yuxiaobohit@gmail.com',
+	url		= 'http://www.xiaobopsu.com',
+	description	= 'A simple printer of nested lists, 1.1.0 - update level indent of the printer, 1.1.1 - update author url, 1.2.0 -> added two other arguments: indent switcher and level',
+)
