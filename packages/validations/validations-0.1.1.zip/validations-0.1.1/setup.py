4be46fd9edc809a1e313f5697c4ef09b4a5f3edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:45 +0000

    Add package validations/0.1.1/validations-0.1.1.zip

---
+++ packages/validations/validations-0.1.1.zip/validations-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="validations",
+    packages=['validations'],
+    version='0.1.1',
+    description='A simple validation DSL.',
+    author='Jesús Martínez',
+    author_email='jesus.a.martinez.v@gmail.com',
+    classifiers=[],
+    download_url='https://github.com/adminMesfix/validations/tarball/0.1.1',
+    url='https://github.com/adminMesfix/validations',
+    keywords=[]
+)
