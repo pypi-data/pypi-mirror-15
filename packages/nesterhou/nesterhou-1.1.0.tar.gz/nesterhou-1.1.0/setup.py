320b6119fd91bf48e2075e2a5217211d3cb9ea29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:21 +0000

    Add package nesterhou/1.1.0/nesterhou-1.1.0.tar.gz

---
+++ packages/nesterhou/nesterhou-1.1.0.tar.gz/nesterhou-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='nesterhou',
+    version='1.1.0',
+    py_modules=['nesterhou'],
+    author='Hou',
+    author_email='hrf05@163.com',
+    url='http://www.baidu.com',
+    description='A simple printer of nested lists',
+)
+
