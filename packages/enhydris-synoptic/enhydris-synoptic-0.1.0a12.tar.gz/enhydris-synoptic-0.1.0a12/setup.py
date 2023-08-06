463988ed4ce4267cddba1ef1b1bc576646542826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:23 +0000

    Add package enhydris-synoptic/0.1.0a12/enhydris-synoptic-0.1.0a12.tar.gz

---
+++ packages/enhydris-synoptic/enhydris-synoptic-0.1.0a12.tar.gz/enhydris-synoptic-0.1.0a12/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+installation_requirements = [
+    "enhydris>=0.8,<0.9",
+    "pandas>=0.14",
+    "matplotlib>=1.4",
+    "six>=1.9",
+    "celery>=3.1,<4",
+]
+
+packages = find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests',
+                                  ])
+
+kwargs = {
+    'name': "enhydris-synoptic",
+    'version': __import__('enhydris_synoptic').__version__,
+    'license': "AGPL3",
+    'description': "View current weather conditions in Enhydris",
+    'author': "Antonis Christofides",
+    'author_email': "anthony@itia.ntua.gr",
+    'url': "https://github.com/openmeteo/enhydris-synoptic",
+    'packages': packages,
+    'package_data': {'enhydris_synoptic': ['templates/*']},
+    'install_requires': installation_requirements,
+    'test_suite': 'runtests.runtests',
+    'tests_require': ['model-mommy>=1.2.4'],
+}
+
+setup(**kwargs)
