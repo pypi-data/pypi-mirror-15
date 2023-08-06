2838320945698b642282eb16e74154c7f231b8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:59 +0000

    Add package jsreport/0.2/jsreport-0.2.tar.gz

---
+++ packages/jsreport/jsreport-0.2.tar.gz/jsreport-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jsreport',
+      version='0.2',
+      description='Simple tool to pull basic reporting data out of JustServe (https://www.justserve.org/).',
+      url='https://github.com/abadstubner/JustServe_Reports',
+      author='abadstubner',
+      author_email='andrew+pypi@badstubner.com',
+      license='MIT',
+      packages=['jsreport'],
+      zip_safe=False)
