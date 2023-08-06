a774fce0e1110014a93e707145e7b6797a5f74bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:59 +0000

    Add package pmnester/1.4.0/pmnester-1.4.0.zip

---
+++ packages/pmnester/pmnester-1.4.0.zip/pmnester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pmnester',
+    version='1.4.0',
+    py_modules=['nester'],
+    author='spacepirate',
+    author_email='spacepirate@piracy.com',
+    url='www.headfirstlabs.com',
+    description='A printer for nested lists',
+)
