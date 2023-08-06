e5b967410ef7b8d62a2f47e7bf2cc5ee32b5262a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:46 +0000

    Add package pyscraper-framework/0.0.26/pyscraper_framework-0.0.26.tar.gz

---
+++ packages/pyscraper-framework/pyscraper_framework-0.0.26.tar.gz/pyscraper_framework-0.0.26/pyscraper_framework/lib/transformer.py
@@ -0,0 +1,8 @@
+import os
+
+class Transformer():
+    def __init__(self):
+        print("Transformer::init() - the base class.")
+
+    def execute(self):
+        print("Transformer::execute() - the base class.")
