97a7bedd679c9b750261e9f0591b2deb40d06321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:03 +0000

    Add package asyncssh/1.5.5/asyncssh-1.5.5.tar.gz

---
+++ packages/asyncssh/asyncssh-1.5.5.tar.gz/asyncssh-1.5.5/examples/direct_client.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3.4
+#
+# Copyright (c) 2013-2015 by Ron Frederick <ronf@timeheart.net>.
+# All rights reserved.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License v1.0 which accompanies this
+# distribution and is available at:
+#
+#     http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     Ron Frederick - initial implementation, API, and documentation
+
+import asyncio, asyncssh, sys
+
+class MySSHTCPSession(asyncssh.SSHTCPSession):
+    def data_received(self, data, datatype):
+        # We use sys.stdout.buffer here because we're writing bytes
+        sys.stdout.buffer.write(data)
+
+    def connection_lost(self, exc):
+        if exc:
+            print('Direct connection error:', str(exc), file=sys.stderr)
+
+@asyncio.coroutine
+def run_client():
+    with (yield from asyncssh.connect('localhost')) as conn:
+        chan, session = yield from conn.create_connection(MySSHTCPSession,
+                                                          'www.google.com', 80)
+
+        # By default, TCP connections send and receive bytes
+        chan.write(b'HEAD / HTTP/1.0\r\n\r\n')
+        chan.write_eof()
+
+        yield from chan.wait_closed()
+
+    yield from conn.wait_closed()
+
+try:
+    asyncio.get_event_loop().run_until_complete(run_client())
+except (OSError, asyncssh.Error) as exc:
+    sys.exit('SSH connection failed: ' + str(exc))
---
+++ packages/asyncssh/asyncssh-1.5.5.tar.gz/asyncssh-1.5.5/tests/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2014-2015 by Ron Frederick <ronf@timeheart.net>.
+# All rights reserved.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License v1.0 which accompanies this
+# distribution and is available at:
+#
+#     http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     Ron Frederick - initial implementation, API, and documentation
+
+"""Unit tests for AsyncSSH"""
+
+from . import test_asn1, test_cipher, test_compression, test_known_hosts
+from . import test_mac, test_public_key, test_saslprep, util
