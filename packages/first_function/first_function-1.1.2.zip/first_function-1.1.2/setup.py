6a9c7fc88214a55e0860cd86635b8c3dff20c3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:28 +0000

    Add package first_function/1.1.2/first_function-1.1.2.zip

---
+++ packages/first_function/first_function-1.1.2.zip/first_function-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'first_function',
+	version			= '1.1.2',
+	py_modules		= ['first_function'],
+	author			= 'hfpython',
+	author_email	= 'hfpython@headfirstlabs.com',
+	url				= 'http://www.headfirstlabs.com',
+	description		= 'A simple printer of nested lists',
+	)
