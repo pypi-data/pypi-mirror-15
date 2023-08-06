d18fefc0d0634236060270586de5ee23d6c4cd08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:29 +0000

    Add package infi.pypi-manager/0.4.34/infi.pypi_manager-0.4.34-py2.7.egg

---
+++ packages/infi.pypi-manager/infi.pypi_manager-0.4.34-py2.7.egg/infi/pypi_manager/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.4.34"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.4.34)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'cb244654789d3bb7df6181343c9811cf87f97398'
+__git_head_subject__ = 'Finished Release v0.4.34'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-06-22 15:48:19'
