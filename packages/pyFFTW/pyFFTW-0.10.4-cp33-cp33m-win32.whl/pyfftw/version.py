22efbc235b6c78515712c455caffb3d4031d4d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:31 +0000

    Add package pyFFTW/0.10.4/pyFFTW-0.10.4-cp33-cp33m-win32.whl

---
+++ packages/pyFFTW/pyFFTW-0.10.4-cp33-cp33m-win32.whl/pyfftw/version.py
@@ -0,0 +1,13 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.10.4'
+version = '0.10.4'
+full_version = '0.10.4'
+git_revision = '4dd6b6b2f330ec65adba0a2c14281a8e875b4d9a'
+release = True
+if not release:
+    version = full_version
+
+if __name__ == "__main__":
+    print(short_version)
+    print(version)
