34a4af9babca4ca19ca19c2693f85e150a7da572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:19 +0000

    Add package invisibleroads-macros/0.6.9.1/invisibleroads-macros-0.6.9.1.tar.gz

---
+++ packages/invisibleroads-macros/invisibleroads-macros-0.6.9.1.tar.gz/invisibleroads-macros-0.6.9.1/invisibleroads_macros/configuration.py
@@ -0,0 +1,36 @@
+import re
+import shlex
+import sys
+from collections import defaultdict
+from six.moves.configparser import RawConfigParser
+
+
+class RawCaseSensitiveConfigParser(RawConfigParser):
+    optionxform = str
+
+
+def get_interpretation_by_name(settings, prefix, interpret_setting):
+    interpretation_by_name = defaultdict(dict)
+    pattern_key = re.compile(prefix.replace('.', r'\.') + r'(.*)\.(.*)')
+    for key, value in settings.items():
+        try:
+            name, attribute = pattern_key.match(key).groups()
+        except AttributeError:
+            continue
+        interpretation = interpretation_by_name[name]
+        interpretation.update(interpret_setting(attribute, value))
+    return interpretation_by_name
+
+
+def unicode_safely(x):
+    # http://stackoverflow.com/a/23085282/192092
+    if not hasattr(x, 'decode'):
+        return x
+    return x.decode(sys.getfilesystemencoding())
+
+
+def split_arguments(x):
+    try:
+        return shlex.split(x)
+    except UnicodeEncodeError:
+        return shlex.split(x.encode('utf-8'))
