b5b8a8acd1ad00b8c41e11e51cdb8c06b01a2ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:20 +0000

    Add package jeju/0.3.0/jeju-0.3.0.linux-x86_64.tar.gz

---
+++ packages/jeju/jeju-0.3.0.linux-x86_64.tar.gz/usr/local/lib/python2.7/dist-packages/jeju/executor/shell.py
@@ -0,0 +1,42 @@
+###########################################
+# This is very naive replacement algorithm
+# TODO
+############################################
+import string
+import uuid
+
+# TODO: configurable variable
+TEMP_DIR = "/tmp"
+
+def replaceable(code, kv):
+    # change keyword to value
+    keys = kv.keys()
+    # find keyword which is ${keyword}
+    # replace value ${keyword} <- kv[keyword]
+    for key in keys:
+        nkey = "${%s}" % key
+        code = string.replace(code, nkey, kv[key])
+    print '#' * 40
+    print code
+    print '#' * 40
+    return code
+
+def shell_bash(**kwargs):
+    code = kwargs['code']
+    kv = kwargs['kv']
+
+    import os
+    # call replaceable
+    rcode = replaceable(code, kv)
+
+    temp = uuid.uuid1()
+    temp_file = "%s/%s" % (TEMP_DIR, str(temp))
+     
+    fp = open(temp_file, 'w')
+    fp.write(rcode)
+    fp.close()
+    os.system("bash %s" % temp_file)
+    os.remove(temp_file)
+    return "Bash executed"
+
+
