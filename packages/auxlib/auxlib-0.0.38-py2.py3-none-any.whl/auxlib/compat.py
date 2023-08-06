80a02ceba0cea7d0bb0a34b0a816ebdaa1f843a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:54 +0000

    Add package auxlib/0.0.38/auxlib-0.0.38-py2.py3-none-any.whl

---
+++ packages/auxlib/auxlib-0.0.38-py2.py3-none-any.whl/auxlib/compat.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+try:
+    from collections import OrderedDict as odict
+except ImportError:
+    from ordereddict import OrderedDict as odict
+
+from ._vendor.five import with_metaclass  # NOQA
+from ._vendor.six import (string_types, text_type, integer_types, iteritems, itervalues,
+                          iterkeys, wraps)  # NOQA
+
+NoneType = type(None)
+primitive_types = (*string_types, *integer_types, float, complex, bool, NoneType)
