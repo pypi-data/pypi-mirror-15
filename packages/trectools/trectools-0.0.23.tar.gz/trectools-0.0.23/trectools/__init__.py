2ceb2165065be08c8e31796e4ac2c9b2b5333d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:12 +0000

    Add package trectools/0.0.23/trectools-0.0.23.tar.gz

---
+++ packages/trectools/trectools-0.0.23.tar.gz/trectools-0.0.23/trectools/__init__.py
@@ -0,0 +1,8 @@
+#from .trec_misc import TrecMisc
+from .trec_res import TrecRes
+from .trec_qrel import TrecQrel
+from .trec_run import TrecRun
+from .trec_pool import TrecPool
+
+__all__ = ["TrecMisc", "TrecRes", "TrecQrel", "TrecRun", "TrecPool"]
+
