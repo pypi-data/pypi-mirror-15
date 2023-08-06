d5de4c37c5c401848ddeff4aca9fae5c9440352d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:02 +0000

    Add package seldon/1.5.3/seldon-1.5.3.tar.gz

---
+++ packages/seldon/seldon-1.5.3.tar.gz/seldon-1.5.3/seldon/__init__.py
@@ -0,0 +1,13 @@
+import logging
+
+from .util import Recommender_wrapper,Recommender,Extension,Extension_wrapper
+
+__version__ = '1.5.3'
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+logger = logging.getLogger(__name__)
+if len(logger.handlers) == 0:# To ensure reload() doesn't add another one
+    logger.addHandler(NullHandler())
