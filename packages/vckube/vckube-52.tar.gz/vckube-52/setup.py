e8d1517f8f6838b4123d54e4888ec12245bb1ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:47 +0000

    Add package vckube/52/vckube-52.tar.gz

---
+++ packages/vckube/vckube-52.tar.gz/vckube-52/setup.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+"""
+Profiler utility for python
+Active8 (04-03-15)
+license: GNU-GPL2
+"""
+
+from setuptools import setup
+setup(name='vckube',
+      version='52',
+      description='Kubernetes Vagrant Provisioning and management script',
+      url='https://github.com/erikdejonge/vckube',
+      author='Erik de Jonge',
+      author_email='erik@a8.nl',
+      license='GPL',
+      packages=['vckube'],
+      zip_safe=True,
+      #install_requires=['paramiko', 'python-vagrant', 'consoleprinter', 'cmdssh'],
+      entry_points={
+          'console_scripts': [
+              'vckube=vckube:main',
+          ],
+      },
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 4 - Beta ",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+          "Operating System :: POSIX",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Software Development :: Testing",
+          "Topic :: System",
+      ])
