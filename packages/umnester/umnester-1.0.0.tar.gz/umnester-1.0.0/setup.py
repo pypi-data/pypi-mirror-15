a870982445214245f20427c127136ae581a5630f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:26 +0000

    Add package umnester/1.0.0/umnester-1.0.0.tar.gz

---
+++ packages/umnester/umnester-1.0.0.tar.gz/umnester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'umnester',
+    version = '1.0.0',
+    py_modules = ['nested'],
+    author = 'Umair',
+    author_email = 'um_trade@yahoo.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
