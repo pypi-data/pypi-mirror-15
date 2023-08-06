940d033d852565953db805f0a544c145280fb1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:23 +0000

    Add package flask_params/1.0.0/flask_params-1.0.0.tar.gz

---
+++ packages/flask_params/flask_params-1.0.0.tar.gz/flask_params-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+
+setup(name='flask_params',
+      packages=['flask_params'],
+      version='1.0.0',
+      description='Processes the Request params for Flask served as a Python library ',
+      long_description=long_description,
+      author='Nimesh Kiran Verma, Rohit khatana',
+      author_email='nimesh.aug11@gmail.com, rohitkhatana.khatana@gmail.com',
+      url='https://github.com/nimeshkverma/Flask-Params',
+      download_url='https://github.com/nimeshkverma/Flask-Params/tarball/1.0.0',
+      py_modules=['flask-params'],
+      install_requires=['Flask', 'jsonschema'],
+      keywords=['params', 'requests', 'post', 'get', 'API', 'REST'],
+      classifiers=[],
+      )
