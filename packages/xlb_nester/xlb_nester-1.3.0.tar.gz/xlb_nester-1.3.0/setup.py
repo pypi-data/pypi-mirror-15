798ae869a2c36935916cd65a781e48434d6b664f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:51 +0000

    Add package xlb_nester/1.3.0/xlb_nester-1.3.0.tar.gz

---
+++ packages/xlb_nester/xlb_nester-1.3.0.tar.gz/xlb_nester-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'xlb_nester',
+    version = '1.3.0',
+    py_modules = ['xlb_nester'],
+    author = 'fhxlb',
+    author_email = 'fhxlb@outlook.com',
+    url = '',
+    description = 'A simple printer of nested lists',
+    )
