adf6ba33af08453db6bbbe14424b5d8ffb794250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:16 +0000

    Add package nester-cx/1.0.0/nester-cx-1.0.0.zip

---
+++ packages/nester-cx/nester-cx-1.0.0.zip/nester-cx-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'nester-cx', 
+        version      = '1.0.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nester-cx'],
+        author       = 'chenx212',
+        author_email = 'chenx212@gmail.com',
+        url          = 'https://www.baidu.com',
+        description  = 'A simple printer of nested lists',
+     )
