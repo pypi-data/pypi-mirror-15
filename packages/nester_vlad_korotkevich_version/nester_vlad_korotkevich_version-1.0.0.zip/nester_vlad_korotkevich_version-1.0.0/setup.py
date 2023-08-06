47b11c37fbaab487815e71b1a62202429205ad3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:57 +0000

    Add package nester_vlad_korotkevich_version/1.0.0/nester_vlad_korotkevich_version-1.0.0.zip

---
+++ packages/nester_vlad_korotkevich_version/nester_vlad_korotkevich_version-1.0.0.zip/nester_vlad_korotkevich_version-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_vlad_korotkevich_version',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'vlad korotkevich',
+    author_email = 'chydak13@yandex.ru',
+    url = 'http://www.headfirstlabs.com',
+    )
