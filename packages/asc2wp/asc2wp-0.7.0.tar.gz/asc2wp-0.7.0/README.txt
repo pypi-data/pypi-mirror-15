9fef4da19eef756a6a4398ba79464ba702652433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:45 +0000

    Add package asc2wp/0.7.0/asc2wp-0.7.0.tar.gz

---
+++ packages/asc2wp/asc2wp-0.7.0.tar.gz/asc2wp-0.7.0/README.txt
@@ -0,0 +1,17 @@
+Asc2wp is file base post solution for wordpress with  Asciidoc format.
+
+- Initialize : Make setting file (setting.yaml).
+
+$ asc2wp init
+
+- Template file : Make Post type or Page type Template file.
+
+$ asc2wp makefile
+
+- Post to Wordpress
+
+$ asc2wp {asciidoc-files}
+
+- Media Upload
+
+$ asc2wp -m {image-files}
---
+++ packages/asc2wp/asc2wp-0.7.0.tar.gz/asc2wp-0.7.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages  
+version = '0.7.0'
+
+setup(  
+        name='asc2wp',
+        version=version,
+        description="File base post to Wordpress with Asciidoc format",
+        classifiers=[
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Natural Language :: English",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 2 :: Only",
+            "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary",
+            "Topic :: Internet :: WWW/HTTP :: Site Management",
+            "Topic :: Text Processing :: Markup",
+            "License :: OSI Approved :: MIT License",
+            ],
+        keywords='wordpress, asciidoc, asciidoctor, markup, api',
+        author='Shuichi Minamie',
+        author_email='s.minamie@gmail.com',
+        url='http://sminamie.github.io/asc2wp-Asciidoc-to-Wordpress/',
+        license='MIT',
+        packages=find_packages(),
+        zip_safe=True,
+        install_requires=["python-wordpress-xmlrpc", "pyyaml"],
+        entry_points="""  
+            [console_scripts]
+            asc2wp = asc2wp:main
+        """,
+    )
