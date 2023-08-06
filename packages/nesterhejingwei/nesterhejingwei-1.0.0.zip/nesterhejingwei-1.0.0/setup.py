d252cf3b51459b3b7831f443cb942adf5337c487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:01 +0000

    Add package nesterhejingwei/1.0.0/nesterhejingwei-1.0.0.zip

---
+++ packages/nesterhejingwei/nesterhejingwei-1.0.0.zip/nesterhejingwei-1.0.0/setup.py
@@ -0,0 +1,13 @@
+# coding =encoding name
+'''这是一个注释'''
+from distutils.core import setup       #从发布工具导入setup函数  
+  
+setup(  
+    name        ='nesterhejingwei',  
+    version     ='1.0.0',  
+    py_modules  =['nesterhejingwei'],  
+    author      ='hejingwei',  
+    author_email='1091875842@qq.com',  
+    url         ='http://www.baidu.com',  
+    description ='A simple printer of nested lists',  
+    )  
