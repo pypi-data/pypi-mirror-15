e1c22374737b37fd5c39ff327b19865ac1f2e8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:14 +0000

    Add package nestershan/1.1.0/nestershan-1.1.0.zip

---
+++ packages/nestershan/nestershan-1.1.0.zip/nestershan-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name 			= 'nestershan',
+		version 		= '1.1.0',
+		py_modules		= ['nester'],
+		author			= 'shan',
+		author_email 	= 'shan.anwer@nayatel.com',
+		url 			= 'http://www.nayatel.com',
+		description 	= 'A simple printer of nested loops',
+	)
