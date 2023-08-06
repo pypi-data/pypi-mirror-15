cfae286d101e796849e346045b0c8ab57761c321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:56 +0000

    Add package cvlops/1.11/cvlops-1.11.tar.gz

---
+++ packages/cvlops/cvlops-1.11.tar.gz/cvlops-1.11/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os,sys
+
+# Following Lapin example
+# https://bitbucket.org/CVisionLab/cvl-python/src/51308c1155a6242fbb914870732187adfc8881f0/setup.py?at=default&fileviewer=file-view-default
+
+root_pkg = 'cvlops'
+
+# find all subpackages
+root = os.path.join(os.path.dirname(__file__), root_pkg)
+
+packages = []
+for subdir, _, _ in os.walk(root):
+        subpath = subdir[len(root):].replace(os.path.sep, '.')
+        packages.append(root_pkg + subpath)
+
+setup(
+    name=root_pkg,
+    description='CVISIONLAB OPS Python Utils',
+    long_description=open('README.txt').read(),
+    version='1.11',
+    author='Skubriev Vladimir',
+    author_email='skubriev@cvisionlab.com',
+    license='GPL',
+    url='https://github.com/cvisionlabops/cvlops-python',
+    install_requires=[
+                  'jinja2'
+    ],
+    packages=packages,
+    package_data={'cvlops': ['jinja2/*.jinja2']}
+)
