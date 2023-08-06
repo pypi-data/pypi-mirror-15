3b0e67323a1a2a8cd9c58f3ba0f2349dc88655c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:10 +0000

    Add package nester1052451/1.3.0/nester1052451-1.3.0.zip

---
+++ packages/nester1052451/nester1052451-1.3.0.zip/nester1052451-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="nester1052451",
+    version="1.3.0",
+    py_modules=['nester'],
+    author='Levee',
+    author_email='584285553@qq.com',
+    description='A simple printer of nested listd',
+    )
