1df997e3f57dd4037af7fd8db929149581242504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:33 +0000

    Add package aioparrot/0.1.0/aioparrot-0.1.0.tar.gz

---
+++ packages/aioparrot/aioparrot-0.1.0.tar.gz/aioparrot-0.1.0/aioparrot/ardrone/client.py
@@ -0,0 +1,193 @@
+import asyncio
+import logging
+from enum import IntEnum
+
+from aioparrot.ardrone.factory import CommandFactory
+
+
+log = logging.getLogger(__name__)
+
+
+class Port(IntEnum):
+    NAVDATA = 5554
+    VIDEO = 5555
+    COMMAND = 5556
+
+
+class _Protocol(object):
+
+    WATCHDOG = 1.0
+
+    def __init__(self, loop):
+        self.loop = loop
+        self._transport = None
+        self._timer = None
+        self._ceiling = 5000
+        self._factory = CommandFactory()
+
+    @property
+    def speed(self):
+        return self._factory.speed
+
+    @speed.setter
+    def speed(self, value):
+        self._factory.speed = value
+
+    @property
+    def ceiling(self):
+        return self._ceiling
+
+    @ceiling.setter
+    def ceiling(self, value):
+        self._ceiling = value
+        self._altitude()
+
+    @property
+    def duration(self):
+        return asyncio.sleep(self.speed * 20)
+
+    def connection_made(self, transport):
+        self._transport = transport
+        self._start_timer()
+
+    def connection_lost(self, exc):
+        if self._timer:
+            self._timer.cancel()
+        log.debug("Command stream closed")
+
+    def datagram_received(self, data, addr):
+        data = data.decode()
+        log.debug("Command stream received: %s", data)
+
+    def error_received(self, exc):
+        log.error("Command stream received an error: %s", str(exc))
+
+    def send(self, data):
+        if self._timer:
+            self._timer.cancel()
+        if not self._transport:
+            log.error("Command stream can't send: %s", data)
+            return
+        self._transport.sendto(data.encode())
+        log.debug("Command stream sent: %s", data)
+        self._start_timer()
+
+    def _start_timer(self):
+        when = self.loop.time() + self.WATCHDOG
+        self._timer = self.loop.call_at(when, self._ping)
+
+    def _ping(self):
+        data = self._factory.ping()
+        self.send(data)
+
+    def _altitude(self):
+        data = self._factory.altitude(self._ceiling)
+        self.send(data)
+
+    def trim(self):
+        data = self._factory.trim()
+        self.send(data)
+
+    def panic(self):
+        data = self._factory.emergency()
+        self.send(data)
+
+    def auto(self, start=True):
+        data = self._factory.auto(start)
+        self.send(data)
+
+    async def halt(self):
+        data = self._factory.emergency()
+        self.send(data)
+
+    async def takeoff(self):
+        self.trim()
+        self._altitude()
+        data = self._factory.takeoff()
+        self.send(data)
+        await self.duration
+
+    async def land(self):
+        data = self._factory.land()
+        self.send(data)
+        await self.duration
+
+    async def hover(self):
+        data = self._factory.hover()
+        self.send(data)
+        await self.duration
+
+    async def left(self, unit=1):
+        data = self._factory.left(unit)
+        self.send(data)
+        await self.duration
+
+    async def right(self, unit=1):
+        data = self._factory.left(unit)
+        self.send(data)
+        await self.duration
+
+    async def forward(self, unit=1):
+        data = self._factory.left(unit)
+        self.send(data)
+        await self.duration
+
+    async def backward(self, unit=1):
+        data = self._factory.left(unit)
+        self.send(data)
+        await self.duration
+
+    async def down(self, unit=1):
+        data = self._factory.left(unit)
+        self.send(data)
+        await self.duration
+
+    async def up(self, unit=1):
+        data = self._factory.left(unit)
+        self.send(data)
+        await self.duration
+
+    async def turn_left(self, unit=1):
+        data = self._factory.left(unit)
+        self.send(data)
+        await self.duration
+
+    async def turn_right(self, unit=1):
+        data = self._factory.left(unit)
+        self.send(data)
+        await self.duration
+
+
+class Client(asyncio.Future):
+
+    DRONE_ADDR = '192.168.1.1'
+
+    def __init__(self, *, loop=None):
+        super().__init__(loop=loop)
+        self._transport = None
+        self._protocol = None
+
+    @property
+    def transport(self):
+        return self._transport
+
+    @property
+    def protocol(self):
+        return self._protocol
+
+    async def start(self):
+        future = self._loop.create_datagram_endpoint(
+            lambda: _Protocol(self._loop),
+            remote_addr=(self.DRONE_ADDR, Port.COMMAND.value)
+        )
+        self._transport, self._protocol = await future
+
+    async def stop(self):
+        await self._protocol.halt()
+        self._transport.close()
+
+    def __getattribute__(self, name):
+        try:
+            return super().__getattribute__(name)
+        except AttributeError:
+            return getattr(self._protocol, name)
---
+++ packages/aioparrot/aioparrot-0.1.0.tar.gz/aioparrot-0.1.0/aioparrot/ardrone/factory.py
@@ -0,0 +1,107 @@
+from aioparrot.ardrone.utils import parrot_str
+
+
+def move(progressive):
+    """
+    * left-right tilt (float): from -1 (left) to +1 (right).
+    * front-back tilt (float): from -1 (forwards) to +1 (backwards).
+    * vertical speed (float): from -1 (down) to +1 (up).
+    * angular speed (float): from -1 (spin left) to +1 (spin right).
+    + progressive (boolean): disable/enable (for hovering).
+    """
+    def wrapper(func):
+        def call(*args, **kwargs):
+            data = func(*args, **kwargs)
+            data = [int(progressive)] + [float(value) for value in data]
+            return args[0].format('PCMD', data)
+        return call
+    return wrapper
+
+
+def action(func):
+    """
+    * flag (binary): 10 bits flag.
+    """
+    def call(*args, **kwargs):
+        data = 0b10001010101000000000000000000
+        data += func(*args, **kwargs)
+        return args[0].format('REF', [data])
+    return call
+
+
+class CommandFactory(object):
+    """
+    ARDrone 1 and 2 controller.
+    """
+    def __init__(self, speed=0.2):
+        self._seq = 0
+        self.speed = speed
+
+    @property
+    def get_seq(self):
+        self._seq += 1
+        return self._seq
+
+    def format(self, command, data):
+        params = [str(self.get_seq)] + [parrot_str(value) for value in data]
+        return 'AT*{}={}\r'.format(command, ','.join(params))
+
+    def altitude(self, max_alt):
+        return self.format('CONFIG', ['control:altitude_max', str(max_alt)])
+
+    @action
+    def takeoff(self):
+        return 0b1000000000
+
+    @action
+    def land(self):
+        return 0b0000000000
+
+    def trim(self):
+        return self.format('FTRIM', [])
+
+    @move(progressive=False)
+    def hover(self):
+        return [0, 0, 0, 0]
+
+    @move(progressive=True)
+    def left(self, unit=1):
+        return [-(self.speed * unit), 0, 0, 0]
+
+    @move(progressive=True)
+    def right(self, unit=1):
+        return [self.speed * unit, 0, 0, 0]
+
+    @move(progressive=True)
+    def forward(self, unit=1):
+        return [0, -(self.speed * unit), 0, 0]
+
+    @move(progressive=True)
+    def backward(self, unit=1):
+        return [0, self.speed * unit, 0, 0]
+
+    @move(progressive=True)
+    def down(self, unit=1):
+        return [0, 0, -(self.speed * unit), 0]
+
+    @move(progressive=True)
+    def up(self, unit=1):
+        return [0, 0, self.speed * unit, 0]
+
+    @move(progressive=True)
+    def turn_left(self, unit=1):
+        return [0, 0, 0, -(self.speed * unit)]
+
+    @move(progressive=True)
+    def turn_right(self, unit=1):
+        return [0, 0, 0, self.speed * unit]
+
+    @action
+    def emergency(self):
+        return 0b0100000000
+
+    def auto(self, start=True):
+        return self.format('AFLIGHT', [int(start)])
+
+    def ping(self):
+        return self.format('COMWDG', [])
