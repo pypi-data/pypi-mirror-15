3ec24b04ce396b45aef721235250fe5e433ba44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:59 +0000

    Add package spark_mooc_meta/1.0.0/spark_mooc_meta-1.0.0.tar.gz

---
+++ packages/spark_mooc_meta/spark_mooc_meta-1.0.0.tar.gz/spark_mooc_meta-1.0.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+  name = 'spark_mooc_meta',
+  packages = ['spark_mooc_meta'],
+  version = '1.0.0',
+  description = 'Meta-package for Spark MOOCs; pulls in other packages.',
+  install_requires = ['spark_notebook_helpers>=1.0.1',
+                      'databricks_test_helper>=1.0.0'],
+  author = 'Databricks, Inc.',
+  author_email = 'training-logins@databricks.com',
+  keywords = ['spark', 'mooc', 'databricks'],
+  license = "Creative Commons Attribution-NonCommercial 4.0 International",
+  classifiers = [],
+)
