71c086b14d646030c575faac537822cbfefcfb9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:45 +0000

    Add package WebLurker/0.3.0/WebLurker-0.3.0.tar.gz

---
+++ packages/WebLurker/WebLurker-0.3.0.tar.gz/WebLurker-0.3.0/WebLurker/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.3.0'
+__github_url__ = 'https://javierlunamolina.wordpress.com/projects/weblurker/'
+from .WebLurker import WebLurker, Delayer, Extractor
+from .cache import PickleCache, BaseCache, FileCache
+from .exception import NoBrowserError
+from .util import HTMLTools
