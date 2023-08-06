392c792bdc842a46f0b6f1711f4fb7052f62613d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:05 +0000

    Add package storage_stats/0.2/storage_stats-0.2-py2-none-any.whl

---
+++ packages/storage_stats/storage_stats-0.2-py2-none-any.whl/storagestats/__main__.py
@@ -0,0 +1,56 @@
+# Copyright 2016 Peter May
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+__author__ = 'pmay'
+
+import argparse
+import os
+import storagestats as ss
+import sys
+
+# Retrieve version information
+__version__ = ''
+with open(os.path.join("..", 'VERSION')) as version_file:
+    __version__ = version_file.read().strip()
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    # Process CLI arguments #
+    ap = argparse.ArgumentParser(prog="storage_stats",
+                                 description="Calculates file size statistics for the specified folder")
+    ap.add_argument("path", help="the folder to characterise")
+    ap.add_argument("-o", dest="output", help="CSV file to output statistics too")
+    ap.add_argument("--no-recursion", dest="recursive", action="store_false",
+                    help="do not include sub-folders in stats")
+    ap.add_argument("-s", "--silent", dest="silent", action="store_true",
+                    help="turn off command line output (useful if you just want to output a CSV file")
+    ap.add_argument("-v", "--version", action="version", version='%(prog)s v'+__version__, help="display program version")
+    args = ap.parse_args()
+
+    if args.path:
+        # process the specified directory and print the stats
+        characteriser = ss.Characteriser()
+        characteriser.process_directory(args.path, args.recursive)
+
+        if not args.silent:
+            characteriser.print_stats()
+
+        if args.output:
+            characteriser.write_csv(args.output)
+
+if __name__ == "__main__":
+    main()
