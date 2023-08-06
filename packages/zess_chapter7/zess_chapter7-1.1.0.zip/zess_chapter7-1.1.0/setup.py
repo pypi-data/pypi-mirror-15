6c11e607c28ec4fb022a8a388b0821464e88d95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:15 +0000

    Add package zess_chapter7/1.1.0/zess_chapter7-1.1.0.zip

---
+++ packages/zess_chapter7/zess_chapter7-1.1.0.zip/zess_chapter7-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'zess_chapter7',
+    version = '1.1.0',
+    py_modules = ['zess_chapter7'],
+    author = 'zess',
+    author_email = 'zess1982@126.com',
+    url = 'http://satellite.sinaapp.com/',
+    description = 'Timing webapp site.',
+    )
