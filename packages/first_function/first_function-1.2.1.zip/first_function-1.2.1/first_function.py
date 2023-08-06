a8532432e76ab7f344a51b05a59be0eff8f41c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:28 +0000

    Add package first_function/1.2.1/first_function-1.2.1.zip

---
+++ packages/first_function/first_function-1.2.1.zip/first_function-1.2.1/first_function.py
@@ -0,0 +1,15 @@
+"""This module contains just 
+one function called print_lol"""
+def print_lol(the_list,indent = False, tabs = 0):
+        """Print List of List function prints list
+        objects and nested list objects. Each 
+        data item in the provided list is 
+        printed to the screen on it's own line"""
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        print_lol(each_item, indent, tabs + 1)
+                else:
+                        if indent:
+                                for num in range(tabs):
+                                        print("\t", end = '')
+                        print(each_item)
