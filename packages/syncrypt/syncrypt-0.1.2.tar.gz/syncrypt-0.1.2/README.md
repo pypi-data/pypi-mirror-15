1d3e307e1edc03c230a457d95b2a0e03e26f62de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:23 +0000

    Add package syncrypt/0.1.2/syncrypt-0.1.2.tar.gz

---
+++ packages/syncrypt/syncrypt-0.1.2.tar.gz/syncrypt-0.1.2/README.md
@@ -0,0 +1,137 @@
+# Syncrypt client
+
+Syncrypt is a fully encrypted file storage service for groups and private
+backups. This is the client that you can use to store and retrieve files.
+The client will also do all encryption.
+
+The source code for this client is released under the GNU General Public License
+Version 3. For more information have a look at the `LICENSE` file in this
+directory. Additional information on the GNU GPLv3 can be found here:
+http://www.gnu.org/licenses/quick-guide-gplv3.html
+
+## Installation
+
+Option 1: Download one of the distribution packages:
+
+* OS X (*TBD*)
+* Linux (*TBD*)
+* Windows (*TBD*)
+
+Option 2: Install from PyPI (*TBD*)
+
+    pip install syncrypt-client
+
+Option 3: Install from source:
+
+    pip install git+https://github.com/syncrypt/client
+
+## Let's get started
+
+The ``syncrypt`` executable is the command line interface for Syncrypt. You
+can login to syncrypt so that you won't have to enter your login for every
+command you issue.
+
+    syncrypt login
+
+This will ask for the email and the password of your Syncrypt account. If you
+do not have an account yet, please [sign up](https://syncrypt.space/) for our
+closed alpha mailing list and we will send you an invite.
+
+You can now initialize an arbitary directory as a vault. Simply change into
+this directory using ``cd`` and call:
+
+    syncrypt init
+
+After you've set up the directory, you can push all of its contents by typing:
+
+    syncrypt push
+
+To retrieve files from Syncrypt, simply write:
+
+    syncrypt pull
+
+Print out vault information via:
+
+    syncrypt info
+
+You can also give your vault a name. This name will be visible to other users
+of the vault as well, but not to anyone else. To set a new name, use:
+
+    syncrypt set vault.name "My Library"
+    syncrypt push
+
+### Securely share your vault
+
+In order to share your vault with another user, use:
+
+    syncrypt add-user mail@example.org
+
+This command will add the user to the vault. To safely transfer the vault's
+private key to the user, the command will also download the user's public
+keys and encrypt the vault information with it. The package will then be
+sent to the new user over the Syncrypt server. Note that the server will
+never be able to see the vault's private key.
+
+Make sure that the other user has uploaded at least one public key. The public
+keys get uploaded automatically after a successful ``login`` (see above) or via:
+
+    syncrypt keys --upload
+
+### Backup your keys
+
+It is important to store your vault's keys at another place other than your
+computer. Otherwise, you won't be able to recover your files in case of a
+disk failure. We can't give you your keys back if you lose them, since we never
+store them on our servers. This is deliberate and to protect your data against
+theft and unauthorized access.
+
+You can create a ZIP file with all necessary information to decrypt a Vault by
+typing:
+
+    syncrypt export -o vault-backup.zip
+
+We recommend to save this file on a USB stick or a similar disconnected
+storage and keep it in a safe place.
+
+In order to restore a backup or download your vault on another machine, copy
+this file into an empty folder, and type:
+
+    unzip vault-backup.zip
+    syncrypt init
+    syncrypt pull
+
+### Advanced Usage
+
+Watch current directory (this is like a daemon running the foreground as it
+also provides the HTTP interface):
+
+    syncrypt watch
+
+For each command listed above, alternate directories can be specified with
+``-d``, like:
+
+    syncrypt -d ~/myfolder watch
+
+Debug logging can be activated via ``-l DEBUG``.
+
+
+## Tests
+
+Run tests:
+
+    bin/pip install -e '.[test]'
+    bin/python setup.py test
+
+## Develop
+
+Setup in virtualenv for development:
+
+    virtualenv .
+    bin/pip install -e '.[dev]'
+
+## Deploy
+
+Make Syncrypt distribution package for the current platform:
+
+    bin/pip install -e '.[dist]'
+    bin/python setup.py dist
---
+++ packages/syncrypt/syncrypt-0.1.2.tar.gz/syncrypt-0.1.2/setup.py
@@ -0,0 +1,137 @@
+# Always prefer setuptools over distutils
+import os
+import sys
+from codecs import open
+from distutils.core import Command, setup
+from os import path
+
+from setuptools import find_packages, setup
+
+__name__ = 'syncrypt'
+__version__ = '0.1.2'
+
+here = path.abspath(path.dirname(__file__))
+
+cmdclass = {}
+
+# import build_ui
+try:
+    from pyqt_distutils.build_ui import build_ui
+    cmdclass['build_ui'] = build_ui
+except ImportError:
+    build_ui = None  # user won't have pyqt_distutils when deploying
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+class DistCommand(Command):
+    description = "packages syncrypt client for the current platform"
+    user_options = []
+
+    def initialize_options(self): pass
+
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+
+    def run(self):
+        import platform
+
+        assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
+        os.system('rm -rf ./dist/syncrypt*.zip')
+        os.system('rm -rf ./dist/syncrypt')
+        os.system('PYTHONPATH=lib/python3.5/site-packages/ pyinstaller syncrypt.spec')
+        #os.system('cp ./dist/syncrypt_gui/* ./dist/syncrypt')
+        zipname = '{name}-{version}.{platform}-{machine}.zip'.format(
+                name=__name__,
+                version=__version__,
+                platform=platform.system().lower(),
+                machine=platform.machine()
+        )
+        os.system('cd dist; zip {0} -r ./syncrypt'.format(zipname))
+        print("Generated {0}".format(os.path.join('dist', zipname)))
+
+cmdclass['dist'] = DistCommand
+
+setup(
+    name=__name__,
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=__version__,
+
+    description='Syncrypt client',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/syncrypt/client',
+
+    # Author details
+    author='Hannes Gräuler',
+    author_email='hannes@syncrypt.space',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Intended Audience :: End Users/Desktop',
+
+        'Development Status :: 3 - Alpha',
+
+        'Topic :: Communications :: File Sharing',
+        'Topic :: System :: Archiving',
+
+        'Programming Language :: Python :: 3',
+    ],
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    scripts=[
+        'scripts/syncrypt',
+    ],
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'pycrypto',
+        'aiofiles',
+        'aiohttp',
+        'umsgpack',
+        'colorlog',
+        'hachiko',
+        'python-snappy',
+        'erlastic',
+        'tzlocal',
+        'iso8601'
+    ],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': [
+            'pyqt-distutils'
+        ],
+        'dist': [
+            'pyinstaller'
+        ],
+        'test': [
+            'pytest-runner',
+            'pytest',
+            'asynctest',
+            'hypothesis'
+        ],
+    },
+
+    cmdclass=cmdclass
+)
