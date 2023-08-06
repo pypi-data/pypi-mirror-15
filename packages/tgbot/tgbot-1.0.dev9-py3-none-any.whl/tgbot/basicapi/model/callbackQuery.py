47c0a65113653661a2af1e4ab434930514d67340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:51 +0000

    Add package tgbot/1.0.dev9/tgbot-1.0.dev9-py3-none-any.whl

---
+++ packages/tgbot/tgbot-1.0.dev9-py3-none-any.whl/tgbot/basicapi/model/callbackQuery.py
@@ -0,0 +1,37 @@
+
+from tgbot.basicapi.model.base import Base
+
+from tgbot.basicapi.model.message import Message
+
+from tgbot.basicapi.model.user import User
+
+class CallBackQuery(Base):
+
+    def __createfromdata__(self, data):
+        self.data = data
+        self.callback_data = data["data"]
+
+        self.callback_id = data["id"]
+
+        self.from_User = User(data=data["from"])
+
+        self.message = Message(data=data["message"])
+
+        self.inline_message_id = data["inline_message_id"]
+
+
+    def __init__(self, callback_data=None,callback_id=None,from_User=None,message=None,inline_message_id=None,data=None):
+        super().__init__()
+        if data:
+            self.__createfromdata__(data)
+        else:
+            self.callback_id = callback_id
+
+            self.from_User = from_User
+
+            self.message = message
+
+            self.inline_message_id = inline_message_id
+
+            self.callback_data = callback_data
+
