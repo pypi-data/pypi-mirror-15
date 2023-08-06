4c0818272f57b3771c041d33f11b416a72a9c624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:27 +0000

    Add package cjmk1/1.0.0/cjmk1-1.0.0.zip

---
+++ packages/cjmk1/cjmk1-1.0.0.zip/cjmk1-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name  = 'cjmk1',
+		version = '1.0.0',
+		py_modules = ['cjmk1'],
+		author = 'xudandan',
+		author_email = '446859696@qq.com',
+		url = 'http://www.headfirstlabs.com',
+		description = 'a simple printer',
+
+)
