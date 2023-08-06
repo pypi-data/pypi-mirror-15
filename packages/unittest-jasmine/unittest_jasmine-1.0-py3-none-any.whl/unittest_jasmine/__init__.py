f392cf8bc32b04ab621e775bae2e1f68ac88672a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:48 +0000

    Add package unittest-jasmine/1.0/unittest_jasmine-1.0-py3-none-any.whl

---
+++ packages/unittest-jasmine/unittest_jasmine-1.0-py3-none-any.whl/unittest_jasmine/__init__.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+# unittest-jasmine
+# Copyright (C) 2015 Moses Palmér
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
+from . import _node as node
+from . import _package_manager as package_manager
+from . import _runner as runner
+from . import _data as data
+from . import _tb as tb
+from . import _unittest as unittest
+
+from ._setuptools import SetuptoolsLoader
