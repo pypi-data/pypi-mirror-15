08d9a770ee7c1bc253396356528b6bd0f5e52caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:24 +0000

    Add package libtmux/0.5.0/libtmux-0.5.0.tar.gz

---
+++ packages/libtmux/libtmux-0.5.0.tar.gz/libtmux-0.5.0/libtmux/exc.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""libtmux exceptions.
+
+libtmux.exc
+~~~~~~~~~~~
+
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals, with_statement)
+
+
+class LibTmuxException(Exception):
+
+    """Base Exception for libtmux Errors."""
+
+
+class TmuxSessionExists(LibTmuxException):
+
+    """Session does not exist in the server."""
+
+    pass
+
+
+class UnknownOption(LibTmuxException):
+
+    """Option unknown to tmux show-option(s) or show-window-option(s)."""
+
+    pass
