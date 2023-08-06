745d79474538d71dc2ba8dfe635f5ad72bc2c9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:38 +0000

    Add package consoletk/1.0/consoletk-1.0.tar.gz

---
+++ packages/consoletk/consoletk-1.0.tar.gz/consoletk-1.0/setup.py
@@ -0,0 +1,12 @@
+ # -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='consoletk',
+      version='1.0',
+      description='stand-alone, ANSI-based console UI toolkit',
+      author='Séverin Lemaignan',
+      author_email='severin@guakamole.org',
+      url='https://github.com/severin-lemaignan/consoletk',
+      py_modules=['consoletk'],
+      )
