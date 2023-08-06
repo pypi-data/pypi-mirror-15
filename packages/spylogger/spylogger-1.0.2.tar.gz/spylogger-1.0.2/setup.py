7b7460395aed135cefd52b30e18f46f18667079a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:36 +0000

    Add package spylogger/1.0.2/spylogger-1.0.2.tar.gz

---
+++ packages/spylogger/spylogger-1.0.2.tar.gz/spylogger-1.0.2/setup.py
@@ -0,0 +1,27 @@
+import os
+from pip.req import parse_requirements
+from pip.download import PipSession
+from setuptools import setup
+
+INSTALL_REQS = parse_requirements('requirements.pip', session=PipSession())
+
+setup(
+    name='spylogger',
+    packages=['spylogger'],
+    version=open('VERSION').read().strip(),
+    include_package_data=True,
+    description='Python logging library',
+    long_description=open('README.rst').read(),
+    url='https://github.com/SPSCommerce/spylogger',
+    author='meganlkm',
+    author_email='webapps@spscommerce.com',
+    install_requires=[str(ir.req) for ir in INSTALL_REQS],
+    keywords=['logging'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Logging',
+    ]
+)
