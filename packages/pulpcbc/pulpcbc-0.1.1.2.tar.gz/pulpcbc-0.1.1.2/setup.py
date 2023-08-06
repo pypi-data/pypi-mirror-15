b8c5967107a6226828514b442bf531b14f308de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:29 +0000

    Add package pulpcbc/0.1.1.2/pulpcbc-0.1.1.2.tar.gz

---
+++ packages/pulpcbc/pulpcbc-0.1.1.2.tar.gz/pulpcbc-0.1.1.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_modules = [Extension('pulpcbc',
+                         ['pulpcbc.pyx', 'CyEventHandler.cpp'],
+                         libraries=['CbcSolver', 'Cbc', 'Cgl', 'Osi', 'OsiClp',
+                                    'Clp', 'CoinUtils', 'rt', 'bz2', 'z',
+                                    'blas', 'lapack'],
+                         extra_compile_args=['-fpermissive'],
+                         extra_link_args=['-Wl,--no-as-needed'],
+                         language="c++",
+                         
+                        )]
+
+setup(
+    name = 'pulpcbc',
+    version = '0.1.1.2',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules,
+#    package_data = {'headers': ['CyEventHandler.h']},
+)
