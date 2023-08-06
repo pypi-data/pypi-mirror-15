c7419e6bb0c0c077ece5c0ea55f5295296d4bec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:12 +0000

    Add package PrintListRecursive/1.1.0/PrintListRecursive-1.1.0.zip

---
+++ packages/PrintListRecursive/PrintListRecursive-1.1.0.zip/PrintListRecursive-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+		name = 'PrintListRecursive',
+		version = '1.1.0',
+		py_modules = ['PrintListRecursive'],
+		author = 'sbworld',
+		author_email = 'mulsae2@daum.net',
+		url = '',
+		description = 'A simple print of nested list',
+	)
