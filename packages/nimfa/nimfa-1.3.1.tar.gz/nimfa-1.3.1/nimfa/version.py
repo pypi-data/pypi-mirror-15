596b0f853c525d8c08e55c0b8826022ba38dc304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:20 +0000

    Add package nimfa/1.3.1/nimfa-1.3.1.tar.gz

---
+++ packages/nimfa/nimfa-1.3.1.tar.gz/nimfa-1.3.1/nimfa/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM NIMFA SETUP.PY
+short_version = '1.3.1'
+version = '1.3.1'
+full_version = '1.3.1'
+git_revision = '8e11177182e76eed898841f07546bf63b9132efd'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
