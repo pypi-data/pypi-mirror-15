1c2d808161c3e02019d905fb1dc946a551cd1f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:50 +0000

    Add package pykern/20160512.164225/pykern-20160512.164225.zip

---
+++ packages/pykern/pykern-20160512.164225.zip/pykern-20160512.164225/tests/pksetup_data/conf1/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from pykern.pksetup import setup
+
+setup(
+    name='conf1',
+    description='Conformance One',
+    author='RadiaSoft LLC.',
+    author_email='pip@pykern.org',
+    url='http://pykern.org',
+    pksetup={
+        'extra_directories':['examples'],
+    },
+)
