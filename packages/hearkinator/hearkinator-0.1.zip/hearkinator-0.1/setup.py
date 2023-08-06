12570b2ae0e5e740d5ad291cf41633709b75189a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:12 +0000

    Add package hearkinator/0.1/hearkinator-0.1.zip

---
+++ packages/hearkinator/hearkinator-0.1.zip/hearkinator-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'hearkinator',
+  packages = ['hearkinator'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Bot Slack HeArkinator',
+  author = 'Julien Baumgartner Charles Ombang Ndo',
+  author_email = 'julien.baumgartner@he-arc.ch',
+  url = 'https://github.com/julien-baumgartner/slackBot', # use the URL to the github repo
+  download_url = 'https://github.com/julien-baumgartner/slackBot/tarball/0.1', # I'll explain this in a second
+  keywords = ['slack', 'bot', 'hearkinator'], # arbitrary keywords
+  classifiers = [],
+)
