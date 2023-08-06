3c97363722a85a441ef21e57c5037dfd531d002a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:57 +0000

    Add package django-scarface/3.0a7/django-scarface-3.0a7.tar.gz

---
+++ packages/django-scarface/django-scarface-3.0a7.tar.gz/django-scarface-3.0a7/scarface/settings.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__author__ = 'janmeier'
+SCARFACE_DEFAULT_PLATFORM_STRATEGIES = [
+    'scarface.platform_strategy.APNPlatformStrategy',
+    'scarface.platform_strategy.APNSSandboxPlatformStrategy',
+    'scarface.platform_strategy.GCMPlatformStrategy'
+]
