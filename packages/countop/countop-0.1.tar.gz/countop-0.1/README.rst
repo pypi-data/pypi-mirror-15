b497b18de6bff1b9b82166ed8ce7f05ec73097b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:31 +0000

    Add package countop/0.1/countop-0.1.tar.gz

---
+++ packages/countop/countop-0.1.tar.gz/countop-0.1/README.rst
@@ -0,0 +1,7 @@
+countop
+--------
+
+To use (with caution), simply do::
+
+    >>> import countop as cop
+    >>> print cop.joke()
---
+++ packages/countop/countop-0.1.tar.gz/countop-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='countop',
+      version='0.1',
+      description='Count basic operations in algorithm implementations.',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 2.7'
+      ],
+      keywords='algorithms, count operations',
+      url='https://github.com/ankur-gupta/countop',
+      author='Ankur Gupta',
+      author_email='ankur@perfectlyrandom.org',
+      license='MIT',
+      packages=['countop'],
+      zip_safe=False)
