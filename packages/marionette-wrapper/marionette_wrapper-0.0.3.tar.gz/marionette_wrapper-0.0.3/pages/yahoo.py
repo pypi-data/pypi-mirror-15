c9731249d4c4698cd5803b95666732f36f975831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:23 +0000

    Add package marionette-wrapper/0.0.3/marionette_wrapper-0.0.3.tar.gz

---
+++ packages/marionette-wrapper/marionette_wrapper-0.0.3.tar.gz/marionette_wrapper-0.0.3/pages/yahoo.py
@@ -0,0 +1,26 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from marionette_driver import By
+
+from base import Base
+
+
+class Yahoo(Base):
+    _yahoo_search_input_box_locator = (By.ID, 'uh-search-box')
+    _yahoo_search_button_locator = (By.ID, 'uh-search-button')
+    _yahoo_search_mozilla_result_link_locator = (
+        By.CSS_SELECTOR, 'ol li.first div div.layoutCenter div.compTitle h3 a')
+
+    def __init__(self, marionette, url):
+        super(Yahoo, self).__init__(marionette)
+        self.launch(url)
+        self.wait_for_element_displayed(*self._yahoo_search_input_box_locator)
+
+    def search_for_query(self, query_string):
+        self.send_keys_to_element(
+            *self._yahoo_search_input_box_locator, string=query_string)
+        self.click_element(*self._yahoo_search_button_locator)
+        from pages.search_results import SearchResults
+        return SearchResults(self.marionette, query_string)
