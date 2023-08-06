07cc85cea05bda7b1d391cb01948ca7ca5fd35a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:06 +0000

    Add package misc_fields/0.1/misc_fields-0.1.tar.gz

---
+++ packages/misc_fields/misc_fields-0.1.tar.gz/misc_fields-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'misc_fields',
+  packages = ['misc_fields'],
+  version = '0.1',
+  description = 'Some fields for use with peewee ORM',
+  author = 'Rony Batista',
+  author_email = 'rony.batista29@gmail.com',
+  url = 'https://github.com/ronyb29/misc_fields',
+  keywords = ['peewee','fields','strategy', 'peewee xml'],
+  classifiers = [],
+)
