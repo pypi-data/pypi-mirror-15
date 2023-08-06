196c8db509c083317fb449a15b22ff1625743e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:39 +0000

    Add package bweb/0.2.0/bweb-0.2.0.tar.gz

---
+++ packages/bweb/bweb-0.2.0.tar.gz/bweb-0.2.0/bweb/application.py
@@ -0,0 +1,17 @@
+# web server application
+# -- tornado is the best way to create an asyncronous, real-time app in python.
+
+import tornado.ioloop, tornado.web
+from bl.dict import Dict
+from bl.log import Log
+
+class Application(tornado.web.Application, Dict):
+    def __init__(self, routes=None, default_host='', transforms=None, **settings):
+        tornado.web.Application.__init__(self, routes, default_host=default_host, transforms=transforms)
+        self.settings = Dict(**settings)
+
+    def __call__(self, port=None):
+        self.listen(port or (self.settings.Site or Dict()).port or 80)
+        self.log(self.settings.json(indent=2))
+        tornado.ioloop.IOLoop.instance().start()
+
