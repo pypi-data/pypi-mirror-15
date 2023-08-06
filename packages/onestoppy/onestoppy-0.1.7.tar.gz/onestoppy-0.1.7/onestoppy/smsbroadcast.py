5662fe9fe70be973734cdf07f3c5a379097283e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:06 +0000

    Add package onestoppy/0.1.7/onestoppy-0.1.7.tar.gz

---
+++ packages/onestoppy/onestoppy-0.1.7.tar.gz/onestoppy-0.1.7/onestoppy/smsbroadcast.py
@@ -0,0 +1,37 @@
+import consul, requests
+
+def send(to, message, from_number=None, source=None):
+    c = consul.Consul(host='consul.service.onestop')
+
+    proxies = {
+      'http': 'http://proxy.query.onestop:3128',
+      'https': 'http://proxy.query.onestop:3128'
+    }
+
+    index, data = c.kv.get('global/config/common/smsbroadcast/username')
+    sms_username = data['Value']
+
+    index, data = c.kv.get('global/config/common/smsbroadcast/username')
+    sms_username = data['Value']
+
+    index, data = c.kv.get('global/config/common/smsbroadcast/password')
+    sms_password = data['Value']
+
+    index, data = c.kv.get('global/config/common/smsbroadcast/url')
+    sms_url = data['Value']
+
+    payload = {
+        'username': sms_username,
+        'password': sms_password,
+        'source': source,
+        'from': from_number,
+        'to': to,
+        'message': message }
+
+    result = requests.post(sms_url, data=payload, proxies=proxies)
+
+    if result.status_code == 200:
+        return True
+
+    return False
+
