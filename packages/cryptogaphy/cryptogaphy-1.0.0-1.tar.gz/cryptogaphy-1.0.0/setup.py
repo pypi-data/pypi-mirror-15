4d654ee309615f948563c2167ea008197d858ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:56 +0000

    Add package cryptogaphy/1.0.0/cryptogaphy-1.0.0-1.tar.gz

---
+++ packages/cryptogaphy/cryptogaphy-1.0.0-1.tar.gz/cryptogaphy-1.0.0/setup.py
@@ -0,0 +1,20 @@
+import os
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    if os.getenv("I_AM_MAKING_THIS_PACKAGE"):
+        setup(
+            name="cryptogaphy",
+            description='I think you meant "cryptography"',
+            license="MIT",
+            version="1.0.0",
+            author="cyli",
+            author_email="cyli@twistedmatrix.com",
+            maintainer="cyli",
+            maintainer_email="cyli@twistedmatrix.com",
+            long_description='I really think you meant "cryptography"',
+            packages=[],
+        )
+    else:
+        raise Exception('I think you meant "cryptography"')
