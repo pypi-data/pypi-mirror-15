729b8064140e42af3598a004aed4f67da3a6e28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:02 +0000

    Add package nesterbrant/1.0.0/nesterbrant-1.0.0.zip

---
+++ packages/nesterbrant/nesterbrant-1.0.0.zip/nesterbrant-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterbrant',
+    version = '1.0.0',
+    py_modules = ['nesterbrant'],
+    author = 'Brant Lv',
+    author_email = 'lvfenglin.love@126.com',
+    url = 'http://www.yitaiedu.com',
+    description = 'A sample printer of nested lists',
+    )
+
+    
