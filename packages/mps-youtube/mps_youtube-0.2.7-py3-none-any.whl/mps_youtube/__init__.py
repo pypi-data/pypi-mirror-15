35f86011982d99dd7a9fd695fa71ecfe6d2372f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:14 +0000

    Add package mps-youtube/0.2.7/mps_youtube-0.2.7-py3-none-any.whl

---
+++ packages/mps-youtube/mps_youtube-0.2.7-py3-none-any.whl/mps_youtube/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.2.7"
+__notes__ = "released 27 June 2016"
+__author__ = "np1"
+__license__ = "GPLv3"
+__url__ = "http://github.com/np1/mps-youtube"
+
+from . import init
+init.init()
+from . import main
