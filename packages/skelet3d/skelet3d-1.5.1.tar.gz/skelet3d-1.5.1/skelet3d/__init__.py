1280856b3838b0ce1101dcfc941c3a9ec6381c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:58 +0000

    Add package skelet3d/1.5.1/skelet3d-1.5.1.tar.gz

---
+++ packages/skelet3d/skelet3d-1.5.1.tar.gz/skelet3d-1.5.1/skelet3d/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ['skelet3d', 'skeleton_analyser', 'tree_processing']
+
+__version__ = "1.5.1"
+
+print "import skelet3d ok"
+import skeleton_analyser
+import tree_processing
+from skeleton_analyser import SkeletonAnalyser
+from tree_processing import TreeGenerator
+from skeletonizer import skelet3d
