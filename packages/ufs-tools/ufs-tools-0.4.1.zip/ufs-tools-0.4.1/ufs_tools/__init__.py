d52c9a59881b3924e4a5ec11b1ff35e6861d395b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:34 +0000

    Add package ufs-tools/0.4.1/ufs-tools-0.4.1.zip

---
+++ packages/ufs-tools/ufs-tools-0.4.1.zip/ufs-tools-0.4.1/ufs_tools/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Wang Richard'
+__email__ = 'richardwangwang@gmail.com'
+__version__ = '0.4.1'
+
+from ufs_tools import *
+from filetools import *
+from package_info import *
