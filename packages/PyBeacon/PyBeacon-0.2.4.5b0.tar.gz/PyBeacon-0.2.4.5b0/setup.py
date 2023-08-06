6eb7ca3df4b4ae1bb7005f4f33c7709d7229f0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:25 +0000

    Add package PyBeacon/0.2.4.5b0/PyBeacon-0.2.4.5b0.tar.gz

---
+++ packages/PyBeacon/PyBeacon-0.2.4.5b0.tar.gz/PyBeacon-0.2.4.5b0/setup.py
@@ -0,0 +1,60 @@
+import os
+from setuptools import setup
+
+version_file = open(os.path.join('PyBeacon', 'VERSION'))
+__version__ = version_file.read().strip()
+
+setup(
+    name = 'PyBeacon',
+    version = __version__,
+    packages = ['PyBeacon'],
+    entry_points = {
+        "console_scripts": ['PyBeacon = PyBeacon.PyBeacon:main']
+    },
+    install_requires = [
+        'bluez',
+        'bluez-hcidump'
+    ],
+
+    summary = 'Python script for scanning and advertising urls over Eddystone-URL.',
+
+    description = 'Python script for scanning and advertising urls over Eddystone-URL.',
+
+    url = 'https://github.com/nirmankarta/PyBeacon',
+
+    download_url = 'https://github.com/nirmankarta/PyBeacon/archive/master.zip',
+
+    author = 'Nirmankarta',
+
+    author_email = 'we@nirmankarta.com',
+
+    maintainer = 'Prabhanshu Attri',
+
+    maintainer_email = 'contact@prabhanshu.com',
+
+    license = 'Apache License 2.0',
+
+    keywords = ['Eddystone', 'Eddystone URL', 'Beacon', 'Raspberry Pi'],
+
+    classifiers = [
+       
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
