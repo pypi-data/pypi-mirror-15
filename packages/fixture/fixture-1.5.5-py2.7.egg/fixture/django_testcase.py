9f6b311786bd8b1c6034c9ce032fb93ceb93c6f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:01 +0000

    Add package fixture/1.5.5/fixture-1.5.5-py2.7.egg

---
+++ packages/fixture/fixture-1.5.5-py2.7.egg/fixture/django_testcase.py
@@ -0,0 +1,21 @@
+from django.test.testcases import TestCase
+
+from fixture import DjangoFixture
+
+
+class FixtureTestCase(TestCase):
+
+    """Overrides django's fixture setup and teardown code to use DataSets.
+
+    See :ref:`Using Fixture With Django <using-fixture-with-django>` for a complete example.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        fixture = DjangoFixture()
+        cls.data = fixture.data(*cls.datasets)
+        cls.data.setup()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.data.teardown()
