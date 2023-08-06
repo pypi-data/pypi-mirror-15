dcb51afafab456ccc08382fd2a7b95afcf23630a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:55 +0000

    Add package jeju/0.3.2/jeju-0.3.2.linux-x86_64.tar.gz

---
+++ packages/jeju/jeju-0.3.2.linux-x86_64.tar.gz/usr/local/lib/python2.7/dist-packages/jeju/executor/python.py
@@ -0,0 +1,48 @@
+###########################################
+# This is very naive replacement algorithm
+# TODO
+############################################
+import subprocess
+import string
+import uuid
+import logging
+
+# TODO: configurable variable
+TEMP_DIR = "/tmp"
+
+def replaceable(code, kv):
+    # change keyword to value
+    keys = kv.keys()
+    # find keyword which is ${keyword}
+    # replace value ${keyword} <- kv[keyword]
+    for key in keys:
+        nkey = "${%s}" % key
+        code = string.replace(code, nkey, kv[key])
+    logging.debug("#" * 20 + "\n%s" % code)
+    logging.debug("#" * 20)
+
+    return code
+
+def execute_python(**kwargs):
+    code = kwargs['code']
+    kv = kwargs['kv']
+
+    import os
+    # call replaceable
+    rcode = replaceable(code, kv)
+
+    temp = uuid.uuid1()
+    temp_file = "%s/%s" % (TEMP_DIR, str(temp))
+
+    fp = open(temp_file, 'w')
+    fp.write(rcode)
+    fp.close()
+
+    # Execute Cmd
+    cmd = ['python',temp_file]
+    proc = subprocess.Popen(cmd, stdout = subprocess.PIPE)
+    (out, err) = proc.communicate()
+
+    os.remove(temp_file)
+    return {'input':rcode, 'output':out, 'error':err}
+
