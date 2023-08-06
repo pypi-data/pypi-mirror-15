44643803d82336df6c48bf8b6e0b885d235ca69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:27 +0000

    Add package manoelgadi12/1.11/manoelgadi12-1.11.tar.gz

---
+++ packages/manoelgadi12/manoelgadi12-1.11.tar.gz/manoelgadi12-1.11/manoelgadi12/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'ersh'
+__email__ = 'ershov@student.ie.edu'
+__version__ = '1'
---
+++ packages/manoelgadi12/manoelgadi12-1.11.tar.gz/manoelgadi12-1.11/tests/test_manoelgadi12.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_manoelgadi12
+----------------------------------
+
+Tests for `manoelgadi12` module.
+"""
+
+
+import sys
+import unittest
+
+from manoelgadi12 import manoelgadi12
+
+
+
+class TestManoelgadi12(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
