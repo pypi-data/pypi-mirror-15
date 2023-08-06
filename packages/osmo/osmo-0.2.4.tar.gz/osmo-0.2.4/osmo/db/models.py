c32bc3b8dd714d2dbfa4ad2bc01b11c42d5bd303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:08 +0000

    Add package osmo/0.2.4/osmo-0.2.4.tar.gz

---
+++ packages/osmo/osmo-0.2.4.tar.gz/osmo-0.2.4/osmo/db/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+#   Refrence
+#       - oslo.db
+#
+#   File
+#       - oslo.db/oslo.db/sqlalchemy/models.py
+#
+
+from oslo_db.sqlalchemy import models
+
+ModelBase = models.ModelBase
