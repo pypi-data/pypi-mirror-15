77631bb93d639c8b294bb2c9249332b0fe9a8992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:33 +0000

    Add package railroad/0.2.0/railroad-0.2.0-py2.py3-none-any.whl

---
+++ packages/railroad/railroad-0.2.0-py2.py3-none-any.whl/railroad/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'Jindrich K. Smitka'
+__email__ = 'smitka.j@gmail.com'
+__version__ = '0.2.0'
+
+from .railroad import (  # noqa
+    prepare,
+    catch,
+    get_or_reraise,
+    compose,
+)
