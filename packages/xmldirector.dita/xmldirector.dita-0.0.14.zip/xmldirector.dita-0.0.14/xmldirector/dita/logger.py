0fe69fc7084b0f97d347d5a8a677e384b2656550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:42 +0000

    Add package xmldirector.dita/0.0.14/xmldirector.dita-0.0.14.zip

---
+++ packages/xmldirector.dita/xmldirector.dita-0.0.14.zip/xmldirector.dita-0.0.14/xmldirector/dita/logger.py
@@ -0,0 +1,12 @@
+################################################################
+# xmldirector.dita
+# (C) 2016,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+LOG = logging.getLogger('xmldirector.dita')
