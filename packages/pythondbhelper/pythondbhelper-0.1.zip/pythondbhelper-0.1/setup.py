a193f531348dfc9b40e7db203d5951275e7e1b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:19 +0000

    Add package pythondbhelper/0.1/pythondbhelper-0.1.zip

---
+++ packages/pythondbhelper/pythondbhelper-0.1.zip/pythondbhelper-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pythondbhelper',
+      version='0.1',
+      description='python mysql db hlelper class for crud.',
+      url='http://goo.gl/6qM6s7',
+      author='Waqas Ali',
+      author_email='waqasali.fastian@gmail.com',
+      license='FAST',
+      packages=['pythondbhelper'],
+      install_requires=[
+          'pymysql',
+      ],
+      zip_safe=False)
