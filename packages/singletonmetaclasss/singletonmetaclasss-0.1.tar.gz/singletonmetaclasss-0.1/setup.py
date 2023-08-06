55bbf44705660f9d79f3bb28d5983b7ea5a18e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:08 +0000

    Add package singletonmetaclasss/0.1/singletonmetaclasss-0.1.tar.gz

---
+++ packages/singletonmetaclasss/singletonmetaclasss-0.1.tar.gz/singletonmetaclasss-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'singletonmetaclasss',
+  packages = ['singletonmetaclasss'],
+  version = '0.1',
+  description = 'A metaclasss singleton',
+  author = 'Iddo Berger',
+  author_email = 'iddoberger@gmail.com',
+  url = 'https://github.com/iddoberger/singletonmetaclasss',
+  download_url = 'https://github.com/iddoberger/singletonmetaclasss/tarball/0.1',
+  keywords = ['singleton',], # arbitrary keywords
+  classifiers = [],
+)
