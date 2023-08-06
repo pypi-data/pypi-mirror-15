d056330d834f9271b68bbe66c4fd04a23de0f72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:26 +0000

    Add package pyxenon/0.2.0/pyxenon-0.2.0.tar.gz

---
+++ packages/pyxenon/pyxenon-0.2.0.tar.gz/pyxenon-0.2.0/xenon/util.py
@@ -0,0 +1,31 @@
+# PyXenon Xenon API wrapper
+#
+# Copyright 2015 Netherlands eScience Center
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import inspect
+import os
+
+
+def _local_function():
+    pass
+
+
+def module_path(local_function=_local_function):
+    """
+    Returns the module path without the use of __file__.
+
+    Requires a function defined locally in the module.
+    From http://stackoverflow.com/questions/729583
+    """
+    return os.path.abspath(inspect.getsourcefile(local_function))
