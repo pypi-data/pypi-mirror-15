aa5341744e9345e207c9d4598d2d5451c1d5d0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:08 +0000

    Add package buildorchestra/0.1.0/buildorchestra-0.1.0.tar.gz

---
+++ packages/buildorchestra/buildorchestra-0.1.0.tar.gz/buildorchestra-0.1.0/buildorchestra/result.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+
+class BuildResult:
+  def __init__(self, artifacts):
+    self.artifacts = artifacts
+
+  def copy_to(self, destDir):
+    os.makedirs(destDir)
+    for artifact in self.artifacts:
+      artifact.copy_to(destDir)
+
+
+class StepResult:
+  def __init__(self, artifacts):
+    self.artifacts = artifacts
+
+  def copy_to(self, destDir):
+    os.makedirs(destDir)
+    for artifact in self.artifacts:
+      artifact.copy_to(destDir)
+
+
+class Artifact:
+  def __init__(self, name, location, target):
+    self.name = name
+    self.location = location
+    self.target = target
+
+  def __str__(self):
+    return '"{}" at {}'.format(self.name, self.location)
+
+  def copy_to(self, destDir):
+    copyLocation = os.path.join(destDir, self.target)
+    print('Copying artifact {} to {}'.format(self, copyLocation))
+    shutil.copyfile(self.location, copyLocation)
