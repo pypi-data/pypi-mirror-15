364bf7f424803db287115d26873296379a2ec9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:07 +0000

    Add package mi_nester/1.0.1/mi_nester-1.0.1.tar.gz

---
+++ packages/mi_nester/mi_nester-1.0.1.tar.gz/mi_nester-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name         = 'mi_nester',
+	version      = '1.0.1',
+	py_modules   = ['mi_nester'],
+	author       = 'miki',
+	author_email = 'fangyu1313@gmail.com',
+	url          = 'http://www.twmp.com.tw',
+	description  = 'A simple',
+)
