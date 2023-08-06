da39da8cc7eb584e2d8741da7f498a8bc5bb63de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:17 +0000

    Add package Barionix/0.0.1/Barionix-0.0.1.tar.gz

---
+++ packages/Barionix/Barionix-0.0.1.tar.gz/Barionix-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'Barionix',
+  packages = ['Barionix'],
+  version = '0.0.1',
+  description = 'A Scraping Lib.',
+  author = 'Junior Mario',
+  author_email = 'myacount221100@gmail.com',
+  url = 'https://github.com/JuniorMario/Barionix', # use the URL to the github repo
+  download_url = 'https://github.com/JuniorMario/Barionix/tarball/0.1', # I'll explain this in a second
+  keywords = ['Barionix', 'Scraping', 'Crawling','requests','python'], # arbitrary keywords
+  classifiers = [],
+)
