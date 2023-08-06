124f5b5c45e0d6045599f6bb2e7a7f8a98183cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:34 +0000

    Add package sw-django-utils/0.0.22/sw-django-utils-0.0.22.tar.gz

---
+++ packages/sw-django-utils/sw-django-utils-0.0.22.tar.gz/sw-django-utils-0.0.22/setup.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# python setup.py sdist register upload
+from distutils.core import setup
+
+setup(
+    name='sw-django-utils',
+    version='0.0.22',
+    description='Soft Way company django utils.',
+    author='Telminov Sergey',
+    url='https://github.com/telminov/sw-django-utils',
+    packages=[
+        'djutils',
+        'djutils.views',
+        'djutils.tests',
+        'djutils.templatetags',
+        'djutils.management',
+        'djutils.management.commands',
+    ],
+    license='The MIT License',
+    install_requires=[
+        'django',
+        'sqlparse==0.1.16',
+    ],
+)
