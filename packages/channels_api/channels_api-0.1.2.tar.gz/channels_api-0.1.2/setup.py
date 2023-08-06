fe190f4ae4d9a8e43c07f24b03ff6a50f66fb871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:57 +0000

    Add package channels_api/0.1.2/channels_api-0.1.2.tar.gz

---
+++ packages/channels_api/channels_api-0.1.2.tar.gz/channels_api-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from channels_api import __version__
+
+setup(
+    name='channels_api',
+    version=__version__,
+    url='https://github.com/linuxlewis/channels-api',
+    author='Sam Bolgert',
+    author_email='sbolgert@gmail.com',
+    description="Helps build a REST-like API on top of WebSockets using channels.",
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.8',
+        'channels>=0.14',
+        'djangorestframework>=3.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
