8afba0883724030805f4fd73c927635671653ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:49 +0000

    Add package hellorap/0.1.1/hellorap-0.1.1.tar.gz

---
+++ packages/hellorap/hellorap-0.1.1.tar.gz/hellorap-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'hellorap',
+  packages = ['hellorap'], # this must be the same as the name above
+  version = '0.1.1',
+  description = 'A random test lib',
+  author = 'Syam Sasi',
+  author_email = 'syamsasi99@gmail.com',
+  keywords = ['testing', 'logging', 'example']
+)
