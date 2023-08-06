733a6bd567d4cdff73477da25cc0c63427ea4768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:54 +0000

    Add package rtv/1.9.1/rtv-1.9.1-py2.py3-none-any.whl

---
+++ packages/rtv/rtv-1.9.1-py2.py3-none-any.whl/rtv/exceptions.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+class EscapeInterrupt(Exception):
+    "Signal that the ESC key has been pressed"
+
+
+class ConfigError(Exception):
+    "There was a problem with the configuration"
+
+
+class RTVError(Exception):
+    "Base RTV error class"
+
+
+class AccountError(RTVError):
+    "Could not access user account"
+
+
+class SubmissionError(RTVError):
+    "Submission could not be loaded"
+
+
+class SubredditError(RTVError):
+    "Subreddit could not be reached"
+
+
+class SubscriptionError(RTVError):
+    "Subscriptions could not be fetched"
+
+
+class ProgramError(RTVError):
+    "Problem executing an external program"
+
+
+class BrowserError(RTVError):
+    "Could not open a web browser tab"
