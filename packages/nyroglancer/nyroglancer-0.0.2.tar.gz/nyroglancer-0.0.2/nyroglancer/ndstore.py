1919c937260c6a0235c039eeb34ca322cf49ef9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:46 +0000

    Add package nyroglancer/0.0.2/nyroglancer-0.0.2.tar.gz

---
+++ packages/nyroglancer/nyroglancer-0.0.2.tar.gz/nyroglancer-0.0.2/nyroglancer/ndstore.py
@@ -0,0 +1,201 @@
+from .intrusion import evaluate
+from PIL import Image as PilImage
+from StringIO import StringIO
+from notebook.base.handlers import IPythonHandler
+from tornado.escape import url_unescape
+import binascii
+import json
+import math
+import numpy as np
+import random
+import zlib
+
+LEN_VOLUME_KEY=16
+
+def create_key():
+
+    return ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789') for i in range(LEN_VOLUME_KEY)])
+
+class Info(IPythonHandler):
+
+    def get(self, cf_volume_key):
+
+        cf = url_unescape(cf_volume_key[:-LEN_VOLUME_KEY])
+        volume_key = cf_volume_key[-LEN_VOLUME_KEY:]
+
+        volume_type = json.loads(evaluate(cf, "nyroglancer.volumes['%s'].vtype" % volume_key))
+        is_segmentation = (volume_type == "segmentation")
+
+        volume_size = json.loads(evaluate(cf, "list(nyroglancer.volumes['%s'].data.shape)" % volume_key))[::-1]
+        resolution = json.loads(evaluate(cf, "nyroglancer.volumes['%s'].resolution" % volume_key))[::-1]
+        offset = json.loads(evaluate(cf, "nyroglancer.volumes['%s'].offset" % volume_key))[::-1]
+        chunk_size = json.loads(evaluate(cf, "nyroglancer.volumes['%s'].chunk_size" % volume_key))[::-1]
+        num_chunks = np.prod([ int(math.ceil(volume_size[d]/chunk_size[d])) for d in range(3) ])
+
+        if is_segmentation:
+            channel_info = {
+                "segmentation": {
+                    "channel_type": "annotation",
+                    # FIXME: neuroglancer does not support uint64 (at the moment),
+                    # therefore we ship uint32 for now
+                    "datatype": "uint32",
+                    "exceptions": 0,
+                    "propagate": 2,
+                    "readonly": 1,
+                    "resolution": 0,
+                    "windowrange": [ 0, 0 ]
+                }
+            }
+        else:
+            channel_info = {
+                "image": {
+                    "channel_type": "image",
+                    "datatype": "uint8",
+                    "exceptions": 0,
+                    "propagate": 2,
+                    "readonly": 1,
+                    "resolution": 0,
+                    "windowrange": [ 0, 0 ]
+                }
+            }
+
+        self.write(json.dumps({
+            "channels": channel_info,
+            "dataset": {
+                "cube_dimension": {
+                    "0": chunk_size
+                },
+                # for neuroglancer
+                "chunk_size": {
+                    "0": chunk_size
+                },
+                "description": "volume " + volume_key,
+                "neariso_imagesize": {
+                    "0": volume_size
+                },
+                "neariso_offset": {
+                    "0": [ 0, 0, 0 ]
+                },
+                "neariso_scaledown": {
+                    "0": 1
+                },
+                "neariso_voxelres": {
+                    "0": resolution
+                },
+                "imagesize": {
+                    "0": volume_size
+                },
+                "offset": {
+                    "0": offset
+                },
+                "voxelres": {
+                    "0": resolution
+                },
+                "resolutions": [
+                    0
+                ],
+                "scaling": "zslices",
+                "scalinglevels": 0,
+                "timerange": [ 0, 0 ],
+            },
+            "metadata": {},
+            "project": {
+                "description": "nyroglancer jupyter extension",
+                "name": "nyroglancer",
+                "version": "0.0"
+            }
+        }))
+        self.set_header("Content-Type", "application/json")
+
+class Image(IPythonHandler):
+
+    @staticmethod
+    def backend(volume, min_x, max_x, min_y, max_y, min_z, max_z):
+        """This function will be called in the kernel that started the viewer.
+        """
+
+        (chunk_min, chunk_max, chunk_size) = parse_dimensions(min_x, max_x, min_y, max_y, min_z, max_z)
+
+        chunk = volume.data[
+            chunk_min[2]:chunk_max[2],
+            chunk_min[1]:chunk_max[1],
+            chunk_min[0]:chunk_max[0],
+        ]
+
+        chunk_stack = np.zeros((chunk_size[1]*chunk_size[2], chunk_size[0]), dtype=np.uint8)
+        for z in range(chunk_size[2]):
+            chunk_stack[z*chunk_size[1]:(z+1)*chunk_size[1],:] = chunk[z,:,:]
+
+        stack_img = PilImage.fromarray(chunk_stack)
+
+        fake_file = StringIO()
+        stack_img.save(fake_file, "jpeg")
+
+        return binascii.b2a_base64(fake_file.getvalue()).strip()
+
+    def get(self, cf_volume_key, resolution, min_x, max_x, min_y, max_y, min_z, max_z):
+
+        cf = url_unescape(cf_volume_key[:-LEN_VOLUME_KEY])
+        volume_key = cf_volume_key[-LEN_VOLUME_KEY:]
+
+        data = evaluate(cf, "nyroglancer.ndstore.Image.backend(nyroglancer.volumes['%s'], %s, %s, %s, %s, %s, %s)" % (volume_key, min_x, max_x, min_y, max_y, min_z, max_z))
+        jpg = binascii.a2b_base64(data)
+        self.write(jpg)
+        self.set_header("Content-Type", "image/jpeg")
+
+class Segmentation(IPythonHandler):
+
+    @staticmethod
+    def backend(volume, min_x, max_x, min_y, max_y, min_z, max_z):
+        """This function will be called in the kernel that started the viewer.
+        """
+
+        (chunk_min, chunk_max, chunk_size) = parse_dimensions(min_x, max_x, min_y, max_y, min_z, max_z)
+
+        # FIXME: neuroglancer does not support uint64 (at the moment), therefore we
+        # ship uint32 for now
+        chunk = volume.data[
+            chunk_min[2]:chunk_max[2],
+            chunk_min[1]:chunk_max[1],
+            chunk_min[0]:chunk_max[0],
+        ].astype(np.uint32)
+
+        fake_file = StringIO()
+        np.save(fake_file, chunk[None,:])
+
+        return binascii.b2a_base64(zlib.compress(fake_file.getvalue())).strip()
+
+    def get(self, cf_volume_key, resolution, min_x, max_x, min_y, max_y, min_z, max_z):
+
+        cf = url_unescape(cf_volume_key[:-LEN_VOLUME_KEY])
+        volume_key = cf_volume_key[-LEN_VOLUME_KEY:]
+
+        pyz = binascii.a2b_base64(evaluate(cf, "nyroglancer.ndstore.Segmentation.backend(nyroglancer.volumes['%s'], %s, %s, %s, %s, %s, %s)" % (volume_key, min_x, max_x, min_y, max_y, min_z, max_z)))
+        self.write(pyz)
+        self.set_header("Content-Type", "application/octet-stream")
+
+def parse_dimensions(min_x, max_x, min_y, max_y, min_z, max_z):
+
+    min_x = int(min_x)
+    max_x = int(max_x)
+    min_y = int(min_y)
+    max_y = int(max_y)
+    min_z = int(min_z)
+    max_z = int(max_z)
+
+    return (
+        [ min_x, min_y, min_z ],
+        [ max_x, max_y, max_z ],
+        [
+            get_pos_diff(min_x, max_x),
+            get_pos_diff(min_y, max_y),
+            get_pos_diff(min_z, max_z)
+        ],
+    )
+
+def get_pos_diff(start, end):
+
+    d = end - start
+    if d < 0:
+        raise RuntimeError("invalid dimensions: %i, %i" % (start, end))
+    return d
---
+++ packages/nyroglancer/nyroglancer-0.0.2.tar.gz/nyroglancer-0.0.2/nyroglancer/viewer.py
@@ -0,0 +1,123 @@
+from IPython.display import HTML
+from jupyter_client import find_connection_file
+from tornado.escape import url_escape
+import collections
+import json
+import ndstore
+import urllib
+
+# volumes of all viewer instances
+volumes = {}
+
+class Volume:
+
+    def __init__(self, data, resolution, offset, vtype, chunk_size, name, shader):
+
+        self.data = data
+        self.resolution = resolution
+        self.offset = offset
+        self.vtype = vtype
+        self.chunk_size = chunk_size
+        self.name = name
+        self.shader = shader
+
+class Viewer:
+
+    def __init__(self):
+
+        self.volumes = []
+        self.setup_ndstore_url()
+        self.hostname = 'localhost:8888'
+        self.large = False
+
+    def set_large(self, large = True):
+        """Let the viewer span the whole width of the browser window.
+
+        This will resize your IPython column to the same width.
+        """
+        self.large = large
+
+    def set_hostname(self, hostname):
+        """Set the name of the server running the Jupyter Notebook.
+
+        Defaults to "localhost:8888". Change this if you use a remote server.
+        """
+        self.hostname = hostname
+
+    def put(self, array, resolution = [1.0, 1.0, 1.0], offset = [0, 0, 0], vtype="raw", chunk_size = None, name = None, shader = None):
+        """
+        Prepare a numpy array for visualization.
+
+        Parameters
+        ----------
+        array: numpy.array
+            The data to show.
+        resolution: [float], optional
+            The resolution of the data.
+        offset: [float], optional
+            The offset of the volume in world units.
+        vtype: string, optional
+            "raw" or "segmentation"
+        chunk_size: [float], optional
+            The chunk size to be used by neuroglancer to load parts of the volume.
+        name: string, optional
+            A human readable name for the volume. Will be shown as the layer name in the viewer.
+        shader: string, optional
+            A GLSL shader used to render the volume. See 
+            https://github.com/google/neuroglancer/blob/master/src/neuroglancer/sliceview/image_layer_rendering.md 
+            for details.
+        """
+
+        key = ndstore.create_key()
+
+        if chunk_size is None:
+
+            # guess a chunk size, such that the max size of the chunk is
+            # 2**21 = 2**(3*7)
+            #
+            # in isotropic volumes, this is [2**7, 2**7, 2**7]
+            #
+            # in unisotropic volumes, we redistribute the exponent according to 
+            # the resolution
+
+            # voxels per world unit
+            vpu = [ 1.0/r for r in resolution ]
+            sum_vpu = sum(vpu)
+            b0 = int((float(vpu[0])/sum_vpu)*21)
+            b1 = int((float(vpu[1])/sum_vpu)*21)
+            b2 = int((float(vpu[2])/sum_vpu)*21)
+            chunk_size = [ 2**b0, 2**b1, 2**b2 ]
+
+        if name is None:
+            name = key
+
+        volume = Volume(array, resolution, offset, vtype, chunk_size, name, shader)
+        self.volumes.append((key, volume))
+
+        global volumes
+        volumes[key] = volume
+
+    def show(self):
+
+        layers = collections.OrderedDict()
+        for (key, volume) in self.volumes:
+
+            layers[volume.name] = {
+                'type': 'image' if volume.vtype is 'raw' else 'segmentation',
+                'source': 'ndstore://http://' + self.hostname + '/' + self.kernel_esc_path + key
+            }
+
+            if volume.shader is not None:
+                layers[volume.name]['shader'] = volume.shader
+
+        arguments = { 'layers': layers }
+        arguments_json = json.dumps(arguments)
+        viewer_url = "http://" + self.hostname + '/viewer#!' + urllib.quote(arguments_json, safe='~@#$&()*!+=:;,.?/\'')
+
+        large_html = "<style>.container { width:100% !important; }</style>" if self.large else ""
+        return HTML(large_html + "<iframe src=\"" + viewer_url + "\" width=\"100%\" height=\"1024px\"><\iframe>")
+
+    def setup_ndstore_url(self):
+
+        connection_file = find_connection_file()
+        self.kernel_esc_path = url_escape(connection_file)
