cb03bf5d43de36b674793e5f6b501e2e5cf64049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:00 +0000

    Add package dork-compose/0.1.15/dork-compose-0.1.15.tar.gz

---
+++ packages/dork-compose/dork-compose-0.1.15.tar.gz/dork-compose-0.1.15/dork_compose/plugins/multi.py
@@ -0,0 +1,17 @@
+import dork_compose.plugin
+
+
+class Plugin(dork_compose.plugin.Plugin):
+    def environment(self):
+        return {
+            'DORK_PROJECT': self.get_project()
+        }
+
+    def get_project(self):
+        return self.basedir.split('/')[-1]
+
+    def info(self, project):
+        return {
+            'Project': self.get_project()
+        }
+
