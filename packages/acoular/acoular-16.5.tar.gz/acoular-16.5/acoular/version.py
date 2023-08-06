33139c937c5486d8264e200b5f0f213cb325e185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:02 +0000

    Add package acoular/16.5/acoular-16.5.tar.gz

---
+++ packages/acoular/acoular-16.5.tar.gz/acoular-16.5/acoular/version.py
@@ -0,0 +1,9 @@
+# coding=UTF-8
+#------------------------------------------------------------------------------
+# Copyright (c) 2007-2016, Acoular Development Team.
+#------------------------------------------------------------------------------
+
+# separate file to find out about version without importing the acoular lib
+__author__ = "Acoular developers"
+__date__ = "11 May 2016"
+__version__ = "16.5"
