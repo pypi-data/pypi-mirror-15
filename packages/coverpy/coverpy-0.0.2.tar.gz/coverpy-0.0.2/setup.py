8d835e7e805ba93d556c56389244d623f03eee50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:48 +0000

    Add package coverpy/0.0.2/coverpy-0.0.2.tar.gz

---
+++ packages/coverpy/coverpy-0.0.2.tar.gz/coverpy-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+	# PyPI specific meta
+	name='coverpy',
+	version='0.0.2',
+	license='MIT License',
+	author="fallenshell",
+	author_email='dev@mxio.us',
+	description="A wrapper for iTunes Search API",
+	long_description=open('README.md').read(),
+	keywords='itunes search api wrapper simple clear cover album art music',
+	# Package routines
+	packages=find_packages(exclude=['scripts', 'tests']),
+	install_requires=['requests'],
+)
