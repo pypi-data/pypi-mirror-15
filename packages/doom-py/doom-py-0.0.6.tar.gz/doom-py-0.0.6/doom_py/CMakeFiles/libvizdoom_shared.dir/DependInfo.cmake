d5047e49a872bdf0e04c671a01e9d254a3a97b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:30 +0000

    Add package doom-py/0.0.6/doom-py-0.0.6.tar.gz

---
+++ packages/doom-py/doom-py-0.0.6.tar.gz/doom-py-0.0.6/doom_py/CMakeFiles/libvizdoom_shared.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jietang/Projects/doom-py/doom_py/src/lib/ViZDoomController.cpp" "/Users/jietang/Projects/doom-py/doom_py/CMakeFiles/libvizdoom_shared.dir/src/lib/ViZDoomController.cpp.o"
+  "/Users/jietang/Projects/doom-py/doom_py/src/lib/ViZDoomExceptions.cpp" "/Users/jietang/Projects/doom-py/doom_py/CMakeFiles/libvizdoom_shared.dir/src/lib/ViZDoomExceptions.cpp.o"
+  "/Users/jietang/Projects/doom-py/doom_py/src/lib/ViZDoomGame.cpp" "/Users/jietang/Projects/doom-py/doom_py/CMakeFiles/libvizdoom_shared.dir/src/lib/ViZDoomGame.cpp.o"
+  "/Users/jietang/Projects/doom-py/doom_py/src/lib/ViZDoomUtilities.cpp" "/Users/jietang/Projects/doom-py/doom_py/CMakeFiles/libvizdoom_shared.dir/src/lib/ViZDoomUtilities.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "src/lib"
+  "/usr/local/include"
+  "src/lib_python"
+  "/usr/local/Cellar/python/2.7.11/Frameworks/Python.framework/Versions/2.7/include/python2.7"
+  "/usr/local/lib/python2.7/site-packages/numpy/core/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
