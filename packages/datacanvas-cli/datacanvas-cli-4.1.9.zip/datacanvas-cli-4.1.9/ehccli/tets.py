140910e7a18dc0005fe75bd5f71ca20cda026e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:33 +0000

    Add package datacanvas-cli/4.1.9/datacanvas-cli-4.1.9.zip

---
+++ packages/datacanvas-cli/datacanvas-cli-4.1.9.zip/datacanvas-cli-4.1.9/ehccli/tets.py
@@ -0,0 +1,525 @@
+login(token)
+
+{"account":"admin","password":"Server2008!"}
+
+{
+  "code": 0,
+  "data": {
+    "autoKey": 1,
+    "name": "admin",
+    "email": "contact@zetyun.com",
+    "token": "pdnYRsPauS3O4fmHrFPGEJRNJEdGDbAGPdKImZQpMUo2EgiMxlGCNzkdkGH5T3Gs",
+    "avatar": "/avatar/avatarupload_99d0c18d309c14b1fdc8b9051989344b.jpg",
+    "group": 0,
+    "ehcUser": {
+      "id": "u-ce7d5a11e585f5",
+      "token": ".eJyrVirIyM9LVbJSMjQ2MTIyNjY2MjJS0lHKTAEKleomp5qnmCYaGqaaWpimmQLF8xJzQYpLUotLgLzU3MTMHCjXoSq1pLI0Ty85P1epFgAWnhm7.FNr8on_7AklyrO3dcvBpS7OdlZk"
+    },
+    "logined": true,
+    "id": 1
+  }
+}
+
+
+projectlist(session,token)
+
+{
+  "code": 0,
+  "data": [
+    {
+      "id": 145,
+      "name": "lending_club for mintq",
+      "description": "Lending_club",
+      "creator": "mintq",
+      "uid": 72,
+      "version": "0.0",
+      "versionId": 2,
+      "lastModifyTime": "2016-05-07T04:22:41.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1462594960423.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 144,
+      "name": "LinearSVC with Plotter(for mintq)",
+      "description": "SVM prediction with a Precision/Recall curve in PDF",
+      "creator": "mintq",
+      "uid": 72,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-05-07T04:21:12.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1462594871842.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 143,
+      "name": "Time Series Forecast(Starbucks stock price) for mintq",
+      "description": "It is Starbucks stock forecast based on time series in R.",
+      "creator": "mintq",
+      "uid": 72,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-05-07T04:18:59.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1462594738187.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 142,
+      "name": "data",
+      "description": "data",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 12,
+      "lastModifyTime": "2016-05-07T06:19:27.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1462601967785.png",
+      "runnable": true,
+      "refNum": 7
+    },
+    {
+      "id": 139,
+      "name": "VECM",
+      "description": "BANK OF CHINA",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 4,
+      "lastModifyTime": "2016-05-04T08:56:34.000Z",
+      "likeCount": 1,
+      "graph": "/avatar/1462352193517.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 135,
+      "name": "test1_flow",
+      "description": "test1_flow",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-04-24T04:12:55.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1461471175269.png",
+      "runnable": true,
+      "refNum": 0
+    },
+    {
+      "id": 133,
+      "name": "Lending_club",
+      "description": "Lending_club",
+      "creator": "zhouty",
+      "uid": 68,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-04-21T06:50:30.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1461221429178.png",
+      "runnable": true,
+      "refNum": 2
+    },
+    {
+      "id": 132,
+      "name": "Testing Framework",
+      "description": "Changing params-json_url for different unit test.",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 9,
+      "lastModifyTime": "2016-04-25T02:49:47.000Z",
+      "likeCount": 1,
+      "graph": "/avatar/1461552586581.png",
+      "runnable": true,
+      "refNum": 6
+    },
+    {
+      "id": 130,
+      "name": "lending_club",
+      "description": "Lending_club",
+      "creator": "liudazhuang",
+      "uid": 67,
+      "version": "0.0",
+      "versionId": 3,
+      "lastModifyTime": "2016-04-05T11:11:32.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1459854691004.png",
+      "runnable": true,
+      "refNum": 2
+    },
+    {
+      "id": 128,
+      "name": "LinearSVC with Plotter",
+      "description": "SVM prediction with a Precision/Recall curve in PDF",
+      "creator": "liudazhuang",
+      "uid": 67,
+      "version": "0.0",
+      "versionId": 3,
+      "lastModifyTime": "2016-04-06T07:49:09.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1459928948755.png",
+      "runnable": true,
+      "refNum": 6
+    },
+    {
+      "id": 126,
+      "name": "Time Series Forecast",
+      "description": "It is Starbucks stock forecast based on time series in R.",
+      "creator": "liudazhuang",
+      "uid": 67,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-31T10:57:30.822Z",
+      "likeCount": 0,
+      "graph": "/avatar/1457419919181.png",
+      "runnable": true,
+      "refNum": 2
+    },
+    {
+      "id": 118,
+      "name": "Time Series Forecast(Starbucks stock price) jt",
+      "description": "It is Starbucks stock forecast based on time series in R.",
+      "creator": "jiangtan",
+      "uid": 63,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-30T06:58:28.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1459321107334.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 117,
+      "name": "LinearSVC",
+      "description": "SVM prediction with a Precision/Recall curve in PDF",
+      "creator": "jiangtan",
+      "uid": 63,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-29T07:33:03.786Z",
+      "likeCount": 0,
+      "graph": "/avatar/1457432121078.png",
+      "runnable": true,
+      "refNum": 0
+    },
+    {
+      "id": 114,
+      "name": "Lending_club_Alabama",
+      "description": "Alabama Loaners",
+      "creator": "jiangtan",
+      "uid": 63,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-29T06:29:38.917Z",
+      "likeCount": 0,
+      "graph": "/avatar/1458915843840.png",
+      "runnable": true,
+      "refNum": 0
+    },
+    {
+      "id": 113,
+      "name": "logistic plot copy",
+      "description": "logistic plot",
+      "creator": "jiangtan",
+      "uid": 63,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-29T06:22:59.118Z",
+      "likeCount": 0,
+      "graph": "/avatar/1459163459146.png",
+      "runnable": true,
+      "refNum": 0
+    },
+    {
+      "id": 112,
+      "name": "lending_club_copy",
+      "description": "Lending_club",
+      "creator": "jiangtan",
+      "uid": 63,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-29T03:42:53.164Z",
+      "likeCount": 0,
+      "graph": "/avatar/1459181715631.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 111,
+      "name": "Lending_club_Alabama",
+      "description": "Alabama Loaners",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-25T14:24:03.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1458915843840.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 110,
+      "name": "Lending_Club_Texas",
+      "description": "Lending Club Loaners in Texas",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 2,
+      "lastModifyTime": "2016-03-28T11:41:32.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1459165290311.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 100,
+      "name": "fix_gbm",
+      "description": "fix_gbm",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 4,
+      "lastModifyTime": "2016-03-24T13:52:53.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1458827573523.png",
+      "runnable": true,
+      "refNum": 0
+    },
+    {
+      "id": 97,
+      "name": "Lending_club",
+      "description": "Lending_club",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 37,
+      "lastModifyTime": "2016-05-09T08:36:57.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1462783014603.png",
+      "runnable": true,
+      "refNum": 125
+    },
+    {
+      "id": 94,
+      "name": "glm_test",
+      "description": "glm_test",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 3,
+      "lastModifyTime": "2016-03-20T16:32:05.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1458491525736.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 93,
+      "name": "logistic_plot",
+      "description": "logistic ",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-19T08:20:09.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1458375609591.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 92,
+      "name": "logistic",
+      "description": "logistic plot",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 3,
+      "lastModifyTime": "2016-03-28T11:10:59.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1459163459146.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 91,
+      "name": "missing_value_present",
+      "description": "missing_value_present",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 14,
+      "lastModifyTime": "2016-03-21T08:41:34.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1458549694175.png",
+      "runnable": true,
+      "refNum": 3
+    },
+    {
+      "id": 90,
+      "name": "new_missing_value",
+      "description": "retry",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-18T03:15:35.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1458270935015.png",
+      "runnable": true,
+      "refNum": 1
+    },
+    {
+      "id": 85,
+      "name": "读取+创建组合权限测试项目",
+      "description": "测试",
+      "creator": "testUser4",
+      "uid": 57,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-09T15:18:23.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1457507903731.png",
+      "runnable": true,
+      "refNum": 0
+    },
+    {
+      "id": 79,
+      "name": "missing_value",
+      "description": "handle the missing value for both numeric and categorical variables",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-03-09T02:25:06.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1457490306526.png",
+      "runnable": true,
+      "refNum": 4
+    },
+    {
+      "id": 78,
+      "name": "try_postgresql",
+      "description": "try_postgresql",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 9,
+      "lastModifyTime": "2016-03-09T02:05:06.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1457489106760.png",
+      "runnable": true,
+      "refNum": 6
+    },
+    {
+      "id": 35,
+      "name": "景越项目-无权",
+      "description": "不分配权限",
+      "creator": "jingyue",
+      "uid": 42,
+      "version": "0.0",
+      "versionId": 0,
+      "lastModifyTime": "2016-02-24T05:36:55.745Z",
+      "likeCount": 0,
+      "graph": null,
+      "runnable": false,
+      "refNum": 0
+    },
+    {
+      "id": 34,
+      "name": "景越项目",
+      "description": "景越的项目",
+      "creator": "jingyue",
+      "uid": 42,
+      "version": "0.0",
+      "versionId": 0,
+      "lastModifyTime": "2016-02-24T05:33:36.681Z",
+      "likeCount": 0,
+      "graph": null,
+      "runnable": false,
+      "refNum": 0
+    },
+    {
+      "id": 33,
+      "name": "张婷的项目",
+      "description": "张婷的项目-分配给其他人测试",
+      "creator": "张婷",
+      "uid": 48,
+      "version": "0.0",
+      "versionId": 0,
+      "lastModifyTime": "2016-02-23T08:04:55.716Z",
+      "likeCount": 0,
+      "graph": null,
+      "runnable": false,
+      "refNum": 0
+    },
+    {
+      "id": 21,
+      "name": "starbucks stock",
+      "description": "test",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 1,
+      "lastModifyTime": "2016-01-06T09:07:40.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1453270255658.png",
+      "runnable": true,
+      "refNum": 3
+    },
+    {
+      "id": 3,
+      "name": "Time Series Forecast(Starbucks stock price)",
+      "description": "It is Starbucks stock forecast based on time series in R.",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 5,
+      "lastModifyTime": "2016-05-06T09:43:44.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1462527823625.png",
+      "runnable": true,
+      "refNum": 38
+    },
+    {
+      "id": 1,
+      "name": "LinearSVC with Plotter",
+      "description": "SVM prediction with a Precision/Recall curve in PDF",
+      "creator": "admin",
+      "uid": 1,
+      "version": "0.0",
+      "versionId": 8,
+      "lastModifyTime": "2016-05-05T01:24:25.000Z",
+      "likeCount": 0,
+      "graph": "/avatar/1462411463954.png",
+      "runnable": true,
+      "refNum": 19
+    }
+  ],
+  "pageNum": 1
+}
+
+
+createproject
+
+token,session
+
+
+
+
+
+
+
+
+
+
+
+
+
