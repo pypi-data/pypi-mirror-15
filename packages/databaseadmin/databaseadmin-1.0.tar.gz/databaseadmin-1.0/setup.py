19e364017ba6b39093d67a03cd25f1707131879d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:48 +0000

    Add package databaseadmin/1.0/databaseadmin-1.0.tar.gz

---
+++ packages/databaseadmin/databaseadmin-1.0.tar.gz/databaseadmin-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='databaseadmin',
+      version='1.0',
+      description = 'just a python wrapper for doing most DB admin functions',
+      author = 'Software Mechanic',
+      author_email = 'softwaremechanic32@gmail.com',
+      url = 'https://github.com/emofeedback/python-DbAdmin.git',
+      packages= ['dbadmin'],
+      )
