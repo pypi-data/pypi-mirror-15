851be01da1485710a74310a9d031dccd7114a60e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:21 +0000

    Add package Miiizh_nester/1.0.0/Miiizh_nester-1.0.0.zip

---
+++ packages/Miiizh_nester/Miiizh_nester-1.0.0.zip/Miiizh_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'Miiizh_nester',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = "Megan",
+    author_email = "zhaozy@nies.net.cn",
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple of nested lists',
+    )
