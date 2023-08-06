c93cc476e53414d7661bfc9bd316f34ace4d1604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:29 +0000

    Add package hatchlib/0.2.7/hatchlib-0.2.7.zip

---
+++ packages/hatchlib/hatchlib-0.2.7.zip/hatchlib-0.2.7/hatchlib/search/kmp_ext.py
@@ -0,0 +1,11 @@
+def prefix(word):
+	prefix=[0]*len(word)
+
+	for q in range(1,len(word)):
+		while k>0 and word[k]!=word[q]:
+			k=prefix[k-1]
+
+		if word[k+1]==word[q]: k+=1
+		prefix[q]=k
+
+	return prefix
