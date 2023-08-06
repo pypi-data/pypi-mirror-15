0ec6fcf0df0cc4f385b15bc96a77d1c9ff95fda5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:55 +0000

    Add package scopetest_1/0.2/scopetest_1-0.2.tar.gz

---
+++ packages/scopetest_1/scopetest_1-0.2.tar.gz/scopetest_1-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scopetest_1',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['scopetest_1'],
+      zip_safe=True,)
