f38b10e09e4872aa41fc87decaf227561132a794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:23 +0000

    Add package cogent/1.9/cogent-1.9-cp27-cp27m-macosx_10_5_x86_64.whl

---
+++ packages/cogent/cogent-1.9-cp27-cp27m-macosx_10_5_x86_64.whl/cogent/maths/stats/cai/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+__all__ = ['adaptor', 'get_by_cai','util']
+
+__author__ = "Rob Knight"
+__copyright__ = "Copyright 2007-2016, The Cogent Project"
+__credits__ = ["Rob Knight", "Stephanie Wilson", "Michael Eaton"]
+__license__ = "GPL"
+__version__ = "1.9"
+__maintainer__ = "Rob Knight"
+__email__ = "rob@spot.colorado.edu"
+__status__ = "Production"
