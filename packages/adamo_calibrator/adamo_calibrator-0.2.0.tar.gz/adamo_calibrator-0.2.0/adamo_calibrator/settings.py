e2d067bb39d840927813a7f33a9ab248aa476d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:14 +0000

    Add package adamo_calibrator/0.2.0/adamo_calibrator-0.2.0.tar.gz

---
+++ packages/adamo_calibrator/adamo_calibrator-0.2.0.tar.gz/adamo_calibrator-0.2.0/adamo_calibrator/settings.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+from os.path import abspath
+
+TEST = False
+FAKE = False
+
+DEBUG = True
+FULLSCREEN = True
+SHOW_CURSOR = True
+
+NPOINTS = 5
+MISCLICK_THRESHOLD = 30
+DUALCLICK_THRESHOLD = 100
+
+# Timeout in milliseconds
+TIMEOUT = 0
+
+# Internationalization Settings
+PO_NAME = 'calibrator'
+DEFAULT_LOCALE = 'en_US'
+RESOURCES_PATH = abspath('resources')
+
+FAST_START = True
+AUTO_CLOSE = True
