2ad04017bbb28b3dc7170a08d44dfd7fc4f64214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:12 +0000

    Add package django-cookie-law/1.0.8/django-cookie-law-1.0.8.tar.gz

---
+++ packages/django-cookie-law/django-cookie-law-1.0.8.tar.gz/django-cookie-law-1.0.8/cookielaw/templatetags/cookielaw_tags.py
@@ -0,0 +1,31 @@
+import warnings
+
+from classytags.helpers import InclusionTag
+from django import template
+from django.template.loader import render_to_string
+
+
+register = template.Library()
+
+
+class CookielawBanner(InclusionTag):
+    """
+    Displays cookie law banner only if user has not dismissed it yet.
+    """
+
+    template = 'cookielaw/banner.html'
+
+    def render_tag(self, context, **kwargs):
+        template_filename = self.get_template(context, **kwargs)
+
+        if 'request' not in context:
+            warnings.warn('No request object in context. '
+                          'Are you sure you have django.core.context_processors.request enabled?')
+
+        if context['request'].COOKIES.get('cookielaw_accepted', False):
+            return ''
+
+        data = self.get_context(context, **kwargs)
+        return render_to_string(template_filename, data, context_instance=context)
+
+register.tag(CookielawBanner)
