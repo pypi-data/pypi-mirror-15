4252f1f34f5dd28d1a2886567b277fcd1bae755b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:33 +0000

    Add package lqq_nester/1.0.0/lqq_nester-1.0.0.zip

---
+++ packages/lqq_nester/lqq_nester-1.0.0.zip/lqq_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'lqq_nester',
+    version = '1.0.0',
+    py_modules = ['lqq_nester'],
+    author = 'lqqpython',
+    author_email = 'addisonlee@126.com',
+    description = 'A simple printer of nested lists',
+    )
+
