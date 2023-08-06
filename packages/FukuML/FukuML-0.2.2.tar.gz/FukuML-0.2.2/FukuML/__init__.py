7111a2d929889a768c5ae6ed0581f8289069cc31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:49 +0000

    Add package FukuML/0.2.2/FukuML-0.2.2.tar.gz

---
+++ packages/FukuML/FukuML-0.2.2.tar.gz/FukuML-0.2.2/FukuML/__init__.py
@@ -0,0 +1,31 @@
+#encoding=utf8
+
+__version__ = '0.2.2'
+__all__ = [
+    'MLBase',
+    'PLA',
+    'PLA.BinaryClassifier',
+    'PLA.MultiClassifier',
+    'PocketPLA',
+    'PocketPLA.BinaryClassifier',
+    'PocketPLA.MultiClassifier',
+    'LinearRegression',
+    'LinearRegression.LinearRegression',
+    'LinearRegression.BinaryClassifier',
+    'LinearRegression.MultiClassifier',
+    'LinearRegression.Accelerator',
+    'LogisticRegression.LogisticRegression',
+    'LogisticRegression.BinaryClassifier',
+    'LogisticRegression.MultiClassifier',
+    'L2RLogisticRegression.L2RLogisticRegression',
+    'L2RLogisticRegression.BinaryClassifier',
+    'RidgeRegression',
+    'RidgeRegression.RidgeRegression',
+    'RidgeRegression.BinaryClassifier',
+    'RidgeRegression.MultiClassifier',
+    'SupportVectorMachine.BinaryClassifier',
+    'Utility',
+    'Utility.DatasetLoader',
+    'Utility.CrossValidator',
+    'Utility.Serializer'
+]
