54e55bd19d2f973068a1ca4437515c136b282bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:44 +0000

    Add package pytrip98/0.2.1/pytrip98-0.2.1-py2.py3-none-any.whl

---
+++ packages/pytrip98/pytrip98-0.2.1-py2.py3-none-any.whl/pytrip/__init__.py
@@ -0,0 +1,37 @@
+"""
+    This file is part of PyTRiP.
+
+    libdedx is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libdedx is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libdedx.  If not, see <http://www.gnu.org/licenses/>
+"""
+__all__ = ['CtxCube', 'VdxCube', 'GetDvh', 'DosCube', 'DensityCube', 'LETCube','dicomhelper','res','guiutil','Rst']
+__author__ = "Jakob Toftegaard, Niels Bassler"
+__version__ = "1.0"
+__email__ = "bassler@phys.au.dk"
+
+from pytrip.ctx import CtxCube
+from pytrip.dos import DosCube
+from pytrip.vdx import VdxCube
+from pytrip.paths import DensityCube
+#import paths
+from pytrip.rst import Rst
+from pytrip.let import LETCube
+from pytrip.get_dvh  import GetDvh
+import pytrip.dicomhelper
+import pytrip.res
+import pytrip.guiutil
+import pytrip.tripexecuter
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
