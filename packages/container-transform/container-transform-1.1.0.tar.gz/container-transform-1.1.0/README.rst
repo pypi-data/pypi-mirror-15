8b5515c82bab16593fee3477210ce3802f89fb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:40 +0000

    Add package container-transform/1.1.0/container-transform-1.1.0.tar.gz

---
+++ packages/container-transform/container-transform-1.1.0.tar.gz/container-transform-1.1.0/README.rst
@@ -0,0 +1,114 @@
+.. image:: https://travis-ci.org/micahhausler/container-transform.png
+   :target: https://travis-ci.org/micahhausler/container-transform
+
+.. image:: https://coveralls.io/repos/micahhausler/container-transform/badge.png?branch=master
+    :target: https://coveralls.io/r/micahhausler/container-transform?branch=master
+
+.. image:: https://readthedocs.org/projects/container-transform/badge/?version=latest
+    :target: http://container-transform.readthedocs.org/en/latest/?badge=latest
+    :alt: Documentation Status
+
+
+container-transform
+===================
+container-transform is a small utility to transform various docker container
+formats to one another.
+
+Currently, container-transform can parse and convert ECS task definitions and
+docker-compose configuration files (v1 and v2). It can also output Systemd unit
+files. Any missing required parameters are printed to STDERR.
+
+Quickstart
+----------
+::
+
+    $ cat docker-compose.yml | container-transform  -v
+    {
+        "family": "python-app",
+        "volumes": [
+            {
+                "name": "host_logs",
+                "host": {
+                    "sourcePath": "/var/log/myapp"
+                }
+            }
+        ],
+        "containerDefinitions": [
+            {
+                "memory": 1024,
+                "image": "postgres:9.3",
+                "name": "db",
+                "essential": true
+            },
+            {
+                "memory": 128,
+                "image": "redis:latest",
+                "name": "redis",
+                "essential": true
+            },
+            {
+                "name": "web",
+                "memory": 64,
+                "command": [
+                    "uwsgi",
+                    "--json",
+                    "uwsgi.json"
+                ],
+                "mountPoints": [
+                    {
+                        "sourceVolume": "host_logs",
+                        "containerPath": "/var/log/uwsgi/"
+                    }
+                ],
+                "environment": [
+                    {
+                        "name": "AWS_ACCESS_KEY_ID",
+                        "value": "AAAAAAAAAAAAAAAAAAAA"
+                    },
+                    {
+                        "name": "AWS_SECRET_ACCESS_KEY",
+                        "value": "1111111111111111111111111111111111111111"
+                    }
+                ],
+                "essential": true
+            }
+        ]
+    }
+    Container db is missing required parameter "cpu".
+    Container redis is missing required parameter "cpu".
+    Container web is missing required parameter "image".
+    Container web is missing required parameter "cpu".
+
+Docker Image
+------------
+
+To get the docker image, run::
+
+    docker pull micahhausler/container-transform:latest
+
+To run the docker image::
+
+    docker run --rm -v $(pwd):/data/ micahhausler/container-transform  docker-compose.yml
+
+    # or
+    cat docker-compose.yml | docker run --rm -i micahhausler/container-transform
+
+
+Installation
+------------
+To install the latest release, type::
+
+    pip install container-transform
+
+To install the latest code directly from source, type::
+
+    pip install git+git://github.com/micahhausler/container-transform.git
+
+Documentation
+-------------
+
+Full documentation is available at http://container-transform.readthedocs.org
+
+License
+-------
+MIT License (see LICENSE)
---
+++ packages/container-transform/container-transform-1.1.0.tar.gz/container-transform-1.1.0/container_transform/schema.py
@@ -0,0 +1,451 @@
+from enum import Enum
+import logging
+from collections import OrderedDict
+
+LOG = logging.getLogger(__name__)
+
+
+class TransformationTypes(Enum):
+    ECS = 'ecs'
+    COMPOSE = 'compose'
+    SYSTEMD = 'systemd'
+    MARATHON = 'marathon'
+
+
+class InputTransformationTypes(Enum):
+    ECS = 'ecs'
+    COMPOSE = 'compose'
+    MARATHON = 'marathon'
+
+
+class OutputTransformationTypes(Enum):
+    ECS = 'ecs'
+    COMPOSE = 'compose'
+    SYSTEMD = 'systemd'
+    MARATHON = 'marathon'
+
+
+ARG_MAP = OrderedDict({
+    'image': {
+        TransformationTypes.ECS.value: {
+            'name': 'image',
+            'required': True,
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'image',
+            'required': True,
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'image',
+            'required': True,
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.image',
+            'required': True,
+        },
+    },
+    'name': {
+        TransformationTypes.ECS.value: {
+            'name': 'name',
+            'required': True,
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'name',
+            'required': True
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'name',
+            'required': True
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'id',
+            'required': True
+        },
+    },
+    'cpu': {
+        TransformationTypes.ECS.value: {
+            'name': 'cpu',
+            'required': True
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'cpu_shares',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'cpu_shares',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'cpus',
+            'required': False
+        },
+    },
+    'memory': {
+        TransformationTypes.ECS.value: {
+            'name': 'memory',
+            'required': True
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'mem_limit',
+            'required': False,
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'mem_limit',
+            'required': False,
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'mem',
+            'required': False,
+        },
+    },
+    'links': {
+        TransformationTypes.ECS.value: {
+            'name': 'links',
+            'required': False,
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'links',
+            'required': False,
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'links',
+            'required': False,
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'dependencies',
+            'required': False,
+        },
+    },
+    'port_mappings': {
+        TransformationTypes.ECS.value: {
+            'name': 'portMappings',
+            'required': False,
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'ports',
+            'required': False,
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'ports',
+            'required': False,
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.portMappings',
+            'required': False,
+        },
+    },
+    'environment': {
+        TransformationTypes.ECS.value: {
+            'name': 'environment',
+            'required': False,
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'environment',
+            'required': False,
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'environment',
+            'required': False,
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'env',
+            'required': False,
+        },
+    },
+    'entrypoint': {
+        TransformationTypes.ECS.value: {
+            'name': 'entryPoint',
+            'required': False,
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'entrypoint',
+            'required': False,
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'entrypoint',
+            'required': False,
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.parameters.entrypoint',
+            'required': False,
+        },
+    },
+    'command': {
+        TransformationTypes.ECS.value: {
+            'name': 'command',
+            'required': False,
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'command',
+            'required': False,
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'command',
+            'required': False,
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'args',
+            'required': False,
+        },
+    },
+    'essential': {
+        TransformationTypes.ECS.value: {
+            'name': 'essential',
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': None,
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'essential',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': None,
+            'required': False
+        },
+    },
+    'volumes_from': {
+        TransformationTypes.ECS.value: {
+            'name': 'volumesFrom',
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'volumes_from',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'volumes_from',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.parameters.volumes-from',
+            'required': False,
+            'type': list
+        },
+    },
+    'volumes': {
+        TransformationTypes.ECS.value: {
+            'name': 'mountPoints',
+            'attribute': '',
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'volumes',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'volumes',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.volumes',
+            'required': False
+        },
+    },
+    'dns': {
+        TransformationTypes.ECS.value: {
+            'name': None,
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'dns',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'dns',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.parameters.dns',
+            'required': False,
+            'type': list
+        },
+    },
+    'work_dir': {
+        TransformationTypes.ECS.value: {
+            'name': None,
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'working_dir',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'working_dir',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.parameters.workdir',
+            'required': False
+        },
+    },
+    'domain': {
+        TransformationTypes.ECS.value: {
+            'name': None,
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'dns_search',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': None,
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.parameters.dns-search',
+            'required': False,
+            'type': list
+        },
+    },
+    'build': {
+        TransformationTypes.ECS.value: {
+            'name': None,
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'build',
+            'required': False,
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': None,
+            'required': False,
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': None,
+            'required': False,
+        },
+    },
+    'expose': {
+        TransformationTypes.ECS.value: {
+            'name': None,
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'expose',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'expose',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.parameters.expose',
+            'required': False,
+            'type': list
+        },
+    },
+    'network': {
+        TransformationTypes.ECS.value: {
+            'name': None,
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'networks',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'net',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.parameters.net',
+            'required': False,
+            'type': list
+        },
+    },
+    'net_mode': {
+        TransformationTypes.ECS.value: {
+            'name': None,
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'network_mode',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'net',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.network',
+            'required': False,
+        },
+    },
+    'privileged': {
+        TransformationTypes.ECS.value: {
+            'name': None,
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'privileged',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'privileged',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.privileged',
+            'required': False
+        },
+    },
+    'labels': {
+        TransformationTypes.ECS.value: {
+            'name': 'dockerLabels',
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'labels',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'labels',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'labels',
+            'required': False
+        },
+    },
+    'logging': {
+        TransformationTypes.ECS.value: {
+            'name': 'logConfiguration',
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'logging',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'logging',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.parameters.log-driver',
+            'required': False,
+            'type': list,
+        },
+    },
+    'user': {
+        TransformationTypes.ECS.value: {
+            'name': 'user',
+            'required': False
+        },
+        TransformationTypes.COMPOSE.value: {
+            'name': 'user',
+            'required': False
+        },
+        TransformationTypes.SYSTEMD.value: {
+            'name': 'user',
+            'required': False
+        },
+        TransformationTypes.MARATHON.value: {
+            'name': 'container.docker.parameters.user',
+            'required': False
+        },
+    }
+    # TODO: 'containername', 'pid'
+})
