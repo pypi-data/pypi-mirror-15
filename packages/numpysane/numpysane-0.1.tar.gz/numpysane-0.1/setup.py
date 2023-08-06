3ec20f962b4a0fb63d1d7ddd22e0a1826bea179b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:36 +0000

    Add package numpysane/0.1/numpysane-0.1.tar.gz

---
+++ packages/numpysane/numpysane-0.1.tar.gz/numpysane-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name         = 'numpysane',
+      version      = '0.1',
+      author       = 'Dima Kogan',
+      author_email = 'dima@secretsauce.net',
+      url          = 'http://github.com/dkogan/numpysane',
+      description  = 'more-reasonable core functionality for numpy',
+      license      = 'LGPL-3+',
+      py_modules   = ['numpysane'],
+      cmdclass     = {})
