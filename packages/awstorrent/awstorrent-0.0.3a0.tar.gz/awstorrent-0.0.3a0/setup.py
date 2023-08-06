17b84d579abf7c8f9384e9d88f9725611a0c2849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:17 +0000

    Add package awstorrent/0.0.3a0/awstorrent-0.0.3a0.tar.gz

---
+++ packages/awstorrent/awstorrent-0.0.3a0.tar.gz/awstorrent-0.0.3a0/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup,find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__),fname)).read()
+
+setup(
+    name='awstorrent',
+    version='0.0.3a',
+    author='Cheng Yan',
+    author_email='firearasi@qq.com',
+    description='A simple bittorent tool with aws',
+    license='GPL',
+    packages=find_packages(),
+    install_requires=['pyperclip'],
+    entry_points = {
+        'console_scripts' : [
+            'awstorrent=awstorrent.__main__:main'
+         ]
+    }
+)
+    
