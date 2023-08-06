dfb5653460206ee6b5e7d57beb63ed8e515007fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:41 +0000

    Add package nester_eji5024/1.2.0/nester_eji5024-1.2.0.zip

---
+++ packages/nester_eji5024/nester_eji5024-1.2.0.zip/nester_eji5024-1.2.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name             = 'nester_eji5024',
+    version          = '1.2.0',
+    py_modules    = ['nester_eji5024'],
+    author            = 'hfpython_eji5024',
+    author_email   = 'eji5024@gmail.com',
+    description     = 'A simple printer of nested lists, from HF Python learning.',
+)
