ec35ef9947aff1b70a62191bd33bd4669610276f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:30 +0000

    Add package aipsetup/3.3.8/aipsetup-3.3.8.tar.gz

---
+++ packages/aipsetup/aipsetup-3.3.8.tar.gz/aipsetup-3.3.8/wayround_org/aipsetup/builder_scripts/scons.py
@@ -0,0 +1,53 @@
+
+import os.path
+import subprocess
+import collections
+
+import wayround_org.aipsetup.builder_scripts.std
+
+# TODO: add fixes and patches
+
+
+class Builder(wayround_org.aipsetup.builder_scripts.std.Builder):
+
+    def define_custom_data(self):
+        # 'python2' value may go here
+        return None
+
+    def define_actions(self):
+        return collections.OrderedDict([
+            ('dst_cleanup', self.builder_action_dst_cleanup),
+            ('src_cleanup', self.builder_action_src_cleanup),
+            ('bld_cleanup', self.builder_action_bld_cleanup),
+            ('extract', self.builder_action_extract),
+            ('bootstrap', self.builder_action_bootstrap),
+            ('distribute', self.builder_action_distribute)
+            ])
+
+    def builder_action_bootstrap(self, called_as, log):
+        p = subprocess.Popen(
+            [
+                'python2',
+                'bootstrap.py',
+                wayround_org.utils.path.join(self.get_src_dir(), 'build', 'scons')
+                ],
+            cwd=self.get_src_dir(),
+            stdout=log.stdout,
+            stderr=log.stderr
+            )
+        ret = p.wait()
+        return ret
+
+    def builder_action_distribute(self, called_as, log):
+        p = subprocess.Popen(
+            [python,
+             'setup.py',
+             'install',
+             '--prefix={}'.format(self.calculate_dst_install_prefix())
+             ],
+            cwd=wayround_org.utils.path.join(self.get_src_dir(), 'build', 'scons'),
+            stdout=log.stdout,
+            stderr=log.stderr
+            )
+        ret = p.wait()
+        return ret
