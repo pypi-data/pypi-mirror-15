8232d09510032aaf1a45545061bc24cb94e02c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:03 +0000

    Add package decisiontree_pca_ra/0.1/decisiontree_pca_ra-0.1.tar.gz

---
+++ packages/decisiontree_pca_ra/decisiontree_pca_ra-0.1.tar.gz/decisiontree_pca_ra-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'decisiontree_pca_ra',
+  packages = ['decisiontree_pca_ra'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A library that will use pca and decision_tree to find variables with low correlation',
+  author = 'Rodrigo Marcos',
+  author_email = 'rodri.pinar@gmail.com',
+  url = 'https://github.com/romcra/decisiontree_pca_ra', # use the URL to the github repo
+  download_url = 'https://github.com/romcra/decisiontree_pca_ra/tarball/0.1', # I'll explain this in a second
+  keywords = ['datascience', 'methodselection'], # arbitrary keywords
+  classifiers = [],
+)
