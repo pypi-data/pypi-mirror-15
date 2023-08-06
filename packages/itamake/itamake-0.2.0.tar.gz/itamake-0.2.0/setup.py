f39b70a7eb6bf071ea167afe214e3b267ea8c63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:14 +0000

    Add package itamake/0.2.0/itamake-0.2.0.tar.gz

---
+++ packages/itamake/itamake-0.2.0.tar.gz/itamake-0.2.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='itamake',
+      version='0.2.0',
+      description='A stripped down cmsMake which *only* makes, nothing more.',
+      author='Algorithm Ninja',
+      author_email='algorithm@ninja',
+      license='GPL3',
+      url='https://github.com/algorithm-ninja/ita-make',
+      packages=find_packages(),
+      install_requires=[
+          'pyyaml',
+          'six',
+          'cmscommon==0.1.0',
+      ],
+      entry_points={
+          'console_scripts': [
+              'itaMake=itamake.itaMake:main'
+          ]
+      })
