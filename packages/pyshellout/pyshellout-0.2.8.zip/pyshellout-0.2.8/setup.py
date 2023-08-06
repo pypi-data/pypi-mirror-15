eb5cd7db14fe43e80e834a3844b026089bf7260f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:09 +0000

    Add package pyshellout/0.2.8/pyshellout-0.2.8.zip

---
+++ packages/pyshellout/pyshellout-0.2.8.zip/pyshellout-0.2.8/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='pyshellout',
+    packages=['pyshellout'],  # this must be the same as the name above
+    version='0.2.8',
+    description='Thin convenience wrappers for shelling out commands easily from python',
+    long_description=long_description,
+    author='Chiel ten Brinke',
+    author_email='ctenbrinke@gmail.com',
+    url='https://github.com/Chiel92/python-shellout',  # use the URL to the github repo
+    keywords=['pyshellout', 'shellout', 'shell', 'CLI'],  # arbitrary keywords
+    classifiers=[],
+)
