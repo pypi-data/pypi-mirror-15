c65e95ea94296729acd03559032739d2a9eea66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:47 +0000

    Add package begen/0.3/begen-0.3.tar.gz

---
+++ packages/begen/begen-0.3.tar.gz/begen-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='begen',
+      version='0.3',
+      description='versatile http/https traffic generator',
+      url='https://github.com/perrin-ay/begen',
+      author='perrin-ay',
+      author_email='arnabchat@hotmail.com',
+      license='',
+      packages=['begen'],
+      install_requires=[''],
+      zip_safe=False)
