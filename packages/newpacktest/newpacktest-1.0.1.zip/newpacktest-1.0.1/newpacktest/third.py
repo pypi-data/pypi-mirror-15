204464630171f389ed7786fd0348534e25996575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:49 +0000

    Add package newpacktest/1.0.1/newpacktest-1.0.1.zip

---
+++ packages/newpacktest/newpacktest-1.0.1.zip/newpacktest-1.0.1/newpacktest/third.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env/ python3
+# -*-coding : utf-8 -*-
+
+__author__='zhang'
+
+#import sys
+class printanum(object):
+	"""docstring for printanum"""
+	def __init__(self, arg):
+		super(printanum, self).__init__()
+		self.arg = arg
+
+
+    def show():
+	    print('a=2')
