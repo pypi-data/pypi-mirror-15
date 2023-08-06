bba327be62746c09e88bfe0c3647f343940cd07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:26 +0000

    Add package maxh/1.0.0/maxh-1.0.0.tar.gz

---
+++ packages/maxh/maxh-1.0.0.tar.gz/maxh-1.0.0/maxh.py
@@ -0,0 +1,7 @@
+"""This is the maxh's module and it provides one function called say_hello()
+   which prints "Hello, Wordl!" onto the screen."""
+
+def say_hello():
+    """This function takes no parameter and just print "Hello, world!"
+       onto the screen."""
+    print("Hello, world!")
---
+++ packages/maxh/maxh-1.0.0.tar.gz/maxh-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name         = 'maxh',
+       version      = '1.0.0',
+       py_modules   = ['maxh'],
+       author       = 'max hsu',
+       author_email = 'max80713@hotmail.com',
+       url          = 'https://github.com/max80713',
+       description  = 'A simple hello world example',
+     )
