09da6a952059ebdef8ef69e5ffc1b2b6ff411e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:57 +0000

    Add package ubiome/0.6.4/ubiome-0.6.4.tar.gz

---
+++ packages/ubiome/ubiome-0.6.4.tar.gz/ubiome-0.6.4/ubiome/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'sprague'
+
+from .ubiome import UbiomeDiffSample, UbiomeSample, UbiomeTaxa
+from .ubiomeMultiSample import UbiomeMultiSample
+
+__all__ = ['UbiomeSample','UbiomeDiffSample','UbiomeMultiSample']
+version = '0.6.4'
