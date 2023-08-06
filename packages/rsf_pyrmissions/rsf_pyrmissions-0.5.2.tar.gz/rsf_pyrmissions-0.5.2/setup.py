5a1d256b3933442d912b6343065afe9b6fd85974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:40 +0000

    Add package rsf_pyrmissions/0.5.2/rsf_pyrmissions-0.5.2.tar.gz

---
+++ packages/rsf_pyrmissions/rsf_pyrmissions-0.5.2.tar.gz/rsf_pyrmissions-0.5.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'rsf_pyrmissions',
+  packages = ['rsf_pyrmissions'],
+  version = '0.5.2',
+  description = 'A Ridiculously Straight Forward Permissioning system (aka Access Control) for python projects',
+  author = 'Syed Rakib Al Hasan',
+  author_email = 'syed.rakib.al.hasan@gmail.com',
+  url = 'https://github.com/syedrakib/rsf-pyrmissions',
+  download_url = 'https://github.com/syedrakib/rsf-pyrmissions/tarball/0.5.2',
+  keywords = ['ridiculous', 'permission', 'acl', 'access', 'control', 'rsf', 'role', 'user', 'privileges'],
+  classifiers = [],
+)
