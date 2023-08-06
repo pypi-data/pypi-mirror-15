af103fc9756434be2ddfe36c8cb6ada9ee36d501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:12 +0000

    Add package popuparchive/1.0.0/popuparchive-1.0.0.tar.gz

---
+++ packages/popuparchive/popuparchive-1.0.0.tar.gz/popuparchive-1.0.0/popuparchive/__init__.py
@@ -0,0 +1,14 @@
+"""
+Pop Up Archive Python Client
+https://developer.popuparchive.com/
+"""
+
+import sys
+import os
+import json
+import requests
+
+from client import *
+
+__version__ = '1.0.0'
+
---
+++ packages/popuparchive/popuparchive-1.0.0.tar.gz/popuparchive-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name    = "popuparchive",
+  packages = [ "popuparchive" ],
+  version = "1.0.0",
+  description = 'Python client for interacting with the Pop Up Archive API.',
+  author = 'Pop Up Archive',
+  author_email = 'edison@popuparchive.com',
+  url = 'https://github.com/popuparchive/pua-sdk-python',
+  download_url = ' https://github.com/popuparchive/pua-sdk-python/tarball/1.0.0',
+  keywords = ['transcription', 'audio', 'archives', 'mp3', 'radio', 'podcasts'],
+  classifiers = [],
+)
