44a0ec424a7a4b0c1235006df75ad0c63025e8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:27 +0000

    Add package katastrophe/2.0.0/katastrophe-2.0.0.tar.gz

---
+++ packages/katastrophe/katastrophe-2.0.0.tar.gz/katastrophe-2.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(name='katastrophe',
+      packages=['katastrophe'],
+      version='2.0.0',
+      description='Download torrents from kat.ph directly through terminal',
+      author='Aly Akhtar',
+      license='MIT',
+      author_email='samurai.aly@gmail.com',
+      url='https://github.com/alyakhtar/katastrophe',
+      download_url = 'https://github.com/alyakhtar/mypackage/tarball/1.1.5',
+      entry_points='''
+               [console_scripts]
+               katastrophe=katastrophe:main
+           ''',
+      install_requires=[
+          'beautifulsoup4', 'tabulate', 'requests', 'lxml', 'docopt','bencode','bitstring'
+      ],
+      keywords=['torrent', 'download', 'kat.ph'], )
