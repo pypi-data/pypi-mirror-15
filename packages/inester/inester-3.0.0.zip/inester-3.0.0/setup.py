c0dbf3070e349ade4cafcff0ffa7e4ae3ff40e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:28 +0000

    Add package inester/3.0.0/inester-3.0.0.zip

---
+++ packages/inester/inester-3.0.0.zip/inester-3.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'inester',
+        version         = '3.0.0',
+        py_modules      = ['inester'],
+        author          = 'Ayo Rotibi',
+        author_email    = 'ayodejirotibi@aol.com',
+        url             = 'http://www.isecureconsulting.com',
+        description     = 'Simple nested print',
+)
