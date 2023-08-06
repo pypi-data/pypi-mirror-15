ef8fe77936e72589d8907bda4e0b403aade44ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:55 +0000

    Add package feeluown/1.0.3.5/feeluown-1.0.3.5-py3.4.egg

---
+++ packages/feeluown/feeluown-1.0.3.5-py3.4.egg/feeluown/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from .consts import LOG_FILE
+from .config import config
+
+
+__version__ = '1.0.3.5'
+
+
+def logger_config():
+    if config.debug:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="[%(levelname)s] [%(filename)s line:%(lineno)d] : %(message)s",
+        )
+    else:
+        logging.basicConfig(
+            format="[%(levelname)s] [%(filename)s line:%(lineno)d] : %(message)s",
+            level=logging.DEBUG,
+            filename=LOG_FILE,
+            filemode='w',
+        )
