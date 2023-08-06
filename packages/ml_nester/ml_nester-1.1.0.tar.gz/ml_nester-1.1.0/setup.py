110a833b4cbb4d4d1f4121ed0ffcdc6c0b46eae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:40 +0000

    Add package ml_nester/1.1.0/ml_nester-1.1.0.tar.gz

---
+++ packages/ml_nester/ml_nester-1.1.0.tar.gz/ml_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'ml_nester',
+		version = '1.1.0',
+		py_modules = ['nester'],
+		author = 'MagnusLislevatn',
+		author_email = 'magnus.lislevatn@gmail.com',
+		url = 'http://magnuslisleva.tn',
+		description = 'A simple printer of nested lists',
+	)
