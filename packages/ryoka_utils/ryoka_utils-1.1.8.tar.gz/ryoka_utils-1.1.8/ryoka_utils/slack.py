0e431001bb02350ba3be4fdf01c79497b9d31b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:03 +0000

    Add package ryoka_utils/1.1.8/ryoka_utils-1.1.8.tar.gz

---
+++ packages/ryoka_utils/ryoka_utils-1.1.8.tar.gz/ryoka_utils-1.1.8/ryoka_utils/slack.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from slacker import Slacker
+from utils import Utils
+ 
+class Slack(Utils, Slacker):
+    def __init__(self, token):
+        Utils.__init__(self)
+        Slacker.__init__(self, token)
+
+        self.channel_list = []
+        self.username = "Slack Bot"
+
+        # Fetch channel list.
+        raw_data = self.channels.list().body
+        for data in raw_data["channels"]:
+            self.channel_list.append(dict(channel_id=data["id"], channel_name=data["name"])) 
+
+    def _is_exist_channel(self, channel):
+        for c in self.channel_list:
+            if (c["channel_name"] == channel): return True
+        return False
+
+    def set_username(self, username):
+        self.username = username
+ 
+    def get_channel_list(self):
+        return self.channel_list
+
+    def post_message_to_channel(self, channel, message):
+        if (not self._is_exist_channel(channel)):
+            print("Not exist channel. [%s]" % (channel))
+            self.exit()
+
+        channel_name = "#" + channel
+        self.chat.post_message(channel_name, message, username=self.username)
+ 
+if __name__ == "__main__":
+    slack = Slack("xoxp-44427877605-44427877701-44425136774-df3a4e42b8")
+
+    slack.post_message_to_channel("general", "TEST")
+
