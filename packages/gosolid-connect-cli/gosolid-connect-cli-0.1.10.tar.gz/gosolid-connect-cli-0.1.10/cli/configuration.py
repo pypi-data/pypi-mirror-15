234205fdcb5e7c4a9095acb937a92d29182402e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:05 +0000

    Add package gosolid-connect-cli/0.1.10/gosolid-connect-cli-0.1.10.tar.gz

---
+++ packages/gosolid-connect-cli/gosolid-connect-cli-0.1.10.tar.gz/gosolid-connect-cli-0.1.10/cli/configuration.py
@@ -0,0 +1,5 @@
+BASE_URL = 'http://test.connect.gosolid.io/api/v1/'
+
+BITBUCKET_URL = 'https://%(username)s:%(password)s@bitbucket.org/gosolid/%(repository)s'
+VAGRANTFILE_NAME = 'Vagrantfile'
+DEFAULT_GIT_BRANCH = 'prod'
