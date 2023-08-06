c5a2f5466c218aae7a3626bba1284a29c3f333c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:40 +0000

    Add package MarkovPy/0.2.0/MarkovPy-0.2.0.tar.gz

---
+++ packages/MarkovPy/MarkovPy-0.2.0.tar.gz/MarkovPy-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='MarkovPy',
+    version='0.2.0',
+    description='Markov-implementation with redis-database',
+    author='Thor77',
+    author_email='thor77@thor77.org',
+    url='https://github.com/Thor77/MarkovPy',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Communications :: Chat',
+        'Topic :: Database',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    keywords='markovpy markov ai',
+    packages=['markov']
+)
