d1599889ae5231914850f6c6d9ebb1cf4c06dff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:50 +0000

    Add package financialdata/0.3/financialdata-0.3.zip

---
+++ packages/financialdata/financialdata-0.3.zip/financialdata-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='financialdata',
+    version='0.3',
+    description='Final Financial Analytics Project Group H',
+    url='https://github.com/christabelle/financialdata_GroupH',
+    download_url='https://github.com/christabelle/financialdata_GroupH/tarball/0.1',
+    author='Group-H',
+    author_email='christabelle@student.ie.edu',
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords=['final', 'financial', 'analytics', 'project'],
+    packages=['financialdata']
+)
