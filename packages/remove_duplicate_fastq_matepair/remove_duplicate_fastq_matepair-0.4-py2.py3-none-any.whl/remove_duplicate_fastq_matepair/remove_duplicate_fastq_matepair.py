44062ef9eaacf320b069633cf5f5f92181d1bf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:30 +0000

    Add package remove_duplicate_fastq_matepair/0.4/remove_duplicate_fastq_matepair-0.4-py2.py3-none-any.whl

---
+++ packages/remove_duplicate_fastq_matepair/remove_duplicate_fastq_matepair-0.4-py2.py3-none-any.whl/remove_duplicate_fastq_matepair/remove_duplicate_fastq_matepair.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import os
+import sys
+import time
+
+import pandas as pd
+from Bio import SeqIO
+
+from cdis_pipe_utils import df_util
+
+def store_dup_db(uuid, fastq_name, duplicate_set, engine):
+    if len(duplicate_set >= 1):
+        duplicate_list = sorted(list(duplicate_set))
+        df = pd.DataFrame({'uuid': [uuid],
+                           'fastq_name': fastq_name
+        })
+        df['qname'] = duplicate_list
+        unique_key_dict = {'uuid': uuid, 'fastq_name': fastq_name}
+        table_name = 'fastq_duplicate_qname'
+        df_util.save_df_to_sqlalchemy(df, unique_key_dict, table_name, engine, logger)
+    return
+
+def remove_dups(infile, outfile, fastq_name, engine, logger):
+    aset = set()
+    duplicate_set = set()
+    readcounter = 0
+    duplicatecounter = 0
+    for record in SeqIO.parse(infile,'fastq'):
+        if record.id in aset:
+            duplicate_set.add(record.id)
+            duplicatecounter += 1
+            logger.info('duplicate: %s' % record.id)
+        else:
+            readcounter += 1
+            aset.add(record.id)
+            SeqIO.write(record, outfile, 'fastq')
+    logger.info('len(aset)=%s' % len(aset))
+    logger.info('readcounter=%s' % str(readcounter))
+    logger.info('duplicatecounter=%s' % str(duplicatecounter))
+    store_dup_db(uuid, fastq_name, duplicate_set, engine)
+    return
+
+def main():
+    parser = argparse.ArgumentParser('remove duplicate reads from fastq')
+    parser.add_argument('infile',
+                        nargs='?',
+                        type=argparse.FileType('r'),
+                        default=sys.stdin
+    )
+    parser.add_argument('outfile',
+                        nargs='?',
+                        type=argparse.FileType('w'),
+                        default=sys.stdout
+    )
+    parser.add_argument('-u', '--uuid',
+                        required = True
+    )
+    parser.add_argument('-f', '--fastq_name',
+                        required = True
+    )
+    args = parser.parse_args()
+
+    infile = args.infile
+    outfile = args.outfile
+    uuid = args.uuid
+    fastq_name = args.fastq_name
+
+    logging.basicConfig(
+        filename=log_file_path,
+        filemode='a',
+        level = logging.INFO,
+    )
+    logger = logging.getLogger(__name__)
+    hostname = os.uname()[1]
+    logger.info('hostname=%s' % hostname)
+
+    sqlite_name = uuid + '.db'
+    engine_path = 'sqlite:///' + sqlite_name
+    engine = sqlalchemy.create_engine(engine_path, isolation_level='SERIALIZABLE')
+
+    start_time = time.time()
+    remove_dups(infile, outfile, fastq_name, engine, logger)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    #store time
+    df = time_util.store_seconds(uuid, elapsed_time, logger)
+    df['fastq_name'] = fastq_name
+    unique_key_dict = {'uuid': uuid, 'fastq_name': fastq_name}
+    table_name = 'time_remove_duplicate_fastq_matepair'
+    return
+
+if __name__ == '__main__':
+    main()
