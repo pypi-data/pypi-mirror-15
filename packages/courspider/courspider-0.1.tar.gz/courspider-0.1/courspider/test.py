78281116e9d205e742ff4221847164216111ce82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:24 +0000

    Add package courspider/0.1/courspider-0.1.tar.gz

---
+++ packages/courspider/courspider-0.1.tar.gz/courspider-0.1/courspider/test.py
@@ -0,0 +1,28 @@
+import json
+
+
+from faculty_calender import FacultyCalender
+from faculty_calender_resources.faculty_of_arts_and_science_calender import FacultyOfArtsAndScienceCalender
+from faculty_calender_resources.session import Session
+from faculty_calender_resources.session import Season
+from faculty_calender_resources.url import URL
+
+cal = FacultyOfArtsAndScienceCalender(Session(2016, Season.FALL),
+                                      URL("http://calendar.artsci.utoronto.ca"))
+
+print('getting department calenders')
+deps = cal.get_department_calenders()
+
+course_datas = []
+
+print('getting course datas')
+for dep in deps:
+    for course in dep.get_courses():
+        course_datas.append(course.to_data())
+
+# print(type(course_datas))
+# print(type(course_datas[0]))
+# print(type(course_datas[0]['Course code']))
+
+file = open("courses.json", 'w')
+file.write(json.dumps({"courses":course_datas}, indent=4))
---
+++ packages/courspider/courspider-0.1.tar.gz/courspider-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'courspider',
+  packages = ['courspider'],
+  version = '0.1',
+  description = 'A webscraper API to scrape course data from u of t calenders',
+  author = 'Tony Xiao',
+  author_email = 'zylphrex@gmail.com',
+  url = 'https://github.com/Zylphrex/courspider',
+  download_url = 'https://github.com/Zylphrex/courspider/tarball/0.1',
+  keywords = ['u of t', 'University', 'Toronto', 'course', 'data'],
+  classifiers = [],
+)
