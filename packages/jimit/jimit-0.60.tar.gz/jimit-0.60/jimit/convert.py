d84f251a8e892d89d2999db3aae0f52a18f71e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:49 +0000

    Add package jimit/0.60/jimit-0.60.tar.gz

---
+++ packages/jimit/jimit-0.60.tar.gz/jimit-0.60/jimit/convert.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'James Iter'
+__date__ = '15/4/27'
+__contact__ = 'james.iter.cn@gmail.com'
+__copyright__ = '(c) 2015 by James Iter.'
+
+import decimal
+import json
+
+from common import *
+
+
+class Convert(object):
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def sql2json(sql_obj):
+        j_sql = dict()
+        result = dict()
+        result['state'] = Common.exchange_state(20000)
+
+        try:
+            for col in sql_obj._sa_class_manager.mapper.mapped_table.columns:
+                if isinstance(getattr(sql_obj, col.name), decimal.Decimal):
+                    j_sql[col.name] = str(getattr(sql_obj, col.name))
+                else:
+                    j_sql[col.name] = getattr(sql_obj, col.name)
+        except Exception, e:
+            result['state'] = Common.exchange_state(50003)
+            result['state']['sub']['detail'] = e.message
+            raise ji.JITError(json.dumps(result))
+
+        result['j_sql'] = j_sql
+        return result['j_sql']
+
+    @staticmethod
+    def dumps(func):
+        def _dumps(*args, **kwargs):
+            ret = func(*args, **kwargs)
+            return json.dumps(ret, ensure_ascii=False)
+        return _dumps
