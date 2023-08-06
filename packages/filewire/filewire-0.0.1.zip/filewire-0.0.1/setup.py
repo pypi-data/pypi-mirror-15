81ba354d3cdd0c370a68fcaccc11238d209a0464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:27 +0000

    Add package filewire/0.0.1/filewire-0.0.1.zip

---
+++ packages/filewire/filewire-0.0.1.zip/filewire-0.0.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import share
+
+print(find_packages())
+
+setup(
+    name='filewire',
+    version='0.0.1',
+    description='Easily send and receive files in LAN',
+    author='zxey',
+    author_email='zxey.bones@gmail.com',
+    url='http://www.google.com',
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+        'Topic :: Communications',
+        'Topic :: Internet'
+    ],
+    keywords='lan share send receive file',
+    entry_points={
+        'console_scripts': [
+            'wire=wire:main',
+        ]
+    }
+)
