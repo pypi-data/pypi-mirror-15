93a3c5b00dd4797baf57ec4e3a3ce83434a298e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:26 +0000

    Add package pynputter/1.01/pynputter-1.01.tar.gz

---
+++ packages/pynputter/pynputter-1.01.tar.gz/pynputter-1.01/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'pynputter',
+    packages = ['pynputter'],
+    version = '1.01',
+    description = 'Python module for writing high level scripts that send key strokes to a computer interface',
+    author = 'James Carson',
+    author_email = 'james.carson.mobile@gmail.com',
+    url = 'https://github.com/Talos-Laboratories/pynputter',
+    download_url = 'https://github.com/Talos-Laboratories/pynputter/tarball/1.01',
+    keywords = ['Automation', 'Keys', 'Data Entry'],
+    classifiers = [],
+    )
