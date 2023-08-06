d27a3fa4d352fcc7225e6c10c3b893dafcf33de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:29 +0000

    Add package vsure/0.9.0/vsure-0.9.0.tar.gz

---
+++ packages/vsure/vsure-0.9.0.tar.gz/vsure-0.9.0/setup.py
@@ -0,0 +1,31 @@
+""" Setup for python-verisure """
+
+from setuptools import setup
+
+setup(
+    name='vsure',
+    version='0.9.0',
+    description='Read and change status of verisure devices through mypages.',
+    long_description='A module for reading and changing status of ' +
+    'verisure devices through mypages. Compatible ' +
+    'with both Python2.7 and Python3.',
+    url='http://github.com/persandstrom/python-verisure',
+    author='Per Sandstrom',
+    author_email='per.j.sandstrom@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Home Automation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='home automation verisure',
+    install_requires=['requests>=2.9.1'],
+    packages=['verisure', 'verisure.devices'],
+    zip_safe=True)
