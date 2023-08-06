1adcf003718b5ee117e7bcb27951864f0bab71dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:40 +0000

    Add package statsmodels/0.8.0rc1/statsmodels-0.8.0rc1-cp27-none-win_amd64.whl

---
+++ packages/statsmodels/statsmodels-0.8.0rc1-cp27-none-win_amd64.whl/statsmodels/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.8.0rc1'
+version = '0.8.0rc1'
+full_version = '0.8.0rc1'
+git_revision = 'a120daad5996434cb6131a000780ea77a12129e7'
+release = True
+
+if not release:
+    version = full_version
