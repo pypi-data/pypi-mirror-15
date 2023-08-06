4c4d946a6687f0956d21a03b336f758fb07b8543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:55 +0000

    Add package RepeaterStatusBot/0.0.5/RepeaterStatusBot-0.0.5.tar.gz

---
+++ packages/RepeaterStatusBot/RepeaterStatusBot-0.0.5.tar.gz/RepeaterStatusBot-0.0.5/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+version = '0.0.5'
+
+# get the requirements from the requirements.txt
+requirements_file = open('requirements.txt').read().split()
+requirements = requirements_file
+
+# get the test requirements from the test_requirements.txt
+test_requirements = []
+
+setup(
+    name='RepeaterStatusBot',
+    version=version,
+    description=(''),
+    long_description='',
+    author='Wim Fournier',
+    author_email='wim@fournier.nl',
+    url='https://github.com/hsmade/RepeaterScripts/tree/master/RepeaterStatusBot',
+    packages=[
+        'RepeaterStatusBot',
+    ],
+    package_dir={'RepeaterStatusBot':
+                 '.'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="WTFPL",
+    zip_safe=False,
+    keywords='RepeaterStatusBot',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+    ],
+    entry_points={
+        'console_scripts': [
+             'RepeaterStatusBot = RepeaterStatusBot:main'
+        ]
+    },
+ 
+)
