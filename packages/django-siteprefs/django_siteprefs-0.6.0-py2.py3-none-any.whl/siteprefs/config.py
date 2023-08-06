6fbef8ed5b851be6561c0a14c2da2b0c312ab535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:19 +0000

    Add package django-siteprefs/0.6.0/django_siteprefs-0.6.0-py2.py3-none-any.whl

---
+++ packages/django-siteprefs/django_siteprefs-0.6.0-py2.py3-none-any.whl/siteprefs/config.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class SiteprefsConfig(AppConfig):
+    """The default siteprefs configuration."""
+
+    name = 'siteprefs'
+    verbose_name = _('Site Preferences')
+
+    def ready(self):
+        from .toolbox import autodiscover_siteprefs
+        autodiscover_siteprefs()
