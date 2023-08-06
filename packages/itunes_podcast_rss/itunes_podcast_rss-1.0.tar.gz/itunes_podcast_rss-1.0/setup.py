2b4a1e7097f4a949b9a50563a80147bb69a0e1a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:08 +0000

    Add package itunes_podcast_rss/1.0/itunes_podcast_rss-1.0.tar.gz

---
+++ packages/itunes_podcast_rss/itunes_podcast_rss-1.0.tar.gz/itunes_podcast_rss-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'itunes_podcast_rss',
+  packages = ['itunes_podcast_rss'],
+  version = '1.0',
+  description = 'Python library to obtain RSS feed URL from iTunes link',
+  author = 'Michal Holub',
+  author_email = 'holub.michal@gmail.com',
+  url = 'https://github.com/wotaen/itunes_podcast_rss',
+  download_url = 'https://codeload.github.com/wotaen/itunes_podcast_rss/legacy.tar.gz/1.0',
+  keywords = ['itunes', 'podcast', 'rss'],
+  classifiers = [],
+)
