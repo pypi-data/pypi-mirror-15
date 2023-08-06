2648249e41951582f6f0925cbc4d4427eb9ae97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:19 +0000

    Add package python-ninegag/1.0/python-ninegag-1.0.zip

---
+++ packages/python-ninegag/python-ninegag-1.0.zip/python-ninegag-1.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    install_requires=['beautifulsoup4', 'requests'],
+    name='python-ninegag',
+    version='1.0',
+    py_modules=['pyninegag'],
+    url='https://github.com/sashgorokhov/python-ninegag',
+    download_url='https://github.com/sashgorokhov/python-ninegag/archive/master.zip',
+    keywords=['ninegag', '9gag'],
+    classifiers=[],
+    long_description=long_description,
+    license='MIT License',
+    author='sashgorokhov',
+    author_email='sashgorokhov@gmail.com',
+    description='Python library to get stuff from 9gag.com'
+)
