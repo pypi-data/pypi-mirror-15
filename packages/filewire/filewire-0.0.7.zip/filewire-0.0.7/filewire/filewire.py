2dc4432c4485e5c4778f0119a904e5ec8583ba97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:36 +0000

    Add package filewire/0.0.7/filewire-0.0.7.zip

---
+++ packages/filewire/filewire-0.0.7.zip/filewire-0.0.7/filewire/filewire.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+	import send
+	import recv
+else:
+	from filewire import send, recv
+import sys
+import argparse
+
+def main():
+	print("name", __name__)
+	parser = argparse.ArgumentParser(description="Sends or receives file over LAN.")
+	parser.add_argument("-v", "--verbose", help="prints verbose output", action="store_true")
+	subparsers = parser.add_subparsers(help="selection", dest="selection")
+	parser_send = subparsers.add_parser("send", help="sends file", aliases=["s"])
+	parser_send.add_argument("ip", type=str)
+	parser_send.add_argument("file", type=str)
+	parser_recv = subparsers.add_parser("recv", help="receives file", aliases=["r"])
+
+	args = parser.parse_args()
+
+	if args.selection in ["recv", "r"]:
+		with recv.Receiver(verbose=args.verbose) as receiver:
+			receiver.run()
+	elif args.selection in ["send", "s"]:
+		with send.Sender(args.ip, args.file, verbose=args.verbose) as sender:
+			sender.run()
+
+if __name__ == "__main__":
+	main()
