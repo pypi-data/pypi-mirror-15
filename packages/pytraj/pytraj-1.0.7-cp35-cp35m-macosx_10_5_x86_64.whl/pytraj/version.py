c4b0898caac0d5fce170dca96197c130dec6da4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:43 +0000

    Add package pytraj/1.0.7/pytraj-1.0.7-cp35-cp35m-macosx_10_5_x86_64.whl

---
+++ packages/pytraj/pytraj-1.0.7-cp35-cp35m-macosx_10_5_x86_64.whl/pytraj/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYTRAJ SETUP.PY
+short_version = '1.0.7'
+version = '1.0.7'
+full_version = '1.0.7'
+git_revision = 'Unknown'
+release = True
+
+if not release:
+    version = full_version
