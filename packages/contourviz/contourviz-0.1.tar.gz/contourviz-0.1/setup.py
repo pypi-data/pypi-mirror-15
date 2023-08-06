22e19d7a95dc31a2ec44b59a9b556bb6fe93efbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:21 +0000

    Add package contourviz/0.1/contourviz-0.1.tar.gz

---
+++ packages/contourviz/contourviz-0.1.tar.gz/contourviz-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'contourviz',
+    version = '0.1',
+    description = 'A package that charts musical contours into a web-based interactive using music21 and D3.js.',
+    url = 'https://github.com/cjwit/contourviz',
+    author = 'Christopher Witulski',
+    author_email = 'chris.witulski@gmail.com',
+    license = 'BSD',
+    packages = ['contourviz'],
+    zip_safe = False)
