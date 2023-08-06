29b5ef1cdfd07f67c40b95c5bff8ba9b25b5b6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:11 +0000

    Add package GameDealBot/1.3.1/GameDealBot-1.3.1.tar.gz

---
+++ packages/GameDealBot/GameDealBot-1.3.1.tar.gz/GameDealBot-1.3.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='GameDealBot',
+      version='1.3.1',
+      description='reddit bot for scraping Game Deals',
+	  author = 'Kevin C.',
+	  author_email = 'kckaiwei@gmail.com',
+      keywords='reddit bot',
+      url='https://github.com/HewlettPackard/python-ilorest-library',
+      install_requires=[
+          'praw'
+      ])
