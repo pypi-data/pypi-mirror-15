e39830fb1fd7e40bb7f8a598520841d31acc9119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:53 +0000

    Add package xdjango/0.3/xdjango-0.3.tar.gz

---
+++ packages/xdjango/xdjango-0.3.tar.gz/xdjango-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+EXCLUDE_FROM_PACKAGES = []
+
+REQUIRES = ["sdklib >= 1.0", "Django==1.8.13", "djangorestframework==3.3.3"]
+
+setup(
+    name = 'xdjango',
+    version = '0.3',
+    description = 'Xtra-django library',
+    author='Ivan Martin',
+    author_email='ivanmar_91@hotmail.com',
+    url='https://bitbucket.org/projectx-team/xdjango',
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+)
