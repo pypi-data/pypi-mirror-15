86656fa2bfa422aef69c3a98eab67cb7f0b20a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:25 +0000

    Add package welcomepython/0.1/welcomepython-0.1.zip

---
+++ packages/welcomepython/welcomepython-0.1.zip/welcomepython-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='welcomepython',
+      version='0.1',
+      description='print message',
+      url='http://github.com/analytics/welcome',
+      author='Waqas Ali',
+      author_email='waqas.ali@analytics.com.pk',
+      license='Analytics',
+      packages=['welcomepython'],
+      zip_safe=False)
---
+++ packages/welcomepython/welcomepython-0.1.zip/welcomepython-0.1/welcomepython/__init__.py
@@ -0,0 +1,8 @@
+'''
+Created on Jun 2, 2016
+
+@author: Waqas Ali
+'''
+
+def printMessage():
+    print('Welcome to python!')
