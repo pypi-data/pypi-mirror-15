77062813cebfa6267d2f4027535ae548d1ba02d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:50 +0000

    Add package autocolorize/0.2.0/autocolorize-0.2.0.tar.gz

---
+++ packages/autocolorize/autocolorize-0.2.0.tar.gz/autocolorize-0.2.0/autocolorize/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import division, print_function, absolute_import
+from .extraction import (extract_sparse, extract, load_classifier,
+                         load_default_classifier, colorize)
+from .color import match_lightness
+from . import image
+from .datasets import load_image_list
+from .checker import checker_main
+
+
+VERSION = (0, 2, 0)
+ISRELEASED = True
+__version__ = '{0}.{1}.{2}'.format(*VERSION)
+if not ISRELEASED:
+    __version__ += '.git'
+
+__all__ = ['extract_sparse',
+           'extract',
+           'load_classifier',
+           'load_default_classifier',
+           'colorize',
+           'match_lightness',
+           'image',
+           'load_image_list',
+           'checker_main']
