755ec497a0d34bca0933315729674de060066d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:21 +0000

    Add package nester_maeng/1.1.0/nester_maeng-1.1.0.zip

---
+++ packages/nester_maeng/nester_maeng-1.1.0.zip/nester_maeng-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name			= 'nester_maeng',
+		version			= '1.1.0',
+		py_modules		= ['nester_maeng'],
+		author			= 'Hyunsoo',
+		author_email 	= 'hyunsoo.maeng@lge.com',
+		url				= 'http://www.lge.com',
+		description		= 'A simple printer of nested lists',
+	)
