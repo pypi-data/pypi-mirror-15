3c9581a5656cce3aceb8aa21bb005cdebafad61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:47 +0000

    Add package god/1.1.0/god-1.1.0.zip

---
+++ packages/god/god-1.1.0.zip/god-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='god',
+	version='1.1.0',
+	py_modules=['nester'],
+	author='hfpython',
+	author_email='hfpython@163.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nested lists',
+      )
