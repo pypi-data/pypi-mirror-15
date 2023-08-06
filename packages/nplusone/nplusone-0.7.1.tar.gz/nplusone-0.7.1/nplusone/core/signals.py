46c6fa6c784aaf511ef540a92c67491aff102b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:43 +0000

    Add package nplusone/0.7.1/nplusone-0.7.1.tar.gz

---
+++ packages/nplusone/nplusone-0.7.1.tar.gz/nplusone-0.7.1/nplusone/core/signals.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import functools
+import contextlib
+
+import blinker
+
+
+load = blinker.Signal()
+lazy_load = blinker.Signal()
+eager_load = blinker.Signal()
+touch = blinker.Signal()
+
+
+def get_worker(*args, **kwargs):
+    return blinker.ANY
+
+
+def signalify(signal, func, parser=None, **context):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        signal.send(
+            get_worker(),
+            args=args,
+            kwargs=kwargs,
+            context=context,
+            parser=parser,
+        )
+        return func(*args, **kwargs)
+    return wrapped
+
+
+def designalify(signal, func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        with ignore(signal):
+            return func(*args, **kwargs)
+    return wrapped
+
+
+@contextlib.contextmanager
+def ignore(signal, sender=None):
+    sender = sender or get_worker()
+    receivers = list(signal.receivers_for(sender))
+    for receiver in receivers:
+        signal.disconnect(receiver, sender=sender)
+    try:
+        yield
+    finally:
+        for receiver in receivers:
+            signal.connect(receiver, sender=sender)
