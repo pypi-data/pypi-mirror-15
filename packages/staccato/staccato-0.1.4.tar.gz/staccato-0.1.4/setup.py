dbbf49e22684b98dbe7114e067aa2e5ece1b0810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:36 +0000

    Add package staccato/0.1.4/staccato-0.1.4.tar.gz

---
+++ packages/staccato/staccato-0.1.4.tar.gz/staccato-0.1.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
+setup(
+    name="staccato",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires = ['requests', 'requests_oauthlib'],
+    author = "minamorl",
+    author_email = "minamorl@minamorl.com",
+    description = "Twitter API wrapper for python 3",
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
+)
