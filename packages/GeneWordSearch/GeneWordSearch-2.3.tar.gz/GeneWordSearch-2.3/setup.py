8dd92838c48d31acb65c33c0c55da3da98db569c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:23 +0000

    Add package GeneWordSearch/2.3/GeneWordSearch-2.3.tar.gz

---
+++ packages/GeneWordSearch/GeneWordSearch-2.3.tar.gz/GeneWordSearch-2.3/setup.py
@@ -0,0 +1,22 @@
+# Basic Setup Script
+# Requires Python3
+
+# Written by Joseph Jeffers
+# Updated Aug 18, 2015
+
+from setuptools import setup, find_packages
+
+setup(name='GeneWordSearch',
+version='2.3',
+license='GPLv2',
+description='Annotation finder for genes.',
+author='Joe Jeffers',
+author_email='jeffe174@umn.edu',
+url='https://github.com/monprin/geneWordSearch/',
+packages=find_packages(),
+package_data={
+'genewordsearch.databases':['*/totalWordCounts.*','*/geneNotes.*'],
+'webapp':['templates/home.html','static/formProcess.js']},
+long_description=open('README.rst').read(),
+scripts=['bin/gws'],
+install_requires=['flask','numpy','scipy'])
