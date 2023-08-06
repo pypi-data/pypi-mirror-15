b485692ed875577c1fb244e730a237f9712f1c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:11 +0000

    Add package laji/1.0.2/laji-1.0.2.zip

---
+++ packages/laji/laji-1.0.2.zip/laji-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'laji',
+    version = '1.0.2',
+    py_modules = ['laji'],
+    author = 'laji',
+    author_email = 'laji@qq.com',
+    url = 'http://www.laji.com',
+    description = 'a laji of laji',
+)
