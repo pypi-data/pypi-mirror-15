d1bbf7f497a2615233cf0090037d7093093630d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:20 +0000

    Add package unirio-sie/0.0.1/unirio-sie-0.0.1.tar.gz

---
+++ packages/unirio-sie/unirio-sie-0.0.1.tar.gz/unirio-sie-0.0.1/unirio/sie/base.py
@@ -0,0 +1,19 @@
+import abc
+from .adapters import ADAPTER
+
+
+class SIE(object):
+    __metaclass__ = abc.ABCMeta
+    cacheTime = 86400
+
+    def __init__(self, adapter=ADAPTER):
+        """
+        :type adapter: SIEDAOBaseAdapter
+        """
+
+        self.__adapter = adapter()
+        self.api = self.__adapter.api
+        self.usuario = self.__adapter.usuario
+
+    def handle_blob(self, arquivo):
+        return self.__adapter.handle_blob(arquivo)  # TODO descarta @staticmethod...
