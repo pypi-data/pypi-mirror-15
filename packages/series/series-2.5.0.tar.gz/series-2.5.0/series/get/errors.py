903745e1953435dadad0de96e139ae94c3eb07be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:25 +0000

    Add package series/2.5.0/series-2.5.0.tar.gz

---
+++ packages/series/series-2.5.0.tar.gz/series-2.5.0/series/get/errors.py
@@ -0,0 +1,15 @@
+from tek.errors import TException
+
+
+class SeriesDException(TException):
+    pass
+
+
+class ArchiverError(SeriesDException):
+    pass
+
+
+class InvalidDBError(SeriesDException):
+    def __init__(self, specifics):
+        text = 'Invalid database: {}'
+        super().__init__(text.format(specifics))
