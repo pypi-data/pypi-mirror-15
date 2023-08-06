d6dec72c531941dc1b04797c38393c2c5f0f30ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:03 +0000

    Add package django-log-formatter-json/0.1.0/django-log-formatter-json-0.1.0.tar.gz

---
+++ packages/django-log-formatter-json/django-log-formatter-json-0.1.0.tar.gz/django-log-formatter-json-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-log-formatter-json',
+    version='0.1.0',
+    packages=['django-log-formatter-json'],
+    author='Dan Hoerst',
+    author_email='dhoerst1@gmail.com',
+    url='https://github.com/danhoerst/json-log-formatter',
+    description='Django JSON log formatter',
+    long_description=open('README.rst').read()
+)
