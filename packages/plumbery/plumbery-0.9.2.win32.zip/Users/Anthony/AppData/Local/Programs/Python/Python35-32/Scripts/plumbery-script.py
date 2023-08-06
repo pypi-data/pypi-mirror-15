bb927b011a904f32a105a126e1b9c3c96b1d0952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:32 +0000

    Add package plumbery/0.9.2/plumbery-0.9.2.win32.zip

---
+++ packages/plumbery/plumbery-0.9.2.win32.zip/Users/Anthony/AppData/Local/Programs/Python/Python35-32/Scripts/plumbery-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\Anthony\AppData\Local\Programs\Python\Python35-32\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'plumbery==0.9.2','console_scripts','plumbery'
+__requires__ = 'plumbery==0.9.2'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('plumbery==0.9.2', 'console_scripts', 'plumbery')()
+    )
