06c23d1ca9e63b43af5d5c4a056620e8fe5382ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:17 +0000

    Add package PyCmdMessenger/0.2/PyCmdMessenger-0.2.tar.gz

---
+++ packages/PyCmdMessenger/PyCmdMessenger-0.2.tar.gz/PyCmdMessenger-0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Try using setuptools first, if it's installed
+from setuptools import setup, find_packages
+
+# Need to add all dependencies to setup as we go!
+setup(name='PyCmdMessenger',
+      packages=['PyCmdMessenger'],
+      version='0.2',
+      description='Python interface for CmdMessenger arduino serial communication library',
+      author='Michael J. Harms',
+      author_email='harmsm@gmail.com',
+      url='https://github.com/harmsm/PyCmdMessenger',
+      download_url='https://github.com/harmsm/PyCmdMessenger/tarball/0.2',
+      zip_safe=False,
+      install_requires=["pyserial"],
+      classifiers=['Programming Language :: Python'])
+
