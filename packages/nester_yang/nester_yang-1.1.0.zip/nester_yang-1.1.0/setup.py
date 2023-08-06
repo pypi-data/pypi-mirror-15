becd62181814f965d1f680873c917036380ce3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:16 +0000

    Add package nester_yang/1.1.0/nester_yang-1.1.0.zip

---
+++ packages/nester_yang/nester_yang-1.1.0.zip/nester_yang-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name='nester_yang',
+      version='1.1.0',
+      py_modules=['nester_yang'],
+      author='hfpython',
+      author_email='hfpython@headfirstlabs.com',
+      url='http://www.headfirstlabs.com',
+      description='A simple printer of nested lists',
+    )
