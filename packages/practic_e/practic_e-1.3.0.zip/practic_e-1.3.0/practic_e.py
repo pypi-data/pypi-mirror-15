dc6e2eb91d6a91798fbf1d7c8a51491a14530d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:59 +0000

    Add package practic_e/1.3.0/practic_e-1.3.0.zip

---
+++ packages/practic_e/practic_e-1.3.0.zip/practic_e-1.3.0/practic_e.py
@@ -0,0 +1,20 @@
+
+"""
+This code is use to iterate item in list and print them.
+"""
+def print_names(lst,indent=False,level=0,fh=sys.stdout):
+    """
+    Use recurise to finish finding all item in list
+    :param lst:
+    :return:
+    """
+    for item in lst:
+        if isinstance(item,list):
+            print_names(item,indent,level+1,fh)
+        else:
+            if indent:
+                for i in range(level):
+                    print("\t",end='',file=fh)
+            print(item,file=fh)
+
+
