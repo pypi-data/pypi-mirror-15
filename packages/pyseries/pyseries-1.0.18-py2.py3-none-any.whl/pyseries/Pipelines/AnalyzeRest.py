107175a2feba9002ba7caae02fdafe8d6b59bde3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:44 +0000

    Add package pyseries/1.0.18/pyseries-1.0.18-py2.py3-none-any.whl

---
+++ packages/pyseries/pyseries-1.0.18-py2.py3-none-any.whl/pyseries/Pipelines/AnalyzeRest.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 12:32:36 2016
+
+@author: user
+"""
+import sys
+sys.path.insert(0, '/Users/user/Desktop/repo_for_pyseries/pyseries/pyseries')
+
+import pyseries.LoadingData as loading
+import pyseries.Preprocessing as prep
+import pyseries.Analysis as analysis
+
+
+
+
+channels = loading.Read_edf.Combine_EDF_XML('/Users/user/Desktop/Nagrania/rest/Rysiek_03_06/',True)
+
+n_samples_back = {"Eyes Open": 0, "Eyes Closed": 0}
+n_samples_forth = {"Eyes Open":  500* 140, "Eyes Closed":  500* 140}
+
+slices = prep.MakeSlices.Make_Slices_for_Channel(channels, ['EEG O1'],n_samples_back, n_samples_forth)
+
+prep.MakeSlices.mark_events(channels, ['EEG 01'])
+
+
+f, pxx = analysis.Explore.PlotPowerSpectrum(slices['EEG O1'])
+
+
+  
+
+
+      
+
+
---
+++ packages/pyseries/pyseries-1.0.18-py2.py3-none-any.whl/pyseries/Pipelines/AnalyzeSsvep.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  8 11:21:35 2016
+
+@author: user
+"""
+
+import sys
+sys.path.insert(0, '/Users/user/Desktop/repo_for_pyseries/pyseries/pyseries')
+
+import pyseries.LoadingData as loading
+import pyseries.Preprocessing as prep
+import pyseries.Analysis as analysis
+
+channels = loading.Read_edf.Combine_EDF_XML('/Users/user/Desktop/Nagrania/ssvep_20hz/Agnieszka_03_06/',True)
+
+n_samples_back = {"iti_screen": 0, "ssvep_ended": 500 * 10}
+n_samples_forth = {"iti_screen":  500* 10, "ssvep_ended": 0}
+
+
+slices = prep.MakeSlices.Make_Slices_for_Channel(channels, ['EEG O1'],n_samples_back, n_samples_forth)
+
+prep.MakeSlices.mark_events(channels, ['EEG 01'])
+
+
+f, pxx = analysis.Explore.PlotPowerSpectrum(slices['EEG O1'])
+
+
+  
+
