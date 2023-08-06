a1ce1819a7823e959c3603c9fe3d493d5203c3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:01 +0000

    Add package print_lolFunction/1.3.0/print_lolFunction-1.3.0.tar.gz

---
+++ packages/print_lolFunction/print_lolFunction-1.3.0.tar.gz/print_lolFunction-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'print_lolFunction',
+    version = '1.3.0',
+    py_modules = ['print_lolFunction'],
+    author = 'guxinkui',
+    author_email = 'guxinkui.126.com',
+    url          = 'http://www.baidu.com',
+    description = 'a function used to be show List item',
+)
