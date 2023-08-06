74aaaa0fbaa6a92d70dd70726b7b4cb09c32e9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:02 +0000

    Add package kkermylib/1.0/kkermylib-1.0.tar.gz

---
+++ packages/kkermylib/kkermylib-1.0.tar.gz/kkermylib-1.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='kkermylib',
+    version='1.0',
+    keywords = ('simple', 'test'),
+    description = 'just a simple test',
+    license = 'MIT License',
+
+    author = 'kker',
+    author_email = 'kkerneo@gmail.com',
+
+    packages = find_packages(),
+    platforms = 'any',
+)
