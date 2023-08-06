40dfcd6f8a11adbf5fceac14f32b9bf1fc59df93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:07 +0000

    Add package Flask-CMDB/0.1.3/Flask-CMDB-0.1.3.tar.gz

---
+++ packages/Flask-CMDB/Flask-CMDB-0.1.3.tar.gz/Flask-CMDB-0.1.3/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+except:
+    README = ''
+    CHANGES = ''
+
+
+setup(
+    name='Flask-CMDB',
+    version='0.1.3',
+    license='BSD',
+    url='https://github.com/lufeng4828/flask_cmdb',
+    author='Feng Lu',
+    author_email='lufeng044@qq.com',
+    maintainer='Feng Lu',
+    maintainer_email='lufeng044@qq.com',
+    description='A cmdb api client for Flask applications.',
+    long_description=README + '\n\n' + CHANGES,
+    zip_safe=False,
+    packages = find_packages(),
+    platforms='any',
+    include_package_data=True,
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
