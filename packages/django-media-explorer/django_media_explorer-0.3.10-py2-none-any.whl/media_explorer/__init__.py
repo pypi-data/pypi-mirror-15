f442929916d2d9904e0c80ee3717eed28b02d0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:42 +0000

    Add package django-media-explorer/0.3.10/django_media_explorer-0.3.10-py2-none-any.whl

---
+++ packages/django-media-explorer/django_media_explorer-0.3.10-py2-none-any.whl/media_explorer/__init__.py
@@ -0,0 +1,14 @@
+"""
+
+  /\/\   ___  __| (_) __ _    _____  ___ __ | | ___  _ __ ___ _ __ 
+ /    \ / _ \/ _` | |/ _` |  / _ \ \/ / '_ \| |/ _ \| '__/ _ \ '__|
+/ /\/\ \  __/ (_| | | (_| | |  __/>  <| |_) | | (_) | | |  __/ |   
+\/    \/\___|\__,_|_|\__,_|  \___/_/\_\ .__/|_|\___/|_|  \___|_|   
+                                        |
+"""
+
+__title__ = 'Django Media Explorer'
+__version__ = '0.3.10'
+__author__ = 'Oxfam America Web Team (webmaster@oxfamamerica.org)'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Oxfam America'
