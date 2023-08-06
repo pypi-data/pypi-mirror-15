29e795e522bc40ea092d5f1ed8c1ece0bc4dede7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:35 +0000

    Add package ethereum-client-utils/0.3.2/ethereum-client-utils-0.3.2.tar.gz

---
+++ packages/ethereum-client-utils/ethereum-client-utils-0.3.2.tar.gz/ethereum-client-utils-0.3.2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+readme = open(os.path.join(DIR, 'README.md')).read()
+
+
+setup(
+    name='ethereum-client-utils',
+    version='0.3.2',
+    description="""Ethereum Client Utils""",
+    long_description=readme,
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/pipermerriam/ethereum-client-utils',
+    include_package_data=True,
+    install_requires=[
+        "six==1.10.0",
+    ],
+    py_modules=['eth_client_utils'],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
