7b1f7a8085353155032708b75221255ff16e242e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:24 +0000

    Add package travis_wonderwall/0.3.0/travis_wonderwall-0.3.0.tar.gz

---
+++ packages/travis_wonderwall/travis_wonderwall-0.3.0.tar.gz/travis_wonderwall-0.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='travis_wonderwall',
+    version='0.3.0',
+    description='Because after all... you want to run a script on travis',
+    author='Glen Mailer',
+    author_email='glen@stainlessed.co.uk',
+    url='https://github.com/glenjamin/travis_wonderwall',
+    license='MIT',
+    scripts=['travis_wonderwall']
+)
