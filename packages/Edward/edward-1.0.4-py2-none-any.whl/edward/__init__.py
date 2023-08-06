3ed3c00ccfd461054cbc3fd92a999c883d18d2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:21 +0000

    Add package Edward/1.0.4/edward-1.0.4-py2-none-any.whl

---
+++ packages/Edward/edward-1.0.4-py2-none-any.whl/edward/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from . import models
+from . import stats
+from . import data
+from . import inferences
+from . import util
+
+# Direct imports for convenience
+from .models import *
+from .data import *
+from .inferences import *
+from .util import set_seed
