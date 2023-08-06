4e4ca9d2410c0fcca15c38ea722ec719bac8edf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:25 +0000

    Add package Gecko/1.0.5/Gecko-1.0.5.tar.gz

---
+++ packages/Gecko/Gecko-1.0.5.tar.gz/Gecko-1.0.5/mine/sensors.py
@@ -0,0 +1,21 @@
+
+from Gecko import GeckoIO
+
+class LightSensor(GeckoIO.Boards.ControlAnything.Pins.Analog):
+
+    # This is optional and typically used during devlopment.
+    # if this doesn't exist, 
+    cik = '98acd7f4dc8dd67cee40462ea623f99c8bc4adf1'
+
+
+    # Pin identifier on the board
+    pin_id = 0
+
+    # Report rate in seconds
+    # Must be a multiple of read_rate, else will be rounded up to read rate.
+    # default is 10 seconds
+    report_rate = 5
+
+    # description of sensor
+    description = "Monitors the light levels in the office"
+
