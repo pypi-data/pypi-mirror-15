64d7c094c9b2e8c15062e1f4f463df7b30456179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:24 +0000

    Add package measurement-stats/0.2.2/measurement_stats-0.2.2.tar.gz

---
+++ packages/measurement-stats/measurement_stats-0.2.2.tar.gz/measurement_stats-0.2.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+def read_me():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='measurement_stats',
+    version='0.2.2',
+    description=
+            'Measurement statistics with uncertainties and error propagation',
+    long_description=read_me(),
+    url='https://github.com/sernst/Measurement_Statistics',
+    author='Scott Ernst',
+    author_email='swernst@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'six'
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    keywords='measurements statistics error propagation',
+)
