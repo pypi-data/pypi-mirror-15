c90058694aa1c406eebfbdf0dc644f8240e1400d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:41 +0000

    Add package fischy/1.1.0/fischy-1.1.0.zip

---
+++ packages/fischy/fischy-1.1.0.zip/fischy-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'fischy',
+    version      = '1.1.0',
+    py_modules   = ['fischy'],
+    author       = 'fischy',
+    author_email = '563310255@qq.com',
+    url          = ' ',
+    description  = 'A sample printer of nested lists',
+    )
