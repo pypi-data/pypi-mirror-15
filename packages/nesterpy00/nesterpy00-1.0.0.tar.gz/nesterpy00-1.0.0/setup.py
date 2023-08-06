15194fa61e86473c5f21bb204b7e3a5ecef8be86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:26 +0000

    Add package nesterpy00/1.0.0/nesterpy00-1.0.0.tar.gz

---
+++ packages/nesterpy00/nesterpy00-1.0.0.tar.gz/nesterpy00-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'nesterpy00', 
+        version      = '1.0.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nester'],
+        author       = 'pyon',
+        author_email = '172771017@qq.com',
+        description  = 'A simple printer of nested lists',
+     )
+
