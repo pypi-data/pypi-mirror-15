8a5845a393357ab5a573a846736501bc4391f02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:20 +0000

    Add package capitals/0.0.4/capitals-0.0.4.tar.gz

---
+++ packages/capitals/capitals-0.0.4.tar.gz/capitals-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(name='capitals',
+      version='0.0.4',
+      url='http://git.km3net.de/jreubelt/capitals.git',
+      description='Capitals quiz',
+      author='Jonas Reubelt',
+      packages=['capitals'],
+      include_package_data=True,
+      platforms='any',
+      install_requires=[
+          'pandas',
+          'urwid',
+          'random',
+      ],
+      entry_points={
+          'console_scripts': [
+              'capitals=capitals.app:main',
+          ],
+      },
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+      ],
+)
+
+__author__ = 'Jonas Reubelt'
