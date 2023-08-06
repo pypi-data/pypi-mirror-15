a66ae6b473694823bfcd8d3de3612468051fa58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:03 +0000

    Add package chenfirst/1.0.0/chenfirst-1.0.0.tar.gz

---
+++ packages/chenfirst/chenfirst-1.0.0.tar.gz/chenfirst-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= "chenfirst",
+	version 	= "1.0.0",
+	py_modules 	= ["neser"],
+	author 		= "chenfei",
+	author_email 	= "389830809@qq.com",
+	url		= "http://www.qq.com",
+	description 	= "a simple printer of nested lists",
+	)
