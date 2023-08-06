02c8f9808147f16c18e43d77d6fb1e20e90e516a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:57 +0000

    Add package hy_nester/1.3.0/hy_nester-1.3.0.zip

---
+++ packages/hy_nester/hy_nester-1.3.0.zip/hy_nester-1.3.0/hy_nester.py
@@ -0,0 +1,16 @@
+"""This Is Module "nesster.py", It Provides A Function Called print_lol(),
+  which prints all elements out of a list whichever it is nested or not."""
+def print_lol(the_list,indent=False,level=0):
+  """This Function takes "the_list" as a parameter and prints all of its
+    elements out, and each elements occupies one row. The Second 
+    Parameter("level") is the number of tabs inserted when the a new 
+    sub-list is nested."""
+  for each_item in the_list:
+    if isinstance(each_item, list):
+      print_lol(each_item,indent,level+1)
+    else:
+      if indent:
+        #print("\t"*level,end='')
+        for tab_stop in range(level):
+          print("\t",end='')
+      print(each_item)
