ba9b30bc5cab0709c587c7734c2b83361f8d7452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:30 +0000

    Add package django-subscribe/0.1.5/django-subscribe-0.1.5.tar.gz

---
+++ packages/django-subscribe/django-subscribe-0.1.5.tar.gz/django-subscribe-0.1.5/subscribe/tests/test_settings.py
@@ -0,0 +1,70 @@
+"""Settings that need to be set in order to run the tests."""
+import os
+
+DEBUG = True
+SITE_ID = 1
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+USE_I18N = True
+
+ROOT_URLCONF = 'subscribe.tests.urls'
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), '../../static/')
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '../../media/')
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'test_static'),
+)
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'APP_DIRS': True,
+    'DIRS': [os.path.join(os.path.dirname(__file__), '../templates')],
+    'OPTIONS': {
+        'context_processors': (
+            'django.contrib.auth.context_processors.auth',
+            'django.core.context_processors.i18n',
+            'django.core.context_processors.request',
+            'django.core.context_processors.media',
+            'django.core.context_processors.static',
+        )
+    }
+}]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+EXTERNAL_APPS = [
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
+]
+
+INTERNAL_APPS = [
+    'subscribe.tests.test_app',
+    'subscribe',
+]
+
+INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS
+
+SECRET_KEY = 'foobar'
