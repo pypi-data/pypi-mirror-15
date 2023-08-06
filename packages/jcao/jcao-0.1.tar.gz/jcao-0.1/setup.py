e9430e334d164a4dd701a6dae0e9435c11e3407c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:46 +0000

    Add package jcao/0.1/jcao-0.1.tar.gz

---
+++ packages/jcao/jcao-0.1.tar.gz/jcao-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jcao',
+      version='0.1',
+      description='This is test only',
+      url='/home/jcao/repos/personal/study/pkg',
+      author='jcao',
+      author_email='zongbo_cao@163.com',
+      license='MIT',
+      packages=['pkg'],
+      zip_safe=False)
