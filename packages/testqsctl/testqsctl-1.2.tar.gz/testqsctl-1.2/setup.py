a47d07979f1dfda950700a4945c846b71b69e4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:47 +0000

    Add package testqsctl/1.2/testqsctl-1.2.tar.gz

---
+++ packages/testqsctl/testqsctl-1.2.tar.gz/testqsctl-1.2/setup.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name = 'testqsctl',
+    version = '1.2',
+    description = 'Advanced command line tool for QingStor.',
+    long_description = open('README.md', 'rb').read().decode('utf-8'),
+    keywords = 'qingcloud qingstor qsctl',
+    author = 'Daniel Zheng',
+    author_email = 'daniel@yunify.com',
+    url = 'https://docs.qingcloud.com',
+    scripts = ['bin/qsctl', 'bin/qsctl.cmd'],
+    packages = find_packages('.'),
+    package_dir = {'qsctl': 'qingstor',},
+    namespace_packages = ['qingstor',],
+    include_package_data = True,
+    install_requires = [
+        'argparse >= 1.1',
+        'PyYAML >= 3.1',
+        'qingcloud-sdk >= 1.0.2',
+    ]
+)
