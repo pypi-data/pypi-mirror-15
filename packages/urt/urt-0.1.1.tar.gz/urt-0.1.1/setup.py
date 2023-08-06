9de1a76f1b838692934c65c09b932623d3abb3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:55 +0000

    Add package urt/0.1.1/urt-0.1.1.tar.gz

---
+++ packages/urt/urt-0.1.1.tar.gz/urt-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='urt',
+    version='0.1.1',
+    description='Urban Terror Server Monitor',
+    url='http://github.com/masnun/urt',
+    author='Abu Ashraf Masnun',
+    author_email='masnun@gmail.com',
+    license='MIT',
+    packages=['urt'],
+    install_requires=[
+        'urwid',
+    ],
+    scripts=['urt/urt']
+)
