9beb6847bffa763c757bfbab30e0f15867e930cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:47 +0000

    Add package arghandler/1.2.0/arghandler-1.2.0.tar.gz

---
+++ packages/arghandler/arghandler-1.2.0.tar.gz/arghandler-1.2.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(	name='arghandler',
+		version='1.2.0',
+		description='argparse extended with awesome feature enhancements to make life easier',
+		author='Derek Ruths',
+		author_email='druths@networkdynamics.org',
+		url='http://www.github.com/druths/arghandler',
+		packages=['arghandler','arghandler.tests'],
+
+		install_requires = [ 'argcomplete' ],
+
+		license='Apache',
+
+		classifiers=[
+			'Development Status :: 4 - Beta',
+
+			'Intended Audience :: Developers',
+			'Topic :: Software Development',
+
+			'License :: OSI Approved :: Apache Software License',
+
+			'Programming Language :: Python :: 2.7',
+			'Programming Language :: Python :: 3',
+			'Programming Language :: Python :: 3.2',
+			'Programming Language :: Python :: 3.3',
+			'Programming Language :: Python :: 3.4'
+		],
+		keywords='argparse command-line parsing'
+	)
