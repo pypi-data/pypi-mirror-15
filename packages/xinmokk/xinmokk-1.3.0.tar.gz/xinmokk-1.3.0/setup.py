57b0681bc4a3b1c317c50e772d1081587c0f57f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:39 +0000

    Add package xinmokk/1.3.0/xinmokk-1.3.0.tar.gz

---
+++ packages/xinmokk/xinmokk-1.3.0.tar.gz/xinmokk-1.3.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+
+setup(
+	name='xinmokk',
+	version='1.3.0',
+	py_modules=['name'],
+	author='nan',
+	author_email='nianernan@163.com',
+	url='http://www.baidu.com',
+	description='A simple printer of name lists',
+)
