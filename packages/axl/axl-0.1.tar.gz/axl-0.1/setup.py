36e019f3a0292f732e57167d105671d374a761ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:49 +0000

    Add package axl/0.1/axl-0.1.tar.gz

---
+++ packages/axl/axl-0.1.tar.gz/axl-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='axl',
+      version='0.1',
+      description='Python package that generates web extensions for Firefox',
+      url='http://github.com/justinpotts/axl',
+      author='Justin Potts',
+      author_email='justin@garblemasher.com',
+      license='MPL',
+      packages=['axl'],
+      zip_safe=False)
