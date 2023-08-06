47847d32b5fd9134e6114ffe53f48c120900f9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:32 +0000

    Add package gaiabase/0.0.8/gaiabase-0.0.8.tar.gz

---
+++ packages/gaiabase/gaiabase-0.0.8.tar.gz/gaiabase-0.0.8/gaia/i18n.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from contextlib import contextmanager
+
+import os
+import os.path as path
+import codecs
+import yaml
+import logging
+
+import gaia.base as base
+
+logger = logging.getLogger('gaia')
+
+i18n_path = os.path.join(base.find_path(), 'i18n')
+
+tables = {}
+
+for root, dirs, files in os.walk(i18n_path, followlinks=True):
+    logger.info("i18n checking [%s, %s, %s] ..." % (root, str(dirs), str(files)))
+    if path.basename(root) == 'i18n':
+        for d in dirs:
+            tables[d] = {}
+    else:
+        lang = path.basename(root)
+        logger.info("initialing language %s ..." % lang)
+
+        for fnm in files:
+            fpth = os.path.join(root, fnm)
+            bndl = ''.join(fnm.split('.')[:-1])
+            logger.info("initialing bundle [%s:%s] ..." % (lang, bndl))
+            with codecs.open(fpth, "r", "utf-8") as f:
+                s = f.read()
+                try:
+                    tables[lang][bndl] = yaml.load(s)
+                    logger.info("init bundle [%s:%s] done ..." % (lang, bndl))
+                except Exception as e:
+                    tables[lang][bndl] = {}
+                    logger.error(e)
+
+
+def table(lang, tname):
+    if lang in tables and tname in tables[lang]:
+        return tables[lang][tname]
+    else:
+        logger.warn('[%s:%s] is not supported, using en_US by default!' % (lang, tname))
+        return tables['en_US'][tname]
+
+
+def get(lang, table, key):
+    if lang in tables and table in tables[lang] and key in tables[lang][table]:
+        return tables[lang][table][key]
+    else:
+        logger.warn('[%s:%s:%s] is not supported, using en_US by default!' % (lang, table, key))
+        return tables['en_US'][table][key]
+
+
+@contextmanager
+def ctx(lang, tname):
+    yield table(lang, tname)
