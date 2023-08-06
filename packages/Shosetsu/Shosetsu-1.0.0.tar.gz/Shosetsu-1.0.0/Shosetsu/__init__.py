eefafe2f850806652b832185973bafc8b96e2d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:06 +0000

    Add package Shosetsu/1.0.0/Shosetsu-1.0.0.tar.gz

---
+++ packages/Shosetsu/Shosetsu-1.0.0.tar.gz/Shosetsu-1.0.0/Shosetsu/__init__.py
@@ -0,0 +1,1 @@
+from VNDB import *
---
+++ packages/Shosetsu/Shosetsu-1.0.0.tar.gz/Shosetsu-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='Shosetsu',
+    version='1.0.0',
+    packages=['Shosetsu'],
+    url='https://github.com/ccubed/Shosetsu',
+    license='MIT',
+    author='Cooper Click',
+    author_email='ccubed.techno@gmail.com',
+    description='Python 3 Asyncio VNDB Bindings',
+    long_description='Shosetsu is a Python 3 Asyncio VNDB Website scraper that foregos the awful TCP based SQL database wrapper called an API.',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries'
+    ],
+    keywords="VNDB asyncio aiohttp scraping",
+    install_requires=['lxml', 'aiohttp', 'bs4'],
+
+)
