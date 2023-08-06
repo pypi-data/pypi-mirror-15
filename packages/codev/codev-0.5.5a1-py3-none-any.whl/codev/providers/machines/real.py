552c6db657a0bc4aaa3e7cf8e216f6a0016372c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:37 +0000

    Add package codev/0.5.5a1/codev-0.5.5a1-py3-none-any.whl

---
+++ packages/codev/codev-0.5.5a1-py3-none-any.whl/codev/providers/machines/real.py
@@ -0,0 +1,46 @@
+from codev.settings import BaseSettings
+from codev.machines import MachinesProvider, BaseMachine
+
+
+class RealMachine(BaseMachine):
+    def exists(self):
+        return True
+
+    def is_started(self):
+        return True
+
+    def start(self):
+        pass
+
+    def create(self, distribution, release, install_ssh_server=False, ssh_key=None):
+        pass
+
+    def destroy(self):
+        pass
+
+    def stop(self):
+        pass
+
+    @property
+    def ip(self):
+        # TODO rename to host
+        # TODO this is workaround
+        return self.ident
+
+
+class RealMachinesSettings(BaseSettings):
+    @property
+    def hosts(self):
+        return self.data.get('hosts')
+
+
+class RealMachinesProvider(MachinesProvider):
+    provider_name = 'real'
+    settings_class = RealMachinesSettings
+    machine_class = RealMachine
+
+    def idents(self):
+        # TODO this is workaround
+        for host in self.settings.hosts:
+            yield host
+
