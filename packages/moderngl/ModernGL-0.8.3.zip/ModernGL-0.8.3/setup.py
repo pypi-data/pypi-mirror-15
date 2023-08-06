e18d280d524b2709a75f6001b77e63d08d432195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:46 +0000

    Add package moderngl/0.8.3/ModernGL-0.8.3.zip

---
+++ packages/moderngl/ModernGL-0.8.3.zip/ModernGL-0.8.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup, Extension
+
+PyModernGL = Extension(
+	'PyModernGL',
+	# include_dirs = ['/usr/local/include'],
+	libraries = ['User32'],
+	# library_dirs = ['/usr/local/lib'],
+	sources = [
+		'Source/WindowsGL.cpp',
+		'Source/OpenGL.cpp',
+		'Source/ModernGL.cpp',
+		'Source/Python-ModernGL.cpp',
+	]
+)
+
+setup(
+	name = 'ModernGL',
+	version = '0.8.3',
+	description = 'ModernGL',
+	url = 'https://github.com/cprogrammer1994/Python-ModernGL',
+	author = 'Szabolcs Dombi',
+	author_email = 'cprogrammer1994@gmail.com',
+	license = 'APACHE',
+	packages = [],
+	ext_modules = [PyModernGL]
+)
