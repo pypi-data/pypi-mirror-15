5a4924dafbad28008017fa531008d28b37c7b1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:02 +0000

    Add package gdrive-manager/0.1.2/gdrive-manager-0.1.2.zip

---
+++ packages/gdrive-manager/gdrive-manager-0.1.2.zip/gdrive-manager-0.1.2/setup.py
@@ -0,0 +1,26 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+
+REQUIRES = [
+	"google-api-python-client==1.5.1",
+	"oauth2client==2.2.0"
+]
+
+
+setup (
+	name='gdrive-manager',    				# This is the name of your PyPI-package.
+	version='0.1.2',                        # Update the version number for new releases
+	scripts=['gdrive-manager'],             # The name of your scipt, and also the command you'll be using for calling it
+    packages=[
+        'gdrive-manager',
+    ],
+	author='Bao Nguyen',
+	author_email='nlbao95@gmail.com',
+	description='Download / upload data using Google Drive APIs.',
+	url='https://github.com/nlbao/gdrive-manager',
+	license='MIT',
+	install_requires=REQUIRES,
+)
