230b2b7161b4a35a28dd3d3e265c38682f9a709b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:00 +0000

    Add package Beer.py/0.2.0/Beer.py-0.2.0.zip

---
+++ packages/Beer.py/Beer.py-0.2.0.zip/Beer.py-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Beer.py',
+    version='0.2.0',
+    packages=['beer'],
+    url='https://github.com/rauhkrusche/Beer.py',
+    license='MIT License',
+    author='Benjamin Leuckefeld',
+    author_email='code@deratrox.de',
+    description='A Beer implementation in Python.',
+    keywords='beer.py beer python library'
+)
