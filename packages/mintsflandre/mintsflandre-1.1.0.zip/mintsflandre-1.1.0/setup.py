df4ec3307555f2a363cee9b979670b9d4238997f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:33 +0000

    Add package mintsflandre/1.1.0/mintsflandre-1.1.0.zip

---
+++ packages/mintsflandre/mintsflandre-1.1.0.zip/mintsflandre-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'mintsflandre',
+    version         = '1.1.0',
+    py_modules      = ["mintsflandre_personal"],
+    author          = 'Mints',
+    author_email    = 'mintsflandre@gmail.com',
+    url             = 'http://www.google.com',
+    description     = 'A personal module',
+    )
