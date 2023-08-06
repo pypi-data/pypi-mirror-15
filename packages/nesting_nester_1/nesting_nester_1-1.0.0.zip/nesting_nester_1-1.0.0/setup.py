d30f77be9e702a1a72ec9c464d77e258657bdced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:20 +0000

    Add package nesting_nester_1/1.0.0/nesting_nester_1-1.0.0.zip

---
+++ packages/nesting_nester_1/nesting_nester_1-1.0.0.zip/nesting_nester_1-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+        name         = 'nesting_nester_1',
+        version      = '1.0.0',
+        py_modules   = ['nesting_nester_1'],
+        author       = 'megha_singh',
+        author_email = 'meghasingh.ms@gmail.com',
+        url          = 'http://www.futurestar.com',
+        description  = 'a simple printer of nested lists',
+     )
