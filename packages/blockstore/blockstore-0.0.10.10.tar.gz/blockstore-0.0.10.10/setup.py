8e6edfd9c042bf35b1ffa0f32ee68264e1e5c552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:37 +0000

    Add package blockstore/0.0.10.10/blockstore-0.0.10.10.tar.gz

---
+++ packages/blockstore/blockstore-0.0.10.10.tar.gz/blockstore-0.0.10.10/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Blockstore
+    ~~~~~
+    copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
+    copyright: (c) 2016 by Blockstack.org
+
+    This file is part of Blockstore
+
+    Blockstore is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Blockstore is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Blockstore. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='blockstore',
+    version='0.0.10.10',
+    url='https://github.com/blockstack/blockstore',
+    license='GPLv3',
+    author='Blockstack.org',
+    author_email='support@blockstack.org',
+    description='Name registrations on the Bitcoin blockchain with external storage',
+    keywords='blockchain bitcoin btc cryptocurrency name key value store data',
+    packages=find_packages(),
+    scripts=['bin/blockstored', 'bin/blockstack-server'],
+    download_url='https://github.com/blockstack/blockstore/archive/master.zip',
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'virtualchain==0.0.6',
+        'kademlia==0.5',
+        'keychain==0.1.4',
+        'blockstore-client==0.0.12.10'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
