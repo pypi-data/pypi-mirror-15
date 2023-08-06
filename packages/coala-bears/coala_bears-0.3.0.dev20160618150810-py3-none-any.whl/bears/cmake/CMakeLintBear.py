18fa7fb791833248973ab92556e6651e88e0baa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:22 +0000

    Add package coala-bears/0.3.0.dev20160618150810/coala_bears-0.3.0.dev20160618150810-py3-none-any.whl

---
+++ packages/coala-bears/coala_bears-0.3.0.dev20160618150810-py3-none-any.whl/bears/cmake/CMakeLintBear.py
@@ -0,0 +1,28 @@
+from coalib.bearlib.abstractions.Linter import linter
+from coalib.bears.requirements.PythonRequirement import PythonRequirement
+from coalib.settings.Setting import path
+
+
+@linter(executable='cmakelint',
+        output_format='regex',
+        output_regex=r'.+:(?P<line>\d+): (?P<message>.*)')
+class CMakeLintBear:
+    """
+    Checks the code with ``cmakelint``.
+    """
+    LANGUAGES = {'CMake'}
+    REQUIREMENTS = PythonRequirement('cmakelint', '1.*')
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+
+    @staticmethod
+    def create_arguments(filename, file, config_file,
+                         cmakelint_config: path=""):
+        """
+        :param cmakelint_config: The location of the cmakelintrc config file.
+        """
+        args = ()
+        if cmakelint_config:
+            args += ('--config=' + cmakelint_config,)
+        return args + (filename,)
