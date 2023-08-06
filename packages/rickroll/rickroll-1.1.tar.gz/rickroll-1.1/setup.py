3a4e6dff72b9b1361c5bb50456db7000a06187ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:33 +0000

    Add package rickroll/1.1/rickroll-1.1.tar.gz

---
+++ packages/rickroll/rickroll-1.1.tar.gz/rickroll-1.1/setup.py
@@ -0,0 +1,33 @@
+import sys
+import webbrowser
+
+from distutils.core import setup
+
+
+if sys.argv[1] == 'install':
+    webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+
+setup(name='rickroll',
+      version='1.1',
+      description="The Python module that's never gonna give you up.",
+      long_description="The Python module that's never gonna let you down",
+      author='James Bennett',
+      author_email='james@b-list.org',
+      url='https://github.com/ubernostrum/rickroll',
+      py_modules=['rickroll'],
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Topic :: Internet',
+                   'Topic :: Sociology'],
+      )
