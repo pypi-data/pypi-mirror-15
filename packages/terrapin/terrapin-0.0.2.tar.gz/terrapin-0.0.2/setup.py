f7f20ef7f70adb1bf9ffe403f67ec2c0aece77ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:51 +0000

    Add package terrapin/0.0.2/terrapin-0.0.2.tar.gz

---
+++ packages/terrapin/terrapin-0.0.2.tar.gz/terrapin-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='terrapin',
+      version="0.0.2",
+      packages=find_packages(),
+      description='A very lightweight template language',
+      url='https://github.com/DistilledLtd/terrapin',
+      author='Distilled Ltd',
+      author_email='randd@distilled.net',
+      )
