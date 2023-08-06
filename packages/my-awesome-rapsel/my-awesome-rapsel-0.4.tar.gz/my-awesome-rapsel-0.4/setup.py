5a4fd3972782ce919254a734e23379818647928e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:41 +0000

    Add package my-awesome-rapsel/0.4/my-awesome-rapsel-0.4.tar.gz

---
+++ packages/my-awesome-rapsel/my-awesome-rapsel-0.4.tar.gz/my-awesome-rapsel-0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='my-awesome-rapsel',  
+    version='0.4',
+    scripts=['test_rap'],
+    author='Syam Sasi',
+    author_email='syamsasi99@gmail.com'
+)
