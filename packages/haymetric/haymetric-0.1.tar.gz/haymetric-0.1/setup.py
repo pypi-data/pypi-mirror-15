cd357e6cc1f6cfea1ea7254c2cbf11f891c5983d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:29 +0000

    Add package haymetric/0.1/haymetric-0.1.tar.gz

---
+++ packages/haymetric/haymetric-0.1.tar.gz/haymetric-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'haymetric',
+  packages = ['haymetric'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Haymetric SDK for Python',
+  author = 'Linh Hong Dang <linh.danghong@gmail.com>, Nguyen Quang Trung <trungnq97@gmai.com>',
+  author_email = 'haymetric@gmail.com',
+  url = 'https://github.com/haymetric/haymetric-sdk-python', # use the URL to the github repo
+  download_url = 'https://github.com/haymetric/haymetric-sdk-python/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'haymetric', 'sdk'], # arbitrary keywords
+  classifiers = [],
+)
