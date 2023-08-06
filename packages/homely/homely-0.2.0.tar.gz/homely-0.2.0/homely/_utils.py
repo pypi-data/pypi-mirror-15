3ffdcc5e9189f8f618bedecffdb1de4fcac8a64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:15 +0000

    Add package homely/0.2.0/homely-0.2.0.tar.gz

---
+++ packages/homely/homely-0.2.0.tar.gz/homely-0.2.0/homely/_utils.py
@@ -0,0 +1,224 @@
+import contextlib
+import os
+import subprocess
+
+import simplejson
+
+from homely._errors import JsonError, RepoError
+
+CONFIG_DIR = os.path.join(os.environ.get('HOME'), '.homely')
+REPO_CONFIG_PATH = os.path.join(CONFIG_DIR, 'repos.json')
+
+
+class JsonConfig(object):
+    jsonpath = None
+    jsondata = None
+
+    def __init__(self):
+        try:
+            with open(self.jsonpath, 'r') as f:
+                data = f.read()
+                if not len(data):
+                    raise FileNotFoundError(None)
+                self.jsondata = simplejson.loads(data)
+                self.checkjson()
+        except FileNotFoundError as err:
+            self.jsondata = self.defaultjson()
+        except simplejson.JSONDecodeError:
+            raise JsonError("%s does not contain valid JSON" % self.jsonpath)
+
+    def checkjson(self):
+        """
+        Child classes should override this method to check if self.jsondata is
+        sane.
+        """
+        raise Exception("This method needs to be overridden")
+
+    def defaultjson(self):
+        """
+        Child classes should override this method to return the default json
+        object for when the config file doesn't exist yet.
+        """
+        raise Exception("This method needs to be overridden")
+
+    def writejson(self):
+        # make dirs needed for config file
+        os.makedirs(os.path.dirname(self.jsonpath), mode=0o755, exist_ok=True)
+        # write the config file now
+        with open(self.jsonpath, 'w') as f:
+            f.write(simplejson.dumps(self.jsondata, indent=' ' * 4))
+
+
+class RepoListConfig(JsonConfig):
+    jsonpath = REPO_CONFIG_PATH
+    jsondata = None
+
+    def defaultjson(self):
+        return []
+
+    def checkjson(self):
+        assert isinstance(self.jsondata, list)
+        for entry in self.jsondata:
+            assert 'commithash' in entry
+            assert 'localpath' in entry
+
+    def add_repo(self, info):
+        assert isinstance(info, RepoInfo)
+        commithash = info.commithash
+        modified = False
+        for repo in self.jsondata:
+            if repo["commithash"] == commithash:
+                # change the local path in the config
+                repo["localpath"] = info.localpath
+                modified = True
+                break
+        if not modified:
+            self.jsondata.append({"commithash": info.commithash,
+                                  "localpath": info.localpath})
+
+    def find_repo(self, hash_or_path):
+        for repo in self.jsondata:
+            if hash_or_path in (repo["commithash"], repo["localpath"]):
+                return RepoInfo(repo["localpath"], repo["commithash"])
+
+    def find_all(self):
+        for repo in self.jsondata:
+            yield RepoInfo(repo["localpath"], repo["commithash"])
+
+
+class RepoScriptConfig(JsonConfig):
+    jsondata = None
+
+    def __init__(self, info):
+        self.jsonpath = os.path.join(CONFIG_DIR,
+                                     'repos',
+                                     info.commithash + '.json')
+        super(RepoScriptConfig, self).__init__()
+
+    def defaultjson(self):
+        return {
+            # a list of things that were installed on the last run
+            "prevthings": [],
+            "prevchanges": {},
+            "questions": {},
+        }
+
+    def checkjson(self):
+        pass
+
+    def getthings(self):
+        import homely.general
+        import homely.install
+        modules = [homely.general, homely.install]
+        for thing in self.jsondata['prevthings']:
+            class_ = thing["class"]
+            identifiers = thing["identifiers"]
+            for module in modules:
+                if hasattr(module, class_):
+                    yield getattr(module, class_).fromidentifiers(identifiers)
+                    break
+            else:
+                raise Exception("No modules own %s" % class_)
+
+    def clearthings(self):
+        self.jsondata["prevthings"] = []
+        return self.jsondata["prevchanges"]
+
+    @staticmethod
+    def _asdict(thing):
+        return {"class": thing.__class__.__name__,
+                "identifiers": thing.identifiers}
+
+    def addthing(self, thing):
+        self.jsondata["prevthings"].append(self._asdict(thing))
+        self.jsondata["prevchanges"].setdefault(thing.uniqueid, {})
+
+    def removething(self, thing):
+        thingdict = self._asdict(thing)
+        prevthings = [t for t in self.jsondata["prevthings"] if t != thingdict]
+        assert (len(self.jsondata["prevthings"]) - len(prevthings)) == 1
+        del self.jsondata["prevchanges"][thing.uniqueid]
+
+    def setchanges(self, uniqueid, changes):
+        self.jsondata["prevchanges"][uniqueid] = changes
+
+    def getprevchanges(self, uniqueid):
+        return self.jsondata["prevchanges"].get(uniqueid, {})
+
+    def getquestionanswer(self, name):
+        return self.jsondata["questions"].get(name, None)
+
+    def setquestionanswer(self, name, value):
+        self.jsondata["questions"][name] = value
+
+
+@contextlib.contextmanager
+def saveconfig(cfg):
+    assert isinstance(cfg, JsonConfig)
+    yield cfg
+    cfg.writejson()
+
+
+class RepoInfo(object):
+    localpath = None
+    commithash = None
+
+    def __init__(self, path, commithash=None):
+        # make sure the path is valid
+        if not os.path.exists(path):
+            raise RepoError("%s does not exist" % path)
+        if not os.path.exists(os.path.join(path, '.git')):
+            raise RepoError("%s is not a valid git repo" % path)
+        self.localpath = path
+        if commithash is None:
+            # ask git for the commit hash
+            cmd = ['git', 'rev-list', '--max-parents=0', 'HEAD']
+            self.commithash = (subprocess.check_output(cmd, cwd=path)
+                               .rstrip()
+                               .decode('utf-8'))
+        else:
+            self.commithash = commithash
+
+    @property
+    def shorthash(self):
+        return self.commithash[0:8]
+
+
+@contextlib.contextmanager
+def filereplacer(filepath):
+    """
+    This context manager yields two file pointers:
+
+    orig:
+        a file descriptor as if you had used open(filepath)
+    tmp:
+        a file descriptor as if you had used open(tempfile.mkstemp(), 'w')
+
+    Note that upon successful exiting of the context manager, the file
+    nominated by filepath will be replaced by the temp file. This will be done
+    using file renames, so it is as close to atomic as we can get it.
+
+    If the context block raises an exception, the original file is not changed,
+    and the temp file is deleted.
+    """
+    import shutil
+    # create the tmp dir if it doesn't exist yet
+    tmpdir = os.path.join(CONFIG_DIR, 'tmp')
+    os.makedirs(tmpdir, mode=0o700, exist_ok=True)
+    tmpname = os.path.join(tmpdir, os.path.basename(filepath))
+    try:
+        if os.path.exists(filepath):
+            shutil.copy2(filepath, tmpname)
+        with open(tmpname, 'w') as tmp:
+            try:
+                with open(filepath, 'r') as orig:
+                    yield tmp, orig
+            except FileNotFoundError:
+                yield tmp, None
+    except:
+        with contextlib.suppress(FileNotFoundError):
+            os.unlink(tmpname)
+        raise
+    with contextlib.suppress(FileNotFoundError):
+        os.unlink(filepath)
+    os.rename(tmpname, filepath)
---
+++ packages/homely/homely-0.2.0.tar.gz/homely-0.2.0/homely/general.py
@@ -0,0 +1,222 @@
+import os
+import re
+import contextlib
+
+from homely._errors import HelperError
+from homely._engine import getengine
+from homely._utils import filereplacer
+from homely._ui import warning
+
+
+def add(updatehelper):
+    getengine().add(updatehelper)
+
+
+@contextlib.contextmanager
+def section(name):
+    if re.match('[^A-Za-z0-9_:\-\[\]<>]', name):
+        raise HelperError("%r is not a valid section name" % name)
+
+    getengine().pushsection(name)
+    yield
+    getengine().popsection(name)
+
+
+def mkdir(path):
+    path = os.path.expanduser(path)
+    getengine().add(MakeDir(path=path))
+
+
+def lineinfile(filename, contents, prefix=None, regex=None):
+    filename = os.path.expanduser(filename)
+    obj = LineInFile(filename=filename, contents=contents)
+    if prefix is not None:
+        obj.findprefix(prefix)
+    elif regex is not None:
+        obj.findregex(regex)
+    getengine().add(obj)
+
+
+class UpdateHelper(object):
+    _section = None
+
+    @property
+    def identifiers(self):
+        raise NotImplementedError(
+            "%s needs to implement @property identifiers()" %
+            self.__class__.__name__)
+
+    @classmethod
+    def fromidentifiers(class_, identifiers):
+        prototype = "@classmethod fromidentifiers(class_, identifiers)"
+        raise NotImplementedError("%s needs to implement %s" %
+                                  (class_.__name__, prototype))
+
+    @property
+    def uniqueid(self):
+        identifiers = self.identifiers
+        items = [self.__class__.__name__]
+        for key in sorted(identifiers):
+            items.extend([key, identifiers[key]])
+        return repr(items)
+
+    def setsection(self, name):
+        self._section = name
+
+    def getsection(self):
+        return self._section
+
+    def iscleanable(self):
+        raise NotImplementedError("%s needs to implement iscleanable()" %
+                                  (self.__class__.__name__))
+
+    def isdone(self):
+        raise NotImplementedError("%s needs to implement isdone()" %
+                                  (self.__class__.__name__))
+
+    def makechanges(self, prevchanges):
+        prototype = "makechanges(self, prevchanges)"
+        raise NotImplementedError("%s needs to implement %s" %
+                                  (self.__class__.__name__, prototype))
+
+    def undochanges(self, prevchanges):
+        prototype = "undochanges(self, prevchanges)"
+        raise NotImplementedError("%s needs to implement %s" %
+                                  (self.__class__.__name__, prototype))
+
+
+class MakeDir(UpdateHelper):
+    _path = None
+
+    def __init__(self, path):
+        self._path = path
+
+    @property
+    def identifiers(self):
+        return dict(path=self._path)
+
+    @classmethod
+    def fromidentifiers(class_, identifiers):
+        return class_(identifiers["path"])
+
+    def iscleanable(self):
+        return os.path.isdir(self._path)
+
+    def isdone(self):
+        return os.path.isdir(self._path)
+
+    def descchanges(self):
+        return "Creating directory %s" % self._path
+
+    def makechanges(self, prevchanges):
+        if os.path.islink(self._path):
+            raise HelperError("%s is already a symlink" % self._path)
+
+        changes = {
+            "dirs_created": prevchanges.get("dirs_created", []),
+        }
+
+        check = []
+        path = self._path
+        prev = None
+        while path != prev:
+            prev = path
+            check.insert(0, path)
+            path = os.path.dirname(path)
+        for path in check:
+            if os.path.isdir(path):
+                continue
+            if os.path.exists(self._path):
+                raise HelperError("%s already exists" % self._path)
+            changes["dirs_created"].append(path)
+            os.makedirs(path)
+
+        return changes
+
+    def undochanges(self, prevchanges):
+        if self._path in prevchanges["dirs_created"]:
+            # ensure the folder is empty before deleting it
+            if len(os.listdir(self._path)):
+                warning("Refusing to clean up non-empty folder %s" %
+                        self._path)
+            else:
+                os.rmdir(self._path)
+
+
+class LineInFile(UpdateHelper):
+    _filename = None
+    _contents = None
+    _findprefix = None
+    _findregex = None
+
+    def __init__(self, filename, contents):
+        super(LineInFile, self).__init__()
+        self._filename = filename
+        self._contents = contents
+
+    def findprefix(self, prefix):
+        self._findprefix = prefix
+
+    def findregex(self, regex):
+        self._findregex = regex
+
+    @property
+    def identifiers(self):
+        return dict(filename=self._filename,
+                    contents=self._contents)
+
+    @classmethod
+    def fromidentifiers(class_, identifiers):
+        return class_(identifiers["filename"], identifiers["contents"])
+
+    def isdone(self):
+        try:
+            with open(self._filename) as f:
+                for line in f.readlines():
+                    if line.rstrip() == self._contents:
+                        return True
+        except FileNotFoundError:
+            pass
+        return False
+
+    def descchanges(self):
+        return "Adding line to %s: %s" % (self._filename, self._contents)
+
+    def makechanges(self, prevchanges):
+        changes = {
+            "old_line": None,
+        }
+
+        if self._findprefix:
+            def matchline(line):
+                return line.startswith(self._findprefix)
+        elif self._findregex:
+            # FIXME: implement regex matching
+            raise Exception("FIXME: implement regex")  # noqa
+        else:
+            def matchline(line):
+                return line.rstrip() == self._contents
+
+        with filereplacer(self._filename) as (tmp, orig):
+            modified = False
+            if orig is not None:
+                # read through the original file and look for a line to replace
+                for line in orig.readlines():
+                    if not modified and matchline(line):
+                        modified = True
+                        tmp.write(self._contents)
+                        # FIXME: respect the existing lines' line endings!
+                        tmp.write("\n")
+                        if "old_line" not in changes:
+                            changes["old_line"] = line.rstrip()
+                    else:
+                        tmp.write(line)
+            # if we didn't write out the new line by replacing parts of the
+            # original, then we'll just have to pop the new line on the end
+            if not modified:
+                tmp.write(self._contents)
+                # FIXME: respect the existing lines' line endings!
+                tmp.write("\n")
+                changes["old_line"] = None
+
+        return changes
