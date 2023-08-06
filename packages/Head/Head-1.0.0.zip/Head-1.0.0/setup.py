aeb8a4ef60a4cd17d95ac94a0b17d35309df9a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:21 +0000

    Add package Head/1.0.0/Head-1.0.0.zip

---
+++ packages/Head/Head-1.0.0.zip/Head-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name         = 'Head',
+	   version      = '1.0.0',
+	   py_modules   = ['Head'],
+	   author       = 'jinxy0501',
+	   author_email = '13520183449@163.com',
+	   url          = 'http://www.headfirstlabs.xom',
+	   description  = 'A simple printer of Head lists',
+	   )
