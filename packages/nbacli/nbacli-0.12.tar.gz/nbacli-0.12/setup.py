13501290bd846dffd2804d2a7cfa399d4886eb9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:32 +0000

    Add package nbacli/0.12/nbacli-0.12.tar.gz

---
+++ packages/nbacli/nbacli-0.12.tar.gz/nbacli-0.12/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import os
+
+setup (
+	name = "nbacli",
+	version = "0.12",
+	author = "chenminhua",
+	author_email = "chenmh@shanghaitech.edu.cn",
+	license = "GPL3",
+	description = "watch nba in your cli, of course, no images and videos",
+	url = "http://github.com/chenminhua/nba",
+	packages = [
+		'nba'
+	],
+	scripts = ['bin/nba'],
+	install_requires = [
+		'beautifulsoup4 >= 4.4'
+	]
+)
