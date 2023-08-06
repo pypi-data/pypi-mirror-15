0dbdef615de5c81115c4d1cee7d290612c2810d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:46 +0000

    Add package detective_nester/1.3.0/detective_nester-1.3.0.zip

---
+++ packages/detective_nester/detective_nester-1.3.0.zip/detective_nester-1.3.0/nester.py
@@ -0,0 +1,10 @@
+﻿"""这一模块的作用是打印列表，其中可能包含嵌套列表"""
+def print_lol(the_list,indent=false,level=0):
+    """这一函数接受参数，可以是任何python列表，函数会将列表的每个数据项递归输出到屏幕，每个数据项占一行"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                print("\t"*level,end='')
+            print(each_item)
