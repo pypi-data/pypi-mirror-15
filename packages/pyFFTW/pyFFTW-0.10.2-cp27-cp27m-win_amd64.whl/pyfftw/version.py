2f6f309b85e27c65e348729567dd89679d063383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:51 +0000

    Add package pyFFTW/0.10.2/pyFFTW-0.10.2-cp27-cp27m-win_amd64.whl

---
+++ packages/pyFFTW/pyFFTW-0.10.2-cp27-cp27m-win_amd64.whl/pyfftw/version.py
@@ -0,0 +1,13 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.10.2'
+version = '0.10.2'
+full_version = '0.10.2'
+git_revision = '777529100d62447631f517f24c6b193e58520338'
+release = True
+if not release:
+    version = full_version
+
+if __name__ == "__main__":
+    print(short_version)
+    print(version)
