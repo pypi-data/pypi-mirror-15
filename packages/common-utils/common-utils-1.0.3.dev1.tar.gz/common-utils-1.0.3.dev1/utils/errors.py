394bf86a789b7e0eb30d185c7e369a2dcae9eecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:44 +0000

    Add package common-utils/1.0.3.dev1/common-utils-1.0.3.dev1.tar.gz

---
+++ packages/common-utils/common-utils-1.0.3.dev1.tar.gz/common-utils-1.0.3.dev1/utils/errors.py
@@ -0,0 +1,12 @@
+"Core exceptions raised by the package of common-utils."
+#! /usr/bin/env python
+# coding=UTF-8
+
+class CommonUtilsError(Exception):
+    pass
+
+class SystemCommandExecuteError(CommonUtilsError):
+    pass
+
+class UnSupportedMethodError(CommonUtilsError):
+    pass
