c56d9d59a9a5ff047d08f9b47972f1ddef38ae80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:42 +0000

    Add package hatcher/0.10.4/hatcher-0.10.4-py2.py3-none-any.whl

---
+++ packages/hatcher/hatcher-0.10.4-py2.py3-none-any.whl/hatcher/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM SETUP.PY
+version = '0.10.4'
+full_version = '0.10.4'
+git_revision = '3ef7a2e46f4bf4b74b9cfc287475930a5624d3c9'
+is_released = True
+
+if not is_released:
+    version = full_version
