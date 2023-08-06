be4c9dca9b432a50bab6197c1a7e813c3b575e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:01 +0000

    Add package jstyleson/0.0.2/jstyleson-0.0.2.tar.gz

---
+++ packages/jstyleson/jstyleson-0.0.2.tar.gz/jstyleson-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='jstyleson',
+    py_modules=['jstyleson'],
+    version='0.0.2',
+    description='Library to parse JSON with js-style comments.',
+    author='linjackson',
+    author_email='linjackson78@gmail.com',
+    url='https://github.com/linjackson78/jstyleson',
+    keywords=['json', 'comment', 'javascript', 'parse'],
+    classifiers=['Development Status :: 3 - Alpha', 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 2.7', 'Programming Language :: Python :: 3.5'],
+    license='MIT',
+    test_suite="tests",
+)
