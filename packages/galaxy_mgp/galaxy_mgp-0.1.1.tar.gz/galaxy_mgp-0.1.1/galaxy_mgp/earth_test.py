56d430b0afaf53d380c6e6b2352dde2b769fe354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:59 +0000

    Add package galaxy_mgp/0.1.1/galaxy_mgp-0.1.1.tar.gz

---
+++ packages/galaxy_mgp/galaxy_mgp-0.1.1.tar.gz/galaxy_mgp-0.1.1/galaxy_mgp/earth_test.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+def say_distance(ffrom=None, tto=None):
+    print "ffrom =%s and tto = %s" %(ffrom, tto)
+    if ((ffrom is None) or (tto is None)):
+        ffrom = "Sun"
+        tto = "Self"
+        print "the distance from %s and %s is %f" % (ffrom, tto, 0)
+    else:
+        print "the distance from %s and %s is %f" % (ffrom, tto ,987889.090)
+
+
+def checkDistance():
+    ffrom = "Sun"
+    tto = "Earth"
+    say_distance(ffrom, tto)
+
+if __name__ == __main__:
+    checkDistance()
