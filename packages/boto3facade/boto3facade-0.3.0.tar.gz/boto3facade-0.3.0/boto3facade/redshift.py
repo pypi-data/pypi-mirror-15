94f76ba4f6b2699c884bc3b0fa4c7b4a95de4856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:12 +0000

    Add package boto3facade/0.3.0/boto3facade-0.3.0.tar.gz

---
+++ packages/boto3facade/boto3facade-0.3.0.tar.gz/boto3facade-0.3.0/boto3facade/redshift.py
@@ -0,0 +1,39 @@
+"""Redshift facade."""
+
+
+from . import ec2
+from .aws import AwsFacade
+from .exceptions import CredentialsError
+
+
+class Redshift(AwsFacade):
+    @property
+    def service(self):
+        return 'redshift'
+
+    def get_cluster_by_tag(self, tags, **kwargs):
+        """Gets a list of clusters that match the provided set of tags"""
+        return self.get_resource_by_tag('Cluster', tags, **kwargs)
+
+    def get_subnet_group_by_name(self, name, **kwargs):
+        """Gets the Redshift subnet group with the given name"""
+        raise NotImplementedError()
+
+    def get_copy_credentials(self):
+        """Produces the credentials parameter for the copy command"""
+        # Attempt to get temporary (role) creds
+        creds = ec2.get_temporary_credentials()
+        if creds is not None:
+            return ("aws_access_key_id={};"
+                    "aws_secret_access_key={};"
+                    "token={}").format(creds.key_id,
+                                       creds.secret_key,
+                                       creds.token)
+        # Attempt to get AWS CLI credentials
+        creds = self.get_credentials()
+        if creds is None:
+            msg = "Unable to retrieve AWS credentials"
+            raise CredentialsError(msg, logger=self.config.logger)
+        else:
+            return "aws_access_key_id={};aws_secret_access_key={}".format(
+                creds.key_id, creds.secret_key)
