07fde73b54ade8a2e79319b0b605ec6143c52635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:11 +0000

    Add package pypolyagamma-3/0.1.7/pypolyagamma-3-0.1.7.tar.gz

---
+++ packages/pypolyagamma-3/pypolyagamma-3-0.1.7.tar.gz/pypolyagamma-3-0.1.7/setup.py
@@ -0,0 +1,49 @@
+import os
+
+from setuptools import setup
+from setuptools.extension import Extension
+
+try:
+    import numpy as np
+except ImportError:
+    print("Please install numpy.")
+
+# Dealing with Cython
+USE_CYTHON = os.environ.get('USE_CYTHON', False)
+ext = '.pyx' if USE_CYTHON else '.cpp'
+
+extensions = [
+    Extension('pypolyagamma.pypolyagamma',
+              ['pypolyagamma/pypolyagamma' + ext],
+              include_dirs=[np.get_include()])
+]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+
+setup(
+    name='pypolyagamma-3',
+    version='0.1.7',
+    description='''Cython wrappers for Polya gamma random number generation based on Jesse
+                   Windle\'s BayesLogit package: https://github.com/jwindle/BayesLogit. Forked
+                   from original pypolyagamma which is authored by Scott Linderman:
+                   https://github.com/slinderman/pypolyagamma.''',
+    author='Matt Burbidge',
+    author_email='mburbidge@savvysherpa.com',
+    url='http://www.github.com/Savvysherpa/pypolyagamma',
+    license="MIT",
+    packages=['pypolyagamma'],
+    ext_modules=extensions,
+    install_requires=[
+        'numpy',
+        'scipy',
+        ],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: C++',
+        ],
+    keywords=['monte-carlo', 'polya', 'gamma'],
+    platforms='ALL',
+)
