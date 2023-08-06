44547d70ed31408402c8226960594d149dcdf651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:15 +0000

    Add package codev/0.5.4a5/codev-0.5.4a5-py3-none-any.whl

---
+++ packages/codev/codev-0.5.4a5-py3-none-any.whl/codev/source.py
@@ -0,0 +1,35 @@
+from .provider import Provider, ConfigurableProvider
+
+
+class Source(Provider, ConfigurableProvider):
+    def __init__(self, options, *args, **kwargs):
+        self.ident = self.create_ident(self.process_options(options))
+        self.options = options
+        super().__init__(*args, **kwargs)
+
+    @property
+    def name(self):
+        return self.__class__.provider_name
+
+    def create_ident(self, ident):
+        return '{name}_{ident}'.format(
+            name=self.name,
+            ident=ident
+        )
+
+    def install(self, performer):
+        raise NotImplementedError()
+
+    def machine_install(self, machine):
+        pass
+
+    def process_options(self, options):
+        return options
+
+    @property
+    def id(self):
+        raise NotImplementedError()
+
+    @property
+    def directory(self):
+        return 'repository'
