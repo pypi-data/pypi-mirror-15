9e47ae36baf8a60d0447d427dbc111f748844544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:22 +0000

    Add package starpath/1.1.4/starpath-1.1.4.tar.gz

---
+++ packages/starpath/starpath-1.1.4.tar.gz/starpath-1.1.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'starpath',
+  packages=['starpath'],
+  version = '1.1.4',
+  description = 'JSON Pointers with wildcards!',
+  author = 'Arnaud Dagnelies',
+  author_email = 'arnaud.dagnelies@gmail.com',
+  url = 'https://github.com/dagnelies/starpath',
+  keywords = ['path', 'json', 'pointer', 'wildcard'],
+  classifiers = [],
+)
