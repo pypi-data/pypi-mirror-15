5ecffebdef0676502a150743b5bf601bd4c7cc68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:17 +0000

    Add package growler/0.7.4/growler-0.7.4-py3-none-any.whl

---
+++ packages/growler/growler-0.7.4-py3-none-any.whl/growler/http/protocol.py
@@ -0,0 +1,118 @@
+#
+# growler/http/protocol.py
+#
+"""
+Code containing Growler's asyncio.Protocol code for handling HTTP requests.
+"""
+
+from growler.protocol import GrowlerProtocol
+from growler.http.responder import GrowlerHTTPResponder
+from growler.http.response import HTTPResponse
+from growler.http.errors import (
+    HTTPError
+)
+
+
+# Or should this be called HTTPGrowlerProtocol?
+class GrowlerHTTPProtocol(GrowlerProtocol):
+    """
+    GrowlerProtocol dealing with HTTP requests. Objects are created with a
+    growler.App instance, which contains the relevant event_loop. The default
+    responder_type is GrowlerHTTPResponder, which does the data parsing and
+    req/res creation.
+
+    Additional responders may be created and used, the req/res pair may be
+    lost, but only one GrowlerHTTPProtocol object will persist through the
+    connection; it may be wise to store HTTP information in this.
+
+    To change the responder type to something other than GrowlerHTTPResponder,
+    overload or replace the http_responder_factory method.
+    """
+
+    client_method = None
+    client_query = None
+    client_headers = None
+
+    def __init__(self, app):
+        """
+        Construct a GrowlerHTTPProtocol object. This should only be called from
+        a growler.HTTPServer instance (or any asyncio.create_server function).
+
+        Parameters
+        ----------
+        app : growler.Application
+            Typically a growler application which is the 'target object' of
+            this protocol. Any callable with a 'loop' attribute and a
+            handle_client_request coroutine method should work.
+        """
+        self.http_application = app
+        super().__init__(loop=app.loop,
+                         responder_factory=self.http_responder_factory)
+
+    @staticmethod
+    def http_responder_factory(proto):
+        """
+        The default factory function which creates a GrowlerHTTPResponder with
+        this object as the parent protocol, and the application's req/res
+        factory functions.
+
+        To change the default responder, overload this method with the same
+        to return your
+        own responder.
+
+        Params
+        ------
+        proto : GrowlerHTTPProtocol
+            Explicitly passed protocol object (actually it's what would be
+            'self'!)
+
+        Note
+        ----
+        This method is decorated with @staticmethod, as the connection_made
+        method of GrowlerProtocol explicitly passes `self` as a parameters,
+        instead of treating as a bound method.
+        """
+        return GrowlerHTTPResponder(
+            proto,
+            request_factory=proto.http_application._request_class,
+            response_factory=proto.http_application._response_class,
+        )
+
+    def handle_error(self, error):
+        """
+        An error handling function which will be called when an error is raised
+        during a responder's on_data() function. There is no default
+        functionality and the subclasses must overload this.
+
+        Parameters
+        ----------
+        error : Exception
+            Exception thrown during code execution
+        """
+
+        err_code = error.code if isinstance(error, HTTPError) else 500
+        err_msg = error.msg if isinstance(error, HTTPError) else "Server Error"
+
+        # for error_handler in self.http_application.next_error_handler(req):
+        err_str = ("<html>"
+                   "<head></head>"
+                   "<body><h1>HTTP Error : %d %s </h1></body>"
+                   "</html>") % (err_code, err_msg)
+
+        header_info = {
+            'code': err_code,
+            'msg': err_msg,
+            'date': HTTPResponse.get_current_time(),
+            'length': len(err_str.encode()),
+            'contents': err_str
+        }
+
+        response = '\r\n'.join((
+            "HTTP/1.1 {code} {msg}",
+            "Content-Type: text/html; charset=UTF-8",
+            "Content-Length: {length}",
+            "Date: {date}",
+            "",
+            "{contents}")).format(**header_info)
+
+        self.transport.write(response.encode())
---
+++ packages/growler/growler-0.7.4-py3-none-any.whl/growler/middleware/session.py
@@ -0,0 +1,126 @@
+#
+# growler/middleware/session.py
+#
+"""
+A standard 'Session' class which holds data about consecutive calls from a
+browser to the server.
+"""
+
+import asyncio
+import uuid
+
+
+class Session:
+    """
+    Session middleware!
+    """
+    _data = {}
+
+    def __init__(self, storage, values={}):
+        self._data = values
+        self._store = storage
+
+    def __getitem__(self, name):
+        return self._data[name]
+
+    def __setitem__(self, name, value):
+        self._data[name] = value
+
+#   def __delitem__(self, name):
+#     del self._data[name]
+
+#   def __getattr__(self, name):
+#     print ("[__getattr__]:", name)
+#     return object.__getattribute__(self, '_data')[name]
+#     return self._data[name]
+
+#   def __setattr__(self, name, value):
+#     print ("[__setattr__]:", name,value)
+#     #self[name] = value
+#     self._data[name] = value
+
+    def get(self, name, default=None):
+        return self._data[name] if name in self._data else default
+
+#     return object.__getitem__(self, '_data')['name']
+#   def __set__(self, name, value):
+#     print ("+++ Seting ",name,vlaue)
+#     self._data[name] = value
+#
+#   def iteritems(self):
+#     return self._data.iteritems()
+#
+#   def keys(self):
+#     return self._data.keys()
+#
+#   def items(self):
+#     return self._data.items()
+#
+#   def iterkeys(self):
+#     return self._data.iterkeys()
+#
+#   def iteritems(self):
+#     return self._data.iteritems()
+#
+    def __iter__(self):
+        return self._data.__iter__()
+#
+#   def __contains__(self, key):
+#     return key in self._data
+#
+#   def __dict__(self):
+#     print ("DICT")
+
+    @asyncio.coroutine
+    def save(self):
+        yield from self._store.save(self)
+
+
+class SessionStorage:
+
+    def __init__(self, **kwargs):
+        print("[SessionStorage]")
+
+    def save(self, sess):
+        raise NotImplementedError
+
+
+class DefaultSessionStorage(SessionStorage):
+    """
+    The growler default session storage uses a standard python dict to store
+    all sessions ids and variables. The application must use a cookie parser
+    (for example, growler.middleware.CookieParser()) BEFORE using the
+    DefaultSessionStorage.
+    .. code: python
+        app.use(CookieParser())
+        app.use(DefaultSessionStorage())
+    """
+    def __init__(self, session_id_name='qid'):
+        """
+
+        """
+        super().__init__()
+        self.session_id_name = session_id_name
+        self._sessions = {}
+
+    def __call__(self, req, res):
+        """
+        The middleware action. Adds a session member to the req object and the
+        session id to the resoponse object.
+        """
+        qid = self.session_id_name
+        try:
+            sid = req.cookies[qid].value
+        except KeyError:
+            sid = req.cookies[qid] = uuid.uuid4()
+
+        res.cookies[qid] = sid
+
+        print("[DefaultSessionStorage] ", sid)
+        if sid not in self._sessions:
+            self._sessions[sid] = {'id': sid}
+        req.session = Session(self, self._sessions[sid])
+
+    def save(self, sess):
+        # print ("[DefaultSessionStorage::save] saving", sess.id)
+        self._sessions[sess.id] = sess._data
