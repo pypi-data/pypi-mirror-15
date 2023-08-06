d27e5864d1e5d690dc2952e51daceb95fe155e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:54 +0000

    Add package umat/0.1.1/umat-0.1.1.zip

---
+++ packages/umat/umat-0.1.1.zip/umat-0.1.1/umat/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from .endpoints import (
+    AdvertiserReportCohortValues,
+    AdvertiserReportLogEvents,
+    AdvertiserReportLogInstalls,
+    AdvertiserReportLogOpens,
+    MatRequestError,
+    MatExportError
+)
+
+
+__version__ = '0.1.1'
