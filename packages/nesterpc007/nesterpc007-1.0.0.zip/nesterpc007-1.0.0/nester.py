3be08f06cbe820b435e8c8221a6ab3151a2ad37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:38 +0000

    Add package nesterpc007/1.0.0/nesterpc007-1.0.0.zip

---
+++ packages/nesterpc007/nesterpc007-1.0.0.zip/nesterpc007-1.0.0/nester.py
@@ -0,0 +1,9 @@
+"""this is "nester.py" modle and it 
+"""
+def print_lol(the_list):
+	"""this function"""
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item)
+		else:
+			print(each_item)
