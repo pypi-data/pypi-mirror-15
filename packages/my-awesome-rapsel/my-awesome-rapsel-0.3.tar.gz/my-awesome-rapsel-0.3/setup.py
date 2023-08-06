44e8e0efe14a1ceef18a8d25f5473be06be0a9a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:41 +0000

    Add package my-awesome-rapsel/0.3/my-awesome-rapsel-0.3.tar.gz

---
+++ packages/my-awesome-rapsel/my-awesome-rapsel-0.3.tar.gz/my-awesome-rapsel-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='my-awesome-rapsel',    # This is the name of your PyPI-package.
+    version='0.3',                          # Update the version number for new releases
+    scripts=['test_rap.py'],                  # The name of your scipt, and also the command you'll be using for calling it
+    author='Syam Sasi',
+    author_email='syamsasi99@gmail.com'
+)
