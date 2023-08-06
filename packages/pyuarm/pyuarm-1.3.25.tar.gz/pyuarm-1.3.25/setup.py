49fe4e0026c6466d5d77b20d9bab75e902bc8a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:12 +0000

    Add package pyuarm/1.3.25/pyuarm-1.3.25.tar.gz

---
+++ packages/pyuarm/pyuarm-1.3.25.tar.gz/pyuarm-1.3.25/setup.py
@@ -0,0 +1,48 @@
+from distutils.util import convert_path
+from setuptools import setup, find_packages
+
+main_ns = {}
+ver_path = convert_path('pyuarm/version.py')
+with open(ver_path) as ver_file:
+        exec(ver_file.read(), main_ns)
+
+version = main_ns['__version__']
+# print main_ns['__version__']
+#
+#
+#
+# import os
+# import json
+#
+long_description = open('README.rst').read()
+#
+# module_dir = os.path.dirname(os.path.abspath(__file__))
+# version_file = os.path.join(module_dir, 'pyuarm','version.json')
+# with open(version_file,'r') as v:
+#     data = json.load(v)
+#     v.close()
+#
+# # pyuarm version
+# version = data['module_version']
+# __version__ = VERSION = version
+#
+# # support version
+# support_versions = data['support_versions']
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='pyuarm',
+    version=version,
+    author='Joey Song/Alex Tan',
+    packages=find_packages(),
+    scripts=['pyuarm/tools/firmware_helper.py', 'pyuarm/tools/calibrate.py', 'pyuarm/tools/list_uarms.py'],
+    package_data={'pyuarm.tools': ['avrdude/*'], 'pyuarm':['version.json']},
+    include_package_data=True,
+    author_email='developer@ufactory.cc',
+    url="https://github.com/uarm-developer/pyuarm",
+    keywords="pyuarm uarm4py uarmForPython uarm ufactory",
+    install_requires=requirements,
+    long_description=long_description,
+    description='A python library for uArm',
+)
