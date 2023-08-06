4d35868f7ff1f2172e5aedd978eb3324edf5fa44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:15 +0000

    Add package img/2.5/img-2.5.tar.gz

---
+++ packages/img/img-2.5.tar.gz/img-2.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'img',
+  packages = ['img'], # this must be the same as the name above
+  version = '2.5',
+  description = 'my description',
+  author = 'Cesar Hernandez',
+  author_email = 'cesarhernandez@campusciff.net',
+  url = 'https://github.com/ciffcesarhernandez/campusciff', # use the URL to the github repo
+  download_url = 'https://github.com/ciffcesarhernandez/campusciff/tarball/2.5',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
