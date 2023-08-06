fcb9e210bd1a23c93a6a2cb271d5f892946e5bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:18 +0000

    Add package Orange3/3.3.5/Orange3-3.3.5-cp35-cp35m-macosx_10_6_intel.whl

---
+++ packages/Orange3/Orange3-3.3.5-cp35-cp35m-macosx_10_6_intel.whl/Orange/classification/tree.py
@@ -0,0 +1,28 @@
+import sklearn.tree as skl_tree
+from Orange.classification import SklLearner, SklModel
+from Orange.preprocess import (RemoveNaNClasses, Continuize,
+                               RemoveNaNColumns, SklImpute)
+
+__all__ = ["TreeLearner"]
+
+
+class TreeClassifier(SklModel):
+    pass
+
+
+class TreeLearner(SklLearner):
+    __wraps__ = skl_tree.DecisionTreeClassifier
+    __returns__ = TreeClassifier
+    name = 'tree'
+    preprocessors = [RemoveNaNClasses(),
+                     RemoveNaNColumns(),
+                     SklImpute(),
+                     Continuize()]
+
+    def __init__(self, criterion="gini", splitter="best", max_depth=None,
+                 min_samples_split=2, min_samples_leaf=1,
+                 max_features=None,
+                 random_state=None, max_leaf_nodes=None,
+                 preprocessors=None):
+        super().__init__(preprocessors=preprocessors)
+        self.params = vars()
---
+++ packages/Orange3/Orange3-3.3.5-cp35-cp35m-macosx_10_6_intel.whl/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.3.5'
+version = '3.3.5'
+full_version = '3.3.5'
+git_revision = ''
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
