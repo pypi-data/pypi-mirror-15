60c5ec497b507c2e62b4f78f2e7b5b4f7a73b361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:12 +0000

    Add package django-intl-tel-input/0.1.0/django-intl-tel-input-0.1.0.zip

---
+++ packages/django-intl-tel-input/django-intl-tel-input-0.1.0.zip/django-intl-tel-input-0.1.0/intl_tel_input/static/intl_tel_input/init.js
@@ -0,0 +1,37 @@
+(function($) {
+  var $el, options, $form,
+      cssClass = '.intl-tel-input',
+      forms = [],
+      inputs = $(cssClass);
+
+  inputs.each(function(i, el) {
+    $el = $(el);
+    options = {
+      initialCountry: "auto",
+      geoIpLookup: function(callback) {
+        $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+          var countryCode = (resp && resp.country) ? resp.country : "";
+          callback(countryCode);
+        });
+      }
+    };
+
+    if ($el.attr('data-utils-script') !== undefined) {
+      options.utilsScript = 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/9.0.1/js/utils.js';
+    }
+
+    $el.intlTelInput(options)
+    .done(function() {
+      $form = $el.closest('form');
+      if (forms.indexOf($form) === -1) {
+        $form.submit(function(e) {
+          $form.find('input' + cssClass).val(function() {
+            return $(this).intlTelInput("getNumber");
+          });
+        });
+
+        forms.push($form);
+      }
+    });
+  });
+})(jQuery);
---
+++ packages/django-intl-tel-input/django-intl-tel-input-0.1.0.zip/django-intl-tel-input-0.1.0/intl_tel_input/widgets.py
@@ -0,0 +1,26 @@
+import json
+from django import forms
+from django.utils.html import format_html
+
+
+class IntlTelInputWidget(forms.TextInput):
+    input_type = 'tel'
+    
+    class Media:
+        css = {
+            'all': ('https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/9.0.1/css/intlTelInput.css',),
+        }
+        js = ('https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/9.0.1/js/intlTelInput.min.js', 'intl_tel_input/init.js')
+
+    def __init__(self, attrs=None, use_utils=True):
+        final_attrs = {'class': 'intl-tel-input', 'size': '20'}
+        if attrs is not None:
+            final_attrs.update(attrs)
+
+        if use_utils:
+            final_attrs['data-utils-script'] = True
+
+        super(IntlTelInputWidget, self).__init__(attrs=final_attrs)
+
+    def get_options(self):
+        return json.dumps(self.options)
