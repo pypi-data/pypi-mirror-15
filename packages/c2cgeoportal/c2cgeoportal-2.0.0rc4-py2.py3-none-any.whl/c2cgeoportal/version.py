ee2b098000b4e06e6c944f19ab6c0c2039435d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:43 +0000

    Add package c2cgeoportal/2.0.0rc4/c2cgeoportal-2.0.0rc4-py2.py3-none-any.whl

---
+++ packages/c2cgeoportal/c2cgeoportal-2.0.0rc4-py2.py3-none-any.whl/c2cgeoportal/version.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2016, Camptocamp SA
+# All rights reserved.
+
+# Auto-generated file. Do not Edit!
+INFO = {
+    "git_hash": "aab78721d1475fe05b749d061a41ad289d3576c0",
+    "version": "2.0",
+    "git_tag": "2.0.0rc4",
+}
