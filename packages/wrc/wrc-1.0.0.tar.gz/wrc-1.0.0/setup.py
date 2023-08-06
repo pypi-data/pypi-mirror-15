ba0ff36ce2c567817ffc554c8ace914a9e98c461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:50 +0000

    Add package wrc/1.0.0/wrc-1.0.0.tar.gz

---
+++ packages/wrc/wrc-1.0.0.tar.gz/wrc-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='wrc',
+      version='1.0.0',
+      description='Tool to build and perform checks on WCA Regulations and Guidelines',
+      url='http://github.com/viroulep/wca-regulations-compiler',
+      author='Philippe Virouleau',
+      author_email='philippe.44@gmail.com',
+      license='GPLv3',
+      packages=find_packages(),
+      install_requires=['ply>=3.7'],
+      package_data={
+          'wrc': ['data/*'],
+      },
+      entry_points={
+          'console_scripts': ['wrc=wrc.wrc:run', 'wrc-languages=wrc.wrc:languages'],
+      },
+      zip_safe=False)
