3c5aef42463ab96aeba61e7b167ae92d6e2193e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:58 +0000

    Add package bw2io/0.5/bw2io-0.5.tar.gz

---
+++ packages/bw2io/bw2io-0.5.tar.gz/bw2io-0.5/bw2io/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+from eight import *
+
+from .packaging import BW2PackageTest
+from .simapro import SimaProCSVImporterTest
+from .utils import UtilsTestCase
+from .strategies import *
