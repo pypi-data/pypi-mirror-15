39091d7f3d28c1022db240526dcdab80fc2eaccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:16 +0000

    Add package tspapi/0.3.5/tspapi-0.3.5.tar.gz

---
+++ packages/tspapi/tspapi-0.3.5.tar.gz/tspapi-0.3.5/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='tspapi',
+    version='0.3.5',
+    url="https://github.com/boundary/pulse-api-python",
+    author='David Gwartney',
+    author_email='david_gwartney@bmc.com',
+    packages=['tspapi', ],
+    license='Apache 2',
+    description='Python Bindings for the TrueSight Pulse REST APIs',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests >= 2.3.0",
+	"six >= 1.10.0",
+	"python-dateutil >= 2.5.2",
+    ],
+)
