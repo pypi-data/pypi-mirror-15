3b6dbd956376a1b8165d1f36f0dacf9dab13a66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:31 +0000

    Add package Eve-Swagger/0.0.2/Eve-Swagger-0.0.2.tar.gz

---
+++ packages/Eve-Swagger/Eve-Swagger-0.0.2.tar.gz/Eve-Swagger-0.0.2/eve_swagger/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    eve-swagger
+    ~~~~~~~~~~~
+    swagger.io extension for Eve-powered REST APIs.
+
+    :copyright: (c) 2015 by Nicola Iarocci.
+    :license: BSD, see LICENSE for more details.
+"""
+from swagger import swagger  # noqa
+
+INFO = 'SWAGGER_INFO'
+HOST = 'SWAGGER_HOST'
+
+__version__ = '0.0.2'
