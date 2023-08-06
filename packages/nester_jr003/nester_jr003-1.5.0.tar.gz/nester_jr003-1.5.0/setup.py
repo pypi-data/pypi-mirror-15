cadaa00b661d0ebe5998abd0e0237430404e3952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:19 +0000

    Add package nester_jr003/1.5.0/nester_jr003-1.5.0.tar.gz

---
+++ packages/nester_jr003/nester_jr003-1.5.0.tar.gz/nester_jr003-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+name= 'nester_jr003',
+version='1.5.0',
+py_modules=['nester_jr003'],
+author='jrichards',
+author_email = 'jonathan1000000@gmail.com',
+url = 'http://www.nanoalmighty.com',
+description='a simple printer of nested lists',
+)
