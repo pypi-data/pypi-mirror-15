154e966924d122db5db7883bc4fd34e53a7409fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:33 +0000

    Add package createpy/0.0.0/createpy-0.0.0.tar.gz

---
+++ packages/createpy/createpy-0.0.0.tar.gz/createpy-0.0.0/createpy/createpy.py
@@ -0,0 +1,18 @@
+"""
+This is the main file containing all the logic for pycreate.
+"""
+import sys
+
+def main():
+    """
+    Main function for pycreate
+
+    :param args: command line parameters as list of strings
+    """
+    print("Hello World")
+
+def run():
+    main()
+
+if __name__ == "__main__":
+    run()
