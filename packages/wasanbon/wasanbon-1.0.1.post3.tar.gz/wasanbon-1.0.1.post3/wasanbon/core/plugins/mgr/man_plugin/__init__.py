3044b04516f531550810222dd26a9db2dde2568d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:24 +0000

    Add package wasanbon/1.0.1.post3/wasanbon-1.0.1.post3.tar.gz

---
+++ packages/wasanbon/wasanbon-1.0.1.post3.tar.gz/wasanbon-1.0.1.post3/wasanbon/core/plugins/mgr/man_plugin/__init__.py
@@ -0,0 +1,39 @@
+import wasanbon
+from wasanbon.core.plugins import PluginFunction, manifest
+
+class Plugin(PluginFunction):
+
+    def __init__(self):
+        #PluginFunction.__init__(self)
+        super(Plugin, self).__init__()
+        pass
+
+    def depends(self):
+        return ['admin.environment']
+
+    def _print_alternatives(self, args):
+        print 'hoo'
+        print 'foo'
+        print 'hoge'
+        print 'yah'
+
+    @manifest
+    def echo(self, argv):
+        """ This is help text
+        """
+        self.parser.add_option('-f', '--force', help='Force option (default=False)', default=False, action='store_true', dest='force_flag')
+        options, argv = self.parse_args(argv[:], self._print_alternatives)
+        verbose = options.verbose_flag # This is default option
+        force   = options.force_flag
+
+        if verbose:
+            print '# Verbosity option is ON!'
+
+        if force:
+            print '# Force Option is ON!'
+
+        print '# This is test mgr plugin'
+        if len(argv) > 2:
+            for p in argv[3:]:
+                print p
+
