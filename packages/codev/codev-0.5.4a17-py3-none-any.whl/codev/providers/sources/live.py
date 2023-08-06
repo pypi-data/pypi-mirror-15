eab607e1e722bff9ab4fbfba7a15465a5e2da50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:13 +0000

    Add package codev/0.5.4a17/codev-0.5.4a17-py3-none-any.whl

---
+++ packages/codev/codev-0.5.4a17-py3-none-any.whl/codev/providers/sources/live.py
@@ -0,0 +1,12 @@
+from os import getcwd
+from .actual import ActualSource
+
+
+class LiveSource(ActualSource):
+    provider_name = 'live'
+
+    def install(self, performer):
+        performer.share(getcwd(), self.directory)
+
+    def machine_install(self, machine):
+        machine.share('.', self.directory)
