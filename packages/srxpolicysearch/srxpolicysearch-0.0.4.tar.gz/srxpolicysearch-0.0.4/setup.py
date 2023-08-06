08ce1814f36055c848030a63909c5bf8363b9069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:01 +0000

    Add package srxpolicysearch/0.0.4/srxpolicysearch-0.0.4.tar.gz

---
+++ packages/srxpolicysearch/srxpolicysearch-0.0.4.tar.gz/srxpolicysearch-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='srxpolicysearch',
+    version='0.0.4',
+    packages=['srxpolicysearch'],
+    url='https://github.com/lampwins/srxpolicysearch',
+    license='MIT',
+    author='John Anderson',
+    author_email='lampwins@gmail.com',
+    description='Search security policy on the Juniper SRX platform',
+    install_requires=['paramiko']
+)
