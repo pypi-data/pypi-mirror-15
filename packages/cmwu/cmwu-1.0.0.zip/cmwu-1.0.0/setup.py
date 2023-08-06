2c825813418a419578b1081923d7a01fa0a17f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:27 +0000

    Add package cmwu/1.0.0/cmwu-1.0.0.zip

---
+++ packages/cmwu/cmwu-1.0.0.zip/cmwu-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup 
+setup(
+        name = 'cmwu',
+        version = '1.0.0',
+        py_module = 'cmwu',
+        author = 'cmwu',
+        description = 'a python test',
+    )
