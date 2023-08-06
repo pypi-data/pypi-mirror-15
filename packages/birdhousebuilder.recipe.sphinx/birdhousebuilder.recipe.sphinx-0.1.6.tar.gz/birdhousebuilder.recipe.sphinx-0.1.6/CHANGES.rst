4c363ba9bc445b6ab3fca327c8c3ae6015df92a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:40 +0000

    Add package birdhousebuilder.recipe.sphinx/0.1.6/birdhousebuilder.recipe.sphinx-0.1.6.tar.gz

---
+++ packages/birdhousebuilder.recipe.sphinx/birdhousebuilder.recipe.sphinx-0.1.6.tar.gz/birdhousebuilder.recipe.sphinx-0.1.6/CHANGES.rst
@@ -0,0 +1,43 @@
+Changes
+*******
+
+0.1.6 (2016-06-01)
+==================
+
+* rst_epilog links added.
+* ignore localhost links.
+* linkcheck_timeout added.
+
+0.1.5 (2016-02-19)
+==================
+
+* added use_autoapi option.
+
+0.1.4 (2016-02-19)
+==================
+
+* added linkcheck options.
+
+0.1.3 (2015-10-24)
+==================
+
+* setups requirements/rtd.txt for readthedocs.
+
+0.1.2 (2015-10-24)
+==================
+
+* replaced autodoc by sphinx-autoapi extension
+* using src option for sphinx-autoapi
+
+0.1.1 (2015-10-23)
+==================
+
+* added extra_paths
+* added sys.path in conf.py template
+
+0.1.0 (2015-10-23)
+==================
+
+Initial Release
+
+
---
+++ packages/birdhousebuilder.recipe.sphinx/birdhousebuilder.recipe.sphinx-0.1.6.tar.gz/birdhousebuilder.recipe.sphinx-0.1.6/setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of birdhousebuilder.recipe.sphinx
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.1.6'
+
+long_description = (
+    read('README.rst') + '\n' +
+    read('AUTHORS.rst') + '\n' +
+    read('CHANGES.rst')
+    )
+
+
+entry_point = 'birdhousebuilder.recipe.sphinx'
+entry_points = {"zc.buildout": [
+    "default = %s:Recipe" % entry_point,
+    ],
+    "zc.buildout.uninstall": [
+    "default = %s:uninstall" % entry_point,
+    ],
+}
+
+tests_require = ['zope.testing', 'zc.buildout', 'manuel']
+    
+setup(name='birdhousebuilder.recipe.sphinx',
+      version=version,
+      description="Buildout recipe to generate and Sphinx-based documentation for Birdhouse.",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        ],
+      keywords='buildout sphinx',
+      author='Birdhouse',
+      author_email='ehbrecht at dkrz.de',
+      url='https://github.com/birdhouse/birdhousebuilder.recipe.sphinx',
+      license='Apache License 2.0',
+      packages = find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+            'setuptools',
+            'zc.buildout',
+            'zc.recipe.egg',
+            'docutils',
+            'Mako',
+            'birdhousebuilder.recipe.conda',
+            'Sphinx>=1.3',
+            'sphinx-autoapi',
+      ],
+      tests_require=tests_require,
+      test_suite = 'birdhousebuilder.recipe.sphinx.tests.test_docs.test_suite',
+      entry_points = entry_points,
+      )
+
