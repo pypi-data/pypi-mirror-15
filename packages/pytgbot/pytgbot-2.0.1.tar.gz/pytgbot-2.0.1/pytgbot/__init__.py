e14ad3f3ba5ba72c99e47954a3f9278d55c72b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:14 +0000

    Add package pytgbot/2.0.1/pytgbot-2.0.1.tar.gz

---
+++ packages/pytgbot/pytgbot-2.0.1.tar.gz/pytgbot-2.0.1/pytgbot/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import json
+import logging
+from .bot import Bot
+
+__author__ = 'luckydonald'
+__version__ = "2.0.0"
+__all__ = ["api_types", "Bot"]
+VERSION = __version__
+
+logger = logging.getLogger(__name__)
+
