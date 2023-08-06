70e7b6478019153de164a1294049f64870e493e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:41 +0000

    Add package Text2image/2.1/Text2image-2.1.tar.gz

---
+++ packages/Text2image/Text2image-2.1.tar.gz/Text2image-2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(name='Text2image',
+      version='2.1',
+      description='Python text2image Text2image',
+      author='aertoria',
+      author_email='ethanwang.dev@gmail.com',
+      url='https://www.python.org',
+      packages = find_packages(),
+      include_package_data=True,
+      license = "Apache License, Version 2.0",
+     )
