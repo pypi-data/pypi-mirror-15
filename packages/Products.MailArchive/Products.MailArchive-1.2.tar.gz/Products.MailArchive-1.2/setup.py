e518c6bde1d82449dd106cb5c5d6fda3f2e652bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:48 +0000

    Add package Products.MailArchive/1.2/Products.MailArchive-1.2.tar.gz

---
+++ packages/Products.MailArchive/Products.MailArchive-1.2.tar.gz/Products.MailArchive-1.2/setup.py
@@ -0,0 +1,31 @@
+""" MailArchive installer
+"""
+import os
+from setuptools import setup, find_packages
+
+NAME = 'Products.MailArchive'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
+
+setup(name=NAME,
+      version=VERSION,
+      description="Browse a mail archive in Unix MBOX format",
+      long_description=open("README.rst").read(),
+      classifiers=[
+           "Framework :: Zope2",
+           "Programming Language :: Python",
+           "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='eea',
+      author='European Environment Agency (EEA)',
+      author_email='webadmin@eea.europa.eu',
+      url="https://github.com/eea/Products.MailArchive",
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      )
