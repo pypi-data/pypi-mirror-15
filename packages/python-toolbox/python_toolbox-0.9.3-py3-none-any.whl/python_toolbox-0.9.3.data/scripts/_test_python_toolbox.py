8cb2b35bcde0cee3d26c6b4aebc0ff0edd6c5622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:37 +0000

    Add package python-toolbox/0.9.3/python_toolbox-0.9.3-py3-none-any.whl

---
+++ packages/python-toolbox/python_toolbox-0.9.3-py3-none-any.whl/python_toolbox-0.9.3.data/scripts/_test_python_toolbox.py
@@ -0,0 +1,15 @@
+#!/root/python-3.3/bin/python3.3
+
+# Copyright 2009-2017 Ram Rachum.
+# This program is distributed under the MIT license.
+
+'''
+Script for launching `python_toolbox` tests when installed in local Python.
+'''
+
+
+import test_python_toolbox
+
+
+if __name__ == '__main__':
+    test_python_toolbox.invoke_nose()
