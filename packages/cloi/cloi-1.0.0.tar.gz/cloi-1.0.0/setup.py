71e72a9eecbc6134e6b4262c100c9a71861deb21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:15 +0000

    Add package cloi/1.0.0/cloi-1.0.0.tar.gz

---
+++ packages/cloi/cloi-1.0.0.tar.gz/cloi-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cloi',
+    version='1.0.0',
+    description='A wrapper to the xtralien library',
+    author='Xtralien',
+    author_email='jack@xtralien.com',
+    url='https://github.com/Xtralien/pycloi.git',
+    packages=['cloi'],
+    install_requires=['xtralien'],
+    license='GPLv3'
+)
