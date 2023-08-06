3b894583a878ba24598f271deb73ef0dea6ba346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:49 +0000

    Add package bok-choy/0.5.3/bok_choy-0.5.3.tar.gz

---
+++ packages/bok-choy/bok_choy-0.5.3.tar.gz/bok_choy-0.5.3/README.rst
@@ -0,0 +1,42 @@
+bok-choy
+========
+
+UI-level acceptance test framework.  `Full documentation available on ReadTheDocs`__.
+
+__ http://bok-choy.readthedocs.org/en/latest/
+
+
+Overview
+--------
+
+A Python framework for writing robust Selenium tests.
+
+
+License
+-------
+
+The code in this repository is licensed under the Apache License, Version 2.0,
+unless otherwise noted.
+
+Please see ``LICENSE.txt`` for details.
+
+
+How to Contribute
+-----------------
+
+Contributions are very welcome. The easiest way is to fork this repo, and then
+make a pull request from your fork. The first time you make a pull request, you
+may be asked to sign a Contributor Agreement.
+
+
+Reporting Security Issues
+-------------------------
+
+Please do not report security issues in public. Please email security@edx.org
+
+
+Mailing List and IRC Channel
+----------------------------
+
+You can discuss this code on the `edx-code Google Group <https://groups.google.com/forum/#!forum/edx-code>`_
+or in the **testing** channel on the `Open edX Slack <https://openedx.slack.com>`_.
---
+++ packages/bok-choy/bok_choy-0.5.3.tar.gz/bok_choy-0.5.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = '0.5.3'
+DESCRIPTION = 'UI-level acceptance test framework'
+with open('requirements.txt') as f:
+    REQUIREMENTS = f.read().splitlines()
+
+setup(
+    name='bok_choy',
+    version=VERSION,
+    author='edX',
+    url='http://github.com/edx/bok-choy',
+    description=DESCRIPTION,
+    license='Apache 2.0',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Topic :: Software Development :: Testing',
+                 'Topic :: Software Development :: Quality Assurance'],
+    packages=['bok_choy', 'bok_choy/a11y'],
+    package_data={'bok_choy': ['vendor/google/*.*', 'vendor/axe-core/*.*']},
+    install_requires=REQUIREMENTS,
+)
