be0305e0518c99177636a7c56b78d588d4b80513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:53 +0000

    Add package bw2calc/1.3.6/bw2calc-1.3.6.tar.gz

---
+++ packages/bw2calc/bw2calc-1.3.6.tar.gz/bw2calc-1.3.6/bw2calc/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    'DenseLCA',
+    'direct_solving_worker',
+    'DirectSolvingMixin',
+    'DirectSolvingMonteCarloLCA',
+    'GraphTraversal',
+    'IndepentLCAMixin',
+    'LCA',
+    'LeastSquaresLCA',
+    'MatrixBuilder',
+    'MonteCarloLCA',
+    'MultiLCA',
+    'MultiMonteCarlo',
+    'ParallelMonteCarlo',
+    'ParameterVectorLCA',
+    'TechnosphereBiosphereMatrixBuilder',
+]
+
+__version__ = (1, 3, 6)
+
+from .lca import LCA
+from .dense_lca import DenseLCA
+from .independent_lca import IndepentLCAMixin
+from .least_squares import LeastSquaresLCA
+from .monte_carlo import (
+    direct_solving_worker,
+    DirectSolvingMixin,
+    DirectSolvingMonteCarloLCA,
+    MonteCarloLCA,
+    MultiMonteCarlo,
+    ParallelMonteCarlo,
+)
+from .multi_lca import MultiLCA
+from .mc_vector import ParameterVectorLCA
+from .graph_traversal import GraphTraversal
+from .matrices import MatrixBuilder, TechnosphereBiosphereMatrixBuilder
