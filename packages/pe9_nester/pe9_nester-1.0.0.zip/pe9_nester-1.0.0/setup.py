7b4767a34117b401e41da7551f04f1ac59e675b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:31 +0000

    Add package pe9_nester/1.0.0/pe9_nester-1.0.0.zip

---
+++ packages/pe9_nester/pe9_nester-1.0.0.zip/pe9_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name='pe9_nester',
+        version='1.0.0',
+        py_modules=['nester'],
+        author='xiangmibb',
+        author_email='395731031@qq.com',
+        url='http://www.qq.com',
+        description='A simple printer of nested lists',
+    )
