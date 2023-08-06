f732a9a68070c9b2f4010ea5ad8c04556bf41486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:29 +0000

    Add package dstufft.testpkg/2016.6.2.1/dstufft.testpkg-2016.6.2.1.tar.gz

---
+++ packages/dstufft.testpkg/dstufft.testpkg-2016.6.2.1.tar.gz/dstufft.testpkg-2016.6.2.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="dstufft.testpkg",
+    version="2016.06.02.1",
+    packages=["dstufft_testpkg"],
+    package_data={"dstufft_testpkg": ["data"]},
+    # url="javascript:alert(0)",
+    download_url="https://example.com/",
+)
