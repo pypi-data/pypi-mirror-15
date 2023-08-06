0699df91b726eb2f1b83524bc29399579b750e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:09 +0000

    Add package nester1234/1.3.0/nester1234-1.3.0.zip

---
+++ packages/nester1234/nester1234-1.3.0.zip/nester1234-1.3.0/nester1234.py
@@ -0,0 +1,17 @@
+"""Head First Python
+	My First Python Module """
+def print_lol(the_list, level=0, indent=False):
+	"""This function takes a positional argument called "the_list",
+		which is any Phthon list (of- possoble - nested lists).
+		Each data item in the provided list is (recursively) printed
+		to the screen on it's own line.'"""
+	for each_item in the_list:
+	#for every each_item
+		if isinstance(each_item, list):
+		#Recursive func call
+			print_lol(each_item, level+1, indent)
+		else:
+			if indent:
+				for tab_stop in range(level):
+					print("\t", end='')
+			print(each_item)
