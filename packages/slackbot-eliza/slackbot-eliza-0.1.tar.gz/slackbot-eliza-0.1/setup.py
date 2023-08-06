cea4b090ee1f5cc753b331d502880eaef8f6ebd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:11 +0000

    Add package slackbot-eliza/0.1/slackbot-eliza-0.1.tar.gz

---
+++ packages/slackbot-eliza/slackbot-eliza-0.1.tar.gz/slackbot-eliza-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='slackbot-eliza',
+    packages=['slackbot-eliza'],
+    version='0.1',
+    description='Slackbot Eliza Plugin',
+    author='David Benjamin',
+    author_email='david@slowbluecamera.com',
+    url='https://github.com/slowbluecamera/slackbot-eliza',
+    download_url = 'https://github.com/slowbluecamera/slackbot-eliza/tarball/0.1'
+)
