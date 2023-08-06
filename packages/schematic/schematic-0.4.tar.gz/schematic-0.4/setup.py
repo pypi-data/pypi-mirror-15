f77c5b7d29e6cb8802b4a42936e25b48fbf37c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:43 +0000

    Add package schematic/0.4/schematic-0.4.tar.gz

---
+++ packages/schematic/schematic-0.4.tar.gz/schematic-0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='schematic',
+    version='0.4',
+    url='https://github.com/mozilla/schematic',
+    author_email='dev-webdev@lists.mozilla.org',
+    scripts=['schematic'],
+    description='The worst schema versioning system, ever?',
+)
