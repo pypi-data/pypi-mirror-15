b414d8efcb70e5f132bc5c40d9b3b83fc1bcd7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:24 +0000

    Add package flask_servicenow/0.1.1/flask_servicenow-0.1.1.tar.gz

---
+++ packages/flask_servicenow/flask_servicenow-0.1.1.tar.gz/flask_servicenow-0.1.1/flask_servicenow/__init__.py
@@ -0,0 +1,44 @@
+
+# -*- coding: utf-8 -*-
+
+
+__author__ = "Robert Wikman <rbw@vault13.org>"
+
+import servicenow_rest.api as sn
+from flask import current_app
+
+try:
+    from flask import _app_ctx_stack as stack
+except ImportError:
+    from flask import _request_ctx_stack as stack
+
+
+class FlaskServiceNow(object):
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        if hasattr(app, 'teardown_appcontext'):
+            app.teardown_appcontext(self.teardown)
+        else:
+            app.teardown_request(self.teardown)
+
+    def connect(self):
+        return sn.Client(current_app.config['SN_INSTANCE'],
+                         current_app.config['SN_USER'],
+                         current_app.config['SN_PASS'])
+
+    @property
+    def connection(self):
+        ctx = stack.top
+        if ctx is not None:
+            if not hasattr(ctx, 'sn_client'):
+                ctx.sn_client = self.connect()
+            return ctx.sn_client
+
+    def teardown(self, exception):
+        ctx = stack.top
+        if hasattr(ctx, 'sn_client'):
+            ctx.sn_client = None
