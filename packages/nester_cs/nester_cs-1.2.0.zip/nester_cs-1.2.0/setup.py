f15fa132f007d543ec31a51cf77c2a86f8518fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:10 +0000

    Add package nester_cs/1.2.0/nester_cs-1.2.0.zip

---
+++ packages/nester_cs/nester_cs-1.2.0.zip/nester_cs-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester_cs',
+        version = '1.2.0',
+        py_modules = ['nester_cs'],
+        author = 'dreamlin',
+        author_email = 'dreamlin@headfirstlabs.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
