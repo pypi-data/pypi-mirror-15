97a9f3de5b1b6c5e19816bcbc3b927229e992873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:20 +0000

    Add package diffpy.mpdf/0.0.0/diffpy.mpdf-0.0.0.tar.gz

---
+++ packages/diffpy.mpdf/diffpy.mpdf-0.0.0.tar.gz/diffpy.mpdf-0.0.0/diffpy/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.mpdf         by Billinge Group
+#                     Simon J. L. Billinge sb2896@columbia.edu
+#                     (c) 2016 trustees of Columbia University in the City of
+#                           New York.
+#                      All rights reserved
+#
+# File coded by:    Benjamin Frandsen
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+
+__import__('pkg_resources').declare_namespace(__name__)
+
---
+++ packages/diffpy.mpdf/diffpy.mpdf-0.0.0.tar.gz/diffpy.mpdf-0.0.0/diffpy/mpdf/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.magpdf       by Billinge Group
+#                     Simon J. L. Billinge sb2896@columbia.edu
+#                     (c) 2016 trustees of Columbia University in the City of
+#                           New York.
+#                      All rights reserved
+#
+# File coded by:    Benjamin Frandsen
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+
+"""Tools for magnetic pair distribution function analysis
+"""
+
+# package version
+from diffpy.mpdf.version import __version__
+
+# convenience imports
+from diffpy.mpdf.magstructure import *
+from diffpy.mpdf.mpdfcalculator import *
+
+# End of file
