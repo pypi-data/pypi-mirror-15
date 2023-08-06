c2362c4d687b6693ded8bdb1c666d836494e41d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:09 +0000

    Add package athletelist_cww/1.2.1/athletelist_cww-1.2.1.zip

---
+++ packages/athletelist_cww/athletelist_cww-1.2.1.zip/athletelist_cww-1.2.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name            = "athletelist_cww",
+    version         = "1.2.1",
+    py_modules      = ["Athelte"],
+    author          = "David",
+    author_email    = "tdavidf@163.com",
+    url             = "http://www.headfirstlabs.com",
+    description     = "A class to order athlete times",
+    )
