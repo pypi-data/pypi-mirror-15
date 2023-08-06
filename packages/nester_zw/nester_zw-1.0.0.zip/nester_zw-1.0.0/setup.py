cf5d942dfbe317040fd2e485451aca5868cc6792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:10 +0000

    Add package nester_zw/1.0.0/nester_zw-1.0.0.zip

---
+++ packages/nester_zw/nester_zw-1.0.0.zip/nester_zw-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import  setup
+setup(
+    name='nester_zw',
+    version='1.0.0',
+    py_modules=['nester_zw'],
+    author='zw',
+    author_email='908353547@qq.com',
+    url='',
+    description='A better simple printer of nested lists',
+
+)
