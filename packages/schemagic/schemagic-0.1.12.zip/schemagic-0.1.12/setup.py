a5a15ce944c25f72faba4c2198a1ed9fef4cf0c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:05 +0000

    Add package schemagic/0.1.12/schemagic-0.1.12.zip

---
+++ packages/schemagic/schemagic-0.1.12.zip/schemagic-0.1.12/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+  name = 'schemagic',
+  packages = ['schemagic'],
+  version = '0.1.12',
+  description = 'Define the shape of your data with simple python data structures. Use those data descriptions to validate your application.',
+  author = 'Tyler Tolton',
+  author_email = 'tjtolton@gmail.com',
+  url = 'https://github.com/TJTolton/schemagic',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1.12',
+  keywords = ['schema', 'schemas', 'schemata',
+              'validate', 'validation', 'validator'
+              'json', 'REST', 'webservice', 'flask', 'POST'
+              'agile',],
+  classifiers = [],
+)
