9e71dc739f3f02d664e251abd97f879f2c09ff13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:32 +0000

    Add package emq_celery/0.0.7/emq_celery-0.0.7.tar.gz

---
+++ packages/emq_celery/emq_celery-0.0.7.tar.gz/emq_celery-0.0.7/emq_celery/__init__.py
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'bac'
+from kombu.transport import TRANSPORT_ALIASES
+# from celery.backends import BACKEND_ALIASES
+
+TRANSPORT_ALIASES["emq"] = __name__ + '.transport:EMQTransport'
+# BACKEND_ALIASES["emqcache"] = 'emq_celery.backend:EMQCacheBackend'
