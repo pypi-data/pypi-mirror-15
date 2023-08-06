0963b0b46ef3efb3b0700b072793fe25e625116b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:52 +0000

    Add package coala-bears/0.3.0.dev20160617100841/coala-bears-0.3.0.dev20160617100841.tar.gz

---
+++ packages/coala-bears/coala-bears-0.3.0.dev20160617100841.tar.gz/coala-bears-0.3.0.dev20160617100841/bears/vimscript/VintBear.py
@@ -0,0 +1,22 @@
+from coalib.bearlib.abstractions.Linter import linter
+from coalib.bears.requirements.PythonRequirement import PythonRequirement
+
+
+@linter(executable='vint', output_format="regex",
+        output_regex=r'.+:(?P<line>\d+):(?P<column>\d+): (?P<message>.+)')
+class VintBear:
+    """
+    Check vimscript code for possible style problems.
+
+    See <https://github.com/Kuniwak/vint> for more information.
+    """
+
+    LANGUAGES = {"VimScript"}
+    REQUIREMENTS = PythonRequirement('vim-vint', '0.3.*')
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return filename,
