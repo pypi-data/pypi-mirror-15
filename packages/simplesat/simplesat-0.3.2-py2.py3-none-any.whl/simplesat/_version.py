dd070ebcfb100d52e33076c7913628350a66e008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:29 +0000

    Add package simplesat/0.3.2/simplesat-0.3.2-py2.py3-none-any.whl

---
+++ packages/simplesat/simplesat-0.3.2-py2.py3-none-any.whl/simplesat/_version.py
@@ -0,0 +1,9 @@
+# THIS FILE IS GENERATED FROM SETUP_EXT
+version = '0.3.2'
+full_version = '0.3.2'
+git_revision = '3380d05ccd4e9ad2ef889b4b92912d73eed627bb'
+is_released = True
+
+msi_version = '0.3.2.775'
+
+version_info = (0, 3, 2, 'final', 0)
