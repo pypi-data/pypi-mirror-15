34db5d504eaeb8e8971efd493aba917db8ff92cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:16 +0000

    Add package prusontchm/1.4/prusontchm-1.4.zip

---
+++ packages/prusontchm/prusontchm-1.4.zip/prusontchm-1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'prusontchm',
+  packages = ['prusontchm'], # this must be the same as the name above
+  version = '1.4',
+  description = 'practica3',
+  author = 'Cesar Hernandez y Sara Otero de Navascues', 
+  author_email = 'cesarhernandez@campusciff.net, saraotero@campusciff',
+  url = 'https://github.com/ciffcesarhernandez/practica3', # use the URL to the github repo
+  download_url = 'https://github.com/ciffcesarhernandez/practica3/tarball/1.4',
+  keywords = ['testing', 'logging', 'example','ciff'],
+  classifiers = [],
+)
