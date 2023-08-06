b0f2d1c9644a44acbdaed80e0a82ff5ea5032bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:22 +0000

    Add package my-first/1.0.0/my-first-1.0.0.zip

---
+++ packages/my-first/my-first-1.0.0.zip/my-first-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'my-first',
+    version = '1.0.0',
+    py_modules = ['my-first'],
+    author = 'wyq',
+    author_email = '1159255468@qq.com',
+    url = 'https://pypi.python.org/pypi',
+    description = 'a function to print list'
+)
