2547a1c58c50444a0789dda0973fcab1eae29fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:59 +0000

    Add package sjc_funniest/0.1/sjc_funniest-0.1.tar.gz

---
+++ packages/sjc_funniest/sjc_funniest-0.1.tar.gz/sjc_funniest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sjc_funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['sjc_funniest'],
+      zip_safe=False)
