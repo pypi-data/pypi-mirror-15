c811c5cdb36c5097ad7d478e737d9e26a697623d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:08 +0000

    Add package djangocms-revolutionslider/0.8/djangocms-revolutionslider-0.8.tar.gz

---
+++ packages/djangocms-revolutionslider/djangocms-revolutionslider-0.8.tar.gz/djangocms-revolutionslider-0.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = "djangocms-revolutionslider",
+    version = "0.8",
+    author = "Royendgel Silberie",
+    author_email = "royendgel@techprocur.com",
+    description = "Quick and easy Implementation of revolution slider",
+    keywords='revolution slider, django, django-cms',
+    url = "https://github.com/royendgel",
+    packages=['revolutionslider'],
+    classifiers=[
+      'Operating System :: MacOS :: MacOS X',
+      'Operating System :: Unix',
+      'Development Status :: 3 - Alpha',
+      'Topic :: Utilities',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+    ],
+    include_package_data=True,
+)
