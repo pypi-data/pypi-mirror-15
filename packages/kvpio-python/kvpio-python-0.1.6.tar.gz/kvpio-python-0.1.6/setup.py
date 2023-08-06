70124f32c82d2781d478ca4c75ef0596fcc8cb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:41 +0000

    Add package kvpio-python/0.1.6/kvpio-python-0.1.6.tar.gz

---
+++ packages/kvpio-python/kvpio-python-0.1.6.tar.gz/kvpio-python-0.1.6/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='kvpio-python',
+    version='0.1.6',
+    author='William Palmer',
+    author_email='will@steelhive.com',
+    description='Official kvp.io python bindings and client',
+    long_description=long_description,
+    url='https://www.kvp.io',
+    include_package_data=True,
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'click'
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov'
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    entry_points={
+        'console_scripts': [
+            'kvpio = kvpio.cli:cli',
+        ]
+    }
+)
