8e44b94ee7acb15d285da8aff3120888d9a63fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:58 +0000

    Add package qifparser/0.6/qifparser-0.6.tar.gz

---
+++ packages/qifparser/qifparser-0.6.tar.gz/qifparser-0.6/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.6'
+long_description = open("README.rst").read() + "\n" + \
+    open("CHANGELOG.rst").read()
+
+setup(name='qifparser',
+      version=version,
+      description="a parser for Quicken interchange format files (.qif).",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.5",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.2",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Utilities",
+      ],
+      keywords='qif, Quicken interchange format, file format',
+      author='Stanislaw Bogatkin, Giacomo Spettoli',
+      author_email='sbog@sbog.ru',
+      url='https://github.com/sorrowless/qifparse.git',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='qifparse',
+      install_requires=[
+          'setuptools',
+          'six',
+      ],
+      entry_points="""
+      """,
+      )
