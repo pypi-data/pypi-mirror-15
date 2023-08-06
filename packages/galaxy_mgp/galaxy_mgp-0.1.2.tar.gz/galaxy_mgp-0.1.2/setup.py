fa4a296f95b61106b1679d1237162845104812f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:00 +0000

    Add package galaxy_mgp/0.1.2/galaxy_mgp-0.1.2.tar.gz

---
+++ packages/galaxy_mgp/galaxy_mgp-0.1.2.tar.gz/galaxy_mgp-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='galaxy_mgp',
+      version='0.1.2',
+      description='The plathora distanced',
+      url='http://github.com/mgpradeepa/py_pip',
+      author='MGP',
+      author_email='mgpradeepa@gmail.com',
+      license='',
+      packages=['galaxy_mgp'],
+      zip_safe=False)
