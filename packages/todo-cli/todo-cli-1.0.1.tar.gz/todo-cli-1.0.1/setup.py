3a09da9c583303faa352e2ad182bfd6d58a538f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:30 +0000

    Add package todo-cli/1.0.1/todo-cli-1.0.1.tar.gz

---
+++ packages/todo-cli/todo-cli-1.0.1.tar.gz/todo-cli-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='todo-cli',
+    version='1.0.1',
+    description='Command line tool to manage To-Do lists',
+    keywords='todo list task productivity project management',
+    author='Francois Chalifour',
+    author_email='francois.chalifour@gmail.com',
+    url='https://github.com/francoischalifour/todo-cli',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'todo=todo:main'
+        ]
+    }
+)
