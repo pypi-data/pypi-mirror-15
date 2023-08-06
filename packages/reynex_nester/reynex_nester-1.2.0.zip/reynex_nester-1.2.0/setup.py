e9755c0b0ef7b503b6e75b754d9d2dd806be537b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:27 +0000

    Add package reynex_nester/1.2.0/reynex_nester-1.2.0.zip

---
+++ packages/reynex_nester/reynex_nester-1.2.0.zip/reynex_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'reynex_nester',
+        version = '1.2.0',
+        py_modules = ['nester'],
+        author = 'reynald',
+        author_email = 'reynex1127@yahoo.com',
+        url = 'http://www.reynex1127.com',
+        description = 'A simple printer of nested lists',
+    )
