b7b95267dccebfe187ed9a561749fb551b5f1f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:16 +0000

    Add package comiccrawler/2016.6.3/comiccrawler-2016.6.3-py3-none-any.whl

---
+++ packages/comiccrawler/comiccrawler-2016.6.3-py3-none-any.whl/comiccrawler/core/grabber.py
@@ -0,0 +1,131 @@
+#! python3
+
+import gzip
+import re
+import requests
+
+from urllib.parse import quote, urlsplit, urlunsplit
+from urllib.request import Request, urlopen
+from mimetypes import guess_extension
+
+from http.cookies import SimpleCookie
+
+from requests.exceptions import HTTPError
+
+from worker import sync, sleep
+from pprint import pformat
+
+from ..config import setting
+from ..io import content_write
+
+default_header = {
+	"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
+	"Accept-Language": "zh-tw,zh;q=0.8,en-us;q=0.5,en;q=0.3",
+	"Accept-Encoding": "gzip, deflate"
+}
+
+def quote_unicode(s):
+	"""Quote unicode characters only."""
+	return quote(s, safe=r"/ !\"#$%&'()*+,:;<=>?@[\\]^`{|}~")
+
+def quote_loosely(s):
+	"""Quote space and others in path part.
+
+	Reference:
+	  http://stackoverflow.com/questions/120951/how-can-i-normalize-a-url-in-python
+	"""
+	return quote(s, safe="%/:=&?~#+!$,;'@()*[]")
+
+def safeurl(url):
+	"""Return a safe url, quote the unicode characters.
+
+	This function should follow this rule:
+	  safeurl(safeurl(url)) == safe(url)
+	"""
+	scheme, netloc, path, query, fragment = urlsplit(url)
+	return urlunsplit((scheme, netloc, quote_loosely(path), query, ""))
+
+def quote_unicode_dict(d):
+	"""Return a safe header, quote the unicode characters."""
+	for key, value in d.items():
+		d[key] = quote_unicode(value)
+	
+def grabber_log(*args):
+	content_write("~/comiccrawler/grabber.log", pformat(args) + "\n\n", append=True)
+
+sessions = {}
+def grabber(url, header=None, *, referer=None, cookie=None):
+	"""Request url, return text or bytes of the content."""
+	scheme, netloc, path, query, frag = urlsplit(url)
+	
+	if netloc not in sessions:
+		s = requests.Session()
+		s.headers.update(default_header)
+		sessions[netloc] = s
+	else:
+		s = sessions[netloc]
+		
+	if header:
+		s.headers.update(header)
+
+	if referer:
+		s.headers['referer'] = referer
+
+	if cookie:
+		quote_unicode_dict(cookie)
+		requests.utils.add_dict_to_cookiejar(s.cookies, cookie)
+
+	while True:
+		r = s.get(url, timeout=20)
+
+		if setting.getboolean("errorlog"):
+			grabber_log(url, r.request.headers, r.headers)
+
+		if r.status_code == 200:
+			break
+			
+		if r.status_code == 429:
+			retry = 20
+			if "retry-after" in r.headers:
+				retry2 = int(r.headers['retry-after'])
+				if retry2 < retry:
+					retry = retry2
+			sleep(retry)
+		else:
+			r.raise_for_status()
+			
+	return r
+			
+	b = r.content
+
+	if raw:
+		return b
+		
+	match = re.search(br"charset=[\"']?([^\"'>]+)", b)
+	if match:
+		r.encoding = match.group(1).decode("latin-1")
+		
+	return r.text
+
+def grabhtml(*args, **kwargs):
+	"""Get html source of given url. Return String."""
+	r = sync(grabber, *args, **kwargs)
+	
+	# decode to text
+	match = re.search(br"charset=[\"']?([^\"'>]+)", r.content)
+	if match:
+		r.encoding = match.group(1).decode("latin-1")
+		
+	return r.text
+
+def grabimg(*args, **kwargs):
+	"""Return byte array."""
+	r = sync(grabber, *args, **kwargs)
+	
+	# find extension
+	ext = None
+	if "Content-Type" in r.headers:
+		mime = re.search("^(.*?)(;|$)", r.headers["Content-Type"]).group(1)
+		mime = mime.strip()
+		ext = guess_extension(mime)
+	return ext, r.content
---
+++ packages/comiccrawler/comiccrawler-2016.6.3-py3-none-any.whl/comiccrawler/mission_manager.py
@@ -0,0 +1,230 @@
+#! python3
+
+"""Mission Manager"""
+
+import json, traceback
+
+from collections import OrderedDict
+from worker import current
+from threading import Lock
+from contextlib import suppress
+
+from .safeprint import print
+from .core import Mission, Episode, MissionProxy
+from .io import content_read, content_write, is_file, backup, open
+
+from .channel import mission_ch
+
+class MissionPoolEncoder(json.JSONEncoder):
+	"""Encode Mission, Episode to json."""
+
+	def default(self, object):
+		if hasattr(object, "tojson"):
+			return object.tojson()
+
+		return vars(object)
+
+class MissionManager:
+	"""Since check_update thread might grab mission from mission_manager, we have to make it thread safe."""
+	def __init__(self):
+		"""Construct."""
+		self.pool = {}
+		self.view = OrderedDict()
+		self.library = OrderedDict()
+		self.edit = False
+		self.lock = Lock()
+		
+		self.load()
+
+		thread = current()
+		mission_ch.sub(thread)
+		@thread.listen("MISSION_PROPERTY_CHANGED")
+		def _(event):
+			"""Set the edit flag after mission changed."""
+			self.edit = True
+
+	def cleanup(self):
+		"""Cleanup unused missions"""
+		main_pool = set(self.pool)
+		view_pool = set(self.view)
+		library_pool = set(self.library)
+
+		for url in main_pool - (view_pool | library_pool):
+			del self.pool[url]
+
+	def save(self):
+		"""Save missions to json."""
+		if not self.edit:
+			return
+
+		with open("~/comiccrawler/pool.json", "w") as fp:
+			json.dump(
+				list(self.pool.values()),
+				fp,
+				cls=MissionPoolEncoder,
+				indent=4,
+				ensure_ascii=False
+			)
+		
+		with open("~/comiccrawler/view.json", "w") as fp:
+			json.dump(
+				list(self.view),
+				fp,
+				indent=4,
+				ensure_ascii=False
+			)
+			
+		with open("~/comiccrawler/library.json", "w") as fp:
+			json.dump(
+				list(self.library),
+				fp,
+				indent=4,
+				ensure_ascii=False
+			)
+			
+		self.edit = False
+		print("Session saved")
+
+	def load(self):
+		"""Load mission from json.
+
+		If it fail to load missions, create json backup in
+		`~/comiccrawler/invalid-save`.
+		"""
+		try:
+			self._load()
+		except Exception as err:
+			print("Failed to load session!")
+			# traceback.print_exc()
+			backup("~/comiccrawler/*.json")
+			raise
+			# mission_ch.pub("MISSION_POOL_LOAD_FAILED", err)
+		self.cleanup()
+		# mission_ch.pub("MISSION_POOL_LOAD_SUCCESS")
+
+	def _load(self):
+		"""Load missions from json. Called by MissionManager.load."""
+		pool = []
+		view = []
+		library = []
+
+		with suppress(OSError):
+			with open("~/comiccrawler/pool.json") as fp:
+				pool = json.load(fp)
+
+		with suppress(OSError):
+			with open("~/comiccrawler/view.json") as fp:
+				view = json.load(fp)
+
+		with suppress(OSError):
+			with open("~/comiccrawler/library.json") as fp:
+				library = json.load(fp)
+
+		for m_data in pool:
+			# reset state
+			if m_data["state"] in ("DOWNLOADING", "ANALYZING"):
+				m_data["state"] = "ERROR"
+			# build episodes
+			episodes = []
+			for ep_data in m_data["episodes"]:
+				# compatible 2916.4.3
+				if "total" not in ep_data:
+					if not ep_data["current_url"]:
+						ep_data["total"] = 0
+						
+					elif ep_data["url"] == ep_data["current_url"]:
+						# first page crawler
+						ep_data["total"] = ep_data["current_page"] - 1
+						
+					else:
+						# per page crawler
+						ep_data["total"] = ep_data["current_page"] - 1
+						ep_data["current_page"] = 1
+					
+					if ep_data["complete"]:
+						ep_data["total"] += 1
+						
+				episodes.append(Episode(**ep_data))
+			m_data["episodes"] = episodes
+			mission = MissionProxy(Mission(**m_data))
+			# self._add(mission)
+			self.pool[mission.url] = mission
+
+		for url in view:
+			self.view[url] = self.pool[url]
+
+		for url in library:
+			self.library[url] = self.pool[url]
+
+		mission_ch.pub("MISSION_LIST_REARRANGED", self.view)
+		mission_ch.pub("MISSION_LIST_REARRANGED", self.library)
+
+	def add(self, pool_name, *missions):
+		"""Add missions to pool."""
+		pool = getattr(self, pool_name)
+
+		with self.lock:
+			for mission in missions:
+				if mission.url not in self.pool:
+					mission_ch.pub("MISSION_ADDED", mission)
+				self.pool[mission.url] = mission					
+				pool[mission.url] = mission
+		
+		mission_ch.pub("MISSION_LIST_REARRANGED", pool)
+		self.edit = True
+
+	def remove(self, pool_name, *missions):
+		"""Remove missions from pool."""
+		pool = getattr(self, pool_name)
+
+		# check mission state
+		missions = [m for m in missions if m.state not in ("ANALYZING", "DOWNLOADING")]
+
+		with self.lock:
+			for mission in missions:
+				del pool[mission.url]
+			self.cleanup()
+			
+		mission_ch.pub("MISSION_LIST_REARRANGED", pool)
+		self.edit = True
+
+	def lift(self, pool_name, *missions):
+		"""Lift missions to the top."""
+		pool = getattr(self, pool_name)
+		with self.lock:
+			for mission in reversed(missions):
+				pool.move_to_end(mission.url, last=False)
+		mission_ch.pub("MISSION_LIST_REARRANGED", pool)
+		self.edit = True
+
+	def drop(self, pool_name, *missions):
+		"""Drop missions to the bottom."""
+		pool = getattr(self, pool_name)
+		with self.lock:
+			for mission in missions:
+				pool.move_to_end(mission.url)
+		mission_ch.pub("MISSION_LIST_REARRANGED", pool)
+		self.edit = True
+
+	def get_by_state(self, pool_name, states, all=False):
+		"""Get missions by states."""
+		with self.lock:
+			if not all:
+				for mission in getattr(self, pool_name).values():
+					if mission.state in states:
+						return mission
+				return None
+			else:
+				output = []
+				for mission in getattr(self, pool_name).values():
+					if mission.state in states:
+						output.append(mission)
+				return output
+
+	def get_by_url(self, url, pool_name=None):
+		"""Get mission by url."""
+		if not pool_name:
+			return self.pool[url]
+		return getattr(self, pool_name)[url]
+
+mission_manager = MissionManager()
