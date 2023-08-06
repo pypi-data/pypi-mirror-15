8d6a8aaba357520fe4dc69894efa54deefa63481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:30 +0000

    Add package alcohol_consumption/1.0.0/alcohol_consumption-1.0.0.zip

---
+++ packages/alcohol_consumption/alcohol_consumption-1.0.0.zip/alcohol_consumption-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'alcohol_consumption',
+    version     = '1.0.0',
+    py_modules  = ['alcohol_consumption'],
+    author      = 'Leslie Yang',
+    author_email= 'leslie.h.yang@gmail.com',
+    url         = 'http://www.headfirstlabs.com',
+    description = 'A sample to analyze the alcohol consumption.'
+    )
