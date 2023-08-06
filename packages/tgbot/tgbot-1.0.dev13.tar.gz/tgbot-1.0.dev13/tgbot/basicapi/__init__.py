c2dff253bbc131b384af38eae7daa2163e58a128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:24 +0000

    Add package tgbot/1.0.dev13/tgbot-1.0.dev13.tar.gz

---
+++ packages/tgbot/tgbot-1.0.dev13.tar.gz/tgbot-1.0.dev13/tgbot/basicapi/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Thomas'
+
+
+from tgbot.basicapi.model.message import Message
+from tgbot.basicapi.model.inlineQuery import InlineQuery
+from tgbot.basicapi.model.callbackQuery import CallBackQuery
+from tgbot.tglogging import logger
+from tgbot.basicapi.parser import parsemessage,parseinline,parsecallbackquery
+import tgbot
+import gettext
+from tgbot.database.language import LanguageGetterAbstract
+
+
+def activatebot(data,wartungsmodus,commands,conversationmethods,callbackclasses,inlinecommands,conversation,language):
+
+    logger.debug("Message arrived.\nMessage: " + str(data))
+    if "message" in data:
+        message = Message(data=data["message"])
+        #tgbot.mainmessage = message
+        parsemessage(message,commands,conversationmethods,wartungsmodus,conversation)
+    elif "callback_query" in data:
+        callback = CallBackQuery(data=data["callback_query"])
+        parsecallbackquery(callback,callbackclasses)
+    elif "inline_query" in data:
+        inline = InlineQuery(data=data["inline_query"])
+        parseinline(inline,inlinecommands)
+        logger.debug("IN INLINE QUERY")
+
