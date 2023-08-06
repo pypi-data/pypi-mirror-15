9f11722921e1bf2aff85db41c03cd018b7487ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:31 +0000

    Add package labelprop/0.1.2/labelprop-0.1.2.tar.gz

---
+++ packages/labelprop/labelprop-0.1.2.tar.gz/labelprop-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import Extension,find_packages
+from os import path
+
+setup(
+    name = 'labelprop',
+    version = '0.1.2',
+    description = 'Python implementation of label propagation',
+    author = 'Lingzhe Teng',
+    author_email = 'zwein27@gmail.com',
+    url = 'https://github.com/ZwEin27/python-labelpropagation',
+    download_url = 'https://github.com/ZwEin27/python-labelpropagation/tarball/0.1.2',
+    packages = find_packages(),
+    keywords = ['label propagation', 'label', 'propagation', 'labelpropagation']
+)
