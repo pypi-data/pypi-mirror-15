b61229f4b54542593880bb06abf1e1bd85e028e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:12 +0000

    Add package ChatterBot/0.4.0/ChatterBot-0.4.0.tar.gz

---
+++ packages/ChatterBot/ChatterBot-0.4.0.tar.gz/ChatterBot-0.4.0/chatterbot/adapters/output/__init__.py
@@ -0,0 +1,5 @@
+from .output_adapter import OutputAdapter
+from .terminal import TerminalAdapter
+from .output_format_adapter import OutputFormatAdapter
+from .multi_adapter import MultiOutputAdapter
+from .mailgun import Mailgun
