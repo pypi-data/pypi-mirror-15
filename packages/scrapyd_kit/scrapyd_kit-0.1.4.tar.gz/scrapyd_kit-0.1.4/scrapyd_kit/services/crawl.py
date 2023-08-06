ba2bfa2f05a57a35d8c697084fb784172720ad67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:28 +0000

    Add package scrapyd_kit/0.1.4/scrapyd_kit-0.1.4.tar.gz

---
+++ packages/scrapyd_kit/scrapyd_kit-0.1.4.tar.gz/scrapyd_kit-0.1.4/scrapyd_kit/services/crawl.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import os
+import json
+from scrapyd.webservice import WsResource
+from subprocess import Popen, PIPE
+from scrapyd.utils import get_crawl_args
+
+class Crawl(WsResource):
+
+    def jsonize(self, request, data):
+        try:
+            data = json.loads(data)
+            return {'code': 0, 'message': 'success', 'data': data}
+        except ValueError as e:
+            request.setResponseCode(500)
+            return {'code': 1, 'message': 'json parse error', 'errors': data}
+
+    def crawl(self, args):
+        env = os.environ.copy()
+        cmd = ['scrapy', 'crawl']
+        args['_spider'] = args.get('spider')
+        args['_project'] = args.get('project')
+        cargs = get_crawl_args(args)
+        cmd = cmd + cargs + ['-t', 'json', '-o', '-']
+        process = Popen(cmd, stdin=PIPE, stdout=PIPE, env=env)
+        out, _ = process.communicate()
+        return out
+
+    def render_POST(self, request):
+        settings = request.args.pop('setting', [])
+        settings = dict(x.split('=', 1) for x in settings)
+        args = dict((k, v[0]) for k, v in request.args.items())
+        out = self.crawl(args)
+        return self.jsonize(request, out)
