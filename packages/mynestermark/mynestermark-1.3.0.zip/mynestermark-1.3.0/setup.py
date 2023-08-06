a71e29482b00351b175b6a7e6243507d2ba1ca06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:33 +0000

    Add package mynestermark/1.3.0/mynestermark-1.3.0.zip

---
+++ packages/mynestermark/mynestermark-1.3.0.zip/mynestermark-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name                = 'mynestermark',
+    version             = '1.3.0',
+    py_modules          = ['mynestermark'],
+    author              = 'Mark',
+    author_email        = 'markchienjung@gmail.com',
+    description         = 'A simple printer of nested lists',
+    )
