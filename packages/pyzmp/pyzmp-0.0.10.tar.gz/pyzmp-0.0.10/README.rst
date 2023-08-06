3d3a5615aeac270f9cd2bf4abb1c7ee1a07be4e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:43 +0000

    Add package pyzmp/0.0.10/pyzmp-0.0.10.tar.gz

---
+++ packages/pyzmp/pyzmp-0.0.10.tar.gz/pyzmp-0.0.10/README.rst
@@ -0,0 +1,28 @@
+PyZMP
+=====
+.. image:: https://travis-ci.org/asmodehn/pyzmp.svg?branch=master
+    :target: https://travis-ci.org/asmodehn/pyzmp
+
+.. image:: https://requires.io/github/asmodehn/pyzmp/requirements.svg?branch=master
+     :target: https://requires.io/github/asmodehn/pyzmp/requirements/?branch=master
+     :alt: Requirements Status
+
+.. image:: https://landscape.io/github/asmodehn/pyzmp/master/landscape.svg?style=flat
+   :target: https://landscape.io/github/asmodehn/pyzmp/master
+   :alt: Code Health
+
+.. image:: https://www.quantifiedcode.com/api/v1/project/6e2a3dc5e5b142e9b7db86d0dcf1be3c/badge.svg
+  :target: https://www.quantifiedcode.com/app/project/6e2a3dc5e5b142e9b7db86d0dcf1be3c
+  :alt: Code issues
+
+PyZMP is a multiprocess library based on ZeroMQ.
+
+Install
+```
+python setup.py develop
+```
+
+Run tests with tox
+```
+tox
+```
---
+++ packages/pyzmp/pyzmp-0.0.10.tar.gz/pyzmp-0.0.10/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open('pyzmp/_version.py') as vf:
+    exec(vf.read())
+
+setup(name='pyzmp',
+    version=__version__,
+    description='ZeroMq based multiprocessing framework.',
+    url='http://github.com/asmodehn/pyzmp',
+    author='AlexV',
+    author_email='asmodehn@gmail.com',
+    license='BSD',
+    packages=[
+        'pyzmp',
+        'pyzmp.tests'
+    ],
+    # this is better than using package data ( since behavior is a bit different from distutils... )
+    include_package_data=True,  # use MANIFEST.in during install.
+    install_requires=[
+        'tblib',  # this might not always install six (latest version does not)
+        'six',
+        'pyzmq',
+        'pytest',  # since tests are embedded in package
+        'pytest-timeout'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    zip_safe=False,  # TODO testing...
+)
