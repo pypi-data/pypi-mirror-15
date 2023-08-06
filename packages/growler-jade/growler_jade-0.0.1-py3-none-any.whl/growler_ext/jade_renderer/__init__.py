68e499cc91f4ab121a5c4e57f93b99991591342e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:17 +0000

    Add package growler-jade/0.0.1/growler_jade-0.0.1-py3-none-any.whl

---
+++ packages/growler-jade/growler_jade-0.0.1-py3-none-any.whl/growler_ext/jade_renderer/__init__.py
@@ -0,0 +1,11 @@
+#
+# growler_ext/jade_renderer.py
+#
+"""
+Loader script for the growler_jade package.
+"""
+
+import sys
+import growler_jade
+
+sys.modules[__name__] = growler_jade
---
+++ packages/growler-jade/growler_jade-0.0.1-py3-none-any.whl/growler_jade/__init__.py
@@ -0,0 +1,15 @@
+#
+# growler_jade/__init__.py
+#
+"""
+Jade renderer package for the Growler framework
+"""
+
+from .__meta__ import (
+    version as __version__,
+    date as __date__,
+    author as __author__,
+    license as __license__,
+)
+
+from .jade_renderer import JadeRenderer
