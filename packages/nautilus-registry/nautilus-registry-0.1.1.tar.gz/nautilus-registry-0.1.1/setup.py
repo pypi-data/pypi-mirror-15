f1a18f9c11fe7b45a183392d5a1797ecfeb226cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:43 +0000

    Add package nautilus-registry/0.1.1/nautilus-registry-0.1.1.tar.gz

---
+++ packages/nautilus-registry/nautilus-registry-0.1.1.tar.gz/nautilus-registry-0.1.1/setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+setup(
+    name='nautilus-registry',
+    version='0.1.1',
+    description='A module for powering a nautilus application with a service registry.',
+    author='Alec Aivazis',
+    author_email='alec@aivazis.com',
+    url='https://github.com/AlecAivazis/nautilus-registry',
+    download_url='https://github.com/aaivazis/nautilus-registry/tarball/0.1.0',
+    keywords=['microservice', 'asyncio', 'graphql'],
+    test_suite='nose2.collector.collector',
+    packages=find_packages(exclude=['example', 'tests']),
+    include_package_data=True,
+    entry_points={'console_scripts': [
+        'naut = nautilus.management:cli',
+    ]},
+    install_requires=[
+        'nautilus',
+        'consul',
+        'graphene',
+        'requests',
+    ]
+)
