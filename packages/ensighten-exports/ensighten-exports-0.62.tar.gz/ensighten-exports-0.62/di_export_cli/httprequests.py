109515c1c7f7d1ded5a8293cc079a525b9f8857f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:05 +0000

    Add package ensighten-exports/0.62/ensighten-exports-0.62.tar.gz

---
+++ packages/ensighten-exports/ensighten-exports-0.62.tar.gz/ensighten-exports-0.62/di_export_cli/httprequests.py
@@ -0,0 +1,21 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import urllib3.contrib.pyopenssl
+from requests.packages.urllib3.util import Retry
+from requests.adapters import HTTPAdapter
+
+urllib3.contrib.pyopenssl.inject_into_urllib3()
+
+
+class HttpRequest:
+
+    def __init__(self, api_url):
+        self.url = api_url
+
+    def get_auth_token(self, username, password):
+        s = requests.Session()
+        retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+        s.mount('https://', HTTPAdapter(max_retries=retries))
+        resp = s.get(self.url + '/api/latest/auth', auth=HTTPBasicAuth(username, password))
+        resp.raise_for_status()
+        return resp
