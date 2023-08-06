ad5c26688915a106d1de9ffb2c176ee6c14a9d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:48 +0000

    Add package thrift3babeltrace/0.2.1/thrift3babeltrace-0.2.1.tar.gz

---
+++ packages/thrift3babeltrace/thrift3babeltrace-0.2.1.tar.gz/thrift3babeltrace-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name= 'thrift3babeltrace',
+	packages = ['thrift3babeltrace'],
+	version = '0.2.1',
+	description = 'Apache Thrift with modifications for compatibility for Python 3',
+	author = 'Victor Araujo',
+	author_email = 've.ar91@gmail.com',
+	url = 'https://github.com/vears91/thrift3-binary-protocol',
+	download_url = 'https://github.com/vears91/thrift3-binary-protocol/tarball/0.2.1',
+	keywords = ['thrift', 'python3', 'binary protocol', 'thrift3', 'babeltrace'],
+	classifiers = []
+)
