38a754a23f811fa827cf5a301cc3f31b36a0548d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:12 +0000

    Add package Flask-Maple/0.2.8/Flask-Maple-0.2.8.tar.gz

---
+++ packages/Flask-Maple/Flask-Maple-0.2.8.tar.gz/Flask-Maple-0.2.8/flask_maple/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding=UTF-8 -*-
+# **************************************************************************
+# Copyright © 2016 jianglin
+# File Name: __init__.py
+# Author: jianglin
+# Email: xiyang0807@gmail.com
+# Created: 2016-04-18 19:04:49 (CST)
+# Last Update:星期三 2016-5-18 17:0:57 (CST)
+#          By: jianglin
+# Description:
+# **************************************************************************
+from .captcha import MapleCaptcha
+from .bootstrap import MapleBootstrap
+from .auth import Auth
+from .error import Error
