fb1b9fb7c5b3d7099f5c02c7e8f14e3dd42591e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:31 +0000

    Add package psyice/1.1.0/psyice-1.1.0.tar.gz

---
+++ packages/psyice/psyice-1.1.0.tar.gz/psyice-1.1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name        = 'psyice',
+    version     = '1.1.0',
+    py_modules  = ['psyice'],
+    author      = 'psyice',
+    author_email= 'synthetic_ice@yahoo.com',
+    url         = 'http://siceprd.top',
+    description = 'psyice\'s personal tool set'
+)
