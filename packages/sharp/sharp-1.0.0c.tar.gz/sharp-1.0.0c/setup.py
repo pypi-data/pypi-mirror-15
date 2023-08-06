895980f3a8e13f3f2bf53d81ba418e5e402d0124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:45 +0000

    Add package sharp/1.0.0c/sharp-1.0.0c.tar.gz

---
+++ packages/sharp/sharp-1.0.0c.tar.gz/sharp-1.0.0c/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(
+	name="sharp",
+	version="1.0.0c",
+	author="Sigurd Naess",
+	author_email="sigurdkn@astro.uio.no",
+	description=("Wrapper for the libsharp spherical harmonics transofrm library"),
+	long_description=("A python wrapper of the libsharp spherical harmonics "
+		"transform library, from before libsharp got its own wrapper. This "
+		"version used to be more general, but that may no longer be the case."),
+	download_url = 'https://github.com/amaurea/sharp/tarball/1.0.0c',
+	license = "CC0",
+	keywords = "science spherical harmonics transform",
+	url = "http://packages.python.org/sharp",
+	classifiers=[
+		"Topic :: Scientific/Engineering",
+		"License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+	],
+	ext_modules = [
+		Extension(
+			name="sharp",
+			sources=["sharp.c"],
+			libraries=["sharp","c_utils","fftpack"],
+			include_dir=["."],
+			extra_link_args = ["-fopenmp"],
+		)
+	]
+)
