14d5059c4d64baa82d1f7b1887ffd736f59e6e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:53 +0000

    Add package quokka-flask-admin/1.4.2.1/quokka-flask-admin-1.4.2.1.tar.gz

---
+++ packages/quokka-flask-admin/quokka-flask-admin-1.4.2.1.tar.gz/quokka-flask-admin-1.4.2.1/setup.py
@@ -0,0 +1,79 @@
+# Fix for older setuptools
+import re
+import os
+
+from setuptools import setup, find_packages
+
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(fname):
+    return open(fpath(fname)).read()
+
+
+def desc():
+    info = read('README.rst')
+    try:
+        return info + '\n\n' + read('doc/changelog.rst')
+    except IOError:
+        return info
+
+# grep flask_admin/__init__.py since python 3.x cannot import it before using 2to3
+file_text = read(fpath('flask_admin/__init__.py'))
+
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, file_text)
+    return strval
+
+
+setup(
+    name='quokka-flask-admin',
+    version='1.4.2.1',
+    url='https://github.com/quokkaproject/flask-admin/',
+    license='BSD',
+    author='quokkaproject',
+    author_email='rochacbruno+quokka@gmail.com',
+    description='Fork of Flask-Admin for QuokkaCMS',
+    long_description=desc(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.7',
+        'wtforms'
+    ],
+    tests_require=[
+        'nose>=1.0',
+        'pillow==2.9.0',
+        'mongoengine',
+        'pymongo',
+        'wtf-peewee',
+        'sqlalchemy',
+        'flask-mongoengine',
+        'flask-sqlalchemy',
+        'flask-babelex',
+        'shapely',
+        'geoalchemy2',
+        'psycopg2',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='nose.collector'
+)
