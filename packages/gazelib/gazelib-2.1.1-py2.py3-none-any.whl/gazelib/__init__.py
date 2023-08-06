a7ace1d951ac146d84ff163800d822754503fa77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:15 +0000

    Add package gazelib/2.1.1/gazelib-2.1.1-py2.py3-none-any.whl

---
+++ packages/gazelib/gazelib-2.1.1-py2.py3-none-any.whl/gazelib/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Gazelib, a toolkit for gaze analysis.
+'''
+from gazelib import package
+__version__ = package.version  # noqa
+
+from gazelib import containers  # noqa
+from gazelib import io  # noqa
+from gazelib import legacy  # noqa
+# import gazelib.settings  # noqa
+from gazelib import statistics  # noqa
+# import gazelib.validation  # noqa
+# from . import conversion  # Must be imported separately
---
+++ packages/gazelib/gazelib-2.1.1-py2.py3-none-any.whl/gazelib/statistics/__init__.py
@@ -0,0 +1,5 @@
+'''
+Statistical tools and methods.
+'''
+from .utils import (arithmetic_mean, weighted_arithmetic_mean, deltas,  # noqa
+                    maximum, minimum)
