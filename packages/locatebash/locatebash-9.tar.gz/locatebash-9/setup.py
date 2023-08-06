2616dea0aaa647001098777124d0e0fb7617d0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:46 +0000

    Add package locatebash/9/locatebash-9.tar.gz

---
+++ packages/locatebash/locatebash-9.tar.gz/locatebash-9/setup.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+"""
+locatebash
+-
+Active8 (04-03-15)
+author: erik@a8.nl
+license: GNU-GPL2
+"""
+from setuptools import setup
+setup(name='locatebash',
+      version='9',
+      description='Combines mdfind and locate',
+      url='https://github.com/erikdejonge/locatebash',
+      author='Erik de Jonge',
+      author_email='erik@a8.nl',
+      license='GPL',
+      entry_points={
+          'console_scripts': [
+              'loc=locatebash:main',
+          ],
+      },
+      packages=['locatebash'],
+      zip_safe=True,
+      install_requires=['consoleprinter', 'arguments', 'fuzzywuzzy', 'argparse', 'pyzmail', 'cmdssh'],
+      classifiers=[
+          "Programming Language :: Python :: 2",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+          "Operating System :: POSIX",
+          "Environment :: MacOS X",
+          "Topic :: System",
+      ])
