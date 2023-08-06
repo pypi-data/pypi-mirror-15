2153d81322c13677e193fce4f6c9f8e6fb250a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:53 +0000

    Add package AccelBrainBeat/0.0.3/AccelBrainBeat-0.0.3.zip

---
+++ packages/AccelBrainBeat/AccelBrainBeat-0.0.3.zip/AccelBrainBeat-0.0.3/setup.py
@@ -0,0 +1,27 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='AccelBrainBeat',
+    version='0.0.3',
+    description='This python script enables you to handle your mind state by a kind of "Brain-Wave Controller" which is generally known as Biaural beat or Monaural beat in a simplified method.',
+    long_description='This python script enables you to handle your mind state by a kind of "Brain-Wave Controller" which is generally known as Biaural beat or Monaural beat in a simplified method.',
+    url='https://github.com/chimera0/Binaural-Beat-and-Monaural-Beat-with-python',
+    author='chimera0',
+    author_email='kimera0kimaira@gmail.com',
+    license='GPL2',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='binaural monaural beat brain wave',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['numpy', 'PyAudio'],
+)
