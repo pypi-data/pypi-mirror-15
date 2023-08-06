3f0d4eb3ba9a82bbfc3062c3cbe8ab92a281a765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:17 +0000

    Add package ENN/0.14/enn-0.14.tar.gz

---
+++ packages/ENN/enn-0.14.tar.gz/enn-0.14/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'enn',
+  packages = ['enn'],
+  package_dir = {'enn' : ''},
+  version = '0.14',
+  description = 'Python Implementation of the Extended Nearest Neighbor Algorithm (ENN)',
+  author = 'Timo Stoettner',
+  author_email = 'mail@timo-stoettner.de',
+  url = 'https://github.com/timo-stoettner/ENN',
+  download_url = 'https://github.com/timo-stoettner/ENN/tarball/0.14',
+  keywords = ['ENN', 'Extended Nearest Neighbor', 'Machine Learning', 'Data Mining', 'KNN'],
+  classifiers = [],
+)
