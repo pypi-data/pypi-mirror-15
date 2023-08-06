d60b8cdd822415a63aede91935e3e6e4eb7bcc89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:18 +0000

    Add package dj-json-env/0.1.2/dj-json-env-0.1.2.tar.gz

---
+++ packages/dj-json-env/dj-json-env-0.1.2.tar.gz/dj-json-env-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='dj-json-env',
+    version='0.1.2',
+    url='https://github.com/vijayendra/dj-json-env',
+    license='MIT',
+    author='Vijayendra Bapte',
+    author_email='vijayendra.bapte@gmail.com',
+    description='Use env to read local settings for django app',
+    py_modules=['dj_json_env'],
+    scripts=["scripts/export_json"],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
