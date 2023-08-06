77d526d2adc19178280a16ef94c423359a6d29f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:03 +0000

    Add package autograder/1.0.0/autograder-1.0.0.tar.gz

---
+++ packages/autograder/autograder-1.0.0.tar.gz/autograder-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'autograder',
+  packages = ['autograder'],
+  version = '1.0.0',
+  description = 'A testing helper for Apache Spark Databricks notebooks',
+  author = 'Cong Yue and contributors from Databricks',
+  author_email = 'training-logins@databricks.com',
+  keywords = ['testing', 'spark', 'databricks','autograder'],
+  classifiers = [],
+)
