899cabbc2a2919a79b20acd746f7978986c5f3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:19 +0000

    Add package ggplot/0.9.5/ggplot-0.9.5-py2-none-any.whl

---
+++ packages/ggplot/ggplot-0.9.5-py2-none-any.whl/ggplot/geoms/geom_line.py
@@ -0,0 +1,29 @@
+from .geom import geom
+
+class geom_line(geom):
+    is_path = False
+    DEFAULT_AES = {'color': 'black', 'alpha': 1.0, 'linetype': 'solid', 'size': 1.0}
+    REQUIRED_AES = {'x', 'y'}
+    DEFAULT_PARAMS = {}
+
+    _aes_renames = {'size': 'linewidth', 'linetype': 'linestyle'}
+
+    def plot(self, ax, data, _aes):
+        params = self._get_plot_args(data, _aes)
+        variables = _aes.data
+        x = data[variables['x']]
+        y = data[variables['y']]
+
+        nulls = (x.isnull() | y.isnull())
+        x = x[nulls==False]
+        y = y[nulls==False]
+
+        if self.is_path:
+            pass
+        else:
+            order = x.argsort()
+            x, y = x.iloc[order], y.iloc[order]
+
+        print("2=>")
+        print(zip(x.tolist(), y.tolist()))
+        ax.plot(x, y, **params)
