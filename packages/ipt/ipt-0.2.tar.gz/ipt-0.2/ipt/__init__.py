ada586c01beda65b8b1c7bf6b02e08f16d71ce0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:19 +0000

    Add package ipt/0.2/ipt-0.2.tar.gz

---
+++ packages/ipt/ipt-0.2.tar.gz/ipt-0.2/ipt/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+__init__.py file for ipt package
+Bryan S. Graham, UC - Berkeley
+bgraham@econ.berkeley.edu
+16 May 2016
+"""
+
+# Import the different functions into the package
+from logit import logit
+from att import att
