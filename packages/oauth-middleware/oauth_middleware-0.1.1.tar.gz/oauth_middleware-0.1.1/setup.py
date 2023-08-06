c3be508086625bbc3d15d896e9b43ab9cc1d24d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:09 +0000

    Add package oauth-middleware/0.1.1/oauth_middleware-0.1.1.tar.gz

---
+++ packages/oauth-middleware/oauth_middleware-0.1.1.tar.gz/oauth_middleware-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from __future__ import division, unicode_literals, print_function, absolute_import
+
+import textwrap
+
+from setuptools import setup, find_packages
+
+setup(
+    name='oauth_middleware',
+    version="0.1.1",  # TODO: Do not forget to update in __init__
+    description="Simple flask_oauthlib based middleware for WSGI app to preform oauth",
+    author="Last G",
+    author_email='sergei.azovskov@zalando.de',
+    url='https://github.com/last-g/oauth_middleware',
+    packages=find_packages(),
+    requires=[
+        'flask',
+        'flask_oauthlib',
+        'werkzeug',
+        'six'
+    ],
+    classifiers=textwrap.dedent("""
+        Development Status :: 3 - Alpha
+        Intended Audience :: Developers
+        License :: OSI Approved :: MIT License
+        Operating System :: OS Independent
+        Programming Language :: Python :: 2.7
+        Programming Language :: Python :: 3
+        Programming Language :: Python :: 3.3
+        Programming Language :: Python :: 3.4
+        Programming Language :: Python :: 3.5
+        Topic :: Software Development :: Libraries :: Python Modules
+        Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware
+    """).strip().splitlines(),
+
+)
