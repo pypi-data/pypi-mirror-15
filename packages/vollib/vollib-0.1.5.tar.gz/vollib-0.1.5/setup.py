9ccc836fcba9c0957a399df444de7e730c6aa638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:13 +0000

    Add package vollib/0.1.5/vollib-0.1.5.tar.gz

---
+++ packages/vollib/vollib-0.1.5.tar.gz/vollib-0.1.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='vollib',
+    version='0.1.5',
+    description='',
+    url='http://vollib.org',
+    # download_url='git+https://github.com/vollib/vollib.git#egg=vollib',
+    maintainer='vollib',
+    maintainer_email='support@quantycarlo.com',
+    license='MIT',
+    install_requires = [
+        'lets_be_rational',
+        'simplejson',
+        'numpy',
+        'pandas'
+    ],
+    packages=find_packages(exclude=['docs', 'tests'])
+)
