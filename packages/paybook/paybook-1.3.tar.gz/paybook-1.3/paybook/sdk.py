1b3848bffa3f2605acc0a8356b61c9cf4b5b8f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:10 +0000

    Add package paybook/1.3/paybook-1.3.tar.gz

---
+++ packages/paybook/paybook-1.3.tar.gz/paybook-1.3/paybook/sdk.py
@@ -0,0 +1,16 @@
+# -​*- coding: utf-8 -*​-
+
+from paybook.classes import *
+
+# Core classes:
+Error = paybook.Error
+Paybook = paybook.Paybook
+User = user.User
+Session = session.Session
+
+# Data classes
+Account = account.Account
+Attachment = attachment.Attachment
+Catalogues = catalogues.Catalogues
+Credentials = credentials.Credentials
+Transaction = transaction.Transaction
