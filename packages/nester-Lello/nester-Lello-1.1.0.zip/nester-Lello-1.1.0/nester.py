cf2540842c9145db250d4f450ff533f7f33e1e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:34 +0000

    Add package nester-Lello/1.1.0/nester-Lello-1.1.0.zip

---
+++ packages/nester-Lello/nester-Lello-1.1.0.zip/nester-Lello-1.1.0/nester.py
@@ -0,0 +1,10 @@
+"""这是我的第一个模块"""
+def print_lol(the_list,level):
+    """本函数是打印刘表the_list内的所有内容，包括列表中的列表"""
+    for each_list in the_list:
+        if isinstance(each_list,list):
+            print_lol(each_list,level+1)
+        else:
+            for tab_stop in range(level):
+                print('\t',end='')
+            print(each_list)
