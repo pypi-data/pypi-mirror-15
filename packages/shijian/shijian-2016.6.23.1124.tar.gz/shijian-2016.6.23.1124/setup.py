bc99009ce1a27071792d113145bb71a5216be321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:31 +0000

    Add package shijian/2016.6.23.1124/shijian-2016.6.23.1124.tar.gz

---
+++ packages/shijian/shijian-2016.6.23.1124.tar.gz/shijian-2016.6.23.1124/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "shijian",
+        version          = "2016.06.23.1124",
+        description      = "change and time utilities",
+        long_description = Markdown_to_reStructuredText("README.md"),
+        url              = "https://github.com/wdbm/shijian",
+        author           = "Will Breaden Madden",
+        author_email     = "w.bm@cern.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "shijian"
+                           ],
+        install_requires = [
+                           "pyprel"
+                           ],
+        entry_points     = """
+            [console_scripts]
+            shijian = shijian:shijian
+        """
+    )
+
+def read(*paths):
+    with open(os.path.join(*paths), "r") as filename:
+        return filename.read()
+
+def Markdown_to_reStructuredText(filename):
+    try:
+        import pypandoc
+        return pypandoc.convert(filename, "rst")
+    except:
+        print("pypandoc not found; long description could be corrupted")
+        return read(filename)
+
+if __name__ == "__main__":
+    main()
