4a1a8963a89f042d2e8f970e8b23b5c3d6d09bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:32 +0000

    Add package python-chaika/0.1.1/python-chaika-0.1.1.tar.gz

---
+++ packages/python-chaika/python-chaika-0.1.1.tar.gz/python-chaika-0.1.1/chaika.py
@@ -0,0 +1,27 @@
+import logging, json, datetime
+from logging.handlers import DatagramHandler
+
+
+class ChaikaHandler(DatagramHandler):
+
+  def __init__(self, host='localhost', port=2435, service='unnamed', logType=None):
+    super(ChaikaHandler, self).__init__(host, port)
+    self.service = service
+    self.logType = logType
+
+  def makePickle(self, record):
+    logTime = datetime.datetime.fromtimestamp(1462875627.825479).strftime('%Y-%m-%d %H:%M:%S.%f')
+    """ Format for log like this 
+    [2016-05-10 11:49:02.268] [DEBUG] [default] - Just a log for chaika-service, count: 1451
+    """
+    message = "[%s] [%s] [%s] - %s" % (logTime, record.levelname, record.name, record.msg + ' ' + " ".join(record.args))
+    
+    return bytes(json.dumps({
+      'time': logTime,
+      'logType': self.logType,
+      'message': message,
+      'level': record.levelname,
+      'service': self.service,
+      'catalog': '[%s]' % record.name
+    },'utf-8'))
+
---
+++ packages/python-chaika/python-chaika-0.1.1.tar.gz/python-chaika-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='python-chaika',
+    py_modules=['chaika'],
+    version='0.1.1',
+    description='Python logging handler for chaika.',
+    author='Thinh "Adolf Weismann" Tran <duythinht@gmail.com>',
+    author_email='duythinht@gmail.com',
+    url='https://github.com/duythinht/python-chaika',
+)
