79b8622ee18f2d1e9ce18a7ef14f17169b7a2c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:30 +0000

    Add package zeep/0.9.1/zeep-0.9.1-py2.py3-none-any.whl

---
+++ packages/zeep/zeep-0.9.1-py2.py3-none-any.whl/zeep/transports.py
@@ -0,0 +1,53 @@
+import requests
+from six.moves.urllib.parse import urlparse
+
+from zeep.cache import SqliteCache
+from zeep.utils import NotSet
+
+
+class Transport(object):
+
+    def __init__(self, cache=NotSet, timeout=300, verify=True, http_auth=None):
+        self.cache = SqliteCache() if cache is NotSet else cache
+        self.timeout = timeout
+        self.verify = verify
+        self.http_auth = http_auth
+
+        self.session = self.create_session()
+        self.session.verify = verify
+        self.session.auth = http_auth
+
+    def create_session(self):
+        return requests.Session()
+
+    def load(self, url):
+        scheme = urlparse(url).scheme
+        if scheme in ('http', 'https'):
+
+            if self.cache:
+                response = self.cache.get(url)
+                if response:
+                    return bytes(response)
+
+            response = self.session.get(url, timeout=self.timeout)
+            response.raise_for_status()
+
+            if self.cache:
+                self.cache.add(url, response.content)
+
+            return response.content
+
+        elif scheme == 'file':
+            if url.startswith('file://'):
+                url = url[7:]
+
+        with open(url, 'rb') as fh:
+            return fh.read()
+
+    def post(self, address, message, headers):
+        response = self.session.post(address, data=message, headers=headers)
+        return response
+
+    def get(self, address, params, headers):
+        response = self.session.get(address, params=params, headers=headers)
+        return response
