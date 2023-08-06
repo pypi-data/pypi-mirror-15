a98e4676250bdffa8d90d0e70f9d04eaa1bfbcd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:06 +0000

    Add package pigshare/0.6.4/pigshare-0.6.4.tar.gz

---
+++ packages/pigshare/pigshare-0.6.4.tar.gz/pigshare-0.6.4/pigshare/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Python client for figshare"""
+
+
+__version__ = '0.6.4'
+__url__ = 'https://github.com/makkus/pigshare'
+__author__ = 'Markus Binsteiner'
+__email__ = 'makkus@gmail.com'
