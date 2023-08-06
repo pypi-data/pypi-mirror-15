a49bda3ede3528f208007b6f6659a807f3e4e19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:16 +0000

    Add package sentry/8.6.0/sentry-8.6.0.tar.gz

---
+++ packages/sentry/sentry-8.6.0.tar.gz/sentry-8.6.0/src/sentry/logging/__init__.py
@@ -0,0 +1,13 @@
+"""
+sentry.logging
+~~~~~~~~~~~~~~
+:copyright: (c) 2010-2016 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import
+
+
+class LoggingFormat(object):
+    HUMAN = 'human'
+    MACHINE = 'machine'
