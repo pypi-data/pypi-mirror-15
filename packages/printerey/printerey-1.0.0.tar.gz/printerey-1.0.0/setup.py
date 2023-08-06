caba9d9fd204a0b71329700b216888f91868849f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:37 +0000

    Add package printerey/1.0.0/printerey-1.0.0.tar.gz

---
+++ packages/printerey/printerey-1.0.0.tar.gz/printerey-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'printerey', 
+        version      = '1.0.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['printerey'],
+        author       = 'theyemkay',
+        author_email = 'riyayemkay@gmail.com',
+        url          = 'http://www.google.com',
+        description  = 'A simple printer of nested lists',
+     )
