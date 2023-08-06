b5d5b6da8b34d35a24a5c2e156367386f0d31197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:15 +0000

    Add package FunctionalPython/0.0.3/FunctionalPython-0.0.3.zip

---
+++ packages/FunctionalPython/FunctionalPython-0.0.3.zip/FunctionalPython-0.0.3/FunctionalPython/src/lists/others.py
@@ -0,0 +1,8 @@
+"""
+Return the rest of the list (beyond the first element).
+If the input list is empty or has only one element, then return `[]`.
+
+Usage: others(list1)
+Author: Yuhang(Steven) Wang
+Date: 06/21/2016
+"""
