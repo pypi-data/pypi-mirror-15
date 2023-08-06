9117c1e278f40ff91851488bccd69d2e902875ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:09 +0000

    Add package nestereirol/1.3.0/nestereirol-1.3.0.tar.gz

---
+++ packages/nestereirol/nestereirol-1.3.0.tar.gz/nestereirol-1.3.0/nester.py
@@ -0,0 +1,23 @@
+"""
+Function that prints out every element of every list that is containted inside a list
+"""
+
+
+
+
+def print_lol(the_list,tabs=0,intend=False):
+    """recursively searches for list type items"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,intend,tabs + 1)
+        else:
+            if intend:
+                for tab in range(tabs): 
+                    print("\t", end='')
+
+            print(each_item)
+            
+        
+movies = [ "The Holy Grail", 1975, "terry Jones & terry Gilliam", 91,
+           ["Graham Chapman",
+            ["MIchael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
