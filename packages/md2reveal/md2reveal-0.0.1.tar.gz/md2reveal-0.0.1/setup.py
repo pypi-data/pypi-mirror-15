753e0c26eb90526491ce67bd8662de398e730925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:14 +0000

    Add package md2reveal/0.0.1/md2reveal-0.0.1.tar.gz

---
+++ packages/md2reveal/md2reveal-0.0.1.tar.gz/md2reveal-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os.path
+
+setup(
+    name='md2reveal',
+    version='0.0.1',
+    author='Lirian Su',
+    author_email='liriansu@gmail.com',
+    url='https://github.com/LKI/md2reveal',
+    license='WTFPL',
+    description='Converting markdown to reveal.js html page',
+    long_description=open('README.rst').read(),
+    entry_points={
+        'console_scripts': ['md2reveal = md2reveal.cmdline:execute']
+    },
+    packages=['md2reveal'],
+    data_files=['template.html']
+)
