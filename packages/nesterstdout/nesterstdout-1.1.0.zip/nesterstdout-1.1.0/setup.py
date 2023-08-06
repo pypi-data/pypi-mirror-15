0f5a0f22a37442f0eb706db6e5d53c807670d15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:18 +0000

    Add package nesterstdout/1.1.0/nesterstdout-1.1.0.zip

---
+++ packages/nesterstdout/nesterstdout-1.1.0.zip/nesterstdout-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="nesterstdout",
+    version="1.1.0",
+    py_modules=['nesterstdout'],
+    author='arun',
+    author_email='aruntakkar5@gmail.com',
+    description='A small module to get the list from nested list and give the \
+        standard output'
+)
