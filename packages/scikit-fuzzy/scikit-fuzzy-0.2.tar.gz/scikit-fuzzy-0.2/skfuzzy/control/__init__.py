fc270811e76446a5fd3994f66c7b39dee480b661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:35 +0000

    Add package scikit-fuzzy/0.2/scikit-fuzzy-0.2.tar.gz

---
+++ packages/scikit-fuzzy/scikit-fuzzy-0.2.tar.gz/scikit-fuzzy-0.2/skfuzzy/control/__init__.py
@@ -0,0 +1,15 @@
+"""
+skfuzzy.control subpackage, providing a high-level API for fuzzy system design.
+
+"""
+
+__all__ = ['Antecedent',
+           'Consequent',
+           'ControlSystem',
+           'ControlSystemSimulation',
+           'Rule',
+           ]
+
+from .antecedent_consequent import Antecedent, Consequent
+from .controlsystem import ControlSystem, ControlSystemSimulation
+from .rule import Rule
