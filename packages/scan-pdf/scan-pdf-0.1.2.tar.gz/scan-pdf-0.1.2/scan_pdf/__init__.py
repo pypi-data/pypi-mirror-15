697560e360d1183fe7d584e3aad467e296d0219a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:49 +0000

    Add package scan-pdf/0.1.2/scan-pdf-0.1.2.tar.gz

---
+++ packages/scan-pdf/scan-pdf-0.1.2.tar.gz/scan-pdf-0.1.2/scan_pdf/__init__.py
@@ -0,0 +1,3 @@
+from scan import Scanner
+from convert import Converter
+from combine import Combiner
---
+++ packages/scan-pdf/scan-pdf-0.1.2.tar.gz/scan-pdf-0.1.2/scan_pdf/combine.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+import subprocess
+
+logger = logging.getLogger(__name__)
+
+
+class Combiner(object):
+    def __init__(self, options):
+        self.options = options
+
+    def combine(self, page_file_names):
+        logger.info("combine %d files into %s", len(page_file_names), self.options.output_file_name)
+        combine_args = ['pdftk']
+        combine_args += page_file_names
+        combine_args += ['output', os.path.basename(self.options.output_file_name), 'compress']
+        return subprocess.call(combine_args)
