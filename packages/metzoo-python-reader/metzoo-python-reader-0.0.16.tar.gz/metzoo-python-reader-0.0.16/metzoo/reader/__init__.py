74eca3a7675ab47ffe501ec8d6fd538b5da89d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:01 +0000

    Add package metzoo-python-reader/0.0.16/metzoo-python-reader-0.0.16.tar.gz

---
+++ packages/metzoo-python-reader/metzoo-python-reader-0.0.16.tar.gz/metzoo-python-reader-0.0.16/metzoo/reader/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'metzoo-python-reader'
+__version__ = '0.0.16'
+__build__ = 0x000010
+__author__ = 'Edrans S.A.'
+__copyright__ = 'Copyright 2016 Edrans S.A.'
+
+from .reader import read
