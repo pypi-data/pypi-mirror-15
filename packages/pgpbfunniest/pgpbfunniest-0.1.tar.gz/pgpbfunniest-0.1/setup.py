a67848ee994af8aced72c34130625d9168d97a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:33 +0000

    Add package pgpbfunniest/0.1/pgpbfunniest-0.1.tar.gz

---
+++ packages/pgpbfunniest/pgpbfunniest-0.1.tar.gz/pgpbfunniest-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pgpbfunniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/pgpbpadilla/pgpbfunniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['pgpbfunniest'],
+      zip_safe=False)
+
