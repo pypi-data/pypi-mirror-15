0fadb5b6dfd59e41a836044d851df578c55e5b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:30 +0000

    Add package ywfnester/1.0.0/ywfnester-1.0.0.zip

---
+++ packages/ywfnester/ywfnester-1.0.0.zip/ywfnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='ywfnester',
+	version='1.0.0',
+	py_modules=['ywfnester'],
+	author='chen_peng',
+	author_email='cp123420008@126.com',
+	url='http://www.dreamone.com',
+	description='A simple printer of nested lists',
+	)
