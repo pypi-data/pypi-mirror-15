d7a6030f7a3a3996e531bdffd8ef4d44dbf93223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:37 +0000

    Add package rtpipe/1.43/rtpipe-1.43.tar.gz

---
+++ packages/rtpipe/rtpipe-1.43.tar.gz/rtpipe-1.43/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages, Extension
+from Cython.Distutils import build_ext
+import numpy, glob
+
+ext_modules = [Extension("rtlib_cython", ["rtlib/rtlib_cython.pyx"], include_dirs=[numpy.get_include()])]
+
+setup(
+    name = 'rtpipe',
+    description = 'Python scripts for fast transient searches with radio interferometer data',
+    author = 'Casey Law',
+    author_email = 'caseyjlaw@gmail.com',
+    version = '1.43',
+    url = 'http://github.com/caseyjlaw/rtpipe',
+    packages = find_packages(),        # get all python scripts in realtime
+    install_requires=['numpy', 'scipy', 'pwkit', 'cython', 'sdmpy>=1.35', 'pyfftw', 'click', 'matplotlib'],
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules,
+    entry_points='''
+        [console_scripts]
+        rtpipe=rtpipe.cli:cli
+'''
+)
