f48d3ed131dd7470ce7280bdbd70359b15eccc6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:22 +0000

    Add package grizzly3/0.01/grizzly3-0.01.zip

---
+++ packages/grizzly3/grizzly3-0.01.zip/grizzly3-0.01/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='grizzly3',
+version='0.01',
+author='Alchin and Browning',
+author_email='authors@propython.com',
+url='http://www.propython.com/',
+packages = ['tests']
+)
