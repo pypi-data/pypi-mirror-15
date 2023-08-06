0812fbeb665758de434dcd58dd1bc5e9538ee271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:51 +0000

    Add package configchecker/0.94/configchecker-0.94.tar.gz

---
+++ packages/configchecker/configchecker-0.94.tar.gz/configchecker-0.94/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='configchecker',
+    version='0.94',
+    description='INI-config validator',
+    url='https://github.com/victorsamun/configvalidator',
+    author='Samun Victor',
+    author_email='victor.samun@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=find_packages()
+)
