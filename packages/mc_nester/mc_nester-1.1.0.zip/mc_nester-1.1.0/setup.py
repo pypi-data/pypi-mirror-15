c25106eaca6f7ce8aa35397dc5d929b2cc9fa9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:14 +0000

    Add package mc_nester/1.1.0/mc_nester-1.1.0.zip

---
+++ packages/mc_nester/mc_nester-1.1.0.zip/mc_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+    name                = 'mc_nester',
+    version             = '1.1.0',
+    py_modules          = ['mc_nester'],
+    author              = 'Micah Gabriel',
+    author_email        ='micah_gabriel@hotmail.com',
+    description         ='a simple printer of nested lists',
+    )
