c520aec7b55956b65749159e4af85a1d774a92eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:27 +0000

    Add package makechat/0.1.25/makechat-0.1.25.tar.gz

---
+++ packages/makechat/makechat-0.1.25.tar.gz/makechat-0.1.25/src/api/__init__.py
@@ -0,0 +1,39 @@
+"""Makechat API core.
+
+All routes or API endpoints should be described here. Also we create WSGI
+application here.
+"""
+
+import falcon
+from makechat.api.users import UserLogin, UserLogout, UserRegister, UserPing, \
+    UserResource
+from makechat.api.tokens import TokenResource
+from makechat.api.rooms import RoomResource
+from makechat.api.middlewares import RequireJSON, JSONTranslator, \
+    MongoengineObjectsPaginator
+
+
+def setting_up_api():
+    """Setting up API instance."""
+    do_login = UserLogin()
+    do_logout = UserLogout()
+    do_register = UserRegister()
+    ping = UserPing()
+    token_resource = TokenResource(items_per_page=25)
+    room_resource = RoomResource(items_per_page=25)
+    user_resource = UserResource(items_per_page=25)
+
+    api = falcon.API(middleware=[
+        RequireJSON(), JSONTranslator(), MongoengineObjectsPaginator()])
+
+    api.add_route('/api/login', do_login)
+    api.add_route('/api/logout', do_logout)
+    api.add_route('/api/register', do_register)
+    api.add_route('/api/tokens', token_resource)
+    api.add_route('/api/rooms/', room_resource)
+    api.add_route('/api/rooms/{room_id}', room_resource)
+    api.add_route('/api/users', user_resource)
+    api.add_route('/api/ping', ping)
+    return api
+
+application = setting_up_api()
