da171876b7bf97417dd4a39b5ded9ab1f7a70640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:00 +0000

    Add package codebehind/0.0.1/codebehind-0.0.1.tar.gz

---
+++ packages/codebehind/codebehind-0.0.1.tar.gz/codebehind-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+
+setup(
+    name='codebehind',
+    version='0.0.1',
+    packages=['codebehind'],
+    description='A collection of magic',
+    long_description=README,
+    author='Michael Henry Pantaleon',
+    author_email='me@iamkel.net',
+    url='https://github.com/michaelhenry/codebehind/',
+    license='MIT',
+    install_requires=[
+        'Django>=1.9','djangorestframework==3.3.3',
+    ]
+)
