d2f2b491d7e9159cc79bcacc54091a89b0ec35ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:07 +0000

    Add package dockhand/0.3.0/dockhand-0.3.0.tar.gz

---
+++ packages/dockhand/dockhand-0.3.0.tar.gz/dockhand-0.3.0/setup.py
@@ -0,0 +1,64 @@
+from __future__ import absolute_import, print_function
+
+import io
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8'),
+    ) as fp:
+        return fp.read()
+
+
+readme = open('README.rst').read()
+history = open('CHANGES.rst').read().replace('.. :changelog:', '')
+
+
+setup(
+    name='dockhand',
+    version='0.3.0',
+    url='https://github.com/graingert/dockhand/',
+    license='Apache Software License',
+    author='Scott Robertson',
+    tests_require=['nose'],
+    install_requires=[
+        'zipper>=0.0.3, <0.1.0',
+        'docker-py>=1.8.1, <2.0.0',
+        'GitPython>=2.0.5, <3.0.0',
+    ],
+    author_email='scott@6sense.com',
+    description=(
+        'The right way to build, tag and ship shared Docker containers.'
+    ),
+    long_description=readme + '\n\n' + history,
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    extras_require={
+        'testing': ['nose'],
+    },
+    entry_points={
+        'console_scripts': [
+            'shipwright = shipwright.cli:main',
+        ],
+    },
+)
