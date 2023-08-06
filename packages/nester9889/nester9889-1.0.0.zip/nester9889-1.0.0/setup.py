1de9ab78816b9d26025510405d783df94f20812e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:48 +0000

    Add package nester9889/1.0.0/nester9889-1.0.0.zip

---
+++ packages/nester9889/nester9889-1.0.0.zip/nester9889-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester9889',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'ANUP',
+    author_email = 'upadhyaya.aneesh@gmail.com',
+    description = 'Prints nested lists',
+    )
