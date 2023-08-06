b52fdc73ebbb209f293063123579c4dbb46ffbcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:05 +0000

    Add package wjNewNester/1.0.5/wjNewNester-1.0.5.tar.gz

---
+++ packages/wjNewNester/wjNewNester-1.0.5.tar.gz/wjNewNester-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        =    "wjNewNester",
+    version     =    "1.0.5",
+    py_modules  =    ['wjNewNester'],
+    author =   'wenjie',
+    author_email  =   '826834678@qq.com',
+    url           = "http://www.baidu.com",
+    description = "test code"
+    )
