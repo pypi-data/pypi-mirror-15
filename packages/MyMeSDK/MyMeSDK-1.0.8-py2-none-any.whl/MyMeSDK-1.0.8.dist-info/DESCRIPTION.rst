32e4fa42b8ce649f513ef1c88c87c764e0aea2bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:30 +0000

    Add package MyMeSDK/1.0.8/MyMeSDK-1.0.8-py2-none-any.whl

---
+++ packages/MyMeSDK/MyMeSDK-1.0.8-py2-none-any.whl/MyMeSDK-1.0.8.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+MyMe Python SDK
+
+
---
+++ packages/MyMeSDK/MyMeSDK-1.0.8-py2-none-any.whl/MyMeSDK/pygatt/backends/__init__.py
@@ -0,0 +1,4 @@
+__version__="1.0.0"
+from .backend import BLEBackend, Characteristic  # noqa
+from .bgapi.bgapi import BGAPIBackend  # noqa
+from .gatttool.gatttool import GATTToolBackend  # noqa
