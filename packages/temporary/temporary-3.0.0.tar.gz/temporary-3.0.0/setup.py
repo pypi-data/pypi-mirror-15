7f7b30db17923e49186afae6418d1cd4303e1cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:15 +0000

    Add package temporary/3.0.0/temporary-3.0.0.tar.gz

---
+++ packages/temporary/temporary-3.0.0.tar.gz/temporary-3.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='temporary',
+    version='3.0.0',
+    packages=('temporary',),
+    url='https://github.com/themattrix/python-temporary',
+    license='MIT',
+    author='Matthew Tardiff',
+    author_email='mattrix@gmail.com',
+    install_requires=(
+        'contextlib2',
+        'pathlib2'),
+    tests_require=(
+        'mock',
+        'simian',),
+    description=(
+        'Context managers for managing temporary files and directories.'),
+    classifiers=(
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'))
