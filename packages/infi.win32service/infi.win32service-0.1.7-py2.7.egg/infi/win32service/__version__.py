f7dcfff2a4ba538834dd606c16b8473aca2588f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:30 +0000

    Add package infi.win32service/0.1.7/infi.win32service-0.1.7-py2.7.egg

---
+++ packages/infi.win32service/infi.win32service-0.1.7-py2.7.egg/infi/win32service/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.7"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.1.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '26fa564aea92a5640ab3cf1be333324566f41896'
+__git_head_subject__ = 'Finished Release v0.1.7'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
