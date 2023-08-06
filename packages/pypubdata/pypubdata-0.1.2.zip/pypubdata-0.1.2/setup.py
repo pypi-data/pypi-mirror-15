55b917d0b50289a309c9d57900da7a579b2fe6eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:13 +0000

    Add package pypubdata/0.1.2/pypubdata-0.1.2.zip

---
+++ packages/pypubdata/pypubdata-0.1.2.zip/pypubdata-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypubdata',
+    version='0.1.2',
+    packages=find_packages(),
+    url='https://github.com/gom2dev/pypubdata',
+    download_url='https://github.com/gom2dev/pypubdata/archive/master.zip',
+    license='LGPLv3',
+    author='gom2dev',
+    author_email='contact@gom2.net',
+    description='It is simple module package for ROK Public Data OpenAPI.',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
