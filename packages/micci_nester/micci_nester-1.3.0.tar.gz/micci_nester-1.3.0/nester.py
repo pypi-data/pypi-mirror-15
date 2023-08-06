d648446eadd9cd701455f4a5267f2d2e441f4e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:19 +0000

    Add package micci_nester/1.3.0/micci_nester-1.3.0.tar.gz

---
+++ packages/micci_nester/micci_nester-1.3.0.tar.gz/micci_nester-1.3.0/nester.py
@@ -0,0 +1,14 @@
+"""Este é o módulo "nester.py", e fornece a função chamada print_lol() que imprime listas que podem ou não incluir
+listas aninhadas"""
+def print_lol(the_list, ident=false, level=0):
+    """esta função requer um argumento posicional chamado "the_list", que é qualquer lista Python. Cada item da lista
+    é recursivamente impressa na tela"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            if ident:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)
+
