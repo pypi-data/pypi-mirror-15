44c5d2bad429dea65a002084c008fce23b6b5087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:59 +0000

    Add package MetaCSV/0.0.2/MetaCSV-0.0.2-py2.py3-none-any.whl

---
+++ packages/MetaCSV/MetaCSV-0.0.2-py2.py3-none-any.whl/metacsv/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+MetaCSV
+-------
+
+Tools for documentation-aware data reading, writing, and analysis
+
+"""
+
+from __future__ import absolute_import, division, print_function, \
+    with_statement, unicode_literals
+
+from .core import *
+from .parsers import *
