6ec3a016522401c6142c97916ab68d26c35f0fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:24 +0000

    Add package firster/1.0.0/firster-1.0.0.zip

---
+++ packages/firster/firster-1.0.0.zip/firster-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='firster',
+	version='1.0.0',
+	py_modules=['firster'],
+	author='hfpython',
+	author_email='hfpython@headfirstlabs.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nested lists',
+	)
