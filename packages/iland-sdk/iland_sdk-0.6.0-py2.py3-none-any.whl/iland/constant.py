3401caf2604b5fcea673be3bb8d565aad5d58249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:45 +0000

    Add package iland-sdk/0.6.0/iland_sdk-0.6.0-py2.py3-none-any.whl

---
+++ packages/iland-sdk/iland_sdk-0.6.0-py2.py3-none-any.whl/iland/constant.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""A library that provides constants for the `iland.Api` object."""
+
+#: Base URL to the iland cloud API.
+BASE_URL = 'https://api.ilandcloud.com/ecs'
+
+#: Access token URL.
+ACCESS_URL = \
+    'https://ecs.ilandcloud.com/auth/realms/iland-core/tokens/grants/access'
+
+#: Refresh token URL.
+REFRESH_URL = \
+    'https://ecs.ilandcloud.com/auth/realms/iland-core/tokens/refresh'
