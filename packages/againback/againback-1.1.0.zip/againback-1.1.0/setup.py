a38b17a8367697c47dfd807cc098185046268ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:06 +0000

    Add package againback/1.1.0/againback-1.1.0.zip

---
+++ packages/againback/againback-1.1.0.zip/againback-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name ='againback',
+        version ='1.1.0',
+        py_modules ='againback',
+        author ='backflow',
+        author_email ='1667161414@qq.com',
+        url ='',
+        description ='just for test'
+    )
