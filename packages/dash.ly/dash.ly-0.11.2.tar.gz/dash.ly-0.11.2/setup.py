d87beecd5be12b06ff94564f99167ff6381146b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:03 +0000

    Add package dash.ly/0.11.2/dash.ly-0.11.2.tar.gz

---
+++ packages/dash.ly/dash.ly-0.11.2.tar.gz/dash.ly-0.11.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dash.ly',
+    version='0.11.2',
+    author='chris p',
+    author_email='chris@plot.ly',
+    packages=['dash'],
+    license='MIT',
+    description='',
+    long_description=open('README.md').read(),
+    install_requires=['flask', 'plotly', 'flask-cors']
+)
