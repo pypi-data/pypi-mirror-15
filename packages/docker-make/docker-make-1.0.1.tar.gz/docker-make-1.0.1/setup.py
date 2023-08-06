9f13c58c71b2fb98854bf45df55d8e920589cfcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:04 +0000

    Add package docker-make/1.0.1/docker-make-1.0.1.tar.gz

---
+++ packages/docker-make/docker-make-1.0.1.tar.gz/docker-make-1.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='docker-make',
+    description='a tool for simplifying docker image building and pushing',
+    version='1.0.1',
+    author='jizhilong',
+    author_email='zhilongji@gmail.com',
+    url='https://github.com/CtripCloud/docker-make',
+    license='Apache',
+    keywords=['docker', 'image',' build'],
+    scripts=['docker-make'],
+    install_requires=[
+        'PyYAML >= 3.10, < 4',
+        'docker-py >= 1.8.1, < 2',
+    ],
+    classifiers=[],
+    )
