ce510c00c2d72be6b44a7fb7a27860c37e68d8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:59 +0000

    Add package tracktotrip/0.1.1/tracktotrip-0.1.1.tar.gz

---
+++ packages/tracktotrip/tracktotrip-0.1.1.tar.gz/tracktotrip-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'tracktotrip',
+  packages = ['tracktotrip'],
+  version = '0.1.1',
+  description = 'Track processing library',
+  author = 'Rui Gil',
+  author_email = 'ruipgil@gmail.com',
+  url = 'https://github.com/ruipgil/TrackToTrip',
+  download_url = 'https://github.com/ruipgil/TrackToTrip/archive/master.zip',
+  keywords = ['track', 'trip', 'GPS', 'GPX'],
+  classifiers = [],
+)
