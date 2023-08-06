75731390217191cff3fbecb58e7bb0b2f7cd8bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:46 +0000

    Add package LocalNote/1.0.3/LocalNote-1.0.3.zip

---
+++ packages/LocalNote/LocalNote-1.0.3.zip/LocalNote-1.0.3/LocalNote/controllers.py
@@ -0,0 +1,185 @@
+import os, time, re
+
+import chardet
+
+from local import Storage as LocalStorage
+from local import html2text, markdown
+from evernoteapi.storage import Storage as EvernoteStorage
+from evernoteapi.controller import EvernoteController
+
+class Controller(object):
+    def __init__(self):
+        self.es = EvernoteStorage()
+        self.ls = LocalStorage()
+        self.token, self.isSpecialToken, self.sandbox, self.isInternational, self.expireTime, self.lastUpdate = self.ls.get_config()
+        self.available, self.ec = self.__check_available()
+        if self.available: self.ls.maxUpload = self.ec.get_upload_limit()
+        self.changesList = []
+    def __check_available(self):
+        if not self.isSpecialToken and self.expireTime < time.time(): return False, None
+        if self.token == '': return False, None
+        try:
+            ec = EvernoteController(self.token, self.isSpecialToken, self.sandbox, self.isInternational)
+            self.ls.update_config(self.token, self.isSpecialToken, self.sandbox, self.isInternational, self.expireTime, self.lastUpdate)
+            return True, ec
+        except:
+            return False, None
+    def log_in(self, config = {}, **kwargs):
+        config = dict(config, **kwargs)
+        if config.get('token') is not None: self.token = config.get('token')
+        if config.get('isSpecialToken') is not None: self.isSpecialToken = config.get('isSpecialToken')
+        if config.get('sandbox') is not None: self.sandbox = config.get('sandbox')
+        if config.get('isInternational') is not None: self.isInternational = config.get('isInternational')
+        if config.get('expireTime') is not None: self.expireTime = config.get('expireTime')
+        if config.get('lastUpdate') is not None: self.lastUpdate = config.get('lastUpdate')
+        available, ec = self.__check_available()
+        if available:
+            self.available = True
+            self.ec = ec
+            self.ls.maxUpload = self.ec.get_upload_limit()
+        return available
+    def fetch_notes(self):
+        if not self.available: return False
+        self.ec.storage.update(self.token, self.ec.noteStore)
+        return True
+    def __get_changes(self, update = False): # -1 for need download, 1 for need upload, 0 for can be uploaded and downloaded
+        if not update: return self.changesList # (fileFullPath, status)
+        r = []
+        fileDict = self.ls.get_file_dict()
+        noteDict = self.es.get_note_dict()
+        for nbName, lNotes in fileDict.items():
+            eNotes = noteDict.get(nbName)
+            if eNotes is None: # notebook exists locally not online
+                r.append((nbName, 0))
+                continue
+            delIndex = []
+            for lNote in lNotes:
+                for i, eNote in enumerate(eNotes):
+                    if lNote[0] != eNote[0]: continue
+                    if self.ls.lastUpdate < lNote[1]: # need upload
+                        if self.ls.lastUpdate < eNote[1]: # need download
+                            r.append((nbName+'/'+lNote[0], 0))
+                        else:
+                            r.append((nbName+'/'+lNote[0], 1))
+                    else:
+                        if self.ls.lastUpdate < eNote[1]:
+                            r.append((nbName+'/'+lNote[0], -1))
+                        # else:
+                            # debug
+                            # r.append((nbName+'/'+lNote[0], 2))
+                    delIndex.append(i)
+                    break
+                else: # note exists locally not online
+                    r.append((nbName+'/'+lNote[0], 0))
+            eNotes = [n for i, n in enumerate(eNotes) if i not in delIndex]
+            for eNote in eNotes: r.append((nbName+'/'+eNote[0], 0)) # note exists online not locally
+            del noteDict[nbName]
+        for nbName in noteDict.keys(): r.append((nbName, 0))
+        self.changesList = r
+        return r
+    def get_changes(self):
+        return self.__get_changes(True)
+    def check_files_format(self):
+        return self.ls.check_files_format()
+    def download_notes(self, update = True):
+        if not self.available: return False
+        noteDict = self.es.get_note_dict()
+        def _download_note(noteFullPath):
+            print(('Downloading '+noteFullPath).decode('utf8'))
+            if self.es.get(noteFullPath) is None: # delete note if is deleted online
+                self.ls.write_note(noteFullPath, {})
+                return
+            contentDict = self.ec.get_attachment(noteFullPath)
+            if contentDict.get(noteFullPath.split('/')[1]+'.md') is None:
+                if contentDict.get(noteFullPath.split('/')[1]+'.html') is None:
+                    contentDict[noteFullPath.split('/')[1]+'.html'] = self.ec.get_content(noteFullPath)
+                else: # avoid mistaken overwrite of attachment
+                    fileNum = 1
+                    while 1:
+                        if contentDict.get(noteFullPath.split('/')[1]+'(%s).html'%fileNum) is None:
+                            contentDict[noteFullPath.split('/')[1]+'(%s).html'%fileNum] = contentDict[noteFullPath.split('/')[1]+'.html']
+                            contentDict[noteFullPath.split('/')[1]+'.html'] = self.ec.get_content(noteFullPath)
+                            break
+                        else:
+                            fileNum += 1
+            self.ls.write_note(noteFullPath, contentDict)
+        for noteFullPath, status in self.__get_changes(update):
+            if status not in (-1, 0): continue
+            if '/' in noteFullPath:
+                _download_note(noteFullPath)
+            else:
+                notes = noteDict.get(noteFullPath)
+                if notes is None:
+                    self.ls.write_note(noteFullPath, {}) # delete folder
+                else:
+                    self.ls.write_note(noteFullPath, {1}) # create folder
+                    for note in notes: _download_note(noteFullPath+'/'+note[0])
+        self.ls.update_config(lastUpdate = time.time() + 1)
+        return True
+    def upload_files(self, update = True):
+        if not self.available: return False
+        def encode_content(content):
+            try:
+                content.decode('utf8')
+            except:
+                try:
+                    content = content.decode(chardet.detect(content)['encoding']).encode('utf8')
+                except:
+                    content = 'Upload encode failed, I\'m sorry! Please contact i7meavnktqegm1b@qq.com with this file.'
+            return content
+        def _upload_files(noteFullPath, attachmentDict):
+            print(('Uploading '+noteFullPath).decode('utf8'))
+            nbName, nName = noteFullPath.split('/')
+            if not attachmentDict:
+                self.ec.delete_note(noteFullPath)
+            elif nName + '.md' in attachmentDict.keys():
+                content = encode_content(attachmentDict[nName+'.md']).decode('utf8')
+                self.ec.update_note(noteFullPath, markdown(content).encode('utf8'), attachmentDict)
+            elif nName + '.html' in attachmentDict.keys():
+                content = encode_content(attachmentDict[nName+'.html'])
+                self.ec.update_note(noteFullPath, content, attachmentDict)
+        for noteFullPath, status in self.__get_changes(update):
+            if status not in (1, 0): continue
+            if '/' in noteFullPath:
+                attachmentDict = self.ls.read_note(noteFullPath)
+                _upload_files(noteFullPath, attachmentDict)
+            else:
+                lns = self.ls.get_file_dict().get(noteFullPath)
+                ens = self.es.get_note_dict().get(noteFullPath)
+                if lns is None:
+                    for note in ens or []: self.ec.delete_note(noteFullPath+'/'+note[0])
+                    self.ec.delete_notebook(noteFullPath)
+                else:
+                    self.ec.create_notebook(noteFullPath)
+                    for note in lns:
+                        attachmentDict = self.ls.read_note(noteFullPath+'/'+note[0])
+                        _upload_files(noteFullPath+'/'+note[0], attachmentDict)
+        self.ls.update_config(lastUpdate = time.time() + 1)
+        return True
+def convert_html(htmlDir, force = None):
+    # FileName for done, 1 for wrong file type, 2 for file missing
+    # 3 for need rename, 4 for unknown encoding
+    fileName, ext = os.path.splitext(htmlDir)
+    if ext != '.html': return 1
+    if not os.path.exists(htmlDir): return 2
+    with open(htmlDir, 'rb') as f: content = f.read()
+    try:
+        content = content.decode('utf8')
+    except:
+        try:
+            content = content.decode(chardet.detect(content)['encoding'])
+        except:
+            return 4
+    if os.path.exists(fileName + '.md'):
+        if force is None:
+            return 3
+        elif force == True:
+            pass
+        elif force == False:
+            index = 1
+            while 1:
+                if not os.path.exists(fileName + '(%s)'%index + '.md'): break
+                index += 1
+            fileName += '(%s)'%index
+    with open(fileName + '.md', 'wb') as f: f.write(html2text(content).encode('utf8'))
+    return fileName + '.md'
---
+++ packages/LocalNote/LocalNote-1.0.3.zip/LocalNote-1.0.3/LocalNote/local/storage.py
@@ -0,0 +1,155 @@
+import json, os, time, sys, re
+from os.path import join, exists
+import chardet
+
+CONFIG_DIR = 'user.cfg'
+
+# fileDictFormat: {
+# 'notebookName':[('note1', timeStamp), ..],
+# }
+# fileFormat: {
+# 'name': "note's name",
+# 'content': u''.encode('utf-8'),
+# 'attachment': [('name', u''.encode),..),],
+# }
+
+class Storage(object):
+    def __init__(self, maxUpload = 0):
+        self.maxUpload = maxUpload
+        self.token, self.isSpecialToken, self.sandbox, self.isInternational, self.expireTime, self.lastUpdate = self.__load_config()
+        self.encoding = sys.stdin.encoding
+    def __load_config(self):
+        if not exists(CONFIG_DIR): return '', False, True, False, 0, 0
+        with open(CONFIG_DIR) as f: r = json.loads(f.read())
+        return r.get('token', ''), r.get('is-special-token', False), r.get('sandbox', True), r.get('is-international', False), r.get('expire-time', 0), r.get('last-update', 0)
+    def __store_config(self):
+        with open(CONFIG_DIR, 'w') as f:
+            f.write(json.dumps({
+                'token': self.token,
+                'is-special-token': self.isSpecialToken,
+                'sandbox': self.sandbox,
+                'is-international': self.isInternational,
+                'expire-time': self.expireTime,
+                'last-update': self.lastUpdate, }))
+    def update_config(self, token=None, isSpecialToken=None, sandbox=None, isInternational=None, expireTime=None, lastUpdate=None):
+        if not token is None: self.token = token
+        if not isSpecialToken is None: self.isSpecialToken = isSpecialToken
+        if not sandbox is None: self.sandbox = sandbox
+        if not isInternational is None: self.isInternational = isInternational
+        if not expireTime is None: self.expireTime = expireTime
+        if not lastUpdate is None: self.lastUpdate = lastUpdate
+        self.__store_config()
+    def get_config(self):
+        return self.token, self.isSpecialToken, self.sandbox, self.isInternational, self.expireTime, self.lastUpdate
+    def __str_c2l(self, s):
+        return s.decode('utf8').encode(sys.stdin.encoding)
+    def __str_l2c(self, s):
+        try:
+            return s.decode(sys.stdin.encoding).encode('utf8')
+        except:
+            return s.decode(chardet.detect(s)['encoding'] or 'utf8').encode('utf8')
+    def read_note(self, noteFullPath):
+        attachmentDict = {}
+        if exists(join(*self.__str_c2l(noteFullPath).split('/'))): # note is a foldernote
+            for attachment in os.walk(join(*self.__str_c2l(noteFullPath).split('/'))).next()[2]:
+                with open(join(*(self.__str_c2l(noteFullPath)+'/'+attachment).split('/')), 'rb') as f:
+                    attachmentDict[self.__str_l2c(attachment)] = f.read()
+        else: # note is a pure file
+            fileList = os.walk(join(*self.__str_c2l(noteFullPath).split('/')[:-1])).next()[2]
+            for postfix in ('.md', '.html'):
+                fName = noteFullPath.split('/')[-1] + postfix
+                if self.__str_c2l(fName) in fileList:
+                    with open(join(*self.__str_c2l(noteFullPath + postfix).split('/'))) as f:
+                        attachmentDict[fName] = f.read()
+        return attachmentDict
+    def write_note(self, noteFullPath, contentDict = {}):
+        if '/' in noteFullPath:
+            nbName, nName = self.__str_c2l(noteFullPath).split('/')
+            # clear environment
+            if exists(nbName):
+                for postfix in ('.md', '.html'):
+                    if exists(join(nbName, nName+postfix)): os.remove(join(nbName, nName+postfix))
+                if exists(join(nbName, nName)):
+                    for fName in os.walk(join(nbName, nName)).next()[2]:
+                        os.remove(join(nbName, nName, fName))
+                    os.rmdir(join(nbName, nName))
+            else:
+                os.mkdir(nbName)
+            # download files
+            if not contentDict:
+                pass
+            elif len(contentDict) == 1:
+                for k, v in contentDict.items():
+                    self.write_file(noteFullPath, v, os.path.splitext(k)[1])
+            else:
+                if not exists(join(nbName, nName)): os.mkdir(join(nbName, nName))
+                for k, v in contentDict.iteritems():
+                    self.write_file(noteFullPath+'/'+k, v, '') # ok, this looks strange, ext is included in k
+        else:
+            if contentDict: # create folder
+                if not exists(self.__str_c2l(noteFullPath)): os.mkdir(self.__str_c2l(noteFullPath))
+            else: # delete folder
+                noteFullPath = self.__str_c2l(noteFullPath)
+                if exists(noteFullPath):
+                    for fName in os.walk(noteFullPath).next()[2]:
+                        os.remove(join(noteFullPath, fName))
+                    for fName in os.walk(noteFullPath).next()[1]:
+                        for dName in os.walk(noteFullPath, fName).next()[2]:
+                            os.remove(join(noteFullPath, fName, dName))
+                        os.rmdir(join(noteFullPath, fName))
+                    os.rmdir(noteFullPath)
+    def write_file(self, noteFullPath, content, postfix = '.md'):
+        if len(noteFullPath.split('/')) < 1: return False
+        if not exists(self.__str_c2l(noteFullPath.split('/')[0])):
+            os.mkdir(self.__str_c2l(noteFullPath.split('/')[0]))
+        try:
+            noteFullPath += postfix
+            with open(self.__str_c2l(join(*noteFullPath.split('/'))), 'wb') as f: f.write(content)
+            return True
+        except:
+            return False
+    def get_file_dict(self):
+        fileDict = {}
+        for nbName in os.walk('.').next()[1]: # get folders
+            nbNameUtf8 = self.__str_l2c(nbName)
+            fileDict[nbNameUtf8] = []
+            for nName in reduce(lambda x,y: x+y, os.walk(nbName).next()[1:]): # get folders and files
+                filePath = join(nbName, nName)
+                if os.path.isdir(nName):
+                    fileDict[nbNameUtf8].append((self.__str_l2c(nName), os.stat(filePath).st_mtime))
+                else:
+                    fileDict[nbNameUtf8].append((self.__str_l2c(os.path.splitext(nName)[0]), os.stat(filePath).st_mtime))
+        return fileDict
+    def check_files_format(self):
+        try:
+            with open('user.cfg') as f: j = json.loads(f.read())
+            if len(j) != 6: raise Exception
+            for k in j.keys():
+                if k not in ('token', 'is-special-token', 'sandbox',
+                        'is-international', 'expire-time', 'last-update'):
+                    raise Exception
+        except:
+            return False, []
+        r = [] # (filename, status) 1 for wrong placement, 2 for too large, 3 for missing main file
+        notebooks, notes = os.walk('.').next()[1:]
+        for note in notes:
+            if note != 'user.cfg': r.append((self.__str_l2c(note), 1))
+        for notebook in notebooks:
+            folderNotes, notes = os.walk(notebook).next()[1:]
+            for note in notes:
+                if re.compile('.+\.(md|html)').match(note):
+                    if self.maxUpload < os.path.getsize(join(notebook, note)):
+                        r.append((self.__str_l2c(join(notebook, note)), 2))
+                else:
+                    r.append((self.__str_l2c(join(notebook, note)), 3))
+            for folderNote in folderNotes:
+                size = 0
+                wrongFolders, attas = os.walk(join(notebook, folderNote)).next()[1:]
+                if filter(lambda x: re.compile('.+\.(md|html)').match(x), attas) == []:
+                    r.append((self.__str_l2c(join(notebook, folderNote), 3)))
+                for atta in attas: size += os.path.getsize(join(notebook, folderNote, atta))
+                for wrongFolder in wrongFolders:
+                    r.append((self.__str_l2c(join(notebook, folderNote, wrongFolder)), 1))
+                if self.maxUpload < size:
+                    r.append((self.__str_l2c(join(notebook, folderNote)), 2))
+        return True, r
