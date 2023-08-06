f1beb2e0b5d3f55ceb1c8f0dbcb2e2dd766a83b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:06 +0000

    Add package paybook/0.5/paybook-0.5.tar.gz

---
+++ packages/paybook/paybook-0.5.tar.gz/paybook-0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='paybook',
+      version='0.5',
+      description='Paybook Python Sync SDK',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+         'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='Financial Processing Banks Tax',
+      url='https://github.com/Paybook/sync-py.git',
+      author='Hugo Ochoa',
+      author_email='hugo@paybook.com',
+      license='MIT',
+      packages=['paybook'],
+      zip_safe=False
+)#End of setup
+
+# Pending: json, sqlite3, flask.ext.cors import CORS, cross_origin
+# sudo python setup.py install
+
