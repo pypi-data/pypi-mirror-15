84d8ebb75f1034eb6d1d9b2c6734f82b7dd84ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:47 +0000

    Add package li-apiclient/2.2.0b4/li_apiclient-2.2.0b4-py2-none-any.whl

---
+++ packages/li-apiclient/li_apiclient-2.2.0b4-py2-none-any.whl/li_api_client/integration.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from li_api_client.utils import ApiClientBase
+
+CRUD_METHODS = {
+    "create": "post",
+    "read": "get",
+    "update": "put",
+    "delete": "delete"
+}
+
+
+class ApiIntegration(ApiClientBase):
+    NOME = "API_INTEGRATION"
+    AUTENTICA_APLICACAO = True
+
+    def send_product(self, crud, account_id, product_sku, product_dict,
+                     version="v1", **kwargs):
+        path = 'integration/{}/{}/{}/{}'.format(
+            version, crud, account_id, product_sku)
+
+        method = CRUD_METHODS.get(crud)
+
+        if not method:
+            raise ValueError(
+                "You need to send a valid CRUD operation:"
+                " create, read, update or delete")
+
+        print("API INTEGRATION: ENVIANDO DADOS "
+              "PARA O VARNISH: PATH: {}, DATA: {}".format(
+                  path,
+                  product_dict
+              ))
+        return self.to_dict(path, method=method, meta=True,
+                            data=product_dict, **kwargs)
