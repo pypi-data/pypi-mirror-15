e9cdf16e5b02295e2fa2db24cf519941dac588d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:53 +0000

    Add package cliquery/1.7.6/cliquery-1.7.6-py2-none-any.whl

---
+++ packages/cliquery/cliquery-1.7.6-py2-none-any.whl/cliquery/__init__.py
@@ -0,0 +1,23 @@
+"""cliquery cuts down on clicking through command-line web searching,
+   page previewing, and page bookmarking, among other features.
+
+   An interactive prompt allows users to easily make successive queries and
+   enter program flags dynamically; simply typing help will list all possible
+   flags to enter. Opening a link will invoke a browser supplied by the user
+   or detected automatically.
+"""
+
+import sys
+
+
+__version__ = '1.7.6'
+
+SYS_VERSION = sys.version_info[0]
+
+# Python 2.x and 3.x compatible builtins
+if SYS_VERSION == 2:
+    input = raw_input
+    range = xrange
+
+CONTINUE = '[Press Enter to continue..] '
+SEE_MORE = 'See more? {0}'.format(CONTINUE)
