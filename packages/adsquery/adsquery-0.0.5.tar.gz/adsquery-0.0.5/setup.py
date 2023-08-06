86484af9fbbef5612c710bfaf926e9392a4c99c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:34 +0000

    Add package adsquery/0.0.5/adsquery-0.0.5.tar.gz

---
+++ packages/adsquery/adsquery-0.0.5.tar.gz/adsquery-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='adsquery',
+      version='0.0.5',
+      description='Query the ADS using python',
+      long_description=readme(),
+      url='https://github.com/cphyc/adsquery',
+      author='cphyc (Corentin Cadiou)',
+      author_email='contact@cphyc.me',
+      license='MIT',
+      packages=['adsquery'],
+      install_requires=[
+          'requests',
+          'ads',
+          'tqdm'
+      ],
+      zip_safe=False,
+      scripts=['bin/adsquery'])
