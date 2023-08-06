ebb38132f9560a7994ae323fbaa7eb0ebc70d4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:42 +0000

    Add package sphinxserve/0.8b1/sphinxserve-0.8b1-py2.py3-none-any.whl

---
+++ packages/sphinxserve/sphinxserve-0.8b1-py2.py3-none-any.whl/sphinxserve/__init__.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+
+'''sphinxserve render sphinx docs when detecting file changes.
+
+usage: sphinxserve [-h] {serve,install,uninstall} ...'''
+
+__version__ = '0.8b1'
+__author__ = 'Daniel Mizyrycki'
+
+import gevent.monkey
+gevent.monkey.patch_all()
+
+from gevent.event import Event
+from gevent import spawn, joinall
+from loadconfig import Config
+from loadconfig.lib import write_file
+import logging as log
+import os
+from os.path import exists
+from sphinx import build_main
+from sphinxserve.lib import capture_streams, fs_event_ctx, Webserver
+import sys
+from textwrap import dedent
+
+
+conf = '''\
+    app:            sphinxserve
+    app_socket:     localhost:8888
+    app_user:       1000
+    docker_image:   mzdaniel/sphinxserve
+    extensions:     [rst, rst~, txt, txt~]
+
+    clg:
+        prog: $app
+        description: |
+            $app $version render sphinx docs when detecting file changes and
+            serve them by default on localhost:8888. It uses gevent and flask
+            for serving the website and watchdog for recursively monitor
+            changes on rst and txt files.
+        default_cmd: serve
+        subparsers:
+            serve:
+                help: |
+                    serve sphinx docs. (For extra help, use:
+                        sphinxserve serve --help)
+                options: &options
+                    debug:
+                        short: d
+                        action: store_true
+                        default: __SUPPRESS__
+                    uid:
+                        short: u
+                        type: int
+                        help: |
+                            numeric system user id for rendered pages on Docker
+                              (use "id" to retrieve it.  def: $app_user)
+                        default: $app_user
+                    socket:
+                        short: s
+                        help: |
+                            Launch web server if defined  (def: $app_socket)
+                        default: $app_socket
+                    output:
+                        short: o
+                        help: |
+                            Directory to which output will be written; defaults
+                            to './html/'.
+                        default: html
+                args: &args
+                    sphinx_path:
+                        help: |
+                            path containing sphinx conf.py
+                              (def: $PWD)
+                        nargs: '?'
+                        default: __SUPPRESS__
+            install:
+                help: |
+                    print commands for docker installation on ~/bin/$app
+                      Use as:  docker run $docker_image install | bash
+                               ~/bin/$app [SPHINX_PATH]
+                options: *options
+                args: *args
+            uninstall:
+                help: print commands for uninstalling $app
+    checkconfig: |
+        import os
+        from os.path import abspath
+        if not self.sphinx_path:
+           self.sphinx_path = os.getcwd()
+        self.sphinx_path = abspath(self.sphinx_path)
+    '''
+
+
+class SphinxServer(object):
+    '''Coordinate sphinx greenlets.
+    manage method render sphinx pages, initialize and give control to serve,
+    watch and render greenlets.
+    '''
+    def __init__(self, c):
+        self.c = c
+        self.watch_ev = Event()
+        self.render_ev = Event()
+
+    def serve(self):
+        '''Serve web requests from path as soon docs are ready.
+        Reload remote browser when updates are rendered using websockets
+        '''
+        host, port = self.c.socket.split(':')
+        server = Webserver(
+            os.path.join(self.c.sphinx_path, self.c.output),
+            host,
+            port,
+            self.render_ev
+        )
+        server.run()
+
+    def watch(self):
+        '''Watch sphinx_path signalling render when rst files change
+        '''
+        with fs_event_ctx(self.c.sphinx_path, self.c.extensions) as fs_ev_iter:
+            for event in fs_ev_iter:
+                log.debug('filesystem event: {}'.format(event, event.ev_name))
+                self.watch_ev.set()
+
+    def render(self):
+        '''Render and listen for doc changes (watcher events)
+        '''
+        while True:
+            self.watch_ev.wait()  # Wait for docs changes
+            self.watch_ev.clear()
+            with capture_streams() as streams:
+                self.build()
+            log.debug(streams.getvalue())
+            self.render_ev.set()
+
+    def build(self):
+        '''Render reStructuredText files with sphinx'''
+        return build_main(['sphinx-build', self.c.sphinx_path,
+            os.path.join(self.c.sphinx_path, self.c.output)])
+
+    def manage(self):
+        '''Manage web server, watcher and sphinx docs renderer
+        '''
+        with capture_streams() as streams:
+            ret = self.build()
+        if ret != 0:
+            sys.exit(streams.getvalue())
+        log.debug(streams.getvalue())
+        workers = [spawn(self.serve), spawn(self.watch), spawn(self.render)]
+        joinall(workers)
+
+
+class Prog(object):
+    '''Execute cli options'''
+
+    def __init__(self, c):
+        self.c = c
+
+    def check_dependencies(self):
+        '''Create sphinx conf.py and index.rst if necessary'''
+        path = self.c.sphinx_path
+        if not exists(path):
+            os.makedirs(path)
+        if not exists(path + '/index.rst'):
+            data = dedent('''\
+                Index rst file
+                ==============
+
+                This is the main reStructuredText page. It is meant as a
+                temporary example, ready to override.''')
+            write_file(path + '/index.rst', data)
+        if not exists(path + '/conf.py'):
+            write_file(path + '/conf.py', "master_doc = 'index'\n")
+
+    def install(self):
+        print(self.c.render(dedent('''\
+            mkdir -p ~/bin
+            cat > ~/bin/$app << EOF
+            #!/bin/bash
+
+            SPHINX_PATH=\${1:-\$PWD}
+            USERID="$uid"
+            SOCKET="$socket"
+            APP_PORT=\${SOCKET#*:}
+
+            usage () {
+                echo "Usage: $app [-h] [SPHINX_PATH]    (default: \$PWD)"
+                exit 1; }
+
+            [ "\$1" == "-h" ] || [ "\$1" == "--help" ] && usage
+
+            docker run -it -u \$USERID -v \$SPHINX_PATH:/host  \
+                -p \$APP_PORT:\$APP_PORT $docker_image \
+                -s 0.0.0.0:\$APP_PORT /host
+            EOF
+            chmod 755 ~/bin/$app
+            ''')))
+
+    def uninstall(self):
+        print(self.c.render('rm -f ~/bin/$app'))
+
+    def serve(self):
+        self.check_dependencies()
+        SphinxServer(self.c).manage()
+
+
+def main(args):
+    c = Config(conf, args=args, version=__version__)
+    if c.debug:
+        log.root.setLevel(log.DEBUG)
+    # Run command selected from cli (corresponding to conf subparser, and
+    # Prog method. Eg: serve)
+    c.run(Prog)
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ packages/sphinxserve/sphinxserve-0.8b1-py2.py3-none-any.whl/sphinxserve/lib.py
@@ -0,0 +1,155 @@
+'''sphinxserve library'''
+
+# Make watchdog non-blocking monkeypatching os.read
+from gevent.os import tp_read
+import os
+os.read = tp_read
+
+from bottle import get, run, static_file
+from contextlib import contextmanager
+import gevent
+from gevent.queue import Queue
+from gevent import sleep
+from loadconfig.lib import Ret
+from loadconfig.py6 import cStringIO
+import re
+import socket
+import sys
+from textwrap import dedent
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.observers import Observer
+
+if sys.platform.startswith('win') or sys.platform.startswith('darwin'):
+    from watchdog.observers.polling import PollingObserver as Observer  # noqa
+
+
+class Webserver(object):
+    '''Serve static content from path, featuring asynchronous reload.
+    Page reload is triggered by sphinx rst updates using gevent on the server
+    and executed after ajax long polling on the browser.
+    '''
+    def __init__(self, path, host, port, reload_ev):
+        self.path, self.host, self.port = path, host, port
+        self.reload_ev = reload_ev
+
+    def run(self):
+        reload_js = dedent('''\
+        <script type="text/javascript">
+            $.ajaxSetup({cache: false})  // drop browser cache for refresh
+            $(document).ready(function() {
+                $.ajax({ type: "GET", async: true, cache: false,
+                    url: location.protocol + "//" + location.host + "/_svwait",
+                    success: function() {window.location.reload(true)} }) })
+        </script>''')
+
+        def after_request(response):
+            '''Add reload javascript and remove googleapis fonts'''
+            r = response
+            r.body = r.body.read().decode('utf-8') if getattr(
+                r.body, 'read', False) else r.body
+            if r.content_type.startswith('text/html'):
+                r.body = re.sub('(</head>)', r'{}\1'.format(reload_js),
+                    r.body, flags=re.IGNORECASE)
+            if r.content_type.startswith('text/css'):
+                r.body = re.sub('@import url\(.+fonts.googleapis.com.+\);', '',
+                    r.body, flags=re.IGNORECASE)
+            return r
+
+        @get('<path:path>')
+        def serve_static(path):
+            path = path + '/index.html' if path.endswith('/') else path
+            response = static_file(path, root=self.path)
+            return after_request(response)
+
+        @get('/_svwait')
+        def wait_server_event():
+            '''Block long polling javascript until reload event'''
+            self.reload_ev.wait()
+            self.reload_ev.clear()
+
+        run(host=self.host, port=int(self.port), server='gevent')
+
+
+@contextmanager
+def fs_event_ctx(path, extensions):
+    '''watchdog context manager wrapper. Return filesystem event iterator'''
+    class EventHandler(PatternMatchingEventHandler):
+        '''Add fs event iterator property to PatternMatchingEventHandler'''
+        def __init__(self, *args, **kwargs):
+            super(EventHandler, self).__init__(*args, **kwargs)
+            self.fs_queue = Queue()
+
+        def on_any_event(self, event):
+            self.fs_queue.put(Ret(event.src_path, ev_name=event.event_type))
+
+        @property
+        def fs_event(self):
+            while True:
+                yield self.fs_queue.get()
+
+    patterns = ['*.{}'.format(p) for p in extensions]
+    evh = EventHandler(patterns=patterns, ignore_directories=True)
+    observer = Observer()
+    observer.schedule(evh, path, recursive=True)
+    observer.start()
+    try:
+        yield evh.fs_event
+    except StopIteration:
+        pass
+    observer.stop()
+    del observer
+    del evh
+
+
+@contextmanager
+def capture_streams():
+    r'''Capture streams (stdout & stderr) in a string
+    >>> with capture_streams() as streams:
+    ...     print('Hi there')
+    >>> streams.getvalue()
+    'Hi there\n'
+    '''
+    stdout = sys.stdout
+    stderr = sys.stderr
+    data = cStringIO()
+    sys.stdout = data
+    sys.stderr = data
+    yield data
+    sys.stdout = stdout
+    sys.stderr = stderr
+    data.flush()
+
+
+class Timeout(gevent.Timeout):
+    '''Add expired attribute to Timeout context manager'''
+    def __init__(self, *args, **kwargs):
+        super(Timeout, self).__init__(*args, **kwargs)
+        self.expired = False
+
+    def __exit__(self, typ, value, tb):
+        self.cancel()
+        if value is self and self.exception is False:
+            self.expired = True
+            return True
+
+
+def check_host(host, port=22, timeout=1, recv=False):
+    '''Return True if socket is active. timeout in seconds.
+    Use recv=False if socket is silent after connection
+    '''
+    with Timeout(timeout) as ctx:
+        while True:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            try:
+                sock.connect((host, int(port)))
+                if not recv or sock.recv(1024):
+                    break
+                sleep(0.01)
+            except socket.error:
+                sleep(1)
+            except (socket.timeout, OSError):
+                pass
+            finally:
+                sock.close()
+    return not ctx.expired
