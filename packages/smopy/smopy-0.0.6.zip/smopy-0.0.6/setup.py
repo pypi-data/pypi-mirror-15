3799d4f1e3a65c539d80d0776e76c0a774d2be71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:50 +0000

    Add package smopy/0.0.6/smopy-0.0.6.zip

---
+++ packages/smopy/smopy-0.0.6.zip/smopy-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="smopy",
+    version='0.0.6',
+    author="Cyrille Rossant",
+    author_email="rossant@github",
+    description="OpenStreetMap image tiles in Python",
+    license="BSD",
+    keywords="openstreetmap matplotlib map maps ipython",
+    url="http://packages.python.org/smopy",
+    py_modules=['smopy'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Framework :: IPython",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=['numpy', 'ipython', 'pillow', 'six', 'matplotlib'],
+)
