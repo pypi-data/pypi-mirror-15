56db317d7ac43279e9e5b764bc362b78da82c00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:47 +0000

    Add package pyscraper-framework/0.0.29/pyscraper_framework-0.0.29.tar.gz

---
+++ packages/pyscraper-framework/pyscraper_framework-0.0.29.tar.gz/pyscraper_framework-0.0.29/pyscraper_framework/lib/extractor.py
@@ -0,0 +1,8 @@
+import os
+
+class Extractor():
+    def __init__(self):
+        print("Extractor::init() - the base class.")
+
+    def execute(self):
+        print("Extractor::execute() - the base class.")
---
+++ packages/pyscraper-framework/pyscraper_framework-0.0.29.tar.gz/pyscraper_framework-0.0.29/pyscraper_framework/lib/loader.py
@@ -0,0 +1,8 @@
+import os
+
+class Loader():
+    def __init__(self):
+        print("Loader::init() - the base class.")
+
+    def execute(self):
+        print("Loader::execute() - the base class.")
