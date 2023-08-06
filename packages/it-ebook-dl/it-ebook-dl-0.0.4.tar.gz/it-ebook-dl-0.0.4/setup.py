88e83e8a718e881268534679e734873115ec8276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:24 +0000

    Add package it-ebook-dl/0.0.4/it-ebook-dl-0.0.4.tar.gz

---
+++ packages/it-ebook-dl/it-ebook-dl-0.0.4.tar.gz/it-ebook-dl-0.0.4/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name':'it-ebook-dl',
+    'version':'0.0.4',
+    'py_modules':['get_pdfs'],
+    'install_requires':[
+         'mechanize==0.2.5',
+         'twill==0.9.1',
+    ],
+    'entry_points':{
+        'console_scripts':
+            ['find-pdfs = get_pdfs:main']
+    },
+}
+
+setup(**config)
