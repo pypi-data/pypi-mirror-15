c4ec27bb625a149353d90fe8eb0406b75f672937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:07 +0000

    Add package pytest123/0.0.1/pytest123-0.0.1.tar.gz

---
+++ packages/pytest123/pytest123-0.0.1.tar.gz/pytest123-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='pytest123',
+      version='0.0.1',
+      description='Test Library',
+      author = 'rboyapat',
+      author_email = 'rboyapat@gmail.com',
+      license='MIT',
+      packages=['pytest123'],
+      zip_safe=False)
