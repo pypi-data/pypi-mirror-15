645a5f472e758ff63a16450b0bfe687a4dab4d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:01 +0000

    Add package ciffmbd2016mcegln/13.0.3/ciffmbd2016mcegln-13.0.3.zip

---
+++ packages/ciffmbd2016mcegln/ciffmbd2016mcegln-13.0.3.zip/ciffmbd2016mcegln-13.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ciffmbd2016mcegln',
+      version='13.0.3',  # Development release
+      description='Autopredict libaries from mat4os',
+      url='https://github.com/mat4os/autopredict',
+      author='mat4os',
+      author_email='mat4os@gmx.es',
+      license='MIT',
+      packages=['ciffmbd2016mcegln'],
+      zip_safe=False)
