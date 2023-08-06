a83e4af9694c0949a85f75ef5db962bbd216c100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:07 +0000

    Add package graphql-core/0.5.2/graphql-core-0.5.2.tar.gz

---
+++ packages/graphql-core/graphql-core-0.5.2.tar.gz/graphql-core-0.5.2/graphql/execution/executors/asyncio.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+from asyncio import Future, ensure_future, get_event_loop, iscoroutine, wait
+
+
+class AsyncioExecutor(object):
+
+    def __init__(self, loop=None):
+        if loop is None:
+            loop = get_event_loop()
+        self.loop = loop
+        self.futures = []
+
+    def wait_until_finished(self):
+        # if there are futures to wait for
+        if self.futures:
+            # wait for the futures to finish
+            self.loop.run_until_complete(wait(self.futures))
+
+    def execute(self, fn, *args, **kwargs):
+        result = fn(*args, **kwargs)
+        if isinstance(result, Future) or iscoroutine(result):
+            future = ensure_future(result)
+            self.futures.append(future)
+            return future
+        return result
