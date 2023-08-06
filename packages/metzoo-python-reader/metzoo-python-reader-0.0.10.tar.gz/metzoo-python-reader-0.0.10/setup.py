1b2624aa4b2a945aaa18ea751b88a680501d49cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:21 +0000

    Add package metzoo-python-reader/0.0.10/metzoo-python-reader-0.0.10.tar.gz

---
+++ packages/metzoo-python-reader/metzoo-python-reader-0.0.10.tar.gz/metzoo-python-reader-0.0.10/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+config = {
+    'name': 'metzoo-python-reader',
+    'version': '0.0.10',
+    'description': 'Python Data Reader for Metzoo',
+    'long_description': readme(),
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    'entry_points': {
+        'console_scripts': [
+            'metzoo-reader = metzoo.reader.reader:main'
+        ]
+    },
+    'data_files': [
+        ('metzoo-python-reader/config', ['config-template.yaml'])
+    ],
+    'keywords': 'metzoo monitoring metric',
+    'url': 'https://bitbucket.org/edrans/metzoo-python-reader',
+    'author': 'Edrans',
+    'author_email': 'info@edrans.com',
+    'license': 'MIT',
+    'packages': ['metzoo', 'metzoo.reader'],
+    'install_requires': ['metzoo-python-dblib'],
+    'zip_safe': False
+}
+
+setup(**config)
