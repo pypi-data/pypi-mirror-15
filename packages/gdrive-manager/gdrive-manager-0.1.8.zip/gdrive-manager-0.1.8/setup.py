ef12924732e979ede930027c1f64f2bd19e7dbf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:17 +0000

    Add package gdrive-manager/0.1.8/gdrive-manager-0.1.8.zip

---
+++ packages/gdrive-manager/gdrive-manager-0.1.8.zip/gdrive-manager-0.1.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+REQUIRES = [
+	"google-api-python-client==1.5.1",
+	"oauth2client==2.2.0"
+]
+
+
+setup (
+	name='gdrive-manager',
+	version='0.1.8',
+	author='Bao Nguyen',
+	author_email='nlbao95@gmail.com',
+	description='Download / upload data using Google Drive APIs.',
+	url='https://github.com/nlbao/gdrive-manager',
+	license='MIT',
+	packages=[
+		'bin',
+	],
+	data_files=[('bin', ['bin/*.json'])],
+	entry_points = {"console_scripts" : [ "gdrive-manager = bin.gdrive_manager:main"]},
+	install_requires=REQUIRES,
+)
