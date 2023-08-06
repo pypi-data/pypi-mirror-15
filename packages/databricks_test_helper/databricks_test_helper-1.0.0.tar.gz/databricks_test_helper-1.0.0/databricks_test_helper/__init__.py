1afed3bdff0f621a2b5fe8df907678bd9fa6027c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:28 +0000

    Add package databricks_test_helper/1.0.0/databricks_test_helper-1.0.0.tar.gz

---
+++ packages/databricks_test_helper/databricks_test_helper-1.0.0.tar.gz/databricks_test_helper-1.0.0/databricks_test_helper/__init__.py
@@ -0,0 +1,1 @@
+from databricks_test_helper import Test
---
+++ packages/databricks_test_helper/databricks_test_helper-1.0.0.tar.gz/databricks_test_helper-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'databricks_test_helper',
+  packages = ['databricks_test_helper'],
+  version = '1.0.0',
+  description = 'A testing helper for Apache Spark Databricks notebooks',
+  author = 'Daniel Liu and contributors from Databricks',
+  author_email = 'training-logins@databricks.com',
+  keywords = ['testing', 'spark', 'databricks'],
+  classifiers = [],
+)
