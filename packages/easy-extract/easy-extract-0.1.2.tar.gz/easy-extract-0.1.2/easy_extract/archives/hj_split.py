9b19c3c367d624399594b973ceadba9ad445a2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:04 +0000

    Add package easy-extract/0.1.2/easy-extract-0.1.2.tar.gz

---
+++ packages/easy-extract/easy-extract-0.1.2.tar.gz/easy-extract-0.1.2/easy_extract/archives/hj_split.py
@@ -0,0 +1,30 @@
+"""HJ Split archive format"""
+import os
+import re
+
+from easy_extract.archive import Archive
+
+EXTENSIONS = [re.compile('\.\d{3}$', re.I)]
+
+
+class HJSplitArchive(Archive):
+    """The HJ Split format"""
+    archive_type = 'hj-split'
+    ALLOWED_EXTENSIONS = EXTENSIONS
+
+    def _extract(self):
+        new_filename = self.escape_filename(self.name)
+        first_archive = self.get_command_filename(self.archives[0])
+
+        print 'Extracting %s...' % new_filename
+
+        os.system('cat %s > %s' % (first_archive, new_filename))
+
+        for archive in self.archives[1:]:
+            archive = self.get_command_filename(archive)
+            os.system('cat %s >> %s' % (archive, new_filename))
+
+        return True
+
+    def remove(self):
+        pass
---
+++ packages/easy-extract/easy-extract-0.1.2.tar.gz/easy-extract-0.1.2/setup.py
@@ -0,0 +1,41 @@
+"""Installing easy-extract"""
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+import easy_extract
+
+setup(
+    name='easy-extract',
+    version=easy_extract.__version__,
+    zip_safe=False,
+
+
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+
+    author=easy_extract.__author__,
+    author_email=easy_extract.__email__,
+    url=easy_extract.__url__,
+
+    license=easy_extract.__license__,
+    platforms='any',
+    description='Easy extraction of archives collections',
+    long_description=open(os.path.join('README.rst')).read(),
+    keywords='extract, rar, zip, xtm, hjsplit',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        ],
+
+    entry_points={
+        'console_scripts': [
+            'easy-extract=easy_extract.scripts.extract:cmdline',
+            'easy-index=easy_extract.scripts.index:cmdline'
+        ]
+    },
+    test_suite='tests.global_test_suite',
+)
