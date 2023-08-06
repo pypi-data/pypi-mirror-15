36b60300e2536e1dd09f966c2c7159322bfc78af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:44 +0000

    Add package my-awesome-rapsel/0.6.2/my-awesome-rapsel-0.6.2.tar.gz

---
+++ packages/my-awesome-rapsel/my-awesome-rapsel-0.6.2.tar.gz/my-awesome-rapsel-0.6.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='my-awesome-rapsel',  
+    version='0.6.2',
+    py_modules=['first'],
+    author='Syam Sasi',
+    author_email='syamsasi99@gmail.com'
+)
