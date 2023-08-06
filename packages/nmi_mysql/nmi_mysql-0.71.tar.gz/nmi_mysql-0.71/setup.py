cbe5f6416429a74aa005bf742ff41da9258cba35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:24 +0000

    Add package nmi_mysql/0.71/nmi_mysql-0.71.tar.gz

---
+++ packages/nmi_mysql/nmi_mysql-0.71.tar.gz/nmi_mysql-0.71/setup.py
@@ -0,0 +1,16 @@
+""" Setup file
+"""
+from distutils.core import setup
+
+
+setup(
+    name='nmi_mysql',
+    packages=['nmi_mysql'],
+    version='0.71',
+    description='A simple mysql client wrapper for sqlalchemy',
+    author='Ninz, Jasper, Jv, Sherwin',
+    author_email='ninz.xp@gmail.com',
+    url='https://github.com/pprmint/nmi_mysql',
+    keywords=['mysql', 'client', 'wrapper', 'library', 'sqlalchemy', 'pymysql'],
+    classifiers=[],
+)
