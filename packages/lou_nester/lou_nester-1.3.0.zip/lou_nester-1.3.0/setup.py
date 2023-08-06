a87eedfc2942d8cf0669ac3e83ecf3d522c894cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:30 +0000

    Add package lou_nester/1.3.0/lou_nester-1.3.0.zip

---
+++ packages/lou_nester/lou_nester-1.3.0.zip/lou_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'lou_nester',
+        version         = '1.3.0',
+        py_modules      = ['nester'],
+        author          = 'xiaohai',
+        author_email    = 'loudapingnb@163.com',
+        url             = 'http://blog.csdn.net/xiaohaijiejie?viewmode=contents',
+        description     = 'A simple printer of nested lists',
+     )
