66069cefaf50c4773cebe1b4131c43faff81f2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:51 +0000

    Add package tm_nester/1.0.0/tm_nester-1.0.0.zip

---
+++ packages/tm_nester/tm_nester-1.0.0.zip/tm_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'tm_nester',
+        version = '1.0.0',
+        py_modules = ['nester'],
+        author = 'th_matt',
+        author_email = 'binary_0111@yahoo.co.in',
+        url = 'http://www.google.com',
+        description = 'A simple printer of nested lists',
+    )
