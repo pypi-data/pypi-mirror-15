811f6802fb6955a4a7e85239614be6088a50a741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:24 +0000

    Add package cogent/1.9/cogent-1.9.tar.gz

---
+++ packages/cogent/cogent-1.9.tar.gz/cogent-1.9/cogent/maths/spatial/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+__all__ = ['ckd3']
+
+__author__ = ""
+__copyright__ = "Copyright 2007-2016, The Cogent Project"
+__credits__ = ["Marcin Cieslik"]
+__license__ = "GPL"
+__version__ = "1.9"
+__maintainer__ = "Marcin Cieslik"
+__email__ = "mpc4p@virginia.edu"
+__status__ = "Production"
