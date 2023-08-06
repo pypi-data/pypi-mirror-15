f6fa5dde72cf67d8567425938e753514861c9dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:27 +0000

    Add package drive-casa/0.7.5/drive-casa-0.7.5.tar.gz

---
+++ packages/drive-casa/drive-casa-0.7.5.tar.gz/drive-casa-0.7.5/drivecasa/commands/subroutines.py
@@ -0,0 +1,23 @@
+
+# Load the columns from an antenna-list config file into lists.
+def_load_antennalist = """
+def drivecasa_load_antennalist(antennalist_path):
+    with open(antennalist_path, 'r') as f:
+        x = [];
+        y = [];
+        z = [];
+        d = []
+        lines = f.readlines()
+        for l in lines:
+            items = l.split()
+            if (items[0] != '#'):
+                x.append(float(items[0]))
+                y.append(float(items[1]))
+                z.append(float(items[2]))
+                d.append(float(items[3]))
+    return x, y, z, d
+    """
+
+all_subroutines = (
+    def_load_antennalist,
+)
