3ddeea8520d1d88adb9887afcddd292329005b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:57 +0000

    Add package tsuru_dashboard/0.3.0rc2/tsuru_dashboard-0.3.0rc2.tar.gz

---
+++ packages/tsuru_dashboard/tsuru_dashboard-0.3.0rc2.tar.gz/tsuru_dashboard-0.3.0rc2/tsuru_dashboard/admin/tests/test_node_add_view.py
@@ -0,0 +1,65 @@
+from mock import patch
+
+from django.test import TestCase
+from django.test.client import RequestFactory
+
+import httpretty
+
+from tsuru_dashboard import settings
+from tsuru_dashboard.admin.views import NodeAdd
+
+import json
+
+
+class NodeAddViewTest(TestCase):
+    def setUp(self):
+        httpretty.enable()
+
+    def tearDown(self):
+        httpretty.disable()
+        httpretty.reset()
+
+    @patch("tsuru_dashboard.auth.views.token_is_valid")
+    def test_view_register_false(self, token_is_valid):
+        token_is_valid.return_value = True
+
+        factory = RequestFactory()
+        request = factory.post('/', data={"key": "value", "register": "false"})
+        request.session = {'tsuru_token': 'tokentest'}
+
+        url = "{}/docker/node".format(settings.TSURU_HOST)
+        httpretty.register_uri(
+            httpretty.POST,
+            url,
+            body=json.dumps({"Message": "", "Error": ""}),
+            status=200
+        )
+
+        response = NodeAdd.as_view()(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("register", httpretty.last_request().body)
+        self.assertIn("false", httpretty.last_request().body)
+
+    @patch("tsuru_dashboard.auth.views.token_is_valid")
+    def test_view_register_true(self, token_is_valid):
+        token_is_valid.return_value = True
+
+        factory = RequestFactory()
+        request = factory.post('/', data={"key": "value", "register": "true"})
+        request.session = {'tsuru_token': 'tokentest'}
+
+        url = "{}/docker/node".format(settings.TSURU_HOST)
+
+        httpretty.register_uri(
+            httpretty.POST,
+            url,
+            body=json.dumps({"Message": "", "Error": ""}),
+            status=200
+        )
+
+        response = NodeAdd.as_view()(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("register", httpretty.last_request().body)
+        self.assertIn("true", httpretty.last_request().body)
