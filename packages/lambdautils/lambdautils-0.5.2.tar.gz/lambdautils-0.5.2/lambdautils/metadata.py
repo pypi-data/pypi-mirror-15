34c59976cb66e5eade4c7fa621a616205cecef5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:19 +0000

    Add package lambdautils/0.5.2/lambdautils-0.5.2.tar.gz

---
+++ packages/lambdautils/lambdautils-0.5.2.tar.gz/lambdautils-0.5.2/lambdautils/metadata.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Project metadata."""
+
+package = "lambdautils"
+project = "lambdautils"
+version = '0.5.2'
+description = "Simple utilities for AWS Lambda functions"
+authors = ["Innovative Travel Ltd"]
+authors_string = ', '.join(authors)
+emails = ['german@innovativetravel.eu']
+license = 'MIT'
+copyright = '2016 ' + authors_string
+url = 'http://github.com/humilis/humilis-lambdautils'
