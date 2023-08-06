7f4cc7a882315d1a2b47419c1ff898d9091a12f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:58 +0000

    Add package pipeables/0.1.42/pipeables-0.1.42.tar.gz

---
+++ packages/pipeables/pipeables-0.1.42.tar.gz/pipeables-0.1.42/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='pipeables',
+      version='0.1.42',
+      description='Simiple queries and piping between different data sources',
+      url='https://bitbucket.org/tdoran/pipeables/branch/v2package',
+      author='Tony Doran',
+      author_email='tdoran@atlassian.com',
+      license='MIT',
+      packages=['pipeables'],
+      entry_points={
+          'console_scripts': [
+              'pipeable=pipeables.command_line:pipeable',
+              'makeList=pipeables.command_line:makeList',
+              'makeValueList=pipeables.command_line:makeValueList',
+              'intersection=pipeables.command_line:intersection'
+          ]
+      },
+      install_requires=[
+          'gdata',
+          'pygresql==4.1.1',
+          'pyhive==0.1.8',
+          'googleads==3.15.0',
+          'requests'
+      ],
+      zip_safe=False)
