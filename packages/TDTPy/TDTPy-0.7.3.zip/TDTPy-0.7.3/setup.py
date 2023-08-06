12809bde8517dc3413ccc9b3e1d28521098e0575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:44 +0000

    Add package TDTPy/0.7.3/TDTPy-0.7.3.zip

---
+++ packages/TDTPy/TDTPy-0.7.3.zip/TDTPy-0.7.3/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+from os import path
+import numpy  # noqa; a requirement, but *don't* have pip try to install it
+try:
+    import setuptools  # noqa, setuptools namespace
+except Exception:
+    pass
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: System :: Hardware',
+    'License :: OSI Approved'
+]
+
+here = path.dirname(path.abspath(__file__))
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read().strip()
+
+long_description += '''
+
+Source code: http://github.com/LABSN/tdtpy
+
+Documentation: http://tdtpy.readthedocs.org
+
+'''
+
+setup(
+    name='TDTPy',
+    version='0.7.3',
+    author='Brad Buran',
+    author_email='bburan@alum.mit.edu',
+    packages=['tdt',
+              'tdt.actxobjects',
+              'tdt.components',
+              'tdt.device'],
+    url='http://tdtpy.readthedocs.org',
+    license='BSD (3-clause)',
+    description='Module for communicating with TDT\'s System 3 hardware',
+    long_description=long_description,
+    install_requires=['six', 'pypiwin32'],
+    package_data={'tdt': ['components/*.rcx']},
+    classifiers=CLASSIFIERS,
+)
