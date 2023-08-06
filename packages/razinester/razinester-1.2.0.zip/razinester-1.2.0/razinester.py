dc9aa29ea03251e4c8e17a121219444e42b7a26e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:58 +0000

    Add package razinester/1.2.0/razinester-1.2.0.zip

---
+++ packages/razinester/razinester-1.2.0.zip/razinester-1.2.0/razinester.py
@@ -0,0 +1,12 @@
+"""nester
+i am a nester"""
+
+def print_lol_test(the_list, level=0):
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        print_lol_test(each_item, level+1)
+                else:
+                        for tab_stop in range(level):
+                                print("    ", end="")
+                        print(each_item)
+
