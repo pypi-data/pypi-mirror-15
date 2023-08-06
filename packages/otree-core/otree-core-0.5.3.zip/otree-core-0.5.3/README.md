ed52eab8395aad4f7ecfa4dcc4c4e924343d3ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:37 +0000

    Add package otree-core/0.5.3/otree-core-0.5.3.zip

---
+++ packages/otree-core/otree-core-0.5.3.zip/otree-core-0.5.3/README.md
@@ -0,0 +1,7 @@
+[Homepage](http://www.otree.org/)
+
+These are the core oTree libraries.
+
+Install this with `pip install -e .`.
+
+[![Build Status](https://travis-ci.org/oTree-org/otree-core.svg?branch=master)](https://travis-ci.org/oTree-org/otree-core)
---
+++ packages/otree-core/otree-core-0.5.3.zip/otree-core-0.5.3/otree/middleware.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+from django.http import HttpResponseServerError
+
+from otree import common_internal
+
+
