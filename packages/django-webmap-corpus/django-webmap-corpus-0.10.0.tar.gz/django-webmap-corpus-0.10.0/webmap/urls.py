e5730d94626ed6060338c705b24bc8bd955aa752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:44 +0000

    Add package django-webmap-corpus/0.10.0/django-webmap-corpus-0.10.0.tar.gz

---
+++ packages/django-webmap-corpus/django-webmap-corpus-0.10.0.tar.gz/django-webmap-corpus-0.10.0/webmap/urls.py
@@ -0,0 +1,15 @@
+"""URLs for the webmap app."""
+from django.conf.urls import include, url
+from webmap import views
+from .rest import router
+from django.conf import settings
+
+
+urlpatterns = [
+    url(r'^kml/([-\w]+)/$', views.kml_view),
+    url(r'^search/([- \w]+)/$', views.search_view),
+]
+
+if getattr(settings, 'REST_ENABLED', False):
+    urlpatterns.append(url(r'^', include(router.urls)))
+    urlpatterns.append(url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')))
