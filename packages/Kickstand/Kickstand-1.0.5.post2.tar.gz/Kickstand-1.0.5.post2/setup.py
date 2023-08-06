4fa426e917d38a2d0be541eacfd657fe799c8a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:06 +0000

    Add package Kickstand/1.0.5.post2/Kickstand-1.0.5.post2.tar.gz

---
+++ packages/Kickstand/Kickstand-1.0.5.post2.tar.gz/Kickstand-1.0.5.post2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+__license__ = 'Copyright (c) 2015, Purdue University'
+__version__ = '1.0.5.post2'
+
+try:
+  from setuptools import setup
+except ImportError:
+  from disutils.core import setup
+
+setup(
+    name             = 'Kickstand',
+    version          = __version__,
+    description      = 'Machine inventory management system client API',
+    long_description = 'An API for interacting with the Kickstand server, which hosts a machine '\
+        'inventory database. This API is used to develop CLI tools that talk to the REST API '\
+        'hosted by the Kickstand server.',
+    author           = 'Seth Cook',
+    author_email     = 'sethcook@purdue.edu',
+    url              = 'http://www.rcac.purdue.edu',
+    packages         = [ 'kickstand', 'kickstand.inventory' ],
+    license          = __license__,
+    classifiers      = [ 'Programming Language :: Python :: 2.6',
+                         'Programming Language :: Python :: 2.7',
+                         'Operating System :: Unix',
+                         'Development Status :: 4 - Beta', ],
+    scripts          = [ 'scripts/kickstand' ],
+    data_files       = [ ('/opt/kickstand/inspections/os', ['inspections/os/structure.yaml',
+                                                            'inspections/os/inspect']),
+                         ('/opt/kickstand/inspections/memory', ['inspections/memory/structure.yaml',
+                                                                'inspections/memory/inspect']),
+                         ('/usr/local/kickstand/inspections', []),
+                       ],
+    install_requires = [ 'ConfigArgParse >= 0.10.0',
+                         'requests >= 1.1.0' ],
+)
