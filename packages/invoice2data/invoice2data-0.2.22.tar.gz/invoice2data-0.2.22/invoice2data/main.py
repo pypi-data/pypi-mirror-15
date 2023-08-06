ba7b72e1807f01c60bdd8802ebf57ccddd4c751a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:10 +0000

    Add package invoice2data/0.2.22/invoice2data-0.2.22.tar.gz

---
+++ packages/invoice2data/invoice2data-0.2.22.tar.gz/invoice2data-0.2.22/invoice2data/main.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import shutil
+import re
+from os import listdir
+from os.path import isfile, isdir, join
+import locale
+import pkg_resources
+import invoice2data.pdftotext as pdftotext
+import invoice2data.image_to_text as image_to_text
+from invoice2data.template import InvoiceTemplate, read_templates
+from invoice2data.output import invoices_to_csv
+import logging
+from unidecode import unidecode
+
+logger = logging.getLogger(__name__)
+
+FILENAME = "{date} {desc}.pdf"
+
+def extract_data(invoicefile, templates=None, debug=False):
+    if templates is None:
+        templates = read_templates(
+            pkg_resources.resource_filename('invoice2data', 'templates'))
+    
+    extracted_str = pdftotext.to_text(invoicefile).decode('utf-8')
+
+    charcount = len(extracted_str)
+    logger.debug('number of char in pdf2text extract: %d', charcount)
+    # Disable OCR for now.
+    #if charcount < 40:
+        #logger.info('Starting OCR')
+        #extracted_str = image_to_text.to_text(invoicefile)
+    logger.debug('START pdftotext result ===========================')
+    logger.debug(extracted_str)
+    logger.debug('END pdftotext result =============================')
+
+    logger.debug('Testing {} template files'.format(len(templates)))
+    for t in templates:
+        optimized_str = t.prepare_input(extracted_str)
+
+        if t.matches_input(optimized_str):
+            return t.extract(optimized_str)
+
+    logger.error('No template for %s', invoicefile)
+    return False
+
+def main():
+    "Take folder or single file and analyze each."
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+
+    parser.add_argument('--debug', dest='debug', action='store_true',
+                        help='Print debug information.')
+
+    parser.add_argument('--copy', '-c', dest='copy',
+                        help='Copy renamed PDFs to specified folder.')
+
+    parser.add_argument('--template-folder', '-t', dest='template_folder',
+                        default=pkg_resources.resource_filename('invoice2data', 'templates'),
+                        help='Folder containing invoice templates in yml file. Required.')
+
+    parser.add_argument('input_files', type=argparse.FileType('r'), nargs='+',
+                        help='File or directory to analyze.')
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    output = []
+    templates = read_templates(args.template_folder)
+    for f in args.input_files:
+        res = extract_data(f.name, templates=templates)
+        if res:
+            logger.info(res)
+            output.append(res)
+            if args.copy:
+                filename = FILENAME.format(
+                    date=res['date'].strftime('%Y-%m-%d'),
+                    desc=res['desc'])
+                shutil.copyfile(f.name, join(args.copy, filename))
+    invoices_to_csv(output, 'invoices-output.csv')
+
+if __name__ == '__main__':
+    main()
+
