cebf878ea613ec91d73a2586c167603786e36de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:55 +0000

    Add package heboan/1.0.0/heboan-1.0.0.zip

---
+++ packages/heboan/heboan-1.0.0.zip/heboan-1.0.0/heboan.py
@@ -0,0 +1,20 @@
+﻿'''This is the "nester.py" module and it provides on function called print_lol()
+which prints lists that may or may not include nested lists.'''
+
+movies = [
+"The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+   ["Graham Chapman",
+        ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle"]]]
+
+def print_lol(the_list):
+    """This function takes one positional argument called "the_list",which is any
+    Python list(of -possibly - nested lists). Each data item in the provided list is 
+    printed to screen on it's own line."""
+
+    for each_item in the_list:
+        if isinstance(print_lol,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
+print_lol(movies)
---
+++ packages/heboan/heboan-1.0.0.zip/heboan-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name             = 'heboan',
+    version          = '1.0.0',
+    py_modules       = ['heboan'],
+    author           = 'heboan',
+    author_email     = 'heboan@qq.com',
+    url              = 'http://cnblogs.com/sellsa',
+    description      = 'A simple printer of nested lists',
+    )
