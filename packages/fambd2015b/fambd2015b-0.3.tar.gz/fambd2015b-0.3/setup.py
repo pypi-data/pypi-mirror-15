f3a84bf04c13907ffd11961ff19cd704cf42dc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:01 +0000

    Add package fambd2015b/0.3/fambd2015b-0.3.tar.gz

---
+++ packages/fambd2015b/fambd2015b-0.3.tar.gz/fambd2015b-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fambd2015b',
+      version='0.3',
+      description='Python package for Financial Analytics!',
+      url='https://github.com/my13/fa_mbd_2015b',
+      author='TEAM B - Master Big Data',
+      author_email='jldelgadodavara@student.ie.edu',
+      license='MIT',
+      packages=['fambd2015b'],
+      zip_safe=False)
