92a45cf6c8ba1631c4a46dbd2a744b658255f9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:09 +0000

    Add package cgpio/0.5/cgpio-0.5.zip

---
+++ packages/cgpio/cgpio-0.5.zip/cgpio-0.5/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'cgpio',
+    version = '0.5',
+    keywords = ('gpio', 'class'),
+    description = 'a gpio class',
+    license = 'MIT License',
+    install_requires = [],
+
+    author = 'caoxp',
+    author_email = 'fpdz2010@qq.com',
+	url='http://fpdz.taobao.com',
+    
+    packages = find_packages(),
+    platforms = 'any',
+)
+
