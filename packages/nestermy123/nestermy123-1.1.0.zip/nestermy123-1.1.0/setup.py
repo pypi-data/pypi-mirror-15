39e7c92d812c8570721878f78d885cde625abb37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:05 +0000

    Add package nestermy123/1.1.0/nestermy123-1.1.0.zip

---
+++ packages/nestermy123/nestermy123-1.1.0.zip/nestermy123-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name		='nestermy123',
+	version		='1.1.0',
+	py_modules	=['nestermy123'],
+	author		='qewryt132465',
+	author_email	='qewryt132465@163.com',
+	url		='http://www.headfirstlab.com',
+	description	='A simple printer pf nested lists',
+	)
