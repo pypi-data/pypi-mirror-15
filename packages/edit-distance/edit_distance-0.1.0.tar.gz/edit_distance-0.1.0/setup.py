8431f8a106cae8de78d3d763316f4785e6b86e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:06 +0000

    Add package edit-distance/0.1.0/edit_distance-0.1.0.tar.gz

---
+++ packages/edit-distance/edit_distance-0.1.0.tar.gz/edit_distance-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='edit_distance',
+    version='0.1.0',
+    author='Ben Lambert',
+    author_email='ben@benjaminlambert.com',
+    packages=['editdistance'],
+    license='LICENSE.txt',
+    description='Computing edit distance on arbitrary Python sequences.',
+    long_description=open('README.md').read(),
+    url='https://github.com/belambert/editdistance',
+    download_url = 'https://github.com/belambert/editdistance/tarball/0.1', 
+    keywords=['edit', 'distance', 'levenshtein'],
+    classifiers = [],
+)
