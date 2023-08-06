2f0efaedc5ec9af599b37d12820b831c4d984a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:13 +0000

    Add package testbakflow/0.0.0/testbakflow-0.0.0.zip

---
+++ packages/testbakflow/testbakflow-0.0.0.zip/testbakflow-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        ='testbakflow',
+        verdion     ='1.1.0',
+        py_modules =['testbackflow'],
+        author      ='backflow',
+        author_email = '16671161414@qq.com',
+        url         ='',
+        description ='A simple printer of nested lists',
+    )
---
+++ packages/testbakflow/testbakflow-0.0.0.zip/testbakflow-0.0.0/testbackflow.py
@@ -0,0 +1,9 @@
+"为了对打包模块进行测试"
+def outall(k_list,times=0):
+    for each_item in k_list:
+        if isinstance(each_item,list):
+            outall(each_item,times+1)
+        else:
+            for table_k in range(times):
+                print("\t",end='')
+            print(each_item)
