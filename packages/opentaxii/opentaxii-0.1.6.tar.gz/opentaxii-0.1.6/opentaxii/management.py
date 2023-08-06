5e37c61b05d508746a451ce567e633c2ab038971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:45 +0000

    Add package opentaxii/0.1.6/opentaxii-0.1.6.tar.gz

---
+++ packages/opentaxii/opentaxii-0.1.6.tar.gz/opentaxii-0.1.6/opentaxii/management.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request, jsonify, abort, current_app
+
+management = Blueprint('management', __name__)
+
+
+@management.route('/auth', methods=['POST'])
+def auth():
+    data = request.get_json() or request.form
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return 'Both username and password are required', 400
+
+    token = current_app.taxii_server.auth.authenticate(username, password)
+
+    if not token:
+        abort(401)
+
+    return jsonify(token=token.decode('utf-8'))
+
+
+@management.route('/health', methods=['GET'])
+def health():
+    return jsonify(alive=True)
